e8d48641de6c5e866dc18708e2e260b7578c1d50
Jeff Lindsay <progrium@gmail.com>
2013-02-20 12:23:47 +0000

    white space

--- client/term.go
+++ client/term.go
@@ -123,11 +123,11 @@ func MakeRaw(fd int) (*State, error) {
         if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), uintptr(getTermios), uintptr(unsafe.Pointer(&oldState.termios)), 0, 0, 0); err != 0 {
                 return nil, err
         }
-	
+
         newState := oldState.termios
         newState.Iflag &^= ISTRIP | INLCR | IGNCR | IXON | IXOFF
-	newState.Iflag |= ICRNL
-	newState.Oflag |= ONLCR
+        newState.Iflag |= ICRNL
+        newState.Oflag |= ONLCR
         newState.Lflag &^= ECHO | ICANON | ISIG
         if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), uintptr(setTermios), uintptr(unsafe.Pointer(&newState)), 0, 0, 0); err != 0 {
                 return nil, err
