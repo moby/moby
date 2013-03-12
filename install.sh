c1fa4efad66a1ae8cbccd7c55bc195be03d94b2f
Jérôme Petazzoni <jp@enix.org>
2013-03-11 23:09:19 -0700

    Try to load, or install, AUFS modules
    When running precise or quantal, installing the matching -extra kernel
    will install the required file.

--- install.sh
+++ install.sh
@@ -2,8 +2,34 @@
 # This script is meant for quick & easy install via 'curl URL-OF-SCRIPT | bash'
 # Courtesy of Jeff Lindsay <progrium@gmail.com>
 
-echo "Ensuring dependencies are installed..."
-apt-get --yes install lxc wget bsdtar 2>&1 > /dev/null
+echo "Ensuring basic dependencies are installed..."
+apt-get -qq update
+apt-get -qq install lxc wget bsdtar
+
+echo "Looking in /proc/filesystems to see if we have AUFS support..."
+if grep -q aufs /proc/filesystems
+then
+    echo "Found."
+else
+    echo "Ahem, it looks like the current kernel does not support AUFS."
+    echo "Let's see if we can load the AUFS module with modprobe..."
+    if modprobe aufs
+    then
+        echo "Module loaded."
+    else
+        echo "Ahem, things didn't turn out as expected."
+        KPKG=linux-image-extra-$(uname -r)
+        echo "Trying to install $KPKG..."
+        if apt-get -qq install $KPKG
+        then
+            echo "Installed."
+        else
+            echo "Oops, we couldn't install the -extra kernel."
+            echo "Are you sure you are running a supported version of Ubuntu?"
+            echo "Proceeding anyway, but Docker will probably NOT WORK!"
+        fi
+    fi
+fi
 
 echo "Downloading docker binary and uncompressing into /usr/local/bin..."
 curl -s http://get.docker.io/builds/$(uname -s)/$(uname -m)/docker-master.tgz |
