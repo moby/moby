af2aac8736d71fe79492f9abf3a16fba97857eb4
Jérôme Petazzoni <jp@enix.org>
2013-03-11 23:07:16 -0700

    Stream and unpack the tarball on the fly
    We don't need temporary files.
    Also, get the tarball from get.docker.io instead of Dropbox.

--- install.sh
+++ install.sh
@@ -2,18 +2,13 @@
 # This script is meant for quick & easy install via 'curl URL-OF-SCRIPT | bash'
 # Courtesy of Jeff Lindsay <progrium@gmail.com>
 
-cd /tmp
-
 echo "Ensuring dependencies are installed..."
 apt-get --yes install lxc wget bsdtar 2>&1 > /dev/null
 
-echo "Downloading docker binary..."
-wget -q https://dl.dropbox.com/u/20637798/docker.tar.gz 2>&1 > /dev/null
-tar -xf docker.tar.gz 2>&1 > /dev/null
-
-echo "Installing into /usr/local/bin..."
-mv docker/docker /usr/local/bin
-mv dockerd/dockerd /usr/local/bin
+echo "Downloading docker binary and uncompressing into /usr/local/bin..."
+curl -s http://get.docker.io/builds/$(uname -s)/$(uname -m)/docker-master.tgz |
+tar -C /usr/local/bin --strip-components=1 -zxf- \
+docker-master/docker docker-master/dockerd
 
 if [[ -f /etc/init/dockerd.conf ]]
 then
@@ -25,11 +20,6 @@ fi
 
 echo "Restarting dockerd..."
 restart dockerd > /dev/null
-
-echo "Cleaning up..."
-rmdir docker
-rmdir dockerd
-rm docker.tar.gz
 
 echo "Finished!"
 echo
