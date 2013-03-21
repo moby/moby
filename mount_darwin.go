6f6eaca861fec53a6d21d420b813041dc93e8b93
Solomon Hykes <solomon@dotcloud.com>
2013-03-20 22:16:02 -0700

    Removed mount code from container. It belongs in graph

--- mount_darwin.go
+++
@@ -1,7 +0,0 @@
-package docker
-
-import "errors"
-
-func mount(source string, target string, fstype string, flags uintptr, data string) (err error) {
-	return errors.New("mount is not implemented on darwin")
-}
