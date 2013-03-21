deb603aaf41d6885e4b51c7ca06acfa940ad2a41
Solomon Hykes <solomon@dotcloud.com>
2013-03-21 01:10:44 -0700

    Removed unused utility future.Pv()

--- future/future.go
+++ future/future.go
@@ -1,9 +1,6 @@
 package future
 
-import (
-	"fmt"
-	"io"
-)
+import ()
 
 func Go(f func() error) chan error {
 	ch := make(chan error)
@@ -11,27 +8,4 @@ func Go(f func() error) chan error {
 		ch <- f()
 	}()
 	return ch
-}
-
-// Pv wraps an io.Reader such that it is passed through unchanged,
-// but logs the number of bytes copied (comparable to the unix command pv)
-func Pv(src io.Reader, info io.Writer) io.Reader {
-	var totalBytes int
-	data := make([]byte, 2048)
-	r, w := io.Pipe()
-	go func() {
-		for {
-			if n, err := src.Read(data); err != nil {
-				w.CloseWithError(err)
-				return
-			} else {
-				totalBytes += n
-				fmt.Fprintf(info, "--> %d bytes\n", totalBytes)
-				if _, err = w.Write(data[:n]); err != nil {
-					return
-				}
-			}
-		}
-	}()
-	return r
 }
