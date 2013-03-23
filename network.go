371225520fc86fa58fadd182730b958a0f7ef9ab
Sridhar Ratnakumar <sridharr@activestate.com>
2013-03-22 21:43:31 -0700

    handle errors during the creation of port mapper
    example:
    
      2013/03/22 21:42:55 Unable to setup port networking: Failed to create DOCKER chain
    
      (which was possibly introduced by commit 3c6b8bb88)

--- network.go
+++ network.go
@@ -348,6 +348,9 @@ func newNetworkManager(bridgeIface string) (*NetworkManager, error) {
 	}
 
 	portMapper, err := newPortMapper()
+	if err != nil {
+		return nil, err
+	}
 
 	manager := &NetworkManager{
 		bridgeIface:   bridgeIface,
