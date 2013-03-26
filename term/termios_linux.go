3cce89d8edc9c3151bd982c6b5acfa8c20d5ae71
shin- <joffrey@dotcloud.com>
2013-03-26 07:10:31 -0700

    Fixed issue with misplaced cursor after output

--- term/termios_linux.go
+++ term/termios_linux.go
@@ -15,7 +15,6 @@ void MakeRaw(int fd) {
   ioctl(fd, TCGETS, &t);
 
   t.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
-  t.c_oflag &= ~OPOST;
   t.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
   t.c_cflag &= ~(CSIZE | PARENB);
   t.c_cflag |= CS8;
