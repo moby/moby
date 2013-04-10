4099a31304863edf5b4d1b594b41c7ce1a33c5e1
Guillaume J. Charmes <guillaume.charmes@dotcloud.com>
2013-04-10 16:23:30 -0700

    Implement the -volumes-from in order to mount volumes from an other container

--- container.go
+++ container.go
@@ -68,6 +68,7 @@ type Config struct {
 	Dns          []string
 	Image        string // Name of the image as it was passed by the operator (eg. could be symbolic)
 	Volumes      map[string]struct{}
+	VolumesFrom  string
 }
 
 func ParseRun(args []string, stdout io.Writer, capabilities *Capabilities) (*Config, error) {
@@ -101,6 +102,8 @@ func ParseRun(args []string, stdout io.Writer, capabilities *Capabilities) (*Con
 
 	flVolumes := NewPathOpts()
 	cmd.Var(flVolumes, "v", "Attach a data volume")
+
+	flVolumesFrom := cmd.String("volumes-from", "", "Mount volumes from the specified container")
 
 	if err := cmd.Parse(args); err != nil {
 		return nil, err
@@ -142,6 +145,7 @@ func ParseRun(args []string, stdout io.Writer, capabilities *Capabilities) (*Con
 		Dns:          flDns,
 		Image:        image,
 		Volumes:      flVolumes,
+		VolumesFrom:  *flVolumesFrom,
 	}
 
 	if *flMemory > 0 && !capabilities.SwapLimit {
@@ -411,6 +415,22 @@ func (container *Container) Start() error {
 				return nil
 			}
 			container.Volumes[volPath] = c.Id
+		}
+	}
+
+	if container.Config.VolumesFrom != "" {
+		c := container.runtime.Get(container.Config.VolumesFrom)
+		if c == nil {
+			return fmt.Errorf("Container %s not found. Impossible to mount its volumes")
+		}
+		for volPath, id := range c.Volumes {
+			if _, exists := container.Volumes[volPath]; exists {
+				return fmt.Errorf("The requested volume %s overlap one of the volume of the container %s", volPath, c.Id)
+			}
+			if err := os.MkdirAll(path.Join(container.RootfsPath(), volPath), 0755); err != nil {
+				return nil
+			}
+			container.Volumes[volPath] = id
 		}
 	}
 
