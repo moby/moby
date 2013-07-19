a41384ad7312a21fd8fe429637c8d6b5c883fa2a
Victor Vieux <victor.vieux@dotcloud.com>
2013-07-19 14:31:42 +0000

    add die event

--- container.go
+++ container.go
@@ -789,7 +789,9 @@ func (container *Container) monitor() {
 		}
 	}
 	utils.Debugf("Process finished")
-
+	if container.runtime != nil && container.runtime.srv != nil {
+		container.runtime.srv.LogEvent("die", container.ShortID())
+	}
 	exitCode := -1
 	if container.cmd != nil {
 		exitCode = container.cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
