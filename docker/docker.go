9fce6f662ae3e31832bbc45938e509824bea67d5
Solomon Hykes <solomon@dotcloud.com>
2013-08-06 20:51:12 -0700

    docker -v: show version and build information without making remote connections

--- docker/docker.go
+++ docker/docker.go
@@ -26,6 +26,7 @@ func main() {
 		return
 	}
 	// FIXME: Switch d and D ? (to be more sshd like)
+	flVersion := flag.Bool("v", false, "Print version information and quit")
 	flDaemon := flag.Bool("d", false, "Daemon mode")
 	flDebug := flag.Bool("D", false, "Debug mode")
 	flAutoRestart := flag.Bool("r", false, "Restart previously running containers")
@@ -37,6 +38,10 @@ func main() {
 	flHosts := docker.ListOpts{fmt.Sprintf("unix://%s", docker.DEFAULTUNIXSOCKET)}
 	flag.Var(&flHosts, "H", "tcp://host:port to bind/connect to or unix://path/to/socket to use")
 	flag.Parse()
+	if *flVersion {
+		showVersion()
+		return
+	}
 	if len(flHosts) > 1 {
 		flHosts = flHosts[1:] //trick to display a nice default value in the usage
 	}
@@ -74,6 +79,10 @@ func main() {
 			os.Exit(-1)
 		}
 	}
+}
+
+func showVersion() {
+	fmt.Printf("Docker version %s, build %s\n", VERSION, GITCOMMIT)
 }
 
 func createPidFile(pidfile string) error {
