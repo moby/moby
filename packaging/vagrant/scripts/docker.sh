27cd6fbe31605850df822a61afe598c7e84f0ea4
Jonas Pfenniger <zimbatm@zimbatm.com>
2013-08-24 21:47:33 +0100

    Enable cgroup swaplimit

--- packaging/vagrant/scripts/docker.sh
+++ packaging/vagrant/scripts/docker.sh
@@ -5,5 +5,9 @@ set -e
 curl https://get.docker.io/gpg | apt-key add -
 echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 apt-get update
+
 apt-get install -qy lxc-docker
 
+# Enable more cgroup features
+sed -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/g' -i /etc/default/grub
+update-grub
