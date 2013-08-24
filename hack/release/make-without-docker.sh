a277b464953e00de8f637caf00c2952cc91c452d
Brandon Philips <brandon@ifup.co>
2013-08-24 06:23:31 -0700

    feat(make-without-docker): use third party deps
    this modifies make-without-docker to use the third_party directory
    instead of running go get.

--- hack/release/make-without-docker.sh
+++ hack/release/make-without-docker.sh
@@ -29,7 +29,7 @@ source $SCRIPT_DIR/common.sh
 PROJECT_DIR=`realpath $SCRIPT_DIR/../../`
 
 # go path
-GOPATH=$PROJECT_DIR/.gopath
+export GOPATH=$PROJECT_DIR/.gopath
 
 # source path
 DOCKER_PACKAGE=github.com/dotcloud/docker
@@ -40,14 +40,21 @@ BIN_DIR=$PROJECT_DIR/bin
 DOCKER_BIN_RELATIVE=bin/docker
 DOCKER_BIN=$PROJECT_DIR/$DOCKER_BIN_RELATIVE
 
-fetch_deps() {
-        cd $DOCKER_DIR/docker; go get -d -a -ldflags='-w -d'
+setup_deps() {
+        mkdir -p $GOPATH/src
+        for i in $PROJECT_DIR/third_party/*; do
+                if [ "$i" = "third_party/src" ]; then
+                        continue
+                fi
+                cp -R "$i" $GOPATH/src
+        done
 }
 
 
-prepare() {
-        if [ -h $DOCKER_DIR ]; then rm -f $DOCKER_DIR; fi; ln -sf $PROJECT_DIR $DOCKER_DIR
-        mkdir -p  $BIN_DIR
+prepare_docker() {
+        if [ ! -h $GOPATH/src/$DOCKER_PACKAGE ]; then
+                ln -s $PROJECT_DIR $GOPATH/src/$DOCKER_PACKAGE
+        fi
 }
 
 
@@ -60,8 +67,8 @@ main() {
 
 ###############################################################################
 EOF
-        prepare
-        fetch_deps
+        setup_deps
+        prepare_docker
         cd $DOCKER_DIR
         CGO_ENABLED=0 go build -a -ldflags "$LDFLAGS" -o $DOCKER_BIN
         echo  $DOCKER_BIN_RELATIVE is created.
