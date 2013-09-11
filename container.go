232ec55c1e933eb35205a01eea51df880282d416
Michael Crosby <crosby.michael@gmail.com>
2013-09-11 18:47:46 +0000

    Add IP to APIPort struct

--- container.go
+++ container.go
@@ -333,6 +333,7 @@ func (settings *NetworkSettings) PortMappingAPI() []APIPort {
 				PrivatePort: int64(p),
 				PublicPort:  int64(h),
 				Type:        port.Proto(),
+				IP:          binding.HostIp,
 			})
 		}
 	}
