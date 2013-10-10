e8a3a05753e7432abe818716a19d91a5bfbafbba
Alexander Larsson <alexl@redhat.com>
2013-10-10 10:53:38 +0200

    Move SysInit to a submodule
    This allows docker-init to not import the main docker
    module, which means it won't e.g. pick up any sqlite
    dependencies.

--- docker/docker.go
+++ docker/docker.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/dotcloud/docker"
+	"github.com/dotcloud/docker/sysinit"
 	"github.com/dotcloud/docker/utils"
 	"io/ioutil"
 	"log"
@@ -23,7 +24,7 @@ var (
 func main() {
 	if selfPath := utils.SelfPath(); selfPath == "/sbin/init" || selfPath == "/.dockerinit" {
 		// Running in init mode
-		docker.SysInit()
+		sysinit.SysInit()
 		return
 	}
 	// FIXME: Switch d and D ? (to be more sshd like)
