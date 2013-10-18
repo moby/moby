07d9e4353bc199db7abb52b4b4bba1c2feb75c1a
Solomon Hykes <solomon@dotcloud.com>
2013-10-18 06:44:30 +0000

    Increase readbility of unit tests by using mkRuntime everywhere

--- api_test.go
+++ api_test.go
@@ -472,10 +472,7 @@ func TestGetContainersChanges(t *testing.T) {
 
 func TestGetContainersTop(t *testing.T) {
 	t.Skip("Fixme. Skipping test for now. Reported error when testing using dind: 'api_test.go:527: Expected 2 processes, found 0.'")
-	runtime, err := newTestRuntime()
-	if err != nil {
-		t.Fatal(err)
-	}
+	runtime := mkRuntime(t)
 	defer nuke(runtime)
 
 	srv := &Server{runtime: runtime}
