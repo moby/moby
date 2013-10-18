ca9dcc0af54a06fd140ad2a06a1cf5da86ffcf55
Tianon Gravi <admwiggin@gmail.com>
2013-10-17 23:40:41 -0600

    Add dynbinary and dyntest scripts for building/testing a separate static dockerinit binary
    After a nice long brainstorming session with @shykes on IRC, we decided on using a SHA1 hash of dockerinit compiled into the dynamic docker binary to ensure that we always use the two in a perfect pair, and never mix and match.

--- hack/make.sh
+++ hack/make.sh
@@ -35,6 +35,8 @@ grep -q "$RESOLVCONF" /proc/mounts || {
 DEFAULT_BUNDLES=(
 	binary
 	test
+	dynbinary
+	dyntest
 	ubuntu
 )
 
@@ -46,6 +48,7 @@ fi
 
 # Use these flags when compiling the tests and final binary
 LDFLAGS='-X main.GITCOMMIT "'$GITCOMMIT'" -X main.VERSION "'$VERSION'" -w'
+LDFLAGS_STATIC='-X github.com/dotcloud/docker/utils.IAMSTATIC true -linkmode external -extldflags "-lpthread -static -Wl,--unresolved-symbols=ignore-in-object-files"'
 BUILDFLAGS='-tags netgo'
 
 bundle() {
