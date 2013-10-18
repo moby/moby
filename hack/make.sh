e2d3a97b21e0f1437afbe40ab9cb533bec5938dd
Tianon Gravi <admwiggin@gmail.com>
2013-10-17 23:38:34 -0600

    Refactor the handling of static-related build flags
    This is as discussed with @shykes on IRC

--- hack/make.sh
+++ hack/make.sh
@@ -45,7 +45,7 @@ if [ -n "$(git status --porcelain)" ]; then
 fi
 
 # Use these flags when compiling the tests and final binary
-LDFLAGS='-X main.GITCOMMIT "'$GITCOMMIT'" -X main.VERSION "'$VERSION'" -w -linkmode external -extldflags "-lpthread -static -Wl,--unresolved-symbols=ignore-in-object-files"'
+LDFLAGS='-X main.GITCOMMIT "'$GITCOMMIT'" -X main.VERSION "'$VERSION'" -w'
 BUILDFLAGS='-tags netgo'
 
 bundle() {
