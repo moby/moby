49082972519538998a14706373025ab899300dcb
Stephen Weinberg <stephen@q5comm.com>
2013-10-21 08:21:36 -0700

    sd_notify

--- api.go
+++ api.go
@@ -1083,8 +1083,6 @@ func createRouter(srv *Server, logging bool) (*mux.Router, error) {
 }
 
 func ListenAndServe(proto, addr string, srv *Server, logging bool) error {
-	log.Printf("Listening for HTTP on %s (%s)\n", addr, proto)
-
 	r, err := createRouter(srv, logging)
 	if err != nil {
 		return err
@@ -1115,5 +1113,10 @@ func ListenAndServe(proto, addr string, srv *Server, logging bool) error {
 		}
 	}
 	httpSrv := http.Server{Addr: addr, Handler: r}
+
+	log.Printf("Listening for HTTP on %s (%s)\n", addr, proto)
+	// Tell the init daemon we are accepting requests
+	go sd_notify("READY=1")
+
 	return httpSrv.Serve(l)
 }
