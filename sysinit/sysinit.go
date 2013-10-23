6c14b8e393f65af5c979ebd952bdcdbd90d19ce3
Josh Poimboeuf <jpoimboe@redhat.com>
2013-10-23 18:34:02 -0500

    dockerinit: create a long-running dockerinit
    Create a long-running dockerinit which does some setup, starts the
    container app, waits for it to finish, and exits with the app's exit
    code.
    
    A long-running dockerinit is a prerequisite for using libvirt-lxc, since
    libvirt provides no way to return an exit code, and no way to split
    stdout and stderr into separate streams.
    
    It also will allow docker to get rid of the ugly Ghost state, and might
    even make it possible to do everything in the docker client and get rid
    of the docker daemon altogether.

--- sysinit/sysinit.go
+++ sysinit/sysinit.go
@@ -11,96 +11,137 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"path"
 	"strconv"
 	"strings"
 	"syscall"
 )
 
-// Setup networking
-func setupNetworking(gw string) {
-	if gw == "" {
-		return
+func setupNetworking(args *DockerInitArgs) error {
+	if args.gateway == "" {
+		return nil
 	}
 
-	ip := net.ParseIP(gw)
+	ip := net.ParseIP(args.gateway)
 	if ip == nil {
-		log.Fatalf("Unable to set up networking, %s is not a valid IP", gw)
-		return
+		return fmt.Errorf("Unable to set up networking, %s is not a valid IP", args.gateway)
 	}
 
 	if err := netlink.AddDefaultGw(ip); err != nil {
-		log.Fatalf("Unable to set up networking: %v", err)
+		return fmt.Errorf("Unable to set up networking: %v", err)
 	}
+	return nil
 }
 
-// Setup working directory
-func setupWorkingDirectory(workdir string) {
-	if workdir == "" {
-		return
+func getCredential(args *DockerInitArgs) (*syscall.Credential, error) {
+	if args.user == "" {
+		return nil, nil
 	}
-	if err := syscall.Chdir(workdir); err != nil {
-		log.Fatalf("Unable to change dir to %v: %v", workdir, err)
-	}
-}
-
-// Takes care of dropping privileges to the desired user
-func changeUser(u string) {
-	if u == "" {
-		return
-	}
-	userent, err := utils.UserLookup(u)
+	userent, err := utils.UserLookup(args.user)
 	if err != nil {
-		log.Fatalf("Unable to find user %v: %v", u, err)
+		return nil, fmt.Errorf("Unable to find user %v: %v", args.user, err)
 	}
 
 	uid, err := strconv.Atoi(userent.Uid)
 	if err != nil {
-		log.Fatalf("Invalid uid: %v", userent.Uid)
+		return nil, fmt.Errorf("Invalid uid: %v", userent.Uid)
 	}
 	gid, err := strconv.Atoi(userent.Gid)
 	if err != nil {
-		log.Fatalf("Invalid gid: %v", userent.Gid)
+		return nil, fmt.Errorf("Invalid gid: %v", userent.Gid)
 	}
 
-	if err := syscall.Setgid(gid); err != nil {
-		log.Fatalf("setgid failed: %v", err)
-	}
-	if err := syscall.Setuid(uid); err != nil {
-		log.Fatalf("setuid failed: %v", err)
-	}
+	return &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)}, nil
 }
 
-// Clear environment pollution introduced by lxc-start
-func cleanupEnv() {
-	os.Clearenv()
-	var lines []string
-	content, err := ioutil.ReadFile("/.dockerenv")
-	if err != nil {
-		log.Fatalf("Unable to load environment variables: %v", err)
-	}
-	err = json.Unmarshal(content, &lines)
-	if err != nil {
-		log.Fatalf("Unable to unmarshal environment variables: %v", err)
-	}
-	for _, kv := range lines {
+func getEnv(args *DockerInitArgs, key string) string {
+	for _, kv := range args.env {
 		parts := strings.SplitN(kv, "=", 2)
-		if len(parts) == 1 {
-			parts = append(parts, "")
+		if parts[0] == key && len(parts) == 2 {
+			return parts[1]
 		}
-		os.Setenv(parts[0], parts[1])
 	}
+	return ""
 }
 
-func executeProgram(name string, args []string) {
-	path, err := exec.LookPath(name)
-	if err != nil {
-		log.Printf("Unable to locate %v", name)
-		os.Exit(127)
+func getCmdPath(args *DockerInitArgs) (string, error) {
+
+	// Set PATH in dockerinit so we can find the cmd
+	envPath := getEnv(args, "PATH")
+	if envPath != "" {
+		os.Setenv("PATH", envPath)
 	}
 
-	if err := syscall.Exec(path, args, os.Environ()); err != nil {
-		panic(err)
+	// Find the cmd
+	cmdPath, err := exec.LookPath(args.args[0])
+	if err != nil {
+		if args.workDir == "" {
+			return "", err
+		}
+		cmdPath, err = exec.LookPath(path.Join(args.workDir, args.args[0]))
+		if err != nil {
+			return "", err
+		}
 	}
+
+	return cmdPath, nil
+}
+
+// Run as pid 1 in the typical Docker usage: an app container that doesn't
+// need its own init process.  Running as pid 1 allows us to monitor the
+// container app and return its exit code.
+func dockerInitApp(args *DockerInitArgs) error {
+
+	// Prepare the cmd based on the given args
+	cmdPath, err := getCmdPath(args)
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command(cmdPath, args.args[1:]...)
+	cmd.Dir = args.workDir
+	cmd.Env = args.env
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Update uid/gid credentials if needed
+	credential, err := getCredential(args)
+	if err != nil {
+		return err
+	}
+	cmd.SysProcAttr = &syscall.SysProcAttr{Credential: credential}
+
+	// Network setup
+	err = setupNetworking(args)
+	if err != nil {
+		return err
+	}
+
+	// Start the app
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	// Wait for it to exit
+	err = cmd.Wait()
+	if err != nil {
+		_, ok := err.(*exec.ExitError)
+		if !ok {
+			return err
+		}
+	}
+	exitCode := cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
+	os.Exit(exitCode)
+	return nil
+}
+
+type DockerInitArgs struct {
+	user    string
+	gateway string
+	workDir string
+	env     []string
+	args    []string
 }
 
 // Sys Init code
@@ -111,15 +152,34 @@ func SysInit() {
 		fmt.Println("You should not invoke dockerinit manually")
 		os.Exit(1)
 	}
-	var u = flag.String("u", "", "username or uid")
-	var gw = flag.String("g", "", "gateway address")
-	var workdir = flag.String("w", "", "workdir")
 
+	// Get cmdline arguments
+	user := flag.String("u", "", "username or uid")
+	gateway := flag.String("g", "", "gateway address")
+	workDir := flag.String("w", "", "workdir")
 	flag.Parse()
 
-	cleanupEnv()
-	setupNetworking(*gw)
-	setupWorkingDirectory(*workdir)
-	changeUser(*u)
-	executeProgram(flag.Arg(0), flag.Args())
+	// Get env
+	var env []string
+	content, err := ioutil.ReadFile("/.dockerenv")
+	if err != nil {
+		log.Fatalf("Unable to load environment variables: %v", err)
+	}
+	err = json.Unmarshal(content, &env)
+	if err != nil {
+		log.Fatalf("Unable to unmarshal environment variables: %v", err)
+	}
+
+	args := &DockerInitArgs{
+		user:    *user,
+		gateway: *gateway,
+		workDir: *workDir,
+		env:     env,
+		args:    flag.Args(),
+	}
+
+	err = dockerInitApp(args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
