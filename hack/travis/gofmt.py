561d1db074edd4e7e7d8b02401abd3fc11b9d51c
Tianon Gravi <admwiggin@gmail.com>
2013-12-04 13:47:37 -0800

    Add Travis CI configuration to validate DCO and gofmt
    After each push, Travis CI will trigger, and check two things:
    
    - make sure that each commit in the push has the Docker certificate of origin
    - make sure that all .go files changed by this sequence of commits are correctly formatted in the most recent commit
    
    Note: there is one edge case; if you do a git force push, we cannot figure out the actual commits in the force push, and we will just run the checks as if upstream master were the base. Pull requests will always be tested correctly, though.
    
    Docker-DCO-1.0-Signed-off-by: Andrew Page <admwiggin@gmail.com> (github: tianon)

---
+++ hack/travis/gofmt.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import subprocess
+
+from env import commit_range
+
+files = subprocess.check_output([
+	'git', 'diff', '--diff-filter=ACMR',
+	'--name-only', '...'.join(commit_range), '--',
+])
+
+exit_status = 0
+
+for filename in files.split('\n'):
+	if filename.endswith('.go'):
+		try:
+			out = subprocess.check_output(['gofmt', '-s', '-l', filename])
+			if out != '':
+				print out,
+				exit_status = 1
+		except subprocess.CalledProcessError:
+			exit_status = 1
+
+if exit_status != 0:
+	print 'Reformat the files listed above with "gofmt -s -w" and try again.'
+	exit(exit_status)
+
+print 'All files pass gofmt.'
+exit(0)
