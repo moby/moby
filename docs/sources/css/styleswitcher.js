ba25883410f4fc13eaae2f0f323be1977e15d013
Charles Merriam <charles.merriam@gmail.com>
2013-12-19 17:00:32 -0800

    Add quick_docker.html Docker Quick Reference page
    This is an initial version of a summary page for intermediate and
    advanced docker users.  It is developed from
    github.com/merriam/quick_reference and the guide is under a CC
    Share-a-like Non-commercial license.   It's still improviing, but
    provides some quick help so far.

---
+++ docs/sources/css/styleswitcher.js
@@ -0,0 +1,28 @@
+function setActiveStyleSheet(title) {
+  var i, a, main;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+      a.disabled = true;
+      if(a.getAttribute("title") == title) a.disabled = false;
+    }
+  }
+}
+
+function getActiveStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
+  }
+  return null;
+}
+
+function getPreferredStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1
+       && a.getAttribute("rel").indexOf("alt") == -1
+       && a.getAttribute("title")
+       ) return a.getAttribute("title");
+  }
+  return null;
+}
