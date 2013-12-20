f9359f59a88f109a7764406c33529a20098b0c7f
Guillaume J. Charmes <charmes.guillaume@gmail.com>
2013-12-20 08:20:08 -0800

    Add dynamic os/arch detection to Images

--- graph.go
+++ graph.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"syscall"
 	"time"
@@ -131,7 +132,8 @@ func (graph *Graph) Create(layerData archive.Archive, container *Container, comm
 		DockerVersion: VERSION,
 		Author:        author,
 		Config:        config,
-		Architecture:  "x86_64",
+		Architecture:  runtime.GOARCH,
+		OS:            runtime.GOOS,
 	}
 	if container != nil {
 		img.Parent = container.Image
