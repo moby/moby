ee93f6185be3ae73c16cf41ae397bae3ce2f6c55
Tianon Gravi <admwiggin@gmail.com>
2013-12-27 10:47:42 -0700

    Move UserLookup functionality into a separate pkg/user submodule that implements proper parsing of /etc/passwd and /etc/group, and use that to add support for "docker run -u user:group" and for getting supplementary groups (if ":group" is not specified)
    Docker-DCO-1.1-Signed-off-by: Andrew Page <admwiggin@gmail.com> (github: tianon)

--- integration/buildfile_test.go
+++ integration/buildfile_test.go
@@ -148,6 +148,86 @@ RUN [ "$(/hello.sh)" = "hello world" ]
 		nil,
 	},
 
+	// Users and groups
+	{
+		`
+FROM {IMAGE}
+
+# Make sure our defaults work
+RUN [ "$(id -u):$(id -g)" = '0:0' ]
+RUN [ "$(id -un):$(id -gn)" = 'root:root' ]
+
+# TODO decide if "args.user = strconv.Itoa(syscall.Getuid())" is acceptable behavior for changeUser in sysvinit instead of "return nil" when "USER" isn't specified (so that we get the proper group list even if that is the empty list, even in the default case of not supplying an explicit USER to run as, which implies USER 0)
+USER root
+RUN [ "$(id -G) -- $(id -Gn)" = '0 -- root' ]
+
+# Setup dockerio user and group
+RUN echo 'dockerio:x:1000:1000::/bin:/bin/false' >> /etc/passwd
+RUN echo 'dockerio:x:1000:' >> /etc/group
+
+# Make sure we can switch to our user and all the information is exactly as we expect it to be
+USER dockerio
+RUN [ "$(id -u):$(id -g)" = '1000:1000' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:dockerio' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1000 -- dockerio' ]
+
+# Switch back to root and double check that worked exactly as we might expect it to
+USER root
+RUN [ "$(id -u):$(id -g)" = '0:0' ]
+RUN [ "$(id -un):$(id -gn)" = 'root:root' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '0 -- root' ]
+
+# Add a "supplementary" group for our dockerio user
+RUN echo 'supplementary:x:1001:dockerio' >> /etc/group
+
+# ... and then go verify that we get it like we expect
+USER dockerio
+RUN [ "$(id -u):$(id -g)" = '1000:1000' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:dockerio' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1000 1001 -- dockerio supplementary' ]
+USER 1000
+RUN [ "$(id -u):$(id -g)" = '1000:1000' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:dockerio' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1000 1001 -- dockerio supplementary' ]
+
+# and finally, super test the new "user:group" syntax
+USER dockerio:dockerio
+RUN [ "$(id -u):$(id -g)" = '1000:1000' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:dockerio' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1000 -- dockerio' ]
+USER 1000:dockerio
+RUN [ "$(id -u):$(id -g)" = '1000:1000' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:dockerio' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1000 -- dockerio' ]
+USER dockerio:1000
+RUN [ "$(id -u):$(id -g)" = '1000:1000' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:dockerio' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1000 -- dockerio' ]
+USER 1000:1000
+RUN [ "$(id -u):$(id -g)" = '1000:1000' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:dockerio' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1000 -- dockerio' ]
+USER dockerio:supplementary
+RUN [ "$(id -u):$(id -g)" = '1000:1001' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:supplementary' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1001 -- supplementary' ]
+USER dockerio:1001
+RUN [ "$(id -u):$(id -g)" = '1000:1001' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:supplementary' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1001 -- supplementary' ]
+USER 1000:supplementary
+RUN [ "$(id -u):$(id -g)" = '1000:1001' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:supplementary' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1001 -- supplementary' ]
+USER 1000:1001
+RUN [ "$(id -u):$(id -g)" = '1000:1001' ]
+RUN [ "$(id -un):$(id -gn)" = 'dockerio:supplementary' ]
+RUN [ "$(id -G) -- $(id -Gn)" = '1001 -- supplementary' ]
+`,
+		nil,
+		nil,
+	},
+
 	// Environment variable
 	{
 		`
