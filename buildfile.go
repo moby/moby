fb63cfa9a502e2410597422f8877cf16b0bbaad2
Tianon Gravi <admwiggin@gmail.com>
2014-01-11 05:46:11 -0700

    Stop ADD from following symlinks outside the context when passed as the first argument
    Docker-DCO-1.1-Signed-off-by: Andrew Page <admwiggin@gmail.com> (github: tianon)

--- buildfile.go
+++ buildfile.go
@@ -287,6 +287,11 @@ func (b *buildFile) CmdVolume(args string) error {
 
 func (b *buildFile) checkPathForAddition(orig string) error {
 	origPath := path.Join(b.contextPath, orig)
+	if p, err := filepath.EvalSymlinks(origPath); err != nil {
+		return err
+	} else {
+		origPath = p
+	}
 	if !strings.HasPrefix(origPath, b.contextPath) {
 		return fmt.Errorf("Forbidden path outside the build context: %s (%s)", orig, origPath)
 	}
