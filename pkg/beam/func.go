803e3a399becabb172487d03d01aa038b9bcc281
Solomon Hykes <solomon@dotcloud.com>
2014-01-29 21:55:07 -0800

    beam: improvements to Func
    Docker-DCO-1.1-Signed-off-by: Solomon Hykes <solomon@docker.com> (github: shykes)
    
    * Func.Send spawns a new goroutine to handle each message
    * As a result, Func.Send never fails
    * After a new stream is received and processed, it is closed. This can be used for synchronization
    * Func.Receive returns io.EOF instead of failing
    * A few tests to enforce the above

--- pkg/beam/func.go
+++ pkg/beam/func.go
@@ -1,6 +1,7 @@
 package beam
 
 import (
+	"io"
 	"fmt"
 	"os"
 )
@@ -9,16 +10,24 @@ import (
 type Func func([]byte, Stream) error
 
 func (f Func) Send(data []byte, s Stream) error {
-	return f(data, s)
+	go func() {
+		f(data, s)
+		if s != nil {
+			s.Close()
+		}
+	}()
+	return nil
 }
 
 func (f Func) Receive() ([]byte, Stream, error) {
-	return nil, nil, fmt.Errorf("receive: operation not supported")
+	return nil, nil, io.EOF
 }
 
 func (f Func) File() (*os.File, error) {
 	return nil, fmt.Errorf("no file descriptor associated with stream")
 }
+
+
 
 func (f Func) Close() error {
 	return fmt.Errorf("receive: operation not supported")
--- pkg/beam/func_test.go
+++ pkg/beam/func_test.go
@@ -12,10 +12,15 @@ func TestFuncSend(t *testing.T) {
 			t.Fatalf("unexpected data: '%v'", data)
 		}
 		sentinel = 42
-		return fmt.Errorf("this is a grave error")
+		return nil
 	})
-	if err := f.Send([]byte("well hello..."), nil); err == nil || err.Error() != "this is a grave error" {
-		t.Fatalf("send: unexpected err='%v'", err)
+	local, remote := Pipe()
+	defer local.Close()
+	if err := f.Send([]byte("well hello..."), remote); err != nil {
+		t.Fatalf("send: %s", err)
+	}
+	if err := Splice(DevNull, local); err != nil {
+		t.Fatalf("splice: %s", err)
 	}
 	if sentinel != 42 {
 		t.Fatalf("sentinel was not set: was the handler func called?")
@@ -26,7 +31,7 @@ func TestFuncSend(t *testing.T) {
 func TestFuncReceive(t *testing.T) {
 	f := Func(nil)
 	_, _, err := f.Receive()
-	if err == nil {
-		t.Fatalf("Func should return an error on Receive")
+	if err != io.EOF {
+		t.Fatalf("Func.Receive should return io.EOF")
 	}
 }
