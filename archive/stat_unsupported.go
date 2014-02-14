d19e998e7a99d24122da3e98d9886e3b7b4b3501
Alexander Larsson <alexl@redhat.com>
2014-02-14 14:24:37 +0100

    Archive: Add Add Lgetxattr and Lsetxattr implementations
    Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)

--- archive/stat_unsupported.go
+++ archive/stat_unsupported.go
@@ -19,3 +19,11 @@ func LUtimesNano(path string, ts []syscall.Timespec) error {
 func UtimesNano(path string, ts []syscall.Timespec) error {
 	return ErrNotImplemented
 }
+
+func Lgetxattr(path string, attr string) ([]byte, error) {
+	return nil, ErrNotImplemented
+}
+
+func Lsetxattr(path string, attr string, data []byte, flags int) error {
+	return ErrNotImplemented
+}
