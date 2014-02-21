ba025cb75cceaa8536d0d512889ea86f13349950
Michael Crosby <michael@crosbymichael.com>
2014-02-21 15:32:50 -0800

    User os.Args[0] as name to reexec Docker-DCO-1.1-Signed-off-by: Michael Crosby <michael@crosbymichael.com> (github: crosbymichael)

--- pkg/libcontainer/nsinit/exec.go
+++ pkg/libcontainer/nsinit/exec.go
@@ -219,7 +219,9 @@ func deletePidFile() error {
 // defined on the container's configuration and use the current binary as the init with the
 // args provided
 func createCommand(container *libcontainer.Container, console, logFile string, pipe uintptr, args []string) *exec.Cmd {
-	command := exec.Command("nsinit", append([]string{
+	// get our binary name so we can always reexec ourself
+	name := os.Args[0]
+	command := exec.Command(name, append([]string{
 		"-console", console,
 		"-pipe", fmt.Sprint(pipe),
 		"-log", logFile,
