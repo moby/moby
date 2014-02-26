d583bf13b5cb30badbab0883c753e610f3d025f8
Solomon Hykes <solomon@dotcloud.com>
2014-02-25 19:29:42 -0800

    Beam: more tests
    Docker-DCO-1.1-Signed-off-by: Solomon Hykes <solomon@docker.com> (github: shykes)

--- pkg/beam/func_test.go
+++ pkg/beam/func_test.go
@@ -2,7 +2,9 @@ package beam
 
 import (
 	"io"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestFuncSend(t *testing.T) {
@@ -24,6 +26,38 @@ func TestFuncSend(t *testing.T) {
 	}
 	if sentinel != 42 {
 		t.Fatalf("sentinel was not set: was the handler func called?")
+	}
+}
+
+func TestSendFunc(t *testing.T) {
+	timer := time.AfterFunc(1*time.Second, func() { t.Fatalf("timeout") })
+	defer timer.Stop()
+	mirror := Func(func(msg Message) error {
+		if msg.Stream != nil {
+			err := msg.Stream.Send(Message{Data: msg.Data})
+			if err != nil {
+				t.Fatalf("send: %s", err)
+			}
+		}
+		return nil
+	})
+	var dumpCalled bool
+	var wg sync.WaitGroup
+	wg.Add(1)
+	dump := Func(func(msg Message) error {
+		defer wg.Done()
+		if string(msg.Data) != "hello" {
+			t.Fatalf("unexpected data: %s", string(msg.Data))
+		}
+		dumpCalled = true
+		return nil
+	})
+	if err := mirror.Send(Message{Data: []byte("hello"), Stream: dump}); err != nil {
+		t.Fatal(err)
+	}
+	wg.Wait()
+	if !dumpCalled {
+		t.Fatalf("function not called")
 	}
 }
 
