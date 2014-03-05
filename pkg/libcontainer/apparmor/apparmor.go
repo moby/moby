37f137c8221be07943babeb9b107e5aee9a736d5
Michael Crosby <michael@crosbymichael.com>
2014-03-05 12:27:31 -0800

    Some cleanup around logs Docker-DCO-1.1-Signed-off-by: Michael Crosby <michael@crosbymichael.com> (github: crosbymichael)

--- pkg/libcontainer/apparmor/apparmor.go
+++ pkg/libcontainer/apparmor/apparmor.go
@@ -1,42 +1,29 @@
 package apparmor
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 )
 
-var AppArmorEnabled bool
-
-var (
-	ErrAppArmorDisabled = errors.New("Error: AppArmor is not enabled on this system")
-)
-
-func init() {
+func IsEnabled() bool {
 	buf, err := ioutil.ReadFile("/sys/module/apparmor/parameters/enabled")
-	AppArmorEnabled = err == nil && len(buf) > 1 && buf[0] == 'Y'
+	return err == nil && len(buf) > 1 && buf[0] == 'Y'
 }
 
 func ApplyProfile(pid int, name string) error {
-	if !AppArmorEnabled {
-		return ErrAppArmorDisabled
+	if !IsEnabled() || name == "" {
+		return nil
 	}
 
 	f, err := os.OpenFile(fmt.Sprintf("/proc/%d/attr/current", pid), os.O_WRONLY, 0)
 	if err != nil {
-		log.Printf("error open: %s\n", err)
 		return err
 	}
 	defer f.Close()
 
 	if _, err := fmt.Fprintf(f, "changeprofile %s", name); err != nil {
-		log.Printf("changeprofile %s", name)
-		log.Printf("Error write: %s\n", err)
 		return err
-	} else {
-		log.Printf("Write success!")
 	}
 	return nil
 }
