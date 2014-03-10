10dc16dcd3aa82be256e5072a25dcf18af8e3844
Guillaume J. Charmes <guillaume@charmes.net>
2014-03-10 13:50:16 -0700

    Create portable signalMap
    Docker-DCO-1.1-Signed-off-by: Guillaume J. Charmes <guillaume@charmes.net> (github: creack)

--- pkg/signal/signal.go
+++ pkg/signal/signal.go
@@ -5,6 +5,14 @@ import (
 	"os/signal"
 )
 
+func CatchAll(sigc chan os.Signal) {
+	handledSigs := []os.Signal{}
+	for _, s := range signalMap {
+		handledSigs = append(handledSigs, s)
+	}
+	signal.Notify(sigc, handledSigs...)
+}
+
 func StopCatch(sigc chan os.Signal) {
 	signal.Stop(sigc)
 	close(sigc)
