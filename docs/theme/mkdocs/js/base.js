d7d9f01e89e7adcd292d6228e2be98445d3dab51
O.S.Tezer <ostezer@gmail.com>
2014-04-08 23:43:41 +0300

    This PR introduces:
     - Auto-scrolling main navigation menu
     - Auto-scrolling TOC pane
     - Redisged TOC pane
     - Better-ised scaffolding that is more native bootstrap than custom hacks.
    
    Docker-DCO-1.1-Signed-off-by: O.S. Tezer <ostezer@gmail.com> (github: ostezer)

--- docs/theme/mkdocs/js/base.js
+++ docs/theme/mkdocs/js/base.js
@@ -1,52 +1,85 @@
-
-/* Prettyify */
-$( document ).ready(function() {
+$(document).ready(function() {
+    /* Prettyify */
     prettyPrint();
+
+    $('#stickynav').scrollToFixed();
+
+    $('#left_menu_navigation').scrollToFixed({
+        marginTop: $('#stickynav').height(),
+        limit: $('#footer').offset().top - $('#left_menu_navigation').height() - 450,
+        zIndex: 1,
+    });
+
+    // /* Sticky TOC via Affix plug-in */
+    // $('#left_menu_navigation').affix({
+    //     offset: {
+    //         top: $('#left_menu_navigation').offset().top,
+    //         bottom: 450,
+    //     }
+    // });
+    // $('#stickynav').affix({
+    //     offset: {
+    //         top: ($('#stickynav').offset().top) + 
+    //              ($('#horizontal_thin_menu').offset().top) +
+    //              ($('#topmostnav').offset().top),
+    //     }
+    // });
+
 });
 
+// $(window).on("resize", function() {
+//     var isWindowTallEnough = $('#left_menu_navigation').height() + 20 < $(window).height();
 
-/* Scrollspy */
-var navHeight = $('.navbar').outerHeight(true) + 10
+//     console.log(isWindowTallEnough);
 
-$('body').scrollspy({
-    target: '.bs-sidebar',
-    offset: navHeight
-})
+//     if (isWindowTallEnough) {
+//         $('#left_menu_navigation').removeClass("affix affix-top");
+//     } else {
+//         $('#left_menu_navigation').addClass("affix affix-top");
+//     }
+// });
 
-
-/* Prevent disabled links from causing a page reload */
-$("li.disabled a").click(function() {
-    event.preventDefault();
-});
-
-
-/* Adjust the scroll height of anchors to compensate for the fixed navbar */
-window.disableShift = false;
-var shiftWindow = function() {
-    if (window.disableShift) {
-        window.disableShift = false;
-    } else {
-        /* If we're at the bottom of the page, don't erronously scroll up */
-        var scrolledToBottomOfPage = (
-            (window.innerHeight + window.scrollY) >= document.body.offsetHeight
-        );
-        if (!scrolledToBottomOfPage) {
-            scrollBy(0, -60);
-        };
-    };
-};
-if (location.hash) {shiftWindow();}
-window.addEventListener("hashchange", shiftWindow);
+// /* Scrollspy */
+// var navHeight = $('#left_menu_navigation').outerHeight(true) + 10;
+// $('body').scrollspy({
+//     target: '#toc_navigation',
+//     offset: navHeight
+// });
 
 
-/* Deal with clicks on nav links that do not change the current anchor link. */
-$("ul.nav a" ).click(function() {
-    var href = this.href;
-    var suffix = location.hash;
-    var matchesCurrentHash = (href.indexOf(suffix, href.length - suffix.length) !== -1);
-    if (location.hash && matchesCurrentHash) {
-        /* Force a single 'hashchange' event to occur after the click event */
-        window.disableShift = true;
-        location.hash='';
-    };
-});
+// /* Prevent disabled links from causing a page reload */
+// $("li.disabled a").click(function() {
+//     event.preventDefault();
+// });
+
+
+// /* Adjust the scroll height of anchors to compensate for the fixed navbar */
+// window.disableShift = false;
+// var shiftWindow = function() {
+//     if (window.disableShift) {
+//         window.disableShift = false;
+//     } else {
+//         /* If we're at the bottom of the page, don't erronously scroll up */
+//         var scrolledToBottomOfPage = (
+//             (window.innerHeight + window.scrollY) >= document.body.offsetHeight
+//         );
+//         if (!scrolledToBottomOfPage) {
+//             scrollBy(0, -60);
+//         };
+//     };
+// };
+// if (location.hash) {shiftWindow();}
+// window.addEventListener("hashchange", shiftWindow);
+
+
+// /* Deal with clicks on nav links that do not change the current anchor link. */
+// $("ul.nav a" ).click(function() {
+//     var href = this.href;
+//     var suffix = location.hash;
+//     var matchesCurrentHash = (href.indexOf(suffix, href.length - suffix.length) !== -1);
+//     if (location.hash && matchesCurrentHash) {
+//         /* Force a single 'hashchange' event to occur after the click event */
+//         window.disableShift = true;
+//         location.hash='';
+//     };
+// });
