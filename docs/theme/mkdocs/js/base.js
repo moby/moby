7216d48279237502e09ee30ccf23d591cd106723
O.S.Tezer <ostezer@gmail.com>
2014-04-11 00:16:54 +0300

    Scroll position tracker (scrollspy) + fixes + CSS library (customised)  minified.
    Docker-DCO-1.1-Signed-off-by: O.S. Tezer <ostezer@gmail.com> (github: ostezer)
    
    Docker-DCO-1.1-Signed-off-by: O.S.Tezer <ostezer@gmail.com> (github: SvenDowideit)

--- docs/theme/mkdocs/js/base.js
+++ docs/theme/mkdocs/js/base.js
@@ -1,44 +1,62 @@
-$(document).ready(function() {
-
-  // Prettyify
+$(document).ready(function()
+{
   prettyPrint();
 
   // Resizing
   resizeMenuDropdown();
   checkToScrollTOC();
 
-  $('#nav_menu').scrollToFixed({
-    dontSetWidth: true,
-    // minWidth: 992,
-  });
-
-  console.log($('#toc_table').height() >= $(window).height());
-
-  $(window).on('resize', function() {
+  $(window).on('resize', function()
+  {
     resizeMenuDropdown();
     checkToScrollTOC();
   });
 
-  $('#toc_table').on('click', function () {
-    $('#toc_table > #toc_navigation').toggle();
+  /* Auto scroll */
+  $('#nav_menu').scrollToFixed({
+    dontSetWidth: true,
+  });
+
+  /* Toggle TOC view for Mobile */
+  $('#toc_table').on('click', function ()
+  {
+    if ( $(window).width() <= 991 )
+    {
+      $('#toc_table > #toc_navigation').slideToggle();
+    }
   })
+
+  /* Follow TOC links (ScrollSpy) */
+  $('body').scrollspy({
+    target: '#toc_table',
+  });
+
+  /* Prevent disabled link clicks */
+  $("li.disabled a").click(function() {
+    event.preventDefault();
+  });
 
 });
 
-function resizeMenuDropdown () {
-  if ( $(window).width() >= 768 ) {
+function resizeMenuDropdown ()
+{
+  if ( $(window).width() >= 768 )
+  {
     $('#main-nav > li > .submenu').css("max-height", ($('body').height() - 160) + 'px');
   }
 }
 
 // https://github.com/bigspotteddog/ScrollToFixed
-function checkToScrollTOC () {
-
-  if ( $(window).width() > 999 ) {
-
-    if ( $('#toc_table').height() >= $(window).height() ) {
+function checkToScrollTOC ()
+{
+  if ( $(window).width() > 999 )
+  {
+    if ( $('#toc_table').height() >= $(window).height() )
+    {
       $('#toc_table').trigger('detach.ScrollToFixed');
-    } else {
+    }
+    else
+    {
       $('#toc_table').scrollToFixed({
         marginTop: $('#nav_menu').height() + 14,
         limit: function () { return $('#footer').offset().top - 450; },
@@ -48,19 +66,5 @@ function checkToScrollTOC () {
         removeOffsets: true,
       });
     }
-
   }
-
 }
-
-// /* Scrollspy */
-// var navHeight = $('#toc_table').outerHeight(true) + 10;
-// $('body').scrollspy({
-//   target: '#toc_navigation',
-//   offset: navHeight
-// });
-
-// /* Prevent disabled links from causing a page reload */
-// $("li.disabled a").click(function() {
-//   event.preventDefault();
-// });
