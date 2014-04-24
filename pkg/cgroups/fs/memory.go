ad924959a9879af7477fa23c19d570882c1f378f
Victor Marmol <vmarmol@google.com>
2014-04-24 22:59:37 +0000

    Add memory usage and max usage stats.
    Docker-DCO-1.1-Signed-off-by: Victor Marmol <vmarmol@google.com> (github: vmarmol)

--- pkg/cgroups/fs/memory.go
+++ pkg/cgroups/fs/memory.go
@@ -2,6 +2,7 @@ package fs
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -56,13 +57,14 @@ func (s *memoryGroup) Stats(d *data) (map[string]float64, error) {
 		return nil, err
 	}
 
-	f, err := os.Open(filepath.Join(path, "memory.stat"))
+	// Set stats from memory.stat.
+	statsFile, err := os.Open(filepath.Join(path, "memory.stat"))
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
+	defer statsFile.Close()
 
-	sc := bufio.NewScanner(f)
+	sc := bufio.NewScanner(statsFile)
 	for sc.Scan() {
 		t, v, err := getCgroupParamKeyValue(sc.Text())
 		if err != nil {
@@ -70,5 +72,19 @@ func (s *memoryGroup) Stats(d *data) (map[string]float64, error) {
 		}
 		paramData[t] = v
 	}
+
+	// Set memory usage and max historical usage.
+	params := []string{
+		"usage_in_bytes",
+		"max_usage_in_bytes",
+	}
+	for _, param := range params {
+		value, err := getCgroupParamFloat64(path, fmt.Sprintf("memory.%s", param))
+		if err != nil {
+			return nil, err
+		}
+		paramData[param] = value
+	}
+
 	return paramData, nil
 }
