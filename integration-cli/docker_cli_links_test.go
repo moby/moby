dc605c8be76760951d0d12e67409602c7b4b7973
Solomon Hykes <solomon@docker.com>
2014-05-05 19:51:03 -0700

    Simplify integration test for link + hostname.
    Docker-DCO-1.1-Signed-off-by: Solomon Hykes <solomon@docker.com> (github: shykes)

--- integration-cli/docker_cli_links_test.go
+++ integration-cli/docker_cli_links_test.go
@@ -18,28 +18,13 @@ func TestPingUnlinkedContainers(t *testing.T) {
 }
 
 func TestPingLinkedContainers(t *testing.T) {
-	cmd := exec.Command(dockerBinary, "run", "-d", "--name", "container1", "busybox", "sleep", "10")
-	out, _, err := runCommandWithOutput(cmd)
-	errorOut(err, t, fmt.Sprintf("run container1 failed with errors: %v", err))
+	var out string
+	out, _, _ = cmd(t, "run", "-d", "--name", "container1", "busybox", "sleep", "10")
 	idA := stripTrailingCharacters(out)
-
-	cmd = exec.Command(dockerBinary, "run", "-d", "--name", "container2", "busybox", "sleep", "10")
-	out, _, err = runCommandWithOutput(cmd)
-	errorOut(err, t, fmt.Sprintf("run container2 failed with errors: %v", err))
+	out, _, _ = cmd("run", "-d", "--name", "container2", "busybox", "sleep", "10")
 	idB := stripTrailingCharacters(out)
-
-	cmd = exec.Command(dockerBinary, "run", "--rm", "--link", "container1:alias1", "--link", "container2:alias2", "busybox", "sh", "-c", "ping -c 1 alias1 -W 1 && ping -c 1 alias2 -W 1")
-	out, _, err = runCommandWithOutput(cmd)
-	fmt.Printf("OUT: %s", out)
-	errorOut(err, t, fmt.Sprintf("run ping failed with errors: %v", err))
-
-	cmd = exec.Command(dockerBinary, "kill", idA)
-	_, err = runCommand(cmd)
-	errorOut(err, t, fmt.Sprintf("failed to kill container1: %v", err))
-
-	cmd = exec.Command(dockerBinary, "kill", idB)
-	_, err = runCommand(cmd)
-	errorOut(err, t, fmt.Sprintf("failed to kill container2: %v", err))
-
+	cmd("run", "--rm", "--link", "container1:alias1", "--link", "container2:alias2", "busybox", "sh", "-c", "ping -c 1 alias1 -W 1 && ping -c 1 alias2 -W 1")
+	cmd("kill", idA)
+	cmd("kill", idB)
 	deleteAllContainers()
 }
