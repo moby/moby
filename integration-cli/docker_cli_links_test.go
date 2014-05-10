7cc27b2075d6293ab2f54a7255fcb1b1e062fd91
Fabio Falci <fabiofalci@gmail.com>
2014-05-10 18:27:24 +0100

    Integration test for link and unlink containers
    Docker-DCO-1.1-Signed-off-by: Fabio Falci <fabiofalci@gmail.com> (github: fabiofalci)

--- integration-cli/docker_cli_links_test.go
+++ integration-cli/docker_cli_links_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/dotcloud/docker/pkg/iptables"
 	"os/exec"
 	"testing"
 )
@@ -27,4 +28,29 @@ func TestPingLinkedContainers(t *testing.T) {
 	cmd(t, "kill", idA)
 	cmd(t, "kill", idB)
 	deleteAllContainers()
+}
+
+func TestIpTablesRulesWhenLinkAndUnlink(t *testing.T) {
+	cmd(t, "run", "-d", "--name", "child", "--publish", "8080:80", "busybox", "sleep", "10")
+	cmd(t, "run", "-d", "--name", "parent", "--link", "child:http", "busybox", "sleep", "10")
+
+	childIp := findContainerIp(t, "child")
+	parentIp := findContainerIp(t, "parent")
+
+	sourceRule := []string{"FORWARD", "-i", "docker0", "-o", "docker0", "-p", "tcp", "-s", childIp, "--sport", "80", "-d", parentIp, "-j", "ACCEPT"}
+	destinationRule := []string{"FORWARD", "-i", "docker0", "-o", "docker0", "-p", "tcp", "-s", parentIp, "--dport", "80", "-d", childIp, "-j", "ACCEPT"}
+	if !iptables.Exists(sourceRule...) || !iptables.Exists(destinationRule...) {
+		t.Fatal("Iptables rules not found")
+	}
+
+	cmd(t, "rm", "--link", "parent/http")
+	if iptables.Exists(sourceRule...) || iptables.Exists(destinationRule...) {
+		t.Fatal("Iptables rules should be removed when unlink")
+	}
+
+	cmd(t, "kill", "child")
+	cmd(t, "kill", "parent")
+	deleteAllContainers()
+
+	logDone("link - verify iptables when link and unlink")
 }
