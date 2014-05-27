8b77e0183eaa7b3a87921f9655e2799d300bf775
Erik Hollensbe <github@hollensbe.org>
2014-05-27 12:04:20 -0700

    Fix race condition in CLI tests: diff was not acquiring a container lock
    Docker-DCO-1.1-Signed-off-by: Erik Hollensbe <github@hollensbe.org> (github: erikh)

--- daemon/container.go
+++ daemon/container.go
@@ -672,6 +672,8 @@ func (container *Container) Mount() error {
 }
 
 func (container *Container) Changes() ([]archive.Change, error) {
+	container.Lock()
+	defer container.Unlock()
 	return container.daemon.Changes(container)
 }
 
