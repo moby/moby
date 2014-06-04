83249767e6a1283a11718c2440e70b8e997c2d56
Alexander Larsson <alexl@redhat.com>
2014-06-04 11:23:17 +0200

    New implementation of /run support
    This mounts a /run tmpfs into the container, with the initial contents
    copies from the /run in the base image, unless NoRunFs is set in the
    HostConfig.
    
    Additionally NoRunFs is always set during a docker build, which means
    any setup of /run in a Dockerfile is saved in the image to be copied
    into the final /run tmpfs when a container is started.
    
    Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)

--- graph/graph.go
+++ graph/graph.go
@@ -257,6 +257,7 @@ func SetupInitLayer(initLayer string) error {
 		"/dev/pts":         "dir",
 		"/dev/shm":         "dir",
 		"/proc":            "dir",
+		"/run":             "dir",
 		"/sys":             "dir",
 		"/.dockerinit":     "file",
 		"/.dockerenv":      "file",
