2edfb1e696d8f46ea4276b3952078b1553ea42eb
LK4D4 <lk4d4math@gmail.com>
2014-06-28 12:20:31 +0400

    Update go-patricia to v1.0.1
    Docker-DCO-1.1-Signed-off-by: Alexandr Morozov <lk4d4math@gmail.com> (github: LK4D4)

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -47,7 +47,7 @@ clone git github.com/gorilla/mux 136d54f81f
 
 clone git github.com/syndtr/gocapability 3c85049eae
 
-clone git github.com/tchap/go-patricia v1.0.0 
+clone git github.com/tchap/go-patricia v1.0.1
 
 clone hg code.google.com/p/go.net 84a4013f96e0
 
