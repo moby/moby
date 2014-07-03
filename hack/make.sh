a6085fd430e8f155e4a0738de752df3c77bc132f
Tianon Gravi <admwiggin@gmail.com>
2014-07-03 15:11:32 -0600

    Add a bunch more tweaks to the parallel test compilation
    - put all the precompiled test binaries in $DEST so they show up in bundles and can be re-run individually afterwards
    - support cases where parallel is not installed (when using dyntest-unit, for example, this is much more common, since it's designed to be run outside the Dockerfile)
    - use "mktemp -d" instead of "/tmp" directly for our temporary parallel HOME
    - update the default PARALLEL_JOBS to be the value of "nproc" instead of 0, since "0 means as many as possible" (see https://www.gnu.org/software/parallel/man.html#jobs_n)
    
    Docker-DCO-1.1-Signed-off-by: Andrew Page <admwiggin@gmail.com> (github: tianon)

--- hack/make.sh
+++ hack/make.sh
@@ -159,6 +159,7 @@ go_test_dir() {
 # Compile phase run by parallel in test-unit. No support for coverpkg
 go_compile_test_dir() {
 	dir=$1
+	out_file="$DEST/precompiled/$dir.test"
 	testcover=()
 	if [ "$HAVE_GO_TEST_COVER" ]; then
 		# if our current go install has -cover, we want to use it :)
@@ -167,12 +168,16 @@ go_compile_test_dir() {
 		coverprofile="$DEST/coverprofiles/${coverprofile//\//-}"
 		testcover=( -cover -coverprofile "$coverprofile" ) # missing $coverpkg
 	fi
-	(
+	if [ "$BUILDFLAGS_FILE" ]; then
 		readarray -t BUILDFLAGS < "$BUILDFLAGS_FILE"
+	fi
+	(
 		cd "$dir"
 		go test "${testcover[@]}" -ldflags "$LDFLAGS" "${BUILDFLAGS[@]}" $TESTFLAGS -c
-		echo "$dir"
 	)
+	mkdir -p "$(dirname "$out_file")"
+	mv "$dir/$(basename "$dir").test" "$out_file"
+	echo "Precompiled: github.com/dotcloud/docker${dir#.}"
 }
 
 # This helper function walks the current directory looking for directories
