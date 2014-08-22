64715b4c40e23fa238eaf8838ca150263033bb96
Lei Jitang <leijitang@huawei.com>
2014-08-22 12:59:01 +0800

    Fix the bug of docker import command when import a image with a tag use REPOSITORY:TAG format

--- api/client/commands.go
+++ api/client/commands.go
@@ -1101,7 +1101,8 @@ func (cli *DockerCli) CmdImport(args ...string) error {
 
 	if repository != "" {
 		//Check if the given image name can be resolved
-		if _, _, err := registry.ResolveRepositoryName(repository); err != nil {
+		repo, _ := parsers.ParseRepositoryTag(repository)
+		if _, _, err := registry.ResolveRepositoryName(repo); err != nil {
 			return err
 		}
 	}
