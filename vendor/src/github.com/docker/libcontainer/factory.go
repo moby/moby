c604dc292e72c0cbb84184b92b4b2bd3631c862e
Michael Crosby <crosbymichael@gmail.com>
2014-09-09 10:49:57 -0700

    Update libcontainer to 84ad9386a0240acb7475429a835
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- vendor/src/github.com/docker/libcontainer/factory.go
+++ vendor/src/github.com/docker/libcontainer/factory.go
@@ -1,14 +1,20 @@
 package libcontainer
 
 type Factory interface {
-	// Creates a new container in the given path. A unique ID is generated for the container and
-	// starts the initial process inside the container.
+
+	// Creates a new container with the given id and starts the initial process inside it.
+	// id must be a string containing only letters, digits and underscores and must contain
+	// between 1 and 1024 characters, inclusive.
+	//
+	// The id must not already be in use by an existing container. Containers created using
+	// a factory with the same path (and file system) must have distinct ids.
 	//
 	// Returns the new container with a running process.
 	//
 	// Errors:
-	// Path already exists
-	// Config or initialConfig is invalid
+	// id is already in use by a container
+	// id has incorrect format
+	// config is invalid
 	// System error
 	//
 	// On error, any partially created container parts are cleaned up (the operation is atomic).
