3ec564bfda57f068aac813f4b985385c1c7e4f23
unclejack <unclejacksons@gmail.com>
2014-09-12 16:51:21 +0300

    integ-cli: better debug output for run & import
    Docker-DCO-1.1-Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com> (github: unclejack)

--- integration-cli/docker_cli_import_test.go
+++ integration-cli/docker_cli_import_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
 	"strings"
 	"testing"
@@ -10,7 +9,9 @@ import (
 func TestImportDisplay(t *testing.T) {
 	importCmd := exec.Command(dockerBinary, "import", "https://github.com/ewindisch/docker-cirros/raw/master/cirros-0.3.0-x86_64-lxc.tar.gz")
 	out, _, err := runCommandWithOutput(importCmd)
-	errorOut(err, t, fmt.Sprintf("import failed with errors: %v", err))
+	if err != nil {
+		t.Errorf("import failed with errors: %v, output: %q", err, out)
+	}
 
 	if n := strings.Count(out, "\n"); n != 2 {
 		t.Fatalf("display is messed up: %d '\\n' instead of 2", n)
