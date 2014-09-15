ada883b198aa5e7a42d344174dcec04933c6fb2a
unclejack <unclejacksons@gmail.com>
2014-09-16 00:13:54 +0300

    graph: validate tags properly & add unit tests
    Docker-DCO-1.1-Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com> (github: unclejack)

--- graph/tags_unit_test.go
+++ graph/tags_unit_test.go
@@ -114,3 +114,21 @@ func TestLookupImage(t *testing.T) {
 		t.Errorf("Expected 1 image, none found")
 	}
 }
+
+func TestValidTagName(t *testing.T) {
+	validTags := []string{"9", "foo", "foo-test", "bar.baz.boo"}
+	for _, tag := range validTags {
+		if err := validateTagName(tag); err != nil {
+			t.Errorf("'%s' should've been a valid tag", tag)
+		}
+	}
+}
+
+func TestInvalidTagName(t *testing.T) {
+	validTags := []string{"-9", ".foo", "-test", ".", "-"}
+	for _, tag := range validTags {
+		if err := validateTagName(tag); err == nil {
+			t.Errorf("'%s' shouldn't have been a valid tag", tag)
+		}
+	}
+}
