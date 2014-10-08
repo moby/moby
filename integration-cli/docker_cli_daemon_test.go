9acf7c765c7e074f6c75eaf162ca06ecfe40d692
Brian Goff <cpuguy83@gmail.com>
2014-10-08 13:13:32 -0400

    Restore volume refs after daemon restart
    Volume refs were not being restored on daemon restart.
    This made it possible to remove a volume being used by other containers
    after a daemon restart.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- integration-cli/docker_cli_daemon_test.go
+++ integration-cli/docker_cli_daemon_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
 	"strings"
 	"testing"
 )
@@ -47,4 +49,36 @@ func TestDaemonRestartWithRunningContainersPorts(t *testing.T) {
 	testRun(map[string]bool{"top1": true, "top2": false}, "After daemon restart: ")
 
 	logDone("daemon - running containers on daemon restart")
+}
+
+func TestDaemonRestartWithVolumesRefs(t *testing.T) {
+	d := NewDaemon(t)
+	if err := d.StartWithBusybox(); err != nil {
+		t.Fatal(err)
+	}
+	defer d.Stop()
+
+	if out, err := d.Cmd("run", "-d", "--name", "volrestarttest1", "-v", "/foo", "busybox"); err != nil {
+		t.Fatal(err, out)
+	}
+	if err := d.Restart(); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := d.Cmd("run", "-d", "--volumes-from", "volrestarttest1", "--name", "volrestarttest2", "busybox"); err != nil {
+		t.Fatal(err)
+	}
+	if out, err := d.Cmd("rm", "-fv", "volrestarttest2"); err != nil {
+		t.Fatal(err, out)
+	}
+	v, err := d.Cmd("inspect", "--format", "{{ json .Volumes }}", "volrestarttest1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	volumes := make(map[string]string)
+	json.Unmarshal([]byte(v), &volumes)
+	if _, err := os.Stat(volumes["/foo"]); err != nil {
+		t.Fatalf("Expected volume to exist: %s - %s", volumes["/foo"], err)
+	}
+
+	logDone("daemon - volume refs are restored")
 }
