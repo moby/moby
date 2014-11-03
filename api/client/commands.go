4deac03c65edf34affd66abd3ef8fb88287d2f5a
Oh Jinkyun <tintypemolly@gmail.com>
2014-11-03 20:11:29 +0900

    Fix for #8777
    Now filter name is trimmed and lowercased before evaluation for case
    insensitive and whitespace trimemd check.
    
    Signed-off-by: Oh Jinkyun <tintypemolly@gmail.com>

--- api/client/commands.go
+++ api/client/commands.go
@@ -47,6 +47,10 @@ const (
 	tarHeaderSize = 512
 )
 
+var (
+	acceptedImageFilterTags = map[string]struct{}{"dangling": {}}
+)
+
 func (cli *DockerCli) CmdHelp(args ...string) error {
 	if len(args) > 1 {
 		method, exists := cli.getMethod(args[:2]...)
@@ -1333,6 +1337,12 @@ func (cli *DockerCli) CmdImages(args ...string) error {
 		imageFilterArgs, err = filters.ParseFlag(f, imageFilterArgs)
 		if err != nil {
 			return err
+		}
+	}
+
+	for name := range imageFilterArgs {
+		if _, ok := acceptedImageFilterTags[name]; !ok {
+			return fmt.Errorf("Invalid filter '%s'", name)
 		}
 	}
 
