6cbe1fa726fb88a1743c3a3da5e699c9bb3ae55a
Alexandr Morozov <lk4d4@docker.com>
2014-11-05 12:24:15 -0800

    Make /etc/hosts records consistent
    Fixes #8972
    
    Signed-off-by: Alexandr Morozov <lk4d4@docker.com>

--- pkg/networkfs/etchosts/etchosts.go
+++ pkg/networkfs/etchosts/etchosts.go
@@ -3,40 +3,54 @@ package etchosts
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"regexp"
 )
 
-var defaultContent = map[string]string{
-	"localhost":                            "127.0.0.1",
-	"localhost ip6-localhost ip6-loopback": "::1",
-	"ip6-localnet":                         "fe00::0",
-	"ip6-mcastprefix":                      "ff00::0",
-	"ip6-allnodes":                         "ff02::1",
-	"ip6-allrouters":                       "ff02::2",
+type Record struct {
+	Hosts string
+	IP    string
 }
 
-func Build(path, IP, hostname, domainname string, extraContent *map[string]string) error {
+func (r Record) WriteTo(w io.Writer) (int64, error) {
+	n, err := fmt.Fprintf(w, "%s\t%s\n", r.IP, r.Hosts)
+	return int64(n), err
+}
+
+var defaultContent = []Record{
+	{Hosts: "localhost", IP: "127.0.0.1"},
+	{Hosts: "localhost ip6-localhost ip6-loopback", IP: "::1"},
+	{Hosts: "ip6-localnet", IP: "fe00::0"},
+	{Hosts: "ip6-mcastprefix", IP: "ff00::0"},
+	{Hosts: "ip6-allnodes", IP: "ff02::1"},
+	{Hosts: "ip6-allrouters", IP: "ff02::2"},
+}
+
+func Build(path, IP, hostname, domainname string, extraContent []Record) error {
 	content := bytes.NewBuffer(nil)
 	if IP != "" {
+		var mainRec Record
+		mainRec.IP = IP
 		if domainname != "" {
-			content.WriteString(fmt.Sprintf("%s\t%s.%s %s\n", IP, hostname, domainname, hostname))
+			mainRec.Hosts = fmt.Sprintf("%s.%s %s", hostname, domainname, hostname)
 		} else {
-			content.WriteString(fmt.Sprintf("%s\t%s\n", IP, hostname))
+			mainRec.Hosts = hostname
 		}
-	}
-
-	for hosts, ip := range defaultContent {
-		if _, err := content.WriteString(fmt.Sprintf("%s\t%s\n", ip, hosts)); err != nil {
+		if _, err := mainRec.WriteTo(content); err != nil {
 			return err
 		}
 	}
 
-	if extraContent != nil {
-		for hosts, ip := range *extraContent {
-			if _, err := content.WriteString(fmt.Sprintf("%s\t%s\n", ip, hosts)); err != nil {
-				return err
-			}
+	for _, r := range defaultContent {
+		if _, err := r.WriteTo(content); err != nil {
+			return err
+		}
+	}
+
+	for _, r := range extraContent {
+		if _, err := r.WriteTo(content); err != nil {
+			return err
 		}
 	}
 
