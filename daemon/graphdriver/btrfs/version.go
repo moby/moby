25154682a5cd57aa4fc3ef88baeee3ce1f204060
Vincent Batts <vbatts@redhat.com>
2014-11-06 15:56:13 -0500

    btrfs: build tag to enable showing version info
    be default it is on, with build tags to disable the version info
    
    Signed-off-by: Vincent Batts <vbatts@redhat.com>

--- daemon/graphdriver/btrfs/version.go
+++ daemon/graphdriver/btrfs/version.go
@@ -1,9 +1,18 @@
-// +build linux
+// +build linux,!btrfs_noversion
 
 package btrfs
 
 /*
 #include <btrfs/version.h>
+
+// because around version 3.16, they did not define lib version yet
+int my_btrfs_lib_version() {
+#ifdef BTRFS_LIB_VERSION
+  return BTRFS_LIB_VERSION;
+#else
+  return -1;
+#endif
+}
 */
 import "C"
 
