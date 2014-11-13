b64c9b521ab4e4082ed874a23a493f4a266304d5
Ahmet Alp Balkan <ahmetb@microsoft.com>
2014-11-13 10:50:57 -0800

    Extract TreeSize to daemon build
    TreeSize uses syscall.Stat_t which is not available on Windows.
    It's called only on daemon path, therefore extracting it to daemon
    with build tag 'daemon'
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@microsoft.com>

--- utils/utils.go
+++ utils/utils.go
@@ -18,7 +18,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"syscall"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/dockerversion"
@@ -451,36 +450,6 @@ func ReadSymlinkedDirectory(path string) (string, error) {
 		return "", fmt.Errorf("canonical path points to a file '%s'", realPath)
 	}
 	return realPath, nil
-}
-
-// TreeSize walks a directory tree and returns its total size in bytes.
-func TreeSize(dir string) (size int64, err error) {
-	data := make(map[uint64]struct{})
-	err = filepath.Walk(dir, func(d string, fileInfo os.FileInfo, e error) error {
-		// Ignore directory sizes
-		if fileInfo == nil {
-			return nil
-		}
-
-		s := fileInfo.Size()
-		if fileInfo.IsDir() || s == 0 {
-			return nil
-		}
-
-		// Check inode to handle hard links correctly
-		inode := fileInfo.Sys().(*syscall.Stat_t).Ino
-		// inode is not a uint64 on all platforms. Cast it to avoid issues.
-		if _, exists := data[uint64(inode)]; exists {
-			return nil
-		}
-		// inode is not a uint64 on all platforms. Cast it to avoid issues.
-		data[uint64(inode)] = struct{}{}
-
-		size += s
-
-		return nil
-	})
-	return
 }
 
 // ValidateContextDirectory checks if all the contents of the directory
