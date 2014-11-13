376ae7780bf04d89ad1532f1c2a752535fa7ac7d
Ahmet Alp Balkan <ahmetb@microsoft.com>
2014-11-13 10:48:19 -0800

    Consolidate tmpdir implementations, include Windows
    Signed-off-by: Ahmet Alp Balkan <ahmetb@microsoft.com>

--- utils/tmpdir.go
+++ utils/tmpdir.go
@@ -1,12 +1,16 @@
-// +build !darwin,!dragonfly,!freebsd,!linux,!netbsd,!openbsd
-
 package utils
 
 import (
 	"os"
+	"path/filepath"
 )
 
 // TempDir returns the default directory to use for temporary files.
-func TempDir(rootdir string) (string error) {
-	return os.TempDir(), nil
+func TempDir(rootDir string) (string, error) {
+	var tmpDir string
+	if tmpDir = os.Getenv("DOCKER_TMPDIR"); tmpDir == "" {
+		tmpDir = filepath.Join(rootDir, "tmp")
+	}
+	err := os.MkdirAll(tmpDir, 0700)
+	return tmpDir, err
 }
