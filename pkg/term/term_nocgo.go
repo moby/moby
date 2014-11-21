244af451e9bdff5c87bca84e4c15193fc9eebc64
Yohei Ueda <yohei@jp.ibm.com>
2014-11-21 22:12:03 +0900

    Use termios via CGO
    Signed-off-by: Yohei Ueda <yohei@jp.ibm.com>

---
+++ pkg/term/term_nocgo.go
@@ -0,0 +1,18 @@
+// +build !windows,!cgo
+
+package term
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+func tcget(fd uintptr, p *Termios) syscall.Errno {
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(getTermios), uintptr(unsafe.Pointer(p)))
+	return err
+}
+
+func tcset(fd uintptr, p *Termios) syscall.Errno {
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, setTermios, uintptr(unsafe.Pointer(p)))
+	return err
+}
