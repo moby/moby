8f8d24cb1c762e74833bbb9b6e2b58438b42d36b
Rémy Greinhofer <remy.greinhofer@livelovely.com>
2014-12-11 15:09:48 -0800

    Report number of times that docker has auto-restarted a container
    Fixes #9469.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@livelovely.com>

--- daemon/inspect.go
+++ daemon/inspect.go
@@ -41,6 +41,7 @@ func (daemon *Daemon) ContainerInspect(job *engine.Job) engine.Status {
 		out.Set("HostnamePath", container.HostnamePath)
 		out.Set("HostsPath", container.HostsPath)
 		out.Set("Name", container.Name)
+		out.SetInt("RestartCount", container.RestartCount)
 		out.Set("Driver", container.Driver)
 		out.Set("ExecDriver", container.ExecDriver)
 		out.Set("MountLabel", container.MountLabel)
