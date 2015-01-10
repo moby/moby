d43f0b9fc5ec3eae816466ec0307682c13945b31
Tianon Gravi <admwiggin@gmail.com>
2015-01-09 17:28:40 -0700

    Fix a few minor issues with building/running inside msysGit
    Signed-off-by: Andrew "Tianon" Page <admwiggin@gmail.com>

--- integration-cli/docker_test_vars.go
+++ integration-cli/docker_test_vars.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"runtime"
 )
 
 var (
@@ -26,28 +25,15 @@ var (
 	workingDirectory string
 )
 
-func binarySearchCommand() *exec.Cmd {
-	if runtime.GOOS == "windows" {
-		// Windows where.exe is included since Windows Server 2003. It accepts
-		// wildcards, which we use here to match the development builds binary
-		// names (such as docker-$VERSION.exe).
-		return exec.Command("where.exe", "docker*.exe")
-	}
-	return exec.Command("which", "docker")
-}
-
 func init() {
 	if dockerBin := os.Getenv("DOCKER_BINARY"); dockerBin != "" {
 		dockerBinary = dockerBin
-	} else {
-		whichCmd := binarySearchCommand()
-		out, _, err := runCommandWithOutput(whichCmd)
-		if err == nil {
-			dockerBinary = stripTrailingCharacters(out)
-		} else {
-			fmt.Printf("ERROR: couldn't resolve full path to the Docker binary (%v)", err)
-			os.Exit(1)
-		}
+	}
+	var err error
+	dockerBinary, err = exec.LookPath(dockerBinary)
+	if err != nil {
+		fmt.Printf("ERROR: couldn't resolve full path to the Docker binary (%v)", err)
+		os.Exit(1)
 	}
 	if registryImage := os.Getenv("REGISTRY_IMAGE"); registryImage != "" {
 		registryImageName = registryImage
