f138f7bd50a1c5a435f3146f0b0298a2a4e260ce
Alexander Morozov <lk4d4@docker.com>
2015-01-12 14:30:19 -0800

    Tests for push to registry v2
    Signed-off-by: Alexander Morozov <lk4d4@docker.com>

--- integration-cli/docker_cli_push_test.go
+++ integration-cli/docker_cli_push_test.go
@@ -10,29 +10,27 @@ import (
 
 // pulling an image from the central registry should work
 func TestPushBusyboxImage(t *testing.T) {
-	// skip this test until we're able to use a registry
-	t.Skip()
+	reg, err := newTestRegistryV2(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer reg.Close()
+	repoName := fmt.Sprintf("%v/dockercli/busybox", reg.URL)
 	// tag the image to upload it tot he private registry
-	repoName := fmt.Sprintf("%v/busybox", privateRegistryURL)
 	tagCmd := exec.Command(dockerBinary, "tag", "busybox", repoName)
 	if out, _, err := runCommandWithOutput(tagCmd); err != nil {
 		t.Fatalf("image tagging failed: %s, %v", out, err)
 	}
-
+	defer deleteImages(repoName)
 	pushCmd := exec.Command(dockerBinary, "push", repoName)
 	if out, _, err := runCommandWithOutput(pushCmd); err != nil {
 		t.Fatalf("pushing the image to the private registry has failed: %s, %v", out, err)
 	}
-
-	deleteImages(repoName)
-
 	logDone("push - push busybox to private registry")
 }
 
 // pushing an image without a prefix should throw an error
 func TestPushUnprefixedRepo(t *testing.T) {
-	// skip this test until we're able to use a registry
-	t.Skip()
 	pushCmd := exec.Command(dockerBinary, "push", "busybox")
 	if out, _, err := runCommandWithOutput(pushCmd); err == nil {
 		t.Fatalf("pushing an unprefixed repo didn't result in a non-zero exit status: %s", out)
