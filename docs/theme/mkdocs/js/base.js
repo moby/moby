60089ace4895d9ab9c9fe9f2fb4b0db8a0d9aeda
Sven Dowideit <SvenDowideit@home.org.au>
2015-01-26 21:44:17 +1000

    as we're not using the search suggestion feature only load the search_content when we have a search ?q= param
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>
    
    Docker-DCO-1.1-Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au> (github: SvenDowideit)

--- docs/theme/mkdocs/js/base.js
+++ docs/theme/mkdocs/js/base.js
@@ -45,11 +45,17 @@ $(document).ready(function ()
     },
   });
 
-  // Tipue Search activation
-  $('#tipue_search_input').tipuesearch({
-    'mode': 'json',
-    'contentLocation': '/search_content.json.gz'
-  });
+  function getURLP(name)
+  {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20')) || null;
+  }
+  if (getURLP("q")) {
+    // Tipue Search activation
+    $('#tipue_search_input').tipuesearch({
+      'mode': 'json',
+      'contentLocation': '/search_content.json.gz'
+    });
+  }
 
 });
 
