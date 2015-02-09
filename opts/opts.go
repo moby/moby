1ce9fa1c601cc6f09f9a39df535515aec355e95e
Phil Estes <estesp@linux.vnet.ibm.com>
2015-02-09 09:59:05 -0500

    Add more helpful error message for -add-host Fixes: #10655
    As noted in the issue, bad format was being returned even if the format
    was appropriate, but the IP was invalid.  This adds a better error
    message for when the IP address fails validation.
    
    Docker-DCO-1.1-Signed-off-by: Phil Estes <estesp@linux.vnet.ibm.com> (github: estesp)

--- opts/opts.go
+++ opts/opts.go
@@ -207,10 +207,10 @@ func ValidateExtraHost(val string) (string, error) {
 	// allow for IPv6 addresses in extra hosts by only splitting on first ":"
 	arr := strings.SplitN(val, ":", 2)
 	if len(arr) != 2 || len(arr[0]) == 0 {
-		return "", fmt.Errorf("bad format for add-host: %s", val)
+		return "", fmt.Errorf("bad format for add-host: %q", val)
 	}
 	if _, err := ValidateIPAddress(arr[1]); err != nil {
-		return "", fmt.Errorf("bad format for add-host: %s", val)
+		return "", fmt.Errorf("invalid IP address in add-host: %q", arr[1])
 	}
 	return val, nil
 }
