28842d3f093e69cf62a090a532aacba1e6ff6d1c
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-02-17 12:27:07 -0800

    pkg/archive: Canonicalize stored paths
    Currently pkg/archive stores nested windows files with
    backslashes (e.g. `dir\`, `dir\file.txt`) and this causes
    tar not being correctly extracted on Linux daemon.
    
    This change assures we canonicalize all paths to unix
    paths and add them to tar with that name independent of platform.
    
    Fixes the following test cases for Windows CI:
    - TestBuildAddFileWithWhitespace
    - TestBuildCopyFileWithWhitespace
    - TestBuildAddDirContentToRoot
    - TestBuildAddDirContentToExistingDir
    - TestBuildCopyDirContentToRoot
    - TestBuildCopyDirContentToExistDir
    - TestBuildDockerignore
    - TestBuildEnvUsage
    - TestBuildEnvUsage2
    
    Signed-off-by: Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>

--- pkg/archive/archive_windows.go
+++ pkg/archive/archive_windows.go
@@ -3,8 +3,25 @@
 package archive
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/docker/docker/vendor/src/code.google.com/p/go/src/pkg/archive/tar"
 )
+
+// canonicalTarNameForPath returns platform-specific filepath
+// to canonical posix-style path for tar archival. p is relative
+// path.
+func canonicalTarNameForPath(p string) (string, error) {
+	// windows: convert windows style relative path with backslashes
+	// into forward slashes. since windows does not allow '/' or '\'
+	// in file names, it is mostly safe to replace however we must
+	// check just in case
+	if strings.Contains(p, "/") {
+		return "", fmt.Errorf("windows path contains forward slash: %s", p)
+	}
+	return strings.Replace(p, "\\", "/", -1), nil
+}
 
 func setHeaderForSpecialDevice(hdr *tar.Header, ta *tarAppender, name string, stat interface{}) (nlink uint32, inode uint64, err error) {
 	// do nothing. no notion of Rdev, Inode, Nlink in stat on Windows
---
+++ pkg/archive/archive_windows_test.go
@@ -0,0 +1,48 @@
+// +build windows
+
+package archive
+
+import (
+	"testing"
+)
+
+func TestCanonicalTarNameForPath(t *testing.T) {
+	cases := []struct {
+		in, expected string
+		shouldFail   bool
+	}{
+		{"foo", "foo", false},
+		{"foo/bar", "___", true}, // unix-styled windows path must fail
+		{`foo\bar`, "foo/bar", false},
+		{`foo\bar`, "foo/bar/", false},
+	}
+	for _, v := range cases {
+		if out, err := canonicalTarNameForPath(v.in); err != nil && !v.shouldFail {
+			t.Fatalf("cannot get canonical name for path: %s: %v", v.in, err)
+		} else if v.shouldFail && err == nil {
+			t.Fatalf("canonical path call should have pailed with error. in=%s out=%s", v.in, out)
+		} else if !v.shouldFail && out != v.expected {
+			t.Fatalf("wrong canonical tar name. expected:%s got:%s", v.expected, out)
+		}
+	}
+}
+
+func TestCanonicalTarName(t *testing.T) {
+	cases := []struct {
+		in       string
+		isDir    bool
+		expected string
+	}{
+		{"foo", false, "foo"},
+		{"foo", true, "foo/"},
+		{`foo\bar`, false, "foo/bar"},
+		{`foo\bar`, true, "foo/bar/"},
+	}
+	for _, v := range cases {
+		if out, err := canonicalTarName(v.in, v.isDir); err != nil {
+			t.Fatalf("cannot get canonical name for path: %s: %v", v.in, err)
+		} else if out != v.expected {
+			t.Fatalf("wrong canonical tar name. expected:%s got:%s", v.expected, out)
+		}
+	}
+}
