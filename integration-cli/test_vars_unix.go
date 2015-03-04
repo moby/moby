1a22418f9f1573ca8521831d52c8e0562cb3ef8f
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-03-03 18:40:16 -0800

    pkg/archive: adjust chmod bits on windows
    This change modifies the chmod bits of build context archives built on
    windows to preserve the execute bit and remove the r/w bits from
    grp/others.
    
    Also adjusted integ-cli tests to verify permissions based on the platform
    the tests are running.
    
    Fixes #11047.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>

--- integration-cli/test_vars_unix.go
+++ integration-cli/test_vars_unix.go
@@ -5,4 +5,6 @@ package main
 const (
 	// identifies if test suite is running on a unix platform
 	isUnixCli = true
+
+	expectedFileChmod = "-rw-r--r--"
 )
--- pkg/archive/archive_windows_test.go
+++ pkg/archive/archive_windows_test.go
@@ -3,6 +3,7 @@
 package archive
 
 import (
+	"os"
 	"testing"
 )
 
@@ -43,6 +44,23 @@ func TestCanonicalTarName(t *testing.T) {
 			t.Fatalf("cannot get canonical name for path: %s: %v", v.in, err)
 		} else if out != v.expected {
 			t.Fatalf("wrong canonical tar name. expected:%s got:%s", v.expected, out)
+		}
+	}
+}
+
+func TestChmodTarEntry(t *testing.T) {
+	cases := []struct {
+		in, expected os.FileMode
+	}{
+		{0000, 0100},
+		{0777, 0711},
+		{0644, 0700},
+		{0755, 0711},
+		{0444, 0500},
+	}
+	for _, v := range cases {
+		if out := chmodTarEntry(v.in); out != v.expected {
+			t.Fatalf("wrong chmod. expected:%v got:%v", v.expected, out)
 		}
 	}
 }
