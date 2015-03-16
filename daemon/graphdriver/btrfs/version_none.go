df6b4d786d840840ccf9ee0bea928882edcda6b3
Dan Walsh <dwalsh@redhat.com>
2015-03-16 14:53:41 -0400

    Btrfs has eliminated the BTRFS_BUILD_VERSION in latest version
    They say we should only use the BTRFS_LIB_VERSION
    
    They will no longer support this since it had to be managed manually
    
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)

--- daemon/graphdriver/btrfs/version_none.go
+++ daemon/graphdriver/btrfs/version_none.go
@@ -5,9 +5,6 @@ package btrfs
 // TODO(vbatts) remove this work-around once supported linux distros are on
 // btrfs utililties of >= 3.16.1
 
-func BtrfsBuildVersion() string {
-	return "-"
-}
 func BtrfsLibVersion() int {
 	return -1
 }
