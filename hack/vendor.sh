631ce6a39838a07b7f036f02d5d2f9626d27127f
Michael Crosby <crosbymichael@gmail.com>
2015-03-23 11:49:02 -0700

    Update libcontainer to f8daab8a96fe2c73974073696d0
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -75,7 +75,7 @@ rm -rf src/github.com/docker/distribution
 mkdir -p src/github.com/docker/distribution
 mv tmp-digest src/github.com/docker/distribution/digest
 
-clone git github.com/docker/libcontainer 4a72e540feb67091156b907c4700e580a99f5a9d
+clone git github.com/docker/libcontainer f8daab8a96fe2c73974073696d00deb4ffb40d47
 # see src/github.com/docker/libcontainer/update-vendor.sh which is the "source of truth" for libcontainer deps (just like this file)
 rm -rf src/github.com/docker/libcontainer/vendor
 eval "$(grep '^clone ' src/github.com/docker/libcontainer/update-vendor.sh | grep -v 'github.com/codegangsta/cli' | grep -v 'github.com/Sirupsen/logrus')"
