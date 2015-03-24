043bc601337ae180459b60978f8f0eebb422f447
cc272309126 <cc272309126@gmail.com>
2015-03-24 22:48:08 +0800

    systemd that doesn't use DefaultDependencies=false should run a container successfully. see the issue:https://github.com/docker/docker/issues/11639
    Signed-off-by: Chen Chao <chenchao@qiyi.com>

--- vendor/src/github.com/docker/libcontainer/cgroups/systemd/apply_systemd.go
+++ vendor/src/github.com/docker/libcontainer/cgroups/systemd/apply_systemd.go
@@ -91,8 +91,14 @@ func UseSystemd() bool {
 		ddf := newProp("DefaultDependencies", false)
 		if _, err := theConn.StartTransientUnit("docker-systemd-test-default-dependencies.scope", "replace", ddf); err != nil {
 			if dbusError, ok := err.(dbus.Error); ok {
-				if dbusError.Name == "org.freedesktop.DBus.Error.PropertyReadOnly" {
-					hasTransientDefaultDependencies = false
+				if strings.Contains(dbusError.Name, "org.freedesktop.systemd1.UnitExists") {
+					if err := theConn.SetUnitProperties("docker-systemd-test-default-dependencies.scope", true, ddf); err !=nil{
+						if dbusError, ok := err.(dbus.Error); ok{
+							if strings.Contains(dbusError.Name, "org.freedesktop.DBus.Error.PropertyReadOnly") {
+								hasTransientDefaultDependencies = false
+							}
+						}
+					}
 				}
 			}
 		}
