7f5937d46cacf43cb89ff3ec9513dea8231416fa
Vincent Batts <vbatts@redhat.com>
2015-03-25 13:26:41 -0400

    btrfs: #ifdef for build version
    We removed it, because upstream removed it. But now it will be coming
    back, so work with it either way.
    
    Signed-off-by: Vincent Batts <vbatts@redhat.com>
    
    Docker-DCO-1.1-Signed-off-by: Jessie Frazelle <princess@docker.com> (github: jfrazelle)
    
    Docker-DCO-1.1-Signed-off-by: Jessie Frazelle <jess@docker.com> (github: jfrazelle)

--- daemon/graphdriver/btrfs/version_none.go
+++ daemon/graphdriver/btrfs/version_none.go
@@ -5,6 +5,10 @@ package btrfs
 // TODO(vbatts) remove this work-around once supported linux distros are on
 // btrfs utililties of >= 3.16.1
 
+func BtrfsBuildVersion() string {
+	return "-"
+}
+
 func BtrfsLibVersion() int {
 	return -1
 }
