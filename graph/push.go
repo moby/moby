rhatdan/docker

Confirm a push to public Docker registry
Ask the user to confirm a push of image to public Docker registry. The
confirmation can be suppressed with the use of `-f, --force` flag.

No confirmation is needed for push to unofficial registry.

Signed-off-by: Michal Minar <miminar@redhat.com>

graph/push.go
@@ -441,6 +441,13 @@ func (s *TagStore) CmdPush(job *engine.Job) engine.Status {
 	tag := job.Getenv("tag")
 	job.GetenvJson("authConfig", authConfig)
 	job.GetenvJson("metaHeaders", &metaHeaders)
+	force := job.GetenvBool("force")
+
+	if repoInfo.Index.Official && !force {
+		return job.Errorf("Error: Status 403 trying to push repository %s to official registry: needs to be forced", localName)
+	} else if repoInfo.Index.Official && force {
+		log.Infof("Push of %s to official registry has been forced", localName)
+	}
 
 	if _, err := s.poolAdd("push", repoInfo.LocalName); err != nil {
 		return job.Error(err)

moby/moby

graph/push.go
@@ -506,6 +506,13 @@ func (s *TagStore) CmdPush(job *engine.Job) error {
 	tag := job.Getenv("tag")
 	job.GetenvJson("authConfig", authConfig)
 	job.GetenvJson("metaHeaders", &metaHeaders)
+	force := job.GetenvBool("force")
+
+	if repoInfo.Index.Official && !force {
+		return fmt.Errorf("Error: Status 403 trying to push repository %s to official registry: needs to be forced", localName)
+	} else if repoInfo.Index.Official && force {
+		logrus.Infof("Push of %s to official registry has been forced", localName)
+	}
 
 	if _, err := s.poolAdd("push", repoInfo.LocalName); err != nil {
 		return err