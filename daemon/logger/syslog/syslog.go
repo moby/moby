ce27fa2716272e0304aae0461304ea101e0d6312
Deng Guangxing <dengguangxing@huawei.com>
2015-04-13 17:56:12 +0800

    move syslog-tag to syslog.New function
    Signed-off-by: Deng Guangxing <dengguangxing@huawei.com>
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>
    (cherry picked from commit 4f91a333d5c9d66ce109c36e7261dbfd3382ebbf)

--- daemon/logger/syslog/syslog.go
+++ daemon/logger/syslog/syslog.go
@@ -17,29 +17,20 @@ type Syslog struct {
 }
 
 func New(tag string) (logger.Logger, error) {
-	log, err := syslog.New(syslog.LOG_USER, path.Base(os.Args[0]))
+	log, err := syslog.New(syslog.LOG_USER, fmt.Sprintf("%s: <%s> ", path.Base(os.Args[0]), tag))
 	if err != nil {
 		return nil, err
 	}
 	return &Syslog{
 		writer: log,
-		tag:    tag,
 	}, nil
 }
 
 func (s *Syslog) Log(msg *logger.Message) error {
-	logMessage := fmt.Sprintf("%s: %s", s.tag, string(msg.Line))
 	if msg.Source == "stderr" {
-		if err := s.writer.Err(logMessage); err != nil {
-			return err
-		}
-
-	} else {
-		if err := s.writer.Info(logMessage); err != nil {
-			return err
-		}
+		return s.writer.Err(string(msg.Line))
 	}
-	return nil
+	return s.writer.Info(string(msg.Line))
 }
 
 func (s *Syslog) Close() error {
