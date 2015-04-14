6c0ffdd70a5f2f0926dd9db562110752dabb521d
Jana Radhakrishnan <mrjana@docker.com>
2015-04-14 01:37:55 +0000

    Added unsupported implementations for sandbox and sandbox test code
    Signed-off-by: Jana Radhakrishnan <mrjana@docker.com>

---
+++ libnetwork/sandbox/sandbox_unsupported_test.go
@@ -0,0 +1,20 @@
+// +build !linux
+
+package sandbox
+
+import (
+	"errors"
+	"testing"
+)
+
+var (
+	ErrNotImplemented = errors.New("not implemented")
+)
+
+func newKey(t *testing.T) (string, error) {
+	return nil, ErrNotImplemented
+}
+
+func verifySandbox(t *testing.T, s Sandbox) {
+	return
+}
