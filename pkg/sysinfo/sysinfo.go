47e5acfbaefc45e536b953af6bf8a3993669c816
Qiang Huang <h.huangqiang@huawei.com>
2015-04-14 08:38:34 +0800

    add devices cgroup check and errors
    Signed-off-by: Qiang Huang <h.huangqiang@huawei.com>

--- pkg/sysinfo/sysinfo.go
+++ pkg/sysinfo/sysinfo.go
@@ -58,5 +58,11 @@ func New(quiet bool) *SysInfo {
 	} else {
 		sysInfo.AppArmor = true
 	}
+
+	// Check if Devices cgroup is mounted, it is hard requirement for container security.
+	if _, err := cgroups.FindCgroupMountpoint("devices"); err != nil {
+		logrus.Fatalf("Error mounting devices cgroup: %v", err)
+	}
+
 	return sysInfo
 }
