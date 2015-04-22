f25bbedc85e8a99c1389dbe8f48436907ce24526
Michael Crosby <crosbymichael@gmail.com>
2015-04-21 17:31:05 -0700

    Allow libcontainer to eval symlink destination
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>
    
    Add tests for mounting into /proc and /sys
    
    These two locations should be prohibited from mounting volumes into
    those destinations.
    
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- integration-cli/docker_cli_run_test.go
+++ integration-cli/docker_cli_run_test.go
@@ -3107,3 +3107,21 @@ func TestRunReadProcLatency(t *testing.T) {
 	}
 	logDone("run - read /proc/latency_stats")
 }
+
+func TestMountIntoProc(t *testing.T) {
+	defer deleteAllContainers()
+	code, err := runCommand(exec.Command(dockerBinary, "run", "-v", "/proc//sys", "busybox", "true"))
+	if err == nil || code == 0 {
+		t.Fatal("container should not be able to mount into /proc")
+	}
+	logDone("run - mount into proc")
+}
+
+func TestMountIntoSys(t *testing.T) {
+	defer deleteAllContainers()
+	code, err := runCommand(exec.Command(dockerBinary, "run", "-v", "/sys/", "busybox", "true"))
+	if err == nil || code == 0 {
+		t.Fatal("container should not be able to mount into /sys")
+	}
+	logDone("run - mount into sys")
+}
