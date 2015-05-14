6ca6575648adf3f4dac5e4d555ef8bd67cda7cd4
Srini Brahmaroutu <sbrahma@us.ibm.com>
2015-05-14 14:39:44 +0000

    Decoder does not work properly with nested pointers using gcc Signed-off-by: Srini Brahmaroutu <srbrahma@us.ibm.com>

--- runconfig/hostconfig.go
+++ runconfig/hostconfig.go
@@ -234,47 +234,15 @@ type HostConfig struct {
 func MergeConfigs(config *Config, hostConfig *HostConfig) *ContainerConfigWrapper {
 	return &ContainerConfigWrapper{
 		config,
-		&hostConfigWrapper{InnerHostConfig: hostConfig},
+		hostConfig,
+		"", nil,
 	}
-}
-
-type hostConfigWrapper struct {
-	InnerHostConfig *HostConfig `json:"HostConfig,omitempty"`
-	Cpuset          string      `json:",omitempty"` // Deprecated. Exported for backwards compatibility.
-
-	*HostConfig // Deprecated. Exported to read attrubutes from json that are not in the inner host config structure.
-}
-
-func (w hostConfigWrapper) GetHostConfig() *HostConfig {
-	hc := w.HostConfig
-
-	if hc == nil && w.InnerHostConfig != nil {
-		hc = w.InnerHostConfig
-	} else if w.InnerHostConfig != nil {
-		if hc.Memory != 0 && w.InnerHostConfig.Memory == 0 {
-			w.InnerHostConfig.Memory = hc.Memory
-		}
-		if hc.MemorySwap != 0 && w.InnerHostConfig.MemorySwap == 0 {
-			w.InnerHostConfig.MemorySwap = hc.MemorySwap
-		}
-		if hc.CpuShares != 0 && w.InnerHostConfig.CpuShares == 0 {
-			w.InnerHostConfig.CpuShares = hc.CpuShares
-		}
-
-		hc = w.InnerHostConfig
-	}
-
-	if hc != nil && w.Cpuset != "" && hc.CpusetCpus == "" {
-		hc.CpusetCpus = w.Cpuset
-	}
-
-	return hc
 }
 
 func DecodeHostConfig(src io.Reader) (*HostConfig, error) {
 	decoder := json.NewDecoder(src)
 
-	var w hostConfigWrapper
+	var w ContainerConfigWrapper
 	if err := decoder.Decode(&w); err != nil {
 		return nil, err
 	}
