2e12c96048d537633dc5b9d6b38379a6afd92e9d
Derek McGowan <derek@mcgstyle.net>
2015-05-15 16:34:00 -0700

    Remove unused and duplicate error types
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- internal/registryclient/errors.go
+++ internal/registryclient/errors.go
@@ -9,34 +9,6 @@ import (
 	"github.com/docker/distribution/registry/api/v2"
 )
 
-// BlobUploadNotFoundError is returned when making a blob upload operation against an
-// invalid blob upload location url.
-// This may be the result of using a cancelled, completed, or stale upload
-// location.
-type BlobUploadNotFoundError struct {
-	Location string
-}
-
-func (e *BlobUploadNotFoundError) Error() string {
-	return fmt.Sprintf("No blob upload found at Location: %s", e.Location)
-}
-
-// BlobUploadInvalidRangeError is returned when attempting to upload an image
-// blob chunk that is out of order.
-// This provides the known BlobSize and LastValidRange which can be used to
-// resume the upload.
-type BlobUploadInvalidRangeError struct {
-	Location       string
-	LastValidRange int
-	BlobSize       int
-}
-
-func (e *BlobUploadInvalidRangeError) Error() string {
-	return fmt.Sprintf(
-		"Invalid range provided for upload at Location: %s. Last Valid Range: %d, Blob Size: %d",
-		e.Location, e.LastValidRange, e.BlobSize)
-}
-
 // UnexpectedHTTPStatusError is returned when an unexpected HTTP status is
 // returned when making a registry api call.
 type UnexpectedHTTPStatusError struct {
