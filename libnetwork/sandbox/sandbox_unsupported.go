d56c6de9b24e577e8dd5501cefec2b891a54e7a6
John Howard <jhoward@microsoft.com>
2015-05-26 10:46:21 -0700

    Windows: Enable compile
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- libnetwork/sandbox/sandbox_unsupported.go
+++ libnetwork/sandbox/sandbox_unsupported.go
@@ -10,6 +10,12 @@ var (
 
 // NewSandbox provides a new sandbox instance created in an os specific way
 // provided a key which uniquely identifies the sandbox
-func NewSandbox(key string) (Sandbox, error) {
+func NewSandbox(key string, osCreate bool) (Sandbox, error) {
 	return nil, ErrNotImplemented
+}
+
+// GenerateKey generates a sandbox key based on the passed
+// container id.
+func GenerateKey(containerID string) string {
+	return ""
 }
