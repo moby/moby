7d5c0b2b7bcbe27d1b9f59edb5b8bd1bc282a456
David Calavera <david.calavera@gmail.com>
2015-06-02 11:00:54 -0700

    Bump libnetwork for 1.7 release.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -55,9 +55,9 @@ clone hg code.google.com/p/go.net 84a4013f96e0
 clone hg code.google.com/p/gosqlite 74691fb6f837
 
 #get libnetwork packages
-clone git github.com/docker/libnetwork 3daf67270570c1e07e3e3184d46a10f0c5d66f87
-clone git github.com/vishvananda/netns 493029407eeb434d0c2d44e02ea072ff2488d322
-clone git github.com/vishvananda/netlink 20397a138846e4d6590e01783ed023ed7e1c38a6
+clone git github.com/docker/libnetwork 4ded6fe3641b71863cc5985652930ce40efc3af4
+clone git github.com/vishvananda/netns 008d17ae001344769b031375bdb38a86219154c6
+clone git github.com/vishvananda/netlink 8eb64238879fed52fd51c5b30ad20b928fb4c36c
 
 # get distribution packages
 clone git github.com/docker/distribution b9eeb328080d367dbde850ec6e94f1e4ac2b5efe
