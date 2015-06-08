d2d7767e104d83e8b8334d5a2851aaff76013eb9
John Howard <jhoward@microsoft.com>
2015-06-08 13:45:28 -0700

    Windows: Fix PR13278 compile break
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- daemon/volumes_windows.go
+++ daemon/volumes_windows.go
@@ -17,10 +17,6 @@ func (container *Container) setupMounts() ([]execdriver.Mount, error) {
 	return nil, nil
 }
 
-func migrateVolume(id, vfs string) error {
+func (daemon *Daemon) migrateVolume(id, vfs string) error {
 	return nil
-}
-
-func validVolumeLayout(files []os.FileInfo) bool {
-	return true
 }
