be64e94a433a07515b7999f37c11bbb4d7afc471
xiekeyang <xiekeyang@huawei.com>
2015-06-09 10:07:48 +0800

    Registry: remove unwanted return variable name
    Signed-off-by: xiekeyang <xiekeyang@huawei.com>

--- registry/token.go
+++ registry/token.go
@@ -13,7 +13,7 @@ type tokenResponse struct {
 	Token string `json:"token"`
 }
 
-func getToken(username, password string, params map[string]string, registryEndpoint *Endpoint) (token string, err error) {
+func getToken(username, password string, params map[string]string, registryEndpoint *Endpoint) (string, error) {
 	realm, ok := params["realm"]
 	if !ok {
 		return "", errors.New("no realm specified for token auth challenge")
