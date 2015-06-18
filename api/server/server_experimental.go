742db1737c26b07fb08f46ce7391586aec674dd1
Madhu Venugopal <madhu@docker.com>
2015-06-18 08:41:17 -0700

    attaching services api and UI chain to docker parent
    Signed-off-by: Madhu Venugopal <madhu@docker.com>

--- api/server/server_experimental.go
+++ api/server/server_experimental.go
@@ -9,4 +9,9 @@ func (s *Server) registerSubRouter() {
 	subrouter.Methods("GET", "POST", "PUT", "DELETE").HandlerFunc(httpHandler)
 	subrouter = s.router.PathPrefix("/networks").Subrouter()
 	subrouter.Methods("GET", "POST", "PUT", "DELETE").HandlerFunc(httpHandler)
+
+	subrouter = s.router.PathPrefix("/v{version:[0-9.]+}/services").Subrouter()
+	subrouter.Methods("GET", "POST", "PUT", "DELETE").HandlerFunc(httpHandler)
+	subrouter = s.router.PathPrefix("/services").Subrouter()
+	subrouter.Methods("GET", "POST", "PUT", "DELETE").HandlerFunc(httpHandler)
 }
