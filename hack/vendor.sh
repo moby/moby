090d26b3cc865eabaf5c22779487cb403c0dcaf8
Vincent Batts <vbatts@redhat.com>
2015-06-20 10:33:14 -0400

    vendor: adding tar-split dependency for graph
    tar-split is a facility to disassemble and reassemble tar archives
    
    Signed-off-by: Vincent Batts <vbatts@redhat.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -34,8 +34,9 @@ clone git github.com/samuel/go-zookeeper d0e0d8e11f318e000a8cc434616d69e329edc37
 clone git github.com/coreos/go-etcd v2.0.0
 clone git github.com/hashicorp/consul v0.5.2
 
-# get distribution packages
+# get graph and distribution packages
 clone git github.com/docker/distribution 419bbc2da637d9b2a812be78ef8436df7caac70d
+clone git github.com/vbatts/tar-split v0.9.4
 
 clone git github.com/opencontainers/runc v0.0.2 # libcontainer
 # libcontainer deps (see src/github.com/docker/libcontainer/update-vendor.sh)
