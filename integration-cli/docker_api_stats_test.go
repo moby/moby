e698770d40637a97537c30abb46cab3883545017
Alessandro Boch <aboch@docker.com>
2015-07-01 09:54:26 -0700

    Stats API to retrieve nw stats from libnetwork - Container networking statistics are no longer   retrievable from libcontainer after the introduction   of libnetwork. This change adds the missing code   for docker daemon to retireve the nw stats from   Endpoint.
    Signed-off-by: Alessandro Boch <aboch@docker.com>

--- integration-cli/docker_api_stats_test.go
+++ integration-cli/docker_api_stats_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/docker/docker/api/types"
@@ -32,4 +34,42 @@ func (s *DockerSuite) TestCliStatsNoStreamGetCpu(c *check.C) {
 	if cpuPercent == 0 {
 		c.Fatalf("docker stats with no-stream get cpu usage failed: was %v", cpuPercent)
 	}
+}
+
+func (s *DockerSuite) TestApiNetworkStats(c *check.C) {
+	// Run container for 30 secs
+	out, _ := dockerCmd(c, "run", "-d", "busybox", "top")
+	id := strings.TrimSpace(out)
+	err := waitRun(id)
+	c.Assert(err, check.IsNil)
+
+	// Retrieve the container address
+	contIP := findContainerIP(c, id)
+	numPings := 10
+
+	// Get the container networking stats before and after pinging the container
+	nwStatsPre := getNetworkStats(c, id)
+	_, err = exec.Command("ping", contIP, "-c", strconv.Itoa(numPings)).Output()
+	c.Assert(err, check.IsNil)
+	nwStatsPost := getNetworkStats(c, id)
+
+	// Verify the stats contain at least the expected number of packets (account for ARP)
+	expRxPkts := 1 + nwStatsPre.RxPackets + uint64(numPings)
+	expTxPkts := 1 + nwStatsPre.TxPackets + uint64(numPings)
+	c.Assert(nwStatsPost.TxPackets >= expTxPkts, check.Equals, true,
+		check.Commentf("Reported less TxPackets than expected. Expected >= %d. Found %d", expTxPkts, nwStatsPost.TxPackets))
+	c.Assert(nwStatsPost.RxPackets >= expRxPkts, check.Equals, true,
+		check.Commentf("Reported less Txbytes than expected. Expected >= %d. Found %d", expRxPkts, nwStatsPost.RxPackets))
+}
+
+func getNetworkStats(c *check.C, id string) types.Network {
+	var st *types.Stats
+
+	_, body, err := sockRequestRaw("GET", fmt.Sprintf("/containers/%s/stats?stream=false", id), nil, "")
+	c.Assert(err, check.IsNil)
+
+	err = json.NewDecoder(body).Decode(&st)
+	c.Assert(err, check.IsNil)
+
+	return st.Network
 }
