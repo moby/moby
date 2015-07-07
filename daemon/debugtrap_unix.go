6bc73881c7629fbe8bf14f39c0934c26d0923c07
John Howard <jhoward@microsoft.com>
2015-07-06 18:58:53 -0700

    Windows: Win32 event for sigusr1 linux equivalence
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- daemon/debugtrap_unix.go
+++ daemon/debugtrap_unix.go
@@ -10,7 +10,7 @@ import (
 	psignal "github.com/docker/docker/pkg/signal"
 )
 
-func setupSigusr1Trap() {
+func setupDumpStackTrap() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGUSR1)
 	go func() {
