2e6932a028f5ee97e4b9fe3c17be9c9104915e5b
Lei Jitang <leijitang@huawei.com>
2015-07-07 11:27:00 +0800

    Fix test TestEventsDefaultEmpty. Fixes #14360
    Signed-off-by: Lei Jitang <leijitang@huawei.com>

--- integration-cli/docker_cli_events_test.go
+++ integration-cli/docker_cli_events_test.go
@@ -779,7 +779,7 @@ func (s *DockerSuite) TestEventsTop(c *check.C) {
 
 // #13753
 func (s *DockerSuite) TestEventsDefaultEmpty(c *check.C) {
-	dockerCmd(c, "run", "-d", "busybox")
+	dockerCmd(c, "run", "busybox")
 	out, _ := dockerCmd(c, "events", fmt.Sprintf("--until=%d", daemonTime(c).Unix()))
 	c.Assert(strings.TrimSpace(out), check.Equals, "")
 }
