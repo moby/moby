289ee90b04a2315cd36d6ff363b41c89f8ebf2aa
Lei Jitang <leijitang@huawei.com>
2015-07-08 11:15:09 +0800

    Fix copy from a "created" container. Fixes #14420
    Signed-off-by: Lei Jitang <leijitang@huawei.com>

--- daemon/container.go
+++ daemon/container.go
@@ -23,6 +23,7 @@ import (
 	"github.com/docker/docker/image"
 	"github.com/docker/docker/pkg/archive"
 	"github.com/docker/docker/pkg/broadcastwriter"
+	"github.com/docker/docker/pkg/fileutils"
 	"github.com/docker/docker/pkg/ioutils"
 	"github.com/docker/docker/pkg/jsonlog"
 	"github.com/docker/docker/pkg/mount"
@@ -625,6 +626,14 @@ func (container *Container) Copy(resource string) (io.ReadCloser, error) {
 		var dest string
 		dest, err = container.GetResourcePath(m.Destination)
 		if err != nil {
+			return nil, err
+		}
+		var stat os.FileInfo
+		stat, err = os.Stat(m.Source)
+		if err != nil {
+			return nil, err
+		}
+		if err = fileutils.CreateIfNotExists(dest, stat.IsDir()); err != nil {
 			return nil, err
 		}
 		if err = mount.Mount(m.Source, dest, "bind", "rbind,ro"); err != nil {
--- pkg/fileutils/fileutils.go
+++ pkg/fileutils/fileutils.go
@@ -167,3 +167,23 @@ func ReadSymlinkedDirectory(path string) (string, error) {
 	}
 	return realPath, nil
 }
+
+// CreateIfNotExists creates a file or a directory only if it does not already exist.
+func CreateIfNotExists(path string, isDir bool) error {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			if isDir {
+				return os.MkdirAll(path, 0755)
+			}
+			if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+				return err
+			}
+			f, err := os.OpenFile(path, os.O_CREATE, 0755)
+			if err != nil {
+				return err
+			}
+			f.Close()
+		}
+	}
+	return nil
+}
