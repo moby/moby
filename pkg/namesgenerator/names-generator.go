c27b93ea26f3a168971360c4f54286ea8653528b
Ajey Charantimath <ajey.charantimath@gmail.com>
2015-07-11 20:11:22 +0530

    Added notable indian scientists to names-generator.go
    Signed-off-by: Ajey Charantimath <ajey.charantimath@gmail.com>

--- pkg/namesgenerator/names-generator.go
+++ pkg/namesgenerator/names-generator.go
@@ -105,6 +105,9 @@ var (
 		// Alexander Graham Bell - an eminent Scottish-born scientist, inventor, engineer and innovator who is credited with inventing the first practical telephone - https://en.wikipedia.org/wiki/Alexander_Graham_Bell
 		"bell",
 
+		// Homi J Bhabha - was an Indian nuclear physicist, founding director, and professor of physics at the Tata Institute of Fundamental Research. Colloquially known as "father of Indian nuclear programme"- https://en.wikipedia.org/wiki/Homi_J._Bhabha
+		"bhabha",
+
 		// Bhaskara II - Ancient Indian mathematician-astronomer whose work on calculus predates Newton and Leibniz by over half a millenium - https://en.wikipedia.org/wiki/Bh%C4%81skara_II#Calculus
 		"bhaskara",
 
@@ -209,6 +212,9 @@ var (
 		// - https://en.wikipedia.org/wiki/Robert_Noyce
 		"kilby",
 		"noyce",
+
+		// A. P. J. Abdul Kalam - is an Indian scientist aka Missile Man of India for his work on the development of ballistic missile and launch vehicle technology - https://en.wikipedia.org/wiki/A._P._J._Abdul_Kalam
+		"kalam",
 
 		// Har Gobind Khorana - Indian-American biochemist who shared the 1968 Nobel Prize for Physiology - https://en.wikipedia.org/wiki/Har_Gobind_Khorana
 		"khorana",
