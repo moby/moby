95781aa63a52e02ffe2271c7157f9e6d8fa02b72
Tibor Vass <tibor@docker.com>
2015-07-13 21:13:28 -0400

    Vendor docker distribution registry client and dependencies
    Signed-off-by: Tibor Vass <tibor@docker.com>

---
+++ vendor/src/github.com/docker/distribution/context/util.go
@@ -0,0 +1,32 @@
+package context
+
+import (
+	"time"
+)
+
+// Since looks up key, which should be a time.Time, and returns the duration
+// since that time. If the key is not found, the value returned will be zero.
+// This is helpful when inferring metrics related to context execution times.
+func Since(ctx Context, key interface{}) time.Duration {
+	startedAtI := ctx.Value(key)
+	if startedAtI != nil {
+		if startedAt, ok := startedAtI.(time.Time); ok {
+			return time.Since(startedAt)
+		}
+	}
+
+	return 0
+}
+
+// GetStringValue returns a string value from the context. The empty string
+// will be returned if not found.
+func GetStringValue(ctx Context, key string) (value string) {
+	stringi := ctx.Value(key)
+	if stringi != nil {
+		if valuev, ok := stringi.(string); ok {
+			value = valuev
+		}
+	}
+
+	return value
+}
