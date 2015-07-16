83ad0536c130cbd0705cec27866ff4f770923912
John Howard <John.Howard@microsoft.com>
2015-07-16 12:07:20 -0700

    Windows: Vendor hcsshim@f674a70f1306dbe20b3a516bedd3285d85db60d9
    Signed-off-by: John Howard <John.Howard@microsoft.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -12,7 +12,7 @@ clone git github.com/go-check/check 64131543e7896d5bcc6bd5a76287eb75ea96c673
 clone git github.com/gorilla/context 14f550f51a
 clone git github.com/gorilla/mux e444e69cbd
 clone git github.com/kr/pty 5cf931ef8f
-clone git github.com/microsoft/hcsshim 2f540b26beafc3d4aded4fc9799af261a1a91352
+clone git github.com/microsoft/hcsshim f674a70f1306dbe20b3a516bedd3285d85db60d9
 clone git github.com/mistifyio/go-zfs v2.1.1
 clone git github.com/natefinch/npipe 0938d701e50e580f5925c773055eb6d6b32a0cbc
 clone git github.com/tchap/go-patricia v2.1.0
