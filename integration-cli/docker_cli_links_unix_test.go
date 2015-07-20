eef6eda7d295a7577bdbe2e4630ce3350c6d7701
Lei <leijitang@huawei.com>
2015-07-20 14:44:22 +0800

    Recfactor: Use dockerCmd when possible in integration-cli tests
    Part of #14603
    integration-cli/docker_cli_links_test.go (coolljt0725)
    integration-cli/docker_cli_links_unix_test.go (coolljt0725)
    integration-cli/docker_cli_logs_test.go (coolljt0725)
    integration-cli/docker_cli_nat_test.go (coolljt0725)
    integration-cli/docker_cli_network_test.go (coolljt0725)
    integration-cli/docker_cli_stats_test.go (coolljt0725)
    integration-cli/docker_cli_tag_test.go (coolljt0725)
    integration-cli/docker_cli_top_test.go (coolljt0725)
    integration-cli/docker_cli_version_test.go (coolljt0725)
    integration-cli/docker_cli_wait_test.go (coolljt0725
    
    Signed-off-by: Lei Jitang <leijitang@huawei.com>

--- integration-cli/docker_cli_links_unix_test.go
+++ integration-cli/docker_cli_links_unix_test.go
@@ -5,19 +5,13 @@ package main
 import (
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"strings"
 
 	"github.com/go-check/check"
 )
 
 func (s *DockerSuite) TestLinksEtcHostsRegularFile(c *check.C) {
-	runCmd := exec.Command(dockerBinary, "run", "--net=host", "busybox", "ls", "-la", "/etc/hosts")
-	out, _, _, err := runCommandWithStdoutStderr(runCmd)
-	if err != nil {
-		c.Fatal(out, err)
-	}
-
+	out, _ := dockerCmd(c, "run", "--net=host", "busybox", "ls", "-la", "/etc/hosts")
 	if !strings.HasPrefix(out, "-") {
 		c.Errorf("/etc/hosts should be a regular file")
 	}
@@ -26,12 +20,7 @@ func (s *DockerSuite) TestLinksEtcHostsRegularFile(c *check.C) {
 func (s *DockerSuite) TestLinksEtcHostsContentMatch(c *check.C) {
 	testRequires(c, SameHostDaemon)
 
-	runCmd := exec.Command(dockerBinary, "run", "--net=host", "busybox", "cat", "/etc/hosts")
-	out, _, _, err := runCommandWithStdoutStderr(runCmd)
-	if err != nil {
-		c.Fatal(out, err)
-	}
-
+	out, _ := dockerCmd(c, "run", "--net=host", "busybox", "cat", "/etc/hosts")
 	hosts, err := ioutil.ReadFile("/etc/hosts")
 	if os.IsNotExist(err) {
 		c.Skip("/etc/hosts does not exist, skip this test")
@@ -44,13 +33,8 @@ func (s *DockerSuite) TestLinksEtcHostsContentMatch(c *check.C) {
 }
 
 func (s *DockerSuite) TestLinksNetworkHostContainer(c *check.C) {
-
-	out, _, err := runCommandWithOutput(exec.Command(dockerBinary, "run", "-d", "--net", "host", "--name", "host_container", "busybox", "top"))
-	if err != nil {
-		c.Fatal(err, out)
-	}
-
-	out, _, err = runCommandWithOutput(exec.Command(dockerBinary, "run", "--name", "should_fail", "--link", "host_container:tester", "busybox", "true"))
+	dockerCmd(c, "run", "-d", "--net", "host", "--name", "host_container", "busybox", "top")
+	out, _, err := dockerCmdWithError(c, "run", "--name", "should_fail", "--link", "host_container:tester", "busybox", "true")
 	if err == nil || !strings.Contains(out, "--net=host can't be used with links. This would result in undefined behavior") {
 		c.Fatalf("Running container linking to a container with --net host should have failed: %s", out)
 	}
