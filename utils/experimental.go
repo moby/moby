17ce34116af4e8c6b66d0c7906efcae2fd75efc6
Hu Keping <hukeping@huawei.com>
2015-07-22 05:20:12 +0800

    golint: use golint to check package util
    Signed-off-by: Hu Keping <hukeping@huawei.com>

--- utils/experimental.go
+++ utils/experimental.go
@@ -2,6 +2,8 @@
 
 package utils
 
+// ExperimentalBuild is a stub which always returns true for
+// builds that include the "experimental" build tag
 func ExperimentalBuild() bool {
 	return true
 }
--- utils/stubs.go
+++ utils/stubs.go
@@ -2,6 +2,8 @@
 
 package utils
 
+// ExperimentalBuild is a stub which always returns false for
+// builds that do not include the "experimental" build tag
 func ExperimentalBuild() bool {
 	return false
 }
