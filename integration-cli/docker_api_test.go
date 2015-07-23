43eda7b7b5b829defb3e40d5e8dbf6d2b85e5032
Antonio Murdaca <runcom@linux.com>
2015-07-23 13:24:14 +0200

    Ensure body is closed after error is checked
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

--- integration-cli/docker_api_test.go
+++ integration-cli/docker_api_test.go
@@ -19,9 +19,9 @@ func (s *DockerSuite) TestApiOptionsRoute(c *check.C) {
 
 func (s *DockerSuite) TestApiGetEnabledCors(c *check.C) {
 	res, body, err := sockRequestRaw("GET", "/version", nil, "")
-	body.Close()
 	c.Assert(err, check.IsNil)
 	c.Assert(res.StatusCode, check.Equals, http.StatusOK)
+	body.Close()
 	// TODO: @runcom incomplete tests, why old integration tests had this headers
 	// and here none of the headers below are in the response?
 	//c.Log(res.Header)
