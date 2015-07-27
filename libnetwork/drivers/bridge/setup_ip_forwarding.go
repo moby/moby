5a259d55f003da5085d0e333e9ef479805e9669b
Tomas Kral <tomas.kral@gmail.com>
2015-07-27 13:31:03 +0200

    modify /proc/sys only if needed
    fixes #405
    
    Signed-off-by: Tomas Kral <tomas.kral@gmail.com>

--- libnetwork/drivers/bridge/setup_ip_forwarding.go
+++ libnetwork/drivers/bridge/setup_ip_forwarding.go
@@ -11,9 +11,18 @@ const (
 )
 
 func setupIPForwarding() error {
-	// Enable IPv4 forwarding
-	if err := ioutil.WriteFile(ipv4ForwardConf, []byte{'1', '\n'}, ipv4ForwardConfPerm); err != nil {
-		return fmt.Errorf("Setup IP forwarding failed: %v", err)
+	// Get current IPv4 forward setup
+	ipv4ForwardData, err := ioutil.ReadFile(ipv4ForwardConf)
+	if err != nil {
+		return fmt.Errorf("Cannot read IP forwarding setup: %v", err)
+	}
+
+	// Enable IPv4 forwarding only if it is not already enabled
+	if ipv4ForwardData[0] != '1' {
+		// Enable IPv4 forwarding
+		if err := ioutil.WriteFile(ipv4ForwardConf, []byte{'1', '\n'}, ipv4ForwardConfPerm); err != nil {
+			return fmt.Errorf("Setup IP forwarding failed: %v", err)
+		}
 	}
 
 	return nil
