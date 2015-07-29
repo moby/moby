6963b9c71694509d8511264655f05c203f5b8f97
Alexander Morozov <lk4d4@docker.com>
2015-07-28 17:14:49 -0700

    Add global instance of *(math/rand).Rand and Reader
    You can read random bytes from Reader without exhausting entropy.
    
    Signed-off-by: Alexander Morozov <lk4d4@docker.com>

--- pkg/random/random.go
+++ pkg/random/random.go
@@ -1,10 +1,18 @@
 package random
 
 import (
+	"io"
 	"math/rand"
 	"sync"
 	"time"
 )
+
+// Rand is a global *rand.Rand instance, which initilized with NewSource() source.
+var Rand = rand.New(NewSource())
+
+// Reader is a global, shared instance of a pseudorandom bytes generator.
+// It doesn't consume entropy.
+var Reader io.Reader = &reader{rnd: Rand}
 
 // copypaste from standard math/rand
 type lockedSource struct {
@@ -30,5 +38,24 @@ func (r *lockedSource) Seed(seed int64) {
 func NewSource() rand.Source {
 	return &lockedSource{
 		src: rand.NewSource(time.Now().UnixNano()),
+	}
+}
+
+type reader struct {
+	rnd *rand.Rand
+}
+
+func (r *reader) Read(b []byte) (int, error) {
+	i := 0
+	for {
+		val := r.rnd.Int63()
+		for val > 0 {
+			b[i] = byte(val)
+			i++
+			if i == len(b) {
+				return i, nil
+			}
+			val >>= 8
+		}
 	}
 }
