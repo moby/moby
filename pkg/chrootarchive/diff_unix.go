5ab9654bc4ebcf49f95c91e16ecb09bbe7779b50
John Howard <jhoward@microsoft.com>
2015-07-30 13:04:36 -0700

    Tidy platform common bits in diff
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- pkg/chrootarchive/diff_unix.go
+++ pkg/chrootarchive/diff_unix.go
@@ -65,22 +65,9 @@ func applyLayer() {
 	os.Exit(0)
 }
 
-// ApplyLayer parses a diff in the standard layer format from `layer`,
-// and applies it to the directory `dest`. The stream `layer` can only be
-// uncompressed.
-// Returns the size in bytes of the contents of the layer.
-func ApplyLayer(dest string, layer archive.ArchiveReader) (size int64, err error) {
-	return applyLayerHandler(dest, layer, true)
-}
-
-// ApplyUncompressedLayer parses a diff in the standard layer format from
-// `layer`, and applies it to the directory `dest`. The stream `layer`
-// can only be uncompressed.
-// Returns the size in bytes of the contents of the layer.
-func ApplyUncompressedLayer(dest string, layer archive.ArchiveReader) (int64, error) {
-	return applyLayerHandler(dest, layer, false)
-}
-
+// applyLayerHandler parses a diff in the standard layer format from `layer`, and
+// applies it to the directory `dest`. Returns the size in bytes of the
+// contents of the layer.
 func applyLayerHandler(dest string, layer archive.ArchiveReader, decompress bool) (size int64, err error) {
 	dest = filepath.Clean(dest)
 	if decompress {
