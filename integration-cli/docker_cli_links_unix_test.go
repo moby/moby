4fccf8adfa50855afa0ffaed1ea5c22ddaa7b2e4
John Howard <jhoward@microsoft.com>
2015-09-01 09:10:24 -0700

    Moved unix links tests to non-platform
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- integration-cli/docker_cli_links_unix_test.go
+++ integration-cli/docker_cli_links_unix_test.go
@@ -5,19 +5,13 @@ package main
 import (
 	"io/ioutil"
 	"os"
-	"strings"
 
 	"github.com/go-check/check"
 )
 
-func (s *DockerSuite) TestLinksEtcHostsRegularFile(c *check.C) {
-	out, _ := dockerCmd(c, "run", "--net=host", "busybox", "ls", "-la", "/etc/hosts")
-	if !strings.HasPrefix(out, "-") {
-		c.Errorf("/etc/hosts should be a regular file")
-	}
-}
-
 func (s *DockerSuite) TestLinksEtcHostsContentMatch(c *check.C) {
+	// In a _unix file as using Unix specific files, and must be on the
+	// same host as the daemon.
 	testRequires(c, SameHostDaemon)
 
 	out, _ := dockerCmd(c, "run", "--net=host", "busybox", "cat", "/etc/hosts")
@@ -28,15 +22,6 @@ func (s *DockerSuite) TestLinksEtcHostsContentMatch(c *check.C) {
 
 	if out != string(hosts) {
 		c.Errorf("container: %s\n\nhost:%s", out, hosts)
-	}
-
-}
-
-func (s *DockerSuite) TestLinksNetworkHostContainer(c *check.C) {
-	dockerCmd(c, "run", "-d", "--net", "host", "--name", "host_container", "busybox", "top")
-	out, _, err := dockerCmdWithError("run", "--name", "should_fail", "--link", "host_container:tester", "busybox", "true")
-	if err == nil || !strings.Contains(out, "--net=host can't be used with links. This would result in undefined behavior") {
-		c.Fatalf("Running container linking to a container with --net host should have failed: %s", out)
 	}
 
 }
