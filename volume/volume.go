126f849e8da96c78bd83e927cb585b2bf1d55785
Dan Walsh <dwalsh@redhat.com>
2015-09-22 09:43:33 -0400

    Chown contents on volume mount if user specified :u
    This patch follows the same pattern as the :Z and :z patch.
    
    Basically it will recursively chown the content on container
    creation iff user specifies :u on volume mount and --user at the
    same time.  Once we get usernamespace this could be extended to
    do the same thing to match the primary UID of the container.
    
    Docker-DCO-1.1-Signed-off-by: Dan Walsh <dwalsh@redhat.com> (github: rhatdan)
    
    Dan Walsh <dwalsh@redhat.com>
    
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>

--- volume/volume.go
+++ volume/volume.go
@@ -1,5 +1,15 @@
 package volume
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"syscall"
+
+	"github.com/opencontainers/runc/libcontainer/user"
+)
+
 // DefaultDriverName is the driver name used for the driver
 // implemented in the local package.
 const DefaultDriverName string = "local"
@@ -29,33 +39,93 @@ type Volume interface {
 	Unmount() error
 }
 
-// read-write modes
-var rwModes = map[string]bool{
-	"rw":   true,
-	"rw,Z": true,
-	"rw,z": true,
-	"z,rw": true,
-	"Z,rw": true,
-	"Z":    true,
-	"z":    true,
+var validModeFlags = map[string]bool{
+	"ro": true,
+	"rw": true,
+	"u":  true,
+	"z":  true,
+	"Z":  true,
 }
 
-// read-only modes
-var roModes = map[string]bool{
-	"ro":   true,
-	"ro,Z": true,
-	"ro,z": true,
-	"z,ro": true,
-	"Z,ro": true,
+func validMode(modes string) bool {
+	arr := strings.Split(modes, ",")
+	for _, mode := range arr {
+		if !validModeFlags[mode] {
+			return false
+		}
+	}
+	return true
+}
+
+func rwMode(modes string) bool {
+	arr := strings.Split(modes, ",")
+	for _, mode := range arr {
+		if mode == "ro" {
+			return false
+		}
+	}
+	return true
 }
 
 // ValidMountMode will make sure the mount mode is valid.
 // returns if it's a valid mount mode or not.
 func ValidMountMode(mode string) bool {
-	return roModes[mode] || rwModes[mode]
+	return validMode(mode) || rwMode(mode)
 }
 
 // ReadWrite tells you if a mode string is a valid read-write mode or not.
 func ReadWrite(mode string) bool {
-	return rwModes[mode]
+	return rwMode(mode)
+}
+
+func rchown(fpath string, uid, gid int) error {
+	exclude_path := []string{"/", "/usr", "/etc"}
+	for _, p := range exclude_path {
+		if fpath == p {
+			return fmt.Errorf("Chowning of %s is not allowed", fpath)
+		}
+	}
+	callback := func(p string, info os.FileInfo, err error) error {
+		return os.Chown(p, uid, gid)
+	}
+
+	return filepath.Walk(fpath, callback)
+}
+
+func FixUidGid(userid, src, modes string) error {
+	chown_user := false
+	if userid == "" {
+		return nil
+	}
+
+	arr := strings.Split(modes, ",")
+	for _, mode := range arr {
+		if mode == "u" {
+			chown_user = true
+			break
+		}
+	}
+	if !chown_user {
+		return nil
+	}
+	// Set up defaults.
+	defaultExecUser := user.ExecUser{
+		Uid:  syscall.Getuid(),
+		Gid:  syscall.Getgid(),
+		Home: "/",
+	}
+	passwdPath, err := user.GetPasswdPath()
+	if err != nil {
+		return err
+	}
+	groupPath, err := user.GetGroupPath()
+	if err != nil {
+		return err
+	}
+	execUser, err := user.GetExecUserPath(userid, &defaultExecUser, passwdPath, groupPath)
+	if err != nil {
+		return err
+	}
+
+	return rchown(src, execUser.Uid, execUser.Gid)
 }
