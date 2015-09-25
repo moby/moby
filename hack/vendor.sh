ac3180728da3308996d6c7193b542a0b81f4d3ea
Srini Brahmaroutu <srbrahma@us.ibm.com>
2015-09-25 23:16:55 +0000

    Vendoring in libnetwork with the fix for compilation error using GCCGo (X86 GCCGO CI) Signed-off-by: Srini Brahmaroutu <srbrahma@us.ibm.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -20,7 +20,7 @@ clone git github.com/tchap/go-patricia v2.1.0
 clone git golang.org/x/net 3cffabab72adf04f8e3b01c5baf775361837b5fe https://github.com/golang/net.git
 
 #get libnetwork packages
-clone git github.com/docker/libnetwork f5423a097e5da89f9ea206ddf8b93b5ac1f51ee7
+clone git github.com/docker/libnetwork 70409acbcd661e6a7bfe04e2b81412a465d29512
 clone git github.com/armon/go-metrics eb0af217e5e9747e41dd5303755356b62d28e3ec
 clone git github.com/hashicorp/go-msgpack 71c2886f5a673a35f909803f38ece5810165097b
 clone git github.com/hashicorp/memberlist 9a1e242e454d2443df330bdd51a436d5a9058fc4
