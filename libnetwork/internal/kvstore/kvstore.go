15e5d4067a310445b1a1b5571f0ec667034c3d3b
Alexandre Beslic <abronan@docker.com>
2015-10-06 06:05:48 -0700

    Change unsupported call error in boltdb backend to use general store error
    Signed-off-by: Alexandre Beslic <abronan@docker.com>

--- libnetwork/internal/kvstore/kvstore.go
+++ libnetwork/internal/kvstore/kvstore.go
@@ -21,10 +21,10 @@ const (
 )
 
 var (
-	// ErrNotSupported is thrown when the backend k/v store is not supported by libkv
-	ErrNotSupported = errors.New("Backend storage not supported yet, please choose one of")
-	// ErrNotImplemented is thrown when a method is not implemented by the current backend
-	ErrNotImplemented = errors.New("Call not implemented in current backend")
+	// ErrBackendNotSupported is thrown when the backend k/v store is not supported by libkv
+	ErrBackendNotSupported = errors.New("Backend storage not supported yet, please choose one of")
+	// ErrCallNotSupported is thrown when a method is not implemented/supported by the current backend
+	ErrCallNotSupported = errors.New("The current call is not supported with this backend")
 	// ErrNotReachable is thrown when the API cannot be reached for issuing common store operations
 	ErrNotReachable = errors.New("Api not reachable")
 	// ErrCannotLock is thrown when there is an error acquiring a lock on a key
