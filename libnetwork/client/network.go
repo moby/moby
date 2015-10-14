e8370e78d0c2f230358796dc140a130fa83db6c2
Madhu Venugopal <madhu@docker.com>
2015-10-14 16:38:46 -0700

    Changed Labels to DriverOpts
    Signed-off-by: Madhu Venugopal <madhu@docker.com>

--- libnetwork/client/network.go
+++ libnetwork/client/network.go
@@ -48,8 +48,8 @@ func (cli *NetworkCli) CmdNetworkCreate(chain string, args ...string) error {
 	}
 
 	// Construct network create request body
-	var labels []string
-	nc := networkCreate{Name: cmd.Arg(0), NetworkType: *flDriver, Labels: labels}
+	var driverOpts []string
+	nc := networkCreate{Name: cmd.Arg(0), NetworkType: *flDriver, DriverOpts: driverOpts}
 	obj, _, err := readBody(cli.call("POST", "/networks", nc, nil))
 	if err != nil {
 		return err
