cbec9c9a6b1e7ad0ec70be078b0a61385d5fbd2b
Madhu Venugopal <madhu@docker.com>
2015-10-14 20:11:53 -0700

    Vendoring libnetwork to Replace the label variable to DriverOpts
    Signed-off-by: Madhu Venugopal <madhu@docker.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -20,7 +20,7 @@ clone git github.com/tchap/go-patricia v2.1.0
 clone git golang.org/x/net 3cffabab72adf04f8e3b01c5baf775361837b5fe https://github.com/golang/net.git
 
 #get libnetwork packages
-clone git github.com/docker/libnetwork dd1c5f0ffe7697f75a82bd8e4bbd6f225ec5e383
+clone git github.com/docker/libnetwork 2934f6bf585fa24c86048cc85f7506a5bb626bf5
 clone git github.com/armon/go-metrics eb0af217e5e9747e41dd5303755356b62d28e3ec
 clone git github.com/hashicorp/go-msgpack 71c2886f5a673a35f909803f38ece5810165097b
 clone git github.com/hashicorp/memberlist 9a1e242e454d2443df330bdd51a436d5a9058fc4
