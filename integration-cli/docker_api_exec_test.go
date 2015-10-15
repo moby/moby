f7c9f3636857cf0cd9eff5d553db7e0896328c8b
Zachary Jaffee <zij@case.edu>
2015-10-15 12:49:24 -0400

    Cleaned up integration-cli/docker_api_exec_test.go
    Signed-off-by: Zachary Jaffee <zij@case.edu>

--- integration-cli/docker_api_exec_test.go
+++ integration-cli/docker_api_exec_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/docker/docker/pkg/integration/checker"
 	"github.com/go-check/check"
 )
 
@@ -19,12 +20,10 @@ func (s *DockerSuite) TestExecApiCreateNoCmd(c *check.C) {
 	dockerCmd(c, "run", "-d", "-t", "--name", name, "busybox", "/bin/sh")
 
 	status, body, err := sockRequest("POST", fmt.Sprintf("/containers/%s/exec", name), map[string]interface{}{"Cmd": nil})
-	c.Assert(err, check.IsNil)
-	c.Assert(status, check.Equals, http.StatusInternalServerError)
+	c.Assert(err, checker.IsNil)
+	c.Assert(status, checker.Equals, http.StatusInternalServerError)
 
-	if !bytes.Contains(body, []byte("No exec command specified")) {
-		c.Fatalf("Expected message when creating exec command with no Cmd specified")
-	}
+	c.Assert(body,checker.Not(checker.Contains),[]byte("No exec command specified"),Commentf("Expected message when creating exec command with no Cmd specified"))
 }
 
 func (s *DockerSuite) TestExecApiCreateNoValidContentType(c *check.C) {
@@ -33,20 +32,16 @@ func (s *DockerSuite) TestExecApiCreateNoValidContentType(c *check.C) {
 	dockerCmd(c, "run", "-d", "-t", "--name", name, "busybox", "/bin/sh")
 
 	jsonData := bytes.NewBuffer(nil)
-	if err := json.NewEncoder(jsonData).Encode(map[string]interface{}{"Cmd": nil}); err != nil {
-		c.Fatalf("Can not encode data to json %s", err)
-	}
+	c.Assert(json.NewEncoder(jsonData).Encode(map[string]interface{}{"Cmd": nil}), checker.IsNil,Commentf("Can not encode data to json"))
 
 	res, body, err := sockRequestRaw("POST", fmt.Sprintf("/containers/%s/exec", name), jsonData, "text/plain")
-	c.Assert(err, check.IsNil)
+	c.Assert(err, checker.IsNil)
 	c.Assert(res.StatusCode, check.Equals, http.StatusInternalServerError)
 
 	b, err := readBody(body)
-	c.Assert(err, check.IsNil)
+	c.Assert(err, checker.IsNil)
 
-	if !bytes.Contains(b, []byte("Content-Type specified")) {
-		c.Fatalf("Expected message when creating exec command with invalid Content-Type specified")
-	}
+	c.Assert(b,checker.Not(checker.Contains),[]byte("Content-Type specified"),Commentf("Expected message when creating exec command with invalid Content-Type specified"))
 }
 
 func (s *DockerSuite) TestExecAPIStart(c *check.C) {
@@ -54,22 +49,22 @@ func (s *DockerSuite) TestExecAPIStart(c *check.C) {
 
 	createExec := func() string {
 		_, b, err := sockRequest("POST", fmt.Sprintf("/containers/%s/exec", "test"), map[string]interface{}{"Cmd": []string{"true"}})
-		c.Assert(err, check.IsNil, check.Commentf(string(b)))
+		c.Assert(err, checker.IsNil, Commentf(string(b)))
 
 		createResp := struct {
 			ID string `json:"Id"`
 		}{}
-		c.Assert(json.Unmarshal(b, &createResp), check.IsNil, check.Commentf(string(b)))
+		c.Assert(json.Unmarshal(b, &createResp), checker.IsNil, Commentf(string(b)))
 		return createResp.ID
 	}
 
 	startExec := func(id string, code int) {
 		resp, body, err := sockRequestRaw("POST", fmt.Sprintf("/exec/%s/start", id), strings.NewReader(`{"Detach": true}`), "application/json")
-		c.Assert(err, check.IsNil)
+		c.Assert(err, checker.IsNil)
 
 		b, err := readBody(body)
-		c.Assert(err, check.IsNil, check.Commentf(string(b)))
-		c.Assert(resp.StatusCode, check.Equals, code, check.Commentf(string(b)))
+		c.Assert(err, checker.IsNil, Commentf(string(b)))
+		c.Assert(resp.StatusCode, checker.Equals, code, Commentf(string(b)))
 	}
 
 	startExec(createExec(), http.StatusOK)
