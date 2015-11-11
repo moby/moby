43b3b6ecb4f838023478c484d32957386b7dd666
Alessandro Boch <aboch@docker.com>
2015-11-10 21:13:25 -0800

    Vendoring libnetwork e8ebc0bf6510343c88d162db08b3d855cbbe75b9
    Signed-off-by: Alessandro Boch <aboch@docker.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -22,7 +22,7 @@ clone git github.com/vdemeester/shakers 3c10293ce22b900c27acad7b28656196fcc2f73b
 clone git golang.org/x/net 3cffabab72adf04f8e3b01c5baf775361837b5fe https://github.com/golang/net.git
 
 #get libnetwork packages
-clone git github.com/docker/libnetwork 5978c276ad20e104d6acd749da6ee6a8930055ae
+clone git github.com/docker/libnetwork e8ebc0bf6510343c88d162db08b3d855cbbe75b9
 clone git github.com/armon/go-metrics eb0af217e5e9747e41dd5303755356b62d28e3ec
 clone git github.com/hashicorp/go-msgpack 71c2886f5a673a35f909803f38ece5810165097b
 clone git github.com/hashicorp/memberlist 9a1e242e454d2443df330bdd51a436d5a9058fc4
