clearlinux/docker

Clear Containers for Docker Engine (v1.9.0)
Signed-off-by: James Hunt <james.o.hunt@intel.com>

daemon/monitor.go
@@ -142,6 +142,32 @@ func (m *containerMonitor) Start() error {
 
 		m.lastStartTime = time.Now()
 
+		// Make the network settings available to the execution
+		// driver to allow for integration with libnetwork networking.
+		m.container.command.NetworkSettings = m.container.NetworkSettings
+
+		// Allow the execution driver to query memory limits
+		m.container.command.HostConfig = m.container.hostConfig
+
+		// Make the network endpoint details available to the execution
+		// driver as well.
+		for _, name := range m.container.NetworkSettings.Networks {
+			n, _err := m.container.daemon.netController.NetworkByID(name)
+			if _err == nil {
+				var eps []map[string]interface{}
+
+				for _, ep := range n.Endpoints() {
+					info, err := ep.DriverInfo()
+					if err != nil {
+						continue
+					}
+					eps = append(eps, info)
+				}
+
+				m.container.command.EndpointInfo = eps
+			}
+		}
+
 		if exitStatus, err = m.container.daemon.run(m.container, pipes, m.callback); err != nil {
 			// if we receive an internal error from the initial start of a container then lets
 			// return it instead of entering the restart loop

moby/moby

daemon/monitor.go
@@ -142,6 +142,32 @@ func (m *containerMonitor) Start() error {
 
 		m.lastStartTime = time.Now()
 
+		// Make the network settings available to the execution
+		// driver to allow for integration with libnetwork networking.
+		m.container.command.NetworkSettings = m.container.NetworkSettings
+
+		// Allow the execution driver to query memory limits
+		m.container.command.HostConfig = m.container.hostConfig
+
+		// Make the network endpoint details available to the execution
+		// driver, to allow it to determine the bridge name.
+		for name, _ := range m.container.NetworkSettings.Networks {
+			n, _err := m.container.daemon.netController.NetworkByName(name)
+			if _err == nil {
+				var eps []map[string]interface{}
+
+				for _, ep := range n.Endpoints() {
+					info, err := ep.DriverInfo()
+					if err != nil {
+						continue
+					}
+					eps = append(eps, info)
+				}
+
+				m.container.command.EndpointInfo = eps
+			}
+		}
+
 		if exitStatus, err = m.container.daemon.run(m.container, pipes, m.callback); err != nil {
 			// if we receive an internal error from the initial start of a container then lets
 			// return it instead of entering the restart loop