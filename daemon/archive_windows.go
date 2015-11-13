8769b1e8ab466a68e26791d90a50e82281be29fc
John Howard <jhoward@microsoft.com>
2015-11-13 12:51:05 -0800

    Windows: Fixes comment
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- daemon/archive_windows.go
+++ daemon/archive_windows.go
@@ -4,7 +4,8 @@ package daemon
 // cannot be in a read-only volume. If it  is not in a volume, the container
 // cannot be configured with a read-only rootfs.
 //
-// This is a no-op on Windows which does not support volumes.
+// This is a no-op on Windows which does not support read-only volumes, or
+// extracting to a mount point inside a volume. TODO Windows: FIXME Post-TP4
 func checkIfPathIsInAVolume(container *Container, absPath string) (bool, error) {
 	return false, nil
 }
