9f712b2d39f1efe7eb0baa82c980a8b106426fcc
mqliang <mqliang.zju@gmail.com>
2015-11-13 17:16:38 +0800

    buildHostnameFile when NetworkMode = IsContainer
    Signed-off-by: mqliang <mqliang.zju@gmail.com>

--- daemon/container_unix.go
+++ daemon/container_unix.go
@@ -1042,7 +1042,6 @@ func (daemon *Daemon) initializeNetworking(container *Container) error {
 		container.ResolvConfPath = nc.ResolvConfPath
 		container.Config.Hostname = nc.Config.Hostname
 		container.Config.Domainname = nc.Config.Domainname
-		return nil
 	}
 
 	if container.hostConfig.NetworkMode.IsHost() {
@@ -1056,7 +1055,6 @@ func (daemon *Daemon) initializeNetworking(container *Container) error {
 			container.Config.Hostname = parts[0]
 			container.Config.Domainname = parts[1]
 		}
-
 	}
 
 	if err := daemon.allocateNetwork(container); err != nil {
