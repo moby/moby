7c1d49d90cbd91d7cc06d92686e2d09dd3dcac27
Stefan Scherer <scherer_stefan@icloud.com>
2015-11-18 11:13:04 +0100

    Move charsToString to architecture dependent source to fix casting problem
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- pkg/platform/architecture_linux.go
+++ pkg/platform/architecture_linux.go
@@ -14,15 +14,3 @@ func GetRuntimeArchitecture() (string, error) {
 	}
 	return charsToString(utsname.Machine), nil
 }
-
-func charsToString(ca [65]int8) string {
-	s := make([]byte, len(ca))
-	var lens int
-	for ; lens < len(ca); lens++ {
-		if ca[lens] == 0 {
-			break
-		}
-		s[lens] = uint8(ca[lens])
-	}
-	return string(s[0:lens])
-}
