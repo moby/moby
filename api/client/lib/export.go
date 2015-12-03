e0549b8cebee4dbe8614688767c9b666afdd32d9
David Calavera <david.calavera@gmail.com>
2015-12-03 14:49:41 -0500

    Implement docker export with standalone client lib.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

---
+++ api/client/lib/export.go
@@ -0,0 +1,18 @@
+package lib
+
+import (
+	"io"
+	"net/url"
+)
+
+// ContainerExport retrieves the raw contents of a container
+// and returns them as a io.ReadCloser. It's up to the caller
+// to close the stream.
+func (cli *Client) ContainerExport(containerID string) (io.ReadCloser, error) {
+	serverResp, err := cli.GET("/containers/"+containerID+"/export", url.Values{}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return serverResp.body, nil
+}
