9073a52ea839ef224931e1105bfa9c715ee48e2c
David Calavera <david.calavera@gmail.com>
2015-12-03 18:49:55 -0500

    Implement docker load with standalone client lib.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

---
+++ api/client/lib/image_load.go
@@ -0,0 +1,17 @@
+package lib
+
+import (
+	"io"
+	"net/url"
+)
+
+// ImageLoad loads an image in the docker host from the client host.
+// It's up to the caller to close the io.ReadCloser returned by
+// this function.
+func (cli *Client) ImageLoad(input io.Reader) (io.ReadCloser, error) {
+	resp, err := cli.POSTRaw("/images/load", url.Values{}, input, nil)
+	if err != nil {
+		return nil, err
+	}
+	return resp.body, nil
+}
