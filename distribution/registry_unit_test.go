a57478d65f1f5782cc746c260f9d841a0907ce54
Aaron Lehmann <aaron.lehmann@docker.com>
2015-12-04 13:42:33 -0800

    Do not fall back to the V1 protocol when we know we are talking to a V2 registry
    If we detect a Docker-Distribution-Api-Version header indicating that
    the registry speaks the V2 protocol, no fallback to V1 should take
    place.
    
    The same applies if a V2 registry operation succeeds while attempting a
    push or pull.
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- distribution/registry_unit_test.go
+++ distribution/registry_unit_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/docker/distribution/registry/client/auth"
 	"github.com/docker/docker/api/types"
 	registrytypes "github.com/docker/docker/api/types/registry"
 	"github.com/docker/docker/reference"
@@ -49,12 +48,6 @@ func TestTokenPassThru(t *testing.T) {
 		TrimHostname: false,
 		TLSConfig:    nil,
 		//VersionHeader: "verheader",
-		Versions: []auth.APIVersion{
-			{
-				Type:    "registry",
-				Version: "2",
-			},
-		},
 	}
 	n, _ := reference.ParseNamed("testremotename")
 	repoInfo := &registry.RepositoryInfo{
@@ -76,7 +69,8 @@ func TestTokenPassThru(t *testing.T) {
 		t.Fatal(err)
 	}
 	p := puller.(*v2Puller)
-	p.repo, err = NewV2Repository(p.repoInfo, p.endpoint, p.config.MetaHeaders, p.config.AuthConfig, "pull")
+	ctx := context.Background()
+	p.repo, _, err = NewV2Repository(ctx, p.repoInfo, p.endpoint, p.config.MetaHeaders, p.config.AuthConfig, "pull")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -84,7 +78,7 @@ func TestTokenPassThru(t *testing.T) {
 	logrus.Debug("About to pull")
 	// We expect it to fail, since we haven't mock'd the full registry exchange in our handler above
 	tag, _ := reference.WithTag(n, "tag_goes_here")
-	_ = p.pullV2Repository(context.Background(), tag)
+	_ = p.pullV2Repository(ctx, tag)
 
 	if !gotToken {
 		t.Fatal("Failed to receive registry token")
