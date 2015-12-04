37d6fee8cfddd8e1afabbdaa232fa64a36494579
David Calavera <david.calavera@gmail.com>
2015-12-04 13:13:43 -0500

    Implement docker rmi with standalone client lib.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

---
+++ api/client/lib/image_remove.go
@@ -0,0 +1,37 @@
+package lib
+
+import (
+	"encoding/json"
+	"net/url"
+
+	"github.com/docker/docker/api/types"
+)
+
+// ImageRemoveOptions holds parameters to remove images.
+type ImageRemoveOptions struct {
+	ImageID       string
+	Force         bool
+	PruneChildren bool
+}
+
+// ImageRemove removes an image from the docker host.
+func (cli *Client) ImageRemove(options ImageRemoveOptions) ([]types.ImageDelete, error) {
+	var query url.Values
+
+	if options.Force {
+		query.Set("force", "1")
+	}
+	if !options.PruneChildren {
+		query.Set("noprune", "1")
+	}
+
+	resp, err := cli.DELETE("/images/"+options.ImageID, query, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer ensureReaderClosed(resp)
+
+	var dels []types.ImageDelete
+	err = json.NewDecoder(resp.body).Decode(&dels)
+	return dels, err
+}
