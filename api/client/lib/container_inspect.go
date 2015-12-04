087674264674eaf946d185691ea92eee16f16a4f
David Calavera <david.calavera@gmail.com>
2015-12-03 19:34:16 -0500

    Implement docker logs with standalone client lib.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

---
+++ api/client/lib/container_inspect.go
@@ -0,0 +1,20 @@
+package lib
+
+import (
+	"encoding/json"
+
+	"github.com/docker/docker/api/types"
+)
+
+// ContainerInspect returns the all the container information.
+func (cli *Client) ContainerInspect(containerID string) (types.ContainerJSON, error) {
+	serverResp, err := cli.GET("/containers/"+containerID+"/json", nil, nil)
+	if err != nil {
+		return types.ContainerJSON{}, err
+	}
+	defer serverResp.body.Close()
+
+	var response types.ContainerJSON
+	json.NewDecoder(serverResp.body).Decode(&response)
+	return response, err
+}
