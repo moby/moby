c0a6dcf19cf66227f3553152b662e1356f029a14
Sean Schulte <sirsean@gmail.com>
2015-12-08 15:12:37 -0600

    unixSyslog logging (need to see what Travis says)

--- daemon/logger/syslog/internal/srslog/srslog_unix.go
+++ daemon/logger/syslog/internal/srslog/srslog_unix.go
@@ -2,6 +2,7 @@ package srslog
 
 import (
 	"errors"
+	"fmt"
 	"net"
 )
 
@@ -9,17 +10,22 @@ import (
 // local machine using a Unix domain socket.
 
 func unixSyslog() (conn serverConn, err error) {
+	fmt.Println("unixSyslog")
 	logTypes := []string{"unixgram", "unix"}
 	logPaths := []string{"/dev/log", "/var/run/syslog", "/var/run/log"}
 	for _, network := range logTypes {
 		for _, path := range logPaths {
+			fmt.Println("dialing", network, path)
 			conn, err := net.Dial(network, path)
 			if err != nil {
+				fmt.Println("failed:", err)
 				continue
 			} else {
+				fmt.Println("success", network, path)
 				return &netConn{conn: conn, local: true}, nil
 			}
 		}
 	}
+	fmt.Println("no unixSyslog")
 	return nil, errors.New("Unix syslog delivery error")
 }
