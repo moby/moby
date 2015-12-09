d059bd742c9c868d14c88e0bd24492a85826d678
Darren Shepherd <darren@rancher.com>
2015-12-09 13:11:41 -0700

    Don't dereference HostConfig.MemorySwapiness if nil
    Signed-off-by: Darren Shepherd <darren@rancher.com>

--- daemon/container_operations_unix.go
+++ daemon/container_operations_unix.go
@@ -199,7 +199,11 @@ func (daemon *Daemon) populateCommand(c *container.Container, env []string) erro
 		BlkioThrottleReadBpsDevice:  readBpsDevice,
 		BlkioThrottleWriteBpsDevice: writeBpsDevice,
 		OomKillDisable:              c.HostConfig.OomKillDisable,
-		MemorySwappiness:            *c.HostConfig.MemorySwappiness,
+		MemorySwappiness:            -1,
+	}
+
+	if c.HostConfig.MemorySwappiness != nil {
+		resources.MemorySwappiness = *c.HostConfig.MemorySwappiness
 	}
 
 	processConfig := execdriver.ProcessConfig{
