edec1ebed7994e69a72d398071a28542fcaedcc0
Darren Shepherd <darren@rancher.com>
2015-12-09 12:39:31 -0700

    Lazy initialize Volume on container Mount object
    Currently on daemon start volumes are "created" which involves invoking
    a volume driver if needed.  If this process fails the mount is left in a
    bad state in which there is no source or Volume set.  This now becomes
    an unrecoverable state in which that container can not be started.  The
    only way to fix is to restart the daemon and hopefully you don't get
    another error on startup.
    
    This change moves "createVolume" to be done at container start.  If the
    start fails it leaves it in the state in which you can try another
    start.  If the second start can contact the volume driver everything
    will recover fine.
    
    Signed-off-by: Darren Shepherd <darren@rancher.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -269,10 +269,6 @@ func (daemon *Daemon) Register(container *container.Container) error {
 		}
 	}
 
-	if err := daemon.prepareMountPoints(container); err != nil {
-		return err
-	}
-
 	return nil
 }
 
