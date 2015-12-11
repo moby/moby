a21c6688717f4f320b3a29d84608fbc26642f6de
David Calavera <david.calavera@gmail.com>
2015-12-11 18:58:54 -0500

    Move client version to the docker cli.
    This removes the dockerversion dependency from the client library.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- api/client/lib/version.go
+++ api/client/lib/version.go
@@ -2,37 +2,19 @@ package lib
 
 import (
 	"encoding/json"
-	"runtime"
 
-	"github.com/docker/docker/api"
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/dockerversion"
-	"github.com/docker/docker/utils"
 )
 
-// SystemVersion returns information of the docker client and server host.
-func (cli *Client) SystemVersion() (types.VersionResponse, error) {
-	client := &types.Version{
-		Version:      dockerversion.Version,
-		APIVersion:   api.Version,
-		GoVersion:    runtime.Version(),
-		GitCommit:    dockerversion.GitCommit,
-		BuildTime:    dockerversion.BuildTime,
-		Os:           runtime.GOOS,
-		Arch:         runtime.GOARCH,
-		Experimental: utils.ExperimentalBuild(),
-	}
-
+// ServerVersion returns information of the docker client and server host.
+func (cli *Client) ServerVersion() (types.Version, error) {
 	resp, err := cli.get("/version", nil, nil)
 	if err != nil {
-		return types.VersionResponse{Client: client}, err
+		return types.Version{}, err
 	}
 	defer ensureReaderClosed(resp)
 
 	var server types.Version
 	err = json.NewDecoder(resp.body).Decode(&server)
-	if err != nil {
-		return types.VersionResponse{Client: client}, err
-	}
-	return types.VersionResponse{Client: client, Server: &server}, nil
+	return server, err
 }
