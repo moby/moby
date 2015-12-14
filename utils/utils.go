c8f66fcdd976acd93c977dc2525cb4896cbc5b49
Tibor Vass <tibor@docker.com>
2015-12-14 11:15:00 +0100

    utils: move dockerignore function to builder/dockerignore
    Signed-off-by: Tibor Vass <tibor@docker.com>
    Signed-off-by: Cedric Davies <cedricda@ntdev.microsoft.com>

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"bufio"
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
@@ -242,31 +241,6 @@ func ValidateContextDirectory(srcPath string, excludes []string) error {
 		}
 		return nil
 	})
-}
-
-// ReadDockerIgnore reads a .dockerignore file and returns the list of file patterns
-// to ignore. Note this will trim whitespace from each line as well
-// as use GO's "clean" func to get the shortest/cleanest path for each.
-func ReadDockerIgnore(reader io.ReadCloser) ([]string, error) {
-	if reader == nil {
-		return nil, nil
-	}
-	defer reader.Close()
-	scanner := bufio.NewScanner(reader)
-	var excludes []string
-
-	for scanner.Scan() {
-		pattern := strings.TrimSpace(scanner.Text())
-		if pattern == "" {
-			continue
-		}
-		pattern = filepath.Clean(pattern)
-		excludes = append(excludes, pattern)
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("Error reading .dockerignore: %v", err)
-	}
-	return excludes, nil
 }
 
 // GetErrorMessage returns the human readable message associated with
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,12 +1,6 @@
 package utils
 
-import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"testing"
-)
+import "testing"
 
 func TestReplaceAndAppendEnvVars(t *testing.T) {
 	var (
@@ -23,51 +17,5 @@ func TestReplaceAndAppendEnvVars(t *testing.T) {
 	}
 	if env[1] != "TERM=xterm" {
 		t.Fatalf("expected TERM=xterm got '%s'", env[1])
-	}
-}
-
-func TestReadDockerIgnore(t *testing.T) {
-	tmpDir, err := ioutil.TempDir("", "dockerignore-test")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(tmpDir)
-
-	di, err := ReadDockerIgnore(nil)
-	if err != nil {
-		t.Fatalf("Expected not to have error, got %v", err)
-	}
-
-	if diLen := len(di); diLen != 0 {
-		t.Fatalf("Expected to have zero dockerignore entry, got %d", diLen)
-	}
-
-	diName := filepath.Join(tmpDir, ".dockerignore")
-	content := fmt.Sprintf("test1\n/test2\n/a/file/here\n\nlastfile")
-	err = ioutil.WriteFile(diName, []byte(content), 0777)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	diFd, err := os.Open(diName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	di, err = ReadDockerIgnore(diFd)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if di[0] != "test1" {
-		t.Fatalf("First element is not test1")
-	}
-	if di[1] != "/test2" {
-		t.Fatalf("Second element is not /test2")
-	}
-	if di[2] != "/a/file/here" {
-		t.Fatalf("Third element is not /a/file/here")
-	}
-	if di[3] != "lastfile" {
-		t.Fatalf("Fourth element is not lastfile")
 	}
 }
