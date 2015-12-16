c65fe927ec52e3823c5201c6b6fb33791522d487
David Calavera <david.calavera@gmail.com>
2015-12-16 12:25:54 -0500

    Vendor go-units and libcontainer.
    Libcontainer depends on the new package now to avoid cycled
    dependencies.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- vendor/src/github.com/opencontainers/runc/libcontainer/process_linux.go
+++ vendor/src/github.com/opencontainers/runc/libcontainer/process_linux.go
@@ -320,3 +320,44 @@ func getPipeFds(pid int) ([]string, error) {
 	}
 	return fds, nil
 }
+
+// InitializeIO creates pipes for use with the process's STDIO
+// and returns the opposite side for each
+func (p *Process) InitializeIO(rootuid int) (i *IO, err error) {
+	var fds []uintptr
+	i = &IO{}
+	// cleanup in case of an error
+	defer func() {
+		if err != nil {
+			for _, fd := range fds {
+				syscall.Close(int(fd))
+			}
+		}
+	}()
+	// STDIN
+	r, w, err := os.Pipe()
+	if err != nil {
+		return nil, err
+	}
+	fds = append(fds, r.Fd(), w.Fd())
+	p.Stdin, i.Stdin = r, w
+	// STDOUT
+	if r, w, err = os.Pipe(); err != nil {
+		return nil, err
+	}
+	fds = append(fds, r.Fd(), w.Fd())
+	p.Stdout, i.Stdout = w, r
+	// STDERR
+	if r, w, err = os.Pipe(); err != nil {
+		return nil, err
+	}
+	fds = append(fds, r.Fd(), w.Fd())
+	p.Stderr, i.Stderr = w, r
+	// change ownership of the pipes incase we are in a user namespace
+	for _, fd := range fds {
+		if err := syscall.Fchown(int(fd), rootuid, rootuid); err != nil {
+			return nil, err
+		}
+	}
+	return i, nil
+}
