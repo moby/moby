7f8f4564989e1ceed9664932c1398ddd4544350f
Daniel Nephin <dnephin@docker.com>
2015-12-21 15:10:53 -0500

    Remove package pkg/ulimit, use go-units instead.
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

--- api/types/client.go
+++ api/types/client.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
-	"github.com/docker/docker/pkg/ulimit"
+	"github.com/docker/go-units"
 )
 
 // ContainerAttachOptions holds parameters to attach to a container.
@@ -137,7 +137,7 @@ type ImageBuildOptions struct {
 	CgroupParent   string
 	ShmSize        string
 	Dockerfile     string
-	Ulimits        []*ulimit.Ulimit
+	Ulimits        []*units.Ulimit
 	BuildArgs      []string
 	AuthConfigs    map[string]AuthConfig
 	Context        io.Reader
