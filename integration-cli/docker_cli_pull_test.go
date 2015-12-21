4d7d1fd5b019853726c6987723b6eb73b7d9d972
Aaron Lehmann <aaron.lehmann@docker.com>
2015-12-21 15:42:04 -0800

    When a manifest is not found, allow fallback to v1
    PR #18590 caused compatibility issues with registries such as gcr.io
    which support both the v1 and v2 protocols, but do not provide the same
    set of images over both protocols. After #18590, pulls from these
    registries would never use the v1 protocol, because of the
    Docker-Distribution-Api-Version header indicating that v2 was supported.
    
    Fix the problem by making an exception for the case where a manifest is
    not found. This should allow fallback to v1 in case that image is
    exposed over the v1 protocol but not the v2 protocol.
    
    This avoids the overly aggressive fallback behavior before #18590 which
    would allow protocol fallback after almost any error, but restores
    interoperability with mixed v1/v2 registry setups.
    
    Fixes #18832
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>
    Signed-off-by: Cedric Davies <cedricda@ntdev.microsoft.com>

--- integration-cli/docker_cli_pull_test.go
+++ integration-cli/docker_cli_pull_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 	"time"
@@ -53,8 +54,10 @@ func (s *DockerHubPullSuite) TestPullNonExistingImage(c *check.C) {
 	} {
 		out, err := s.CmdWithError("pull", e.Alias)
 		c.Assert(err, checker.NotNil, check.Commentf("expected non-zero exit status when pulling non-existing image: %s", out))
-		// Hub returns 401 rather than 404 for nonexistent library/ repos.
-		c.Assert(out, checker.Contains, "unauthorized: access to the requested resource is not authorized", check.Commentf("expected unauthorized error message"))
+		// Hub returns 401 rather than 404 for nonexistent repos over
+		// the v2 protocol - but we should end up falling back to v1,
+		// which does return a 404.
+		c.Assert(out, checker.Contains, fmt.Sprintf("Error: image %s not found", e.Repo), check.Commentf("expected image not found error messages"))
 	}
 }
 
