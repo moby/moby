4e2c0f385c3032b0b15105c81b18e3981e6e40ad
David Calavera <david.calavera@gmail.com>
2015-12-21 12:55:37 -0500

    Add godoc comment about client tls verification.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- api/client/lib/client.go
+++ api/client/lib/client.go
@@ -37,6 +37,7 @@ type Client struct {
 // Use DOCKER_HOST to set the url to the docker server.
 // Use DOCKER_API_VERSION to set the version of the API to reach, leave empty for latest.
 // Use DOCKER_CERT_PATH to load the tls certificates from.
+// Use DOCKER_TLS_VERIFY to enable or disable TLS verification, off by default.
 func NewEnvClient() (*Client, error) {
 	var transport *http.Transport
 	if dockerCertPath := os.Getenv("DOCKER_CERT_PATH"); dockerCertPath != "" {
