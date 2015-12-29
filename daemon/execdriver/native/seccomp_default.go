b025844ed4e10217d8caf9ee4b4c5602647615dd
Justin Cormack <justin.cormack@unikernel.com>
2015-12-29 10:22:05 +0000

    Allow use of robust list syscalls
    The set_robust_list syscall sets the list of futexes which are
    cleaned up on thread exit, and are needed to avoid mutexes
    being held forever on thread exit.
    
    See for example in Musl libc mutex handling:
    http://git.musl-libc.org/cgit/musl/tree/src/thread/pthread_mutex_trylock.c#n22
    
    Signed-off-by: Justin Cormack <justin.cormack@unikernel.com>
    Signed-off-by: Cedric Davies <cedricda@ntdev.microsoft.com>

--- daemon/execdriver/native/seccomp_default.go
+++ daemon/execdriver/native/seccomp_default.go
@@ -98,12 +98,6 @@ var defaultSeccompProfile = &configs.Seccomp{
 			Args:   []*configs.Arg{},
 		},
 		{
-			// Deny getting the list of robust futexes
-			Name:   "get_robust_list",
-			Action: configs.Errno,
-			Args:   []*configs.Arg{},
-		},
-		{
 			// Deny manipulation and functions on kernel modules.
 			Name:   "init_module",
 			Action: configs.Errno,
@@ -277,12 +271,6 @@ var defaultSeccompProfile = &configs.Seccomp{
 		{
 			// deny associating a thread with a namespace
 			Name:   "setns",
-			Action: configs.Errno,
-			Args:   []*configs.Arg{},
-		},
-		{
-			// Deny setting the list of robust futexes
-			Name:   "set_robust_list",
 			Action: configs.Errno,
 			Args:   []*configs.Arg{},
 		},
