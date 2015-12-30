a2e9448bdc139af8865e5bf12c4df296688ded9a
Jessica Frazelle <acidburn@docker.com>
2015-12-30 11:20:23 -0800

    add more seccomp profile tests
    Signed-off-by: Jessica Frazelle <acidburn@docker.com>
    Signed-off-by: Cedric Davies <cedricda@ntdev.microsoft.com>

--- contrib/syscall-test/userns.c
+++ contrib/syscall-test/userns.c
@@ -1,17 +1,15 @@
 #define _GNU_SOURCE
-#include <sched.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-#include <limits.h>
 #include <errno.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-#define STACKSIZE (1024*1024)
-static char child_stack[STACKSIZE];
+#define STACK_SIZE (1024 * 1024)	/* Stack size for cloned child */
 
 struct clone_args {
 	char **argv;
@@ -37,10 +35,21 @@ int main(int argc, char **argv)
 
 	int clone_flags = CLONE_NEWUSER | SIGCHLD;
 
+	// allocate stack for child
+	char *stack;		/* Start of stack buffer */
+	char *child_stack;	/* End of stack buffer */
+	stack =
+	    mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
+		 MAP_SHARED | MAP_ANON | MAP_STACK, -1, 0);
+	if (stack == MAP_FAILED) {
+		fprintf(stderr, "mmap failed: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	child_stack = stack + STACK_SIZE;	/* Assume stack grows downward */
+
 	// the result of this call is that our child_exec will be run in another
 	// process returning it's pid
-	pid_t pid =
-	    clone(child_exec, child_stack + STACKSIZE, clone_flags, &args);
+	pid_t pid = clone(child_exec, child_stack, clone_flags, &args);
 	if (pid < 0) {
 		fprintf(stderr, "clone failed: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
