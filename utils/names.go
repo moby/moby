33fba63a2e3943b03e76edf6fbdf686eb9292c3f
Jessica Frazelle <acidburn@docker.com>
2016-01-04 15:00:49 -0800

    update volume name regex
    Disallow creating a volume starting with a /.
    
    Signed-off-by: Jessica Frazelle <acidburn@docker.com>

--- utils/names.go
+++ utils/names.go
@@ -7,3 +7,6 @@ const RestrictedNameChars = `[a-zA-Z0-9][a-zA-Z0-9_.-]`
 
 // RestrictedNamePattern is a regular expression to validate names against the collection of restricted characters.
 var RestrictedNamePattern = regexp.MustCompile(`^/?` + RestrictedNameChars + `+$`)
+
+// RestrictedVolumeNamePattern is a regular expression to validate volume names against the collection of restricted characters.
+var RestrictedVolumeNamePattern = regexp.MustCompile(`^` + RestrictedNameChars + `+$`)
