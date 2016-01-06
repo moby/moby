6855ba2983e03815a2d7a618c1533d8d5963f129
John Howard <jhoward@microsoft.com>
2016-01-06 10:26:37 -0800

    Windows: Fix security warning regression
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- api/client/build.go
+++ api/client/build.go
@@ -269,8 +269,9 @@ func (cli *DockerCli) CmdBuild(args ...string) error {
 		}
 	}
 
-	// Windows: show error message about modified file permissions.
-	if response.OSType == "windows" {
+	// Windows: show error message about modified file permissions if the
+	// daemon isn't running Windows.
+	if response.OSType != "windows" && runtime.GOOS == "windows" {
 		fmt.Fprintln(cli.err, `SECURITY WARNING: You are building a Docker image from Windows against a non-Windows Docker host. All files and directories added to build context will have '-rwxr-xr-x' permissions. It is recommended to double check and reset permissions for sensitive files and directories.`)
 	}
 
