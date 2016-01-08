82ac047520cf7eebe1fb85776cf2628a8fa0a312
Sean Schulte <sirsean@gmail.com>
2016-01-08 20:02:21 +0000

    The Dialer tests passed on OS X but failed on Linux (Fedora 22).
    This should resolve that by using regexp to check into the
    function pointer gotten from runtime reflection.

--- daemon/logger/syslog/internal/srslog/dialer_test.go
+++ daemon/logger/syslog/internal/srslog/dialer_test.go
@@ -3,11 +3,23 @@ package srslog
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"io/ioutil"
 	"reflect"
+	"regexp"
 	"runtime"
 	"testing"
 )
+
+// match checks if the given function (f) is the one inside the function pointer
+// (name) which is read via runtime reflection. This is necessary because different
+// platforms have a different reflection path. (IE on OS X it starts with "github.com..."
+// but on Linux it has an absolute path, "/home/<username>/go/src/github.com...")
+func match(f, name string) bool {
+	re := fmt.Sprintf(".*github.com/RackSec/srslog\\.\\(Writer\\)\\.\\(.*github.com/RackSec/srslog.%v\\)-fm", f)
+	matched, _ := regexp.MatchString(re, name)
+	return matched
+}
 
 func TestGetDialer(t *testing.T) {
 	w := Writer{
@@ -20,35 +32,35 @@ func TestGetDialer(t *testing.T) {
 
 	dialer := w.getDialer()
 	name := runtime.FuncForPC(reflect.ValueOf(dialer).Pointer()).Name()
-	if name != "github.com/RackSec/srslog.(Writer).(github.com/RackSec/srslog.unixDialer)-fm" {
+	if !match("unixDialer", name) {
 		t.Errorf("should get unixDialer, got: %v", name)
 	}
 
 	w.network = "tcp+tls"
 	dialer = w.getDialer()
 	name = runtime.FuncForPC(reflect.ValueOf(dialer).Pointer()).Name()
-	if name != "github.com/RackSec/srslog.(Writer).(github.com/RackSec/srslog.tlsDialer)-fm" {
+	if !match("tlsDialer", name) {
 		t.Errorf("should get tlsDialer, got: %v", name)
 	}
 
 	w.network = "tcp"
 	dialer = w.getDialer()
 	name = runtime.FuncForPC(reflect.ValueOf(dialer).Pointer()).Name()
-	if name != "github.com/RackSec/srslog.(Writer).(github.com/RackSec/srslog.basicDialer)-fm" {
+	if !match("basicDialer", name) {
 		t.Errorf("should get basicDialer, got: %v", name)
 	}
 
 	w.network = "udp"
 	dialer = w.getDialer()
 	name = runtime.FuncForPC(reflect.ValueOf(dialer).Pointer()).Name()
-	if name != "github.com/RackSec/srslog.(Writer).(github.com/RackSec/srslog.basicDialer)-fm" {
+	if !match("basicDialer", name) {
 		t.Errorf("should get basicDialer, got: %v", name)
 	}
 
 	w.network = "something else entirely"
 	dialer = w.getDialer()
 	name = runtime.FuncForPC(reflect.ValueOf(dialer).Pointer()).Name()
-	if name != "github.com/RackSec/srslog.(Writer).(github.com/RackSec/srslog.basicDialer)-fm" {
+	if !match("basicDialer", name) {
 		t.Errorf("should get basicDialer, got: %v", name)
 	}
 }
