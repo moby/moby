ba19b6927fd79be4d676ff228128d72393cbf76a
Euan <euank@amazon.com>
2016-01-08 23:09:47 +0000

    Increase integration cli test memory
    Signed-off-by: Euan <euank@amazon.com>

--- integration-cli/docker_cli_oom_killed_test.go
+++ integration-cli/docker_cli_oom_killed_test.go
@@ -11,7 +11,7 @@ func (s *DockerSuite) TestInspectOomKilledTrue(c *check.C) {
 	testRequires(c, DaemonIsLinux, memoryLimitSupport)
 
 	name := "testoomkilled"
-	_, exitCode, _ := dockerCmdWithError("run", "--name", name, "-m", "10MB", "busybox", "sh", "-c", "x=a; while true; do x=$x$x$x$x; done")
+	_, exitCode, _ := dockerCmdWithError("run", "--name", name, "--memory", "32MB", "busybox", "sh", "-c", "x=a; while true; do x=$x$x$x$x; done")
 
 	c.Assert(exitCode, checker.Equals, 137, check.Commentf("OOM exit should be 137"))
 
@@ -24,7 +24,7 @@ func (s *DockerSuite) TestInspectOomKilledFalse(c *check.C) {
 	testRequires(c, DaemonIsLinux, memoryLimitSupport)
 
 	name := "testoomkilled"
-	dockerCmd(c, "run", "--name", name, "-m", "10MB", "busybox", "sh", "-c", "echo hello world")
+	dockerCmd(c, "run", "--name", name, "--memory", "32MB", "busybox", "sh", "-c", "echo hello world")
 
 	oomKilled, err := inspectField(name, "State.OOMKilled")
 	c.Assert(oomKilled, checker.Equals, "false")
