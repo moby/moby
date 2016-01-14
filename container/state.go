f683b05491eb3f87d3e0484fa2c750e3270d1778
Brian Goff <cpuguy83@gmail.com>
2016-01-14 13:38:09 -0500

    Revert "Break big lock into some tiny locks"
    This reverts commit 1326f0cba5f933674e23769de1385d3b0841e758.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>
    Signed-off-by: Cedric Davies <cedricda@ntdev.microsoft.com>

--- container/state.go
+++ container/state.go
@@ -179,13 +179,6 @@ func (s *State) getExitCode() int {
 	return res
 }
 
-// SetRunningLocking locks container and sets it to "running"
-func (s *State) SetRunningLocking(pid int) {
-	s.Lock()
-	s.SetRunning(pid)
-	s.Unlock()
-}
-
 // SetRunning sets the state of the container to "running".
 func (s *State) SetRunning(pid int) {
 	s.Error = ""
@@ -199,7 +192,7 @@ func (s *State) SetRunning(pid int) {
 	s.waitChan = make(chan struct{})
 }
 
-// SetStoppedLocking locks the container state and sets it to "stopped".
+// SetStoppedLocking locks the container state is sets it to "stopped".
 func (s *State) SetStoppedLocking(exitStatus *execdriver.ExitStatus) {
 	s.Lock()
 	s.SetStopped(exitStatus)
