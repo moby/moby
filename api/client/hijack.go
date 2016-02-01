f020527c99ade5c27975d37512a83f0c935a38bc
Brian Goff <cpuguy83@gmail.com>
2016-02-01 16:34:12 -0500

    Revert "Move tty set and restore to caller"
    This reverts commit a1eb6029cc4e4ccd820b0aac147a6121f1967ba2.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- api/client/hijack.go
+++ api/client/hijack.go
@@ -2,19 +2,41 @@ package client
 
 import (
 	"io"
+	"os"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/docker/pkg/term"
 	"github.com/docker/engine-api/types"
 )
 
-func (cli *DockerCli) holdHijackedConnection(tty bool, inputStream io.ReadCloser, outputStream, errorStream io.Writer, resp types.HijackedResponse) error {
-	var err error
+func (cli *DockerCli) holdHijackedConnection(setRawTerminal bool, inputStream io.ReadCloser, outputStream, errorStream io.Writer, resp types.HijackedResponse) error {
+	var (
+		err      error
+		oldState *term.State
+	)
+	if inputStream != nil && setRawTerminal && cli.isTerminalIn && os.Getenv("NORAW") == "" {
+		oldState, err = term.SetRawTerminal(cli.inFd)
+		if err != nil {
+			return err
+		}
+		defer term.RestoreTerminal(cli.inFd, oldState)
+	}
+
 	receiveStdout := make(chan error, 1)
 	if outputStream != nil || errorStream != nil {
 		go func() {
+			defer func() {
+				if inputStream != nil {
+					if setRawTerminal && cli.isTerminalIn {
+						term.RestoreTerminal(cli.inFd, oldState)
+					}
+					inputStream.Close()
+				}
+			}()
+
 			// When TTY is ON, use regular copy
-			if tty && outputStream != nil {
+			if setRawTerminal && outputStream != nil {
 				_, err = io.Copy(outputStream, resp.Reader)
 			} else {
 				_, err = stdcopy.StdCopy(outputStream, errorStream, resp.Reader)
