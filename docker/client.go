78b0defcf344294874202e819dcb3f8a0daedf43
Brian Goff <cpuguy83@gmail.com>
2016-02-01 14:36:40 -0500

    handle debug mode for clients
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- docker/client.go
+++ docker/client.go
@@ -6,6 +6,7 @@ import (
 	"github.com/docker/docker/cli"
 	"github.com/docker/docker/cliconfig"
 	flag "github.com/docker/docker/pkg/mflag"
+	"github.com/docker/docker/utils"
 )
 
 var clientFlags = &cli.ClientFlags{FlagSet: new(flag.FlagSet), Common: commonFlags}
@@ -23,6 +24,10 @@ func init() {
 
 		if clientFlags.Common.TrustKey == "" {
 			clientFlags.Common.TrustKey = filepath.Join(cliconfig.ConfigDir(), defaultTrustKeyFile)
+		}
+
+		if clientFlags.Common.Debug {
+			utils.EnableDebug()
 		}
 	}
 }
