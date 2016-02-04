fae09e25698006a190d14fe64f1576f68431fabf
Lei Jitang <leijitang@huawei.com>
2016-02-03 21:31:47 -0500

    Add progress bar to docker load
    Signed-off-by: Lei Jitang <leijitang@huawei.com>

--- image/image.go
+++ image/image.go
@@ -116,7 +116,7 @@ type History struct {
 
 // Exporter provides interface for exporting and importing images
 type Exporter interface {
-	Load(io.ReadCloser, io.Writer) error
+	Load(io.ReadCloser, io.Writer, bool) error
 	// TODO: Load(net.Context, io.ReadCloser, <- chan StatusMessage) error
 	Save([]string, io.Writer) error
 }
