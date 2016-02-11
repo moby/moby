8f26fe4f59ce515c68440da1443ace4c96e89d4a
Aaron Lehmann <aaron.lehmann@docker.com>
2016-02-11 14:08:49 -0800

    Push/pull errors improvement and cleanup
    Several improvements to error handling:
    
    - Introduce ImageConfigPullError type, wrapping errors related to
      downloading the image configuration blob in schema2. This allows for a
      more descriptive error message to be seen by the end user.
    
    - Change some logrus.Debugf calls that display errors to logrus.Errorf.
      Add log lines in the push/pull fallback cases to make sure the errors
      leading to the fallback are shown.
    
    - Move error-related types and functions which are only used by the
      distribution package out of the registry package.
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- distribution/pull_v1.go
+++ distribution/pull_v1.go
@@ -38,7 +38,7 @@ type v1Puller struct {
 func (p *v1Puller) Pull(ctx context.Context, ref reference.Named) error {
 	if _, isCanonical := ref.(reference.Canonical); isCanonical {
 		// Allowing fallback, because HTTPS v1 is before HTTP v2
-		return fallbackError{err: registry.ErrNoSupport{Err: errors.New("Cannot pull by digest with v1 registry")}}
+		return fallbackError{err: ErrNoSupport{Err: errors.New("Cannot pull by digest with v1 registry")}}
 	}
 
 	tlsConfig, err := p.config.RegistryService.TLSConfig(p.repoInfo.Index.Name)
