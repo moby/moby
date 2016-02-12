2541a23c3aa44fed115a22b36ca7f1770255bbc2
Frederik Nordahl Jul Sabroe <frederikns@gmail.com>
2016-02-12 18:39:12 +0100

    Fish completion lists all containers on "docker rm -f"
    Signed-off-by: Frederik Nordahl Jul Sabroe <frederikns@gmail.com>

--- contrib/completion/fish/docker.fish
+++ contrib/completion/fish/docker.fish
@@ -290,6 +290,7 @@ complete -c docker -A -f -n '__fish_seen_subcommand_from rm' -l help -d 'Print u
 complete -c docker -A -f -n '__fish_seen_subcommand_from rm' -s l -l link -d 'Remove the specified link and not the underlying container'
 complete -c docker -A -f -n '__fish_seen_subcommand_from rm' -s v -l volumes -d 'Remove the volumes associated with the container'
 complete -c docker -A -f -n '__fish_seen_subcommand_from rm' -a '(__fish_print_docker_containers stopped)' -d "Container"
+complete -c docker -A -f -n '__fish_seen_subcommand_from rm' -s f -l force -a '(__fish_print_docker_containers all)' -d "Container"
 
 # rmi
 complete -c docker -f -n '__fish_docker_no_subcommand' -a rmi -d 'Remove one or more images'
