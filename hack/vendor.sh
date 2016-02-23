4fc5bd295eedbefe1b429d98be52f794f1461f2f
Alexander Morozov <lk4d4@docker.com>
2016-02-23 13:37:20 -0800

    Update libcontainer to 2c3115481ee1782ad687a9e0b4834f89533c2acf
    It includes fix for parsing systemd cgroup names
    
    Signed-off-by: Alexander Morozov <lk4d4@docker.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -59,7 +59,7 @@ clone git github.com/miekg/pkcs11 80f102b5cac759de406949c47f0928b99bd64cdf
 clone git github.com/docker/go v1.5.1-1-1-gbaf439e
 clone git github.com/agl/ed25519 d2b94fd789ea21d12fac1a4443dd3a3f79cda72c
 
-clone git github.com/opencontainers/runc ce72f86a2b54bc114d6ffb51f6500479b2d42154 # libcontainer
+clone git github.com/opencontainers/runc 2c3115481ee1782ad687a9e0b4834f89533c2acf # libcontainer
 clone git github.com/seccomp/libseccomp-golang 1b506fc7c24eec5a3693cdcbed40d9c226cfc6a1
 # libcontainer deps (see src/github.com/opencontainers/runc/Godeps/Godeps.json)
 clone git github.com/coreos/go-systemd v4
