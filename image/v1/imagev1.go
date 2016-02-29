bb16c53d59186519284428b304d981e216365ce1
Antonio Murdaca <runcom@redhat.com>
2016-02-29 13:40:45 +0100

    Revert "resolve the config file from the sudo user"
    This reverts commit afde6450ee7bd4a43765fdc0a9799b411276d9e4.
    
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- image/v1/imagev1.go
+++ image/v1/imagev1.go
@@ -142,7 +142,7 @@ func rawJSON(value interface{}) *json.RawMessage {
 // ValidateID checks whether an ID string is a valid image ID.
 func ValidateID(id string) error {
 	if ok := validHex.MatchString(id); !ok {
-		return fmt.Errorf("image ID %q is invalid", id)
+		return fmt.Errorf("image ID '%s' is invalid ", id)
 	}
 	return nil
 }
