377435fbd0ccc9b29906bc921e22fc358283d4fb
Ron Peleg <ron.peleg@gmail.com>
2016-03-02 12:13:59 +0200

    Resolve #20648 using a coarse grained lock on check duplicate network name and create new network
    Signed-off-by: Ron Peleg <ron.peleg@gmail.com>

--- api/server/router/network/network_routes.go
+++ api/server/router/network/network_routes.go
@@ -80,6 +80,11 @@ func (n *networkRouter) postNetworkCreate(ctx context.Context, w http.ResponseWr
 			fmt.Sprintf("%s is a pre-defined network and cannot be created", create.Name))
 	}
 
+	if create.CheckDuplicate { // make sure GetNetworkByName and the checkDup code run in sync
+		n.mutex.Lock()
+		defer n.mutex.Unlock()
+	}
+
 	nw, err := n.backend.GetNetworkByName(create.Name)
 	if _, ok := err.(libnetwork.ErrNoSuchNetwork); err != nil && !ok {
 		return err
