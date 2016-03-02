7c7050e095bc1700a73195bf5df49e24959a566a
Ron Peleg <ron.peleg@gmail.com>
2016-03-02 21:38:14 +0200

    added comments to name_locker
    Signed-off-by: Ron Peleg <ron.peleg@gmail.com>

--- api/server/router/network/name_locker.go
+++ api/server/router/network/name_locker.go
@@ -15,6 +15,15 @@ func (n *nameLocker) init() {
 	}
 }
 
+// Each goroutine that reaches this function races to create a named entry in
+// the protected map. The first one per name to succeed will:
+// 1. create the named entry
+// 2. create a channel bound to this entry
+// 3. continue to exit the function and do its thing
+//
+// The other (per name) will wait on the channel, until the first goroutine
+// calls unlock (which removes the named entry and closes the channel, releasing
+// all). They will now race to be the next to recreate the entry/channel
 func (n *nameLocker) lock(name string) chan bool {
 	acquired := false
 	for !acquired {
@@ -31,6 +40,8 @@ func (n *nameLocker) lock(name string) chan bool {
 	return n.muxMap[name]
 }
 
+// the goroutine that reaches unlock makes sure that the entry is removed, only
+// then releaseing the rest that wait on this channel
 func (n *nameLocker) unlock(name string, c chan bool) {
 	n.mutex.Lock()
 	if n.muxMap[name] != nil {
