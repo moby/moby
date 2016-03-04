246c5643bace697767f83d263dfd63d64c3457bf
Phil Estes <estesp@linux.vnet.ibm.com>
2016-03-04 12:00:05 -0500

    Fixups for updated runc/libcontainer namespaces code
    Changes to match updates/changes in libcontainer
    
    Docker-DCO-1.1-Signed-off-by: Phil Estes <estesp@linux.vnet.ibm.com> (github: estesp)

--- daemon/execdriver/native/init.go
+++ daemon/execdriver/native/init.go
@@ -5,7 +5,9 @@ package native
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"runtime"
+	"strconv"
 
 	"github.com/docker/docker/pkg/reexec"
 	"github.com/opencontainers/runc/libcontainer"
@@ -16,6 +18,22 @@ func init() {
 }
 
 func fatal(err error) {
+	// return proper unix error codes
+	if exerr, ok := err.(*exec.Error); ok {
+		switch exerr.Err {
+		case os.ErrPermission:
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(126)
+		case exec.ErrNotFound:
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(127)
+		default:
+			if os.IsNotExist(exerr.Err) {
+				fmt.Fprintf(os.Stderr, "exec: %s: %v\n", strconv.Quote(exerr.Name), os.ErrNotExist)
+				os.Exit(127)
+			}
+		}
+	}
 	if lerr, ok := err.(libcontainer.Error); ok {
 		lerr.Detail(os.Stderr)
 		os.Exit(1)
