928ea1e957812c351b77bb33be7f3ef9fd629231
Tibor Vass <tibor@docker.com>
2016-03-23 12:26:23 -0400

    Bump API Version to v1.24
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- api/common.go
+++ api/common.go
@@ -18,7 +18,7 @@ import (
 // Common constants for daemon and client.
 const (
 	// Version of Current REST API
-	DefaultVersion version.Version = "1.23"
+	DefaultVersion version.Version = "1.24"
 
 	// MinVersion represents Minimum REST API version supported
 	MinVersion version.Version = "1.12"
