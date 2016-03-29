6fa0239772e672eefb98cef91ca8d806b86182b0
John Starks <jostarks@microsoft.com>
2016-03-28 17:35:56 -0700

    Windows: escape entrypoint before passing to libcontainerd
    This makes Windows behavior consistent with Linux -- the entry point must
    be an executable, not an executable and set of arguments.
    
    Signed-off-by: John Starks <jostarks@microsoft.com>

--- daemon/exec_windows.go
+++ daemon/exec_windows.go
@@ -8,7 +8,6 @@ import (
 
 func execSetPlatformOpt(c *container.Container, ec *exec.Config, p *libcontainerd.Process) error {
 	// Process arguments need to be escaped before sending to OCI.
-	// TODO (jstarks): escape the entrypoint too once the tests are fixed to not rely on this behavior
-	p.Args = append([]string{p.Args[0]}, escapeArgs(p.Args[1:])...)
+	p.Args = escapeArgs(p.Args)
 	return nil
 }
