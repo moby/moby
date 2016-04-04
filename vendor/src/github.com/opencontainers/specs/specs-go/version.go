cc5c9013d90ea6679fc74c32f6db388e7be33e64
Tonis Tiigi <tonistiigi@gmail.com>
2016-04-04 14:27:44 -0700

    Define readonly/mask paths in spec
    This vendors in new spec/runc that supports
    setting readonly and masked paths in the
    configuration. Using this allows us to make an
    exception for `—-privileged`.
    
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>
    (cherry picked from commit 3f81b4935292d5daedea9de4e2db0895986115da)

--- vendor/src/github.com/opencontainers/specs/specs-go/version.go
+++ vendor/src/github.com/opencontainers/specs/specs-go/version.go
@@ -6,12 +6,12 @@ const (
 	// VersionMajor is for an API incompatible changes
 	VersionMajor = 0
 	// VersionMinor is for functionality in a backwards-compatible manner
-	VersionMinor = 4
+	VersionMinor = 5
 	// VersionPatch is for backwards-compatible bug fixes
 	VersionPatch = 0
 
 	// VersionDev indicates development branch. Releases will be empty string.
-	VersionDev = ""
+	VersionDev = "-dev"
 )
 
 // Version is the specification version that the package types support.
