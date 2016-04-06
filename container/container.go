084e8717a1cd0ef75ebe68dbe731dd145432b3ba
Dan Walsh <dwalsh@redhat.com>
2016-04-06 08:09:21 -0400

    Standardize the container json log name.
    Currently the json log includes the UUID of the container, even though
    the log file is already stored in a RootResource Directory with is UUID based.
    All of the other content in the RootResource Directory is not UUID based so
    using a standard name does not seem to be a problem.  Changing it to a standard
    name will allow us to label it correctly on creation using SELinux.  This will
    allow other logging tools to be able to access just the log files but none of
    the other data related to the container.
    
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>

--- container/container.go
+++ container/container.go
@@ -311,7 +311,7 @@ func (container *Container) StartLogger(cfg containertypes.LogConfig) (logger.Lo
 
 	// Set logging file for "json-logger"
 	if cfg.Type == jsonfilelog.Name {
-		ctx.LogPath, err = container.GetRootResourcePath(fmt.Sprintf("%s-json.log", container.ID))
+		ctx.LogPath, err = container.GetRootResourcePath("container-json.log")
 		if err != nil {
 			return nil, err
 		}
