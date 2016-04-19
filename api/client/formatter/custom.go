38316633255e9d2ae800dbf89aa3882438cbf842
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-04-19 17:59:45 +0100

    Add color arguments to the `--format` command
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- api/client/formatter/custom.go
+++ api/client/formatter/custom.go
@@ -11,6 +11,7 @@ import (
 	"github.com/docker/docker/pkg/stringutils"
 	"github.com/docker/engine-api/types"
 	"github.com/docker/go-units"
+	"github.com/fatih/color"
 )
 
 const (
@@ -240,4 +241,184 @@ func stripNamePrefix(ss []string) []string {
 	}
 
 	return sss
+}
+
+// colors
+
+// Base
+
+func (c *containerContext) Reset(s string) string {
+	return color.New(color.Reset).SprintFunc()(s)
+}
+
+func (c *containerContext) Bold(s string) string {
+	return color.New(color.Bold).SprintFunc()(s)
+}
+
+func (c *containerContext) Faint(s string) string {
+	return color.New(color.Faint).SprintFunc()(s)
+}
+
+func (c *containerContext) Italic(s string) string {
+	return color.New(color.Italic).SprintFunc()(s)
+}
+
+func (c *containerContext) Underline(s string) string {
+	return color.New(color.Underline).SprintFunc()(s)
+}
+
+func (c *containerContext) BlinkSlow(s string) string {
+	return color.New(color.BlinkSlow).SprintFunc()(s)
+}
+
+func (c *containerContext) BlinkRapide(s string) string {
+	return color.New(color.BlinkRapid).SprintFunc()(s)
+}
+
+func (c *containerContext) ReverseVideo(s string) string {
+	return color.New(color.ReverseVideo).SprintFunc()(s)
+}
+
+func (c *containerContext) Concealed(s string) string {
+	return color.New(color.Concealed).SprintFunc()(s)
+}
+
+func (c *containerContext) CrossedOut(s string) string {
+	return color.New(color.CrossedOut).SprintFunc()(s)
+}
+
+// Foreground
+
+func (c *containerContext) Black(s string) string {
+	return color.BlackString(s)
+}
+
+func (c *containerContext) Red(s string) string {
+	return color.RedString(s)
+}
+
+func (c *containerContext) Green(s string) string {
+	return color.GreenString(s)
+}
+
+func (c *containerContext) Yellow(s string) string {
+	return color.YellowString(s)
+}
+
+func (c *containerContext) Blue(s string) string {
+	return color.BlueString(s)
+}
+
+func (c *containerContext) Magenta(s string) string {
+	return color.MagentaString(s)
+}
+
+func (c *containerContext) Cyan(s string) string {
+	return color.CyanString(s)
+}
+
+func (c *containerContext) White(s string) string {
+	return color.CyanString(s)
+}
+
+// Foreground Hi-Intensity text colors
+
+func (c *containerContext) HiBlack(s string) string {
+	return color.New(color.FgHiBlack).SprintFunc()(s)
+}
+
+func (c *containerContext) HiRed(s string) string {
+	return color.New(color.FgHiRed).SprintFunc()(s)
+}
+
+func (c *containerContext) HiGreen(s string) string {
+	return color.New(color.FgHiGreen).SprintFunc()(s)
+}
+
+func (c *containerContext) HiYellow(s string) string {
+	return color.New(color.FgHiYellow).SprintFunc()(s)
+}
+
+func (c *containerContext) HiBlue(s string) string {
+	return color.New(color.FgHiBlue).SprintFunc()(s)
+}
+
+func (c *containerContext) HiMagenta(s string) string {
+	return color.New(color.FgHiMagenta).SprintFunc()(s)
+}
+
+func (c *containerContext) HiCyan(s string) string {
+	return color.New(color.FgHiCyan).SprintFunc()(s)
+}
+
+func (c *containerContext) HiWhite(s string) string {
+	return color.New(color.FgHiWhite).SprintFunc()(s)
+}
+
+// Background BgBlack colors
+
+func (c *containerContext) BgBlack(s string) string {
+	return color.New(color.BgBlack).SprintFunc()(s)
+}
+
+func (c *containerContext) BgRed(s string) string {
+	return color.New(color.BgRed).SprintFunc()(s)
+}
+
+func (c *containerContext) BgGreen(s string) string {
+	return color.New(color.BgGreen).SprintFunc()(s)
+}
+
+func (c *containerContext) BgYellow(s string) string {
+	return color.New(color.BgYellow).SprintFunc()(s)
+}
+
+func (c *containerContext) BgBlue(s string) string {
+	return color.New(color.BgBlue).SprintFunc()(s)
+}
+
+func (c *containerContext) BgMagenta(s string) string {
+	return color.New(color.BgMagenta).SprintFunc()(s)
+}
+
+func (c *containerContext) BgCyan(s string) string {
+	return color.New(color.BgCyan).SprintFunc()(s)
+}
+
+func (c *containerContext) BgWhite(s string) string {
+	return color.New(color.BgWhite).SprintFunc()(s)
+}
+
+// Background Hi-Intensity text colors
+
+func (c *containerContext) BgHiBlack(s string) string {
+	return color.New(color.BgHiBlack).SprintFunc()(s)
+}
+
+func (c *containerContext) BgHiRed(s string) string {
+	return color.New(color.BgHiRed).SprintFunc()(s)
+}
+
+func (c *containerContext) BgHiGreen(s string) string {
+	return color.New(color.BgHiGreen).SprintFunc()(s)
+}
+
+func (c *containerContext) BgHiYellow(s string) string {
+	return color.New(color.BgHiYellow).SprintFunc()(s)
+}
+
+func (c *containerContext) BgHiBlue(s string) string {
+	return color.New(color.BgHiBlue).SprintFunc()(s)
+}
+
+func (c *containerContext) BgHiMagenta(s string) string {
+	return color.New(color.BgHiMagenta).SprintFunc()(s)
+}
+
+func (c *containerContext) BgHiCyan(s string) string {
+	return color.New(color.BgHiCyan).SprintFunc()(s)
+}
+
+func (c *containerContext) BgHiWhite(s string) string {
+	return color.New(color.BgHiWhite).SprintFunc()(s)
 }
