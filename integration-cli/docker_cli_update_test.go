cdffa2ea56973a46bfbbce480475eda65c28ad4d
Pranay Kanwar <pranay.kanwar@gmail.com>
2016-06-05 19:50:44 +0530

    Add support for updating log options via update cli command
    Signed-off-by: Pranay Kanwar <pranay.kanwar@gmail.com>

--- integration-cli/docker_cli_update_test.go
+++ integration-cli/docker_cli_update_test.go
@@ -29,3 +29,42 @@ func (s *DockerSuite) TestUpdateRestartPolicy(c *check.C) {
 	maximumRetryCount := inspectField(c, id, "HostConfig.RestartPolicy.MaximumRetryCount")
 	c.Assert(maximumRetryCount, checker.Equals, "5")
 }
+
+func (s *DockerSuite) TestUpdateLogOptRunningContainer(c *check.C) {
+	name := "test-update-container"
+	dockerCmd(c, "run", "-d", "--name", name, "--log-driver=json-file", "--log-opt", "max-file=1", "--log-opt", "max-size=1m", "busybox", "top")
+	dockerCmd(c, "update", "--log-opt", "max-file=2", "--log-opt", "max-size=2m", name)
+
+	out, _ := dockerCmd(c, "inspect", "-f", "{{ .HostConfig.LogConfig.Config }}", name)
+	c.Assert(out, checker.Contains, "max-size:2m")
+	c.Assert(out, checker.Contains, "max-file:2")
+}
+
+func (s *DockerSuite) TestUpdateLogOptStoppedContainer(c *check.C) {
+	name := "test-update-container"
+
+	dockerCmd(c, "run", "--name", name, "--log-driver=json-file", "--log-opt", "max-file=1", "--log-opt", "max-size=1m", "busybox", "ls")
+	dockerCmd(c, "update", "--log-opt", "max-file=2", "--log-opt", "max-size=2m", name)
+
+	out, _ := dockerCmd(c, "inspect", "-f", "{{ .HostConfig.LogConfig.Config }}", name)
+	c.Assert(out, checker.Contains, "max-size:2m")
+	c.Assert(out, checker.Contains, "max-file:2")
+}
+
+func (s *DockerSuite) TestUpdateLogOptPausedContainer(c *check.C) {
+	name := "test-update-container"
+
+	dockerCmd(c, "run", "-d", "--name", name, "--log-driver=json-file", "--log-opt", "max-file=1", "--log-opt", "max-size=1m", "busybox", "top")
+	dockerCmd(c, "pause", name)
+	dockerCmd(c, "update", "--log-opt", "max-file=2", "--log-opt", "max-size=2m", name)
+
+	out, _ := dockerCmd(c, "inspect", "-f", "{{ .HostConfig.LogConfig.Config }}", name)
+	c.Assert(out, checker.Contains, "max-size:2m")
+	c.Assert(out, checker.Contains, "max-file:2")
+
+	dockerCmd(c, "unpause", name)
+
+	out, _ = dockerCmd(c, "inspect", "-f", "{{ .HostConfig.LogConfig.Config }}", name)
+	c.Assert(out, checker.Contains, "max-size:2m")
+	c.Assert(out, checker.Contains, "max-file:2")
+}
