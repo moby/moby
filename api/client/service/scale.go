5d07eb38e25392ddfb4e24da1ab45952c3961145
Daniel Nephin <dnephin@docker.com>
2016-06-13 11:22:54 -0400

    Small UX improvements to 'docker service scale'
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

--- api/client/service/scale.go
+++ api/client/service/scale.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+
+	"golang.org/x/net/context"
 
 	"github.com/docker/docker/api/client"
 	"github.com/docker/docker/cli"
@@ -11,20 +14,40 @@ import (
 
 func newScaleCommand(dockerCli *client.DockerCli) *cobra.Command {
 	return &cobra.Command{
-		Use:   "scale SERVICE=<SCALE> [SERVICE=<SCALE>...]",
+		Use:   "scale SERVICE=SCALE [SERVICE=SCALE...]",
 		Short: "Scale one or multiple services",
-		Args:  cli.RequiresMinArgs(1),
+		Args:  scaleArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runScale(dockerCli, args)
 		},
 	}
 }
 
+func scaleArgs(cmd *cobra.Command, args []string) error {
+	if err := cli.RequiresMinArgs(1)(cmd, args); err != nil {
+		return err
+	}
+	for _, arg := range args {
+		if parts := strings.SplitN(arg, "=", 2); len(parts) != 2 {
+			return fmt.Errorf(
+				"Invalid scale specifier '%s'.\nSee '%s --help'.\n\nUsage:  %s\n\n%s",
+				arg,
+				cmd.CommandPath(),
+				cmd.UseLine(),
+				cmd.Short,
+			)
+		}
+	}
+	return nil
+}
+
 func runScale(dockerCli *client.DockerCli, args []string) error {
 	var errors []string
 	for _, arg := range args {
-		if err := runServiceScale(dockerCli, arg); err != nil {
-			errors = append(errors, err.Error())
+		parts := strings.SplitN(arg, "=", 2)
+		serviceID, scale := parts[0], parts[1]
+		if err := runServiceScale(dockerCli, serviceID, scale); err != nil {
+			errors = append(errors, fmt.Sprintf("%s: %s", serviceID, err.Error()))
 		}
 	}
 
@@ -34,15 +57,30 @@ func runScale(dockerCli *client.DockerCli, args []string) error {
 	return fmt.Errorf(strings.Join(errors, "\n"))
 }
 
-func runServiceScale(dockerCli *client.DockerCli, arg string) error {
-	var parts []string
-	if parts = strings.SplitN(arg, "=", 2); len(parts) != 2 {
-		return fmt.Errorf("%s: invalid scale specifier (expected \"<service>=<value>\")", arg)
+func runServiceScale(dockerCli *client.DockerCli, serviceID string, scale string) error {
+	client := dockerCli.Client()
+	ctx := context.Background()
+
+	service, err := client.ServiceInspect(ctx, serviceID)
+	if err != nil {
+		return err
 	}
 
-	flags := newUpdateCommand(dockerCli).Flags()
-	if err := flags.Set("scale", parts[1]); err != nil {
-		return fmt.Errorf("%s: invalid value %q for scale", parts[0], parts[1])
+	serviceMode := &service.Spec.Mode
+	if serviceMode.Replicated == nil {
+		return fmt.Errorf("scale can only be used with replicated mode")
 	}
-	return runUpdate(dockerCli, flags, parts[0])
+	uintScale, err := strconv.ParseUint(scale, 10, 64)
+	if err != nil {
+		return fmt.Errorf("invalid scale value %s: %s", scale, err.Error())
+	}
+	serviceMode.Replicated.Instances = &uintScale
+
+	err = client.ServiceUpdate(ctx, service.ID, service.Version, service.Spec)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(dockerCli.Out(), "%s scaled to %s", serviceID, scale)
+	return nil
 }
