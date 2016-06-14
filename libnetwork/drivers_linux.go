fe56a8d3dc7fa739830cac3035dcf134e77413c6
Madhu Venugopal <madhu@docker.com>
2016-06-13 19:38:18 -0700

    Moving MacVlan driver out of experimental
    Signed-off-by: Madhu Venugopal <madhu@docker.com>

--- libnetwork/drivers_linux.go
+++ libnetwork/drivers_linux.go
@@ -3,6 +3,7 @@ package libnetwork
 import (
 	"github.com/docker/libnetwork/drivers/bridge"
 	"github.com/docker/libnetwork/drivers/host"
+	"github.com/docker/libnetwork/drivers/macvlan"
 	"github.com/docker/libnetwork/drivers/null"
 	"github.com/docker/libnetwork/drivers/overlay"
 	"github.com/docker/libnetwork/drivers/remote"
@@ -12,6 +13,7 @@ func getInitializers() []initializer {
 	in := []initializer{
 		{bridge.Init, "bridge"},
 		{host.Init, "host"},
+		{macvlan.Init, "macvlan"},
 		{null.Init, "null"},
 		{remote.Init, "remote"},
 		{overlay.Init, "overlay"},
