680425a923cbd318dd9205b9343bad35db74f77d
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-06-16 23:50:54 +0300

    Parametrize docker install version
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

--- hack/install.sh
+++ hack/install.sh
@@ -304,7 +304,12 @@ do_install() {
 		amzn)
 			(
 			set -x
-			$sh_c 'sleep 3; yum -y -q install docker'
+			if [ -z "$DOCKER_VERSION" ]; then
+				$sh_c 'sleep 3; yum -y -q install docker'
+			else
+				$sh_c "sleep 3; yum -y -q install docker-${DOCKER_VERSION}"
+			fi
+			
 			)
 			echo_docker_as_nonroot
 			exit 0
@@ -363,7 +368,11 @@ do_install() {
 			fi
 			(
 				set -x
-				zypper -n install docker
+				if [ -z "$DOCKER_VERSION" ]; then
+					zypper -n install docker
+				else
+					zypper -n install "docker=${DOCKER_VERSION}"
+				fi
 			)
 			echo_docker_as_nonroot
 			exit 0
@@ -430,7 +439,13 @@ do_install() {
 			$sh_c "apt-key adv -k ${gpg_fingerprint} >/dev/null"
 			$sh_c "mkdir -p /etc/apt/sources.list.d"
 			$sh_c "echo deb \[arch=$(dpkg --print-architecture)\] ${apt_url}/repo ${lsb_dist}-${dist_version} ${repo} > /etc/apt/sources.list.d/docker.list"
-			$sh_c 'sleep 3; apt-get update; apt-get install -y -q docker-engine'
+			$sh_c 'sleep 3; apt-get update'
+			
+			if [ -z "$DOCKER_VERSION" ]; then
+			    $sh_c 'apt-get install -y -q docker-engine'
+			else
+			    $sh_c "apt-get -o Dpkg::Options::=\"--force-confnew\" install -y -q docker-engine=$DOCKER_VERSION"
+			fi
 			)
 			echo_docker_as_nonroot
 			exit 0
@@ -448,12 +463,20 @@ do_install() {
 			if [ "$lsb_dist" = "fedora" ] && [ "$dist_version" -ge "22" ]; then
 				(
 					set -x
-					$sh_c 'sleep 3; dnf -y -q install docker-engine'
+					if [ -z "$DOCKER_VERSION" ]; then
+					    $sh_c 'sleep 3; dnf -y -q install docker-engine'
+					else
+					    $sh_c "sleep 3; dnf -y -q install docker-engine-${DOCKER_VERSION}"
+					fi
 				)
 			else
 				(
 					set -x
-					$sh_c 'sleep 3; yum -y -q install docker-engine'
+					if [ -z "$DOCKER_VERSION" ]; then
+					    $sh_c 'sleep 3; yum -y -q install docker-engine'
+					else
+					    $sh_c "sleep 3; yum -y -q install docker-engine-${DOCKER_VERSION}"
+					fi
 				)
 			fi
 			echo_docker_as_nonroot
@@ -482,7 +505,11 @@ do_install() {
 
 			(
 				set -x
-				$sh_c 'sleep 3; emerge app-emulation/docker'
+				if [ -z "$DOCKER_VERSION" ]; then
+					$sh_c 'sleep 3; emerge app-emulation/docker'
+				else
+					$sh_c "sleep 3; emerge =app-emulation/docker-${DOCKER_VERSION}"
+				fi
 			)
 			exit 0
 			;;
