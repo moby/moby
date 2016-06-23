8fcc533ca4f2646ced83ccad9c4355cf3980e9d8
Daniel Nephin <dnephin@docker.com>
2016-06-23 13:03:40 -0400

    Move canonical image and container commands into a command group
    Hide some top level commands
    Add docker container and image inspect commands.
    
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

--- cli/command/stack/cmd.go
+++ cli/command/stack/cmd.go
@@ -32,8 +32,8 @@ func NewStackCommand(dockerCli *command.DockerCli) *cobra.Command {
 
 // NewTopLevelDeployCommand returns a command for `docker deploy`
 func NewTopLevelDeployCommand(dockerCli *command.DockerCli) *cobra.Command {
-	cmd := newDeployCommand(dockerCli)
+	cmd := *newDeployCommand(dockerCli)
 	// Remove the aliases at the top level
 	cmd.Aliases = []string{}
-	return cmd
+	return &cmd
 }
