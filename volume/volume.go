29fea0fd2e9970eb5df0feb0ebef41c5425295f0
Robert Terhaar <robbyt@users.noreply.github.com>
2016-07-01 17:29:08 -0400

    fixes minor typo in comment
    Signed-off-by: Robert Terhaar <rterhaar@atlanticdynamic.com>

--- volume/volume.go
+++ volume/volume.go
@@ -34,7 +34,7 @@ type Driver interface {
 	List() ([]Volume, error)
 	// Get retrieves the volume with the requested name
 	Get(name string) (Volume, error)
-	// Scope returns the scope of the driver (e.g. `golbal` or `local`).
+	// Scope returns the scope of the driver (e.g. `global` or `local`).
 	// Scope determines how the driver is handled at a cluster level
 	Scope() string
 }
