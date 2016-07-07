3c338c7d8aa3680f912c96df231eaf2aefca04e4
Justin Cormack <justin.cormack@docker.com>
2016-07-07 12:58:43 +0100

    Update swarmkit vendoring
    Needed for libnetwork vendoring
    
    Update Secret API name change correspondingly
    
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>
    (cherry picked from commit d428a7a425f071ee9e5707c7319d3197540adc19)
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- vendor/src/github.com/docker/swarmkit/manager/state/raft/membership/cluster.go
+++ vendor/src/github.com/docker/swarmkit/manager/state/raft/membership/cluster.go
@@ -165,19 +165,11 @@ func (c *Cluster) ValidateConfigurationChange(cc raftpb.ConfChange) error {
 // that might block or harm the Cluster on Member recovery
 func (c *Cluster) CanRemoveMember(from uint64, id uint64) bool {
 	members := c.Members()
-
-	nmembers := 0
 	nreachable := 0
 
 	for _, m := range members {
-		// Skip the node that is going to be deleted
-		if m.RaftID == id {
-			continue
-		}
-
 		// Local node from where the remove is issued
 		if m.RaftID == from {
-			nmembers++
 			nreachable++
 			continue
 		}
@@ -186,8 +178,6 @@ func (c *Cluster) CanRemoveMember(from uint64, id uint64) bool {
 		if err == nil && connState == grpc.Ready {
 			nreachable++
 		}
-
-		nmembers++
 	}
 
 	// Special case of 2 managers
@@ -195,7 +185,7 @@ func (c *Cluster) CanRemoveMember(from uint64, id uint64) bool {
 		return false
 	}
 
-	nquorum := nmembers/2 + 1
+	nquorum := (len(members)+1)/2 + 1
 	if nreachable < nquorum {
 		return false
 	}
