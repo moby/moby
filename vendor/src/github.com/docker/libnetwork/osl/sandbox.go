cabc5b27c0d93a7665a24d205572a9462bb85da6
Alessandro Boch <aboch@docker.com>
2016-07-12 21:51:11 -0700

    Vendoring libnetwork 905d374
    Signed-off-by: Alessandro Boch <aboch@docker.com>

--- vendor/src/github.com/docker/libnetwork/osl/sandbox.go
+++ vendor/src/github.com/docker/libnetwork/osl/sandbox.go
@@ -91,6 +91,9 @@ type IfaceOptionSetter interface {
 	// LinkLocalAddresses returns an option setter to set the link-local IP addresses.
 	LinkLocalAddresses([]*net.IPNet) IfaceOption
 
+	// IPAliases returns an option setter to set IP address Aliases
+	IPAliases([]*net.IPNet) IfaceOption
+
 	// Master returns an option setter to set the master interface if any for this
 	// interface. The master interface name should refer to the srcname of a
 	// previously added interface of type bridge.
@@ -146,6 +149,9 @@ type Interface interface {
 
 	// LinkLocalAddresses returns the link-local IP addresses assigned to the interface.
 	LinkLocalAddresses() []*net.IPNet
+
+	// IPAliases returns the IP address aliases assigned to the interface.
+	IPAliases() []*net.IPNet
 
 	// IP routes for the interface.
 	Routes() []*net.IPNet
