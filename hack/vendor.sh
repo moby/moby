db9bc51cbeb40204ff15ea1a40db8d3736ad3845
Aaron Lehmann <aaron.lehmann@docker.com>
2016-07-18 10:36:07 -0700

    Update vendored go-events
    This is needed to suppress a log message about a harmless condition
    which was previously logged at the WARNING log level with potentially
    high frequency (https://github.com/docker/go-events/pull/11).
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -66,7 +66,7 @@ clone git github.com/imdario/mergo 0.2.1
 
 #get libnetwork packages
 clone git github.com/docker/libnetwork 905d374c096ca1f3a9b75529e52518b7540179f3
-clone git github.com/docker/go-events 39718a26497694185f8fb58a7d6f31947f3dc42d
+clone git github.com/docker/go-events afb2b9f2c23f33ada1a22b03651775fdc65a5089
 clone git github.com/armon/go-radix e39d623f12e8e41c7b5529e9a9dd67a1e2261f80
 clone git github.com/armon/go-metrics eb0af217e5e9747e41dd5303755356b62d28e3ec
 clone git github.com/hashicorp/go-msgpack 71c2886f5a673a35f909803f38ece5810165097b
