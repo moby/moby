05b7fe417073ef1d0b8542916d73f41e8ecfe730
Anusha Ragunathan <anusha@docker.com>
2016-07-19 08:35:36 -0700

    Remove extra map lookup for volume drivers.
    Signed-off-by: Anusha Ragunathan <anusha@docker.com>
    (cherry picked from commit 5d4627331ee0c685116b083e9f25f2644f0d1bd0)
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- volume/drivers/extpoint.go
+++ volume/drivers/extpoint.go
@@ -107,19 +107,15 @@ func lookup(name string) (volume.Driver, error) {
 		return nil, fmt.Errorf("Error looking up volume plugin %s: %v", name, err)
 	}
 
-	drivers.Lock()
-	defer drivers.Unlock()
-	if ext, ok := drivers.extensions[name]; ok {
-		return ext, nil
-	}
-
 	d := NewVolumeDriver(name, p.Client())
 	if err := validateDriver(d); err != nil {
 		return nil, err
 	}
 
 	if p.IsLegacy() {
+		drivers.Lock()
 		drivers.extensions[name] = d
+		drivers.Unlock()
 	}
 	return d, nil
 }
