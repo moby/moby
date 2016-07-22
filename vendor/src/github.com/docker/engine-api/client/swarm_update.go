9a3e47511ab9faac599b8f085a1b755ec941e49c
Sebastiaan van Stijn <github@gone.nl>
2016-07-23 01:05:45 +0200

    bump engine-api to ebb728a1346926edc2ad9418f9b6045901810b20
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/src/github.com/docker/engine-api/client/swarm_update.go
+++ vendor/src/github.com/docker/engine-api/client/swarm_update.go
@@ -13,8 +13,8 @@ import (
 func (cli *Client) SwarmUpdate(ctx context.Context, version swarm.Version, swarm swarm.Spec, flags swarm.UpdateFlags) error {
 	query := url.Values{}
 	query.Set("version", strconv.FormatUint(version.Index, 10))
-	query.Set("rotate_worker_token", fmt.Sprintf("%v", flags.RotateWorkerToken))
-	query.Set("rotate_manager_token", fmt.Sprintf("%v", flags.RotateManagerToken))
+	query.Set("rotateWorkerToken", fmt.Sprintf("%v", flags.RotateWorkerToken))
+	query.Set("rotateManagerToken", fmt.Sprintf("%v", flags.RotateManagerToken))
 	resp, err := cli.post(ctx, "/swarm/update", query, swarm, nil)
 	ensureReaderClosed(resp)
 	return err
