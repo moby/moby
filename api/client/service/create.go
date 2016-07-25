262006406f6baa539062d62df93ea1510caa55ce
Vincent Demeester <vincent@sbr.pm>
2016-07-25 11:38:01 +0200

    Add container labels to service create/update
    Swarm mode makes it possible through the API to set labels to containers
    but not through command line. This tries to fix it.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- api/client/service/create.go
+++ api/client/service/create.go
@@ -30,6 +30,7 @@ func newCreateCommand(dockerCli *client.DockerCli) *cobra.Command {
 	addServiceFlags(cmd, opts)
 
 	flags.VarP(&opts.labels, flagLabel, "l", "Service labels")
+	flags.Var(&opts.containerLabels, flagContainerLabel, "Container labels")
 	flags.VarP(&opts.env, flagEnv, "e", "Set environment variables")
 	flags.Var(&opts.mounts, flagMount, "Attach a mount to the service")
 	flags.StringSliceVar(&opts.constraints, flagConstraint, []string{}, "Placement constraints")
