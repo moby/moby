0f555c133cffb05c01981fe75fd818ea4622413c
Brian Goff <cpuguy83@gmail.com>
2016-07-25 15:24:34 -0400

    Add formatter for service inspect
    Allows the user to use `pretty` as the format string.
    This enables users to put custom format options into their CLI config
    just like is supported for `docker ps` and `docker images`
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- cliconfig/configfile/file.go
+++ cliconfig/configfile/file.go
@@ -22,15 +22,16 @@ const (
 
 // ConfigFile ~/.docker/config.json file info
 type ConfigFile struct {
-	AuthConfigs      map[string]types.AuthConfig `json:"auths"`
-	HTTPHeaders      map[string]string           `json:"HttpHeaders,omitempty"`
-	PsFormat         string                      `json:"psFormat,omitempty"`
-	ImagesFormat     string                      `json:"imagesFormat,omitempty"`
-	NetworksFormat   string                      `json:"networksFormat,omitempty"`
-	VolumesFormat    string                      `json:"volumesFormat,omitempty"`
-	DetachKeys       string                      `json:"detachKeys,omitempty"`
-	CredentialsStore string                      `json:"credsStore,omitempty"`
-	Filename         string                      `json:"-"` // Note: for internal use only
+	AuthConfigs          map[string]types.AuthConfig `json:"auths"`
+	HTTPHeaders          map[string]string           `json:"HttpHeaders,omitempty"`
+	PsFormat             string                      `json:"psFormat,omitempty"`
+	ImagesFormat         string                      `json:"imagesFormat,omitempty"`
+	NetworksFormat       string                      `json:"networksFormat,omitempty"`
+	VolumesFormat        string                      `json:"volumesFormat,omitempty"`
+	DetachKeys           string                      `json:"detachKeys,omitempty"`
+	CredentialsStore     string                      `json:"credsStore,omitempty"`
+	Filename             string                      `json:"-"` // Note: for internal use only
+	ServiceInspectFormat string                      `json:"serviceInspectFormat,omitempty"`
 }
 
 // LegacyLoadFromReader reads the non-nested configuration data given and sets up the
