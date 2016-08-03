9be0fb45c25e4d8d3cf0aa444da5ae41dd18f435
Tonis Tiigi <tonistiigi@gmail.com>
2016-08-03 15:28:52 -0700

    libcontainerd: mark container exited after failed restart
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- libcontainerd/container_windows.go
+++ libcontainerd/container_windows.go
@@ -260,14 +260,16 @@ func (ctr *container) waitExit(process *process, isFirstProcessToStart bool) err
 			err := <-waitRestart
 			ctr.restarting = false
 			ctr.client.deleteContainer(ctr.friendlyName)
+			if err == nil {
+				if err = ctr.client.Create(ctr.containerID, ctr.ociSpec, ctr.options...); err != nil {
+					logrus.Errorf("libcontainerd: error restarting %v", err)
+				}
+			}
 			if err != nil {
 				si.State = StateExit
 				if err := ctr.client.backend.StateChanged(ctr.containerID, si); err != nil {
 					logrus.Error(err)
 				}
-				logrus.Error(err)
-			} else {
-				ctr.client.Create(ctr.containerID, ctr.ociSpec, ctr.options...)
 			}
 		}()
 	}
