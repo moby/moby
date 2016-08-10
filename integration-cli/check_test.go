e34244842ca968ff96769d1b5eab52fecddee80d
Brian Goff <cpuguy83@gmail.com>
2016-08-10 15:18:35 -0400

    Ignore socket rm errors on daemon suite teardown
    Errors here are not important and not really related to set success/failure.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- integration-cli/check_test.go
+++ integration-cli/check_test.go
@@ -9,7 +9,6 @@ import (
 	"testing"
 
 	"github.com/docker/docker/cliconfig"
-	"github.com/docker/docker/pkg/integration/checker"
 	"github.com/docker/docker/pkg/reexec"
 	"github.com/docker/engine-api/types/swarm"
 	"github.com/go-check/check"
@@ -189,18 +188,18 @@ func (s *DockerDaemonSuite) TearDownTest(c *check.C) {
 }
 
 func (s *DockerDaemonSuite) TearDownSuite(c *check.C) {
-	err := filepath.Walk(daemonSockRoot, func(path string, fi os.FileInfo, err error) error {
+	filepath.Walk(daemonSockRoot, func(path string, fi os.FileInfo, err error) error {
 		if err != nil {
-			return err
+			// ignore errors here
+			// not cleaning up sockets is not really an error
+			return nil
 		}
 		if fi.Mode() == os.ModeSocket {
 			syscall.Unlink(path)
 		}
 		return nil
 	})
-	c.Assert(err, checker.IsNil, check.Commentf("error while cleaning up daemon sockets"))
-	err = os.RemoveAll(daemonSockRoot)
-	c.Assert(err, checker.IsNil, check.Commentf("could not cleanup daemon socket root"))
+	os.RemoveAll(daemonSockRoot)
 }
 
 const defaultSwarmPort = 2477
