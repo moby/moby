55ea7ae0b3e76559ff30dea7ed43db2eeaef514a
Sven Dowideit <SvenDowideit@users.noreply.github.com>
2016-08-22 15:47:23 +1000

    Merge pull request #25835 from decoursin/patch-1
    Update apparmor error message
    (cherry picked from commit e168f0b4ad4dfc11a4423a6785d80153f0969808)
    
    Signed-off-by: Charles Smith <charles.smith@docker.com>

--- hack/install.sh
+++ hack/install.sh
@@ -421,7 +421,7 @@ do_install() {
 				if command -v apparmor_parser >/dev/null 2>&1; then
 					echo 'apparmor is enabled in the kernel and apparmor utils were already installed'
 				else
-					echo 'apparmor is enabled in the kernel, but apparmor_parser missing'
+					echo 'apparmor is enabled in the kernel, but apparmor_parser is missing. Trying to install it..'
 					apt_get_update
 					( set -x; $sh_c 'sleep 3; apt-get install -y -q apparmor' )
 				fi
