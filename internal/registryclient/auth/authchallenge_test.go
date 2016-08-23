12510505bf0d0c49181faf208323ac0fe3a7184a
spacexnice <yaoyao.xyy@alibaba-inc.com>
2016-08-23 22:09:18 +0800

    fix simpleAuthChallge concurrent problem
    Signed-off-by: spacexnice <yaoyao.xyy@alibaba-inc.com>

--- internal/registryclient/auth/authchallenge_test.go
+++ internal/registryclient/auth/authchallenge_test.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"strings"
 	"testing"
+	"sync"
 )
 
 func TestAuthChallengeParse(t *testing.T) {
@@ -43,6 +44,7 @@ func TestAuthChallengeParse(t *testing.T) {
 func TestAuthChallengeNormalization(t *testing.T) {
 	testAuthChallengeNormalization(t, "reg.EXAMPLE.com")
 	testAuthChallengeNormalization(t, "bɿɒʜɔiɿ-ɿɘƚƨim-ƚol-ɒ-ƨʞnɒʜƚ.com")
+	testAuthChallengeConcurrent(t, "reg.EXAMPLE.com")
 }
 
 func testAuthChallengeNormalization(t *testing.T, host string) {
@@ -78,4 +80,59 @@ func testAuthChallengeNormalization(t *testing.T, host string) {
 	if len(c) == 0 {
 		t.Fatal("Expected challenge for lower-cased-host URL")
 	}
+}
+
+
+func testAuthChallengeConcurrent(t *testing.T, host string) {
+
+	scm := NewSimpleChallengeManager()
+
+	url, err := url.Parse(fmt.Sprintf("http://%s/v2/", host))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp := &http.Response{
+		Request: &http.Request{
+			URL: url,
+		},
+		Header:     make(http.Header),
+		StatusCode: http.StatusUnauthorized,
+	}
+	resp.Header.Add("WWW-Authenticate", fmt.Sprintf("Bearer realm=\"https://%s/token\",service=\"registry.example.com\"", host))
+	var s sync.WaitGroup
+	s.Add(2)
+	go func() {
+		i := 200
+		for {
+			//time.Sleep(500 * time.Millisecond)
+			err = scm.AddResponse(resp)
+			if err != nil {
+				t.Fatal(err)
+			}
+			i = i -1
+			if i < 0{
+				break
+			}
+		}
+		s.Done()
+	}()
+	go func() {
+		lowered := *url
+		lowered.Host = strings.ToLower(lowered.Host)
+		k:= 200
+		for {
+			_, err := scm.GetChallenges(lowered)
+			if err != nil {
+				t.Fatal(err)
+			}
+			k = k -1
+			if k < 0{
+				break
+			}
+		}
+
+		s.Done()
+	}()
+	s.Wait()
 }
