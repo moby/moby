f2a48d2ff3b6efab4148389d291072250c2628b8
Yong Tang <yong.tang.github@outlook.com>
2016-08-23 21:08:23 -0700

    Fix AuthZ plugins headers change issue
    This fix tries to address the issue raised in 25927 where
    the HTTP headers have been chaged when AUthZ plugin is in
    place.
    
    This issue is that in `FlushAll` (`pkg/authorization/response.go`),
    the headers have been written (with `WriteHeader`) before all the
    headers have bee copied.
    
    This fix fixes the issue by placing `WriteHeader` after.
    
    A test has been added to cover the changes.`
    
    This fix fixes 25927
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    (cherry picked from commit 9cb8fb6ea03fcd78010ce7dd33585d96cd73e38c)
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- pkg/authorization/response.go
+++ pkg/authorization/response.go
@@ -175,16 +175,18 @@ func (rm *responseModifier) Flush() {
 
 // FlushAll flushes all data to the HTTP response
 func (rm *responseModifier) FlushAll() error {
-	// Copy the status code
-	if rm.statusCode > 0 {
-		rm.rw.WriteHeader(rm.statusCode)
-	}
-
 	// Copy the header
 	for k, vv := range rm.header {
 		for _, v := range vv {
 			rm.rw.Header().Add(k, v)
 		}
+	}
+
+	// Copy the status code
+	// Also WriteHeader needs to be done after all the headers
+	// have been copied (above).
+	if rm.statusCode > 0 {
+		rm.rw.WriteHeader(rm.statusCode)
 	}
 
 	var err error
