2998945a54577e24a6414d576bc861e58fa87359
Kenfe-Mickael Laventure <mickael.laventure@gmail.com>
2016-08-30 09:44:06 -0700

    Re-export container state's ExitCode and Error fields
    Those are needed in order to reload their value upon docker daemon
    restart.
    
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>

--- container/state_unix.go
+++ container/state_unix.go
@@ -5,6 +5,6 @@ package container
 // setFromExitStatus is a platform specific helper function to set the state
 // based on the ExitStatus structure.
 func (s *State) setFromExitStatus(exitStatus *ExitStatus) {
-	s.exitCode = exitStatus.ExitCode
+	s.ExitCodeValue = exitStatus.ExitCode
 	s.OOMKilled = exitStatus.OOMKilled
 }
