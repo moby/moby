084ff6dc6da4458e8ca0e4fffa9d133a5d39ee92
Stephen J Day <stephen.day@docker.com>
2016-09-06 17:04:43 -0700

    api/client/node: correct ordering in context argument
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- api/client/node/cmd.go
+++ api/client/node/cmd.go
@@ -3,13 +3,11 @@ package node
 import (
 	"fmt"
 
-	"golang.org/x/net/context"
-
-	"github.com/spf13/cobra"
-
 	"github.com/docker/docker/api/client"
 	"github.com/docker/docker/cli"
 	apiclient "github.com/docker/engine-api/client"
+	"github.com/spf13/cobra"
+	"golang.org/x/net/context"
 )
 
 // NewNodeCommand returns a cobra command for `node` subcommands
@@ -37,7 +35,7 @@ func NewNodeCommand(dockerCli *client.DockerCli) *cobra.Command {
 // Reference returns the reference of a node. The special value "self" for a node
 // reference is mapped to the current node, hence the node ID is retrieved using
 // the `/info` endpoint.
-func Reference(client apiclient.APIClient, ctx context.Context, ref string) (string, error) {
+func Reference(ctx context.Context, client apiclient.APIClient, ref string) (string, error) {
 	if ref == "self" {
 		info, err := client.Info(ctx)
 		if err != nil {
