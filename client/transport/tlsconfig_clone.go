f97f9dc05ab5ce6da3b2954651766a7023a92a25
Antonio Murdaca <runcom@redhat.com>
2016-09-08 12:16:10 +0200

    client: transport: fix tlsconfig.Clone
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- client/transport/tlsconfig_clone.go
+++ client/transport/tlsconfig_clone.go
@@ -1,11 +1,31 @@
-// +build !go1.7,!windows
+// +build !go1.8,!go1.7,!windows
 
 package transport
 
 import "crypto/tls"
 
 // TLSConfigClone returns a clone of tls.Config. This function is provided for
-// compatibility for go1.7 that doesn't include this method in stdlib.
+// compatibility for go < 1.7 that doesn't include this method in stdlib.
 func TLSConfigClone(c *tls.Config) *tls.Config {
-	return c.Clone()
+	return &tls.Config{
+		Rand:                     c.Rand,
+		Time:                     c.Time,
+		Certificates:             c.Certificates,
+		NameToCertificate:        c.NameToCertificate,
+		GetCertificate:           c.GetCertificate,
+		RootCAs:                  c.RootCAs,
+		NextProtos:               c.NextProtos,
+		ServerName:               c.ServerName,
+		ClientAuth:               c.ClientAuth,
+		ClientCAs:                c.ClientCAs,
+		InsecureSkipVerify:       c.InsecureSkipVerify,
+		CipherSuites:             c.CipherSuites,
+		PreferServerCipherSuites: c.PreferServerCipherSuites,
+		SessionTicketsDisabled:   c.SessionTicketsDisabled,
+		SessionTicketKey:         c.SessionTicketKey,
+		ClientSessionCache:       c.ClientSessionCache,
+		MinVersion:               c.MinVersion,
+		MaxVersion:               c.MaxVersion,
+		CurvePreferences:         c.CurvePreferences,
+	}
 }
