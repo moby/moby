ecdabde09b9f8dcdeeb3901c841ab352197a2e32
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2016-09-19 07:42:21 +0000

    Fix a race in libcontainerd/pausemonitor_linux.go
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

--- libcontainerd/pausemonitor_linux.go
+++ libcontainerd/pausemonitor_linux.go
@@ -1,11 +1,18 @@
 package libcontainerd
 
+import (
+	"sync"
+)
+
 // pauseMonitor is helper to get notifications from pause state changes.
 type pauseMonitor struct {
+	sync.Mutex
 	waiters map[string][]chan struct{}
 }
 
 func (m *pauseMonitor) handle(t string) {
+	m.Lock()
+	defer m.Unlock()
 	if m.waiters == nil {
 		return
 	}
@@ -20,6 +27,8 @@ func (m *pauseMonitor) handle(t string) {
 }
 
 func (m *pauseMonitor) append(t string, waiter chan struct{}) {
+	m.Lock()
+	defer m.Unlock()
 	if m.waiters == nil {
 		m.waiters = make(map[string][]chan struct{})
 	}
