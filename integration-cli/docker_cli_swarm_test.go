b5eac429ed7e0c8dea17becd6d666a3bd126be23
Alexander Morozov <lk4d4@docker.com>
2016-09-20 11:44:40 -0700

    integration: fix flaky TestSwarmContainerAutoStart
    Fix #26588
    
    Signed-off-by: Alexander Morozov <lk4d4@docker.com>

--- integration-cli/docker_cli_swarm_test.go
+++ integration-cli/docker_cli_swarm_test.go
@@ -250,17 +250,13 @@ func (s *DockerSwarmSuite) TestSwarmContainerAutoStart(c *check.C) {
 	c.Assert(err, checker.IsNil)
 	c.Assert(strings.TrimSpace(out), checker.Not(checker.Equals), "")
 
-	out, err = d.Cmd("run", "-id", "--restart=always", "--net=foo", "--name=test", "busybox", "top")
+	out, err = d.Cmd("run", "-d", "--restart=always", "--net=foo", "--name=test", "busybox", "top")
 	c.Assert(err, checker.IsNil)
 	c.Assert(strings.TrimSpace(out), checker.Not(checker.Equals), "")
 
-	out, err = d.Cmd("ps", "-q")
-	c.Assert(err, checker.IsNil)
-	c.Assert(strings.TrimSpace(out), checker.Not(checker.Equals), "")
+	c.Assert(waitRun("test", "-H", d.sock()), check.IsNil)
 
 	d.Restart()
 
-	out, err = d.Cmd("ps", "-q")
-	c.Assert(err, checker.IsNil)
-	c.Assert(strings.TrimSpace(out), checker.Not(checker.Equals), "")
+	c.Assert(waitRun("test", "-H", d.sock()), check.IsNil)
 }
