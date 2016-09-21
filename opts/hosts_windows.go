1361828e895c41effe7662159073879c219ec557
Justin Cormack <justin.cormack@docker.com>
2016-09-21 15:38:35 +0100

    Try connecting to ~/.docker/docker.sock if main socket missing
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- opts/hosts_windows.go
+++ opts/hosts_windows.go
@@ -4,3 +4,8 @@ package opts
 
 // DefaultHost constant defines the default host string used by docker on Windows
 var DefaultHost = "npipe://" + DefaultNamedPipe
+
+// DefaultOrAltHost function returns the name of either the default host if found or the alternate Unix socket
+func DefaultOrAltHost() string {
+	return DefaultHost
+}
