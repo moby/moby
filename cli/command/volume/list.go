78101b26afe4f7edbc4218d49238044e3feef54b
Daniel Nephin <dnephin@docker.com>
2016-09-22 14:11:08 -0400

    Read long description from a file.
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

--- cli/command/volume/list.go
+++ cli/command/volume/list.go
@@ -34,7 +34,6 @@ func newListCommand(dockerCli *command.DockerCli) *cobra.Command {
 		Use:     "ls [OPTIONS]",
 		Aliases: []string{"list"},
 		Short:   "List volumes",
-		Long:    listDescription,
 		Args:    cli.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runList(dockerCli, opts)
@@ -73,19 +72,3 @@ func runList(dockerCli *command.DockerCli, opts listOptions) error {
 	}
 	return formatter.VolumeWrite(volumeCtx, volumes.Volumes)
 }
-
-var listDescription = `
-
-Lists all the volumes Docker manages. You can filter using the **-f** or
-**--filter** flag. The filtering format is a **key=value** pair. To specify
-more than one filter,  pass multiple flags (for example,
-**--filter "foo=bar" --filter "bif=baz"**)
-
-The currently supported filters are:
-
-* **dangling** (boolean - **true** or **false**, **1** or **0**)
-* **driver** (a volume driver's name)
-* **label** (**label=<key>** or **label=<key>=<value>**)
-* **name** (a volume's name)
-
-`
