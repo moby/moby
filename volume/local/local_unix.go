2a5e85e2e871bf037c976471cf8a14da22ddd9a9
Brian Goff <cpuguy83@gmail.com>
2016-09-23 10:38:19 -0400

    Fix some places where low-level errors bubbled up
    Found a couple of places where pretty low level errors were never being
    wrapped with any sort of context.
    
    For example, if you try to create a local volume using some bad mount
    options, the kernel will return `invalid argument` when we try to mount
    it at container start.
    What would happen is a user would `docker run` with this volume and get
    an error like `Error response from daemon: invalid argument`.
    
    This uses github.com/pkg/errors to provide some context to the error
    message without masking the original error.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- volume/local/local_unix.go
+++ volume/local/local_unix.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"src/github.com/pkg/errors"
+
 	"github.com/docker/docker/pkg/mount"
 )
 
@@ -27,6 +29,10 @@ type optsConfig struct {
 	MountType   string
 	MountOpts   string
 	MountDevice string
+}
+
+func (o *optsConfig) String() string {
+	return fmt.Sprintf("type='%s' device='%s' o='%s'", o.MountType, o.MountDevice, o.MountOpts)
 }
 
 // scopedPath verifies that the path where the volume is located
@@ -65,5 +71,6 @@ func (v *localVolume) mount() error {
 	if v.opts.MountDevice == "" {
 		return fmt.Errorf("missing device in volume options")
 	}
-	return mount.Mount(v.opts.MountDevice, v.path, v.opts.MountType, v.opts.MountOpts)
+	err := mount.Mount(v.opts.MountDevice, v.path, v.opts.MountType, v.opts.MountOpts)
+	return errors.Wrapf(err, "error while mounting volume with options: %s", v.opts)
 }
