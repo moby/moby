57e12037ac8f8eb48cc05979c3030853d011dfea
Tonis Tiigi <tonistiigi@gmail.com>
2016-09-30 11:46:37 -0700

    Fix error reporting in `CopyFileWithTar`
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- pkg/archive/archive.go
+++ pkg/archive/archive.go
@@ -1050,7 +1050,7 @@ func (archiver *Archiver) CopyFileWithTar(src, dst string) (err error) {
 		return nil
 	})
 	defer func() {
-		if er := <-errC; err != nil {
+		if er := <-errC; err == nil && er != nil {
 			err = er
 		}
 	}()
