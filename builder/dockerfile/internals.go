anikwai/docker

Windows: Support credential specs
Signed-off-by: John Howard <jhoward@microsoft.com>

builder/dockerfile/internals.go
@@ -497,9 +497,10 @@ func (b *Builder) create() (string, error) {
 
 	// TODO: why not embed a hostconfig in builder?
 	hostConfig := &container.HostConfig{
-		Isolation: b.options.Isolation,
-		ShmSize:   b.options.ShmSize,
-		Resources: resources,
+		CredentialSpec: b.options.CredentialSpec,
+		Isolation:      b.options.Isolation,
+		ShmSize:        b.options.ShmSize,
+		Resources:      resources,
 	}
 
 	config := *b.runConfig

moby/moby

builder/dockerfile/internals.go
@@ -484,9 +484,10 @@ func (b *Builder) create() (string, error) {
 
 	// TODO: why not embed a hostconfig in builder?
 	hostConfig := &container.HostConfig{
-		Isolation: b.options.Isolation,
-		ShmSize:   b.options.ShmSize,
-		Resources: resources,
+		SecurityOpt: b.options.SecurityOpt,
+		Isolation:   b.options.Isolation,
+		ShmSize:     b.options.ShmSize,
+		Resources:   resources,
 	}
 
 	config := *b.runConfig