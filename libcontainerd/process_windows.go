ae35c0f70e96de011ad376c8fffba8e8a52ec21f
Darren Stahl <darst@microsoft.com>
2016-10-11 18:03:12 -0700

    Stop returning errors that should be ignored while closing stdin
    Signed-off-by: Darren Stahl <darst@microsoft.com>

--- libcontainerd/process_windows.go
+++ libcontainerd/process_windows.go
@@ -38,17 +38,14 @@ func createStdInCloser(pipe io.WriteCloser, process hcsshim.Process) io.WriteClo
 			return err
 		}
 
-		// We do not need to lock container ID here, even though
-		// we are calling into hcsshim. This is safe, because the
-		// only place that closes this process handle is this method.
 		err := process.CloseStdin()
-		if err != nil && !hcsshim.IsNotExist(err) {
+		if err != nil && !hcsshim.IsNotExist(err) && !hcsshim.IsAlreadyClosed(err) {
 			// This error will occur if the compute system is currently shutting down
 			if perr, ok := err.(*hcsshim.ProcessError); ok && perr.Err != hcsshim.ErrVmcomputeOperationInvalidState {
 				return err
 			}
 		}
 
-		return err
+		return nil
 	})
 }
