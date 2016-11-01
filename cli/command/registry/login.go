e41a39dbae007b04e12504e75ef69fb1ae6ab7c9
Sebastiaan van Stijn <github@gone.nl>
2016-11-01 09:12:27 -0700

    Update deprecation versions for "email" and colon in "security options"
    These features were originally scheduled
    for removal in docker 1.13, but we changed
    our deprecation policy to keep features
    for three releases instead of two.
    
    This updates the deprecation version
    to match the deprecation policy.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/registry/login.go
+++ cli/command/registry/login.go
@@ -39,9 +39,9 @@ func NewLoginCommand(dockerCli *command.DockerCli) *cobra.Command {
 	flags.StringVarP(&opts.user, "username", "u", "", "Username")
 	flags.StringVarP(&opts.password, "password", "p", "", "Password")
 
-	// Deprecated in 1.11: Should be removed in docker 1.13
+	// Deprecated in 1.11: Should be removed in docker 1.14
 	flags.StringVarP(&opts.email, "email", "e", "", "Email")
-	flags.MarkDeprecated("email", "will be removed in 1.13.")
+	flags.MarkDeprecated("email", "will be removed in 1.14.")
 
 	return cmd
 }
