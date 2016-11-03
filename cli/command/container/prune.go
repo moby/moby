eadb5b315be2ca51aa59ce55ef34478fbdceefa2
Victor Vieux <vieux@docker.com>
2016-11-03 10:33:17 -0700

    fix double [y/N] in container prune
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- cli/command/container/prune.go
+++ cli/command/container/prune.go
@@ -44,7 +44,7 @@ func NewPruneCommand(dockerCli *command.DockerCli) *cobra.Command {
 }
 
 const warning = `WARNING! This will remove all stopped containers.
-Are you sure you want to continue? [y/N] `
+Are you sure you want to continue?`
 
 func runPrune(dockerCli *command.DockerCli, opts pruneOptions) (spaceReclaimed uint64, output string, err error) {
 	if !opts.force && !command.PromptForConfirmation(dockerCli.In(), dockerCli.Out(), warning) {
