1b4fe9b5a5db8f867253170ccf736fd799a84cf5
Evan Hazlett <ejhazlett@gmail.com>
2016-11-03 22:43:52 -0400

    review updates
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- api/types/container/secret.go
+++ api/types/container/secret.go
@@ -2,6 +2,8 @@ package container
 
 import "os"
 
+// ContainerSecret represents a secret in a container.  This gets realized
+// in the container tmpfs
 type ContainerSecret struct {
 	Name   string
 	Target string
