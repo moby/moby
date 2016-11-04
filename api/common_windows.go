e4af39aeb311a08fd2d15e01eabd6bae9db36382
John Howard <jhoward@microsoft.com>
2016-11-04 09:54:58 -0700

    Windows: Allow API v1.24
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- api/common_windows.go
+++ api/common_windows.go
@@ -1,4 +1,8 @@
 package api
 
 // MinVersion represents Minimum REST API version supported
-const MinVersion string = "1.25"
+// Technically the first daemon API version released on Windows is v1.25 in
+// engine version 1.13. However, some clients are explicitly using downlevel
+// APIs (eg docker-compose v2.1 file format) and that is just too restrictive.
+// Hence also allowing 1.24 on Windows.
+const MinVersion string = "1.24"
