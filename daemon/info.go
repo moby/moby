70e7fcc595527358f91082e6b196fffc73dd7665
Kenfe-Mickael Laventure <mickael.laventure@gmail.com>
2016-11-11 08:02:23 -0800

    Remove extra binaries commit variables from windows build
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>

--- daemon/info.go
+++ daemon/info.go
@@ -1,11 +1,8 @@
 package daemon
 
 import (
-	"context"
 	"os"
-	"os/exec"
 	"runtime"
-	"strings"
 	"sync/atomic"
 	"time"
 
@@ -135,76 +132,8 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 		Isolation:          daemon.defaultIsolation,
 	}
 
-	// TODO Windows. Refactor this more once sysinfo is refactored into
-	// platform specific code. On Windows, sysinfo.cgroupMemInfo and
-	// sysinfo.cgroupCpuInfo will be nil otherwise and cause a SIGSEGV if
-	// an attempt is made to access through them.
-	if runtime.GOOS != "windows" {
-		v.MemoryLimit = sysInfo.MemoryLimit
-		v.SwapLimit = sysInfo.SwapLimit
-		v.KernelMemory = sysInfo.KernelMemory
-		v.OomKillDisable = sysInfo.OomKillDisable
-		v.CPUCfsPeriod = sysInfo.CPUCfsPeriod
-		v.CPUCfsQuota = sysInfo.CPUCfsQuota
-		v.CPUShares = sysInfo.CPUShares
-		v.CPUSet = sysInfo.Cpuset
-		v.Runtimes = daemon.configStore.GetAllRuntimes()
-		v.DefaultRuntime = daemon.configStore.GetDefaultRuntimeName()
-		v.InitBinary = daemon.configStore.GetInitPath()
-
-		v.ContainerdCommit.Expected = dockerversion.ContainerdCommitID
-		if sv, err := daemon.containerd.GetServerVersion(context.Background()); err == nil {
-			v.ContainerdCommit.ID = sv.Revision
-		} else {
-			logrus.Warnf("failed to retrieve containerd version: %v", err)
-			v.ContainerdCommit.ID = "N/A"
-		}
-
-		v.RuncCommit.Expected = dockerversion.RuncCommitID
-		if rv, err := exec.Command(DefaultRuntimeBinary, "--version").Output(); err == nil {
-			parts := strings.Split(strings.TrimSpace(string(rv)), "\n")
-			if len(parts) == 3 {
-				parts = strings.Split(parts[1], ": ")
-				if len(parts) == 2 {
-					v.RuncCommit.ID = strings.TrimSpace(parts[1])
-				}
-			}
-
-			if v.RuncCommit.ID == "" {
-				logrus.Warnf("failed to retrieve %s version: unknown output format: %s", DefaultRuntimeBinary, string(rv))
-				v.RuncCommit.ID = "N/A"
-			}
-		} else {
-			logrus.Warnf("failed to retrieve %s version: %v", DefaultRuntimeBinary, err)
-			v.RuncCommit.ID = "N/A"
-		}
-
-		v.InitCommit.Expected = dockerversion.InitCommitID
-		if rv, err := exec.Command(DefaultInitBinary, "--version").Output(); err == nil {
-			parts := strings.Split(strings.TrimSpace(string(rv)), " - ")
-			if len(parts) == 2 {
-				if dockerversion.InitCommitID[0] == 'v' {
-					vs := strings.TrimPrefix(parts[0], "tini version ")
-					v.InitCommit.ID = "v" + vs
-				} else {
-					// Get the sha1
-					gitParts := strings.Split(parts[1], ".")
-					if len(gitParts) == 2 && gitParts[0] == "git" {
-						v.InitCommit.ID = gitParts[1]
-						v.InitCommit.Expected = dockerversion.InitCommitID[0:len(gitParts[1])]
-					}
-				}
-			}
-
-			if v.InitCommit.ID == "" {
-				logrus.Warnf("failed to retrieve %s version: unknown output format: %s", DefaultInitBinary, string(rv))
-				v.InitCommit.ID = "N/A"
-			}
-		} else {
-			logrus.Warnf("failed to retrieve %s version", DefaultInitBinary)
-			v.InitCommit.ID = "N/A"
-		}
-	}
+	// Retrieve platform specific info
+	daemon.FillPlatformInfo(v, sysInfo)
 
 	hostname := ""
 	if hn, err := os.Hostname(); err != nil {
