7bf5f52dd46752c847c405feaf0c5dda8e491c91
Evan Hazlett <ejhazlett@gmail.com>
2016-11-22 11:18:28 -0500

    update secret inspect to support IDs
    This updates secret inspect to support inspect by ID in addition to name
    as well as inspecting multiple secrets.  This also cleans up the
    help text for consistency.
    
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- cli/command/secret/utils.go
+++ cli/command/secret/utils.go
@@ -18,3 +18,30 @@ func getSecretsByName(ctx context.Context, client client.APIClient, names []stri
 		Filters: args,
 	})
 }
+
+func getCliRequestedSecretIDs(ctx context.Context, client client.APIClient, names []string) ([]string, error) {
+	ids := names
+
+	// attempt to lookup secret by name
+	secrets, err := getSecretsByName(ctx, client, ids)
+	if err != nil {
+		return nil, err
+	}
+
+	lookup := make(map[string]struct{})
+	for _, id := range ids {
+		lookup[id] = struct{}{}
+	}
+
+	if len(secrets) > 0 {
+		ids = []string{}
+
+		for _, s := range secrets {
+			if _, ok := lookup[s.Spec.Annotations.Name]; ok {
+				ids = append(ids, s.ID)
+			}
+		}
+	}
+
+	return ids, nil
+}
