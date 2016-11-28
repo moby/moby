773ae1f42e87174f8242e1e670d5119bb6829c6b
yuexiao-wang <wang.yuexiao@zte.com.cn>
2016-11-29 01:21:47 +0800

    Fix some typos
    Signed-off-by: yuexiao-wang <wang.yuexiao@zte.com.cn>

--- cli/command/registry/search.go
+++ cli/command/registry/search.go
@@ -118,7 +118,7 @@ func runSearch(dockerCli *command.DockerCli, opts searchOptions) error {
 	return nil
 }
 
-// SearchResultsByStars sorts search results in descending order by number of stars.
+// searchResultsByStars sorts search results in descending order by number of stars.
 type searchResultsByStars []registrytypes.SearchResult
 
 func (r searchResultsByStars) Len() int           { return len(r) }
--- layer/ro_layer.go
+++ layer/ro_layer.go
@@ -21,7 +21,7 @@ type roLayer struct {
 	references     map[Layer]struct{}
 }
 
-// TarStream for roLayer guarentees that the data that is produced is the exact
+// TarStream for roLayer guarantees that the data that is produced is the exact
 // data that the layer was registered with.
 func (rl *roLayer) TarStream() (io.ReadCloser, error) {
 	r, err := rl.layerStore.store.TarSplitReader(rl.chainID)
@@ -45,7 +45,7 @@ func (rl *roLayer) TarStream() (io.ReadCloser, error) {
 	return rc, nil
 }
 
-// TarStreamFrom does not make any guarentees to the correctness of the produced
+// TarStreamFrom does not make any guarantees to the correctness of the produced
 // data. As such it should not be used when the layer content must be verified
 // to be an exact match to the registered layer.
 func (rl *roLayer) TarStreamFrom(parent ChainID) (io.ReadCloser, error) {
