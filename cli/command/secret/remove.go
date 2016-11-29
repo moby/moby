36b8a98ab1b4c895984094b058b40b3d44ed3752
allencloud <allen.sun@daocloud.io>
2016-11-29 14:28:46 +0800

    change secret remove logic in cli
    Signed-off-by: allencloud <allen.sun@daocloud.io>

--- cli/command/secret/remove.go
+++ cli/command/secret/remove.go
@@ -2,6 +2,7 @@ package secret
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/docker/docker/cli"
 	"github.com/docker/docker/cli/command"
@@ -36,12 +37,19 @@ func runSecretRemove(dockerCli *command.DockerCli, opts removeOptions) error {
 		return err
 	}
 
+	var errs []string
+
 	for _, id := range ids {
 		if err := client.SecretRemove(ctx, id); err != nil {
-			fmt.Fprintf(dockerCli.Out(), "WARN: %s\n", err)
+			errs = append(errs, err.Error())
+			continue
 		}
 
 		fmt.Fprintln(dockerCli.Out(), id)
+	}
+
+	if len(errs) > 0 {
+		return fmt.Errorf("%s", strings.Join(errs, "\n"))
 	}
 
 	return nil
