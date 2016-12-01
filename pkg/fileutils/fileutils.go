5fac6a1675d6a53dc12af9a2b9375b373bd25edb
Doug Davis <dug@us.ibm.com>
2016-12-01 15:03:57 -0800

    Fix use of **/ in .dockerignore
    .dockerignore pattern of **/.foo incorrectly matched **/bar.foo
    because **/.foo was getting converted into a .*\.foo regex
    instead of (.*/)*\.foo
    
    Closes #29014
    
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- pkg/fileutils/fileutils.go
+++ pkg/fileutils/fileutils.go
@@ -161,17 +161,19 @@ func regexpMatch(pattern, path string) (bool, error) {
 				// is some flavor of "**"
 				scan.Next()
 
+				// Treat **/ as ** so eat the "/"
+				if string(scan.Peek()) == sl {
+					scan.Next()
+				}
+
 				if scan.Peek() == scanner.EOF {
 					// is "**EOF" - to align with .gitignore just accept all
 					regStr += ".*"
 				} else {
 					// is "**"
-					regStr += "((.*" + escSL + ")|([^" + escSL + "]*))"
-				}
-
-				// Treat **/ as ** so eat the "/"
-				if string(scan.Peek()) == sl {
-					scan.Next()
+					// Note that this allows for any # of /'s (even 0) because
+					// the .* will eat everything, even /'s
+					regStr += "(.*" + escSL + ")?"
 				}
 			} else {
 				// is "*" so map it to anything but "/"
