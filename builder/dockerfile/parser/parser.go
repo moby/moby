877c2b5ac0cc402ad415a209f7b143d241e63421
Vincent Demeester <vincent@sbr.pm>
2016-12-02 14:15:55 +0100

    Revert "Fix dockerfile parser with empty line after escape"
    This reverts commit 3e1b539e8d0ed4abf695b0a8c42346fba6d5a6b0.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>
    (cherry picked from commit 6e0666e622a8b1fc19c19eb490d218eebca15edc)
    Signed-off-by: Victor Vieux <victorvieux@gmail.com>

--- builder/dockerfile/parser/parser.go
+++ builder/dockerfile/parser/parser.go
@@ -176,17 +176,10 @@ func Parse(rwc io.Reader, d *Directive) (*Node, error) {
 				newline := scanner.Text()
 				currentLine++
 
-				// If escape followed by a comment line then stop
-				// Note here that comment line starts with `#` at
-				// the first pos of the line
-				if stripComments(newline) == "" {
-					break
+				if stripComments(strings.TrimSpace(newline)) == "" {
+					continue
 				}
 
-				// If escape followed by an empty line then stop
-				if strings.TrimSpace(newline) == "" {
-					break
-				}
 				line, child, err = ParseLine(line+newline, d, false)
 				if err != nil {
 					return nil, err
