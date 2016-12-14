cb6be9b1ac687706f029aad9b9ed6a5dcec99d0d
Ma Shimiao <mashimiao.fnst@cn.fujitsu.com>
2016-12-14 17:34:29 +0800

    oci/namespace: remove unnecessary variable idx
    Signed-off-by: Ma Shimiao <mashimiao.fnst@cn.fujitsu.com>

--- oci/namespaces.go
+++ oci/namespaces.go
@@ -4,13 +4,10 @@ import specs "github.com/opencontainers/runtime-spec/specs-go"
 
 // RemoveNamespace removes the `nsType` namespace from OCI spec `s`
 func RemoveNamespace(s *specs.Spec, nsType specs.NamespaceType) {
-	idx := -1
 	for i, n := range s.Linux.Namespaces {
 		if n.Type == nsType {
-			idx = i
+			s.Linux.Namespaces = append(s.Linux.Namespaces[:i], s.Linux.Namespaces[i+1:]...)
+			return
 		}
-	}
-	if idx >= 0 {
-		s.Linux.Namespaces = append(s.Linux.Namespaces[:idx], s.Linux.Namespaces[idx+1:]...)
 	}
 }
