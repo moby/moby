37ea8104b06ef2f4680b6395797ab6828059ee6d
Vincent Demeester <vincent@sbr.pm>
2016-12-15 07:24:26 +0100

    Remove --port and update --publish for services to support syntaxes
    Add support for simple and complex syntax to `--publish` through the
    use of `PortOpt`.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- opts/port.go
+++ opts/port.go
@@ -3,10 +3,12 @@ package opts
 import (
 	"encoding/csv"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/go-connections/nat"
 )
 
 const (
@@ -23,59 +25,75 @@ type PortOpt struct {
 
 // Set a new port value
 func (p *PortOpt) Set(value string) error {
-	csvReader := csv.NewReader(strings.NewReader(value))
-	fields, err := csvReader.Read()
+	longSyntax, err := regexp.MatchString(`\w+=\w+(,\w+=\w+)*`, value)
 	if err != nil {
 		return err
 	}
-
-	pConfig := swarm.PortConfig{}
-	for _, field := range fields {
-		parts := strings.SplitN(field, "=", 2)
-		if len(parts) != 2 {
-			return fmt.Errorf("invalid field %s", field)
+	if longSyntax {
+		csvReader := csv.NewReader(strings.NewReader(value))
+		fields, err := csvReader.Read()
+		if err != nil {
+			return err
 		}
 
-		key := strings.ToLower(parts[0])
-		value := strings.ToLower(parts[1])
-
-		switch key {
-		case portOptProtocol:
-			if value != string(swarm.PortConfigProtocolTCP) && value != string(swarm.PortConfigProtocolUDP) {
-				return fmt.Errorf("invalid protocol value %s", value)
+		pConfig := swarm.PortConfig{}
+		for _, field := range fields {
+			parts := strings.SplitN(field, "=", 2)
+			if len(parts) != 2 {
+				return fmt.Errorf("invalid field %s", field)
 			}
 
-			pConfig.Protocol = swarm.PortConfigProtocol(value)
-		case portOptMode:
-			if value != string(swarm.PortConfigPublishModeIngress) && value != string(swarm.PortConfigPublishModeHost) {
-				return fmt.Errorf("invalid publish mode value %s", value)
-			}
+			key := strings.ToLower(parts[0])
+			value := strings.ToLower(parts[1])
 
-			pConfig.PublishMode = swarm.PortConfigPublishMode(value)
-		case portOptTargetPort:
-			tPort, err := strconv.ParseUint(value, 10, 16)
-			if err != nil {
-				return err
-			}
+			switch key {
+			case portOptProtocol:
+				if value != string(swarm.PortConfigProtocolTCP) && value != string(swarm.PortConfigProtocolUDP) {
+					return fmt.Errorf("invalid protocol value %s", value)
+				}
 
-			pConfig.TargetPort = uint32(tPort)
-		case portOptPublishedPort:
-			pPort, err := strconv.ParseUint(value, 10, 16)
-			if err != nil {
-				return err
-			}
+				pConfig.Protocol = swarm.PortConfigProtocol(value)
+			case portOptMode:
+				if value != string(swarm.PortConfigPublishModeIngress) && value != string(swarm.PortConfigPublishModeHost) {
+					return fmt.Errorf("invalid publish mode value %s", value)
+				}
 
-			pConfig.PublishedPort = uint32(pPort)
-		default:
-			return fmt.Errorf("invalid field key %s", key)
+				pConfig.PublishMode = swarm.PortConfigPublishMode(value)
+			case portOptTargetPort:
+				tPort, err := strconv.ParseUint(value, 10, 16)
+				if err != nil {
+					return err
+				}
+
+				pConfig.TargetPort = uint32(tPort)
+			case portOptPublishedPort:
+				pPort, err := strconv.ParseUint(value, 10, 16)
+				if err != nil {
+					return err
+				}
+
+				pConfig.PublishedPort = uint32(pPort)
+			default:
+				return fmt.Errorf("invalid field key %s", key)
+			}
 		}
-	}
 
-	if pConfig.TargetPort == 0 {
-		return fmt.Errorf("missing mandatory field %q", portOptTargetPort)
-	}
+		if pConfig.TargetPort == 0 {
+			return fmt.Errorf("missing mandatory field %q", portOptTargetPort)
+		}
 
-	p.ports = append(p.ports, pConfig)
+		p.ports = append(p.ports, pConfig)
+	} else {
+		// short syntax
+		portConfigs := []swarm.PortConfig{}
+		// We can ignore errors because the format was already validated by ValidatePort
+		ports, portBindings, _ := nat.ParsePortSpecs([]string{value})
+
+		for port := range ports {
+			portConfigs = append(portConfigs, ConvertPortToPortConfig(port, portBindings)...)
+		}
+		p.ports = append(p.ports, portConfigs...)
+	}
 	return nil
 }
 
@@ -97,4 +115,23 @@ func (p *PortOpt) String() string {
 // Value returns the ports
 func (p *PortOpt) Value() []swarm.PortConfig {
 	return p.ports
+}
+
+// ConvertPortToPortConfig converts ports to the swarm type
+func ConvertPortToPortConfig(
+	port nat.Port,
+	portBindings map[nat.Port][]nat.PortBinding,
+) []swarm.PortConfig {
+	ports := []swarm.PortConfig{}
+
+	for _, binding := range portBindings[port] {
+		hostPort, _ := strconv.ParseUint(binding.HostPort, 10, 16)
+		ports = append(ports, swarm.PortConfig{
+			//TODO Name: ?
+			Protocol:      swarm.PortConfigProtocol(strings.ToLower(port.Proto())),
+			TargetPort:    uint32(port.Int()),
+			PublishedPort: uint32(hostPort),
+		})
+	}
+	return ports
 }
