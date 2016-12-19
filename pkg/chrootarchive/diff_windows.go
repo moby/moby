9501f2ea978bf13d2f050f31223b15fad5b26622
lixiaobing10051267 <li.xiaobing1@zte.com.cn>
2016-12-19 15:41:29 +0800

    Use wrong ApplyLayer in diff_windows.go
    Signed-off-by: lixiaobing10051267 <li.xiaobing1@zte.com.cn>

--- pkg/chrootarchive/diff_windows.go
+++ pkg/chrootarchive/diff_windows.go
@@ -38,7 +38,7 @@ func applyLayerHandler(dest string, layer io.Reader, options *archive.TarOptions
 	s, err := archive.UnpackLayer(dest, layer, nil)
 	os.RemoveAll(tmpDir)
 	if err != nil {
-		return 0, fmt.Errorf("ApplyLayer %s failed UnpackLayer to %s", err, dest)
+		return 0, fmt.Errorf("ApplyLayer %s failed UnpackLayer to %s: %s", layer, dest, err)
 	}
 
 	return s, nil
