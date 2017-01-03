2aed1e1f8d64d86d5a640c84b5f33ac55573f75a
Sebastiaan van Stijn <github@gone.nl>
2017-01-03 20:48:01 +0100

    Print security warning on stdout instead of stderr
    When building a Dockerfile from a Windows client
    on a Linux daemon, a "security warning" is printed
    on stderr.
    
    Having this warning printed on stderr makes it
    difficult to distinguish a failed build from
    one that's succeeding, and the only way to
    suppress the warning is through the `-q` option,
    which also suppresses every output.
    
    This change prints the warning on stdout,
    instead of stderr, to resolve this situation.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cli/command/image/build.go
+++ cli/command/image/build.go
@@ -334,7 +334,7 @@ func runBuild(dockerCli *command.DockerCli, options buildOptions) error {
 	// Windows: show error message about modified file permissions if the
 	// daemon isn't running Windows.
 	if response.OSType != "windows" && runtime.GOOS == "windows" && !options.quiet {
-		fmt.Fprintln(dockerCli.Err(), `SECURITY WARNING: You are building a Docker image from Windows against a non-Windows Docker host. All files and directories added to build context will have '-rwxr-xr-x' permissions. It is recommended to double check and reset permissions for sensitive files and directories.`)
+		fmt.Fprintln(dockerCli.Out(), `SECURITY WARNING: You are building a Docker image from Windows against a non-Windows Docker host. All files and directories added to build context will have '-rwxr-xr-x' permissions. It is recommended to double check and reset permissions for sensitive files and directories.`)
 	}
 
 	// Everything worked so if -q was provided the output from the daemon
