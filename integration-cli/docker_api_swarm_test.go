d377b074fdcbf735d9fbafdb9bcab6878e5c0ae9
Tonis Tiigi <tonistiigi@gmail.com>
2017-01-05 15:46:07 -0800

    Add test for swarm error handling
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- integration-cli/docker_api_swarm_test.go
+++ integration-cli/docker_api_swarm_test.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -1316,4 +1317,15 @@ func (s *DockerSwarmSuite) TestAPISwarmUnlockNotLocked(c *check.C) {
 	err := d.Unlock(swarm.UnlockRequest{UnlockKey: "wrong-key"})
 	c.Assert(err, checker.NotNil)
 	c.Assert(err.Error(), checker.Contains, "swarm is not locked")
+}
+
+// #29885
+func (s *DockerSwarmSuite) TestAPISwarmErrorHandling(c *check.C) {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", defaultSwarmPort))
+	c.Assert(err, checker.IsNil)
+	defer ln.Close()
+	d := s.AddDaemon(c, false, false)
+	err = d.Init(swarm.InitRequest{})
+	c.Assert(err, checker.NotNil)
+	c.Assert(err.Error(), checker.Contains, "address already in use")
 }
