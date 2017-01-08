20dcc38edb6484c4ebe18408e5e1a7ba335288b0
Daniel Zhang <jmzwcn@gmail.com>
2017-01-08 16:07:14 +0800

    [feature]: add daemon flag to set no_new_priv as default for unprivileged containers #29951
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- daemon/daemon_unix.go
+++ daemon/daemon_unix.go
@@ -159,6 +159,21 @@ func getBlkioWeightDevices(config containertypes.Resources) ([]specs.WeightDevic
 	return blkioWeightDevices, nil
 }
 
+func parseAndSetSecurityOpt(daemon *Daemon, container *container.Container, config *containertypes.HostConfig) error {
+	var findNNP bool
+	err := parseSecurityOpt(container, config)
+	for _, opt := range config.SecurityOpt {
+		if strings.Contains(opt, "no-new-privileges") {
+			findNNP = true
+			break
+		}
+	}
+	if !findNNP {
+		container.NoNewPrivileges = daemon.configStore.NoNewPrivileges
+	}
+	return err
+}
+
 func parseSecurityOpt(container *container.Container, config *containertypes.HostConfig) error {
 	var (
 		labelOpts []string
@@ -166,11 +181,6 @@ func parseSecurityOpt(container *container.Container, config *containertypes.Hos
 	)
 
 	for _, opt := range config.SecurityOpt {
-		if opt == "no-new-privileges" {
-			container.NoNewPrivileges = true
-			continue
-		}
-
 		var con []string
 		if strings.Contains(opt, "=") {
 			con = strings.SplitN(opt, "=", 2)
@@ -190,6 +200,12 @@ func parseSecurityOpt(container *container.Container, config *containertypes.Hos
 			container.AppArmorProfile = con[1]
 		case "seccomp":
 			container.SeccompProfile = con[1]
+		case "no-new-privileges":
+			noNewPrivileges, err := strconv.ParseBool(con[1])
+			if err != nil {
+				return fmt.Errorf("invalid --security-opt 2: %q", opt)
+			}
+			container.NoNewPrivileges = noNewPrivileges
 		default:
 			return fmt.Errorf("invalid --security-opt 2: %q", opt)
 		}
