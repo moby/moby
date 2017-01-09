f27a297595f2b38cba4b28d60bb8dd8702bfa841
Daniel Nephin <dnephin@docker.com>
2017-01-09 14:15:32 -0500

    Fix parsing resources from compose file for stack deploy.
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

--- cli/command/stack/deploy_test.go
+++ cli/command/stack/deploy_test.go
@@ -5,6 +5,7 @@ import (
 
 	composetypes "github.com/aanand/compose-file/types"
 	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/pkg/testutil/assert"
 )
 
@@ -27,4 +28,27 @@ func TestConvertVolumeToMountInvalidFormat(t *testing.T) {
 		_, err := convertVolumeToMount(vol, map[string]composetypes.VolumeConfig{}, namespace)
 		assert.Error(t, err, "invalid volume: "+vol)
 	}
+}
+
+func TestConvertResourcesOnlyMemory(t *testing.T) {
+	source := composetypes.Resources{
+		Limits: &composetypes.Resource{
+			MemoryBytes: composetypes.UnitBytes(300000000),
+		},
+		Reservations: &composetypes.Resource{
+			MemoryBytes: composetypes.UnitBytes(200000000),
+		},
+	}
+	resources, err := convertResources(source)
+	assert.NilError(t, err)
+
+	expected := &swarm.ResourceRequirements{
+		Limits: &swarm.Resources{
+			MemoryBytes: 300000000,
+		},
+		Reservations: &swarm.Resources{
+			MemoryBytes: 200000000,
+		},
+	}
+	assert.DeepEqual(t, resources, expected)
 }
