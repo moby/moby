e9779fd7df2d84d90477a0e714e77a24b5f7acfa
Anusha Ragunathan <anusha.ragunathan@docker.com>
2017-01-13 14:03:51 -0800

    Fix pluginv1 Windows volumes
    c54b717 caused a regression for pluginv1 on Windows, where extraneous
    backslashes were added to BasePath of the plugin. For pluginv1 on windows,
    BasePath() should return an empty string, since the plugin is fully aware
    of the mount path. Also, unlike Linux where all paths are relative to "/",
    Windows paths are dependent on system drives and mapped drives.
    
    Fixes #30148
    
    Signed-off-by: Anusha Ragunathan <anusha.ragunathan@docker.com>
    (cherry picked from commit 0ef21eb0e30d2ea036730a7c5502f888c6b763d1)
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -78,12 +78,6 @@ type Plugin struct {
 	handlersRun bool
 }
 
-// BasePath returns the path to which all paths returned by the plugin are relative to.
-// For v1 plugins, this always returns the host's root directory.
-func (p *Plugin) BasePath() string {
-	return "/"
-}
-
 // Name returns the name of the plugin.
 func (p *Plugin) Name() string {
 	return p.name
