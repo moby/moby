HammerOfThor/docker

Fix windows test cases
With new `/containers/{name:*}/stats` handler, one windows test case
hangs, this commit fixes the hanging issue.

Signed-off-by: Zhang Wei <zhangwei555@huawei.com>

integration-cli/docker_api_containers_test.go
@@ -215,7 +215,7 @@ func (s *DockerSuite) TestGetContainerStatsRmRunning(c *check.C) {
 	out, _ := runSleepingContainer(c)
 	id := strings.TrimSpace(out)
 
-	buf := &integration.ChannelBuffer{make(chan []byte, 1)}
+	buf := &integration.ChannelBuffer{make(chan []byte, 128)}
 	defer buf.Close()
 
 	_, body, err := sockRequestRaw("GET", "/containers/"+id+"/stats?stream=1", nil, "application/json")

moby/moby

integration-cli/docker_api_containers_test.go
@@ -215,7 +215,7 @@ func (s *DockerSuite) TestGetContainerStatsRmRunning(c *check.C) {
 	out, _ := runSleepingContainer(c)
 	id := strings.TrimSpace(out)
 
-	buf := &testutil.ChannelBuffer{make(chan []byte, 1)}
+	buf := &testutil.ChannelBuffer{make(chan []byte, 128)}
 	defer buf.Close()
 
 	_, body, err := request.SockRequestRaw("GET", "/containers/"+id+"/stats?stream=1", nil, "application/json", daemonHost())