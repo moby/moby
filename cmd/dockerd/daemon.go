1405026607a1a521fd7d69a7db80e5abecf4aa25
Tao Wang <twang2218@gmail.com>
2017-01-20 11:45:22 +1100

    Output the configuration in the log
    As docker engine can use both command arguments and daemon.json for
    the configuration, it would be easier to see the effective config if
    it's in the log.
    
    This PR will output the configuration in JSON format, at the engine
    starting and the reloading of the configuration.
    
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- cmd/dockerd/daemon.go
+++ cmd/dockerd/daemon.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -260,6 +261,12 @@ func (cli *DaemonCli) start(opts daemonOptions) (err error) {
 		<-stopc // wait for daemonCli.start() to return
 	})
 
+	if b, err := json.Marshal(cli.Config); err != nil {
+		logrus.Errorf("Cannot format configuration in JSON: %v", err)
+	} else {
+		logrus.Infof("Loaded configuration: %s", string(b))
+	}
+
 	d, err := daemon.NewDaemon(cli.Config, registryService, containerdRemote)
 	if err != nil {
 		return fmt.Errorf("Error starting daemon: %v", err)
@@ -347,6 +354,12 @@ func (cli *DaemonCli) reloadConfig() {
 		if err := cli.d.Reload(config); err != nil {
 			logrus.Errorf("Error reconfiguring the daemon: %v", err)
 			return
+		}
+
+		if b, err := json.Marshal(cli.Config); err != nil {
+			logrus.Errorf("Cannot format configuration in JSON: %v", err)
+		} else {
+			logrus.Infof("Reloaded configuration: %s", string(b))
 		}
 
 		if config.IsValueSet("debug") {
