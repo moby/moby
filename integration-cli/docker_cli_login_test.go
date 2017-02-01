c0dcd51f3ac18660a0a26a80d69b73eafe525acf
Daniel Panteleit <panteleit@cs.uni-bonn.de>
2017-02-01 15:43:15 +0100

    Add flag for reading password from file for "docker login"
    Signed-off-by: Daniel Panteleit <panteleit@cs.uni-bonn.de>

--- integration-cli/docker_cli_login_test.go
+++ integration-cli/docker_cli_login_test.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/docker/docker/integration-cli/checker"
 	"github.com/go-check/check"
+	"io/ioutil"
+	"os"
 )
 
 func (s *DockerSuite) TestLoginWithoutTTY(c *check.C) {
@@ -27,4 +29,19 @@ func (s *DockerRegistryAuthHtpasswdSuite) TestLoginToPrivateRegistry(c *check.C)
 
 	// now it's fine
 	dockerCmd(c, "login", "-u", s.reg.Username(), "-p", s.reg.Password(), privateRegistryURL)
+}
+
+func (s *DockerRegistryAuthHtpasswdSuite) TestLoginToPrivateRegistryWithPasswordFile(c *check.C) {
+	content := []byte(s.reg.Password())
+	tmpfile, err := ioutil.TempFile("", "dockerpass")
+	c.Assert(err, checker.IsNil)
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	_, err = tmpfile.Write(content)
+	c.Assert(err, checker.IsNil)
+
+	err = tmpfile.Close()
+	c.Assert(err, checker.IsNil)
+
+	dockerCmd(c, "login", "-u", s.reg.Username(), "--password-file", tmpfile.Name(), privateRegistryURL)
 }
