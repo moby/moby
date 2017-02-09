9d30525a04e2d4efe51ac9e6c114ce36276aafa8
allencloud <allen.sun@daocloud.io>
2017-02-09 15:06:44 +0800

    make secret update support name and id prefix
    Signed-off-by: allencloud <allen.sun@daocloud.io>

--- integration-cli/docker_api_swarm_secret_test.go
+++ integration-cli/docker_api_swarm_secret_test.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/docker/docker/api/types/swarm"
@@ -55,4 +56,64 @@ func (s *DockerSwarmSuite) TestAPISwarmSecretsDelete(c *check.C) {
 	status, out, err := d.SockRequest("GET", "/secrets/"+id, nil)
 	c.Assert(err, checker.IsNil)
 	c.Assert(status, checker.Equals, http.StatusNotFound, check.Commentf("secret delete: %s", string(out)))
+}
+
+func (s *DockerSwarmSuite) TestAPISwarmSecretsUpdate(c *check.C) {
+	d := s.AddDaemon(c, true, true)
+
+	testName := "test_secret"
+	id := d.CreateSecret(c, swarm.SecretSpec{
+		swarm.Annotations{
+			Name: testName,
+			Labels: map[string]string{
+				"test": "test1",
+			},
+		},
+		[]byte("TESTINGDATA"),
+	})
+	c.Assert(id, checker.Not(checker.Equals), "", check.Commentf("secrets: %s", id))
+
+	secret := d.GetSecret(c, id)
+	c.Assert(secret.ID, checker.Equals, id, check.Commentf("secret: %v", secret))
+
+	// test UpdateSecret with full ID
+	d.UpdateSecret(c, id, func(s *swarm.Secret) {
+		s.Spec.Labels = map[string]string{
+			"test": "test1",
+		}
+	})
+
+	secret = d.GetSecret(c, id)
+	c.Assert(secret.Spec.Labels["test"], checker.Equals, "test1", check.Commentf("secret: %v", secret))
+
+	// test UpdateSecret with full name
+	d.UpdateSecret(c, secret.Spec.Name, func(s *swarm.Secret) {
+		s.Spec.Labels = map[string]string{
+			"test": "test2",
+		}
+	})
+
+	secret = d.GetSecret(c, id)
+	c.Assert(secret.Spec.Labels["test"], checker.Equals, "test2", check.Commentf("secret: %v", secret))
+
+	// test UpdateSecret with prefix ID
+	d.UpdateSecret(c, id[:1], func(s *swarm.Secret) {
+		s.Spec.Labels = map[string]string{
+			"test": "test3",
+		}
+	})
+
+	secret = d.GetSecret(c, id)
+	c.Assert(secret.Spec.Labels["test"], checker.Equals, "test3", check.Commentf("secret: %v", secret))
+
+	// test UpdateSecret in updating Data which is not supported in daemon
+	// this test will produce an error in func UpdateSecret
+	secret = d.GetSecret(c, id)
+	secret.Spec.Data = []byte("TESTINGDATA2")
+
+	url := fmt.Sprintf("/secrets/%s/update?version=%d", secret.ID, secret.Version.Index)
+	status, out, err := d.SockRequest("POST", url, secret.Spec)
+
+	c.Assert(err, checker.IsNil, check.Commentf(string(out)))
+	c.Assert(status, checker.Equals, http.StatusInternalServerError, check.Commentf("output: %q", string(out)))
 }
