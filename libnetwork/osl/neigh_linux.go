200410dd184f0ba656f2c40167587828d1967e20
Santhosh Manohar <santhosh@docker.com>
2017-03-02 00:04:09 -0800

    Lock linux namespace structure when adding neighbors
    Signed-off-by: Santhosh Manohar <santhosh@docker.com>

--- libnetwork/osl/neigh_linux.go
+++ libnetwork/osl/neigh_linux.go
@@ -147,7 +147,9 @@ func (n *networkNamespace) AddNeighbor(dstIP net.IP, dstMac net.HardwareAddr, op
 		return fmt.Errorf("could not add neighbor entry: %v", err)
 	}
 
+	n.Lock()
 	n.neighbors = append(n.neighbors, nh)
+	n.Unlock()
 
 	return nil
 }
