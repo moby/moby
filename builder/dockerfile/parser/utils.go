2b79dbc89bf15c276145a0fa3d3728308349f86e
fate-grand-order <chenjg@harmonycloud.cn>
2017-03-20 19:52:00 +0800

    fix misspell in utils.go
    Signed-off-by: Helen Xie <chenjg@harmonycloud.cn>

--- builder/dockerfile/parser/utils.go
+++ builder/dockerfile/parser/utils.go
@@ -116,7 +116,7 @@ func extractBuilderFlags(line string) (string, []string, error) {
 				return line[pos:], words, nil
 			}
 
-			phase = inWord // found someting with "--", fall through
+			phase = inWord // found something with "--", fall through
 		}
 		if (phase == inWord || phase == inQuote) && (pos == len(line)) {
 			if word != "--" && (blankOK || len(word) > 0) {
