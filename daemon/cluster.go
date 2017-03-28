8f4f85dd5b5b6b7268428b8f95f56c1ba8608ac3
Sebastiaan van Stijn <github@gone.nl>
2017-03-28 12:46:26 +0200

    Add NetworkManager and ClusterStatus interfaces
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/cluster.go
+++ daemon/cluster.go
@@ -6,6 +6,18 @@ import (
 
 // Cluster is the interface for github.com/docker/docker/daemon/cluster.(*Cluster).
 type Cluster interface {
+	ClusterStatus
+	NetworkManager
+}
+
+// ClusterStatus interface provides information about the Swarm status of the Cluster
+type ClusterStatus interface {
+	IsAgent() bool
+	IsManager() bool
+}
+
+// NetworkManager provides methods to manage networks
+type NetworkManager interface {
 	GetNetwork(input string) (apitypes.NetworkResource, error)
 	GetNetworks() ([]apitypes.NetworkResource, error)
 	RemoveNetwork(input string) error
