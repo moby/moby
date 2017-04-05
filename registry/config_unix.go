68a8e70ea74d1d261b5a7ee219b805f168eea228
Thomas Voß <thomas.voss.bochum@gmail.com>
2017-04-05 11:39:55 +0200

    docker/registry: Make CertsDir configurable.
    On linux hosts, it would be helpful if certificates for
    accessing a remote registry could be configured at runtime
    for specific setups. One example are snaps and third-parties
    who want to work with their own registries.
    
    Signed-off-by: Thomas Voß <thomas.voss.bochum@gmail.com>

--- registry/config_unix.go
+++ registry/config_unix.go
@@ -3,12 +3,22 @@
 package registry
 
 import (
+	"os"
+
 	"github.com/spf13/pflag"
 )
 
+func initCertsDir() string {
+	dir := os.Getenv("DOCKER_REGISTRY_CERTS_DIR")
+	if len(dir) == 0 {
+		dir = "/etc/docker/certs.d"
+	}
+	return dir
+}
+
 var (
 	// CertsDir is the directory where certificates are stored
-	CertsDir = "/etc/docker/certs.d"
+	CertsDir = initCertsDir()
 )
 
 // cleanPath is used to ensure that a directory name is valid on the target
