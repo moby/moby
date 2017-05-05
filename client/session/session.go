0d2f25b8fb4edce326265e8dacf9d06188e339ef
Simon Ferquel <simon.ferquel@docker.com>
2017-05-05 15:15:40 +0200

    Added support for low level streaming methods
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

--- client/session/session.go
+++ client/session/session.go
@@ -8,6 +8,8 @@ import (
 
 	"google.golang.org/grpc"
 
+	"strings"
+
 	"github.com/docker/docker/pkg/stringid"
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
@@ -23,16 +25,28 @@ type Caller interface {
 	Name() string
 	SharedKey() string
 	Close() error
+	ListStreamInstances(serviceName string) []string
+	SupportsStream(serviceName, instanceId, method string) bool
+	ConnectToStream(ctx context.Context, serviceName, instanceId, method string, handler MessageStreamHandler) error
 }
+
+type MessageStream interface {
+	Context() context.Context
+	SendMsg(m interface{}) error
+	RecvMsg(m interface{}) error
+}
+
+type MessageStreamHandler func(MessageStream) error
 
 // Session is a long running connection between client and a daemon
 type Session struct {
-	uuid      string
-	name      string
-	sharedKey string
-	ctx       context.Context
-	cancelCtx func()
-	done      chan struct{}
+	uuid           string
+	name           string
+	sharedKey      string
+	ctx            context.Context
+	cancelCtx      func()
+	done           chan struct{}
+	streamServices map[string]*streamServiceDesc
 }
 type ClientSession struct {
 	Session
@@ -43,15 +57,22 @@ type ServerSession struct {
 	Session
 	grpcServer *grpc.Server
 }
+type streamServiceDesc struct {
+	instances map[string]*streamInstanceDesc
+}
+type streamInstanceDesc struct {
+	methods map[string]MessageStreamHandler
+}
 
 // NewSession returns a new long running session
 func NewServerSession(name, sharedKey string) (*ServerSession, error) {
 	uuid := stringid.GenerateRandomID()
 	s := &ServerSession{
 		Session: Session{
-			uuid:      uuid,
-			name:      name,
-			sharedKey: sharedKey,
+			uuid:           uuid,
+			name:           name,
+			sharedKey:      sharedKey,
+			streamServices: make(map[string]*streamServiceDesc),
 		},
 		grpcServer: grpc.NewServer(),
 	}
@@ -63,13 +84,49 @@ func (s *ServerSession) Allow(serviceDesc *grpc.ServiceDesc, serviceImpl interfa
 	s.grpcServer.RegisterService(serviceDesc, serviceImpl)
 }
 
+func (s *ServerSession) AllowStream(serviceName, instanceId, method string, handler MessageStreamHandler) error {
+	serviceName = strings.ToLower(serviceName)
+	instanceId = strings.ToLower(instanceId)
+	method = strings.ToLower(method)
+	if strings.Contains(serviceName, ".") {
+		return errors.New("serviceName should not contain character'.'")
+	}
+	svc, ok := s.streamServices[serviceName]
+	if !ok {
+		svc = &streamServiceDesc{instances: make(map[string]*streamInstanceDesc)}
+		s.streamServices[serviceName] = svc
+	}
+	instance, ok := svc.instances[instanceId]
+	if !ok {
+		instance = &streamInstanceDesc{methods: make(map[string]MessageStreamHandler)}
+		svc.instances[instanceId] = instance
+	}
+	instance.methods[method] = handler
+	return nil
+}
+
 // UUID returns unique identifier for the session
 func (s *Session) UUID() string {
 	return s.uuid
 }
 
+func grpcServiceName(serviceName, instanceId string) string {
+	return serviceName + "." + instanceId
+}
+func serviceNameAndInstance(grpcServiceName string) (serviceName, instanceId string) {
+	parts := strings.SplitN(grpcServiceName, ".", 2)
+	if len(parts) >= 1 {
+		serviceName = parts[0]
+	}
+	if len(parts) >= 2 {
+		instanceId = parts[1]
+	}
+	return
+}
+
 // Run activates the session
 func (s *ServerSession) Run(ctx context.Context, dialer Dialer) error {
+
 	ctx, cancel := context.WithCancel(ctx)
 	s.cancelCtx = cancel
 	s.done = make(chan struct{})
@@ -87,6 +144,34 @@ func (s *ServerSession) Run(ctx context.Context, dialer Dialer) error {
 		serviceNames = append(serviceNames, svc)
 	}
 	meta["X-Docker-Expose-Session-Services"] = serviceNames
+
+	// register low level streams
+	for svcName, svcDesc := range s.streamServices {
+		meta["X-Docker-Expose-Session-Streamservices"] = append(meta["X-Docker-Expose-Session-Streamservices"], svcName)
+		svcInstancesHeader := "X-Docker-Expose-Session-Streaminstances-" + headerize(svcName)
+		for instanceId, instanceDesc := range svcDesc.instances {
+			grpcSvcDesc := grpc.ServiceDesc{
+				ServiceName: grpcServiceName(svcName, instanceId),
+				HandlerType: (*interface{})(nil),
+				Methods:     make([]grpc.MethodDesc, 0, 0),
+			}
+			meta[svcInstancesHeader] = append(meta[svcInstancesHeader], instanceId)
+			methodsHeader := "X-Docker-Expose-Session-Streammethods-" + headerize(grpcServiceName(svcName, instanceId))
+			for methName, methImpl := range instanceDesc.methods {
+				meta[methodsHeader] = append(meta[methodsHeader], methName)
+				grpcSvcDesc.Streams = append(grpcSvcDesc.Streams,
+					grpc.StreamDesc{
+						ClientStreams: true,
+						ServerStreams: true,
+						StreamName:    methName,
+						Handler: func(srv interface{}, stream grpc.ServerStream) error {
+							return methImpl(stream)
+						},
+					})
+			}
+			s.grpcServer.RegisterService(&grpcSvcDesc, (*interface{})(nil))
+		}
+	}
 
 	conn, err := dialer(ctx, "gRPC", meta)
 	if err != nil {
@@ -179,12 +264,13 @@ func (sm *Manager) HandleHTTPRequest(ctx context.Context, w http.ResponseWriter,
 
 	s := &ClientSession{
 		Session: Session{
-			uuid:      uuid,
-			name:      name,
-			sharedKey: sharedKey,
-			ctx:       ctx,
-			cancelCtx: cancel,
-			done:      make(chan struct{}),
+			uuid:           uuid,
+			name:           name,
+			sharedKey:      sharedKey,
+			ctx:            ctx,
+			cancelCtx:      cancel,
+			done:           make(chan struct{}),
+			streamServices: make(map[string]*streamServiceDesc),
 		},
 		caller:            caller,
 		supportedServices: make(map[string]struct{}),
@@ -193,6 +279,21 @@ func (sm *Manager) HandleHTTPRequest(ctx context.Context, w http.ResponseWriter,
 	serviceNames := r.Header["X-Docker-Expose-Session-Services"]
 	for _, svcName := range serviceNames {
 		s.supportedServices[svcName] = struct{}{}
+	}
+
+	streamServiceNames := r.Header["X-Docker-Expose-Session-Streamservices"]
+	for _, svcName := range streamServiceNames {
+		svc := &streamServiceDesc{instances: make(map[string]*streamInstanceDesc)}
+		s.streamServices[svcName] = svc
+		instanceNames := r.Header["X-Docker-Expose-Session-Streaminstances-"+headerize(svcName)]
+		for _, instanceId := range instanceNames {
+			instance := &streamInstanceDesc{methods: make(map[string]MessageStreamHandler)}
+			svc.instances[instanceId] = instance
+			methNames := r.Header["X-Docker-Expose-Session-Streammethods-"+headerize(grpcServiceName(svcName, instanceId))]
+			for _, methName := range methNames {
+				instance.methods[methName] = nil
+			}
+		}
 	}
 
 	sm.sessions[uuid] = s
@@ -211,6 +312,14 @@ func (sm *Manager) HandleHTTPRequest(ctx context.Context, w http.ResponseWriter,
 	close(s.done)
 
 	return nil
+}
+
+func headerize(value string) string {
+	parts := strings.Split(value, "-")
+	for i := 0; i < len(parts); i++ {
+		parts[i] = strings.ToUpper(parts[i][:1]) + strings.ToLower(parts[i][1:])
+	}
+	return strings.Join(parts, "-")
 }
 
 // GetSession returns a session by UUID
@@ -272,4 +381,50 @@ func (sc *sessionCaller) Supports(serviceName string) bool {
 }
 func (sc *sessionCaller) GetGrpcConn() *grpc.ClientConn {
 	return sc.s.caller.cc
+}
+
+func (sc *sessionCaller) ListStreamInstances(serviceName string) []string {
+	serviceName = strings.ToLower(serviceName)
+	result := []string{}
+	if svc, ok := sc.s.streamServices[serviceName]; ok {
+		for name, _ := range svc.instances {
+			result = append(result, name)
+		}
+	}
+	return result
+}
+
+func (sc *sessionCaller) SupportsStream(serviceName, instanceId, method string) bool {
+	serviceName = strings.ToLower(serviceName)
+	instanceId = strings.ToLower(instanceId)
+	method = strings.ToLower(method)
+	svc, ok := sc.s.streamServices[serviceName]
+	if !ok {
+		return false
+	}
+	instance, ok := svc.instances[instanceId]
+	if !ok {
+		return false
+	}
+	_, ok = instance.methods[method]
+	return ok
+}
+
+func (sc *sessionCaller) ConnectToStream(ctx context.Context, serviceName, instanceId, method string, handler MessageStreamHandler) error {
+	serviceName = strings.ToLower(serviceName)
+	instanceId = strings.ToLower(instanceId)
+	method = strings.ToLower(method)
+	url := "/" + grpcServiceName(serviceName, instanceId) + "/" + method
+	desc := &grpc.StreamDesc{
+		StreamName:    method,
+		Handler:       nil,
+		ServerStreams: true,
+		ClientStreams: true,
+	}
+	stream, err := grpc.NewClientStream(ctx, desc, sc.GetGrpcConn(), url)
+	if err != nil {
+		return err
+	}
+	defer stream.CloseSend()
+	return handler(stream)
 }
