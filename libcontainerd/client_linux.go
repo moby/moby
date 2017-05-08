9771780a01e73200f96c84aa83689b7f34092772
Deng Guangxing <dengguangxing@huawei.com>
2017-05-08 11:18:51 +0800

    fix inconsistent state string with containerd
    should be `stopped` according to containerd:
      https://github.com/containerd/containerd/blob/v0.2.x/runtime/runtime.go#L104
    
    Signed-off-by: Deng Guangxing <dengguangxing@huawei.com>

--- libcontainerd/client_linux.go
+++ libcontainerd/client_linux.go
@@ -467,7 +467,7 @@ func (clnt *client) Restore(containerID string, attachStdio StdioCallback, optio
 	cont, err := clnt.getContainerdContainer(containerID)
 	// Get its last event
 	ev, eerr := clnt.getContainerLastEvent(containerID)
-	if err != nil || cont.Status == "Stopped" {
+	if err != nil || cont.Status == "stopped" {
 		if err != nil {
 			logrus.Warnf("libcontainerd: failed to retrieve container %s state: %v", containerID, err)
 		}
