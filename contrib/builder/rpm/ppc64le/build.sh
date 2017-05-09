1e7b3f0a578d369aa2034170596429f615e312c1
Utz Bacher <utz.bacher@de.ibm.com>
2017-05-09 23:56:08 +0200

    adding rpm builds for ppc64le and s390x
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>
    Signed-off-by: Christopher Jones <tophj@linux.vnet.ibm.com>
    Signed-off-by: Dominik Dingel <dingel@de.ibm.com>
    Signed-off-by: Thorsten Winkler <thorsten.winkler@de.ibm.com>
    Signed-off-by: Utz Bacher <utz.bacher@de.ibm.com>

--- contrib/builder/rpm/ppc64le/build.sh
+++ contrib/builder/rpm/ppc64le/build.sh
@@ -3,6 +3,8 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+wget http://download.opensuse.org/repositories/Virtualization:/containers:/images:/openSUSE-Tumbleweed/images/openSUSE-Tumbleweed-docker-guest-docker.ppc64le.tar.xz && unxz openSUSE-Tumbleweed-docker-guest-docker.ppc64le.tar.xz && docker load -i openSUSE-Tumbleweed-docker-guest-docker.ppc64le.tar 
+
 set -x
 ./generate.sh
 for d in */; do
