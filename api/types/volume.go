2cddc993de42360d47a8e0c1a58c653ae34ddaf7
Sebastiaan van Stijn <github@gone.nl>
2017-07-19 16:08:38 +0200

    Improve API docs for UsageData
    The docs did not mention when this information
    was set, and what the `-1` value indicated.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 194f635ce7d097f550986bc3169ab59158f5aa68)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/types/volume.go
+++ api/types/volume.go
@@ -44,15 +44,23 @@ type Volume struct {
 	UsageData *VolumeUsageData `json:"UsageData,omitempty"`
 }
 
-// VolumeUsageData volume usage data
+// VolumeUsageData Usage details about the volume. This information is used by the
+// `GET /system/df` endpoint, and omitted in other endpoints.
+//
 // swagger:model VolumeUsageData
 type VolumeUsageData struct {
 
-	// The number of containers referencing this volume.
+	// The number of containers referencing this volume. This field
+	// is set to `-1` if the reference-count is not available.
+	//
 	// Required: true
 	RefCount int64 `json:"RefCount"`
 
-	// The disk space used by the volume (local driver only)
+	// Amount of disk space used by the volume (in bytes). This information
+	// is only available for volumes created with the `"local"` volume
+	// driver. For volumes created with other volume drivers, this field
+	// is set to `-1` ("not available")
+	//
 	// Required: true
 	Size int64 `json:"Size"`
 }
