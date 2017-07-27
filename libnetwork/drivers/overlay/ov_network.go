5c52ff49e053a47145907f73e2a28c29f0133a0e
Flavio Crisciani <flavio.crisciani@docker.com>
2017-07-27 11:43:13 -0700

    Funnel peerAdd and peerDelete in a channel
    Remove the need for the wait group and avoid new
    locks
    Added utility to print the method name and the caller name
    
    Signed-off-by: Flavio Crisciani <flavio.crisciani@docker.com>

--- libnetwork/drivers/overlay/ov_network.go
+++ libnetwork/drivers/overlay/ov_network.go
@@ -765,10 +765,7 @@ func (n *network) watchMiss(nlSock *nl.NetlinkSocket) {
 					logrus.Errorf("could not resolve peer %q: %v", ip, err)
 					continue
 				}
-
-				if err := n.driver.peerAdd(n.id, "dummy", ip, IPmask, mac, vtep, true, l2Miss, l3Miss); err != nil {
-					logrus.Errorf("could not add neighbor entry for missed peer %q: %v", ip, err)
-				}
+				n.driver.peerAdd(n.id, "dummy", ip, IPmask, mac, vtep, true, l2Miss, l3Miss, false)
 			} else {
 				// If the gc_thresh values are lower kernel might knock off the neighor entries.
 				// When we get a L3 miss check if its a valid peer and reprogram the neighbor
