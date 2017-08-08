5459ef0ecf429ed0293e975abee11bdbbc2a2f14
John Howard <jhoward@microsoft.com>
2017-08-08 12:54:06 -0700

    LCOW: Don't block export
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- daemon/export.go
+++ daemon/export.go
@@ -13,13 +13,13 @@ import (
 // ContainerExport writes the contents of the container to the given
 // writer. An error is returned if the container cannot be found.
 func (daemon *Daemon) ContainerExport(name string, out io.Writer) error {
-	if runtime.GOOS == "windows" {
-		return fmt.Errorf("the daemon on this platform does not support export of a container")
-	}
-
 	container, err := daemon.GetContainer(name)
 	if err != nil {
 		return err
+	}
+
+	if runtime.GOOS == "windows" && container.Platform == "windows" {
+		return fmt.Errorf("the daemon on this platform does not support exporting Windows containers")
 	}
 
 	data, err := daemon.containerExport(container)
