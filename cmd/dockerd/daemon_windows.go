e942513ac46656c3f54cd103e990e2b7bd5c2b14
Tobias Klauser <tklauser@distanz.ch>
2017-08-21 12:58:09 +0200

    Use event functions from golang.org/x/sys/windows
    Use CreateEvent, OpenEvent (which both map to the respective *EventW
    function) and PulseEvent from golang.org/x/sys instead of local copies.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- cmd/dockerd/daemon_windows.go
+++ cmd/dockerd/daemon_windows.go
@@ -7,7 +7,6 @@ import (
 	"path/filepath"
 
 	"github.com/docker/docker/libcontainerd"
-	"github.com/docker/docker/pkg/system"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/windows"
 )
@@ -61,8 +60,8 @@ func (cli *DaemonCli) setupConfigReloadTrap() {
 		sa := windows.SecurityAttributes{
 			Length: 0,
 		}
-		ev := "Global\\docker-daemon-config-" + fmt.Sprint(os.Getpid())
-		if h, _ := system.CreateEvent(&sa, false, false, ev); h != 0 {
+		ev, _ := windows.UTF16PtrFromString("Global\\docker-daemon-config-" + fmt.Sprint(os.Getpid()))
+		if h, _ := windows.CreateEvent(&sa, 0, 0, ev); h != 0 {
 			logrus.Debugf("Config reload - waiting signal at %s", ev)
 			for {
 				windows.WaitForSingleObject(h, windows.INFINITE)
