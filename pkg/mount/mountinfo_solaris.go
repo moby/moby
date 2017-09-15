593dbfd1448e8dac08488786fde6fe7fb057bdac
Tobias Klauser <tklauser@distanz.ch>
2017-09-15 09:53:54 +0200

    Fix CString memory leaks
    Make sure to call C.free on C string allocated using C.CString in every
    exit path.
    
    C.CString allocates memory in the C heap using malloc. It is the callers
    responsibility to free them. See
    https://golang.org/cmd/cgo/#hdr-Go_references_to_C for details.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- pkg/mount/mountinfo_solaris.go
+++ pkg/mount/mountinfo_solaris.go
@@ -4,16 +4,23 @@ package mount
 
 /*
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mnttab.h>
 */
 import "C"
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 func parseMountTable() ([]*Info, error) {
-	mnttab := C.fopen(C.CString(C.MNTTAB), C.CString("r"))
+	path := C.CString(C.MNTTAB)
+	defer C.free(unsafe.Pointer(path))
+	mode := C.CString("r")
+	defer C.free(unsafe.Pointer(mode))
+
+	mnttab := C.fopen(path, mode)
 	if mnttab == nil {
 		return nil, fmt.Errorf("Failed to open %s", C.MNTTAB)
 	}
