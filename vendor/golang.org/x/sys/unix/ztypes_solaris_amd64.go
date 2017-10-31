6d068bc25b35a420e63e295ea4ab4ac4a6e6665b
Tobias Klauser <tklauser@distanz.ch>
2017-10-31 10:32:03 +0100

    Simplify Utsname string conversion
    Update golang.org/x/sys to 95c6576299259db960f6c5b9b69ea52422860fce in
    order to get the unix.Utsname with byte array instead of int8/uint8
    members.
    
    This allows to use simple byte slice to string conversions instead of
    using charsToString or its open-coded version.
    
    Also see golang/go#20753 for details.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- vendor/golang.org/x/sys/unix/ztypes_solaris_amd64.go
+++ vendor/golang.org/x/sys/unix/ztypes_solaris_amd64.go
@@ -263,11 +263,11 @@ type FdSet struct {
 }
 
 type Utsname struct {
-	Sysname  [257]int8
-	Nodename [257]int8
-	Release  [257]int8
-	Version  [257]int8
-	Machine  [257]int8
+	Sysname  [257]byte
+	Nodename [257]byte
+	Release  [257]byte
+	Version  [257]byte
+	Machine  [257]byte
 }
 
 type Ustat_t struct {
@@ -438,3 +438,22 @@ type Winsize struct {
 	Xpixel uint16
 	Ypixel uint16
 }
+
+type PollFd struct {
+	Fd      int32
+	Events  int16
+	Revents int16
+}
+
+const (
+	POLLERR    = 0x8
+	POLLHUP    = 0x10
+	POLLIN     = 0x1
+	POLLNVAL   = 0x20
+	POLLOUT    = 0x4
+	POLLPRI    = 0x2
+	POLLRDBAND = 0x80
+	POLLRDNORM = 0x40
+	POLLWRBAND = 0x100
+	POLLWRNORM = 0x4
+)
