850499222fc9184c547f0d174fa435cf76968a74
Justin Cormack <justin.cormack@docker.com>
2017-11-03 15:12:22 +0000

    Add /proc/scsi to masked paths
    This is writeable, and can be used to remove devices. Containers do
    not need to know about scsi devices.
    
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>
    (cherry picked from commit a21ecdf3c8a343a7c94e4c4d01b178c87ca7aaa1)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- oci/defaults.go
+++ oci/defaults.go
@@ -132,6 +132,7 @@ func DefaultLinuxSpec() specs.Spec {
 			"/proc/timer_list",
 			"/proc/timer_stats",
 			"/proc/sched_debug",
+			"/proc/scsi",
 		},
 		ReadonlyPaths: []string{
 			"/proc/asound",
