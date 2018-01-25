a115150e4f6e6ede39a071c919b334d5cba061e4
Nikita Gerasimov <nikita.gerasimov@oracle.com>
2018-01-25 03:46:49 +0300

    Change btrfs graphdriver to keep subvolumes in readonly state until used.
    Also remove reference to deprecated IOCTL BTRFS_IOC_SUBVOL_CREATE.
    BTRFS_IOC_SUBVOL_CREATE_V2 and BTRFS_IOC_SNAP_CREATE_V2 have identical
    signature and utilize the same function on kernel side. So both calls
    are processed by subvolSnapshot now. In case of src == "" it's creates
    empty snapshot - subvol. Therefore, there is no need for a separate
    func subvolCreate.
    
    Signed-off-by: Nikita Gerasimov <nikita.gerasimov@oracle.com>

--- daemon/graphdriver/btrfs/btrfs_test.go
+++ daemon/graphdriver/btrfs/btrfs_test.go
@@ -3,10 +3,13 @@
 package btrfs // import "github.com/docker/docker/daemon/graphdriver/btrfs"
 
 import (
+	"io/ioutil"
 	"os"
 	"path"
+	"syscall"
 	"testing"
 
+	"github.com/docker/docker/daemon/graphdriver"
 	"github.com/docker/docker/daemon/graphdriver/graphtest"
 )
 
@@ -43,7 +46,7 @@ func TestBtrfsSubvolDelete(t *testing.T) {
 
 	dir := dirFS.Path()
 
-	if err := subvolCreate(dir, "subvoltest"); err != nil {
+	if err := subvolSnapshot("", dir, "subvoltest"); err != nil {
 		t.Fatal(err)
 	}
 
@@ -57,6 +60,73 @@ func TestBtrfsSubvolDelete(t *testing.T) {
 
 	if _, err := os.Stat(path.Join(dir, "subvoltest")); !os.IsNotExist(err) {
 		t.Fatalf("expected not exist error on nested subvol, got: %v", err)
+	}
+}
+
+func TestBtrfsSubvolRO(t *testing.T) {
+	d := graphtest.GetDriver(t, "btrfs")
+	defer graphtest.PutDriver(t)
+
+	x := d.(*graphtest.Driver).Driver.(*graphdriver.NaiveDiffDriver).ProtoDriver.(*Driver)
+	subdir := x.subvolumesDirID("0subvol")
+	snapdir := x.subvolumesDirID("1snap")
+
+	if err := d.Create("0subvol", "", nil); err != nil {
+		t.Fatal(err)
+	}
+	// Try write into RO subvolume
+	if err := ioutil.WriteFile(path.Join(subdir, "testfile0"), []byte("test"), 0700); err.(*os.PathError).Err != syscall.EROFS {
+		t.Fatal(err)
+	}
+
+	if err := d.Create("1snap", "0subvol", nil); err != nil {
+		t.Fatal(err)
+	}
+	// Try write into RO snapshot
+	if err := ioutil.WriteFile(path.Join(subdir, "testfile1"), []byte("test"), 0700); err.(*os.PathError).Err != syscall.EROFS {
+		t.Fatal(err)
+	}
+
+	if _, err := d.Get("1snap", ""); err != nil {
+		t.Fatal(err)
+	}
+	// Write into RW snapshot
+	filepath := path.Join(snapdir, "testfile2")
+	if err := ioutil.WriteFile(filepath, []byte("test"), 0700); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := d.Put("1snap"); err != nil {
+		t.Fatal(err)
+	}
+	// Try delete from RO snapshot
+	if err := os.Remove(filepath); err.(*os.PathError).Err != syscall.EROFS {
+		t.Fatal(err)
+	}
+
+	if err := d.Remove("1snap"); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := d.Get("0subvol", ""); err != nil {
+		t.Fatal(err)
+	}
+	// Write into RW subvolume
+	filepath = path.Join(subdir, "testfile3")
+	if err := ioutil.WriteFile(filepath, []byte("test"), 0700); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := d.Put("0subvol"); err != nil {
+		t.Fatal(err)
+	}
+	// Try delete from RO subvolume
+	if err := os.Remove(filepath); err.(*os.PathError).Err != syscall.EROFS {
+		t.Fatal(err)
+	}
+
+	if err := d.Remove("0subvol"); err != nil {
+		t.Fatal(err)
 	}
 }
 
