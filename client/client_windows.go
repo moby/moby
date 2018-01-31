772edd020cc784913973387b00c4d0c526a10a26
Vincent Demeester <vincent@sbr.pm>
2018-01-31 11:28:33 -0800

    Introduce NewClientWithOpts func to build custom client easily
    This allows to create a client with default values and override those
    using functors. As an example, `NewEnvClient()` becomes
    `NewClientWithOpts(FromEnv)` ; and if you want a different api version
    for this client : `NewClientWithOpts(FromEnv, WithVersion("1.35"))`
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- client/client_windows.go
+++ client/client_windows.go
@@ -2,3 +2,6 @@ package client // import "github.com/docker/docker/client"
 
 // DefaultDockerHost defines os specific default if DOCKER_HOST is unset
 const DefaultDockerHost = "npipe:////./pipe/docker_engine"
+
+const defaultProto = "npipe"
+const defaultAddr = "//./pipe/docker_engine"
