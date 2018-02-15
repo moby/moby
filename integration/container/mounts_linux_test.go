ce35439015e4d2190bd82a3b6dfec98f7a12ac90
Sebastiaan van Stijn <github@gone.nl>
2018-02-15 15:29:45 +0100

    Fix import path
    The utils package was moved to "internal" in commit
    af306d149e76b100e08972cda364647bd7bcfe1e
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- integration/container/mounts_linux_test.go
+++ integration/container/mounts_linux_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/integration-cli/daemon"
-	"github.com/docker/docker/integration/util/request"
+	"github.com/docker/docker/integration/internal/request"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/docker/pkg/system"
 	"github.com/gotestyourself/gotestyourself/fs"
