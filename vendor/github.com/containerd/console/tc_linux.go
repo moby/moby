c76f748e6232c06a6a1219d5f2d042b4abdc2128
Sebastiaan van Stijn <github@gone.nl>
2018-02-28 21:43:20 +0100

    bump containerd/console to 2748ece16665b45a47f884001d5831ec79703880
    Fix runc exec on big-endian, causing:
    
        container_linux.go:265: starting container process caused "open /dev/pts/4294967296: no such file or directory"
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit aab5eaddccb8cb196fdb1e285890dfa94a071b14)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/containerd/console/tc_linux.go
+++ vendor/github.com/containerd/console/tc_linux.go
@@ -13,25 +13,21 @@ const (
 	cmdTcSet = unix.TCSETS
 )
 
-func ioctl(fd, flag, data uintptr) error {
-	if _, _, err := unix.Syscall(unix.SYS_IOCTL, fd, flag, data); err != 0 {
+// unlockpt unlocks the slave pseudoterminal device corresponding to the master pseudoterminal referred to by f.
+// unlockpt should be called before opening the slave side of a pty.
+func unlockpt(f *os.File) error {
+	var u int32
+	if _, _, err := unix.Syscall(unix.SYS_IOCTL, f.Fd(), unix.TIOCSPTLCK, uintptr(unsafe.Pointer(&u))); err != 0 {
 		return err
 	}
 	return nil
 }
 
-// unlockpt unlocks the slave pseudoterminal device corresponding to the master pseudoterminal referred to by f.
-// unlockpt should be called before opening the slave side of a pty.
-func unlockpt(f *os.File) error {
-	var u int32
-	return ioctl(f.Fd(), unix.TIOCSPTLCK, uintptr(unsafe.Pointer(&u)))
-}
-
 // ptsname retrieves the name of the first available pts for the given master.
 func ptsname(f *os.File) (string, error) {
-	n, err := unix.IoctlGetInt(int(f.Fd()), unix.TIOCGPTN)
-	if err != nil {
+	var u uint32
+	if _, _, err := unix.Syscall(unix.SYS_IOCTL, f.Fd(), unix.TIOCGPTN, uintptr(unsafe.Pointer(&u))); err != 0 {
 		return "", err
 	}
-	return fmt.Sprintf("/dev/pts/%d", n), nil
+	return fmt.Sprintf("/dev/pts/%d", u), nil
 }
