1606abcc04cddea2e2b451c9b887b1920b6aeb5a
Arash Deshmeh <adeshmeh@ca.ibm.com>
2018-03-21 17:47:49 -0400

    use unique names for resources used by integration tests container/inspect_test, container/ps_test, container/stop_test
    Signed-off-by: Arash Deshmeh <adeshmeh@ca.ibm.com>
    (cherry picked from commit 78e4be91332e2237c0fa14eb3ba0fb5b915c3256)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- integration/container/stop_test.go
+++ integration/container/stop_test.go
@@ -21,7 +21,7 @@ func TestStopContainerWithRestartPolicyAlways(t *testing.T) {
 	client := request.NewAPIClient(t)
 	ctx := context.Background()
 
-	names := []string{"verifyRestart1", "verifyRestart2"}
+	names := []string{"verifyRestart1-" + t.Name(), "verifyRestart2-" + t.Name()}
 	for _, name := range names {
 		container.Run(t, ctx, client, container.WithName(name), container.WithCmd("false"), func(c *container.TestContainerConfig) {
 			c.HostConfig.RestartPolicy.Name = "always"
@@ -49,7 +49,7 @@ func TestDeleteDevicemapper(t *testing.T) {
 	client := request.NewAPIClient(t)
 	ctx := context.Background()
 
-	id := container.Run(t, ctx, client, container.WithName("foo"), container.WithCmd("echo"))
+	id := container.Run(t, ctx, client, container.WithName("foo-"+t.Name()), container.WithCmd("echo"))
 
 	poll.WaitOn(t, container.IsStopped(ctx, client, id), poll.WithDelay(100*time.Millisecond))
 
