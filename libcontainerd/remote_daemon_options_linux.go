2c682d5209d1d1cdd232ce3909fa25b6023f9a5b
Brian Goff <cpuguy83@gmail.com>
2018-03-23 14:25:53 -0400

    Use containerd client `Reconnect()` API.
    This fixes an issue where the containerd client is cached in a container
    object in libcontainerd and becomes stale after containerd is restarted.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- libcontainerd/remote_daemon_options_linux.go
+++ libcontainerd/remote_daemon_options_linux.go
@@ -16,19 +16,3 @@ func (o oomScore) Apply(r Remote) error {
 	}
 	return fmt.Errorf("WithOOMScore option not supported for this remote")
 }
-
-// WithSubreaper sets whether containerd should register itself as a
-// subreaper
-func WithSubreaper(reap bool) RemoteOption {
-	return subreaper(reap)
-}
-
-type subreaper bool
-
-func (s subreaper) Apply(r Remote) error {
-	if remote, ok := r.(*remote); ok {
-		remote.NoSubreaper = !bool(s)
-		return nil
-	}
-	return fmt.Errorf("WithSubreaper option not supported for this remote")
-}
