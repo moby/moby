3df2c022ac23216d751e680ea0f6593cd72c8935
Stephen J Day <stephen.day@docker.com>
2018-03-30 11:47:50 -0700

    containerd: update to 1.0.3 release
    Signed-off-by: Stephen J Day <stephen.day@docker.com>
    (cherry picked from commit 554d657c1f7a0f4dce51f0d26407fb05a0580fa1)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/containerd/cgroups/devices.go
+++ vendor/github.com/containerd/cgroups/devices.go
@@ -1,3 +1,19 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package cgroups
 
 import (
@@ -58,11 +74,11 @@ func (d *devicesController) Update(path string, resources *specs.LinuxResources)
 }
 
 func deviceString(device specs.LinuxDeviceCgroup) string {
-	return fmt.Sprintf("%c %s:%s %s",
-		&device.Type,
+	return fmt.Sprintf("%s %s:%s %s",
+		device.Type,
 		deviceNumber(device.Major),
 		deviceNumber(device.Minor),
-		&device.Access,
+		device.Access,
 	)
 }
 
