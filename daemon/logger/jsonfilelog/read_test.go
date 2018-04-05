94a10150f64a24793216f5324a34e62be3f31a2d
Brian Goff <cpuguy83@gmail.com>
2018-04-05 12:39:28 -0400

    Decouple logfile from tailfile.
    This makes it so consumers of `LogFile` should pass in how to get an
    io.Reader to the requested number of lines to tail.
    
    This is also much more efficient when tailing a large number of lines.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- daemon/logger/jsonfilelog/read_test.go
+++ daemon/logger/jsonfilelog/read_test.go
@@ -2,6 +2,7 @@ package jsonfilelog // import "github.com/docker/docker/daemon/logger/jsonfilelo
 
 import (
 	"bytes"
+	"io"
 	"testing"
 	"time"
 
@@ -61,4 +62,33 @@ func BenchmarkJSONFileLoggerReadLogs(b *testing.B) {
 			}
 		}
 	}
+}
+
+func TestEncodeDecode(t *testing.T) {
+	t.Parallel()
+
+	m1 := &logger.Message{Line: []byte("hello 1"), Timestamp: time.Now(), Source: "stdout"}
+	m2 := &logger.Message{Line: []byte("hello 2"), Timestamp: time.Now(), Source: "stdout"}
+	m3 := &logger.Message{Line: []byte("hello 3"), Timestamp: time.Now(), Source: "stdout"}
+
+	buf := bytes.NewBuffer(nil)
+	assert.Assert(t, marshalMessage(m1, nil, buf))
+	assert.Assert(t, marshalMessage(m2, nil, buf))
+	assert.Assert(t, marshalMessage(m3, nil, buf))
+
+	decode := decodeFunc(buf)
+	msg, err := decode()
+	assert.Assert(t, err)
+	assert.Assert(t, string(msg.Line) == "hello 1\n", string(msg.Line))
+
+	msg, err = decode()
+	assert.Assert(t, err)
+	assert.Assert(t, string(msg.Line) == "hello 2\n")
+
+	msg, err = decode()
+	assert.Assert(t, err)
+	assert.Assert(t, string(msg.Line) == "hello 3\n")
+
+	_, err = decode()
+	assert.Assert(t, err == io.EOF)
 }
