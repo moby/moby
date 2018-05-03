c6e2af54256211b8ac757e9b25caa6fb6c9b3c6e
Kir Kolyshkin <kolyshkin@gmail.com>
2018-05-03 14:08:25 -0700

    aufs: use a single logger
    Simplify the code by using a single logger instance.
    
    While at it, use WithError in Umount.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- daemon/graphdriver/aufs/mount.go
+++ daemon/graphdriver/aufs/mount.go
@@ -5,14 +5,13 @@ package aufs // import "github.com/docker/docker/daemon/graphdriver/aufs"
 import (
 	"os/exec"
 
-	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 )
 
 // Unmount the target specified.
 func Unmount(target string) error {
 	if err := exec.Command("auplink", target, "flush").Run(); err != nil {
-		logrus.WithField("storage-driver", "aufs").Warnf("Couldn't run auplink before unmount %s: %s", target, err)
+		logger.WithError(err).Warnf("Couldn't run auplink before unmount %s", target)
 	}
 	return unix.Unmount(target, 0)
 }
