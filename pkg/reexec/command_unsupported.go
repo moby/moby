21537b818de5e4167c5a3856c25b9b06cf16d87d
Fabian Raetz <fabian.raetz@gmail.com>
2018-06-16 17:53:50 +0200

    fix build on OpenBSD by defining Self()
    Signed-off-by: Fabian Raetz <fabian.raetz@gmail.com>

--- pkg/reexec/command_unsupported.go
+++ pkg/reexec/command_unsupported.go
@@ -6,6 +6,10 @@ import (
 	"os/exec"
 )
 
+func Self() string {
+	return ""
+}
+
 // Command is unsupported on operating systems apart from Linux, Windows, and Darwin.
 func Command(args ...string) *exec.Cmd {
 	return nil
