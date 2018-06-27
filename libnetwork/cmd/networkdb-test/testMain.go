b09cb39fa5cb326bc8c55aa2743a9759b5543a72
Flavio Crisciani <flavio.crisciani@docker.com>
2018-06-26 21:20:57 -0700

    Enhance testing infra
    Allow to write and delete X number of entries
    Allow to query the queue length
    
    Signed-off-by: Flavio Crisciani <flavio.crisciani@docker.com>

--- libnetwork/cmd/networkdb-test/testMain.go
+++ libnetwork/cmd/networkdb-test/testMain.go
@@ -10,6 +10,10 @@ import (
 )
 
 func main() {
+	formatter := &logrus.TextFormatter{
+		FullTimestamp: true,
+	}
+	logrus.SetFormatter(formatter)
 	logrus.Infof("Starting the image with these args: %v", os.Args)
 	if len(os.Args) < 1 {
 		log.Fatal("You need at least 1 argument [client/server]")
