21789cb9ee0a6beb31bcf3abd37db43acbe62d5a
Tibor Vass <tibor@docker.com>
2018-07-25 20:17:58 +0000

    buildutil: support for session
    Signed-off-by: Tibor Vass <tibor@docker.com>
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- builder/buildutil/util.go
+++ builder/buildutil/util.go
@@ -4,13 +4,18 @@ Package buildutil is a wrapper around client package for easier to use build fun
 package buildutil
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/moby/buildkit/session"
+	"github.com/moby/buildkit/session/filesync"
+	"github.com/sirupsen/logrus"
 )
 
 // BuildResult encapsulates the result of a build.
@@ -19,16 +24,57 @@ type BuildResult struct {
 	Output []byte
 }
 
-// BuildInput specifies how to get the context.
+// BuildInput specifies how to get the context or Dockerfile.
 type BuildInput struct {
-	Context io.Reader
+	Context    io.Reader
+	ContextDir string
+	Dockerfile []byte
 }
 
 // Build will call client package's ImageBuild and parse the JSONMessage responses into an easy to consume BuildResult.
 func Build(c client.APIClient, input BuildInput, options types.ImageBuildOptions) (*BuildResult, error) {
 	ctx := context.Background()
 	br := &BuildResult{}
-	resp, err := c.ImageBuild(ctx, input.Context, options)
+	var sess *session.Session
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	buildCtx := input.Context
+	if input.Context == nil && options.RemoteContext == "" {
+		sess, err := trySession(c, input.ContextDir)
+		if err != nil {
+			return br, err
+		}
+		if sess == nil {
+			return br, fmt.Errorf("session not supported")
+		}
+
+		dirs := make([]filesync.SyncedDir, 0, 1)
+		if input.ContextDir != "" {
+			dirs = append(dirs, filesync.SyncedDir{Dir: input.ContextDir})
+		}
+		if input.Dockerfile != nil {
+			buildCtx = bytes.NewReader(input.Dockerfile)
+		}
+		sess.Allow(filesync.NewFSSyncProvider(dirs))
+
+		options.SessionID = sess.ID()
+		options.RemoteContext = clientSessionRemote
+
+		go func() {
+			if err := sess.Run(ctx, c.DialSession); err != nil {
+				logrus.Error(err)
+				cancel()
+			}
+		}()
+	}
+
+	if sess != nil {
+		defer sess.Close()
+	}
+
+	resp, err := c.ImageBuild(ctx, buildCtx, options)
 	if err != nil {
 		return nil, err
 	}
