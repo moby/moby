7dac70324d0ce6acd23458b0bef06f099837d648
Tibor Vass <tibor@docker.com>
2018-09-06 20:18:07 +0000

    pkg/progress: work around closing closed channel panic
    I could not reproduce the panic in #37735, so here's a bandaid.
    
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- pkg/progress/progress.go
+++ pkg/progress/progress.go
@@ -39,6 +39,10 @@ type Output interface {
 type chanOutput chan<- Progress
 
 func (out chanOutput) WriteProgress(p Progress) error {
+	// FIXME: workaround for panic in #37735
+	defer func() {
+		recover()
+	}()
 	out <- p
 	return nil
 }
