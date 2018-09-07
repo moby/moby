763a708c4e47cb934c52734df2a01ddf715534a0
max8899 <lgong@alauda.io>
2018-09-07 12:20:46 +0800

    Fix typo: transtions ->  transitions
    Signed-off-by: Lei Gong <lgong@alauda.io>

--- integration-cli/docker_cli_run_unix_test.go
+++ integration-cli/docker_cli_run_unix_test.go
@@ -1140,7 +1140,7 @@ func (s *DockerSuite) TestRunSeccompDefaultProfileNS(c *check.C) {
 }
 
 // TestRunNoNewPrivSetuid checks that --security-opt='no-new-privileges=true' prevents
-// effective uid transtions on executing setuid binaries.
+// effective uid transitions on executing setuid binaries.
 func (s *DockerSuite) TestRunNoNewPrivSetuid(c *check.C) {
 	testRequires(c, DaemonIsLinux, NotUserNamespace, SameHostDaemon)
 	ensureNNPTest(c)
