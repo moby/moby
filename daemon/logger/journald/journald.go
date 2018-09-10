fe85c72a2eac4cbf249d2c4c754684bb447eefdd
Kir Kolyshkin <kolyshkin@gmail.com>
2018-09-10 16:14:37 -0700

    daemon/logger/journald: simplify readers field
    As in other similar drivers (jsonlog, local), use a set
    (i.e. `map[whatever]struct{}`), making the code simpler.
    
    While at it, make sure we remove the reader from the set
    after calling `ProducerGone()` on it.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>
    (cherry picked from commit b2b169f13f681cd0d591ccb06d6cfff97933db77)
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- daemon/logger/journald/journald.go
+++ daemon/logger/journald/journald.go
@@ -20,12 +20,8 @@ const name = "journald"
 type journald struct {
 	mu      sync.Mutex
 	vars    map[string]string // additional variables and values to send to the journal along with the log message
-	readers readerList
+	readers map[*logger.LogWatcher]struct{}
 	closed  bool
-}
-
-type readerList struct {
-	readers map[*logger.LogWatcher]*logger.LogWatcher
 }
 
 func init() {
@@ -84,7 +80,7 @@ func New(info logger.Info) (logger.Logger, error) {
 	for k, v := range extraAttrs {
 		vars[k] = v
 	}
-	return &journald{vars: vars, readers: readerList{readers: make(map[*logger.LogWatcher]*logger.LogWatcher)}}, nil
+	return &journald{vars: vars, readers: make(map[*logger.LogWatcher]struct{})}, nil
 }
 
 // We don't actually accept any options, but we have to supply a callback for
