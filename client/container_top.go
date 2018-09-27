7b08314855e7f3faeda85dd68d06433ef4ac7e96
Lifubang <lifubang@acmcoder.com>
2018-09-27 12:19:21 +0800

    fixes error when use strings.Join(psArgs, " ") if psArgs contains space
    Signed-off-by: Lifubang <lifubang@acmcoder.com>

--- client/container_top.go
+++ client/container_top.go
@@ -4,17 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"net/url"
-	"strings"
 
 	"github.com/docker/docker/api/types/container"
 )
 
 // ContainerTop shows process information from within a container.
+// for `docker top redisstor -C "sto redis-server"`, we can't use string.Join(arguments, " ")
 func (cli *Client) ContainerTop(ctx context.Context, containerID string, arguments []string) (container.ContainerTopOKBody, error) {
 	var response container.ContainerTopOKBody
 	query := url.Values{}
 	if len(arguments) > 0 {
-		query.Set("ps_args", strings.Join(arguments, " "))
+		argsjson, err := json.Marshal(arguments)
+		if err != nil {
+			return response, err
+		}
+		query.Set("ps_args", string(argsjson))
 	}
 
 	resp, err := cli.get(ctx, "/containers/"+containerID+"/top", query, nil)
