9da92768417fe5d42198f87f9e22a52b911fe550
Sebastiaan van Stijn <github@gone.nl>
2018-10-05 12:30:10 +0200

    Add containerd, runc, and docker-init versions to /version
    This patch adds version information about the containerd,
    runc, and docker-init components to the /version endpoint.
    
    With this patch applied, running:
    
    ```
    curl --unix-socket /var/run/docker.sock http://localhost/version | jq .
    ```
    
    Will produce this response:
    
    ```json
    {
      "Platform": {
        "Name": ""
      },
      "Components": [
        {
          "Name": "Engine",
          "Version": "dev",
          "Details": {
            "ApiVersion": "1.40",
            "Arch": "amd64",
            "BuildTime": "2018-11-08T10:23:42.000000000+00:00",
            "Experimental": "false",
            "GitCommit": "7d02782d2f",
            "GoVersion": "go1.11.2",
            "KernelVersion": "4.9.93-linuxkit-aufs",
            "MinAPIVersion": "1.12",
            "Os": "linux"
          }
        },
        {
          "Name": "containerd",
          "Version": "v1.1.4",
          "Details": {
            "GitCommit": "9f2e07b1fc1342d1c48fe4d7bbb94cb6d1bf278b"
          }
        },
        {
          "Name": "runc",
          "Version": "1.0.0-rc5+dev",
          "Details": {
            "GitCommit": "a00bf0190895aa465a5fbed0268888e2c8ddfe85"
          }
        },
        {
          "Name": "docker-init",
          "Version": "0.18.0",
          "Details": {
            "GitCommit": "fec3683"
          }
        }
      ],
      "Version": "dev",
      "ApiVersion": "1.40",
      "MinAPIVersion": "1.12",
      "GitCommit": "7d02782d2f",
      "GoVersion": "go1.11.2",
      "Os": "linux",
      "Arch": "amd64",
      "KernelVersion": "4.9.93-linuxkit-aufs",
      "BuildTime": "2018-11-08T10:23:42.000000000+00:00"
    }
    ```
    
    When using a recent version of the CLI, that information is included in the
    output of `docker version`:
    
    ```
    Client: Docker Engine - Community
     Version:           18.09.0
     API version:       1.39
     Go version:        go1.10.4
     Git commit:        4d60db4
     Built:             Wed Nov  7 00:46:51 2018
     OS/Arch:           linux/amd64
     Experimental:      false
    
    Server:
     Engine:
      Version:          dev
      API version:      1.40 (minimum version 1.12)
      Go version:       go1.11.2
      Git commit:       7d02782d2f
      Built:            Thu Nov  8 10:23:42 2018
      OS/Arch:          linux/amd64
      Experimental:     false
     containerd:
      Version:          v1.1.4
      GitCommit:        9f2e07b1fc1342d1c48fe4d7bbb94cb6d1bf278b
     runc:
      Version:          1.0.0-rc5+dev
      GitCommit:        a00bf0190895aa465a5fbed0268888e2c8ddfe85
     docker-init:
      Version:          0.18.0
      GitCommit:        fec3683
    ```
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/info_unix_test.go
+++ daemon/info_unix_test.go
@@ -5,49 +5,99 @@ package daemon // import "github.com/docker/docker/daemon"
 import (
 	"testing"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/dockerversion"
 	"gotest.tools/assert"
 	is "gotest.tools/assert/cmp"
 )
 
 func TestParseInitVersion(t *testing.T) {
 	tests := []struct {
+		output  string
 		version string
-		result  types.Commit
+		commit  string
 		invalid bool
 	}{
 		{
-			version: "tini version 0.13.0 - git.949e6fa",
-			result:  types.Commit{ID: "949e6fa", Expected: dockerversion.InitCommitID[0:7]},
+			output:  "tini version 0.13.0 - git.949e6fa",
+			version: "0.13.0",
+			commit:  "949e6fa",
 		}, {
-			version: "tini version 0.13.0\n",
-			result:  types.Commit{ID: "v0.13.0", Expected: dockerversion.InitCommitID},
+			output:  "tini version 0.13.0\n",
+			version: "0.13.0",
 		}, {
-			version: "tini version 0.13.2",
-			result:  types.Commit{ID: "v0.13.2", Expected: dockerversion.InitCommitID},
+			output:  "tini version 0.13.2",
+			version: "0.13.2",
 		}, {
-			version: "tini version0.13.2",
-			result:  types.Commit{ID: "N/A", Expected: dockerversion.InitCommitID},
+			output:  "tini version0.13.2",
 			invalid: true,
 		}, {
-			version: "",
-			result:  types.Commit{ID: "N/A", Expected: dockerversion.InitCommitID},
+			output:  "",
 			invalid: true,
 		}, {
-			version: "hello world",
-			result:  types.Commit{ID: "N/A", Expected: dockerversion.InitCommitID},
+			output:  "hello world",
 			invalid: true,
 		},
 	}
 
 	for _, test := range tests {
-		ver, err := parseInitVersion(string(test.version))
+		version, commit, err := parseInitVersion(string(test.output))
 		if test.invalid {
 			assert.Check(t, is.ErrorContains(err, ""))
 		} else {
 			assert.Check(t, err)
 		}
-		assert.Check(t, is.DeepEqual(test.result, ver))
+		assert.Equal(t, test.version, version)
+		assert.Equal(t, test.commit, commit)
+	}
+}
+
+func TestParseRuncVersion(t *testing.T) {
+	tests := []struct {
+		output  string
+		version string
+		commit  string
+		invalid bool
+	}{
+		{
+			output: `
+runc version 1.0.0-rc5+dev
+commit: 69663f0bd4b60df09991c08812a60108003fa340
+spec: 1.0.0
+`,
+			version: "1.0.0-rc5+dev",
+			commit:  "69663f0bd4b60df09991c08812a60108003fa340",
+		},
+		{
+			output: `
+runc version 1.0.0-rc5+dev
+spec: 1.0.0
+`,
+			version: "1.0.0-rc5+dev",
+		},
+		{
+			output: `
+commit: 69663f0bd4b60df09991c08812a60108003fa340
+spec: 1.0.0
+`,
+			commit: "69663f0bd4b60df09991c08812a60108003fa340",
+		},
+		{
+			output:  "",
+			invalid: true,
+		},
+		{
+			output:  "hello world",
+			invalid: true,
+		},
+	}
+
+	for _, test := range tests {
+		version, commit, err := parseRuncVersion(string(test.output))
+		if test.invalid {
+			assert.Check(t, is.ErrorContains(err, ""))
+		} else {
+			assert.Check(t, err)
+		}
+		assert.Equal(t, test.version, version)
+		assert.Equal(t, test.commit, commit)
 	}
 }
