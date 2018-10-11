abd1a5b4904277e71e0a33e94f3f095339647f6d
Sebastiaan van Stijn <github@gone.nl>
2018-10-11 16:35:50 +0200

    Remove skip evaluation of symlinks to data root on IoT Core
    This fix was added in 8e71b1e210dc0eff980f39271d6c1dd48d87024e to work around
    a go issue (https://github.com/golang/go/issues/20506).
    
    That issue was fixed in
    https://github.com/golang/go/commit/66c03d39f3aa65ec522c41e56c569391786539a7,
    which is part of Go 1.10 and up. This reverts the changes that were made in
    8e71b1e210dc0eff980f39271d6c1dd48d87024e, and are no longer needed.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    Signed-off-by: Lukas Heeren <Lukas-heeren@hotmail.com>

--- daemon/daemon_windows.go
+++ daemon/daemon_windows.go
@@ -12,7 +12,6 @@ import (
 	"github.com/docker/docker/container"
 	"github.com/docker/docker/daemon/config"
 	"github.com/docker/docker/pkg/containerfs"
-	"github.com/docker/docker/pkg/fileutils"
 	"github.com/docker/docker/pkg/idtools"
 	"github.com/docker/docker/pkg/parsers"
 	"github.com/docker/docker/pkg/platform"
@@ -649,16 +648,6 @@ func setupDaemonProcess(config *config.Config) error {
 
 func (daemon *Daemon) setupSeccompProfile() error {
 	return nil
-}
-
-func getRealPath(path string) (string, error) {
-	if system.IsIoTCore() {
-		// Due to https://github.com/golang/go/issues/20506, path expansion
-		// does not work correctly on the default IoT Core configuration.
-		// TODO @darrenstahlmsft remove this once golang/go/20506 is fixed
-		return path, nil
-	}
-	return fileutils.ReadSymlinkedDirectory(path)
 }
 
 func (daemon *Daemon) loadRuntimes() error {
