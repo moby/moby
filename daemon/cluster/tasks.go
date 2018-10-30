97dcc3a83b3624020fd47bc2a394f769a57a0bd9
Drew Erny <drew.erny@docker.com>
2018-10-30 12:43:50 -0500

    Use swarmkit ListTasksStream API
    Uses a new swarmkit ListTasksStream gRPC endpoint to avoid a bug where
    having too many tasks could cause the gRPC message size to exceed the
    max and prevent the task list from succeeding.
    
    Fixes moby/moby#37997
    
    Signed-off-by: Drew Erny <drew.erny@docker.com>

--- daemon/cluster/tasks.go
+++ daemon/cluster/tasks.go
@@ -2,17 +2,19 @@ package cluster // import "github.com/docker/docker/daemon/cluster"
 
 import (
 	"context"
+	"io"
 
 	apitypes "github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	types "github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/daemon/cluster/convert"
 	swarmapi "github.com/docker/swarmkit/api"
+	"github.com/pkg/errors"
 )
 
 // GetTasks returns a list of tasks matching the filter options.
 func (c *Cluster) GetTasks(options apitypes.TaskListOptions) ([]types.Task, error) {
-	var r *swarmapi.ListTasksResponse
+	swarmTasks := []*swarmapi.Task{}
 
 	if err := c.lockedManagerAction(func(ctx context.Context, state nodeState) error {
 		filterTransform := func(filter filters.Args) error {
@@ -51,16 +53,41 @@ func (c *Cluster) GetTasks(options apitypes.TaskListOptions) ([]types.Task, erro
 			return err
 		}
 
-		r, err = state.controlClient.ListTasks(
+		stream, err := state.controlClient.ListTasksStream(
 			ctx,
-			&swarmapi.ListTasksRequest{Filters: filters})
+			&swarmapi.ListTasksRequest{Filters: filters},
+		)
+		if err != nil {
+			return err
+		}
+
+		// iterate through the stream
+		for {
+			resp, err := stream.Recv()
+			// io.EOF signifies that the stream is finished
+			if err == io.EOF {
+				return nil
+			}
+			// but any other error signifies a problem
+			if err != nil {
+				return err
+			}
+			if resp == nil {
+				// this should not be possible
+				return errors.New("recieved nil response in stream")
+			}
+
+			// otherwise, add all of the tasks in this response to the total of
+			// all tasks
+			swarmTasks = append(swarmTasks, resp.Tasks...)
+		}
 		return err
 	}); err != nil {
 		return nil, err
 	}
 
-	tasks := make([]types.Task, 0, len(r.Tasks))
-	for _, task := range r.Tasks {
+	tasks := make([]types.Task, 0, len(swarmTasks))
+	for _, task := range swarmTasks {
 		t, err := convert.TaskFromGRPC(*task)
 		if err != nil {
 			return nil, err
