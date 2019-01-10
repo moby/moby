900d981f44fea5e7aee73395d9a9ab400585ff42
Sebastiaan van Stijn <github@gone.nl>
2019-01-10 02:23:38 +0100

    Revert "Bash scripts; use double brackets, fix bare variables, add quotes"
    This reverts commit 297b30df5ff4deaaedb6ceb17d7bd2e306a580ab.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- hack/dockerfile/install/install.sh
+++ hack/dockerfile/install/install.sh
@@ -9,7 +9,7 @@ TMP_GOPATH=${TMP_GOPATH:-""}
 
 : ${PREFIX:="/usr/local/bin"}
 
-if [[ -z "$TMP_GOPATH" ]]; then
+if [ -z "$TMP_GOPATH" ]; then
 	export GOPATH="$(mktemp -d)"
 	RM_GOPATH=1
 else
@@ -21,10 +21,10 @@ dir="$(dirname $0)"
 bin=$1
 shift
 
-if [[ ! -f "${dir}/${bin}.installer" ]]; then
+if [ ! -f "${dir}/${bin}.installer" ]; then
 	echo "Could not find installer for \"$bin\""
 	exit 1
 fi
 
-. ${dir}/${bin}.installer
-install_${bin} "$@"
+. $dir/$bin.installer
+install_$bin "$@"
