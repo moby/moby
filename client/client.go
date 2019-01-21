69d9ff34556d75e6f5d5ce004870e368b525dcba
Sebastiaan van Stijn <github@gone.nl>
2019-01-21 15:01:48 +0100

    Move deprecated client constructors to a separate file
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/client.go
+++ client/client.go
@@ -100,14 +100,6 @@ func CheckRedirect(req *http.Request, via []*http.Request) error {
 	return ErrRedirect
 }
 
-// NewEnvClient initializes a new API client based on environment variables.
-// See FromEnv for a list of support environment variables.
-//
-// Deprecated: use NewClientWithOpts(FromEnv)
-func NewEnvClient() (*Client, error) {
-	return NewClientWithOpts(FromEnv)
-}
-
 // NewClientWithOpts initializes a new API client with default values. It takes functors
 // to modify values when creating it, like `NewClientWithOpts(WithVersion(…))`
 // It also initializes the custom http headers to add to each request.
@@ -165,18 +157,6 @@ func defaultHTTPClient(host string) (*http.Client, error) {
 		Transport:     transport,
 		CheckRedirect: CheckRedirect,
 	}, nil
-}
-
-// NewClient initializes a new API client for the given host and API version.
-// It uses the given http client as transport.
-// It also initializes the custom http headers to add to each request.
-//
-// It won't send any version information if the version number is empty. It is
-// highly recommended that you set a version or your client may break if the
-// server is upgraded.
-// Deprecated: use NewClientWithOpts
-func NewClient(host string, version string, client *http.Client, httpHeaders map[string]string) (*Client, error) {
-	return NewClientWithOpts(WithHost(host), WithVersion(version), WithHTTPClient(client), WithHTTPHeaders(httpHeaders))
 }
 
 // Close the transport used by the client
