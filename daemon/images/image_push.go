4ad44160147a3affd3596918f1612c667a0107e9
Derek McGowan <derek@mcgstyle.net>
2019-02-11 10:38:25 -0800

    Add push support through containerd
    Uses containerd client to push.
    Temporarily resolves current platform on push to match current behavior.
    
    Signed-off-by: Derek McGowan <derek@mcgstyle.net>

--- daemon/images/image_push.go
+++ daemon/images/image_push.go
@@ -2,15 +2,23 @@ package images // import "github.com/docker/docker/daemon/images"
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
+	"sort"
 	"time"
 
-	"github.com/docker/distribution/manifest/schema2"
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/content"
+	"github.com/containerd/containerd/errdefs"
+	"github.com/containerd/containerd/images"
+	"github.com/containerd/containerd/platforms"
 	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/distribution"
 	progressutils "github.com/docker/docker/distribution/utils"
 	"github.com/docker/docker/pkg/progress"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/pkg/errors"
 )
 
 // PushImage initiates a push operation on the repository named localName.
@@ -20,11 +28,29 @@ func (i *ImageService) PushImage(ctx context.Context, image, tag string, metaHea
 	if err != nil {
 		return err
 	}
+	is := i.client.ImageService()
+
+	var imgs []images.Image
 	if tag != "" {
 		// Push by digest is not supported, so only tags are supported.
 		ref, err = reference.WithTag(ref, tag)
 		if err != nil {
 			return err
+		}
+
+		img, err := is.Get(ctx, ref.String())
+		if err != nil {
+			return errors.Wrap(err, "unable to get image")
+		}
+		imgs = append(imgs, img)
+	} else {
+		// TODO(containerd): Escape '.' in ref
+		imgs, err := is.List(ctx, fmt.Sprintf("name~=^%s:.*$", ref.String()))
+		if err != nil {
+			return errors.Wrap(err, "unable to get image")
+		}
+		if len(imgs) == 0 {
+			return errors.Wrap(errdefs.ErrNotFound, "no matching images")
 		}
 	}
 
@@ -41,26 +67,59 @@ func (i *ImageService) PushImage(ctx context.Context, image, tag string, metaHea
 		close(writesDone)
 	}()
 
-	imagePushConfig := &distribution.ImagePushConfig{
-		Config: distribution.Config{
-			MetaHeaders:      metaHeaders,
-			AuthConfig:       authConfig,
-			ProgressOutput:   progress.ChanOutput(progressChan),
-			RegistryService:  i.registryService,
-			ImageEventLogger: i.LogImageEvent,
-			MetadataStore:    i.distributionMetadataStore,
-			ImageStore:       distribution.NewImageConfigStoreFromStore(i.imageStore),
-			ReferenceStore:   i.referenceStore,
-		},
-		ConfigMediaType: schema2.MediaTypeImageConfig,
-		LayerStores:     distribution.NewLayerProvidersFromStores(i.layerStores),
-		TrustKey:        i.trustKey,
-		UploadManager:   i.uploadManager,
+	// TODO(containerd): Handle authConfig
+	// TODO(containerd): Handle metaHeaders
+	opts := []containerd.RemoteOpt{}
+
+	for _, img := range imgs {
+		// TODO(containerd): Check for migrations to do
+
+		switch img.Target.MediaType {
+		case images.MediaTypeDockerSchema2ManifestList, ocispec.MediaTypeImageIndex:
+			// TODO(containerd): Don't resolve manifest, rely on cross manifest push
+			// or push all platforms
+			p, err := content.ReadBlob(ctx, i.client.ContentStore(), img.Target)
+			if err != nil {
+				return errors.Wrap(err, "unable to read manifest list")
+			}
+
+			var idx ocispec.Index
+			if err := json.Unmarshal(p, &idx); err != nil {
+				return err
+			}
+
+			platform := platforms.Default()
+			var descs []ocispec.Descriptor
+			for _, d := range idx.Manifests {
+				if d.Platform == nil || platform.Match(*d.Platform) {
+					descs = append(descs, d)
+				}
+			}
+			if len(descs) > 0 {
+				sort.SliceStable(descs, func(i, j int) bool {
+					if descs[i].Platform == nil {
+						return false
+					}
+					if descs[j].Platform == nil {
+						return true
+					}
+					return platform.Less(*descs[i].Platform, *descs[j].Platform)
+				})
+				img.Target = descs[0]
+			}
+
+		default:
+			// Keep target
+		}
+		if err = i.client.Push(ctx, img.Name, img.Target, opts...); err != nil {
+			err = errors.Wrap(err, "failed to push")
+			break
+		}
 	}
 
-	err = distribution.Push(ctx, ref, imagePushConfig)
-	close(progressChan)
-	<-writesDone
+	//close(progressChan)
+	//<-writesDone
 	imageActions.WithValues("push").UpdateSince(start)
+
 	return err
 }
