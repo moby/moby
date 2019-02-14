201f0fb041601076714f75ca5e59a964300e8cd8
Sebastiaan van Stijn <github@gone.nl>
2019-02-15 00:59:41 +0100

    Explicitly set `rw` / `ro` for mounts
    The current code assumed that mounts were mounted "read-write"
    by default, and added "ro" to make them read-only.
    
    In some situations, omitting the "rw" option could result in
    `sysfs` mounts becoming read-only (possibly inheriting defaults
    from the host).
    
    This patch changes the approach to always set either "ro" or
    "rw", instead of omitting these options (thus ambiguous).
    
    A similar fix was already implemented in containerd/cri
    https://github.com/containerd/cri/pull/763/commits/a5d1332e8fd2b44cd4d00823d38f8e0c8fcf6c5d
    
    Reported-by: Lantao Liu <lantaol@google.com>
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/oci_linux_test.go
+++ daemon/oci_linux_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/docker/docker/container"
 	"github.com/docker/docker/daemon/config"
 	"github.com/docker/docker/daemon/network"
+	"github.com/docker/docker/oci"
 	"github.com/docker/docker/pkg/containerfs"
 	"github.com/docker/docker/pkg/idtools"
 	"github.com/docker/libnetwork"
@@ -147,4 +148,131 @@ func TestGetSourceMount(t *testing.T) {
 	assert.NilError(t, err)
 	_, _, err = getSourceMount(cwd)
 	assert.NilError(t, err)
+}
+
+// TestMountsHaveWriteMode checks that every mount has a write-mode set (either 'ro' or 'rw')
+func TestMountsHaveWriteMode(t *testing.T) {
+	d := Daemon{idMapping: &idtools.IdentityMapping{}}
+	c := &container.Container{HostConfig: &containertypes.HostConfig{}}
+
+	spec := oci.DefaultSpec()
+	err := setMounts(&d, &spec, c, []container.Mount{})
+	assert.Check(t, err)
+
+	for _, m := range spec.Mounts {
+		hasWriteMode := inSlice(m.Options, "rw") || inSlice(m.Options, "ro")
+		assert.Check(t, hasWriteMode, "expected mount to have 'ro' or 'rw': %s", m.Options)
+	}
+}
+
+func TestSetMount(t *testing.T) {
+	d := Daemon{idMapping: &idtools.IdentityMapping{}}
+	c := &container.Container{HostConfig: &containertypes.HostConfig{}}
+	spec := oci.DefaultSpec()
+
+	tests := []struct {
+		name       string
+		hc         containertypes.HostConfig
+		userMounts []container.Mount
+		expected   map[string][]string
+	}{
+		{
+			name: "default",
+			userMounts: []container.Mount{
+				{
+					Source:      "/readonly",
+					Destination: "/readonly",
+					Writable:    false,
+				},
+				{
+					Source:      "/writable",
+					Destination: "/writable",
+					Writable:    true,
+				},
+			},
+			expected: map[string][]string{
+				"/readonly":      {"ro"},
+				"/writable":      {"rw"},
+				"/proc":          {"rw"},
+				"/dev":           {"rw"},
+				"/dev/pts":       {"rw"},
+				"/sys":           {"ro"},
+				"/sys/fs/cgroup": {"ro"},
+				"/dev/mqueue":    {"rw"},
+				"/dev/shm":       {"rw"},
+			},
+		},
+		{
+			name: "privileged",
+			hc:   containertypes.HostConfig{Privileged: true},
+			expected: map[string][]string{
+				"/proc":          {"rw"},
+				"/dev":           {"rw"},
+				"/dev/pts":       {"rw"},
+				"/sys":           {"rw"},
+				"/sys/fs/cgroup": {"rw"},
+				"/dev/mqueue":    {"rw"},
+				"/dev/shm":       {"rw"},
+			},
+		},
+		{
+			name: "readonly",
+			hc:   containertypes.HostConfig{ReadonlyRootfs: true},
+			userMounts: []container.Mount{
+				{
+					Source:      "/readonly",
+					Destination: "/readonly",
+					Writable:    false,
+				},
+				{
+					Source:      "/writable",
+					Destination: "/writable",
+					Writable:    true,
+				},
+			},
+			expected: map[string][]string{
+				"/readonly":      {"ro"},
+				"/writable":      {"rw"},
+				"/proc":          {"rw"},
+				"/dev":           {"rw"},
+				"/dev/pts":       {"rw"},
+				"/sys":           {"ro"},
+				"/sys/fs/cgroup": {"ro"},
+				"/dev/mqueue":    {"rw"},
+				"/dev/shm":       {"rw"},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			c.HostConfig = &tc.hc
+			s := spec
+			if c.HostConfig.ReadonlyRootfs {
+				// TODO why does setMounts look at spec.Root.Readonly, and not at HostConfig?
+				s.Root.Readonly = true
+			}
+			err := setMounts(&d, &s, c, []container.Mount{})
+			assert.Check(t, err)
+
+			for _, m := range spec.Mounts {
+				if expectedOpts, ok := tc.expected[m.Destination]; ok {
+					for _, opt := range expectedOpts {
+						assert.Check(t, inSlice(m.Options, opt), "option %q not found for mount %q", opt, m.Destination)
+					}
+				}
+			}
+
+		})
+	}
+}
+
+// inSlice tests whether a string is contained in a slice of strings or not.
+// Comparison is case sensitive
+func inSlice(slice []string, s string) bool {
+	for _, ss := range slice {
+		if s == ss {
+			return true
+		}
+	}
+	return false
 }
