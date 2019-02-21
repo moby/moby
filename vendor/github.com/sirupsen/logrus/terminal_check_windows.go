12eb9c9e81a40c295c490909c73578aec2904a2b
John Howard <jhoward@microsoft.com>
2019-02-20 16:46:40 -0800

    Vendor sirupsen/logrus@v1.3.0
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- vendor/github.com/sirupsen/logrus/terminal_check_windows.go
+++ vendor/github.com/sirupsen/logrus/terminal_check_windows.go
@@ -1,18 +1,19 @@
-// +build !appengine,!js,!windows,!aix
+// +build !appengine,!js,windows
 
 package logrus
 
 import (
 	"io"
 	"os"
-
-	"golang.org/x/crypto/ssh/terminal"
+	"syscall"
 )
 
 func checkIfTerminal(w io.Writer) bool {
 	switch v := w.(type) {
 	case *os.File:
-		return terminal.IsTerminal(int(v.Fd()))
+		var mode uint32
+		err := syscall.GetConsoleMode(syscall.Handle(v.Fd()), &mode)
+		return err == nil
 	default:
 		return false
 	}
