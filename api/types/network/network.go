ca91918dd49e1ade9145ce955c8eb297f58389fb
Sebastiaan van Stijn <github@gone.nl>
2019-02-28 17:39:57 +0100

    docs follow-ups for networks "dangling" filter
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/types/network/network.go
+++ api/types/network/network.go
@@ -112,13 +112,13 @@ type ConfigReference struct {
 }
 
 var acceptedFilters = map[string]bool{
+	"dangling": true,
 	"driver":   true,
-	"type":     true,
-	"name":     true,
 	"id":       true,
 	"label":    true,
+	"name":     true,
 	"scope":    true,
-	"dangling": true,
+	"type":     true,
 }
 
 // ValidateFilters validates the list of filter args with the available filters.
