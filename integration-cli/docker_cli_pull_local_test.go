17df03f133075ea4a39f424234edb9507f1b792b
Brian Goff <cpuguy83@gmail.com>
2019-02-28 16:07:10 -0800

    Remove Schema1 integration test suite
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- integration-cli/docker_cli_pull_local_test.go
+++ integration-cli/docker_cli_pull_local_test.go
@@ -55,10 +55,6 @@ func (s *DockerRegistrySuite) TestPullImageWithAliases(c *check.C) {
 	testPullImageWithAliases(c)
 }
 
-func (s *DockerSchema1RegistrySuite) TestPullImageWithAliases(c *check.C) {
-	testPullImageWithAliases(c)
-}
-
 // testConcurrentPullWholeRepo pulls the same repo concurrently.
 func testConcurrentPullWholeRepo(c *check.C) {
 	repoName := fmt.Sprintf("%v/dockercli/busybox", privateRegistryURL)
@@ -111,10 +107,6 @@ func (s *DockerRegistrySuite) testConcurrentPullWholeRepo(c *check.C) {
 	testConcurrentPullWholeRepo(c)
 }
 
-func (s *DockerSchema1RegistrySuite) testConcurrentPullWholeRepo(c *check.C) {
-	testConcurrentPullWholeRepo(c)
-}
-
 // testConcurrentFailingPull tries a concurrent pull that doesn't succeed.
 func testConcurrentFailingPull(c *check.C) {
 	repoName := fmt.Sprintf("%v/dockercli/busybox", privateRegistryURL)
@@ -139,10 +131,6 @@ func testConcurrentFailingPull(c *check.C) {
 }
 
 func (s *DockerRegistrySuite) testConcurrentFailingPull(c *check.C) {
-	testConcurrentFailingPull(c)
-}
-
-func (s *DockerSchema1RegistrySuite) testConcurrentFailingPull(c *check.C) {
 	testConcurrentFailingPull(c)
 }
 
@@ -195,10 +183,6 @@ func testConcurrentPullMultipleTags(c *check.C) {
 }
 
 func (s *DockerRegistrySuite) TestConcurrentPullMultipleTags(c *check.C) {
-	testConcurrentPullMultipleTags(c)
-}
-
-func (s *DockerSchema1RegistrySuite) TestConcurrentPullMultipleTags(c *check.C) {
 	testConcurrentPullMultipleTags(c)
 }
 
@@ -259,10 +243,6 @@ func (s *DockerRegistrySuite) TestPullIDStability(c *check.C) {
 	testPullIDStability(c)
 }
 
-func (s *DockerSchema1RegistrySuite) TestPullIDStability(c *check.C) {
-	testPullIDStability(c)
-}
-
 // #21213
 func testPullNoLayers(c *check.C) {
 	repoName := fmt.Sprintf("%v/dockercli/scratch", privateRegistryURL)
@@ -276,10 +256,6 @@ func testPullNoLayers(c *check.C) {
 }
 
 func (s *DockerRegistrySuite) TestPullNoLayers(c *check.C) {
-	testPullNoLayers(c)
-}
-
-func (s *DockerSchema1RegistrySuite) TestPullNoLayers(c *check.C) {
 	testPullNoLayers(c)
 }
 
