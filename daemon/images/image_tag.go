8a682de54ab1facfc83957abf88a1bda3300b5cf
Derek McGowan <derek@mcgstyle.net>
2019-03-04 17:09:35 -0800

    Update pull to add canonical tag
    Update tag to avoid deprecate cache
    
    Signed-off-by: Derek McGowan <derek@mcgstyle.net>

--- daemon/images/image_tag.go
+++ daemon/images/image_tag.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/containerd/containerd/images"
 	"github.com/docker/distribution/reference"
-	"github.com/docker/docker/errdefs"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
 )
@@ -13,14 +12,9 @@ import (
 // TagImage creates the tag specified by newTag, pointing to the image named
 // imageName (alternatively, imageName can also be an image ID).
 func (i *ImageService) TagImage(ctx context.Context, imageName, repository, tag string) (string, error) {
-	img, err := i.getImageByRef(ctx, imageName)
+	desc, err := i.ResolveImage(ctx, imageName)
 	if err != nil {
 		return "", err
-	}
-
-	if img.target == nil {
-		// TODO(containerd): Choose a better target based on other references?
-		img.target = &img.cached.config
 	}
 
 	newTag, err := reference.ParseNormalizedNamed(repository)
@@ -32,43 +26,25 @@ func (i *ImageService) TagImage(ctx context.Context, imageName, repository, tag 
 			return "", err
 		}
 	}
-	img.name = newTag
 
-	// TODO(containerd): Also add image for name@digest
-	err = i.tagImage(ctx, img)
+	err = i.TagImageWithReference(ctx, desc, newTag)
 	return reference.FamiliarString(newTag), err
 }
 
 // TagImageWithReference adds the given reference to the image ID provided.
 func (i *ImageService) TagImageWithReference(ctx context.Context, target ocispec.Descriptor, newTag reference.Named) error {
-	c, err := i.getCache(ctx)
-	if err != nil {
-		return err
-	}
-	ci := c.byTarget(target.Digest)
-	if ci == nil {
-		return errdefs.NotFound(errors.New("target not found"))
-	}
-	// TODO(containerd): Also add image for name@digest
-	return i.tagImage(ctx, imageLink{
-		name:   newTag,
-		target: &target,
-		cached: ci,
-	})
-}
-
-func (i *ImageService) tagImage(ctx context.Context, img imageLink) error {
 	im := images.Image{
-		Name:   img.name.String(),
-		Target: *img.target,
+		Name:   newTag.String(),
+		Target: target,
 	}
+
 	is := i.client.ImageService()
 	_, err := is.Create(ctx, im)
 	if err != nil {
 		return errors.Wrap(err, "failed to create image")
 	}
 
-	// TODO(containerd): Set last updated for target
-	i.LogImageEvent(ctx, img.target.Digest.String(), reference.FamiliarString(img.name), "tag")
-	return i.updateCache(ctx, img)
+	i.LogImageEvent(ctx, target.Digest.String(), reference.FamiliarString(newTag), "tag")
+
+	return nil
 }
