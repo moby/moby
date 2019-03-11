19026f1fd9e92cf00dd1958bd65d3113f4954a55
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2019-03-11 18:42:21 +0300

    add missing ")

--- image/rootfs.go
+++ image/rootfs.go
@@ -38,7 +38,7 @@ func (r *RootFS) Append(id layer.DiffID) {
 func (r *RootFS) Clone() *RootFS {
 	newRoot := NewRootFS()
 	newRoot.Type = r.Type
-	newRoot.DiffIDs = make([]layer.DiffID, len(r.DiffIDs)
+	newRoot.DiffIDs = make([]layer.DiffID, len(r.DiffIDs))
 	copy(newRoot.DiffIDs, r.DiffIDs)
 	return newRoot
 }
