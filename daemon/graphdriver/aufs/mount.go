d1eae8959057eadc9fffe84b59e8ebc20c370e5d
Kir Kolyshkin <kolyshkin@gmail.com>
2019-04-17 19:52:36 -0700

    aufs: use mount.Unmount
    1. Use mount.Unmount() which ignores EINVAL ("not mounted") error,
    and provides better error diagnostics (so we don't have to explicitly
    add target to error messages).
    
    2. Since we're ignoring "not mounted" error, we can call
    multiple unmounts without any locking -- but since "auplink flush"
    is still involved and can produce an error in logs, let's keep
    the check for fs being mounted (it's just a statfs so should be fast).
    
    2. While at it, improve the "can't unmount" error message in Put().
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>
    (cherry picked from commit 4beee98026feabe4f4f0468215b8fd9b56f90d5e)

--- daemon/graphdriver/aufs/mount.go
+++ daemon/graphdriver/aufs/mount.go
@@ -5,7 +5,7 @@ package aufs // import "github.com/docker/docker/daemon/graphdriver/aufs"
 import (
 	"os/exec"
 
-	"golang.org/x/sys/unix"
+	"github.com/docker/docker/pkg/mount"
 )
 
 // Unmount the target specified.
@@ -13,5 +13,5 @@ func Unmount(target string) error {
 	if err := exec.Command("auplink", target, "flush").Run(); err != nil {
 		logger.WithError(err).Warnf("Couldn't run auplink before unmount %s", target)
 	}
-	return unix.Unmount(target, 0)
+	return mount.Unmount(target)
 }
