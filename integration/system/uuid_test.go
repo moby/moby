05bd9958f26269d5f2996d3dd89bb1ab296c14ee
Sebastiaan van Stijn <github@gone.nl>
2019-04-19 20:10:06 +0200

    Skip TestUUIDGeneration on API < v1.40
    Older versions did not use an UUID as ID
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- integration/system/uuid_test.go
+++ integration/system/uuid_test.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"testing"
 
+	"github.com/docker/docker/api/types/versions"
 	"github.com/google/uuid"
 	"gotest.tools/assert"
+	"gotest.tools/skip"
 )
 
 func TestUUIDGeneration(t *testing.T) {
+	skip.If(t, versions.LessThan(testEnv.DaemonAPIVersion(), "1.40"), "ID format changed")
 	defer setupTest(t)()
 
 	c := testEnv.APIClient()
@@ -16,5 +19,5 @@ func TestUUIDGeneration(t *testing.T) {
 	assert.NilError(t, err)
 
 	_, err = uuid.Parse(info.ID)
-	assert.NilError(t, err)
+	assert.NilError(t, err, info.ID)
 }
