fa0e021b9eeb4086933549b727b7b9be31ebf700
Lukas Heeren <lukas-heeren@hotmail.com>
2019-06-24 13:26:14 +0200

    remoed *
    Signed-off-by: Lukas Heeren <lukas-heeren@hotmail.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1055,7 +1055,7 @@ func NewDaemon(ctx context.Context, config *config.Config, pluginStore *plugin.S
 	go d.execCommandGC()
 
 	// set max download attempts 
-	xfer.MaxDownloadAttempts = *config.MaxDownloadAttempts
+	xfer.MaxDownloadAttempts = config.MaxDownloadAttempts
 
 	d.containerd, err = libcontainerd.NewClient(ctx, d.containerdCli, filepath.Join(config.ExecRoot, "containerd"), ContainersNamespace, d)
 	if err != nil {
