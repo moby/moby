045c5c0e5ac96a5595670975da4faf990dcef963
Lukas Heeren <lukas-heeren@hotmail.com>
2019-06-24 14:17:34 +0200

    changed maxDownloadAttempts to MaxDownloadAttempts in download_test.go
    Signed-off-by: Lukas Heeren <lukas-heeren@hotmail.com>
    Signed-off-by: Lukas Heeren <Lukas-heeren@hotmail.com>

--- distribution/xfer/download.go
+++ distribution/xfer/download.go
@@ -284,10 +284,6 @@ func (ldm *LayerDownloadManager) makeDownloadFunc(descriptor DownloadDescriptor,
 				default:
 				}
 				
-				
-				//logrus.Debugf(*MaxDownloadAttempts, &MaxDownloadAttempts, MaxDownloadAttempts)
-				
-				
 				retries++
 				if _, isDNR := err.(DoNotRetry); isDNR || retries == *MaxDownloadAttempts {
 					logrus.Errorf("Download failed: %v", err)
