a63a02fefd32d852b4de100e00a12a3a5803298e
Sebastiaan van Stijn <github@gone.nl>
2019-07-08 18:42:08 +0200

    integration-cli: remove defaultSleepImage constant
    Both Linux and Windows now use busybox, so no need to keep a
    constant for this.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 27f432ca57fb6d4d0409fc3c5358b74feae228cc)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- integration-cli/test_vars_windows_test.go
+++ integration-cli/test_vars_windows_test.go
@@ -8,8 +8,4 @@ const (
 
 	// this is the expected file permission set on windows: gh#11395
 	expectedFileChmod = "-rwxr-xr-x"
-
-	// On Windows, the busybox image doesn't have the `top` command, so we rely
-	// on `sleep` with a high duration.
-	defaultSleepImage = "busybox"
 )
