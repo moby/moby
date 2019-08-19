80d7ea6736223e2afe5815ed826d2f5a8c8ab3de
Yong Tang <yong.tang.github@outlook.com>
2019-08-19 23:57:28 +0000

    Update TestAPIGetEnabledCORS to set the deamon flag,
    This fix updates the TestAPIGetEnabledCORS test, to
    create a new daemon with the right flag.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- integration/system/options_test.go
+++ integration/system/options_test.go
@@ -1,8 +1,14 @@
 package system
 
 import (
+	"net"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"testing"
+	"time"
+
+	"github.com/docker/docker/internal/test/daemon"
 
 	req "github.com/docker/docker/internal/test/request"
 	"gotest.tools/assert"
@@ -17,8 +23,29 @@ func TestAPIOptionsRoute(t *testing.T) {
 }
 
 func TestAPIGetEnabledCORS(t *testing.T) {
-	resp, body, err := req.Get("/version")
+	defer setupTest(t)()
+
+	d := daemon.New(t)
+
+	d.Start(t, "--api-cors-header='*'")
+	defer d.Stop(t)
+
+	daemonURL, err := url.Parse(d.Sock())
 	assert.NilError(t, err)
-	defer body.Close()
+
+	conn, err := net.DialTimeout(daemonURL.Scheme, daemonURL.Path, time.Second*10)
+	assert.NilError(t, err)
+
+	c := httputil.NewClientConn(conn, nil)
+
+	req, err := http.NewRequest("GET", "/_ping", nil)
+	assert.NilError(t, err)
+
+	resp, err := c.Do(req)
+	assert.NilError(t, err)
+
 	assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+	assert.Equal(t, len(resp.Header["Access-Control-Allow-Origin"]), 1)
+	assert.Equal(t, resp.Header["Access-Control-Allow-Origin"][0], "'*'")
 }
