1e26b431c944402e62f0e652362b54ac24925cfc
Tibor Vass <tibor@docker.com>
2019-09-24 17:53:39 +0000

    daemon/config: fix filter type in BuildKit GC config
    For backwards compatibility, the old incorrect object format for
    builder.GC.Rule.Filter still works but is deprecated in favor of array of
    strings akin to what needs to be passed on the CLI.
    
    Signed-off-by: Tibor Vass <tibor@docker.com>
    (cherry picked from commit fbdd437d295595e88466b33a550a8707b9ebb709)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/config/builder.go
+++ daemon/config/builder.go
@@ -1,12 +1,38 @@
 package config
 
-import "github.com/docker/docker/api/types/filters"
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/docker/docker/api/types/filters"
+)
 
 // BuilderGCRule represents a GC rule for buildkit cache
 type BuilderGCRule struct {
-	All         bool         `json:",omitempty"`
-	Filter      filters.Args `json:",omitempty"`
-	KeepStorage string       `json:",omitempty"`
+	All         bool            `json:",omitempty"`
+	Filter      BuilderGCFilter `json:",omitempty"`
+	KeepStorage string          `json:",omitempty"`
+}
+
+type BuilderGCFilter filters.Args
+
+func (x *BuilderGCFilter) UnmarshalJSON(data []byte) error {
+	var arr []string
+	f := filters.NewArgs()
+	if err := json.Unmarshal(data, &arr); err != nil {
+		// backwards compat for deprecated buggy form
+		err := json.Unmarshal(data, &f)
+		*x = BuilderGCFilter(f)
+		return err
+	}
+	for _, s := range arr {
+		fields := strings.SplitN(s, "=", 2)
+		name := strings.ToLower(strings.TrimSpace(fields[0]))
+		value := strings.TrimSpace(fields[1])
+		f.Add(name, value)
+	}
+	*x = BuilderGCFilter(f)
+	return nil
 }
 
 // BuilderGCConfig contains GC config for a buildkit builder
