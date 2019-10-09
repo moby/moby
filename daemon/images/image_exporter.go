tonistiigi/docker

Use containerd exporter
Update load to use full image name annotation

Signed-off-by: Derek McGowan <derek@mcgstyle.net>

daemon/images/image_exporter.go
@@ -1,9 +1,13 @@
 package images // import "github.com/docker/docker/daemon/images"
 
 import (
+	"context"
 	"io"
 
-	"github.com/containerd/containerd/errdefs"
+	"github.com/containerd/containerd/images/archive"
+	digest "github.com/opencontainers/go-digest"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/pkg/errors"
 )
 
 // ExportImage exports a list of images to the given output stream. The
@@ -11,10 +15,39 @@ import (
 // stream. All images with the given tag and all versions containing
 // the same tag are exported. names is the set of tags to export, and
 // outStream is the writer which the images are written to.
-func (i *ImageService) ExportImage(names []string, outStream io.Writer) error {
-	// TODO(containerd): use containerd's archive exporter?
-	// This may require special logic to output the Docker format
-	//imageExporter := tarexport.NewTarExporter(i.imageStore, i.layerStores, i.referenceStore, i)
-	//return imageExporter.Save(names, outStream)
-	return errdefs.ErrNotImplemented
+func (i *ImageService) ExportImage(ctx context.Context, names []string, w io.Writer) error {
+	images := map[digest.Digest]struct {
+		target ocispec.Descriptor
+		names  []string
+	}{}
+
+	for _, name := range names {
+		desc, named, err := i.resolveImageName(ctx, name)
+		if err != nil {
+			return errors.Wrapf(err, "failed to resolve %s to an image", name)
+		}
+		ei, ok := images[desc.Digest]
+		if !ok {
+			ei.target = desc
+		}
+		if named != nil {
+			ei.names = append(ei.names, named.String())
+		}
+		images[desc.Digest] = ei
+	}
+
+	opts := []archive.ExportOpt{
+		archive.WithPlatform(i.platforms),
+	}
+
+	for _, img := range images {
+		if len(img.names) > 0 {
+			opts = append(opts, archive.WithNamedManifest(img.target, img.names...))
+		} else {
+			opts = append(opts, archive.WithManifest(img.target))
+		}
+	}
+
+	// Add each manifest
+	return archive.Export(ctx, i.client.ContentStore(), w, opts...)
 }

moby/moby

daemon/images/image_exporter.go
@@ -1,9 +1,13 @@
 package images // import "github.com/docker/docker/daemon/images"
 
 import (
+	"context"
 	"io"
 
-	"github.com/containerd/containerd/errdefs"
+	"github.com/containerd/containerd/images/archive"
+	digest "github.com/opencontainers/go-digest"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/pkg/errors"
 )
 
 // ExportImage exports a list of images to the given output stream. The
@@ -11,10 +15,35 @@ import (
 // stream. All images with the given tag and all versions containing
 // the same tag are exported. names is the set of tags to export, and
 // outStream is the writer which the images are written to.
-func (i *ImageService) ExportImage(names []string, outStream io.Writer) error {
-	// TODO(containerd): use containerd's archive exporter?
-	// This may require special logic to output the Docker format
-	//imageExporter := tarexport.NewTarExporter(i.imageStore, i.layerStores, i.referenceStore, i)
-	//return imageExporter.Save(names, outStream)
-	return errdefs.ErrNotImplemented
+func (i *ImageService) ExportImage(ctx context.Context, names []string, w io.Writer) error {
+	images := map[digest.Digest]struct {
+		target ocispec.Descriptor
+		names  []string
+	}{}
+
+	for _, name := range names {
+		desc, named, err := i.resolveImageName(ctx, name)
+		if err != nil {
+			return errors.Wrapf(err, "failed to resolve %s to an image", name)
+		}
+		ei, ok := images[desc.Digest]
+		if !ok {
+			ei.target = desc
+		}
+		if named != nil {
+			ei.names = append(ei.names, named.String())
+		}
+		images[desc.Digest] = ei
+	}
+
+	opts := []archive.ExportOpt{
+		archive.WithPlatform(i.platforms),
+	}
+
+	for _, img := range images {
+		opts = append(opts, archive.WithManifest(img.target, img.names...))
+	}
+
+	// Add each manifest
+	return archive.Export(ctx, i.client.ContentStore(), w, opts...)
 }