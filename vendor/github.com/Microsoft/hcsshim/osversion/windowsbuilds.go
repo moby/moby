e2f226b5b41c958fa518f677eb213eb1462f90a8
Vikram bir Singh <vikrambir.singh@docker.com>
2019-11-25 09:58:10 -0800

    Bump hcsshim to b3f49c06ffaeef24d09c6c08ec8ec8425a
    Among other things, this is required to pull in
    microsoft/hcsshim#718
    
    Also fixes microsoft/hcsshim#737
    which was caught by checks while attempting to bump
    up hcsshim version.
    
    Signed-off-by: Vikram bir Singh <vikrambir.singh@docker.com>
    (cherry picked from commit a7b6c3f0bf5d10c6227a29bac7dd46b9a7a779bc)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/Microsoft/hcsshim/osversion/windowsbuilds.go
+++ vendor/github.com/Microsoft/hcsshim/osversion/windowsbuilds.go
@@ -14,10 +14,14 @@ const (
 	RS3 = 16299
 
 	// RS4 (version 1803, codename "Redstone 4") corresponds to Windows Server
-	// 1809 (Semi-Annual Channel (SAC)), and Windows 10 (April 2018 Update).
+	// 1803 (Semi-Annual Channel (SAC)), and Windows 10 (April 2018 Update).
 	RS4 = 17134
 
 	// RS5 (version 1809, codename "Redstone 5") corresponds to Windows Server
 	// 2019 (ltsc2019), and Windows 10 (October 2018 Update).
 	RS5 = 17763
+
+	// V19H1 (version 1903) corresponds to Windows Server 1903 (semi-annual
+	// channel).
+	V19H1 = 18362
 )
