a45b3a92f60324d34cff24265d79a6f5d7fc99c2
Sebastiaan van Stijn <github@gone.nl>
2019-11-27 15:40:23 +0100

    pkg/idtools: normalize comment formatting
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/idtools/utils_unix.go
+++ pkg/idtools/utils_unix.go
@@ -18,8 +18,8 @@ func resolveBinary(binname string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	//only return no error if the final resolved binary basename
-	//matches what was searched for
+	// only return no error if the final resolved binary basename
+	// matches what was searched for
 	if filepath.Base(resolvedPath) == binname {
 		return resolvedPath, nil
 	}
