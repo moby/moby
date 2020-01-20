4d249ae1f33d3e57be78ec1ce573589397720741
Daniel Helfand <helfand.4@gmail.com>
2020-01-20 00:02:09 -0500

    remove extra space in code comment in ping.go
    Signed-off-by: Daniel Helfand <helfand.4@gmail.com>

--- client/ping.go
+++ client/ping.go
@@ -17,7 +17,7 @@ func (cli *Client) Ping(ctx context.Context) (types.Ping, error) {
 	var ping types.Ping
 
 	// Using cli.buildRequest() + cli.doRequest() instead of cli.sendRequest()
-	// because ping requests are used during  API version negotiation, so we want
+	// because ping requests are used during API version negotiation, so we want
 	// to hit the non-versioned /_ping endpoint, not /v1.xx/_ping
 	req, err := cli.buildRequest(http.MethodHead, path.Join(cli.basePath, "/_ping"), nil, nil)
 	if err != nil {
