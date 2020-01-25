tiborvass/docker

First step to implement full garbage collector for image layers
Refactored exiting logic on way that layers are first marked to be under
removal so if actual removal fails they can be found from disk and
cleaned up.

Full garbage collector will be implemented as part of containerd
migration.

Signed-off-by: Olli Janatuinen <olli.janatuinen@gmail.com>

internal/test/daemon/daemon.go
@@ -16,6 +16,7 @@ import (
 	"github.com/tiborvass/docker/api/types"
 	"github.com/tiborvass/docker/api/types/events"
 	"github.com/tiborvass/docker/client"
+	"github.com/tiborvass/docker/daemon/images"
 	"github.com/tiborvass/docker/internal/test"
 	"github.com/tiborvass/docker/internal/test/request"
 	"github.com/tiborvass/docker/opts"
@@ -71,6 +72,7 @@ type Daemon struct {
 	init                       bool
 	dockerdBinary              string
 	log                        logT
+	imageService               *images.ImageService
 
 	// swarm related field
 	swarmListenAddr string
@@ -699,4 +701,9 @@ func cleanupRaftDir(t testingT, rootPath string) {
 	if err := os.RemoveAll(walDir); err != nil {
 		t.Logf("error removing %v: %v", walDir, err)
 	}
+}
+
+// ImageService returns the Daemon's ImageService
+func (d *Daemon) ImageService() *images.ImageService {
+	return d.imageService
 }

moby/moby

(cherry picked from commit 213681b66a41d7728445e30125176834ce349c2a)
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

internal/test/daemon/daemon.go
@@ -16,6 +16,7 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/daemon/images"
 	"github.com/docker/docker/internal/test"
 	"github.com/docker/docker/internal/test/request"
 	"github.com/docker/docker/opts"
@@ -77,6 +78,7 @@ type Daemon struct {
 	init          bool
 	dockerdBinary string
 	log           logT
+	imageService  *images.ImageService
 
 	// swarm related field
 	swarmListenAddr string
@@ -719,4 +721,9 @@ func cleanupRaftDir(t testingT, rootPath string) {
 			t.Logf("error removing %v: %v", dir, err)
 		}
 	}
+}
+
+// ImageService returns the Daemon's ImageService
+func (d *Daemon) ImageService() *images.ImageService {
+	return d.imageService
 }