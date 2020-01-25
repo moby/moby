tiborvass/docker

testutil: make testing packages public
This was done with something along the lines of:

```
mv internal/test testutil
pushd testutil/; grep -IRl "package test" | xargs -I '{}' sed -i -e 's|package test|package testutil|g' {}; popd
mv internal/testutil/*.go testutil/ && rm -rf internal/
grep -IRl "github.com\/docker\/docker\/internal\/test" | xargs -I '{}' sed -i -e 's|github.com/docker/docker/internal/test|github.com/docker/docker/test|g' {}
goimports .
```

I also modified the basic plugin path in testutil/fixtures/plugin.

Signed-off-by: Sam Whited <sam@samwhited.com>

integration-cli/check_test.go
@@ -19,13 +19,13 @@ import (
 	"github.com/tiborvass/docker/integration-cli/cli"
 	"github.com/tiborvass/docker/integration-cli/daemon"
 	"github.com/tiborvass/docker/integration-cli/environment"
-	testdaemon "github.com/tiborvass/docker/internal/test/daemon"
-	ienv "github.com/tiborvass/docker/internal/test/environment"
-	"github.com/tiborvass/docker/internal/test/fakestorage"
-	"github.com/tiborvass/docker/internal/test/fixtures/plugin"
-	"github.com/tiborvass/docker/internal/test/registry"
 	"github.com/tiborvass/docker/internal/test/suite"
 	"github.com/tiborvass/docker/pkg/reexec"
+	testdaemon "github.com/tiborvass/docker/testutil/daemon"
+	ienv "github.com/tiborvass/docker/testutil/environment"
+	"github.com/tiborvass/docker/testutil/fakestorage"
+	"github.com/tiborvass/docker/testutil/fixtures/plugin"
+	"github.com/tiborvass/docker/testutil/registry"
 	"gotest.tools/assert"
 )
 

integration/service/plugin_test.go
@@ -14,9 +14,9 @@ import (
 	swarmtypes "github.com/tiborvass/docker/api/types/swarm"
 	"github.com/tiborvass/docker/api/types/swarm/runtime"
 	"github.com/tiborvass/docker/integration/internal/swarm"
-	"github.com/tiborvass/docker/internal/test/daemon"
-	"github.com/tiborvass/docker/internal/test/fixtures/plugin"
-	"github.com/tiborvass/docker/internal/test/registry"
+	"github.com/tiborvass/docker/testutil/daemon"
+	"github.com/tiborvass/docker/testutil/fixtures/plugin"
+	"github.com/tiborvass/docker/testutil/registry"
 	"gotest.tools/assert"
 	"gotest.tools/poll"
 	"gotest.tools/skip"

testutil/daemon/ops.go
@@ -1,0 +1,79 @@
+package daemon
+
+import "github.com/tiborvass/docker/testutil/environment"
+
+// WithDefaultCgroupNamespaceMode sets the default cgroup namespace mode for the daemon
+func WithDefaultCgroupNamespaceMode(mode string) func(*Daemon) {
+	return func(d *Daemon) {
+		d.defaultCgroupNamespaceMode = mode
+	}
+}
+
+// WithExperimental sets the daemon in experimental mode
+func WithExperimental(d *Daemon) {
+	d.experimental = true
+	d.init = true
+}
+
+// WithInit sets the daemon init
+func WithInit(d *Daemon) {
+	d.init = true
+}
+
+// WithDockerdBinary sets the dockerd binary to the specified one
+func WithDockerdBinary(dockerdBinary string) func(*Daemon) {
+	return func(d *Daemon) {
+		d.dockerdBinary = dockerdBinary
+	}
+}
+
+// WithSwarmPort sets the swarm port to use for swarm mode
+func WithSwarmPort(port int) func(*Daemon) {
+	return func(d *Daemon) {
+		d.SwarmPort = port
+	}
+}
+
+// WithSwarmListenAddr sets the swarm listen addr to use for swarm mode
+func WithSwarmListenAddr(listenAddr string) func(*Daemon) {
+	return func(d *Daemon) {
+		d.swarmListenAddr = listenAddr
+	}
+}
+
+// WithSwarmDefaultAddrPool sets the swarm default address pool to use for swarm mode
+func WithSwarmDefaultAddrPool(defaultAddrPool []string) func(*Daemon) {
+	return func(d *Daemon) {
+		d.DefaultAddrPool = defaultAddrPool
+	}
+}
+
+// WithSwarmDefaultAddrPoolSubnetSize sets the subnet length mask of swarm default address pool to use for swarm mode
+func WithSwarmDefaultAddrPoolSubnetSize(subnetSize uint32) func(*Daemon) {
+	return func(d *Daemon) {
+		d.SubnetSize = subnetSize
+	}
+}
+
+// WithSwarmDataPathPort sets the  swarm datapath port to use for swarm mode
+func WithSwarmDataPathPort(datapathPort uint32) func(*Daemon) {
+	return func(d *Daemon) {
+		d.DataPathPort = datapathPort
+	}
+}
+
+// WithEnvironment sets options from testutil/environment.Execution struct
+func WithEnvironment(e environment.Execution) func(*Daemon) {
+	return func(d *Daemon) {
+		if e.DaemonInfo.ExperimentalBuild {
+			d.experimental = true
+		}
+	}
+}
+
+// WithStorageDriver sets store driver option
+func WithStorageDriver(driver string) func(d *Daemon) {
+	return func(d *Daemon) {
+		d.storageDriver = driver
+	}
+}

moby/moby

(cherry picked from commit b37c214e3c8c4ff5450bd5fc1225d53827d240e7)
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

integration-cli/check_test.go
@@ -18,13 +18,13 @@ import (
 	"github.com/docker/docker/integration-cli/cli"
 	"github.com/docker/docker/integration-cli/daemon"
 	"github.com/docker/docker/integration-cli/environment"
-	testdaemon "github.com/docker/docker/internal/test/daemon"
-	ienv "github.com/docker/docker/internal/test/environment"
-	"github.com/docker/docker/internal/test/fakestorage"
-	"github.com/docker/docker/internal/test/fixtures/plugin"
-	"github.com/docker/docker/internal/test/registry"
 	"github.com/docker/docker/internal/test/suite"
 	"github.com/docker/docker/pkg/reexec"
+	testdaemon "github.com/docker/docker/testutil/daemon"
+	ienv "github.com/docker/docker/testutil/environment"
+	"github.com/docker/docker/testutil/fakestorage"
+	"github.com/docker/docker/testutil/fixtures/plugin"
+	"github.com/docker/docker/testutil/registry"
 	"gotest.tools/assert"
 )
 

integration/service/plugin_test.go
@@ -12,9 +12,9 @@ import (
 	swarmtypes "github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/api/types/swarm/runtime"
 	"github.com/docker/docker/integration/internal/swarm"
-	"github.com/docker/docker/internal/test/daemon"
-	"github.com/docker/docker/internal/test/fixtures/plugin"
-	"github.com/docker/docker/internal/test/registry"
+	"github.com/docker/docker/testutil/daemon"
+	"github.com/docker/docker/testutil/fixtures/plugin"
+	"github.com/docker/docker/testutil/registry"
 	"gotest.tools/assert"
 	"gotest.tools/poll"
 	"gotest.tools/skip"

testutil/daemon/ops.go
@@ -1,0 +1,72 @@
+package daemon
+
+import "github.com/docker/docker/testutil/environment"
+
+// WithExperimental sets the daemon in experimental mode
+func WithExperimental(d *Daemon) {
+	d.experimental = true
+	d.init = true
+}
+
+// WithInit sets the daemon init
+func WithInit(d *Daemon) {
+	d.init = true
+}
+
+// WithDockerdBinary sets the dockerd binary to the specified one
+func WithDockerdBinary(dockerdBinary string) func(*Daemon) {
+	return func(d *Daemon) {
+		d.dockerdBinary = dockerdBinary
+	}
+}
+
+// WithSwarmPort sets the swarm port to use for swarm mode
+func WithSwarmPort(port int) func(*Daemon) {
+	return func(d *Daemon) {
+		d.SwarmPort = port
+	}
+}
+
+// WithSwarmListenAddr sets the swarm listen addr to use for swarm mode
+func WithSwarmListenAddr(listenAddr string) func(*Daemon) {
+	return func(d *Daemon) {
+		d.swarmListenAddr = listenAddr
+	}
+}
+
+// WithSwarmDefaultAddrPool sets the swarm default address pool to use for swarm mode
+func WithSwarmDefaultAddrPool(defaultAddrPool []string) func(*Daemon) {
+	return func(d *Daemon) {
+		d.DefaultAddrPool = defaultAddrPool
+	}
+}
+
+// WithSwarmDefaultAddrPoolSubnetSize sets the subnet length mask of swarm default address pool to use for swarm mode
+func WithSwarmDefaultAddrPoolSubnetSize(subnetSize uint32) func(*Daemon) {
+	return func(d *Daemon) {
+		d.SubnetSize = subnetSize
+	}
+}
+
+// WithSwarmDataPathPort sets the  swarm datapath port to use for swarm mode
+func WithSwarmDataPathPort(datapathPort uint32) func(*Daemon) {
+	return func(d *Daemon) {
+		d.DataPathPort = datapathPort
+	}
+}
+
+// WithEnvironment sets options from testutil/environment.Execution struct
+func WithEnvironment(e environment.Execution) func(*Daemon) {
+	return func(d *Daemon) {
+		if e.DaemonInfo.ExperimentalBuild {
+			d.experimental = true
+		}
+	}
+}
+
+// WithStorageDriver sets store driver option
+func WithStorageDriver(driver string) func(d *Daemon) {
+	return func(d *Daemon) {
+		d.storageDriver = driver
+	}
+}