7b575f98132b9a61662382d9fcc1afba7254c29c
Sebastiaan van Stijn <github@gone.nl>
2020-01-29 18:36:06 +0100

    vendor: update golang.org/x/crypto 69ecbb4d6d5dab05e49161c6e77ea40a030884e1
    full diff: https://github.com/golang/crypto/compare/88737f569e3a9c7ab309cdc09a07fe7fc87233c3...69ecbb4d6d5dab05e49161c6e77ea40a030884e1
    
    Includes https://github.com/golang/crypto/commit/69ecbb4d6d5dab05e49161c6e77ea40a030884e1
    (forward-port of https://github.com/golang/crypto/commit/8b5121be2f68d8fc40bb06467003bdde1040a094),
    which fixes CVE-2020-7919:
    
    - Panic in crypto/x509 certificate parsing and golang.org/x/crypto/cryptobyte
      On 32-bit architectures, a malformed input to crypto/x509 or the ASN.1 parsing
      functions of golang.org/x/crypto/cryptobyte can lead to a panic.
      The malformed certificate can be delivered via a crypto/tls connection to a
      client, or to a server that accepts client certificates. net/http clients can
      be made to crash by an HTTPS server, while net/http servers that accept client
      certificates will recover the panic and are unaffected.
      Thanks to Project Wycheproof for providing the test cases that led to the
      discovery of this issue. The issue is CVE-2020-7919 and Go issue golang.org/issue/36837.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit b606c8e440144b0ae7c18b5d9a7b557ef3ddd9d9)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/golang.org/x/crypto/poly1305/sum_ppc64le.go
+++ vendor/golang.org/x/crypto/poly1305/sum_ppc64le.go
@@ -1,8 +1,8 @@
-// Copyright 2012 The Go Authors. All rights reserved.
+// Copyright 2019 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build amd64,!gccgo,!appengine
+// +build ppc64le,!gccgo,!appengine
 
 package poly1305
 
