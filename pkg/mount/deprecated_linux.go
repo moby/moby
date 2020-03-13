59c0495409a52a720dc1a5177bdd0941148ee63e
Kir Kolyshkin <kolyshkin@gmail.com>
2020-03-13 15:51:55 -0700

    pkg/mount: make it shallow
    Make pkg/mount a shallow package, relying on
    github.com/moby/sys/mount and github.com/moby/sys/mountinfo
    
    The plan is to
     - switch the rest of this repo to use moby/sys directly
     - add deprecation notice to pkg/mount
     - (eventually) remove pkg/mount
    
    The nolint:golint annotation is added to suppress warnings like
    "exported XXX should have comment or be unexported".
    
    The ForceMount function is deprecated (and is identical to Mount),
    so let's not use it (but still provide an alias for those who do).
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

---
+++ pkg/mount/deprecated_linux.go
@@ -0,0 +1,18 @@
+package mount // import "github.com/docker/docker/pkg/mount"
+
+import (
+	sysmount "github.com/moby/sys/mount"
+)
+
+//nolint:golint
+var (
+	MakeMount       = sysmount.MakeMount
+	MakeShared      = sysmount.MakeShared
+	MakeRShared     = sysmount.MakeRShared
+	MakePrivate     = sysmount.MakePrivate
+	MakeRPrivate    = sysmount.MakeRPrivate
+	MakeSlave       = sysmount.MakeSlave
+	MakeRSlave      = sysmount.MakeRSlave
+	MakeUnbindable  = sysmount.MakeUnbindable
+	MakeRUnbindable = sysmount.MakeRUnbindable
+)
