tiborvass/docker

Add some tests to the volume store
Signed-off-by: Brian Goff <cpuguy83@gmail.com>

volume/store/db.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/boltdb/bolt"
+	"github.com/tiborvass/docker/errdefs"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -28,7 +29,10 @@ func setMeta(tx *bolt.Tx, name string, meta volumeMetadata) error {
 	if err != nil {
 		return err
 	}
-	b := tx.Bucket(volumeBucketName)
+	b, err := tx.CreateBucketIfNotExists(volumeBucketName)
+	if err != nil {
+		return errors.Wrap(err, "error creating volume bucket")
+	}
 	return errors.Wrap(b.Put([]byte(name), metaJSON), "error setting volume metadata")
 }
 
@@ -42,8 +46,11 @@ func (s *VolumeStore) getMeta(name string) (volumeMetadata, error) {
 
 func getMeta(tx *bolt.Tx, name string, meta *volumeMetadata) error {
 	b := tx.Bucket(volumeBucketName)
+	if b == nil {
+		return errdefs.NotFound(errors.New("volume bucket does not exist"))
+	}
 	val := b.Get([]byte(name))
-	if string(val) == "" {
+	if len(val) == 0 {
 		return nil
 	}
 	if err := json.Unmarshal(val, meta); err != nil {

volume/store/restore_test.go
@@ -1,0 +1,55 @@
+package store
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/tiborvass/docker/volume"
+	volumedrivers "github.com/tiborvass/docker/volume/drivers"
+	volumetestutils "github.com/tiborvass/docker/volume/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRestore(t *testing.T) {
+	t.Parallel()
+
+	dir, err := ioutil.TempDir("", "test-restore")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	driverName := "test-restore"
+	volumedrivers.Register(volumetestutils.NewFakeDriver(driverName), driverName)
+	defer volumedrivers.Unregister("test-restore")
+
+	s, err := New(dir)
+	require.NoError(t, err)
+	defer s.Shutdown()
+
+	_, err = s.Create("test1", driverName, nil, nil)
+	require.NoError(t, err)
+
+	testLabels := map[string]string{"a": "1"}
+	testOpts := map[string]string{"foo": "bar"}
+	_, err = s.Create("test2", driverName, testOpts, testLabels)
+	require.NoError(t, err)
+
+	s.Shutdown()
+
+	s, err = New(dir)
+	require.NoError(t, err)
+
+	v, err := s.Get("test1")
+	require.NoError(t, err)
+
+	dv := v.(volume.DetailedVolume)
+	var nilMap map[string]string
+	require.Equal(t, nilMap, dv.Options())
+	require.Equal(t, nilMap, dv.Labels())
+
+	v, err = s.Get("test2")
+	require.NoError(t, err)
+	dv = v.(volume.DetailedVolume)
+	require.Equal(t, testOpts, dv.Options())
+	require.Equal(t, testLabels, dv.Labels())
+}

moby/moby

(cherry picked from commit 834d0e262ac248191c09bcdb2b86ee92edb6aaf0)
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

volume/store/db.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/boltdb/bolt"
+	"github.com/docker/docker/errdefs"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -28,7 +29,10 @@ func setMeta(tx *bolt.Tx, name string, meta volumeMetadata) error {
 	if err != nil {
 		return err
 	}
-	b := tx.Bucket(volumeBucketName)
+	b, err := tx.CreateBucketIfNotExists(volumeBucketName)
+	if err != nil {
+		return errors.Wrap(err, "error creating volume bucket")
+	}
 	return errors.Wrap(b.Put([]byte(name), metaJSON), "error setting volume metadata")
 }
 
@@ -42,8 +46,11 @@ func (s *VolumeStore) getMeta(name string) (volumeMetadata, error) {
 
 func getMeta(tx *bolt.Tx, name string, meta *volumeMetadata) error {
 	b := tx.Bucket(volumeBucketName)
+	if b == nil {
+		return errdefs.NotFound(errors.New("volume bucket does not exist"))
+	}
 	val := b.Get([]byte(name))
-	if string(val) == "" {
+	if len(val) == 0 {
 		return nil
 	}
 	if err := json.Unmarshal(val, meta); err != nil {

volume/store/restore_test.go
@@ -1,0 +1,55 @@
+package store
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/docker/docker/volume"
+	volumedrivers "github.com/docker/docker/volume/drivers"
+	volumetestutils "github.com/docker/docker/volume/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRestore(t *testing.T) {
+	t.Parallel()
+
+	dir, err := ioutil.TempDir("", "test-restore")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	driverName := "test-restore"
+	volumedrivers.Register(volumetestutils.NewFakeDriver(driverName), driverName)
+	defer volumedrivers.Unregister("test-restore")
+
+	s, err := New(dir)
+	require.NoError(t, err)
+	defer s.Shutdown()
+
+	_, err = s.Create("test1", driverName, nil, nil)
+	require.NoError(t, err)
+
+	testLabels := map[string]string{"a": "1"}
+	testOpts := map[string]string{"foo": "bar"}
+	_, err = s.Create("test2", driverName, testOpts, testLabels)
+	require.NoError(t, err)
+
+	s.Shutdown()
+
+	s, err = New(dir)
+	require.NoError(t, err)
+
+	v, err := s.Get("test1")
+	require.NoError(t, err)
+
+	dv := v.(volume.DetailedVolume)
+	var nilMap map[string]string
+	require.Equal(t, nilMap, dv.Options())
+	require.Equal(t, nilMap, dv.Labels())
+
+	v, err = s.Get("test2")
+	require.NoError(t, err)
+	dv = v.(volume.DetailedVolume)
+	require.Equal(t, testOpts, dv.Options())
+	require.Equal(t, testLabels, dv.Labels())
+}