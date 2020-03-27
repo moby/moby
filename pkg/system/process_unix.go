573ff4994afcf8f4d09c68f1fcd68df77d9996d5
屈骏 <qujun@tiduyun.com>
2020-03-27 10:58:11 +0800

    add zombie check when kill container
    Signed-off-by: 屈骏 <qujun@tiduyun.com>

--- pkg/system/process_unix.go
+++ pkg/system/process_unix.go
@@ -3,6 +3,9 @@
 package system // import "github.com/docker/docker/pkg/system"
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
 	"syscall"
 
 	"golang.org/x/sys/unix"
@@ -21,4 +24,22 @@ func IsProcessAlive(pid int) bool {
 // KillProcess force-stops a process.
 func KillProcess(pid int) {
 	unix.Kill(pid, unix.SIGKILL)
+}
+
+
+// IsProcessZombie return true if process has a state with "Z"
+// http://man7.org/linux/man-pages/man5/proc.5.html
+func IsProcessZombie(pid int) (bool, error) {
+	statPath := fmt.Sprintf("/proc/%d/stat", 31391)
+	dataBytes, err := ioutil.ReadFile(statPath)
+	if err != nil {
+		return false, err
+	}
+	data := string(dataBytes)
+	sdata := strings.Split(data, " ")
+	if len(sdata) >= 3 && sdata[2] == "S" {
+		return true, nil
+	}else {
+		return false, nil
+	}
 }
