6796c2cc9b1d454208cb1e9f249b2c70a78f5a7d
Sebastiaan van Stijn <github@gone.nl>
2020-06-04 12:48:37 +0200

    client/mflag: remove use of docker/docker/pkg/homedir
    The homedir package was only used to print default values for
    flags that contained paths inside the user's home-directory in
    a slightly nicer way (replace `/users/home` with `~`).
    
    Given that this is not critical, we can replace this with golang's
    function, which does not depend on libcontainer.
    
    There's still one use of the homedir package in docker/docker/opts,
    which is used by the dnet binary (but only requires the homedir
    package when running in rootless mode)
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libnetwork/vendor/github.com/docker/docker/pkg/system/meminfo_unsupported.go
+++
@@ -1,8 +0,0 @@
-// +build !linux,!windows
-
-package system // import "github.com/docker/docker/pkg/system"
-
-// ReadMemInfo is not supported on platforms other than linux and windows.
-func ReadMemInfo() (*MemInfo, error) {
-	return nil, ErrNotSupportedPlatform
-}
