64e852ee6501d95e820ed304b24296cc8e988fc6
Sebastiaan van Stijn <github@gone.nl>
2020-08-31 18:46:57 +0200

    vendor github.com/Microsoft/hcsshim 9dcb42f100215f8d375b4a9265e5bba009217a85
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libnetwork/vendor/github.com/Microsoft/hcsshim/internal/schema1/schema1.go
+++ libnetwork/vendor/github.com/Microsoft/hcsshim/internal/schema1/schema1.go
@@ -214,9 +214,10 @@ type MappedVirtualDiskController struct {
 
 // GuestDefinedCapabilities is part of the GuestConnectionInfo returned by a GuestConnection call on a utility VM
 type GuestDefinedCapabilities struct {
-	NamespaceAddRequestSupported bool `json:",omitempty"`
-	SignalProcessSupported       bool `json:",omitempty"`
-	DumpStacksSupported          bool `json:",omitempty"`
+	NamespaceAddRequestSupported  bool `json:",omitempty"`
+	SignalProcessSupported        bool `json:",omitempty"`
+	DumpStacksSupported           bool `json:",omitempty"`
+	DeleteContainerStateSupported bool `json:",omitempty"`
 }
 
 // GuestConnectionInfo is the structure of an iterm return by a GuestConnection call on a utility VM
