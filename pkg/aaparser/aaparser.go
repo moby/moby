ecd3baca25918936ffdda5dec9c55314b96a9c3b
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-10-02 17:33:52 +0900

    pkg/aaparser: support parsing version like "3.0.0-beta1"
    Fix #41517
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
    (cherry picked from commit ee079e4692c26417eb4c711a1e489203624a6972)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/aaparser/aaparser.go
+++ pkg/aaparser/aaparser.go
@@ -56,6 +56,9 @@ func parseVersion(output string) (int, error) {
 	words := strings.Split(lines[0], " ")
 	version := words[len(words)-1]
 
+	// trim "-beta1" suffix from version="3.0.0-beta1" if exists
+	version = strings.SplitN(version, "-", 2)[0]
+
 	// split by major minor version
 	v := strings.Split(version, ".")
 	if len(v) == 0 || len(v) > 3 {
