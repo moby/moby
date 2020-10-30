053962af9259be9a4ce8f50894a1a5ada9387322
Sebastiaan van Stijn <github@gone.nl>
2020-10-30 21:17:34 +0100

    Move HostGatewayName const to opts, and change vars to consts
    This prevents consumers of the opts package to also having to
    depend on daemon/network, and everything related.
    
    We can probably change some of the other constants to strings,
    for easier concatenating, and need to review the windows-specific
    "127.0.0.1" (instead of "localhost"), which may no longer be
    needed.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/network/constants.go
+++
@@ -1,8 +0,0 @@
-package network
-
-const (
-	// HostGatewayName is the string value that can be passed
-	// to the IPAddr section in --add-host that is replaced by
-	// the value of HostGatewayIP daemon config value
-	HostGatewayName = "host-gateway"
-)
