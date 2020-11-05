080d9df63e87d3b6ee081c04f9734c26bed68f27
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2020-11-05 21:33:17 +1100

    Don't clear the exit code from the Finally block
    It turns out that the Finally block does not see the exit code from the
    `exit` call that triggered it, but from an earlier state. And it seems
    that actions take in the Finally block other than `exit` will not affect
    the $LastErrorCode set by the `exit` that triggered the Finally block.
    
    Signed-off-by: Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>

--- hack/ci/windows.ps1
+++ hack/ci/windows.ps1
@@ -1012,9 +1012,6 @@ Catch [Exception] {
     Throw $_
 }
 Finally {
-    # Preserve the LastExitCode of the tests
-    $tmpLastExitCode = $LastExitCode
-
     $ErrorActionPreference="SilentlyContinue"
     $global:ProgressPreference=$origProgressPreference
     Write-Host  -ForegroundColor Green "INFO: Tidying up at end of run"
@@ -1053,6 +1050,4 @@ Finally {
 
     $Dur=New-TimeSpan -Start $StartTime -End $(Get-Date)
     Write-Host -ForegroundColor $FinallyColour "`nINFO: executeCI.ps1 exiting at $(date). Duration $dur`n"
-
-    exit $tmpLastExitCode
 }
