e1d10be45e283d8a14efcfb3ca3c51f648b414c0
Sebastiaan van Stijn <github@gone.nl>
2020-11-05 12:42:11 +0100

    pkg/mount: fix compile failure on Windows (move linux-only stubs0
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/mount/deprecated_linux.go
+++ pkg/mount/deprecated_linux.go
@@ -7,13 +7,45 @@ import (
 // Deprecated: use github.com/moby/sys/mount instead.
 //nolint:golint
 var (
-	MakeMount       = sysmount.MakeMount
-	MakeShared      = sysmount.MakeShared
-	MakeRShared     = sysmount.MakeRShared
-	MakePrivate     = sysmount.MakePrivate
-	MakeRPrivate    = sysmount.MakeRPrivate
-	MakeSlave       = sysmount.MakeSlave
-	MakeRSlave      = sysmount.MakeRSlave
-	MakeUnbindable  = sysmount.MakeUnbindable
-	MakeRUnbindable = sysmount.MakeRUnbindable
+	ForceMount        = sysmount.Mount // a deprecated synonym
+	MakeMount         = sysmount.MakeMount
+	MakePrivate       = sysmount.MakePrivate
+	MakeRPrivate      = sysmount.MakeRPrivate
+	MakeRShared       = sysmount.MakeRShared
+	MakeRSlave        = sysmount.MakeRSlave
+	MakeRUnbindable   = sysmount.MakeRUnbindable
+	MakeShared        = sysmount.MakeShared
+	MakeSlave         = sysmount.MakeSlave
+	MakeUnbindable    = sysmount.MakeUnbindable
+	MergeTmpfsOptions = sysmount.MergeTmpfsOptions
+	Mount             = sysmount.Mount
+	RecursiveUnmount  = sysmount.RecursiveUnmount
+	Unmount           = sysmount.Unmount
+)
+
+// Deprecated: use github.com/moby/sys/mount instead.
+//nolint:golint
+const (
+	RDONLY      = sysmount.RDONLY
+	NOSUID      = sysmount.NOSUID
+	NOEXEC      = sysmount.NOEXEC
+	SYNCHRONOUS = sysmount.SYNCHRONOUS
+	NOATIME     = sysmount.NOATIME
+	BIND        = sysmount.BIND
+	DIRSYNC     = sysmount.DIRSYNC
+	MANDLOCK    = sysmount.MANDLOCK
+	NODEV       = sysmount.NODEV
+	NODIRATIME  = sysmount.NODIRATIME
+	UNBINDABLE  = sysmount.UNBINDABLE
+	RUNBINDABLE = sysmount.RUNBINDABLE
+	PRIVATE     = sysmount.PRIVATE
+	RPRIVATE    = sysmount.RPRIVATE
+	SHARED      = sysmount.SHARED
+	RSHARED     = sysmount.RSHARED
+	SLAVE       = sysmount.SLAVE
+	RSLAVE      = sysmount.RSLAVE
+	RBIND       = sysmount.RBIND
+	RELATIME    = sysmount.RELATIME
+	REMOUNT     = sysmount.REMOUNT
+	STRICTATIME = sysmount.STRICTATIME
 )
