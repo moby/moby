9bf93e90fa9ce74b6f7309a22838da8189838b3b
Tibor Vass <tibor@docker.com>
2021-02-25 01:14:37 +0000

    builder: fix incorrect cache match for inline cache with empty layers
    See https://github.com/moby/buildkit/pull/1993
    
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- builder/builder-next/adapters/localinlinecache/inlinecache.go
+++ builder/builder-next/adapters/localinlinecache/inlinecache.go
@@ -22,6 +22,11 @@ import (
 	"github.com/pkg/errors"
 )
 
+func init() {
+	// See https://github.com/moby/buildkit/pull/1993.
+	v1.EmptyLayerRemovalSupported = false
+}
+
 // ResolveCacheImporterFunc returns a resolver function for local inline cache
 func ResolveCacheImporterFunc(sm *session.Manager, resolverFunc docker.RegistryHosts, cs content.Store, rs reference.Store, is imagestore.Store) remotecache.ResolveCacheImporterFunc {
 
