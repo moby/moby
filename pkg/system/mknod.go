f0f731e483b6ee5ac4ac111d51fd621f3d4b2b72
Artem Khramov <akhramov@pm.me>
2021-09-19 13:58:57 +0300

    Port pkg/system/mknod.go to FreeBSD
    Because FreeBSD uses 64-bit device nodes (see
    https://reviews.freebsd.org/rS318736), Linux implementation of
    `system.Mknod` & `system.Mkdev` is not sufficient.
    
    This change adds freebsd-specific implementations for `Mknod` and
    Mkdev`.
    
    Signed-off-by: Artem Khramov <akhramov@pm.me>

--- pkg/system/mknod.go
+++ pkg/system/mknod.go
@@ -7,12 +7,6 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// Mknod creates a filesystem node (file, device special file or named pipe) named path
-// with attributes specified by mode and dev.
-func Mknod(path string, mode uint32, dev int) error {
-	return unix.Mknod(path, mode, dev)
-}
-
 // Mkdev is used to build the value of linux devices (in /dev/) which specifies major
 // and minor number of the newly created device special file.
 // Linux device nodes are a bit weird due to backwards compat with 16 bit device nodes.
