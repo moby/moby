docker/docker

new security-opt: privileged-without-host-devices
`docker run --runtime=kata --privileged` is insecure despite of Kata's
VM isolation because host devices are visible to the container.

This commit adds a new security-opt `privileged-without-host-devices` to
allow privileged mode without mounting host devices.
The daemon returns an error if the opt is specified but privileged is
not specified.

A common use-case of this is to run Docker-in-Docker securely.

Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

daemon/oci_linux.go
@@ -856,8 +856,10 @@ func WithDevices(daemon *Daemon, c *container.Container) coci.SpecOpts {
 		// Build lists of devices allowed and created within the container.
 		var devs []specs.LinuxDevice
 		devPermissions := s.Linux.Resources.Devices
-
-		if c.HostConfig.Privileged && !rsystem.RunningInUserNS() {
+		if c.PrivilegedWithoutHostDevices && !c.HostConfig.Privileged {
+			return errors.New("privileged-without-host-devices requires privileged mode to be enabled")
+		}
+		if c.HostConfig.Privileged && !rsystem.RunningInUserNS() && !c.PrivilegedWithoutHostDevices {
 			hostDevices, err := devices.HostDevices()
 			if err != nil {
 				return err

moby/moby

daemon/oci_linux.go
@@ -873,7 +873,10 @@ func WithDevices(daemon *Daemon, c *container.Container) coci.SpecOpts {
 		var devs []specs.LinuxDevice
 		devPermissions := s.Linux.Resources.Devices
 
-		if c.HostConfig.Privileged && !userns.RunningInUserNS() {
+		if c.PrivilegedWithoutHostDevices && !c.HostConfig.Privileged {
+			return errors.New("privileged-without-host-devices requires privileged mode to be enabled")
+		}
+		if c.HostConfig.Privileged && !userns.RunningInUserNS() && !c.PrivilegedWithoutHostDevices {
 			hostDevices, err := devices.HostDevices()
 			if err != nil {
 				return err