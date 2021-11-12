amiraliucsc/Docker

Fix HTTP status-code when loading invalid tar
Before this change:

    echo "hello world, I am not a tar" | curl -v --unix-socket  /var/run/docker.sock -H "Content-Type: application/x-tar"  --data-binary @- -X POST http://localhost/v1.29/images/load
    *   Trying /var/run/docker.sock...
    * Connected to localhost (/Users/sebastiaan/Library/Containers/com.dock) port 80 (#0)
    > POST /v1.29/images/load HTTP/1.1
    > Host: localhost
    > User-Agent: curl/7.51.0
    > Accept: */*
    > Content-Type: application/x-tar
    > Content-Length: 28
    >
    * upload completely sent off: 28 out of 28 bytes
    < HTTP/1.1 200 OK
    < Api-Version: 1.31
    < Content-Type: application/json
    < Date: Mon, 07 Aug 2017 15:11:35 GMT
    < Docker-Experimental: true
    < Ostype: linux
    < Server: Docker/17.07.0-ce-rc1 (linux)
    < Transfer-Encoding: chunked
    <
    {"errorDetail":{"message":"Error processing tar file(exit status 1): unexpected EOF"},"error":"Error processing tar file(exit status 1): unexpected EOF"}
    * Curl_http_done: called premature == 0
    * Connection #0 to host localhost left intact

After this change:

    echo "hello world, I am not a tar" | curl -v --unix-socket  /var/run/docker.sock -H "Content-Type: application/x-tar"  --data-binary @- http://localhost/v1.29/images/load
    *   Trying /var/run/docker.sock...
    * Connected to localhost (/var/run/docker.sock) port 80 (#0)
    > POST /v1.29/images/load HTTP/1.1
    > Host: localhost
    > User-Agent: curl/7.55.0
    > Accept: */*
    > Content-Type: application/x-tar
    > Content-Length: 28
    >
    * upload completely sent off: 28 out of 28 bytes
    < HTTP/1.1 400 Bad Request
    < Api-Version: 1.32
    < Content-Type: application/json
    < Docker-Experimental: false
    < Ostype: linux
    < Server: Docker/17.06.0-dev (linux)
    < Date: Thu, 17 Aug 2017 11:14:55 GMT
    < Transfer-Encoding: chunked
    <
    Api-Version: 1.32
    Content-Type: application/json
    Docker-Experimental

api/server/router/image/image_routes.go
@@ -179,6 +179,9 @@ func (s *imageRouter) postImagesLoad(ctx context.Context, w http.ResponseWriter,
 	output := ioutils.NewWriteFlusher(w)
 	defer output.Close()
 	if err := s.backend.LoadImage(r.Body, output, quiet); err != nil {
+		if !output.Flushed() {
+			w.WriteHeader(httputils.GetHTTPErrorStatusCode(err))
+		}
 		output.Write(streamformatter.FormatError(err))
 	}
 	return nil

moby/moby

api/server/router/image/image_routes.go
@@ -173,6 +173,9 @@ func (s *imageRouter) postImagesLoad(ctx context.Context, w http.ResponseWriter,
 	output := ioutils.NewWriteFlusher(w)
 	defer output.Close()
 	if err := s.backend.LoadImage(r.Body, output, quiet); err != nil {
+		if !output.Flushed() {
+			w.WriteHeader(errdefs.GetHTTPErrorStatusCode(err))
+		}
 		_, _ = output.Write(streamformatter.FormatError(err))
 	}
 	return nil