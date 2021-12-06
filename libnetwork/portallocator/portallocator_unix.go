414dd017b3836c816a2cf74cfe53ed66ca940c4d
Albin Kerouanton <albinker@gmail.com>
2021-12-06 12:26:32 +0100

    Revert "Added API to set ephemeral port allocator range."
    Since commit 2c4a868f64e6e13bf06589a6670122196651e82e, Docker doesn't
    use the value of net.ipv4.ip_local_port_range when choosing an ephemeral
    port. This change reverts back to the previous behavior.
    
    Fixes #43054.
    
    Signed-off-by: Albin Kerouanton <albinker@gmail.com>

--- libnetwork/portallocator/portallocator_unix.go
+++ libnetwork/portallocator/portallocator_unix.go
@@ -4,10 +4,10 @@
 package portallocator
 
 const (
-	// defaultPortRangeStart indicates the first port in port range
-	defaultPortRangeStart = 49153
-	// defaultPortRangeEnd indicates the last port in port range
+	// DefaultPortRangeStart indicates the first port in port range
+	DefaultPortRangeStart = 49153
+	// DefaultPortRangeEnd indicates the last port in port range
 	// consistent with default /proc/sys/net/ipv4/ip_local_port_range
 	// upper bound on linux
-	defaultPortRangeEnd = 60999
+	DefaultPortRangeEnd = 65535
 )
--- libnetwork/portallocator/portallocator_windows.go
+++ libnetwork/portallocator/portallocator_windows.go
@@ -1,12 +1,12 @@
 package portallocator
 
 const (
-	// defaultPortRangeStart indicates the first port in port range
-	defaultPortRangeStart = 60000
-	// defaultPortRangeEnd indicates the last port in port range
-	defaultPortRangeEnd = 65000
+	// DefaultPortRangeStart indicates the first port in port range
+	DefaultPortRangeStart = 60000
+	// DefaultPortRangeEnd indicates the last port in port range
+	DefaultPortRangeEnd = 65000
 )
 
 func getDynamicPortRange() (start int, end int, err error) {
-	return defaultPortRangeStart, defaultPortRangeEnd, nil
+	return DefaultPortRangeStart, DefaultPortRangeEnd, nil
 }
