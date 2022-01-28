12744335c08d110c6066cb729a95d14658c6f1d2
Cory Snider <csnider@mirantis.com>
2022-01-28 13:13:54 -0500

    logger/journald: simplify build constraints
    Ensure the package can be imported, no matter the build constratints, by
    adding an unconstrained doc.go containing a package statement.
    
    Signed-off-by: Cory Snider <csnider@mirantis.com>

--- daemon/logger/journald/journald.go
+++ daemon/logger/journald/journald.go
@@ -1,8 +1,6 @@
 //go:build linux
 // +build linux
 
-// Package journald provides the log driver for forwarding server logs
-// to endpoints that receive the systemd format.
 package journald // import "github.com/docker/docker/daemon/logger/journald"
 
 import (
--- daemon/logger/journald/journald_unsupported.go
+++
@@ -1,7 +0,0 @@
-//go:build !linux
-// +build !linux
-
-package journald // import "github.com/docker/docker/daemon/logger/journald"
-
-type journald struct {
-}
