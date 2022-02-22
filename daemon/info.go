amiraliucsc/Docker

Add DNS configuration to /info endpoint
The `/info` endpoint currently doesn't show what DNS options
are configured on the daemon. This information can be
useful, for example in situations where a "local" DNS server
is configured (which may not work if the IP address of the
local DNS overlaps with the container's IP range).

This patch adds the DNS configuration that is set on the daemon
to the output of the `/info` endpoint.

Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

daemon/info.go
@@ -130,6 +130,11 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 		ServerVersion:      dockerversion.Version,
 		ClusterStore:       daemon.configStore.ClusterStore,
 		ClusterAdvertise:   daemon.configStore.ClusterAdvertise,
+		DNSDefaults: types.DNSInfo{
+			DNSServers: daemon.configStore.DNS,
+			DNSOptions: daemon.configStore.DNSOptions,
+			DNSSearch:  daemon.configStore.DNSSearch,
+		},
 		HTTPProxy:          sockets.GetProxyEnv("http_proxy"),
 		HTTPSProxy:         sockets.GetProxyEnv("https_proxy"),
 		NoProxy:            sockets.GetProxyEnv("no_proxy"),

moby/moby

daemon/info.go
@@ -63,6 +63,11 @@ func (daemon *Daemon) SystemInfo() *types.Info {
 		Labels:             daemon.configStore.Labels,
 		ExperimentalBuild:  daemon.configStore.Experimental,
 		ServerVersion:      dockerversion.Version,
+		DNSDefaults: types.DNSInfo{
+			DNS:        daemon.configStore.DNS,
+			DNSOptions: daemon.configStore.DNSOptions,
+			DNSSearch:  daemon.configStore.DNSSearch,
+		},
 		HTTPProxy:          config.MaskCredentials(getConfigOrEnv(daemon.configStore.HTTPProxy, "HTTP_PROXY", "http_proxy")),
 		HTTPSProxy:         config.MaskCredentials(getConfigOrEnv(daemon.configStore.HTTPSProxy, "HTTPS_PROXY", "https_proxy")),
 		NoProxy:            getConfigOrEnv(daemon.configStore.NoProxy, "NO_PROXY", "no_proxy"),