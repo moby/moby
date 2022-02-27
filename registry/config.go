d9261561f997f299fcc6a1838a358f70a35a15ec
Sebastiaan van Stijn <github@gone.nl>
2022-02-27 13:38:22 +0100

    registry: add TODO for removing ParseSearchIndexInfo()
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- registry/config.go
+++ registry/config.go
@@ -432,6 +432,11 @@ func ParseRepositoryInfo(reposName reference.Named) (*RepositoryInfo, error) {
 }
 
 // ParseSearchIndexInfo will use repository name to get back an indexInfo.
+//
+// TODO(thaJeztah) this function is only used by the CLI, and used to get
+// information of the registry (to provide credentials if needed). We should
+// move this function (or equivalent) to the CLI, as it's doing too much just
+// for that.
 func ParseSearchIndexInfo(reposName string) (*registrytypes.IndexInfo, error) {
 	indexName, _ := splitReposSearchTerm(reposName)
 
