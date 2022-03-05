3bb2d0026b9353e02a011fc64c821534bd1cae36
Sebastiaan van Stijn <github@gone.nl>
2022-03-05 23:55:51 +0100

    api: rename container.ContainerCreateCreatedBody to container.CreateResponse
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/types/container/deprecated.go
+++ api/types/container/deprecated.go
@@ -1,5 +1,10 @@
 package container // import "github.com/docker/docker/api/types/container"
 
+// ContainerCreateCreatedBody OK response to ContainerCreate operation
+//
+// Deprecated: use CreateResponse
+type ContainerCreateCreatedBody = CreateResponse
+
 // ContainerWaitOKBody OK response to ContainerWait operation
 //
 // Deprecated: use WaitResponse
