b4c97810b410119a79fc024ed40fc02db5df9ea2
Kitae Kim <superkkt@gmail.com>
2022-03-05 19:57:37 +0900

    Fix incorrect operating system information reported on Windows
    Signed-off-by: Kitae Kim <superkkt@gmail.com>

--- pkg/parsers/operatingsystem/operatingsystem_windows.go
+++ pkg/parsers/operatingsystem/operatingsystem_windows.go
@@ -2,43 +2,80 @@ package operatingsystem // import "github.com/docker/docker/pkg/parsers/operatin
 
 import (
 	"fmt"
+	"syscall"
+	"unsafe"
 
 	"github.com/Microsoft/hcsshim/osversion"
+	"golang.org/x/sys/windows"
 	"golang.org/x/sys/windows/registry"
+)
+
+var (
+	libWinbrand          = windows.NewLazySystemDLL("winbrand.dll")
+	libKernel32          = windows.NewLazySystemDLL("kernel32.dll")
+	brandingFormatString = libWinbrand.NewProc("BrandingFormatString")
+	globalFree           = libKernel32.NewProc("GlobalFree")
 )
 
 // GetOperatingSystem gets the name of the current operating system.
 func GetOperatingSystem() (string, error) {
-	os, err := withCurrentVersionRegistryKey(func(key registry.Key) (os string, err error) {
-		if os, _, err = key.GetStringValue("ProductName"); err != nil {
-			return "", err
-		}
+	os, err := callBrandingFormatString()
+	if err != nil {
+		// Default return value
+		return "Unknown Operating System", nil
+	}
 
-		releaseId, _, err := key.GetStringValue("ReleaseId")
-		if err != nil {
-			return
+	version, err := withCurrentVersionRegistryKey(func(key registry.Key) (version string, err error) {
+		version, _, err = key.GetStringValue("DisplayVersion")
+		if err != nil || version == "" {
+			// Fallback.
+			version, _, err = key.GetStringValue("ReleaseId")
+			if err != nil {
+				return "", err
+			}
 		}
-		os = fmt.Sprintf("%s Version %s", os, releaseId)
-
 		buildNumber, _, err := key.GetStringValue("CurrentBuildNumber")
 		if err != nil {
-			return
+			return "", err
 		}
 		ubr, _, err := key.GetIntegerValue("UBR")
 		if err != nil {
-			return
+			return "", err
 		}
-		os = fmt.Sprintf("%s (OS Build %s.%d)", os, buildNumber, ubr)
 
-		return
+		return fmt.Sprintf("Version %s (OS Build %s.%d)", version, buildNumber, ubr), nil
 	})
-
-	if os == "" {
-		// Default return value
-		os = "Unknown Operating System"
+	if err != nil {
+		return "", err
 	}
 
-	return os, err
+	return fmt.Sprintf("%s %s", os, version), nil
+}
+
+func callBrandingFormatString() (string, error) {
+	if err := brandingFormatString.Find(); err != nil {
+		return "", err
+	}
+
+	arg, err := windows.UTF16PtrFromString("%WINDOWS_LONG%")
+	if err != nil {
+		return "", err
+	}
+	r1, _, err := brandingFormatString.Call(uintptr(unsafe.Pointer(arg)))
+	if err != syscall.Errno(0) {
+		return "", err
+	}
+	defer callGlobalFree(r1)
+
+	return windows.UTF16PtrToString((*uint16)(unsafe.Pointer(r1))), nil
+}
+
+func callGlobalFree(v uintptr) {
+	// Just in case.
+	if err := globalFree.Find(); err != nil {
+		return
+	}
+	globalFree.Call(v)
 }
 
 func withCurrentVersionRegistryKey(f func(registry.Key) (string, error)) (string, error) {
