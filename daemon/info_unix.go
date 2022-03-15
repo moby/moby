docker/docker

version: add RootlessKit, slirp4netns, and VPNKit version
```console
$ docker --context=rootless version
...
Server:
...
 rootlesskit:
  Version:          0.14.2
  ApiVersion:       1.1.1
  NetworkDriver:    slirp4netns
  PortDriver:       builtin
  StateDir:         /tmp/rootlesskit245426514
 slirp4netns:
  Version:          1.1.9
  GitCommit:        4e37ea557562e0d7a64dc636eff156f64927335e
```

Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

daemon/info_unix.go
@@ -13,6 +13,7 @@ import (
 	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/dockerversion"
 	"github.com/docker/docker/pkg/sysinfo"
+	"github.com/docker/docker/rootless"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -216,6 +217,62 @@ func (daemon *Daemon) fillPlatformVersion(v *types.Version) {
 		}
 	} else {
 		logrus.Warnf("failed to retrieve %s version: %s", defaultInitBinary, err)
+	}
+
+	daemon.fillRootlessVersion(v)
+}
+
+func (daemon *Daemon) fillRootlessVersion(v *types.Version) {
+	if !rootless.RunningWithRootlessKit() {
+		return
+	}
+	rlc, err := rootless.GetRootlessKitClient()
+	if err != nil {
+		logrus.Warnf("failed to retrieve RootlessKit version: %s", err)
+		return
+	}
+	rlInfo, err := rlc.Info(context.TODO())
+	if err != nil {
+		logrus.Warnf("failed to retrieve RootlessKit version: %s", err)
+		return
+	}
+	v.Components = append(v.Components, types.ComponentVersion{
+		Name:    "rootlesskit",
+		Version: rlInfo.Version,
+		Details: map[string]string{
+			"ApiVersion":    rlInfo.APIVersion,
+			"StateDir":      rlInfo.StateDir,
+			"NetworkDriver": rlInfo.NetworkDriver.Driver,
+			"PortDriver":    rlInfo.PortDriver.Driver,
+		},
+	})
+
+	switch rlInfo.NetworkDriver.Driver {
+	case "slirp4netns":
+		if rv, err := exec.Command("slirp4netns", "--version").Output(); err == nil {
+			if _, ver, commit, err := parseRuntimeVersion(string(rv)); err != nil {
+				logrus.Warnf("failed to parse slirp4netns version: %v", err)
+			} else {
+				v.Components = append(v.Components, types.ComponentVersion{
+					Name:    "slirp4netns",
+					Version: ver,
+					Details: map[string]string{
+						"GitCommit": commit,
+					},
+				})
+			}
+		} else {
+			logrus.Warnf("failed to retrieve slirp4netns version: %v", err)
+		}
+	case "vpnkit":
+		if rv, err := exec.Command("vpnkit", "--version").Output(); err == nil {
+			v.Components = append(v.Components, types.ComponentVersion{
+				Name:    "vpnkit",
+				Version: strings.TrimSpace(string(rv)),
+			})
+		} else {
+			logrus.Warnf("failed to retrieve vpnkit version: %v", err)
+		}
 	}
 }
 

vendor/github.com/rootless-containers/rootlesskit/pkg/api/client/client.go
@@ -1,0 +1,214 @@
+package client
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/pkg/errors"
+
+	"github.com/rootless-containers/rootlesskit/pkg/api"
+	"github.com/rootless-containers/rootlesskit/pkg/port"
+)
+
+type Client interface {
+	HTTPClient() *http.Client
+	PortManager() port.Manager
+	Info(context.Context) (*api.Info, error)
+}
+
+// New creates a client.
+// socketPath is a path to the UNIX socket, without unix:// prefix.
+func New(socketPath string) (Client, error) {
+	if _, err := os.Stat(socketPath); err != nil {
+		return nil, err
+	}
+	hc := &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
+				var d net.Dialer
+				return d.DialContext(ctx, "unix", socketPath)
+			},
+		},
+	}
+	return NewWithHTTPClient(hc), nil
+}
+
+func NewWithHTTPClient(hc *http.Client) Client {
+	return &client{
+		Client:    hc,
+		version:   "v1",
+		dummyHost: "rootlesskit",
+	}
+}
+
+type client struct {
+	*http.Client
+	// version is always "v1"
+	// TODO(AkihiroSuda): negotiate the version
+	version   string
+	dummyHost string
+}
+
+func (c *client) HTTPClient() *http.Client {
+	return c.Client
+}
+
+func (c *client) PortManager() port.Manager {
+	return &portManager{
+		client: c,
+	}
+}
+
+func (c *client) Info(ctx context.Context) (*api.Info, error) {
+	u := fmt.Sprintf("http://%s/%s/info", c.dummyHost, c.version)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	resp, err := c.HTTPClient().Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := successful(resp); err != nil {
+		return nil, err
+	}
+	var info api.Info
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&info); err != nil {
+		return nil, err
+	}
+	return &info, nil
+}
+
+func readAtMost(r io.Reader, maxBytes int) ([]byte, error) {
+	lr := &io.LimitedReader{
+		R: r,
+		N: int64(maxBytes),
+	}
+	b, err := ioutil.ReadAll(lr)
+	if err != nil {
+		return b, err
+	}
+	if lr.N == 0 {
+		return b, errors.Errorf("expected at most %d bytes, got more", maxBytes)
+	}
+	return b, nil
+}
+
+// HTTPStatusErrorBodyMaxLength specifies the maximum length of HTTPStatusError.Body
+const HTTPStatusErrorBodyMaxLength = 64 * 1024
+
+// HTTPStatusError is created from non-2XX HTTP response
+type HTTPStatusError struct {
+	// StatusCode is non-2XX status code
+	StatusCode int
+	// Body is at most HTTPStatusErrorBodyMaxLength
+	Body string
+}
+
+// Error implements error.
+// If e.Body is a marshalled string of api.ErrorJSON, Error returns ErrorJSON.Message .
+// Otherwise Error returns a human-readable string that contains e.StatusCode and e.Body.
+func (e *HTTPStatusError) Error() string {
+	if e.Body != "" && len(e.Body) < HTTPStatusErrorBodyMaxLength {
+		var ej api.ErrorJSON
+		if json.Unmarshal([]byte(e.Body), &ej) == nil {
+			return ej.Message
+		}
+	}
+	return fmt.Sprintf("unexpected HTTP status %s, body=%q", http.StatusText(e.StatusCode), e.Body)
+}
+
+func successful(resp *http.Response) error {
+	if resp == nil {
+		return errors.New("nil response")
+	}
+	if resp.StatusCode/100 != 2 {
+		b, _ := readAtMost(resp.Body, HTTPStatusErrorBodyMaxLength)
+		return &HTTPStatusError{
+			StatusCode: resp.StatusCode,
+			Body:       string(b),
+		}
+	}
+	return nil
+}
+
+type portManager struct {
+	*client
+}
+
+func (pm *portManager) AddPort(ctx context.Context, spec port.Spec) (*port.Status, error) {
+	m, err := json.Marshal(spec)
+	if err != nil {
+		return nil, err
+	}
+	u := fmt.Sprintf("http://%s/%s/ports", pm.client.dummyHost, pm.client.version)
+	req, err := http.NewRequest("POST", u, bytes.NewReader(m))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req = req.WithContext(ctx)
+	resp, err := pm.client.HTTPClient().Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := successful(resp); err != nil {
+		return nil, err
+	}
+	dec := json.NewDecoder(resp.Body)
+	var status port.Status
+	if err := dec.Decode(&status); err != nil {
+		return nil, err
+	}
+	return &status, nil
+}
+func (pm *portManager) ListPorts(ctx context.Context) ([]port.Status, error) {
+	u := fmt.Sprintf("http://%s/%s/ports", pm.client.dummyHost, pm.client.version)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	resp, err := pm.client.HTTPClient().Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := successful(resp); err != nil {
+		return nil, err
+	}
+	var statuses []port.Status
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&statuses); err != nil {
+		return nil, err
+	}
+	return statuses, nil
+}
+func (pm *portManager) RemovePort(ctx context.Context, id int) error {
+	u := fmt.Sprintf("http://%s/%s/ports/%d", pm.client.dummyHost, pm.client.version, id)
+	req, err := http.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return err
+	}
+	req = req.WithContext(ctx)
+	resp, err := pm.client.HTTPClient().Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if err := successful(resp); err != nil {
+		return err
+	}
+	return nil
+}

moby/moby

daemon/info_unix.go
@@ -13,6 +13,7 @@ import (
 	"github.com/docker/docker/api/types"
 	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/pkg/sysinfo"
+	"github.com/docker/docker/rootless"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -207,6 +208,62 @@ func (daemon *Daemon) fillPlatformVersion(v *types.Version) {
 		}
 	} else {
 		logrus.Warnf("failed to retrieve %s version: %s", defaultInitBinary, err)
+	}
+
+	daemon.fillRootlessVersion(v)
+}
+
+func (daemon *Daemon) fillRootlessVersion(v *types.Version) {
+	if !rootless.RunningWithRootlessKit() {
+		return
+	}
+	rlc, err := rootless.GetRootlessKitClient()
+	if err != nil {
+		logrus.Warnf("failed to create RootlessKit client: %v", err)
+		return
+	}
+	rlInfo, err := rlc.Info(context.TODO())
+	if err != nil {
+		logrus.Warnf("failed to retrieve RootlessKit version: %v", err)
+		return
+	}
+	v.Components = append(v.Components, types.ComponentVersion{
+		Name:    "rootlesskit",
+		Version: rlInfo.Version,
+		Details: map[string]string{
+			"ApiVersion":    rlInfo.APIVersion,
+			"StateDir":      rlInfo.StateDir,
+			"NetworkDriver": rlInfo.NetworkDriver.Driver,
+			"PortDriver":    rlInfo.PortDriver.Driver,
+		},
+	})
+
+	switch rlInfo.NetworkDriver.Driver {
+	case "slirp4netns":
+		if rv, err := exec.Command("slirp4netns", "--version").Output(); err == nil {
+			if _, ver, commit, err := parseRuntimeVersion(string(rv)); err != nil {
+				logrus.Warnf("failed to parse slirp4netns version: %v", err)
+			} else {
+				v.Components = append(v.Components, types.ComponentVersion{
+					Name:    "slirp4netns",
+					Version: ver,
+					Details: map[string]string{
+						"GitCommit": commit,
+					},
+				})
+			}
+		} else {
+			logrus.Warnf("failed to retrieve slirp4netns version: %v", err)
+		}
+	case "vpnkit":
+		if rv, err := exec.Command("vpnkit", "--version").Output(); err == nil {
+			v.Components = append(v.Components, types.ComponentVersion{
+				Name:    "vpnkit",
+				Version: strings.TrimSpace(string(rv)),
+			})
+		} else {
+			logrus.Warnf("failed to retrieve vpnkit version: %v", err)
+		}
 	}
 }
 

vendor/github.com/rootless-containers/rootlesskit/pkg/api/client/client.go
@@ -1,0 +1,212 @@
+package client
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/rootless-containers/rootlesskit/pkg/api"
+	"github.com/rootless-containers/rootlesskit/pkg/port"
+)
+
+type Client interface {
+	HTTPClient() *http.Client
+	PortManager() port.Manager
+	Info(context.Context) (*api.Info, error)
+}
+
+// New creates a client.
+// socketPath is a path to the UNIX socket, without unix:// prefix.
+func New(socketPath string) (Client, error) {
+	if _, err := os.Stat(socketPath); err != nil {
+		return nil, err
+	}
+	hc := &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
+				var d net.Dialer
+				return d.DialContext(ctx, "unix", socketPath)
+			},
+		},
+	}
+	return NewWithHTTPClient(hc), nil
+}
+
+func NewWithHTTPClient(hc *http.Client) Client {
+	return &client{
+		Client:    hc,
+		version:   "v1",
+		dummyHost: "rootlesskit",
+	}
+}
+
+type client struct {
+	*http.Client
+	// version is always "v1"
+	// TODO(AkihiroSuda): negotiate the version
+	version   string
+	dummyHost string
+}
+
+func (c *client) HTTPClient() *http.Client {
+	return c.Client
+}
+
+func (c *client) PortManager() port.Manager {
+	return &portManager{
+		client: c,
+	}
+}
+
+func (c *client) Info(ctx context.Context) (*api.Info, error) {
+	u := fmt.Sprintf("http://%s/%s/info", c.dummyHost, c.version)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	resp, err := c.HTTPClient().Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := successful(resp); err != nil {
+		return nil, err
+	}
+	var info api.Info
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&info); err != nil {
+		return nil, err
+	}
+	return &info, nil
+}
+
+func readAtMost(r io.Reader, maxBytes int) ([]byte, error) {
+	lr := &io.LimitedReader{
+		R: r,
+		N: int64(maxBytes),
+	}
+	b, err := io.ReadAll(lr)
+	if err != nil {
+		return b, err
+	}
+	if lr.N == 0 {
+		return b, fmt.Errorf("expected at most %d bytes, got more", maxBytes)
+	}
+	return b, nil
+}
+
+// HTTPStatusErrorBodyMaxLength specifies the maximum length of HTTPStatusError.Body
+const HTTPStatusErrorBodyMaxLength = 64 * 1024
+
+// HTTPStatusError is created from non-2XX HTTP response
+type HTTPStatusError struct {
+	// StatusCode is non-2XX status code
+	StatusCode int
+	// Body is at most HTTPStatusErrorBodyMaxLength
+	Body string
+}
+
+// Error implements error.
+// If e.Body is a marshalled string of api.ErrorJSON, Error returns ErrorJSON.Message .
+// Otherwise Error returns a human-readable string that contains e.StatusCode and e.Body.
+func (e *HTTPStatusError) Error() string {
+	if e.Body != "" && len(e.Body) < HTTPStatusErrorBodyMaxLength {
+		var ej api.ErrorJSON
+		if json.Unmarshal([]byte(e.Body), &ej) == nil {
+			return ej.Message
+		}
+	}
+	return fmt.Sprintf("unexpected HTTP status %s, body=%q", http.StatusText(e.StatusCode), e.Body)
+}
+
+func successful(resp *http.Response) error {
+	if resp == nil {
+		return errors.New("nil response")
+	}
+	if resp.StatusCode/100 != 2 {
+		b, _ := readAtMost(resp.Body, HTTPStatusErrorBodyMaxLength)
+		return &HTTPStatusError{
+			StatusCode: resp.StatusCode,
+			Body:       string(b),
+		}
+	}
+	return nil
+}
+
+type portManager struct {
+	*client
+}
+
+func (pm *portManager) AddPort(ctx context.Context, spec port.Spec) (*port.Status, error) {
+	m, err := json.Marshal(spec)
+	if err != nil {
+		return nil, err
+	}
+	u := fmt.Sprintf("http://%s/%s/ports", pm.client.dummyHost, pm.client.version)
+	req, err := http.NewRequest("POST", u, bytes.NewReader(m))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req = req.WithContext(ctx)
+	resp, err := pm.client.HTTPClient().Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := successful(resp); err != nil {
+		return nil, err
+	}
+	dec := json.NewDecoder(resp.Body)
+	var status port.Status
+	if err := dec.Decode(&status); err != nil {
+		return nil, err
+	}
+	return &status, nil
+}
+func (pm *portManager) ListPorts(ctx context.Context) ([]port.Status, error) {
+	u := fmt.Sprintf("http://%s/%s/ports", pm.client.dummyHost, pm.client.version)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	resp, err := pm.client.HTTPClient().Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := successful(resp); err != nil {
+		return nil, err
+	}
+	var statuses []port.Status
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&statuses); err != nil {
+		return nil, err
+	}
+	return statuses, nil
+}
+func (pm *portManager) RemovePort(ctx context.Context, id int) error {
+	u := fmt.Sprintf("http://%s/%s/ports/%d", pm.client.dummyHost, pm.client.version, id)
+	req, err := http.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return err
+	}
+	req = req.WithContext(ctx)
+	resp, err := pm.client.HTTPClient().Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if err := successful(resp); err != nil {
+		return err
+	}
+	return nil
+}