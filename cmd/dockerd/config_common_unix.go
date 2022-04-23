92d9e892b53e9193726c1a0135b3050865252e05
Sebastiaan van Stijn <github@gone.nl>
2022-04-23 17:23:24 +0200

    cmd/dockerd: combine installUnixConfigFlags with installConfigFlags
    installConfigFlags already has separate implementations for Linux and
    Windows, so no need to further differentiate.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cmd/dockerd/config_common_unix.go
+++ cmd/dockerd/config_common_unix.go
@@ -6,11 +6,7 @@ package main
 import (
 	"path/filepath"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/daemon/config"
-	"github.com/docker/docker/opts"
 	"github.com/docker/docker/pkg/homedir"
-	"github.com/spf13/pflag"
 )
 
 func getDefaultPidFile() (string, error) {
@@ -44,20 +40,4 @@ func getDefaultExecRoot() (string, error) {
 		return "", err
 	}
 	return filepath.Join(runtimeDir, "docker"), nil
-}
-
-// installUnixConfigFlags adds command-line options to the top-level flag parser for
-// the current process that are common across Unix platforms.
-func installUnixConfigFlags(conf *config.Config, flags *pflag.FlagSet) {
-	conf.Runtimes = make(map[string]types.Runtime)
-
-	flags.StringVarP(&conf.SocketGroup, "group", "G", "docker", "Group for the unix socket")
-	flags.StringVar(&conf.BridgeConfig.IP, "bip", "", "Specify network bridge IP")
-	flags.StringVarP(&conf.BridgeConfig.Iface, "bridge", "b", "", "Attach containers to a network bridge")
-	flags.StringVar(&conf.BridgeConfig.FixedCIDR, "fixed-cidr", "", "IPv4 subnet for fixed IPs")
-	flags.Var(opts.NewIPOpt(&conf.BridgeConfig.DefaultGatewayIPv4, ""), "default-gateway", "Container default gateway IPv4 address")
-	flags.Var(opts.NewIPOpt(&conf.BridgeConfig.DefaultGatewayIPv6, ""), "default-gateway-v6", "Container default gateway IPv6 address")
-	flags.BoolVar(&conf.BridgeConfig.InterContainerCommunication, "icc", true, "Enable inter-container communication")
-	flags.Var(opts.NewIPOpt(&conf.BridgeConfig.DefaultIP, "0.0.0.0"), "ip", "Default IP when binding container ports")
-	flags.Var(opts.NewNamedRuntimeOpt("runtimes", &conf.Runtimes, config.StockRuntimeName), "add-runtime", "Register an additional OCI compatible runtime")
 }
--- cmd/dockerd/config_unix.go
+++ cmd/dockerd/config_unix.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 
 	"github.com/containerd/cgroups"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/daemon/config"
 	"github.com/docker/docker/opts"
 	"github.com/docker/docker/pkg/homedir"
@@ -25,16 +26,16 @@ func installConfigFlags(conf *config.Config, flags *pflag.FlagSet) error {
 		return err
 	}
 
-	// Then install flags common to unix platforms
-	installUnixConfigFlags(conf, flags)
-
 	conf.Ulimits = make(map[string]*units.Ulimit)
 	conf.NetworkConfig.DefaultAddressPools = opts.PoolsOpt{}
 
 	// Set default value for `--default-shm-size`
 	conf.ShmSize = opts.MemBytes(config.DefaultShmSize)
+	conf.Runtimes = make(map[string]types.Runtime)
 
 	// Then platform-specific install flags
+	flags.Var(opts.NewNamedRuntimeOpt("runtimes", &conf.Runtimes, config.StockRuntimeName), "add-runtime", "Register an additional OCI compatible runtime")
+	flags.StringVarP(&conf.SocketGroup, "group", "G", "docker", "Group for the unix socket")
 	flags.StringVarP(&conf.GraphDriver, "storage-driver", "s", "", "Storage driver to use")
 	flags.BoolVar(&conf.EnableSelinuxSupport, "selinux-enabled", false, "Enable selinux support")
 	flags.Var(opts.NewNamedUlimitOpt("default-ulimits", &conf.Ulimits), "default-ulimit", "Default ulimits for containers")
@@ -43,7 +44,14 @@ func installConfigFlags(conf *config.Config, flags *pflag.FlagSet) error {
 	flags.BoolVar(&conf.BridgeConfig.EnableIPForward, "ip-forward", true, "Enable net.ipv4.ip_forward")
 	flags.BoolVar(&conf.BridgeConfig.EnableIPMasq, "ip-masq", true, "Enable IP masquerading")
 	flags.BoolVar(&conf.BridgeConfig.EnableIPv6, "ipv6", false, "Enable IPv6 networking")
+	flags.StringVar(&conf.BridgeConfig.IP, "bip", "", "Specify network bridge IP")
+	flags.StringVarP(&conf.BridgeConfig.Iface, "bridge", "b", "", "Attach containers to a network bridge")
+	flags.StringVar(&conf.BridgeConfig.FixedCIDR, "fixed-cidr", "", "IPv4 subnet for fixed IPs")
 	flags.StringVar(&conf.BridgeConfig.FixedCIDRv6, "fixed-cidr-v6", "", "IPv6 subnet for fixed IPs")
+	flags.Var(opts.NewIPOpt(&conf.BridgeConfig.DefaultGatewayIPv4, ""), "default-gateway", "Container default gateway IPv4 address")
+	flags.Var(opts.NewIPOpt(&conf.BridgeConfig.DefaultGatewayIPv6, ""), "default-gateway-v6", "Container default gateway IPv6 address")
+	flags.BoolVar(&conf.BridgeConfig.InterContainerCommunication, "icc", true, "Enable inter-container communication")
+	flags.Var(opts.NewIPOpt(&conf.BridgeConfig.DefaultIP, "0.0.0.0"), "ip", "Default IP when binding container ports")
 	flags.BoolVar(&conf.BridgeConfig.EnableUserlandProxy, "userland-proxy", true, "Use userland proxy for loopback traffic")
 	defaultUserlandProxyPath := ""
 	if rootless.RunningWithRootlessKit() {
