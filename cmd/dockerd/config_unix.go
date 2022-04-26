5d352f6a8754e8d1f2758669336280e5bae47968
Sebastiaan van Stijn <github@gone.nl>
2022-04-27 00:52:49 +0200

    cmd/dockerd: remove config_common_unix.go
    This file was originally part of the work to support Solaris, and
    there's nothing "not common unix" anymmore, so merging the files.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cmd/dockerd/config_unix.go
+++ cmd/dockerd/config_unix.go
@@ -97,3 +97,36 @@ func configureCertsDir() {
 		}
 	}
 }
+
+func getDefaultPidFile() (string, error) {
+	if !honorXDG {
+		return "/var/run/docker.pid", nil
+	}
+	runtimeDir, err := homedir.GetRuntimeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(runtimeDir, "docker.pid"), nil
+}
+
+func getDefaultDataRoot() (string, error) {
+	if !honorXDG {
+		return "/var/lib/docker", nil
+	}
+	dataHome, err := homedir.GetDataHome()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(dataHome, "docker"), nil
+}
+
+func getDefaultExecRoot() (string, error) {
+	if !honorXDG {
+		return "/var/run/docker", nil
+	}
+	runtimeDir, err := homedir.GetRuntimeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(runtimeDir, "docker"), nil
+}
