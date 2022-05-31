c51d34dcfb67c0198f215d5d071162433335f70c
Sebastiaan van Stijn <github@gone.nl>
2022-05-31 15:40:51 +0200

    vendor: github.com/moby/ipvs v1.0.2
    full diff: https://github.com/moby/ipvs/compare/v1.0.1...v1.0.2
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/moby/ipvs/constants.go
+++ vendor/github.com/moby/ipvs/constants.go
@@ -88,20 +88,20 @@ const (
 	ipvsDestAttrAddressFamily
 )
 
-// IPVS Svc Statistics constancs
+// IPVS Statistics constants
 
 const (
-	ipvsSvcStatsUnspec int = iota
-	ipvsSvcStatsConns
-	ipvsSvcStatsPktsIn
-	ipvsSvcStatsPktsOut
-	ipvsSvcStatsBytesIn
-	ipvsSvcStatsBytesOut
-	ipvsSvcStatsCPS
-	ipvsSvcStatsPPSIn
-	ipvsSvcStatsPPSOut
-	ipvsSvcStatsBPSIn
-	ipvsSvcStatsBPSOut
+	ipvsStatsUnspec int = iota
+	ipvsStatsConns
+	ipvsStatsPktsIn
+	ipvsStatsPktsOut
+	ipvsStatsBytesIn
+	ipvsStatsBytesOut
+	ipvsStatsCPS
+	ipvsStatsPPSIn
+	ipvsStatsPPSOut
+	ipvsStatsBPSIn
+	ipvsStatsBPSOut
 )
 
 // Destination forwarding methods
