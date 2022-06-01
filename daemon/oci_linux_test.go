e12cf055e072e48f647e8672699469d6ed738c96
Sebastiaan van Stijn <github@gone.nl>
2022-06-01 14:19:16 +0200

    Try fixing unmounts in TestSysctlxxx
        [2022-06-01T12:07:16.248Z] === FAIL: daemon TestSysctlOverride (0.00s)
        [2022-06-01T12:07:16.248Z]     testing.go:1090: TempDir RemoveAll cleanup: unlinkat /tmp/TestSysctlOverride982023519/001/mounts/shm: device or resource busy
        [2022-06-01T12:07:16.248Z]
        [2022-06-01T12:07:16.248Z] === FAIL: daemon TestSysctlOverrideHost (0.00s)
        [2022-06-01T12:07:16.248Z]     testing.go:1090: TempDir RemoveAll cleanup: unlinkat /tmp/TestSysctlOverrideHost881080732/001/mounts/shm: device or resource busy
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/oci_linux_test.go
+++ daemon/oci_linux_test.go
@@ -23,7 +23,7 @@ import (
 func setupFakeDaemon(t *testing.T, c *container.Container) *Daemon {
 	t.Helper()
 	root := t.TempDir()
-
+	cRoot := filepath.Join(root, "containers", "fakecontainer")
 	rootfs := filepath.Join(root, "rootfs")
 	err := os.MkdirAll(rootfs, 0o755)
 	assert.NilError(t, err)
@@ -41,7 +41,7 @@ func setupFakeDaemon(t *testing.T, c *container.Container) *Daemon {
 		PluginStore:   plugin.NewStore(),
 	}
 
-	c.Root = root
+	c.Root = cRoot
 	c.BaseFS = rootfs
 
 	if c.Config == nil {
