798021af9f7ab8078bd3d93ae0ade666fae3a0a1
Sebastiaan van Stijn <github@gone.nl>
2022-07-01 10:35:12 +0200

    libnetwork: macvlan: set network ID as part of parseNetworkOptions
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libnetwork/drivers/macvlan/macvlan_network.go
+++ libnetwork/drivers/macvlan/macvlan_network.go
@@ -29,7 +29,6 @@ func (d *driver) CreateNetwork(nid string, option map[string]interface{}, nInfo 
 	if err != nil {
 		return err
 	}
-	config.ID = nid
 	err = config.processIPAM(nid, ipV4Data, ipV6Data)
 	if err != nil {
 		return err
@@ -193,7 +192,7 @@ func parseNetworkOptions(id string, option options.Generic) (*configuration, err
 			config.Internal = true
 		}
 	}
-
+	config.ID = id
 	return config, nil
 }
 
