438e84cf08aec077ee28ea4876a8d9161d64421b
Paweł Gronowski <pawel.gronowski@docker.com>
2022-07-08 15:12:35 +0200

    containerd: Implement push
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/containerd/image_push.go
+++ daemon/containerd/image_push.go
@@ -2,14 +2,73 @@ package containerd
 
 import (
 	"context"
-	"errors"
 	"io"
 
+	"github.com/containerd/containerd"
+	containerdimages "github.com/containerd/containerd/images"
+	"github.com/containerd/containerd/images/converter"
+	"github.com/containerd/containerd/platforms"
+	"github.com/containerd/containerd/remotes"
+	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/api/types/registry"
-	"github.com/docker/docker/errdefs"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 // PushImage initiates a push operation on the repository named localName.
 func (i *ImageService) PushImage(ctx context.Context, image, tag string, metaHeaders map[string][]string, authConfig *registry.AuthConfig, outStream io.Writer) error {
-	return errdefs.NotImplemented(errors.New("not implemented"))
+	// TODO: Pass this from user?
+	platformMatcher := platforms.DefaultStrict()
+
+	ref, err := reference.ParseNormalizedNamed(image)
+	if err != nil {
+		return err
+	}
+	if tag != "" {
+		// Push by digest is not supported, so only tags are supported.
+		ref, err = reference.WithTag(ref, tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	is := i.client.ImageService()
+
+	img, err := is.Get(ctx, ref.String())
+	if err != nil {
+		return errors.Wrap(err, "Failed to get image")
+	}
+
+	target := img.Target
+
+	// Create a temporary image which is stripped from content that references other platforms.
+	// We or the remote may not have them and referencing them will end with an error.
+	if platformMatcher != platforms.All {
+		tmpRef := ref.String() + "-tmp-platformspecific"
+		platformImg, err := converter.Convert(ctx, i.client, tmpRef, ref.String(), converter.WithPlatform(platformMatcher))
+		if err != nil {
+			return errors.Wrap(err, "Failed to convert image to platform specific")
+		}
+
+		target = platformImg.Target
+		defer i.client.ImageService().Delete(ctx, platformImg.Name, containerdimages.SynchronousDelete())
+	}
+
+	imageHandler := containerdimages.HandlerFunc(func(ctx context.Context, desc ocispec.Descriptor) (subdescs []ocispec.Descriptor, err error) {
+		logrus.WithField("desc", desc).Debug("Pushing")
+		return nil, nil
+	})
+	imageHandler = remotes.SkipNonDistributableBlobs(imageHandler)
+
+	resolver := newResolverFromAuthConfig(authConfig)
+
+	logrus.WithField("desc", target).WithField("ref", ref.String()).Info("Pushing desc to remote ref")
+	err = i.client.Push(ctx, ref.String(), target,
+		containerd.WithResolver(resolver),
+		containerd.WithPlatformMatcher(platformMatcher),
+		containerd.WithImageHandler(imageHandler),
+	)
+
+	return err
 }
