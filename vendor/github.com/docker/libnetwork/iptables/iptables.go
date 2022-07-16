903cd53ce41f0ab016d89e055e0f9ca21a109979
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2022-07-16 22:29:27 +0900

    vendor: libnetwork 0dde5c895075df6e3630e76f750a447cf63f4789
    https://github.com/moby/libnetwork/compare/f6ccccb1c082a432c2a5814aaedaca56af33d9ea...0dde5c895075df6e3630e76f750a447cf63f4789
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- vendor/github.com/docker/libnetwork/iptables/iptables.go
+++ vendor/github.com/docker/libnetwork/iptables/iptables.go
@@ -11,6 +11,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/docker/docker/rootless"
 	"github.com/sirupsen/logrus"
 )
 
@@ -102,6 +103,12 @@ func probe() {
 }
 
 func initFirewalld() {
+	// When running with RootlessKit, firewalld is running as the root outside our network namespace
+	// https://github.com/moby/moby/issues/43781
+	if rootless.RunningWithRootlessKit() {
+		logrus.Info("skipping firewalld management for rootless mode")
+		return
+	}
 	if err := FirewalldInit(); err != nil {
 		logrus.Debugf("Fail to initialize firewalld: %v, using raw iptables instead", err)
 	}
