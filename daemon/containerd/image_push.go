360d38adb16921e85670cfd83c67237f16475aa7
Sebastiaan van Stijn <github@gone.nl>
2022-07-29 15:36:39 +0200

    daemon/containerd: use types/registry.AuthConfig
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/containerd/image_push.go
+++ daemon/containerd/image_push.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"io"
 
-	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/registry"
 )
 
 // PushImage initiates a push operation on the repository named localName.
-func (i *ImageService) PushImage(ctx context.Context, image, tag string, metaHeaders map[string][]string, authConfig *types.AuthConfig, outStream io.Writer) error {
+func (i *ImageService) PushImage(ctx context.Context, image, tag string, metaHeaders map[string][]string, authConfig *registry.AuthConfig, outStream io.Writer) error {
 	panic("not implemented")
 }
--- daemon/containerd/image_search.go
+++ daemon/containerd/image_search.go
@@ -3,9 +3,8 @@ package containerd
 import (
 	"context"
 
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
-	registrytypes "github.com/docker/docker/api/types/registry"
+	"github.com/docker/docker/api/types/registry"
 )
 
 // SearchRegistryForImages queries the registry for images matching
@@ -13,6 +12,6 @@ import (
 //
 // TODO: this could be implemented in a registry service instead of the image
 // service.
-func (i *ImageService) SearchRegistryForImages(ctx context.Context, searchFilters filters.Args, term string, limit int, authConfig *types.AuthConfig, metaHeaders map[string][]string) (*registrytypes.SearchResults, error) {
+func (i *ImageService) SearchRegistryForImages(ctx context.Context, searchFilters filters.Args, term string, limit int, authConfig *registry.AuthConfig, metaHeaders map[string][]string) (*registry.SearchResults, error) {
 	panic("not implemented")
 }
