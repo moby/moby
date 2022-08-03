d4d5e0ae0c9d79ada765da484950811d41c286bc
Sebastiaan van Stijn <github@gone.nl>
2022-08-03 10:01:14 +0200

    libcontainerd/supervisor: remove unused options
    This removes the `WithRemoteAddr()`, `WithRemoteAddrUser()`, `WithDebugAddress()`,
    and `WithMetricsAddress()` options, added in ddae20c032058a0fd42c34c2e9750ee8f6296ac8,
    but most of them were never used, and `WithRemoteAddr()` no longer in use since
    dd2e19ebd58cd8896d79b4b8db61144b93717b33.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libcontainerd/supervisor/remote_daemon_options.go
+++ libcontainerd/supervisor/remote_daemon_options.go
@@ -1,45 +1,10 @@
 package supervisor // import "github.com/docker/docker/libcontainerd/supervisor"
 
-// WithRemoteAddr sets the external containerd socket to connect to.
-func WithRemoteAddr(addr string) DaemonOpt {
-	return func(r *remote) error {
-		r.GRPC.Address = addr
-		return nil
-	}
-}
-
-// WithRemoteAddrUser sets the uid and gid to create the RPC address with
-func WithRemoteAddrUser(uid, gid int) DaemonOpt {
-	return func(r *remote) error {
-		r.GRPC.UID = uid
-		r.GRPC.GID = gid
-		return nil
-	}
-}
-
 // WithLogLevel defines which log level to starts containerd with.
 // This only makes sense if WithStartDaemon() was set to true.
 func WithLogLevel(lvl string) DaemonOpt {
 	return func(r *remote) error {
 		r.Debug.Level = lvl
-		return nil
-	}
-}
-
-// WithDebugAddress defines at which location the debug GRPC connection
-// should be made
-func WithDebugAddress(addr string) DaemonOpt {
-	return func(r *remote) error {
-		r.Debug.Address = addr
-		return nil
-	}
-}
-
-// WithMetricsAddress defines at which location the debug GRPC connection
-// should be made
-func WithMetricsAddress(addr string) DaemonOpt {
-	return func(r *remote) error {
-		r.Metrics.Address = addr
 		return nil
 	}
 }
