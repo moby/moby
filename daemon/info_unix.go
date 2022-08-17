dfcb3e17ae386235877b8e0195f55efce7a51d03
Brian Goff <cpuguy83@gmail.com>
2022-08-17 18:50:19 +0000

    Allow containerd shim refs in default-runtime
    Since runtimes can now just be containerd shims, we need to check if the
    reference is possibly a containerd shim.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>
    (cherry picked from commit e6ee27a541d4e23ed73ab88b4959e2294b62bc4f)
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- daemon/info_unix.go
+++ daemon/info_unix.go
@@ -45,15 +45,16 @@ func (daemon *Daemon) fillPlatformInfo(v *types.Info, sysInfo *sysinfo.SysInfo) 
 	v.ContainerdCommit.ID = "N/A"
 	v.InitCommit.ID = "N/A"
 
-	defaultRuntimeBinary := daemon.configStore.GetRuntime(v.DefaultRuntime).Path
-	if rv, err := exec.Command(defaultRuntimeBinary, "--version").Output(); err == nil {
-		if _, _, commit, err := parseRuntimeVersion(string(rv)); err != nil {
-			logrus.Warnf("failed to parse %s version: %v", defaultRuntimeBinary, err)
+	if rt := daemon.configStore.GetRuntime(v.DefaultRuntime); rt != nil {
+		if rv, err := exec.Command(rt.Path, "--version").Output(); err == nil {
+			if _, _, commit, err := parseRuntimeVersion(string(rv)); err != nil {
+				logrus.Warnf("failed to parse %s version: %v", rt.Path, err)
+			} else {
+				v.RuncCommit.ID = commit
+			}
 		} else {
-			v.RuncCommit.ID = commit
+			logrus.Warnf("failed to retrieve %s version: %v", rt.Path, err)
 		}
-	} else {
-		logrus.Warnf("failed to retrieve %s version: %v", defaultRuntimeBinary, err)
 	}
 
 	if rv, err := daemon.containerd.Version(context.Background()); err == nil {
@@ -176,21 +177,22 @@ func (daemon *Daemon) fillPlatformVersion(v *types.Version) {
 	}
 
 	defaultRuntime := daemon.configStore.GetDefaultRuntimeName()
-	defaultRuntimeBinary := daemon.configStore.GetRuntime(defaultRuntime).Path
-	if rv, err := exec.Command(defaultRuntimeBinary, "--version").Output(); err == nil {
-		if _, ver, commit, err := parseRuntimeVersion(string(rv)); err != nil {
-			logrus.Warnf("failed to parse %s version: %v", defaultRuntimeBinary, err)
+	if rt := daemon.configStore.GetRuntime(defaultRuntime); rt != nil {
+		if rv, err := exec.Command(rt.Path, "--version").Output(); err == nil {
+			if _, ver, commit, err := parseRuntimeVersion(string(rv)); err != nil {
+				logrus.Warnf("failed to parse %s version: %v", rt.Path, err)
+			} else {
+				v.Components = append(v.Components, types.ComponentVersion{
+					Name:    defaultRuntime,
+					Version: ver,
+					Details: map[string]string{
+						"GitCommit": commit,
+					},
+				})
+			}
 		} else {
-			v.Components = append(v.Components, types.ComponentVersion{
-				Name:    defaultRuntime,
-				Version: ver,
-				Details: map[string]string{
-					"GitCommit": commit,
-				},
-			})
+			logrus.Warnf("failed to retrieve %s version: %v", rt.Path, err)
 		}
-	} else {
-		logrus.Warnf("failed to retrieve %s version: %v", defaultRuntimeBinary, err)
 	}
 
 	defaultInitBinary := daemon.configStore.GetInitPath()
