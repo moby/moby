7e3a9d22541ff2011b19310a06e1c30b38e4986f
Paweł Gronowski <pawel.gronowski@docker.com>
2022-08-17 15:39:37 +0200

    c8d/exporter: Use configured snapshotter instead of default
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/containerd/image_exporter.go
+++ daemon/containerd/image_exporter.go
@@ -19,7 +19,7 @@ import (
 )
 
 func (i *ImageService) PerformWithBaseFS(ctx context.Context, c *container.Container, fn func(root string) error) error {
-	snapshotter := i.client.SnapshotService(containerd.DefaultSnapshotter)
+	snapshotter := i.client.SnapshotService(i.snapshotter)
 	mounts, err := snapshotter.Mounts(ctx, c.ID)
 	if err != nil {
 		return err
