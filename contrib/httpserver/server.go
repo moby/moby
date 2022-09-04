2609d4e252818b151a2a654c6244bfb0afb0b834
Sebastiaan van Stijn <github@gone.nl>
2022-09-04 14:44:55 +0200

    linting: gosec: fix or suppress G112, G114 in test code
    Updating test-code only; set ReadHeaderTimeout for some, or suppress the linter
    error for others.
    
         contrib/httpserver/server.go:11:12: G114: Use of net/http serve function that has no support for setting timeouts (gosec)
            log.Panic(http.ListenAndServe(":80", nil))
                      ^
         integration/plugin/logging/cmd/close_on_start/main.go:42:12: G112: Potential Slowloris Attack because ReadHeaderTimeout is not configured in the http.Server (gosec)
            server := http.Server{
                Addr:    l.Addr().String(),
                Handler: mux,
            }
         integration/plugin/logging/cmd/discard/main.go:17:12: G112: Potential Slowloris Attack because ReadHeaderTimeout is not configured in the http.Server (gosec)
            server := http.Server{
                Addr:    l.Addr().String(),
                Handler: mux,
            }
         integration/plugin/logging/cmd/dummy/main.go:14:12: G112: Potential Slowloris Attack because ReadHeaderTimeout is not configured in the http.Server (gosec)
            server := http.Server{
                Addr:    l.Addr().String(),
                Handler: http.NewServeMux(),
            }
         integration/plugin/volumes/cmd/dummy/main.go:14:12: G112: Potential Slowloris Attack because ReadHeaderTimeout is not configured in the http.Server (gosec)
            server := http.Server{
                Addr:    l.Addr().String(),
                Handler: http.NewServeMux(),
            }
         testutil/fixtures/plugin/basic/basic.go:25:12: G112: Potential Slowloris Attack because ReadHeaderTimeout is not configured in the http.Server (gosec)
            server := http.Server{
                Addr:    l.Addr().String(),
                Handler: http.NewServeMux(),
            }
         volume/testutils/testutils.go:170:5: G114: Use of net/http serve function that has no support for setting timeouts (gosec)
            go http.Serve(l, mux)
               ^
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 31fb92c6099110b6cc036be263ab4cd02e0d2bf7)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- contrib/httpserver/server.go
+++ contrib/httpserver/server.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("/static"))
 	http.Handle("/", fs)
-	log.Panic(http.ListenAndServe(":80", nil))
+	log.Panic(http.ListenAndServe(":80", nil)) // #nosec G114 -- Ignoring for test-code: G114: Use of net/http serve function that has no support for setting timeouts (gosec)
 }
