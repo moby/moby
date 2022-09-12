f3e4e78535728644c0edb01fa9bfcb6e723ed505
Djordje Lukic <djordje.lukic@docker.com>
2022-09-12 10:48:52 +0200

    Refactor resolving/getting images
    There were a lot of useless duplicated code
    
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/containerd/image.go
+++ daemon/containerd/image.go
@@ -62,12 +62,8 @@ func (i *ImageService) GetImage(ctx context.Context, refOrID string, options ima
 }
 
 func (i *ImageService) getImage(ctx context.Context, refOrID string, platform *ocispec.Platform) (containerd.Image, *image.Image, error) {
-	desc, err := i.ResolveImage(ctx, refOrID)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	ctrdimg, err := i.resolveImageName2(ctx, refOrID)
+	// TODO(rumpl): pass the platform
+	ctrdimg, err := i.resolveImage(ctx, refOrID)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -102,7 +98,7 @@ func (i *ImageService) getImage(ctx context.Context, refOrID string, platform *o
 	}
 	return containerdImage, &image.Image{
 		V1Image: image.V1Image{
-			ID:           string(desc.Digest),
+			ID:           string(ctrdimg.Target.Digest),
 			OS:           ociimage.OS,
 			Architecture: ociimage.Architecture,
 			Config: &containertypes.Config{
@@ -119,18 +115,13 @@ func (i *ImageService) getImage(ctx context.Context, refOrID string, platform *o
 	}, nil
 }
 
-// ResolveImage searches for an image based on the given
+// resolveImage searches for an image based on the given
 // reference or identifier. Returns the descriptor of
 // the image, could be manifest list, manifest, or config.
-func (i *ImageService) ResolveImage(ctx context.Context, refOrID string) (d ocispec.Descriptor, err error) {
-	d, _, err = i.resolveImageName(ctx, refOrID)
-	return
-}
-
-func (i *ImageService) resolveImageName2(ctx context.Context, refOrID string) (img containerdimages.Image, err error) {
+func (i *ImageService) resolveImage(ctx context.Context, refOrID string) (img containerdimages.Image, err error) {
 	parsed, err := reference.ParseAnyReference(refOrID)
 	if err != nil {
-		return img, errdefs.InvalidParameter(err)
+		return containerdimages.Image{}, errdefs.InvalidParameter(err)
 	}
 
 	is := i.client.ImageService()
@@ -139,15 +130,15 @@ func (i *ImageService) resolveImageName2(ctx context.Context, refOrID string) (i
 	if !ok {
 		digested, ok := parsed.(reference.Digested)
 		if !ok {
-			return img, errdefs.InvalidParameter(errors.New("bad reference"))
+			return containerdimages.Image{}, errdefs.InvalidParameter(errors.New("bad reference"))
 		}
 
 		imgs, err := is.List(ctx, fmt.Sprintf("target.digest==%s", digested.Digest()))
 		if err != nil {
-			return img, errors.Wrap(err, "failed to lookup digest")
+			return containerdimages.Image{}, errors.Wrap(err, "failed to lookup digest")
 		}
 		if len(imgs) == 0 {
-			return img, errdefs.NotFound(errors.New("image not found with digest"))
+			return containerdimages.Image{}, errdefs.NotFound(errors.New("image not found with digest"))
 		}
 
 		return imgs[0], nil
@@ -164,11 +155,11 @@ func (i *ImageService) resolveImageName2(ctx context.Context, refOrID string) (i
 		}
 		imgs, err := is.List(ctx, filters...)
 		if err != nil {
-			return img, err
+			return containerdimages.Image{}, err
 		}
 
 		if len(imgs) == 0 {
-			return img, errdefs.NotFound(errors.New("list returned no images"))
+			return containerdimages.Image{}, errdefs.NotFound(errors.New("list returned no images"))
 		}
 		if len(imgs) > 1 {
 			digests := map[digest.Digest]struct{}{}
@@ -180,7 +171,7 @@ func (i *ImageService) resolveImageName2(ctx context.Context, refOrID string) (i
 			}
 
 			if len(digests) > 1 {
-				return img, errdefs.NotFound(errors.New("ambiguous reference"))
+				return containerdimages.Image{}, errdefs.NotFound(errors.New("ambiguous reference"))
 			}
 		}
 
@@ -193,7 +184,7 @@ func (i *ImageService) resolveImageName2(ctx context.Context, refOrID string) (i
 	if err != nil {
 		// TODO(containerd): error translation can use common function
 		if !cerrdefs.IsNotFound(err) {
-			return img, err
+			return containerdimages.Image{}, err
 		}
 		// Some clients (such as the docker 17.03 CLI used in CI) still
 		// perform string-matching to detect that the *image* wasn't found.
@@ -201,82 +192,8 @@ func (i *ImageService) resolveImageName2(ctx context.Context, refOrID string) (i
 		// Without this prefix, the CLI prints the error message, and exits.
 		//
 		// FIXME(thaJeztah): we need to fix this; if needed, gated by API version (API < X add the prefix)
-		return img, errdefs.NotFound(errors.New("No such image: " + refOrID))
+		return containerdimages.Image{}, errdefs.NotFound(errors.New("No such image: " + refOrID))
 	}
 
 	return img, nil
-}
-
-func (i *ImageService) resolveImageName(ctx context.Context, refOrID string) (ocispec.Descriptor, reference.Named, error) {
-	parsed, err := reference.ParseAnyReference(refOrID)
-	if err != nil {
-		return ocispec.Descriptor{}, nil, errdefs.InvalidParameter(err)
-	}
-
-	is := i.client.ImageService()
-
-	namedRef, ok := parsed.(reference.Named)
-	if !ok {
-		digested, ok := parsed.(reference.Digested)
-		if !ok {
-			return ocispec.Descriptor{}, nil, errdefs.InvalidParameter(errors.New("bad reference"))
-		}
-
-		imgs, err := is.List(ctx, fmt.Sprintf("target.digest==%s", digested.Digest()))
-		if err != nil {
-			return ocispec.Descriptor{}, nil, errors.Wrap(err, "failed to lookup digest")
-		}
-		if len(imgs) == 0 {
-			return ocispec.Descriptor{}, nil, errdefs.NotFound(errors.New("image not found with digest"))
-		}
-
-		return imgs[0].Target, nil, nil
-	}
-
-	namedRef = reference.TagNameOnly(namedRef)
-
-	// If the identifier could be a short ID, attempt to match
-	if shortID.MatchString(refOrID) {
-		ref := namedRef.String()
-		filters := []string{
-			fmt.Sprintf("name==%q", ref),
-			fmt.Sprintf(`target.digest~=/sha256:%s[0-9a-fA-F]{%d}/`, refOrID, 64-len(refOrID)),
-		}
-		imgs, err := is.List(ctx, filters...)
-		if err != nil {
-			return ocispec.Descriptor{}, nil, err
-		}
-
-		if len(imgs) == 0 {
-			return ocispec.Descriptor{}, nil, errdefs.NotFound(errors.New("list returned no images"))
-		}
-		if len(imgs) > 1 {
-			digests := map[digest.Digest]struct{}{}
-			for _, img := range imgs {
-				if img.Name == ref {
-					return img.Target, namedRef, nil
-				}
-				digests[img.Target.Digest] = struct{}{}
-			}
-
-			if len(digests) > 1 {
-				return ocispec.Descriptor{}, nil, errdefs.NotFound(errors.New("ambiguous reference"))
-			}
-		}
-
-		if imgs[0].Name != ref {
-			namedRef = nil
-		}
-		return imgs[0].Target, namedRef, nil
-	}
-	img, err := is.Get(ctx, namedRef.String())
-	if err != nil {
-		// TODO(containerd): error translation can use common function
-		if !cerrdefs.IsNotFound(err) {
-			return ocispec.Descriptor{}, nil, err
-		}
-		return ocispec.Descriptor{}, nil, errdefs.NotFound(errors.New("id not found"))
-	}
-
-	return img.Target, namedRef, nil
 }
