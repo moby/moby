5e15ce3a4a0f019fca92a52cf8108b29bd809e2e
Sebastiaan van Stijn <github@gone.nl>
2022-10-05 11:11:26 +0200

    pkg/directory: remove unused MoveToSubdir() utility
    This utility was added in 442b45628ee12ebd8e8bd08497896d5fa8eec4bd as part of
    user-namespaces, and first used in 44e1023a93a0107d63d5400695cbbc6da498a425 to
    set up the daemon root, and move the existing content;
    https://github.com/docker/docker/blob/44e1023a93a0107d63d5400695cbbc6da498a425/daemon/daemon_experimental.go#L68-L71
    
    A later iteration no longer _moved_ the existing root directory, and removed the
    use of `directory.MoveToSubdir()` e8532023f20498e6eb1ce5c079dc8a09aeae3061
    
    It looks like there's no external consumers of this utility, so we should be
    save to remove it.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 26659d5eb83330269ef634713435a995caa1e2e6)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/directory/directory.go
+++ pkg/directory/directory.go
@@ -1,28 +1,6 @@
 package directory // import "github.com/docker/docker/pkg/directory"
 
-import (
-	"context"
-	"os"
-	"path/filepath"
-)
-
-// MoveToSubdir moves all contents of a directory to a subdirectory underneath the original path
-func MoveToSubdir(oldpath, subdir string) error {
-	infos, err := os.ReadDir(oldpath)
-	if err != nil {
-		return err
-	}
-	for _, info := range infos {
-		if info.Name() != subdir {
-			oldName := filepath.Join(oldpath, info.Name())
-			newName := filepath.Join(oldpath, subdir, info.Name())
-			if err := os.Rename(oldName, newName); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
+import "context"
 
 // Size walks a directory tree and returns its total size in bytes.
 func Size(ctx context.Context, dir string) (int64, error) {
