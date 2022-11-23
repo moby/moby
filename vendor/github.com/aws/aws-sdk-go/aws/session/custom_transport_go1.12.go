2cc6a86fd3f3fb6f27ffd93382e6ee4633f699e0
Sebastiaan van Stijn <github@gone.nl>
2022-11-23 18:20:40 +0100

    vendor: github.com/aws/aws-sdk-go v1.37.0
    full diff: https://github.com/aws/aws-sdk-go/compare/v1.31.6...v1.37.0
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/aws/aws-sdk-go/aws/session/custom_transport_go1.12.go
+++ vendor/github.com/aws/aws-sdk-go/aws/session/custom_transport_go1.12.go
@@ -1,4 +1,4 @@
-// +build !go1.7,go1.6
+// +build !go1.13,go1.7
 
 package session
 
@@ -10,13 +10,16 @@ import (
 
 // Transport that should be used when a custom CA bundle is specified with the
 // SDK.
-func getCABundleTransport() *http.Transport {
+func getCustomTransport() *http.Transport {
 	return &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
-		Dial: (&net.Dialer{
+		DialContext: (&net.Dialer{
 			Timeout:   30 * time.Second,
 			KeepAlive: 30 * time.Second,
-		}).Dial,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 	}
