bb5cae2c9468b4cfdc6cc0d826f8eb9599c6949e
Sebastiaan van Stijn <github@gone.nl>
2023-01-13 14:55:18 +0100

    vendor: github.com/vishvananda/netns v0.0.1
    The project started tagging releases for go modules.
    
    full diff: https://github.com/vishvananda/netns/compare/2eb08e3e575f...v0.0.1
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/vishvananda/netns/netns_unspecified.go
+++ vendor/github.com/vishvananda/netns/netns_unspecified.go
@@ -10,12 +10,24 @@ var (
 	ErrNotImplemented = errors.New("not implemented")
 )
 
+func Setns(ns NsHandle, nstype int) (err error) {
+	return ErrNotImplemented
+}
+
 func Set(ns NsHandle) (err error) {
 	return ErrNotImplemented
 }
 
 func New() (ns NsHandle, err error) {
 	return -1, ErrNotImplemented
+}
+
+func NewNamed(name string) (NsHandle, error) {
+	return -1, ErrNotImplemented
+}
+
+func DeleteNamed(name string) error {
+	return ErrNotImplemented
 }
 
 func Get() (NsHandle, error) {
