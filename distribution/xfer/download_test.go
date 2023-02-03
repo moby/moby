0c68b655f6c491697d20c06bc64e2bcc132872ee
Cory Snider <csnider@mirantis.com>
2023-02-03 12:27:37 -0500

    Fix loop-closure bugs in tests
    ...which were flagged by golangci-lint v1.51.
    
    Signed-off-by: Cory Snider <csnider@mirantis.com>

--- distribution/xfer/download_test.go
+++ distribution/xfer/download_test.go
@@ -389,6 +389,7 @@ func TestMaxDownloadAttempts(t *testing.T) {
 		},
 	}
 	for _, tc := range tests {
+		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			layerStore := &mockLayerStore{make(map[layer.ChainID]*mockLayer)}
