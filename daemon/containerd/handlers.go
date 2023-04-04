edf802956ebeeffdde4d0a92f15ddfd1fd03d7f7
Paweł Gronowski <pawel.gronowski@docker.com>
2023-04-04 17:54:41 +0200

    c8d/list: Implement `label` and `label!` filter
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

---
+++ daemon/containerd/handlers.go
@@ -0,0 +1,37 @@
+package containerd
+
+import (
+	"context"
+
+	"github.com/containerd/containerd/content"
+	cerrdefs "github.com/containerd/containerd/errdefs"
+	containerdimages "github.com/containerd/containerd/images"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
+// presentChildrenHandler is a handler wrapper which traverses all children
+// descriptors that are present in the store and calls specified handler.
+func presentChildrenHandler(store content.Store, h containerdimages.HandlerFunc) containerdimages.HandlerFunc {
+	return func(ctx context.Context, desc ocispec.Descriptor) ([]ocispec.Descriptor, error) {
+		_, err := store.Info(ctx, desc.Digest)
+		if err != nil {
+			if cerrdefs.IsNotFound(err) {
+				return nil, nil
+			}
+			return nil, err
+		}
+
+		children, err := h(ctx, desc)
+		if err != nil {
+			return nil, err
+		}
+
+		c, err := containerdimages.Children(ctx, store, desc)
+		if err != nil {
+			return nil, err
+		}
+		children = append(children, c...)
+
+		return children, nil
+	}
+}
