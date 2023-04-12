5a922dc162bbe0a03450165da4e6aceca55073d4
Sebastiaan van Stijn <github@gone.nl>
2023-04-12 23:37:43 +0200

    daemon: deprecate --oom-score-adjust for the daemon
    The `oom-score-adjust` option was added in a894aec8d81de5484152a76d76b80809df9edd71,
    to prevent the daemon from being OOM-killed before other processes. This
    option was mostly added as a "convenience", as running the daemon as a
    systemd unit was not yet common.
    
    Having the daemon set its own limits is not best-practice, and something
    better handled by the process-manager starting the daemon.
    
    Commit cf7a5be0f2ee8bb4a3f7961682e13b89bc625299 fixed this option to allow
    disabling it, and 2b8e68ef06ca3daf2b0e17e51ba72999773b6270 removed the default
    score adjust.
    
    This patch deprecates the option altogether, recommending users to set these
    limits through the process manager used, such as the "OOMScoreAdjust" option
    in systemd units.
    
    With this patch:
    
        dockerd --oom-score-adjust=-500 --validate
        Flag --oom-score-adjust has been deprecated, and will be removed in the next release.
        configuration OK
    
        echo '{"oom-score-adjust":-500}' > /etc/docker/daemon.json
        dockerd
        INFO[2023-04-12T21:34:51.133389627Z] Starting up
        INFO[2023-04-12T21:34:51.135607544Z] containerd not running, starting managed containerd
        WARN[2023-04-12T21:34:51.135629086Z] DEPRECATED: The "oom-score-adjust" config parameter and the dockerd "--oom-score-adjust" option will be removed in the next release.
    
        docker info
        Client:
          Context:    default
          Debug Mode: false
        ...
        DEPRECATED: The "oom-score-adjust" config parameter and the dockerd "--oom-score-adjust" option will be removed in the next release
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libcontainerd/supervisor/remote_daemon_options_linux.go
+++ libcontainerd/supervisor/remote_daemon_options_linux.go
@@ -1,6 +1,8 @@
 package supervisor // import "github.com/docker/docker/libcontainerd/supervisor"
 
 // WithOOMScore defines the oom_score_adj to set for the containerd process.
+//
+// Deprecated: setting the oom-score-adjust from the daemon itself is deprecated, and should be handled by the process-manager starting the daemon instead.
 func WithOOMScore(score int) DaemonOpt {
 	return func(r *remote) error {
 		r.oomScore = score
