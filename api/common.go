fd0b109bf71f3a649ea472fdb47f9ca9eedad6f2
Sebastiaan van Stijn <github@gone.nl>
2023-04-17 13:19:56 +0200

    API: bump version to 1.44
    The 24.0 branch was created, so changes in master/main should now be
    targeting the next version of the API (1.44).
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/common.go
+++ api/common.go
@@ -3,7 +3,7 @@ package api // import "github.com/docker/docker/api"
 // Common constants for daemon and client.
 const (
 	// DefaultVersion of Current REST API
-	DefaultVersion = "1.43"
+	DefaultVersion = "1.44"
 
 	// NoBaseImageSpecifier is the symbol used by the FROM
 	// command to specify that no base image is to be used.
