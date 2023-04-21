6da63a3ed518a0fdc71485df64eb9ae9dc85b245
Sebastiaan van Stijn <github@gone.nl>
2023-04-21 11:54:05 +0200

    client: VolumePruneOptions: add "All" option
    Abstract away that the "all" option is currently implemented as
    a filter (which may change in future).
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/volume_prune.go
+++ client/volume_prune.go
@@ -6,11 +6,17 @@ import (
 	"fmt"
 	"net/url"
 
+	"github.com/containerd/errdefs"
 	"github.com/moby/moby/api/types/volume"
 )
 
-// VolumePruneOptions holds parameters to prune networks.
+// VolumePruneOptions holds parameters to prune volumes.
 type VolumePruneOptions struct {
+	// All controls whether named volumes should also be pruned. By
+	// default, only anonymous volumes are pruned.
+	All bool
+
+	// Filters to apply when pruning.
 	Filters Filters
 }
 
@@ -21,6 +27,16 @@ type VolumePruneResult struct {
 
 // VolumesPrune requests the daemon to delete unused data
 func (cli *Client) VolumesPrune(ctx context.Context, opts VolumePruneOptions) (VolumePruneResult, error) {
+	if opts.All {
+		if _, ok := opts.Filters["all"]; ok {
+			return VolumePruneResult{}, errdefs.ErrInvalidArgument.WithMessage(`conflicting options: cannot specify both "all" and "all" filter`)
+		}
+		if opts.Filters == nil {
+			opts.Filters = Filters{}
+		}
+		opts.Filters.Add("all", "true")
+	}
+
 	query := url.Values{}
 	opts.Filters.updateURLValues(query)
 
