dea870f4eafac1efb1d3a70d8196da11bfa6c59c
Cory Snider <csnider@mirantis.com>
2023-06-05 18:44:51 -0400

    daemon: stop setting container resources to zero
    Many of the fields in LinuxResources struct are pointers to scalars for
    some reason, presumably to differentiate between set-to-zero and unset
    when unmarshaling from JSON, despite zero being outside the acceptable
    range for the corresponding kernel tunables. When creating the OCI spec
    for a container, the daemon sets the container's OCI spec CPUShares and
    BlkioWeight parameters to zero when the corresponding Docker container
    configuration values are zero, signifying unset, despite the minimum
    acceptable value for CPUShares being two, and BlkioWeight ten. This has
    gone unnoticed as runC does not distingiush set-to-zero from unset as it
    also uses zero internally to represent unset for those fields. However,
    kata-containers v3.2.0-alpha.3 tries to apply the explicit-zero resource
    parameters to the container, exactly as instructed, and fails loudly.
    The OCI runtime-spec is silent on how the runtime should handle the case
    when those parameters are explicitly set to out-of-range values and
    kata's behaviour is not unreasonable, so the daemon must therefore be in
    the wrong.
    
    Translate unset values in the Docker container's resources HostConfig to
    omit the corresponding fields in the container's OCI spec when starting
    and updating a container in order to maximize compatibility with
    runtimes.
    
    Signed-off-by: Cory Snider <csnider@mirantis.com>

--- daemon/oci_linux.go
+++ daemon/oci_linux.go
@@ -973,13 +973,11 @@ func WithResources(c *container.Container) coci.SpecOpts {
 		if err != nil {
 			return err
 		}
-		blkioWeight := r.BlkioWeight
 
 		specResources := &specs.LinuxResources{
 			Memory: memoryRes,
 			CPU:    cpuRes,
 			BlockIO: &specs.LinuxBlockIO{
-				Weight:                  &blkioWeight,
 				WeightDevice:            weightDevices,
 				ThrottleReadBpsDevice:   readBpsDevice,
 				ThrottleWriteBpsDevice:  writeBpsDevice,
@@ -987,6 +985,10 @@ func WithResources(c *container.Container) coci.SpecOpts {
 				ThrottleWriteIOPSDevice: writeIOpsDevice,
 			},
 			Pids: getPidsLimit(r),
+		}
+		if r.BlkioWeight != 0 {
+			w := r.BlkioWeight
+			specResources.BlockIO.Weight = &w
 		}
 
 		if s.Linux.Resources != nil && len(s.Linux.Resources.Devices) > 0 {
