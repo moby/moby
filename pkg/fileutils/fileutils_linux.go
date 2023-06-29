521fbb36c06db6c441642b2684ce4b2076d2fcbd
Sebastiaan van Stijn <github@gone.nl>
2023-06-30 00:46:46 +0200

    pkg/fileutils: GetTotalUsedFds(): use fast-path for Kernel 6.2 and up
    Linux 6.2 and up (commit [f1f1f2569901ec5b9d425f2e91c09a0e320768f3][1])
    provides a fast path for the number of open files for the process.
    
    From the [Linux docs][2]:
    
    > The number of open files for the process is stored in 'size' member of
    > `stat()` output for /proc/<pid>/fd for fast access.
    
    [1]: https://github.com/torvalds/linux/commit/f1f1f2569901ec5b9d425f2e91c09a0e320768f3
    [2]: https://docs.kernel.org/filesystems/proc.html#proc-pid-fd-list-of-symlinks-to-open-files
    
    This patch adds a fast-path for Kernels that support this, and falls back
    to the slow path if the Size fields is zero.
    
    Comparing on a Fedora 38 (kernel 6.2.9-300.fc38.x86_64):
    
    Before/After:
    
        go test -bench ^BenchmarkGetTotalUsedFds$ -run ^$ ./pkg/fileutils/
        BenchmarkGetTotalUsedFds        57264     18595 ns/op     408 B/op      10 allocs/op
        BenchmarkGetTotalUsedFds       370392      3271 ns/op      40 B/op       3 allocs/op
    
    Note that the slow path has 1 more file-descriptor, due to the open
    file-handle for /proc/<pid>/fd during the calculation.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit ec79d0fc054a97b1d18adec794c6c30bfb11a488)
    Resolved conflicts:
    	pkg/fileutils/fileutils_linux.go
    Signed-off-by: Bjorn Neergaard <bjorn.neergaard@docker.com>

--- pkg/fileutils/fileutils_linux.go
+++ pkg/fileutils/fileutils_linux.go
@@ -6,12 +6,27 @@ import (
 	"os"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
 
 // GetTotalUsedFds Returns the number of used File Descriptors by
 // reading it via /proc filesystem.
 func GetTotalUsedFds() int {
 	name := fmt.Sprintf("/proc/%d/fd", os.Getpid())
+
+	// Fast-path for Linux 6.2 (since [f1f1f2569901ec5b9d425f2e91c09a0e320768f3]).
+	// From the [Linux docs]:
+	//
+	// "The number of open files for the process is stored in 'size' member of
+	// stat() output for /proc/<pid>/fd for fast access."
+	//
+	// [Linux docs]: https://docs.kernel.org/filesystems/proc.html#proc-pid-fd-list-of-symlinks-to-open-files:
+	// [f1f1f2569901ec5b9d425f2e91c09a0e320768f3]: https://github.com/torvalds/linux/commit/f1f1f2569901ec5b9d425f2e91c09a0e320768f3
+	var stat unix.Stat_t
+	if err := unix.Stat(name, &stat); err == nil && stat.Size > 0 {
+		return int(stat.Size)
+	}
+
 	f, err := os.Open(name)
 	if err != nil {
 		logrus.WithError(err).Error("Error listing file descriptors")
@@ -30,5 +45,7 @@ func GetTotalUsedFds() int {
 			return -1
 		}
 	}
+	// Note that the slow path has 1 more file-descriptor, due to the open
+	// file-handle for /proc/<pid>/fd during the calculation.
 	return fdCount
 }
