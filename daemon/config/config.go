13472c6cf1bfbe16b51b1ac4eff42305a4d07f1f
Sebastiaan van Stijn <github@gone.nl>
2023-06-30 10:52:31 +0200

    daemon/config: remove unused CommonConfig.Context
    This field was added in f0e6e135a8d733af173bf0b8732c704c9ec716d7, and
    from that change I suspect it was intended to store the default SELinux
    mount-labels to be set on containers.
    
    However, it was never used, so let's remove it.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/config/config.go
+++ daemon/config/config.go
@@ -144,22 +144,21 @@ type DNSConfig struct {
 // It includes json tags to deserialize configuration from a file
 // using the same names that the flags in the command line use.
 type CommonConfig struct {
-	AuthorizationPlugins  []string            `json:"authorization-plugins,omitempty"` // AuthorizationPlugins holds list of authorization plugins
-	AutoRestart           bool                `json:"-"`
-	Context               map[string][]string `json:"-"`
-	DisableBridge         bool                `json:"-"`
-	ExecOptions           []string            `json:"exec-opts,omitempty"`
-	GraphDriver           string              `json:"storage-driver,omitempty"`
-	GraphOptions          []string            `json:"storage-opts,omitempty"`
-	Labels                []string            `json:"labels,omitempty"`
-	Mtu                   int                 `json:"mtu,omitempty"`
-	NetworkDiagnosticPort int                 `json:"network-diagnostic-port,omitempty"`
-	Pidfile               string              `json:"pidfile,omitempty"`
-	RawLogs               bool                `json:"raw-logs,omitempty"`
-	Root                  string              `json:"data-root,omitempty"`
-	ExecRoot              string              `json:"exec-root,omitempty"`
-	SocketGroup           string              `json:"group,omitempty"`
-	CorsHeaders           string              `json:"api-cors-header,omitempty"`
+	AuthorizationPlugins  []string `json:"authorization-plugins,omitempty"` // AuthorizationPlugins holds list of authorization plugins
+	AutoRestart           bool     `json:"-"`
+	DisableBridge         bool     `json:"-"`
+	ExecOptions           []string `json:"exec-opts,omitempty"`
+	GraphDriver           string   `json:"storage-driver,omitempty"`
+	GraphOptions          []string `json:"storage-opts,omitempty"`
+	Labels                []string `json:"labels,omitempty"`
+	Mtu                   int      `json:"mtu,omitempty"`
+	NetworkDiagnosticPort int      `json:"network-diagnostic-port,omitempty"`
+	Pidfile               string   `json:"pidfile,omitempty"`
+	RawLogs               bool     `json:"raw-logs,omitempty"`
+	Root                  string   `json:"data-root,omitempty"`
+	ExecRoot              string   `json:"exec-root,omitempty"`
+	SocketGroup           string   `json:"group,omitempty"`
+	CorsHeaders           string   `json:"api-cors-header,omitempty"`
 
 	// Proxies holds the proxies that are configured for the daemon.
 	Proxies `json:"proxies"`
