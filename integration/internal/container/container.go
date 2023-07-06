d7fb4dd170826c314caeae1096dbfe90d25141f2
Albin Kerouanton <albinker@gmail.com>
2023-07-06 10:39:13 +0200

    integration: Extract stream demultiplexing from container.Exec
    The original code in container.Exec was potentially leaking the copy
    goroutine when the context was cancelled or timed out. The new
    `demultiplexStreams()` function won't return until the goroutine has
    finished its work, and to ensure that it takes care of closing the
    hijacked connection.
    
    Signed-off-by: Albin Kerouanton <albinker@gmail.com>

--- integration/internal/container/container.go
+++ integration/internal/container/container.go
@@ -1,14 +1,17 @@
 package container
 
 import (
+	"bytes"
 	"context"
 	"runtime"
+	"sync"
 	"testing"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"gotest.tools/v3/assert"
 )
@@ -70,4 +73,37 @@ func Run(ctx context.Context, t *testing.T, client client.APIClient, ops ...func
 	assert.NilError(t, err)
 
 	return id
+}
+
+type streams struct {
+	stdout, stderr bytes.Buffer
+}
+
+// demultiplexStreams starts a goroutine to demultiplex stdout and stderr from the types.HijackedResponse resp and
+// waits until either multiplexed stream reaches EOF or the context expires. It unconditionally closes resp and waits
+// until the demultiplexing goroutine has finished its work before returning.
+func demultiplexStreams(ctx context.Context, resp types.HijackedResponse) (streams, error) {
+	var s streams
+	outputDone := make(chan error, 1)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		_, err := stdcopy.StdCopy(&s.stdout, &s.stderr, resp.Reader)
+		outputDone <- err
+		wg.Done()
+	}()
+
+	var err error
+	select {
+	case copyErr := <-outputDone:
+		err = copyErr
+		break
+	case <-ctx.Done():
+		err = ctx.Err()
+	}
+
+	resp.Close()
+	wg.Wait()
+	return s, err
 }
