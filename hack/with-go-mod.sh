5563b09ac242fb07e031e935c6f28f15baa41e62
Bjorn Neergaard <bjorn.neergaard@docker.com>
2023-07-11 07:36:03 -0600

    hack: use Git-free ROOTDIR convention
    Signed-off-by: Bjorn Neergaard <bjorn.neergaard@docker.com>

--- hack/with-go-mod.sh
+++ hack/with-go-mod.sh
@@ -9,7 +9,7 @@
 set -e
 
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOTDIR="$(git -C "$SCRIPTDIR" rev-parse --show-toplevel)"
+ROOTDIR="$(cd "${SCRIPTDIR}/.." && pwd)"
 
 if test -e "${ROOTDIR}/go.mod"; then
 	{
