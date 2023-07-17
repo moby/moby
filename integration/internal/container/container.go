e129aa0bd72552058f2fa09d51d3de6b40135a50
Albin Kerouanton <albinker@gmail.com>
2023-07-17 16:51:00 +0200

    integration: Add RunAttach helper
    Signed-off-by: Albin Kerouanton <albinker@gmail.com>

--- integration/internal/container/container.go
+++ integration/internal/container/container.go
@@ -1,7 +1,10 @@
 package container
 
 import (
+	"bytes"
 	"context"
+	"errors"
+	"io"
 	"runtime"
 	"testing"
 
@@ -9,6 +12,7 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"gotest.tools/v3/assert"
 )
@@ -79,4 +83,74 @@ func Inspect(ctx context.Context, t *testing.T, cli client.APIClient, cid string
 	assert.NilError(t, err)
 
 	return c
+}
+
+type RunResult struct {
+	ContainerID string
+	ExitCode    int
+	Stdout      *bytes.Buffer
+	Stderr      *bytes.Buffer
+}
+
+func RunAttach(ctx context.Context, t *testing.T, client client.APIClient, ops ...func(config *TestContainerConfig)) RunResult {
+	t.Helper()
+
+	ops = append(ops, func(c *TestContainerConfig) {
+		c.Config.AttachStdout = true
+		c.Config.AttachStderr = true
+	})
+	id := Create(ctx, t, client, ops...)
+
+	aresp, err := client.ContainerAttach(ctx, id, types.ContainerAttachOptions{
+		Stream: true,
+		Stdout: true,
+		Stderr: true,
+	})
+	assert.NilError(t, err)
+	defer aresp.Close()
+
+	err = client.ContainerStart(ctx, id, types.ContainerStartOptions{})
+	assert.NilError(t, err)
+
+	s, err := demultiplexStreams(ctx, aresp.Reader)
+	if !errors.Is(err, context.DeadlineExceeded) {
+		assert.NilError(t, err)
+	}
+
+	// Inspect to get the exit code. A new context is used here to make sure that if the context passed as argument as
+	// reached timeout during the demultiplexStream call, we still return a RunResult.
+	resp, err := client.ContainerInspect(context.Background(), id)
+	assert.NilError(t, err)
+
+	return RunResult{ContainerID: id, ExitCode: resp.State.ExitCode, Stdout: s.stdout, Stderr: s.stderr}
+}
+
+type streams struct {
+	stdout *bytes.Buffer
+	stderr *bytes.Buffer
+}
+
+func demultiplexStreams(ctx context.Context, src io.Reader) (streams, error) {
+	s := streams{
+		stdout: &bytes.Buffer{},
+		stderr: &bytes.Buffer{},
+	}
+	outputDone := make(chan error, 1)
+
+	go func() {
+		_, err := stdcopy.StdCopy(s.stdout, s.stderr, src)
+		outputDone <- err
+	}()
+
+	select {
+	case err := <-outputDone:
+		if err != nil {
+			return s, err
+		}
+		break
+	case <-ctx.Done():
+		return s, ctx.Err()
+	}
+
+	return s, nil
 }
