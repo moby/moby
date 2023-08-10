05deecaa45d45673a548d18ca72c14b17d762b6e
Sebastiaan van Stijn <github@gone.nl>
2023-08-10 12:36:33 +0200

    internal/testutils/netnsutils: move utils that were not used on Windows
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- internal/testutils/netnsutils/context_windows.go
+++ internal/testutils/netnsutils/context_windows.go
@@ -1,19 +1,8 @@
 package netnsutils
 
-import (
-	"testing"
+import "testing"
 
-	"github.com/docker/docker/internal/testutils"
-)
-
-type OSContext struct{}
-
-func SetupTestOSContextEx(*testing.T) *OSContext {
-	return nil
-}
-
-func (*OSContext) Cleanup(t *testing.T) {}
-
-func (*OSContext) Set() (func(testutils.Logger), error) {
-	return func(testutils.Logger) {}, nil
+// SetupTestOSContext is a no-op on Windows.
+func SetupTestOSContext(*testing.T) func() {
+	return func() {}
 }
