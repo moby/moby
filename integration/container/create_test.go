docker/docker

Warn (and remove) on empty env-var names instead of failing
This patch;

- Improves validation of empty env-var names to account for additional white-space
- Instead of failing the container, produce a warning, and strip the empty env-var
- Improves the error-message, but putting quotes around the value (making it easier
  to find the issue)

Before this patch, creating a container with an empty env-var would fail;

```bash
docker run --rm --name test -e "" busybox
invalid argument "" for "-e, --env" flag: invalid environment variable:
See 'docker run --help'.
```

However, the validation was too specific, and would fail to detect env-var
names containing only white-space:

```bash
docker run --rm --name test -e " = " -e "  " busybox

docker container create --name test -e " = " -e "  " busybox
5816ece7bc80289055a725783e18a2c3d7df21b3debdcc09cd99dc532868354a

docker container inspect --format '{{json .Config.Env}}' test
[" = ","  ","PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"]

curl -s \
  --unix-socket /var/run/docker.sock \
  -X POST \
  "http://localhost/v1.37/containers/create?name=test" \
  -H "Content-Type: application/json" \
  -d '{"Image": "busybox:latest","Env":[""],"HostConfig":{"AutoRemove":true}}'

{"message":"invalid environment variable: "}
```

With this patch applied, both empty and whitespace-only env-var names are detected,
and instead of failing the container, the empty env-vars are removed, and a
warning is returned instead:

```bash
docker run --rm --name test -e " = " -e "  " busybox
WARNING: invalid environment variable: " = "
WARNING: invalid environment variable: "  "

docker container create --name test -e " = " -e "  " busybox
WARNING: invalid environment variable: " = "
WARNING: invalid environment variable: "  "
25106252d65e322fc45fb3f0164584cf4854fa0d6f47ced423e4490c2197868b

docker container inspect --format '{{json .Config.Env}}' test
["PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"]

curl -

integration/container/create_test.go
@@ -92,20 +92,20 @@ func TestCreateWithInvalidEnv(t *testing.T) {
 	client := testEnv.APIClient()
 
 	testCases := []struct {
-		env           string
-		expectedError string
+		env             string
+		expectedWarning string
 	}{
 		{
-			env:           "",
-			expectedError: "invalid environment variable:",
+			env:             "",
+			expectedWarning: `invalid environment variable: ""`,
 		},
 		{
-			env:           "=",
-			expectedError: "invalid environment variable: =",
+			env:             "=",
+			expectedWarning: `invalid environment variable: "="`,
 		},
 		{
-			env:           "=foo",
-			expectedError: "invalid environment variable: =foo",
+			env:             "=foo",
+			expectedWarning: `invalid environment variable: "=foo"`,
 		},
 	}
 
@@ -113,7 +113,7 @@ func TestCreateWithInvalidEnv(t *testing.T) {
 		tc := tc
 		t.Run(strconv.Itoa(index), func(t *testing.T) {
 			t.Parallel()
-			_, err := client.ContainerCreate(context.Background(),
+			resp, err := client.ContainerCreate(context.Background(),
 				&container.Config{
 					Image: "busybox",
 					Env:   []string{tc.env},
@@ -122,8 +122,15 @@ func TestCreateWithInvalidEnv(t *testing.T) {
 				&network.NetworkingConfig{},
 				"",
 			)
-			assert.Check(t, is.ErrorContains(err, tc.expectedError))
-			assert.Check(t, errdefs.IsInvalidParameter(err))
+			assert.NilError(t, err)
+			var found bool
+			for _, w := range resp.Warnings {
+				if w == tc.expectedWarning {
+					found = true
+					break
+				}
+			}
+			assert.Check(t, found)
 		})
 	}
 }
@@ -172,6 +179,7 @@ func TestCreateTmpfsMountsTarget(t *testing.T) {
 		assert.Check(t, errdefs.IsInvalidParameter(err))
 	}
 }
+
 func TestCreateWithCustomMaskedPaths(t *testing.T) {
 	skip.If(t, testEnv.DaemonInfo.OSType != "linux")
 

moby/moby

integration/container/create_test.go
@@ -94,20 +94,20 @@ func TestCreateWithInvalidEnv(t *testing.T) {
 	apiClient := testEnv.APIClient()
 
 	testCases := []struct {
-		env           string
-		expectedError string
+		env             string
+		expectedWarning string
 	}{
 		{
-			env:           "",
-			expectedError: "invalid environment variable:",
+			env:             "",
+			expectedWarning: `invalid environment variable: ""`,
 		},
 		{
-			env:           "=",
-			expectedError: "invalid environment variable: =",
+			env:             "=",
+			expectedWarning: `invalid environment variable: "="`,
 		},
 		{
-			env:           "=foo",
-			expectedError: "invalid environment variable: =foo",
+			env:             "=foo",
+			expectedWarning: `invalid environment variable: "=foo"`,
 		},
 	}
 
@@ -115,7 +115,7 @@ func TestCreateWithInvalidEnv(t *testing.T) {
 		tc := tc
 		t.Run(strconv.Itoa(index), func(t *testing.T) {
 			t.Parallel()
-			_, err := apiClient.ContainerCreate(context.Background(),
+			resp, err := apiClient.ContainerCreate(context.Background(),
 				&container.Config{
 					Image: "busybox",
 					Env:   []string{tc.env},
@@ -125,8 +125,15 @@ func TestCreateWithInvalidEnv(t *testing.T) {
 				nil,
 				"",
 			)
-			assert.Check(t, is.ErrorContains(err, tc.expectedError))
-			assert.Check(t, errdefs.IsInvalidParameter(err))
+			assert.NilError(t, err)
+			var found bool
+			for _, w := range resp.Warnings {
+				if w == tc.expectedWarning {
+					found = true
+					break
+				}
+			}
+			assert.Check(t, found)
 		})
 	}
 }