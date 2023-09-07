5f59f7bb4961a259aaf21e6c504c4591d1b54cea
Sebastiaan van Stijn <github@gone.nl>
2023-09-07 15:35:02 +0200

    integration/container: combine TestResize tests into subtests
    Reduce some of the boiler-plating, and by combining the tests, we skip
    the testenv.Clean() in between each of the tests. Performance gain isn't
    really measurable, but every bit should help :)
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- integration/container/resize_test.go
+++ integration/container/resize_test.go
@@ -18,37 +18,39 @@ func TestResize(t *testing.T) {
 	ctx := setupTest(t)
 	apiClient := testEnv.APIClient()
 
-	cID := container.Run(ctx, t, apiClient, container.WithTty(true))
-	err := apiClient.ContainerResize(ctx, cID, types.ResizeOptions{
-		Height: 40,
-		Width:  40,
+	t.Run("success", func(t *testing.T) {
+		cID := container.Run(ctx, t, apiClient, container.WithTty(true))
+		err := apiClient.ContainerResize(ctx, cID, types.ResizeOptions{
+			Height: 40,
+			Width:  40,
+		})
+		assert.NilError(t, err)
+		// TODO(thaJeztah): also check if the resize happened
+		//
+		// Note: container inspect shows the initial size that was
+		// set when creating the container. Actual resize happens in
+		// containerd, and currently does not update the container's
+		// config after running (but does send a "resize" event).
 	})
-	assert.NilError(t, err)
-}
 
-func TestResizeWithInvalidSize(t *testing.T) {
-	skip.If(t, versions.LessThan(testEnv.DaemonAPIVersion(), "1.32"), "broken in earlier versions")
-	ctx := setupTest(t)
-	apiClient := testEnv.APIClient()
+	t.Run("invalid size", func(t *testing.T) {
+		skip.If(t, versions.LessThan(testEnv.DaemonAPIVersion(), "1.32"), "broken in earlier versions")
+		cID := container.Run(ctx, t, apiClient)
 
-	cID := container.Run(ctx, t, apiClient)
-
-	// Manually creating a request here, as the APIClient would invalidate
-	// these values before they're sent.
-	res, _, err := req.Post(ctx, "/containers/"+cID+"/resize?h=foo&w=bar")
-	assert.NilError(t, err)
-	assert.Check(t, is.DeepEqual(http.StatusBadRequest, res.StatusCode))
-}
-
-func TestResizeWhenContainerNotStarted(t *testing.T) {
-	ctx := setupTest(t)
-	apiClient := testEnv.APIClient()
-
-	cID := container.Create(ctx, t, apiClient, container.WithCmd("echo"))
-	err := apiClient.ContainerResize(ctx, cID, types.ResizeOptions{
-		Height: 40,
-		Width:  40,
+		// Manually creating a request here, as the APIClient would invalidate
+		// these values before they're sent.
+		res, _, err := req.Post(ctx, "/containers/"+cID+"/resize?h=foo&w=bar")
+		assert.NilError(t, err)
+		assert.Check(t, is.DeepEqual(http.StatusBadRequest, res.StatusCode))
 	})
-	assert.Check(t, is.ErrorType(err, errdefs.IsConflict))
-	assert.Check(t, is.ErrorContains(err, "is not running"))
+
+	t.Run("invalid state", func(t *testing.T) {
+		cID := container.Create(ctx, t, apiClient, container.WithCmd("echo"))
+		err := apiClient.ContainerResize(ctx, cID, types.ResizeOptions{
+			Height: 40,
+			Width:  40,
+		})
+		assert.Check(t, is.ErrorType(err, errdefs.IsConflict))
+		assert.Check(t, is.ErrorContains(err, "is not running"))
+	})
 }
