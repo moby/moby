cpuguy83/docker

plugin: Plumb context through
This required some additional changes in the container event handler,
hence why this is not touching only the plugin system.

This did not change the `PluginGetter` interface which is defined in
pkg/plugins as this is another semi-large change across different
packages.

Signed-off-by: Brian Goff <cpuguy83@gmail.com>

daemon/monitor.go
@@ -27,7 +27,7 @@ func (daemon *Daemon) setStateCounter(c *container.Container) {
 	}
 }
 
-func (daemon *Daemon) handleContainerExit(c *container.Container, e *libcontainerdtypes.EventInfo) error {
+func (daemon *Daemon) handleContainerExit(ctx context.Context, c *container.Container, e *libcontainerdtypes.EventInfo) error {
 	var exitStatus container.ExitStatus
 	c.Lock()
 
@@ -39,7 +39,7 @@ func (daemon *Daemon) handleContainerExit(c *container.Container, e *libcontaine
 
 	tsk, ok := c.Task()
 	if ok {
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 		es, err := tsk.Delete(ctx)
 		cancel()
 		if err != nil {
@@ -55,7 +55,7 @@ func (daemon *Daemon) handleContainerExit(c *container.Container, e *libcontaine
 		}
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
 	c.StreamConfig.Wait(ctx)
 	cancel()
 
@@ -144,7 +144,7 @@ func (daemon *Daemon) handleContainerExit(c *container.Container, e *libcontaine
 }
 
 // ProcessEvent is called by libcontainerd whenever an event occurs
-func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei libcontainerdtypes.EventInfo) error {
+func (daemon *Daemon) ProcessEvent(ctx context.Context, id string, e libcontainerdtypes.EventType, ei libcontainerdtypes.EventInfo) error {
 	c, err := daemon.GetContainer(id)
 	if err != nil {
 		return errors.Wrapf(err, "could not find container %s", id)
@@ -168,7 +168,7 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 		daemon.LogContainerEvent(c, "oom")
 	case libcontainerdtypes.EventExit:
 		if ei.ProcessID == ei.ContainerID {
-			return daemon.handleContainerExit(c, &ei)
+			return daemon.handleContainerExit(ctx, c, &ei)
 		}
 
 		exitCode := 127
@@ -186,7 +186,7 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 			execConfig.ExitCode = &ec
 			execConfig.Running = false
 
-			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+			ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
 			execConfig.StreamConfig.Wait(ctx)
 			cancel()
 
@@ -206,7 +206,7 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 			// logical race condition.
 			if execConfig.Process != nil {
 				go func() {
-					if _, err := execConfig.Process.Delete(context.Background()); err != nil {
+					if _, err := execConfig.Process.Delete(ctx); err != nil {
 						log.G(ctx).WithFields(logrus.Fields{
 							logrus.ErrorKey: err,
 							"container":     ei.ContainerID,
@@ -227,12 +227,12 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 
 		// This is here to handle start not generated by docker
 		if !c.Running {
-			ctr, err := daemon.containerd.LoadContainer(context.Background(), c.ID)
+			ctr, err := daemon.containerd.LoadContainer(ctx, c.ID)
 			if err != nil {
 				if errdefs.IsNotFound(err) {
 					// The container was started by not-docker and so could have been deleted by
 					// not-docker before we got around to loading it from containerd.
-					log.G(context.TODO()).WithFields(logrus.Fields{
+					log.G(ctx).WithFields(logrus.Fields{
 						logrus.ErrorKey: err,
 						"container":     c.ID,
 					}).Debug("could not load containerd container for start event")
@@ -240,10 +240,10 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 				}
 				return err
 			}
-			tsk, err := ctr.Task(context.Background())
+			tsk, err := ctr.Task(ctx)
 			if err != nil {
 				if errdefs.IsNotFound(err) {
-					log.G(context.TODO()).WithFields(logrus.Fields{
+					log.G(ctx).WithFields(logrus.Fields{
 						logrus.ErrorKey: err,
 						"container":     c.ID,
 					}).Debug("failed to load task for externally-started container")

moby/moby

daemon/monitor.go
@@ -14,6 +14,7 @@ import (
 	libcontainerdtypes "github.com/docker/docker/libcontainerd/types"
 	"github.com/docker/docker/restartmanager"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 func (daemon *Daemon) setStateCounter(c *container.Container) {
@@ -27,7 +28,7 @@ func (daemon *Daemon) setStateCounter(c *container.Container) {
 	}
 }
 
-func (daemon *Daemon) handleContainerExit(c *container.Container, e *libcontainerdtypes.EventInfo) error {
+func (daemon *Daemon) handleContainerExit(ctx context.Context, c *container.Container, e *libcontainerdtypes.EventInfo) error {
 	var exitStatus container.ExitStatus
 	c.Lock()
 
@@ -39,7 +40,7 @@ func (daemon *Daemon) handleContainerExit(c *container.Container, e *libcontaine
 
 	tsk, ok := c.Task()
 	if ok {
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 		es, err := tsk.Delete(ctx)
 		cancel()
 		if err != nil {
@@ -55,7 +56,7 @@ func (daemon *Daemon) handleContainerExit(c *container.Container, e *libcontaine
 		}
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
 	c.StreamConfig.Wait(ctx)
 	cancel()
 
@@ -148,7 +149,7 @@ func (daemon *Daemon) handleContainerExit(c *container.Container, e *libcontaine
 }
 
 // ProcessEvent is called by libcontainerd whenever an event occurs
-func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei libcontainerdtypes.EventInfo) error {
+func (daemon *Daemon) ProcessEvent(ctx context.Context, id string, e libcontainerdtypes.EventType, ei libcontainerdtypes.EventInfo) error {
 	c, err := daemon.GetContainer(id)
 	if err != nil {
 		return errors.Wrapf(err, "could not find container %s", id)
@@ -172,7 +173,7 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 		daemon.LogContainerEvent(c, events.ActionOOM)
 	case libcontainerdtypes.EventExit:
 		if ei.ProcessID == ei.ContainerID {
-			return daemon.handleContainerExit(c, &ei)
+			return daemon.handleContainerExit(ctx, c, &ei)
 		}
 
 		exitCode := 127
@@ -190,8 +191,8 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 			execConfig.ExitCode = &ec
 			execConfig.Running = false
 
-			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-			execConfig.StreamConfig.Wait(ctx)
+			ctxT, cancel := context.WithTimeout(ctx, 2*time.Second)
+			execConfig.StreamConfig.Wait(ctxT)
 			cancel()
 
 			if err := execConfig.CloseStreams(); err != nil {
@@ -210,11 +211,11 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 			// logical race condition.
 			if execConfig.Process != nil {
 				go func() {
-					if _, err := execConfig.Process.Delete(context.Background()); err != nil {
+					if _, err := execConfig.Process.Delete(ctx); err != nil {
 						log.G(ctx).WithFields(log.Fields{
-							"error":     err,
-							"container": ei.ContainerID,
-							"process":   ei.ProcessID,
+							logrus.ErrorKey: err,
+							"container":     ei.ContainerID,
+							"process":       ei.ProcessID,
 						}).Warn("failed to delete process")
 					}
 				}()
@@ -230,12 +231,12 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 
 		// This is here to handle start not generated by docker
 		if !c.Running {
-			ctr, err := daemon.containerd.LoadContainer(context.Background(), c.ID)
+			ctr, err := daemon.containerd.LoadContainer(ctx, c.ID)
 			if err != nil {
 				if errdefs.IsNotFound(err) {
 					// The container was started by not-docker and so could have been deleted by
 					// not-docker before we got around to loading it from containerd.
-					log.G(context.TODO()).WithFields(log.Fields{
+					log.G(ctx).WithFields(log.Fields{
 						"error":     err,
 						"container": c.ID,
 					}).Debug("could not load containerd container for start event")
@@ -243,10 +244,10 @@ func (daemon *Daemon) ProcessEvent(id string, e libcontainerdtypes.EventType, ei
 				}
 				return err
 			}
-			tsk, err := ctr.Task(context.Background())
+			tsk, err := ctr.Task(ctx)
 			if err != nil {
 				if errdefs.IsNotFound(err) {
-					log.G(context.TODO()).WithFields(log.Fields{
+					log.G(ctx).WithFields(log.Fields{
 						"error":     err,
 						"container": c.ID,
 					}).Debug("failed to load task for externally-started container")