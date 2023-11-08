a7acfffea62dddb180775de80937e9b9d4bc18b5
Djordje Lukic <djordje.lukic@docker.com>
2023-11-08 10:17:58 +0100

    test: Add a test for userns remapped daemon commit
    The files in an image created by a commit should have the right uid and gid set
    
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

--- integration/image/commit_test.go
+++ integration/image/commit_test.go
@@ -1,12 +1,14 @@
 package image // import "github.com/docker/docker/integration/image"
 
 import (
+	"context"
 	"strings"
 	"testing"
 
 	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/versions"
 	"github.com/docker/docker/integration/internal/container"
+	"github.com/docker/docker/testutil/daemon"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
 	"gotest.tools/v3/skip"
@@ -46,4 +48,27 @@ func TestCommitInheritsEnv(t *testing.T) {
 	assert.NilError(t, err)
 	expectedEnv2 := []string{"PATH=/usr/bin:/bin"}
 	assert.Check(t, is.DeepEqual(expectedEnv2, image2.Config.Env))
+}
+
+// Verify that files created are owned by the remapped user even after a commit
+func TestUsernsCommit(t *testing.T) {
+	skip.If(t, testEnv.DaemonInfo.OSType != "linux")
+	skip.If(t, testEnv.IsRemoteDaemon())
+	skip.If(t, !testEnv.IsUserNamespaceInKernel())
+	skip.If(t, testEnv.IsRootless())
+
+	ctx := context.Background()
+	dUserRemap := daemon.New(t, daemon.WithUserNsRemap("default"))
+	dUserRemap.StartWithBusybox(ctx, t)
+	clientUserRemap := dUserRemap.NewClientT(t)
+	defer clientUserRemap.Close()
+
+	container.Run(ctx, t, clientUserRemap, container.WithName(t.Name()), container.WithImage("busybox"), container.WithCmd("sh", "-c", "echo hello world > /hello.txt && chown 1000:1000 /hello.txt"))
+	img, err := clientUserRemap.ContainerCommit(ctx, t.Name(), containertypes.CommitOptions{})
+	assert.NilError(t, err)
+
+	res := container.RunAttach(ctx, t, clientUserRemap, container.WithImage(img.ID), container.WithCmd("sh", "-c", "stat -c %u:%g /hello.txt"))
+	assert.Check(t, is.Equal(res.ExitCode, 0))
+	assert.Check(t, is.Equal(res.Stderr.String(), ""))
+	assert.Assert(t, is.Equal(strings.TrimSpace(res.Stdout.String()), "1000:1000"))
 }
