8b38739e3f06b76556816e830d63f74267e3eecf
Sebastiaan van Stijn <github@gone.nl>
2023-12-15 14:26:31 +0100

    add //go:build directives to prevent downgrading to go1.16 language
    This repository is not yet a module (i.e., does not have a `go.mod`). This
    is not problematic when building the code in GOPATH or "vendor" mode, but
    when using the code as a module-dependency (in module-mode), different semantics
    are applied since Go1.21, which switches Go _language versions_ on a per-module,
    per-package, or even per-file base.
    
    A condensed summary of that logic [is as follows][1]:
    
    - For modules that have a go.mod containing a go version directive; that
      version is considered a minimum _required_ version (starting with the
      go1.19.13 and go1.20.8 patch releases: before those, it was only a
      recommendation).
    - For dependencies that don't have a go.mod (not a module), go language
      version go1.16 is assumed.
    - Likewise, for modules that have a go.mod, but the file does not have a
      go version directive, go language version go1.16 is assumed.
    - If a go.work file is present, but does not have a go version directive,
      language version go1.17 is assumed.
    
    When switching language versions, Go _downgrades_ the language version,
    which means that language features (such as generics, and `any`) are not
    available, and compilation fails. For example:
    
        # github.com/docker/cli/cli/context/store
        /go/pkg/mod/github.com/docker/cli@v25.0.0-beta.2+incompatible/cli/context/store/storeconfig.go:6:24: predeclared any requires go1.18 or later (-lang was set to go1.16; check go.mod)
        /go/pkg/mod/github.com/docker/cli@v25.0.0-beta.2+incompatible/cli/context/store/store.go:74:12: predeclared any requires go1.18 or later (-lang was set to go1.16; check go.mod)
    
    Note that these fallbacks are per-module, per-package, and can even be
    per-file, so _(indirect) dependencies_ can still use modern language
    features, as long as their respective go.mod has a version specified.
    
    Unfortunately, these failures do not occur when building locally (using
    vendor / GOPATH mode), but will affect consumers of the module.
    
    Obviously, this situation is not ideal, and the ultimate solution is to
    move to go modules (add a go.mod), but this comes with a non-insignificant
    risk in other areas (due to our complex dependency tree).
    
    We can revert to using go1.16 language features only, but this may be
    limiting, and may still be problematic when (e.g.) matching signatures
    of dependencies.
    
    There is an escape hatch: adding a `//go:build` directive to files that
    make use of go language features. From the [go toolchain docs][2]:
    
    > The go line for each module sets the language version the compiler enforces
    > when compiling packages in that module. The language version can be changed
    > on a per-file basis by using a build constraint.
    >
    > For example, a module containing code that uses the Go 1.21 language version
    > should have a `go.mod` file with a go line such as `go 1.21` or `go 1.21.3`.
    > If a specific source file should be compiled only when using a newer Go
    > toolchain, adding `//go:build go1.22` to that source file both ensures that
    > only Go 1.22 and newer toolchains will compile the file and also changes
    > the language version in that file to Go 1.22.
    
    This patch adds `//go:build` directives to those files using recent additions
    to the language. It's currently using go1.19 as version to match the version
    in our "vendor.mod", but we can consider being more permissive ("any" requires
    go1.18 or up), or more "optimistic" (force go1.21, which is the version we
    currently use to build).
    
    For completeness sake, note that any file _without_ a `//go:build` directive
    will continue to use go1.16 language version when used as a module.
    
    [1]: https://github.com/golang/go/blob/58c28ba286dd0e98fe4cca80f5d64bbcb824a685/src/cmd/go/internal/gover/version.go#L9-L56
    [2]: https://go.dev/doc/toolchain
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 2cf230951fe29e2c2eda9eed43042524435c761a)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,3 +1,6 @@
+// FIXME(thaJeztah): remove once we are a module; the go:build directive prevents go from downgrading language version to go1.16:
+//go:build go1.19
+
 // Package daemon exposes the functions that occur on the host server
 // that the Docker daemon is running.
 //
