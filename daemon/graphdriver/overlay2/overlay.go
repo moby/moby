48220008d89c267a918728be0838847a9f67e586
Sebastiaan van Stijn <github@gone.nl>
2023-12-28 12:25:57 +0100

    daemon/graphdriver: remove/rename err-returns and remove naked returns
    Prevent accidentally shadowing these errors, which are used in defers, and
    while at it, also fixed some linting warnings about unhandled errors.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/graphdriver/overlay2/overlay.go
+++ daemon/graphdriver/overlay2/overlay.go
@@ -674,7 +674,7 @@ func (d *Driver) isParent(id, parent string) bool {
 }
 
 // ApplyDiff applies the new layer into a root
-func (d *Driver) ApplyDiff(id string, parent string, diff io.Reader) (size int64, err error) {
+func (d *Driver) ApplyDiff(id string, parent string, diff io.Reader) (size int64, _ error) {
 	if useNaiveDiff(d.home) || !d.isParent(id, parent) {
 		return d.naiveDiff.ApplyDiff(id, parent, diff)
 	}
@@ -703,7 +703,7 @@ func (d *Driver) getDiffPath(id string) string {
 // DiffSize calculates the changes between the specified id
 // and its parent and returns the size in bytes of the changes
 // relative to its base filesystem directory.
-func (d *Driver) DiffSize(id, parent string) (size int64, err error) {
+func (d *Driver) DiffSize(id, parent string) (int64, error) {
 	if useNaiveDiff(d.home) || !d.isParent(id, parent) {
 		return d.naiveDiff.DiffSize(id, parent)
 	}
