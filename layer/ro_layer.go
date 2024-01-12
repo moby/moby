3c73232f0330d0b6a79e537416a86e1d0ce586be
Paweł Gronowski <pawel.gronowski@docker.com>
2024-01-12 17:54:32 +0100

    roLayer: Fill important Descriptor fields if empty
    If passed descriptor is empty, create one based on the layer properties.
    
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- layer/ro_layer.go
+++ layer/ro_layer.go
@@ -5,6 +5,7 @@ import (
 	"io"
 
 	"github.com/docker/distribution"
+	"github.com/docker/distribution/manifest/schema2"
 	"github.com/opencontainers/go-digest"
 )
 
@@ -141,12 +142,19 @@ func storeLayer(tx *fileMetadataTransaction, layer *roLayer) error {
 	if err := tx.SetCacheID(layer.cacheID); err != nil {
 		return err
 	}
-	// Do not store empty descriptors
-	if layer.descriptor.Digest != "" {
-		if err := tx.SetDescriptor(layer.descriptor); err != nil {
-			return err
-		}
+
+	desc := layer.descriptor
+	if desc.Digest == "" {
+		desc.Digest = digest.Digest(layer.diffID)
+		desc.Size = layer.size
 	}
+	if desc.MediaType == "" {
+		desc.MediaType = schema2.MediaTypeUncompressedLayer
+	}
+	if err := tx.SetDescriptor(layer.descriptor); err != nil {
+		return err
+	}
+
 	if layer.parent != nil {
 		if err := tx.SetParent(layer.parent.chainID); err != nil {
 			return err
