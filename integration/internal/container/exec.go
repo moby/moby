312450d07948c88f9d137dd1886241733a4f1e73
Cory Snider <csnider@mirantis.com>
2024-01-15 15:30:22 -0500

    integration: test container healthcheck is reset
    Update the TestDaemonRestartKilContainers integration test to assert
    that a container's healthcheck status is always reset to the Starting
    state after a daemon restart, even when the container is live-restored.
    
    Signed-off-by: Cory Snider <csnider@mirantis.com>

--- integration/internal/container/exec.go
+++ integration/internal/container/exec.go
@@ -3,6 +3,7 @@ package container
 import (
 	"bytes"
 	"context"
+	"testing"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -16,18 +17,30 @@ type ExecResult struct {
 }
 
 // Stdout returns stdout output of a command run by Exec()
-func (res *ExecResult) Stdout() string {
+func (res ExecResult) Stdout() string {
 	return res.outBuffer.String()
 }
 
 // Stderr returns stderr output of a command run by Exec()
-func (res *ExecResult) Stderr() string {
+func (res ExecResult) Stderr() string {
 	return res.errBuffer.String()
 }
 
 // Combined returns combined stdout and stderr output of a command run by Exec()
-func (res *ExecResult) Combined() string {
+func (res ExecResult) Combined() string {
 	return res.outBuffer.String() + res.errBuffer.String()
+}
+
+// AssertSuccess fails the test and stops execution if the command exited with a
+// nonzero status code.
+func (res ExecResult) AssertSuccess(t testing.TB) {
+	t.Helper()
+	if res.ExitCode != 0 {
+		t.Logf("expected exit code 0, got %d", res.ExitCode)
+		t.Logf("stdout: %s", res.Stdout())
+		t.Logf("stderr: %s", res.Stderr())
+		t.FailNow()
+	}
 }
 
 // Exec executes a command inside a container, returning the result
@@ -71,4 +84,14 @@ func Exec(ctx context.Context, apiClient client.APIClient, id string, cmd []stri
 	}
 
 	return ExecResult{ExitCode: iresp.ExitCode, outBuffer: &s.stdout, errBuffer: &s.stderr}, nil
+}
+
+// ExecT calls Exec() and aborts the test if an error occurs.
+func ExecT(ctx context.Context, t testing.TB, apiClient client.APIClient, id string, cmd []string, ops ...func(*types.ExecConfig)) ExecResult {
+	t.Helper()
+	res, err := Exec(ctx, apiClient, id, cmd, ops...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return res
 }
