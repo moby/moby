0b1921649f4f80fb7439ed786504459b62cd0e3f
Sebastiaan van Stijn <github@gone.nl>
2024-01-21 14:01:00 +0100

    api/types: remove deprecated system info types and functions
    These types were deprecated in c90229ed9aea7c5bde53a11437c54dd3fc60e71a
    (v25.0), and moved to api/types/system.
    
    This patch removes the aliases for;
    
    - api/types.Info
    - api/types.Commit
    - api/types.PluginsInfo
    - api/types.NetworkAddressPool
    - api/types.Runtime
    - api/types.SecurityOpt
    - api/types.KeyValue
    - api/types.DecodeSecurityOptions
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/types/types_deprecated.go
+++ api/types/types_deprecated.go
@@ -5,7 +5,6 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/api/types/swarm"
-	"github.com/docker/docker/api/types/system"
 )
 
 // CheckpointCreateOptions holds parameters to create a checkpoint from a container.
@@ -27,44 +26,6 @@ type CheckpointDeleteOptions = checkpoint.DeleteOptions
 //
 // Deprecated: use [checkpoint.Summary].
 type Checkpoint = checkpoint.Summary
-
-// Info contains response of Engine API:
-// GET "/info"
-//
-// Deprecated: use [system.Info].
-type Info = system.Info
-
-// Commit holds the Git-commit (SHA1) that a binary was built from, as reported
-// in the version-string of external tools, such as containerd, or runC.
-//
-// Deprecated: use [system.Commit].
-type Commit = system.Commit
-
-// PluginsInfo is a temp struct holding Plugins name
-// registered with docker daemon. It is used by [system.Info] struct
-//
-// Deprecated: use [system.PluginsInfo].
-type PluginsInfo = system.PluginsInfo
-
-// NetworkAddressPool is a temp struct used by [system.Info] struct.
-//
-// Deprecated: use [system.NetworkAddressPool].
-type NetworkAddressPool = system.NetworkAddressPool
-
-// Runtime describes an OCI runtime.
-//
-// Deprecated: use [system.Runtime].
-type Runtime = system.Runtime
-
-// SecurityOpt contains the name and options of a security option.
-//
-// Deprecated: use [system.SecurityOpt].
-type SecurityOpt = system.SecurityOpt
-
-// KeyValue holds a key/value pair.
-//
-// Deprecated: use [system.KeyValue].
-type KeyValue = system.KeyValue
 
 // ImageDeleteResponseItem image delete response item.
 //
@@ -128,14 +89,6 @@ type ContainerLogsOptions = container.LogsOptions
 //
 // Deprecated: use [container.RemoveOptions].
 type ContainerRemoveOptions = container.RemoveOptions
-
-// DecodeSecurityOptions decodes a security options string slice to a type safe
-// [system.SecurityOpt].
-//
-// Deprecated: use [system.DecodeSecurityOptions].
-func DecodeSecurityOptions(opts []string) ([]system.SecurityOpt, error) {
-	return system.DecodeSecurityOptions(opts)
-}
 
 // ImageImportOptions holds information to import images from the client host.
 //
