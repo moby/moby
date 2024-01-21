f0dd554e3c44225fdb03f3418cc4626d6a302929
Sebastiaan van Stijn <github@gone.nl>
2024-01-21 18:27:54 +0100

    api: remove code for ContainerInspect on api < v1.20
    API v1.23 and older are deprecated, so we can remove the code to adjust
    responses for API v1.19 and lower.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/inspect_linux.go
+++ daemon/inspect_linux.go
@@ -1,11 +1,8 @@
 package daemon // import "github.com/docker/docker/daemon"
 
 import (
-	"context"
-
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/backend"
-	"github.com/docker/docker/api/types/versions/v1p19"
 	"github.com/docker/docker/container"
 )
 
@@ -17,47 +14,6 @@ func setPlatformSpecificContainerFields(container *container.Container, contJSON
 	contJSONBase.HostsPath = container.HostsPath
 
 	return contJSONBase
-}
-
-// containerInspectPre120 gets containers for pre 1.20 APIs.
-func (daemon *Daemon) containerInspectPre120(ctx context.Context, name string) (*v1p19.ContainerJSON, error) {
-	ctr, err := daemon.GetContainer(name)
-	if err != nil {
-		return nil, err
-	}
-
-	ctr.Lock()
-	defer ctr.Unlock()
-
-	base, err := daemon.getInspectData(&daemon.config().Config, ctr)
-	if err != nil {
-		return nil, err
-	}
-
-	volumes := make(map[string]string)
-	volumesRW := make(map[string]bool)
-	for _, m := range ctr.MountPoints {
-		volumes[m.Destination] = m.Path()
-		volumesRW[m.Destination] = m.RW
-	}
-
-	return &v1p19.ContainerJSON{
-		ContainerJSONBase: base,
-		Volumes:           volumes,
-		VolumesRW:         volumesRW,
-		Config: &v1p19.ContainerConfig{
-			Config:          ctr.Config,
-			MacAddress:      ctr.Config.MacAddress, //nolint:staticcheck // ignore SA1019: field is deprecated, but still used on API < v1.44.
-			NetworkDisabled: ctr.Config.NetworkDisabled,
-			ExposedPorts:    ctr.Config.ExposedPorts,
-			VolumeDriver:    ctr.HostConfig.VolumeDriver,
-			Memory:          ctr.HostConfig.Memory,
-			MemorySwap:      ctr.HostConfig.MemorySwap,
-			CPUShares:       ctr.HostConfig.CPUShares,
-			CPUSet:          ctr.HostConfig.CpusetCpus,
-		},
-		NetworkSettings: daemon.getBackwardsCompatibleNetworkSettings(ctr.NetworkSettings),
-	}, nil
 }
 
 func inspectExecProcessConfig(e *container.ExecConfig) *backend.ExecProcessConfig {
