97cec59d92645f54607cf5ee83796f1db697174e
Sebastiaan van Stijn <github@gone.nl>
2024-02-10 13:43:01 +0100

    libnetwork/ipams/remote: inline decodeToMap
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libnetwork/ipams/remote/remote_test.go
+++ libnetwork/ipams/remote/remote_test.go
@@ -18,15 +18,10 @@ import (
 	is "gotest.tools/v3/assert/cmp"
 )
 
-func decodeToMap(r *http.Request) (map[string]interface{}, error) {
-	var res map[string]interface{}
-	err := json.NewDecoder(r.Body).Decode(&res)
-	return res, err
-}
-
 func handle(t *testing.T, mux *http.ServeMux, method string, h func(map[string]interface{}) interface{}) {
 	mux.HandleFunc(fmt.Sprintf("/%s.%s", ipamapi.PluginEndpointType, method), func(w http.ResponseWriter, r *http.Request) {
-		ask, err := decodeToMap(r)
+		var ask map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&ask)
 		if err != nil && err != io.EOF {
 			t.Fatal(err)
 		}
