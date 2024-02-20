a4aab7b1f654ae22b40e8d520839f08d65c87619
Sebastiaan van Stijn <github@gone.nl>
2024-02-20 13:19:24 +0100

    cmd/dockerd: apply options when creating daemonCLI, not when starting
    Validate and apply options when creating the CLI, so that starting the
    CLI does not have to mutate the config, and to have a clearer separation
    between "creating", "validating", and starting the daemon.
    
    This also allows skipping the service-registration code in situations
    where we only want to validate the config.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- cmd/dockerd/docker_unix.go
+++ cmd/dockerd/docker_unix.go
@@ -3,14 +3,24 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"os"
 
 	"github.com/containerd/log"
 )
 
 func runDaemon(opts *daemonOptions) error {
-	daemonCli := NewDaemonCli()
-	return daemonCli.start(opts)
+	cli, err := NewDaemonCli(opts)
+	if err != nil {
+		return err
+	}
+	if opts.Validate {
+		// If config wasn't OK we wouldn't have made it this far.
+		_, _ = fmt.Fprintln(os.Stderr, "configuration OK")
+		return nil
+	}
+	return cli.start()
 }
 
 func initLogging(_, stderr io.Writer) {
