akerouanton/docker

libnet: Controller: add ctx to store methods
Signed-off-by: Albin Kerouanton <albinker@gmail.com>

libnetwork/drivers/bridge/bridge_store.go
@@ -12,6 +12,9 @@ import (
 	"github.com/docker/docker/libnetwork/datastore"
 	"github.com/docker/docker/libnetwork/netlabel"
 	"github.com/docker/docker/libnetwork/types"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
@@ -95,7 +98,11 @@ func (d *driver) populateEndpoints() error {
 	return nil
 }
 
-func (d *driver) storeUpdate(kvObject datastore.KVObject) error {
+func (d *driver) storeUpdate(ctx context.Context, kvObject datastore.KVObject) error {
+	_, span := otel.Tracer("").Start(ctx, "libnetwork.drivers.bridge.storeUpdate", trace.WithAttributes(
+		attribute.String("kvObject", fmt.Sprintf("%+v", kvObject.Key()))))
+	defer span.End()
+
 	if d.store == nil {
 		log.G(context.TODO()).Warnf("bridge store not initialized. kv object %s is not added to the store", datastore.Key(kvObject.Key()...))
 		return nil

libnetwork/store.go
@@ -8,6 +8,7 @@ import (
 	"github.com/containerd/log"
 	"github.com/docker/docker/libnetwork/datastore"
 	"github.com/docker/docker/libnetwork/scope"
+	"go.opentelemetry.io/otel"
 )
 
 func (c *Controller) getNetworkFromStore(nid string) (*Network, error) {
@@ -114,7 +115,10 @@ func (n *Network) getEndpointsFromStore() ([]*Endpoint, error) {
 	return epl, nil
 }
 
-func (c *Controller) updateToStore(kvObject datastore.KVObject) error {
+func (c *Controller) updateToStore(ctx context.Context, kvObject datastore.KVObject) error {
+	_, span := otel.Tracer("").Start(ctx, "libnetwork.Controller.updateToStore")
+	defer span.End()
+
 	if err := c.store.PutObjectAtomic(kvObject); err != nil {
 		if err == datastore.ErrKeyModified {
 			return err

moby/moby

libnetwork/drivers/bridge/bridge_store.go
@@ -12,6 +12,9 @@ import (
 	"github.com/docker/docker/libnetwork/datastore"
 	"github.com/docker/docker/libnetwork/netlabel"
 	"github.com/docker/docker/libnetwork/types"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
@@ -95,9 +98,13 @@ func (d *driver) populateEndpoints() error {
 	return nil
 }
 
-func (d *driver) storeUpdate(kvObject datastore.KVObject) error {
+func (d *driver) storeUpdate(ctx context.Context, kvObject datastore.KVObject) error {
+	ctx, span := otel.Tracer("").Start(ctx, "libnetwork.drivers.bridge.storeUpdate", trace.WithAttributes(
+		attribute.String("kvObject", fmt.Sprintf("%+v", kvObject.Key()))))
+	defer span.End()
+
 	if d.store == nil {
-		log.G(context.TODO()).Warnf("bridge store not initialized. kv object %s is not added to the store", datastore.Key(kvObject.Key()...))
+		log.G(ctx).Warnf("bridge store not initialized. kv object %s is not added to the store", datastore.Key(kvObject.Key()...))
 		return nil
 	}
 

libnetwork/store.go
@@ -8,6 +8,7 @@ import (
 	"github.com/containerd/log"
 	"github.com/docker/docker/libnetwork/datastore"
 	"github.com/docker/docker/libnetwork/scope"
+	"go.opentelemetry.io/otel"
 )
 
 func (c *Controller) getNetworkFromStore(nid string) (*Network, error) {
@@ -114,7 +115,10 @@ func (n *Network) getEndpointsFromStore() ([]*Endpoint, error) {
 	return epl, nil
 }
 
-func (c *Controller) updateToStore(kvObject datastore.KVObject) error {
+func (c *Controller) updateToStore(ctx context.Context, kvObject datastore.KVObject) error {
+	ctx, span := otel.Tracer("").Start(ctx, "libnetwork.Controller.updateToStore")
+	defer span.End()
+
 	if err := c.store.PutObjectAtomic(kvObject); err != nil {
 		if err == datastore.ErrKeyModified {
 			return err