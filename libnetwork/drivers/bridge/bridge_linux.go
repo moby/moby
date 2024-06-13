c86e46d6126cba00c3230265afd366bb505c2afe
Rob Murray <rob.murray@docker.com>
2024-06-13 15:33:02 +0100

    Add option com.docker.network.bridge.host_binding_ip
    Existing option com.docker.network.bridge.host_binding_ipv4 accepts
    IPv6 addresses - so, added a synonym without the 'v4'.
    
    It's an error to specify both.
    
    The existing handling is surprising - apart from accepting v6 in
    an option named v4, "0.0.0.0" means "any v4 or v6 address" but "::"
    means "any v6 address". So, added an integration test to illustrate
    that and make sure it's not "fixed" in passing.
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/drivers/bridge/bridge_linux.go
+++ libnetwork/drivers/bridge/bridge_linux.go
@@ -279,6 +279,15 @@ func (c *networkConfiguration) Conflicts(o *networkConfiguration) error {
 }
 
 func (c *networkConfiguration) fromLabels(labels map[string]string) error {
+	// DefaultBindingIPv4 is a synonym for DefaultBindingIP. Don't
+	// allow both to be specified.
+	if _, ok := labels[DefaultBindingIPv4]; ok {
+		if _, ok := labels[DefaultBindingIP]; ok {
+			return errdefs.InvalidParameter(errors.New(
+				DefaultBindingIPv4 + " is a synonym for " + DefaultBindingIP + ", use one or the other"))
+		}
+	}
+
 	var err error
 	for label, value := range labels {
 		switch label {
@@ -317,6 +326,10 @@ func (c *networkConfiguration) fromLabels(labels map[string]string) error {
 				return parseErr(label, value, err.Error())
 			}
 		case DefaultBindingIP:
+			if c.DefaultBindingIP = net.ParseIP(value); c.DefaultBindingIP == nil {
+				return parseErr(label, value, "nil ip")
+			}
+		case DefaultBindingIPv4:
 			if c.DefaultBindingIP = net.ParseIP(value); c.DefaultBindingIP == nil {
 				return parseErr(label, value, "nil ip")
 			}
