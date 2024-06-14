akerouanton/docker

container: add a span to CheckpointTo
Signed-off-by: Albin Kerouanton <albinker@gmail.com>

container/container.go
@@ -42,6 +42,9 @@ import (
 	"github.com/moby/sys/symlink"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
@@ -200,7 +203,12 @@ func (container *Container) toDisk() (*Container, error) {
 
 // CheckpointTo makes the Container's current state visible to queries, and persists state.
 // Callers must hold a Container lock.
-func (container *Container) CheckpointTo(store *ViewDB) error {
+func (container *Container) CheckpointTo(ctx context.Context, store *ViewDB) error {
+	ctx, span := otel.Tracer("").Start(ctx, "daemon.CheckpointTo", trace.WithAttributes(
+		attribute.String("container.ID", container.ID),
+		attribute.String("container.Name", container.Name)))
+	defer span.End()
+
 	deepCopy, err := container.toDisk()
 	if err != nil {
 		return err

moby/moby

container/container.go
@@ -42,6 +42,9 @@ import (
 	"github.com/moby/sys/symlink"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/pkg/errors"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
@@ -200,7 +203,12 @@ func (container *Container) toDisk() (*Container, error) {
 
 // CheckpointTo makes the Container's current state visible to queries, and persists state.
 // Callers must hold a Container lock.
-func (container *Container) CheckpointTo(store *ViewDB) error {
+func (container *Container) CheckpointTo(ctx context.Context, store *ViewDB) error {
+	ctx, span := otel.Tracer("").Start(ctx, "container.CheckpointTo", trace.WithAttributes(
+		attribute.String("container.ID", container.ID),
+		attribute.String("container.Name", container.Name)))
+	defer span.End()
+
 	deepCopy, err := container.toDisk()
 	if err != nil {
 		return err