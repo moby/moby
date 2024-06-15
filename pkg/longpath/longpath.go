424c22390ea3431fadc742432f623771e776c975
Sebastiaan van Stijn <github@gone.nl>
2024-06-15 17:11:25 +0200

    pkg/longpath: deprecate Prefix const
    This const was exported because it was in use by pkg/symlink. This
    dependency was removed in a48c6e30057bed6092ffe82cd1976c7ae0ec227d,
    after which this const was only used internally.
    
    This patch deprecates the const and introduces a non-exported const
    to use.
    
    There are no known external consumers of this const, so we may skip
    deprecating it.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/longpath/longpath.go
+++ pkg/longpath/longpath.go
@@ -12,17 +12,22 @@ import (
 )
 
 // Prefix is the longpath prefix for Windows file paths.
-const Prefix = `\\?\`
+//
+// Deprecated: this const is only used internally, and will be removed in the next release
+const Prefix = longPathPrefix
+
+// longPathPrefix is the longpath prefix for Windows file paths.
+const longPathPrefix = `\\?\`
 
 // AddPrefix adds the Windows long path prefix to the path provided if
 // it does not already have it.
 func AddPrefix(path string) string {
-	if !strings.HasPrefix(path, Prefix) {
+	if !strings.HasPrefix(path, longPathPrefix) {
 		if strings.HasPrefix(path, `\\`) {
 			// This is a UNC path, so we need to add 'UNC' to the path as well.
-			path = Prefix + `UNC` + path[1:]
+			path = longPathPrefix + `UNC` + path[1:]
 		} else {
-			path = Prefix + path
+			path = longPathPrefix + path
 		}
 	}
 	return path
