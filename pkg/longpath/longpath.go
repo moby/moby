48ff86ec64a84bc729b0047cd714b23832078804
Sebastiaan van Stijn <github@gone.nl>
2024-06-15 17:13:36 +0200

    pkg/longpath: AddPrefix: use early returns
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/longpath/longpath.go
+++ pkg/longpath/longpath.go
@@ -22,15 +22,14 @@ const longPathPrefix = `\\?\`
 // AddPrefix adds the Windows long path prefix to the path provided if
 // it does not already have it.
 func AddPrefix(path string) string {
-	if !strings.HasPrefix(path, longPathPrefix) {
-		if strings.HasPrefix(path, `\\`) {
-			// This is a UNC path, so we need to add 'UNC' to the path as well.
-			path = longPathPrefix + `UNC` + path[1:]
-		} else {
-			path = longPathPrefix + path
-		}
+	if strings.HasPrefix(path, longPathPrefix) {
+		return path
 	}
-	return path
+	if strings.HasPrefix(path, `\\`) {
+		// This is a UNC path, so we need to add 'UNC' to the path as well.
+		return longPathPrefix + `UNC` + path[1:]
+	}
+	return longPathPrefix + path
 }
 
 // MkdirTemp is the equivalent of [os.MkdirTemp], except that on Windows
