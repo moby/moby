2954b05b0375f970459ad2a0b2b9d7758a2cf47f
Sebastiaan van Stijn <github@gone.nl>
2024-06-16 21:55:29 +0200

    runconfig: remove conversion code for API < v1.18
    The runconfig package contained compatibility code to unmarshal API
    requests on API < v1.18, and to convert them to current API versions.
    These fields were marked as deprecated, but didn't mention relevant API
    versions, so some digging was needed to track back history;
    
    API versions before 1.18 accepted top-level `Memory`, `MemorySwap`,
    `CpuShares`, and `Cpuset` fields as part of the container create requests.
    These fields were not considered "portable", and therefore moved to the
    `HostConfig` struct in 837eec064d2d40a4d86acbc6f47fada8263e0d4c. The
    API version at that time was [v1.18]. For backward-compatibility, the
    existing top-level fields were kept, and conversion code was added in
    [ContainerHostConfigFromJob] to copy their values to `HostConfig` if
    present.
    
    A refactor in 767df67e3149b83255db0809f6543b449a4f652e introduced a new
    `ContainerConfigWrapper` struct, which embedded the container-config and
    a (non-exported) `hostConfigWrapper`. This resulted in an incompatibility
    when compiling with gccgo, sn eb97de7dee5f36e6b6a710a0def245c11cb4cba5
    removed the non-exported `hostConfigWrapper`, instead embedding the
    `HostConfig` and adding a `CpuSet` field. The API version at that time
    was [v1.19].
    
    With the introduction of Windows containers, which did not need conversion
    code as it never supported previous API versions, the `ContainerConfigWrapper`
    was split to Linux and Windows implementation in f6ed590596cbf73764b40ff4f32f90b1cdb8b213.
    This change introduced a `SetDefaultNetModeIfBlank` function to set the
    default network-mode on Linux. Windows did not have a default network,
    but did require a separate `ValidateNetMode` implemenation.
    
    The `ContainerConfigWrapper` was expanded to include `NetworkingConfig`
    in 2bb3fc1bc522059e9be5bd967b6a5c49917f5d0c for API [v1.22], but did
    not involve backward-compatiblity / conversion code.
    
    Based on the above, all conversion code present in runconfig is related
    to API versions [v1.18] or before. 19a04efa2f1cc7bc6a6d1567c63202a68e643c98,
    and other commits in [moby PR 47155] removed support for API < v1.24, so
    this conversion code is no longer needed.
    
    This patch removes the legacy fields from the `ContainerConfigWrapper`,
    and removes the corresponding conversion code. The `InnerHostConfig` field
    is also renamed, as it is no longer shadowed by the `container.HostConfig`
    that was embedded for backward-compatibility.
    
    [v1.18]: https://github.com/moby/moby/blob/837eec064d2d40a4d86acbc6f47fada8263e0d4c/api/common.go#L18
    [v1.19]: https://github.com/moby/moby/blob/767df67e3149b83255db0809f6543b449a4f652e/api/common.go#L20
    [v1.22]: https://github.com/moby/moby/blob/2bb3fc1bc522059e9be5bd967b6a5c49917f5d0c/api/common.go#L21
    [moby PR 47155]: https://github.com/moby/moby/pull/47155
    [ContainerHostConfigFromJob]: https://github.com/hqhq/moby/blob/837eec064d2d40a4d86acbc6f47fada8263e0d4c/runconfig/hostconfig.go#L149-L162
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- runconfig/config_unix.go
+++ runconfig/config_unix.go
@@ -11,45 +11,14 @@ import (
 // and the corresponding HostConfig (non-portable).
 type ContainerConfigWrapper struct {
 	*container.Config
-	InnerHostConfig       *container.HostConfig          `json:"HostConfig,omitempty"`
-	Cpuset                string                         `json:",omitempty"` // Deprecated. Exported for backwards compatibility.
-	NetworkingConfig      *networktypes.NetworkingConfig `json:"NetworkingConfig,omitempty"`
-	*container.HostConfig                                // Deprecated. Exported to read attributes from json that are not in the inner host config structure.
+	HostConfig       *container.HostConfig          `json:"HostConfig,omitempty"`
+	NetworkingConfig *networktypes.NetworkingConfig `json:"NetworkingConfig,omitempty"`
 }
 
 // getHostConfig gets the HostConfig of the Config.
 // It's mostly there to handle Deprecated fields of the ContainerConfigWrapper
 func (w *ContainerConfigWrapper) getHostConfig() *container.HostConfig {
 	hc := w.HostConfig
-
-	if hc == nil && w.InnerHostConfig != nil {
-		hc = w.InnerHostConfig
-	} else if hc != nil && w.InnerHostConfig != nil {
-		if hc.Memory != 0 && w.InnerHostConfig.Memory == 0 {
-			w.InnerHostConfig.Memory = hc.Memory
-		}
-		if hc.MemorySwap != 0 && w.InnerHostConfig.MemorySwap == 0 {
-			w.InnerHostConfig.MemorySwap = hc.MemorySwap
-		}
-		if hc.CPUShares != 0 && w.InnerHostConfig.CPUShares == 0 {
-			w.InnerHostConfig.CPUShares = hc.CPUShares
-		}
-		if hc.CpusetCpus != "" && w.InnerHostConfig.CpusetCpus == "" {
-			w.InnerHostConfig.CpusetCpus = hc.CpusetCpus
-		}
-
-		if hc.VolumeDriver != "" && w.InnerHostConfig.VolumeDriver == "" {
-			w.InnerHostConfig.VolumeDriver = hc.VolumeDriver
-		}
-
-		hc = w.InnerHostConfig
-	}
-
-	if hc != nil {
-		if w.Cpuset != "" && hc.CpusetCpus == "" {
-			hc.CpusetCpus = w.Cpuset
-		}
-	}
 
 	// Make sure NetworkMode has an acceptable value. We do this to ensure
 	// backwards compatible API behavior.
