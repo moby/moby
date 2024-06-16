e42503213da4de99a9c6f8db60a3745960b15984
Sebastiaan van Stijn <github@gone.nl>
2024-06-16 21:59:32 +0200

    runconfig: ContainerConfigWrapper: unify Linux and Windows implementations
    Now that the backward-compatibility code has been removed, the Linux and
    Windows implementations of this struct are identical, so the platform-
    specific code can be removed.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- runconfig/config.go
+++ runconfig/config.go
@@ -9,6 +9,14 @@ import (
 	"github.com/docker/docker/pkg/sysinfo"
 )
 
+// ContainerConfigWrapper is a Config wrapper that holds the container Config (portable)
+// and the corresponding HostConfig (non-portable).
+type ContainerConfigWrapper struct {
+	*container.Config
+	HostConfig       *container.HostConfig          `json:"HostConfig,omitempty"`
+	NetworkingConfig *networktypes.NetworkingConfig `json:"NetworkingConfig,omitempty"`
+}
+
 // ContainerDecoder implements httputils.ContainerDecoder
 // calling DecodeContainerConfig.
 type ContainerDecoder struct {
--- runconfig/config_unix.go
+++ runconfig/config_unix.go
@@ -2,18 +2,7 @@
 
 package runconfig // import "github.com/docker/docker/runconfig"
 
-import (
-	"github.com/docker/docker/api/types/container"
-	networktypes "github.com/docker/docker/api/types/network"
-)
-
-// ContainerConfigWrapper is a Config wrapper that holds the container Config (portable)
-// and the corresponding HostConfig (non-portable).
-type ContainerConfigWrapper struct {
-	*container.Config
-	HostConfig       *container.HostConfig          `json:"HostConfig,omitempty"`
-	NetworkingConfig *networktypes.NetworkingConfig `json:"NetworkingConfig,omitempty"`
-}
+import "github.com/docker/docker/api/types/container"
 
 // getHostConfig gets the HostConfig of the Config.
 // It's mostly there to handle Deprecated fields of the ContainerConfigWrapper
