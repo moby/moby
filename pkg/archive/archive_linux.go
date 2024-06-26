c2ae605ee592688d5629f946fd35e79906239d22
Sebastiaan van Stijn <github@gone.nl>
2024-06-26 02:08:00 +0200

    pkg/archive: reformat code to make #nosec comment work again
    Looks like the way it picks up #nosec comments changed, causing the
    linter error to re-appear;
    
        pkg/archive/archive_linux.go:57:17: G305: File traversal when extracting zip/tar archive (gosec)
                        Name:       filepath.Join(hdr.Name, WhiteoutOpaqueDir),
                                    ^
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit d4160d5aa75100eba75a6099f376ffdf9087e480)
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

--- pkg/archive/archive_linux.go
+++ pkg/archive/archive_linux.go
@@ -48,7 +48,7 @@ func (overlayWhiteoutConverter) ConvertWrite(hdr *tar.Header, path string, fi os
 			wo = &tar.Header{
 				Typeflag:   tar.TypeReg,
 				Mode:       hdr.Mode & int64(os.ModePerm),
-				Name:       filepath.Join(hdr.Name, WhiteoutOpaqueDir),
+				Name:       filepath.Join(hdr.Name, WhiteoutOpaqueDir), // #nosec G305 -- An archive is being created, not extracted.
 				Size:       0,
 				Uid:        hdr.Uid,
 				Uname:      hdr.Uname,
@@ -56,7 +56,7 @@ func (overlayWhiteoutConverter) ConvertWrite(hdr *tar.Header, path string, fi os
 				Gname:      hdr.Gname,
 				AccessTime: hdr.AccessTime,
 				ChangeTime: hdr.ChangeTime,
-			} //#nosec G305 -- An archive is being created, not extracted.
+			}
 		}
 	}
 
