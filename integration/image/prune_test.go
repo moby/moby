e4c2eb9d8a4c3a50797a9123219bd9d94c53fe12
Paweł Gronowski <pawel.gronowski@docker.com>
2024-06-26 15:50:13 +0200

    c8d/prune: Keep the last tagged image instead of creating dangling image
    Don't turn images into dangling when they are used by containers created
    with an image specified by an ID only (e.g. `docker run 82d1e9d`).
    
    Keep the last image reference with the same target when all other
    references would be pruned.
    
    If the container was created with a digested and tagged reference (e.g.
    `docker run alpine:latest@sha256:82d1e9d7ed48a7523bdebc18cf6290bdb97b82302a8a9c27d4fe885949ea94d1`),
    the `alpine:latest` image won't get untagged.
    
    This change makes the behavior consistent with the graphdriver
    implementation.
    
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- integration/image/prune_test.go
+++ integration/image/prune_test.go
@@ -4,11 +4,16 @@ import (
 	"strings"
 	"testing"
 
+	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
 	"github.com/docker/docker/integration/internal/container"
 	"github.com/docker/docker/internal/testutils/specialimage"
+	"github.com/docker/docker/testutil"
 	"github.com/docker/docker/testutil/daemon"
 	"gotest.tools/v3/assert"
+	is "gotest.tools/v3/assert/cmp"
 	"gotest.tools/v3/skip"
 )
 
@@ -46,4 +51,133 @@ func TestPruneDontDeleteUsedDangling(t *testing.T) {
 
 	_, _, err = client.ImageInspectWithRaw(ctx, danglingID)
 	assert.NilError(t, err, "Test dangling image should still exist")
+}
+
+// Regression test for https://github.com/moby/moby/issues/48063
+func TestPruneDontDeleteUsedImage(t *testing.T) {
+	skip.If(t, testEnv.DaemonInfo.OSType == "windows", "cannot start multiple daemons on windows")
+	skip.If(t, testEnv.IsRemoteDaemon, "cannot run daemon when remote daemon")
+
+	ctx := setupTest(t)
+
+	for _, env := range []struct {
+		name    string
+		prepare func(t *testing.T, client *daemon.Daemon, apiClient *client.Client) error
+		check   func(t *testing.T, apiClient *client.Client, pruned image.PruneReport)
+	}{
+		{
+			// Container uses the busybox:latest image and it's the only image
+			// tag with the same target.
+			name: "single tag",
+			check: func(t *testing.T, apiClient *client.Client, pruned image.PruneReport) {
+				assert.Check(t, is.Len(pruned.ImagesDeleted, 0))
+
+				_, _, err := apiClient.ImageInspectWithRaw(ctx, "busybox:latest")
+				assert.NilError(t, err, "Busybox image should still exist")
+			},
+		},
+		{
+			// Container uses the busybox:latest image and there's also a second
+			// busybox:other tag pointing to the same image.
+			name: "two tags",
+			prepare: func(t *testing.T, d *daemon.Daemon, apiClient *client.Client) error {
+				return apiClient.ImageTag(ctx, "busybox:latest", "busybox:other")
+			},
+			check: func(t *testing.T, apiClient *client.Client, pruned image.PruneReport) {
+				if assert.Check(t, is.Len(pruned.ImagesDeleted, 1)) {
+					assert.Check(t, is.Equal(pruned.ImagesDeleted[0].Deleted, ""))
+					t.Log("Untagged", pruned.ImagesDeleted[0].Untagged)
+				}
+
+				_, _, err1 := apiClient.ImageInspectWithRaw(ctx, "busybox:latest")
+				_, _, err2 := apiClient.ImageInspectWithRaw(ctx, "busybox:other")
+
+				assert.Check(t, err1 != err2 && (err1 == nil || err2 == nil), "One of the images should still exist")
+			},
+		},
+	} {
+		for _, tc := range []struct {
+			name    string
+			imageID func(t *testing.T, inspect image.InspectResponse) string
+		}{
+			{
+				name: "full id",
+				imageID: func(t *testing.T, inspect image.InspectResponse) string {
+					return inspect.ID
+				},
+			},
+			{
+				name: "full id without sha256 prefix",
+				imageID: func(t *testing.T, inspect image.InspectResponse) string {
+					return strings.TrimPrefix(inspect.ID, "sha256:")
+				},
+			},
+			{
+				name: "truncated id (without sha256 prefix)",
+				imageID: func(t *testing.T, inspect image.InspectResponse) string {
+					return strings.TrimPrefix(inspect.ID, "sha256:")[:8]
+				},
+			},
+			{
+				name: "repo and digest without tag",
+				imageID: func(t *testing.T, inspect image.InspectResponse) string {
+					skip.If(t, !testEnv.UsingSnapshotter())
+
+					return "busybox@" + inspect.ID
+				},
+			},
+			{
+				name: "tagged and digested",
+				imageID: func(t *testing.T, inspect image.InspectResponse) string {
+					skip.If(t, !testEnv.UsingSnapshotter())
+
+					return "busybox:latest@" + inspect.ID
+				},
+			},
+			{
+				name: "repo digest",
+				imageID: func(t *testing.T, inspect image.InspectResponse) string {
+					// graphdriver won't have a repo digest
+					skip.If(t, len(inspect.RepoDigests) == 0, "no repo digest")
+
+					return inspect.RepoDigests[0]
+				},
+			},
+		} {
+			tc := tc
+			t.Run(env.name+"/"+tc.name, func(t *testing.T) {
+				ctx := testutil.StartSpan(ctx, t)
+				d := daemon.New(t)
+				d.Start(t)
+				defer d.Stop(t)
+
+				apiClient := d.NewClientT(t)
+				defer apiClient.Close()
+
+				d.LoadBusybox(ctx, t)
+
+				if env.prepare != nil {
+					err := env.prepare(t, d, apiClient)
+					assert.NilError(t, err, "prepare failed")
+				}
+
+				inspect, _, err := apiClient.ImageInspectWithRaw(ctx, "busybox:latest")
+				assert.NilError(t, err)
+
+				image := tc.imageID(t, inspect)
+				t.Log(image)
+
+				cid := container.Run(ctx, t, apiClient,
+					container.WithImage(image),
+					container.WithCmd("sleep", "60"))
+				defer container.Remove(ctx, t, apiClient, cid, containertypes.RemoveOptions{Force: true})
+
+				// dangling=false also prunes unused images
+				pruned, err := apiClient.ImagesPrune(ctx, filters.NewArgs(filters.Arg("dangling", "false")))
+				assert.NilError(t, err)
+
+				env.check(t, apiClient, pruned)
+			})
+		}
+	}
 }
