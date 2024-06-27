03dc388f6332fabab8217d5506c9aac7009034a9
Paweł Gronowski <pawel.gronowski@docker.com>
2024-06-27 17:42:43 +0200

    c8d/build: Log `image tag` event when image was built with Buildkit
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>
    (cherry picked from commit 53bc396ef48058290007dc213622d991b159695a)
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/build.go
+++ daemon/build.go
@@ -3,6 +3,8 @@ package daemon
 import (
 	"context"
 
+	"github.com/distribution/reference"
+	"github.com/docker/docker/api/types/events"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
@@ -11,5 +13,14 @@ import (
 // When no tag is given, buildkit doesn't call the image service so it has no
 // way of knowing the image was created.
 func (daemon *Daemon) ImageExportedByBuildkit(ctx context.Context, id string, desc ocispec.Descriptor) {
-	daemon.imageService.LogImageEvent(id, id, "create")
+	daemon.imageService.LogImageEvent(id, id, events.ActionCreate)
+}
+
+// ImageNamedByBuildkit is a callback that is called when an image is tagged by buildkit.
+// Note: It is only called if the buildkit didn't call the image service itself to perform the tagging.
+// Currently this only happens when the containerd image store is used.
+func (daemon *Daemon) ImageNamedByBuildkit(ctx context.Context, ref reference.NamedTagged, desc ocispec.Descriptor) {
+	id := desc.Digest.String()
+	name := reference.FamiliarString(ref)
+	daemon.imageService.LogImageEvent(id, name, events.ActionTag)
 }
