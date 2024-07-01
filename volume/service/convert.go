3a3bb1cb501d991f995f7c66e1707d1ccb0a1e17
Sebastiaan van Stijn <github@gone.nl>
2024-07-01 20:38:18 +0200

    pkg/directory: deprecate, and move to internal
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- volume/service/convert.go
+++ volume/service/convert.go
@@ -10,7 +10,7 @@ import (
 	"github.com/docker/docker/api/types/filters"
 	volumetypes "github.com/docker/docker/api/types/volume"
 	"github.com/docker/docker/errdefs"
-	"github.com/docker/docker/pkg/directory"
+	"github.com/docker/docker/internal/directory"
 	"github.com/docker/docker/volume"
 )
 
