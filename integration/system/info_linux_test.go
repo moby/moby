5e4ddd81a2dd0eae2a18671df02f5ae7e4ab3f9b
Sebastiaan van Stijn <github@gone.nl>
2024-07-02 01:29:36 +0200

    api/types/system: remove Info.ExecutionDriver
    The execution-driver was replaced with containerd since docker 1.11 (API
    v1.23) in 9c4570a958df42d1ad19364b1a8da55b891d850a, after which the value
    was no longer set. The field was left in the type definition.
    Commit 1fb1136fecfd761300a38f64ac9178979cc0b270 removed its use from the
    CLI and [docker/engine-api@39c7d7e] removed it from the API type, followed
    by an update to the API docs in 3c6ef4c29d28e92ea29816d6117412162d829c60.
    
    Changes to the API types were not pulled into the engine until v1.13, and
    probably because of that gated it on API version < 1.25 instead of < 1.24
    (see 6d98e344c7702a8a713cb9e02a19d83a79d3f930); setting a "not supported"
    value for older versions.
    
    Based on the above; this field was deprecated in API v1.23, and empty
    since then. Given that the minimum API version supported by the engine
    is not v1.24, we can safely remove it.
    
    [docker/engine-api@39c7d7e]: https://github.com/docker/engine-api/commit/39c7d7ec192b065f757ffdb85b36a7b3ede4e10b
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit e4d792a06d7568b1136cba88749fc83041b12555)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- integration/system/info_linux_test.go
+++ integration/system/info_linux_test.go
@@ -3,11 +3,8 @@
 package system // import "github.com/docker/docker/integration/system"
 
 import (
-	"net/http"
 	"testing"
 
-	"github.com/docker/docker/testutil"
-	req "github.com/docker/docker/testutil/request"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
 )
@@ -27,21 +24,4 @@ func TestInfoBinaryCommits(t *testing.T) {
 
 	assert.Check(t, "N/A" != info.RuncCommit.ID)
 	assert.Check(t, is.Equal(info.RuncCommit.Expected, info.RuncCommit.ID))
-}
-
-func TestInfoAPIVersioned(t *testing.T) {
-	ctx := testutil.StartSpan(baseContext, t)
-
-	res, body, err := req.Get(ctx, "/v1.24/info")
-	assert.NilError(t, err)
-	assert.Check(t, is.DeepEqual(res.StatusCode, http.StatusOK))
-
-	b, err := req.ReadBody(body)
-	assert.NilError(t, err)
-
-	// Verify the old response on API 1.24 and older before commit
-	// 6d98e344c7702a8a713cb9e02a19d83a79d3f930.
-	out := string(b)
-	assert.Check(t, is.Contains(out, "ExecutionDriver"))
-	assert.Check(t, is.Contains(out, "not supported"))
 }
