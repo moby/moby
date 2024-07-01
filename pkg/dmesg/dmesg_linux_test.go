b7d26f2f8f46c56665ffec3410082bc6c1c5bc9c
Sebastiaan van Stijn <github@gone.nl>
2024-07-01 20:55:06 +0200

    pkg/dmesg: remove deprecated package
    This package was deprecated in 805ccd23656e301fdcd007d63a0b25be9d8b267b
    and is no longer used.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/dmesg/dmesg_linux_test.go
+++
@@ -1,9 +0,0 @@
-package dmesg // import "github.com/docker/docker/pkg/dmesg"
-
-import (
-	"testing"
-)
-
-func TestDmesg(t *testing.T) {
-	t.Logf("dmesg output follows:\n%v", string(Dmesg(512)))
-}
