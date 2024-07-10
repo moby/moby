dac7ffa3404138a4f291c16586e5a2c68dad4151
Rob Murray <rob.murray@docker.com>
2024-07-10 17:42:38 +0100

    Remove rootlesskit-docker-proxy
    It's not needed, now the daemon tells RootlessKit about port
    mappings directly.
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- pkg/rootless/rootless.go
+++ pkg/rootless/rootless.go
@@ -2,9 +2,6 @@ package rootless // import "github.com/docker/docker/pkg/rootless"
 
 import "os"
 
-// RootlessKitDockerProxyBinary is the binary name of rootlesskit-docker-proxy
-const RootlessKitDockerProxyBinary = "rootlesskit-docker-proxy"
-
 // RunningWithRootlessKit returns true if running under RootlessKit namespaces.
 func RunningWithRootlessKit() bool {
 	return os.Getenv("ROOTLESSKIT_STATE_DIR") != ""
