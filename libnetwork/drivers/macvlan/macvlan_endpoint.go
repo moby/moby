f57e0dc2ffcffa0bbe98c2a6002d5f4152f47e4b
Rob Murray <rob.murray@docker.com>
2024-07-12 17:25:04 +0100

    Fix some macvlan error types for the API
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/drivers/macvlan/macvlan_endpoint.go
+++ libnetwork/drivers/macvlan/macvlan_endpoint.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/containerd/log"
+	"github.com/docker/docker/errdefs"
 	"github.com/docker/docker/libnetwork/driverapi"
 	"github.com/docker/docker/libnetwork/netlabel"
 	"github.com/docker/docker/libnetwork/netutils"
@@ -21,7 +22,7 @@ func (d *driver) CreateEndpoint(ctx context.Context, nid, eid string, ifInfo dri
 	}
 	n, err := d.getNetwork(nid)
 	if err != nil {
-		return fmt.Errorf("network id %q not found", nid)
+		return errdefs.System(fmt.Errorf("network id %q not found", nid))
 	}
 	ep := &endpoint{
 		id:     eid,
@@ -31,7 +32,7 @@ func (d *driver) CreateEndpoint(ctx context.Context, nid, eid string, ifInfo dri
 		mac:    ifInfo.MacAddress(),
 	}
 	if ep.addr == nil && ep.addrv6 == nil {
-		return fmt.Errorf("create endpoint was not passed an IP address")
+		return errdefs.InvalidParameter(fmt.Errorf("create endpoint was not passed an IP address"))
 	}
 	if ep.mac == nil {
 		if ep.addr != nil {
