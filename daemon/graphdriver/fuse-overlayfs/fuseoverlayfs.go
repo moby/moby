a5736eea3eb1b8601219096108c5c5bacf29c121
Sebastiaan van Stijn <github@gone.nl>
2024-07-22 18:54:04 +0200

    daemon/graphdriver/fuse-overlayfs: remove kernel-version check for < 4.18.0
    This check was in place for CentOS/RHEL 7, which use kernel 3.10. Now
    that both reached EOL, and we stopped packaging for those distros, we
    can remove this condition. RHEL 8 uses kernel 4.18, so does not need
    this check.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/graphdriver/fuse-overlayfs/fuseoverlayfs.go
+++ daemon/graphdriver/fuse-overlayfs/fuseoverlayfs.go
@@ -19,7 +19,6 @@ import (
 	"github.com/docker/docker/daemon/internal/fstype"
 	"github.com/docker/docker/daemon/internal/mountref"
 	"github.com/docker/docker/internal/containerfs"
-	"github.com/docker/docker/pkg/parsers/kernel"
 	"github.com/moby/go-archive"
 	"github.com/moby/go-archive/chrootarchive"
 	"github.com/moby/locker"
@@ -76,9 +75,6 @@ func init() {
 func Init(home string, options []string, idMap user.IdentityMapping) (graphdriver.Driver, error) {
 	if _, err := exec.LookPath(binary); err != nil {
 		logger.Error(err)
-		return nil, graphdriver.ErrNotSupported
-	}
-	if !kernel.CheckKernelVersion(4, 18, 0) {
 		return nil, graphdriver.ErrNotSupported
 	}
 
