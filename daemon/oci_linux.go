a19c2ccc7b87b4a8106e2f6fc5b90a45f34739dc
Sebastiaan van Stijn <github@gone.nl>
2024-07-26 13:16:10 +0200

    daemon: rename "mounts" type to reduce shadowing
    Use a more distinct name, so that local variables can use it. While
    at it, also added GoDoc to describe its functionality.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/oci_linux.go
+++ daemon/oci_linux.go
@@ -506,11 +506,9 @@ func inSlice(slice []string, s string) bool {
 }
 
 // withMounts sets the container's mounts
-func withMounts(daemon *Daemon, daemonCfg *configStore, c *container.Container, ms []container.Mount) coci.SpecOpts {
+func withMounts(daemon *Daemon, daemonCfg *configStore, c *container.Container, mounts []container.Mount) coci.SpecOpts {
 	return func(ctx context.Context, _ coci.Client, _ *containers.Container, s *coci.Spec) (err error) {
-		sortMounts(ms)
-
-		mounts := ms
+		sortMounts(mounts)
 
 		userMounts := make(map[string]struct{})
 		for _, m := range mounts {
