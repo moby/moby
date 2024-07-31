cdaa993299cf1a8ca8dd24a3683d8902ef5128e6
Rob Murray <rob.murray@docker.com>
2024-07-31 16:50:55 +0100

    EnableIPv4:false is no longer --experimental
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- integration/networking/bridge_linux_test.go
+++ integration/networking/bridge_linux_test.go
@@ -624,7 +624,7 @@ func TestDisableIPv6Addrs(t *testing.T) {
 // IPv4 addresses.
 func TestDisableIPv4(t *testing.T) {
 	ctx := setupTest(t)
-	d := daemon.New(t, daemon.WithExperimental())
+	d := daemon.New(t)
 	d.StartWithBusybox(ctx, t)
 	defer d.Stop(t)
 
@@ -970,7 +970,7 @@ func TestGatewaySelection(t *testing.T) {
 	skip.If(t, testEnv.IsRootless, "proxies run in child namespace")
 
 	ctx := setupTest(t)
-	d := daemon.New(t, daemon.WithExperimental())
+	d := daemon.New(t)
 	d.StartWithBusybox(ctx, t)
 	defer d.Stop(t)
 	c := d.NewClientT(t)
