6b65cb9b225fd674f9f558f6d2b7c71369b8686f
Cory Snider <csnider@mirantis.com>
2024-08-06 18:24:30 -0400

    man: add tooling to build dockerd man page
    Signed-off-by: Cory Snider <csnider@mirantis.com>

--- man/md2man-all.sh
+++ man/md2man-all.sh
@@ -9,14 +9,17 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 	pwd
 }
 
+mkdir -p build
+GO111MODULE=auto go build -o build/go-md2man github.com/cpuguy83/go-md2man/v2
+
 for FILE in *.md; do
 	base="$(basename "$FILE")"
 	name="${base%.md}"
 	num="${name##*.}"
-	if [ -z "$num" -o "$name" = "$num" ]; then
+	if [[ -z "$num" || "$name" = "$num" ]]; then
 		# skip files that aren't of the format xxxx.N.md (like README.md)
 		continue
 	fi
 	mkdir -p "./man${num}"
-	go-md2man -in "$FILE" -out "./man${num}/${name}"
+	build/go-md2man -in "$FILE" -out "./man${num}/${name}"
 done
---
+++ man/vendor/github.com/cpuguy83/go-md2man/v2/README.md
@@ -0,0 +1,15 @@
+go-md2man
+=========
+
+Converts markdown into roff (man pages).
+
+Uses blackfriday to process markdown into man pages.
+
+### Usage
+
+./md2man -in /path/to/markdownfile.md -out /manfile/output/path
+
+### How to contribute
+
+We use go modules to manage dependencies.
+As such you must be using at lest go1.11.
