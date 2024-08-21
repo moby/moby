c4c2efb8557563b0cfea828c0904b25d82c12b3c
Sebastiaan van Stijn <github@gone.nl>
2024-08-21 16:48:51 +0200

    volume/testutils: fix non-constant format string in call (govet)
        volume/testutils/testutils.go:98:26: printf: non-constant format string in call to fmt.Errorf (govet)
                return nil, fmt.Errorf(opts["error"])
                                       ^
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit f434cdd14a99c38dc47f55680f624b4bae44e196)
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

--- volume/testutils/testutils.go
+++ volume/testutils/testutils.go
@@ -95,7 +95,7 @@ func (d *FakeDriver) Name() string { return d.name }
 // It returns an error if the options include an "error" key with a message
 func (d *FakeDriver) Create(name string, opts map[string]string) (volume.Volume, error) {
 	if opts != nil && opts["error"] != "" {
-		return nil, fmt.Errorf(opts["error"])
+		return nil, errors.New(opts["error"])
 	}
 	v := NewFakeVolume(name, d.name)
 	d.vols[name] = v
@@ -105,7 +105,7 @@ func (d *FakeDriver) Create(name string, opts map[string]string) (volume.Volume,
 // Remove deletes a volume.
 func (d *FakeDriver) Remove(v volume.Volume) error {
 	if _, exists := d.vols[v.Name()]; !exists {
-		return fmt.Errorf("no such volume")
+		return errors.New("no such volume")
 	}
 	delete(d.vols, v.Name())
 	return nil
@@ -125,7 +125,7 @@ func (d *FakeDriver) Get(name string) (volume.Volume, error) {
 	if v, exists := d.vols[name]; exists {
 		return v, nil
 	}
-	return nil, fmt.Errorf("no such volume")
+	return nil, errors.New("no such volume")
 }
 
 // Scope returns the local scope
