2f0180934d5420001e90c71133435d92822edc8b
Sebastiaan van Stijn <github@gone.nl>
2024-08-23 21:59:22 +0200

    vendor: tags.cncf.io/container-device-interface v0.8.0
    Breaking change: The .ToOCI() functions in the specs-go package have been
    removed. This removes the dependency on the OCI runtime specification from
    the CDI specification definition itself.
    
    What's Changed
    
    - Add workflow to mark prs and issues as stale
    - Remove the ToOCI functions from the specs-go package
    - docs: add a pointer to community meetings in our docs.
    - Bump spec version to v0.8.0
    - Update spec version in README
    
    Full diff:  https://github.com/cncf-tags/container-device-interface/compare/v0.7.2...v0.8.0
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/tags.cncf.io/container-device-interface/specs-go/config.go
+++ vendor/tags.cncf.io/container-device-interface/specs-go/config.go
@@ -3,7 +3,7 @@ package specs
 import "os"
 
 // CurrentVersion is the current version of the Spec.
-const CurrentVersion = "0.7.0"
+const CurrentVersion = "0.8.0"
 
 // Spec is the base configuration for CDI
 type Spec struct {
