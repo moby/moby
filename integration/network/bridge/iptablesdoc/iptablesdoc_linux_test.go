28aca3c7607b2704ece3dc37e906459898816c7d
Rob Murray <rob.murray@docker.com>
2024-10-10 18:51:13 +0100

    Add iptables doc for a network with icc=false
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- integration/network/bridge/iptablesdoc/iptablesdoc_linux_test.go
+++ integration/network/bridge/iptablesdoc/iptablesdoc_linux_test.go
@@ -95,6 +95,20 @@ var index = []section{
 			},
 		}},
 	},
+	{
+		name:            "usernet-portmap-noicc.md",
+		noUserlandProxy: true,
+		networks: []bridgeNetwork{{
+			bridge: "bridge1",
+			noICC:  true,
+			containers: []ctr{
+				{
+					name:         "c1",
+					portMappings: nat.PortMap{"80/tcp": {{HostPort: "8080"}}},
+				},
+			},
+		}},
+	},
 }
 
 // iptCmdType is used to look up iptCmds in the markdown (can't use an int
