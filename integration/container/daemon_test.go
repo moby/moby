91f74c2fe04f6d5901c8bf8dbe5d6f20ab4c464d
Rob Murray <rob.murray@docker.com>
2024-10-11 13:59:03 +0100

    Disable iptables/ip6tables in two tests to remove conflict
    TestAttachDisconnectLeak starts its own daemon with iptables disabled,
    but disabling ip6tables was missed when we enabled ip6tables by default.
    
    TestNetworkStateCleanupOnDaemonStart also starts its own daemon, with
    iptables and ip6tables both enabled. It isn't trying to test anything
    iptables related.
    
    These tests run in parallel, so they both modify ip6tables in the host
    namespace - and could break each other by adding/removing chains at
    awkward moments.
    
    Disable iptables and ip6tables in both tests.
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- integration/container/daemon_test.go
+++ integration/container/daemon_test.go
@@ -67,7 +67,7 @@ func TestNetworkStateCleanupOnDaemonStart(t *testing.T) {
 	d := daemon.New(t)
 	defer d.Cleanup(t)
 
-	d.StartWithBusybox(ctx, t)
+	d.StartWithBusybox(ctx, t, "--iptables=false", "--ip6tables=false")
 	defer d.Stop(t)
 
 	apiClient := d.NewClientT(t)
