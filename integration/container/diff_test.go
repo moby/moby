bdb21cd779499a5d8191488533c9a0859641b785
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2024-10-22 00:16:26 +0900

    integration: add wait
    Cherry-picked several WIP commits from
    https://github.com/moby/moby/commits/b0a592798f4d9d7162f8aedca89ada3a29d60e2c/
    
    Originally-authored-by: Rodrigo Campos <rodrigoca@microsoft.com>
    Co-Authored-by: Kir Kolyshkin <kolyshkin@gmail.com>
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit fb6e650ab9dec7f9e8a67b278104881f03f63d08)
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

--- integration/container/diff_test.go
+++ integration/container/diff_test.go
@@ -23,6 +23,7 @@ func TestDiff(t *testing.T) {
 		{Kind: containertypes.ChangeAdd, Path: "/foo/bar"},
 	}
 
+	poll.WaitOn(t, container.IsStopped(ctx, apiClient, cID))
 	items, err := apiClient.ContainerDiff(ctx, cID)
 	assert.NilError(t, err)
 	assert.DeepEqual(t, expected, items)
