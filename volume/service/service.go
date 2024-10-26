352b4ff2f1acc17e899886d8f806a41e62d58722
Sebastiaan van Stijn <github@gone.nl>
2024-10-26 17:38:58 +0200

    volume: VolumesService.Create: fix log-level for debug logs
    These log-entries were added in 10d57fde4497fb1e141f2020697528acece38425,
    but it looks like I accidentally left them as Error-logs following some
    debugging (whoops!).
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- volume/service/service.go
+++ volume/service/service.go
@@ -73,9 +73,9 @@ func (s *VolumesService) Create(ctx context.Context, name, driverName string, op
 	if name == "" {
 		name = stringid.GenerateRandomID()
 		options = append(options, opts.WithCreateLabel(AnonymousLabel, ""))
-		log.G(ctx).WithField("volume-name", name).Error("Creating anonymous volume")
+		log.G(ctx).WithField("volume-name", name).Debug("Creating anonymous volume")
 	} else {
-		log.G(ctx).WithField("volume-name", name).Error("Creating named volume")
+		log.G(ctx).WithField("volume-name", name).Debug("Creating named volume")
 	}
 	v, err := s.vs.Create(ctx, name, driverName, options...)
 	if err != nil {
