71c3c977b3cb4c5aedade23779aa0fadc38e0afc
Paweł Gronowski <pawel.gronowski@docker.com>
2024-10-28 12:44:16 +0100

    daemon/images: Make ImageDelete take opts struct
    Pass whole `RemoveOptions` struct instead of individual `force` and
    `prune` parameters.
    
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/images/image_delete.go
+++ daemon/images/image_delete.go
@@ -62,7 +62,7 @@ const (
 // If prune is true, ancestor images will each attempt to be deleted quietly,
 // meaning any delete conflicts will cause the image to not be deleted and the
 // conflict will not be reported.
-func (i *ImageService) ImageDelete(ctx context.Context, imageRef string, force, prune bool) ([]imagetypes.DeleteResponse, error) {
+func (i *ImageService) ImageDelete(ctx context.Context, imageRef string, options imagetypes.RemoveOptions) ([]imagetypes.DeleteResponse, error) {
 	start := time.Now()
 	records := []imagetypes.DeleteResponse{}
 
@@ -90,6 +90,8 @@ func (i *ImageService) ImageDelete(ctx context.Context, imageRef string, force, 
 		return false
 	}
 
+	force := options.Force
+	prune := options.PruneChildren
 	var removedRepositoryRef bool
 	if !isImageIDPrefix(imgID.String(), imageRef) {
 		// A repository reference was given and should be removed
