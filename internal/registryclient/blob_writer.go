5dcbb92f17c95b3f4b22fe3e5e4ae70902e5cbb2
Sebastiaan van Stijn <github@gone.nl>
2024-10-31 12:24:55 +0100

    internal/registryclient: remove uses of deprecated io/ioutil
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- internal/registryclient/blob_writer.go
+++ internal/registryclient/blob_writer.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -36,7 +35,7 @@ func (hbu *httpBlobUpload) handleErrorResponse(resp *http.Response) error {
 }
 
 func (hbu *httpBlobUpload) ReadFrom(r io.Reader) (n int64, err error) {
-	req, err := http.NewRequest("PATCH", hbu.location, ioutil.NopCloser(r))
+	req, err := http.NewRequest("PATCH", hbu.location, io.NopCloser(r))
 	if err != nil {
 		return 0, err
 	}
