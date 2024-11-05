5cfd326aa40c14bc34c21e6b50f7f6797006de73
Sebastiaan van Stijn <github@gone.nl>
2024-11-05 12:54:49 +0100

    client: Client.ImageHistory: don't decorate error twice
    I forgot to include this patch in 96039276b66f1bb48d227d716f55d60a695b2c70,
    which introduced the encodePlatform, which already decorates the error to
    have a `invalid platform:` prefix.
    
    While updating, also be more explicit on no result being returned on error.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/image_history.go
+++ client/image_history.go
@@ -3,7 +3,6 @@ package client // import "github.com/docker/docker/client"
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/url"
 
 	"github.com/docker/docker/api/types/image"
@@ -19,18 +18,18 @@ func (cli *Client) ImageHistory(ctx context.Context, imageID string, opts image.
 
 		p, err := encodePlatform(opts.Platform)
 		if err != nil {
-			return nil, fmt.Errorf("invalid platform: %v", err)
+			return nil, err
 		}
 		query.Set("platform", p)
 	}
 
-	var history []image.HistoryResponseItem
 	serverResp, err := cli.get(ctx, "/images/"+imageID+"/history", query, nil)
 	defer ensureReaderClosed(serverResp)
 	if err != nil {
-		return history, err
+		return nil, err
 	}
 
+	var history []image.HistoryResponseItem
 	err = json.NewDecoder(serverResp.body).Decode(&history)
 	return history, err
 }
