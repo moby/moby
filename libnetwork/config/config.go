b453aa65fa30032359bf85e180b73b4663240ba3
Sebastiaan van Stijn <github@gone.nl>
2024-11-12 12:10:40 +0100

    update go:build tags to use go1.22
    commit a0807e7cfe0ac30dc37d50d5ad63259c897fa90f configured golangci-lint
    to use go1.23 semantics, which alowed linters like `copyloopvar` to lint
    using thee correct semantics.
    
    go1.22 now creates a copy of variables when assigned in a loop; make sure we
    don't have files that may downgrade semantics to go1.21 in case that also means
    disabling that feature; https://go.dev/ref/spec#Go_1.22
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libnetwork/config/config.go
+++ libnetwork/config/config.go
@@ -1,5 +1,5 @@
 // FIXME(thaJeztah): remove once we are a module; the go:build directive prevents go from downgrading language version to go1.16:
-//go:build go1.21
+//go:build go1.22
 
 package config
 
