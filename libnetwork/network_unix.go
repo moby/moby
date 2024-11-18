584538a159ba5007bbdd15d8647cd5d0085277f7
Rob Murray <rob.murray@docker.com>
2024-11-18 15:53:12 +0000

    Configurable count and interval for gratuitous ARP/NA messages
    The default is to send 3 messages at 1s intervals.
    
    That can be overridden in "docker network create" using:
      -o com.docker.network.advertise_addr_nmsgs=3
      -o com.docker.network.advertise_addr_interval=1s
    
    Or, in daemon.json for each driver:
      "default-network-opts": {
        "bridge": {
          "com.docker.network.advertise_addr_nmsgs": "3",
          "com.docker.network.advertise_addr_interval": "1s"
        }
      }
    
    The allowed range is 0-3 for the number of messages, and
    100ms-2s for the interval. Setting nmsgs to 0 disables the
    gratuitous ARP/NA messages.
    
    The default bridge will always use the built-in defaults,
    it cannot be configured.
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/network_unix.go
+++ libnetwork/network_unix.go
@@ -4,6 +4,12 @@ package libnetwork
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/docker/docker/libnetwork/netlabel"
+	"github.com/docker/docker/libnetwork/osl"
 
 	"github.com/docker/docker/libnetwork/ipams/defaultipam"
 )
@@ -31,4 +37,36 @@ func deleteEpFromResolver(epName string, epIface *EndpointInterface, resolvers [
 
 func defaultIpamForNetworkType(networkType string) string {
 	return defaultipam.DriverName
+}
+
+func (n *Network) validateAdvertiseAddrNMsgs() (*int, error) {
+	nMsgsStr, ok := n.DriverOptions()[netlabel.AdvertiseAddrNMsgs]
+	if !ok {
+		return nil, nil
+	}
+	nMsgs, err := strconv.Atoi(nMsgsStr)
+	if err != nil {
+		return nil, fmt.Errorf("value for option "+netlabel.AdvertiseAddrNMsgs+" %q must be an integer", nMsgsStr)
+	}
+	if nMsgs < osl.AdvertiseAddrNMsgsMin || nMsgs > osl.AdvertiseAddrNMsgsMax {
+		return nil, fmt.Errorf(netlabel.AdvertiseAddrNMsgs+" must be in the range %d to %d",
+			osl.AdvertiseAddrNMsgsMin, osl.AdvertiseAddrNMsgsMax)
+	}
+	return &nMsgs, nil
+}
+
+func (n *Network) validateAdvertiseAddrInterval() (*time.Duration, error) {
+	intervalStr, ok := n.DriverOptions()[netlabel.AdvertiseAddrInterval]
+	if !ok {
+		return nil, nil
+	}
+	interval, err := time.ParseDuration(intervalStr)
+	if err != nil {
+		return nil, fmt.Errorf("value for option "+netlabel.AdvertiseAddrInterval+" %q must be parseable by time.ParseDuration()", intervalStr)
+	}
+	if interval < osl.AdvertiseAddrIntervalMin || interval > osl.AdvertiseAddrIntervalMax {
+		return nil, fmt.Errorf(netlabel.AdvertiseAddrInterval+" must be in the range %v to %v",
+			osl.AdvertiseAddrIntervalMin, osl.AdvertiseAddrIntervalMax)
+	}
+	return &interval, nil
 }
