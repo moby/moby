8798eba504aaa46aaa143ccd0f75cf15a94ae140
Sebastiaan van Stijn <github@gone.nl>
2024-12-08 12:53:48 +0100

    registry: Service.lookupV2Endpoints: add arg to skip mirrors
    This function unconditionally constructed endpoints for mirrors when
    requesting endpoints for the default (Docker Hub) registry. Doing so
    involves validating the config, which involves;
    
    - parsing the hostname
    - constructing TLS config
    - performing a DNS lookup to resolve the host's IP address and matching
      it against CIDR masks for insecure registries.
    
    When looking up push endpoints or endpoints to consider for authentication,
    mirror endpoints were discarded to prevent sending credentials of the upstream
    registry to a mirror.
    
    This patch adds a "includeMirrors" argument to skip constructing endpoints
    for mirrors when not needed. While at it, also removing named output variables,
    as they didn't add much.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- registry/service_v2.go
+++ registry/service_v2.go
@@ -7,27 +7,30 @@ import (
 	"github.com/docker/go-connections/tlsconfig"
 )
 
-func (s *Service) lookupV2Endpoints(hostname string) (endpoints []APIEndpoint, err error) {
+func (s *Service) lookupV2Endpoints(hostname string, includeMirrors bool) ([]APIEndpoint, error) {
 	ana := s.config.allowNondistributableArtifacts(hostname)
 
+	var endpoints []APIEndpoint
 	if hostname == DefaultNamespace || hostname == IndexHostname {
-		for _, mirror := range s.config.Mirrors {
-			if !strings.HasPrefix(mirror, "http://") && !strings.HasPrefix(mirror, "https://") {
-				mirror = "https://" + mirror
+		if includeMirrors {
+			for _, mirror := range s.config.Mirrors {
+				if !strings.HasPrefix(mirror, "http://") && !strings.HasPrefix(mirror, "https://") {
+					mirror = "https://" + mirror
+				}
+				mirrorURL, err := url.Parse(mirror)
+				if err != nil {
+					return nil, invalidParam(err)
+				}
+				mirrorTLSConfig, err := newTLSConfig(mirrorURL.Host, s.config.isSecureIndex(mirrorURL.Host))
+				if err != nil {
+					return nil, err
+				}
+				endpoints = append(endpoints, APIEndpoint{
+					URL:       mirrorURL,
+					Mirror:    true,
+					TLSConfig: mirrorTLSConfig,
+				})
 			}
-			mirrorURL, err := url.Parse(mirror)
-			if err != nil {
-				return nil, invalidParam(err)
-			}
-			mirrorTLSConfig, err := newTLSConfig(mirrorURL.Host, s.config.isSecureIndex(mirrorURL.Host))
-			if err != nil {
-				return nil, err
-			}
-			endpoints = append(endpoints, APIEndpoint{
-				URL:       mirrorURL,
-				Mirror:    true,
-				TLSConfig: mirrorTLSConfig,
-			})
 		}
 		endpoints = append(endpoints, APIEndpoint{
 			URL:       DefaultV2Registry,
