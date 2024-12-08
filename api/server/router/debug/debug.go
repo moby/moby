95cbfda5a09bd265173ec3faef2e827e81cd0986
Sebastiaan van Stijn <github@gone.nl>
2024-12-08 14:23:57 +0100

    api/server/router/debug: NewRouter: inline debugRouter.initRoutes
    Putting these in a separate methods didn't add real value; rewrite it
    to return a  struct literal.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/server/router/debug/debug.go
+++ api/server/router/debug/debug.go
@@ -10,27 +10,23 @@ import (
 	"github.com/docker/docker/api/server/router"
 )
 
-// NewRouter creates a new debug router
-// The debug router holds endpoints for debug the daemon, such as those for pprof.
-func NewRouter() router.Router {
-	r := &debugRouter{}
-	r.initRoutes()
-	return r
-}
-
 type debugRouter struct {
 	routes []router.Route
 }
 
-func (r *debugRouter) initRoutes() {
-	r.routes = []router.Route{
-		router.NewGetRoute("/vars", frameworkAdaptHandler(expvar.Handler())),
-		router.NewGetRoute("/pprof/", frameworkAdaptHandlerFunc(pprof.Index)),
-		router.NewGetRoute("/pprof/cmdline", frameworkAdaptHandlerFunc(pprof.Cmdline)),
-		router.NewGetRoute("/pprof/profile", frameworkAdaptHandlerFunc(pprof.Profile)),
-		router.NewGetRoute("/pprof/symbol", frameworkAdaptHandlerFunc(pprof.Symbol)),
-		router.NewGetRoute("/pprof/trace", frameworkAdaptHandlerFunc(pprof.Trace)),
-		router.NewGetRoute("/pprof/{name}", handlePprof),
+// NewRouter creates a new debug router
+// The debug router holds endpoints for debug the daemon, such as those for pprof.
+func NewRouter() router.Router {
+	return &debugRouter{
+		routes: []router.Route{
+			router.NewGetRoute("/vars", frameworkAdaptHandler(expvar.Handler())),
+			router.NewGetRoute("/pprof/", frameworkAdaptHandlerFunc(pprof.Index)),
+			router.NewGetRoute("/pprof/cmdline", frameworkAdaptHandlerFunc(pprof.Cmdline)),
+			router.NewGetRoute("/pprof/profile", frameworkAdaptHandlerFunc(pprof.Profile)),
+			router.NewGetRoute("/pprof/symbol", frameworkAdaptHandlerFunc(pprof.Symbol)),
+			router.NewGetRoute("/pprof/trace", frameworkAdaptHandlerFunc(pprof.Trace)),
+			router.NewGetRoute("/pprof/{name}", handlePprof),
+		},
 	}
 }
 
