b42c6ad242919c89985182fad2e9f2ca0108f67f
Derek McGowan <derek@mcg.dev>
2024-12-12 22:37:29 -0800

    Update internal use of idtools to usergroup
    Signed-off-by: Derek McGowan <derek@mcg.dev>

---
+++ pkg/idtools/const_windows.go
@@ -0,0 +1,14 @@
+package idtools
+
+const (
+	// Deprecated: copy value locally
+	SeTakeOwnershipPrivilege = "SeTakeOwnershipPrivilege"
+)
+
+const (
+	// Deprecated: copy value locally
+	ContainerAdministratorSidString = "S-1-5-93-2-1"
+
+	// Deprecated: copy value locally
+	ContainerUserSidString = "S-1-5-93-2-2"
+)
