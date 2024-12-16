17f7c1a143c9f1dbf1b7f149268518963f8c0123
Paweł Gronowski <pawel.gronowski@docker.com>
2024-12-16 15:55:21 +0100

    container/rwLayer: Remove unused TarStreamer, ApplyDiff, Size and Name
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- container/rwlayer.go
+++ container/rwlayer.go
@@ -1,18 +1,7 @@
 package container
 
-import (
-	"io"
-
-	"github.com/docker/docker/layer"
-)
-
 // RWLayer represents a writable layer for a container.
 type RWLayer interface {
-	layer.TarStreamer
-
-	// Name of mounted layer
-	Name() string
-
 	// Mount mounts the RWLayer and returns the filesystem path
 	// to the writable layer.
 	Mount(mountLabel string) (string, error)
@@ -22,14 +11,6 @@ type RWLayer interface {
 	// this operation will only decrement the internal mount counter.
 	Unmount() error
 
-	// Size represents the size of the writable layer
-	// as calculated by the total size of the files
-	// changed in the mutable layer.
-	Size() (int64, error)
-
 	// Metadata returns the low level metadata for the mutable layer
 	Metadata() (map[string]string, error)
-
-	// ApplyDiff applies the diff to the RW layer
-	ApplyDiff(diff io.Reader) (int64, error)
 }
