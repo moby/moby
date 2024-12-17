46a8e5f8c5f7a70ce2751d5f99d895312279ff92
Sebastiaan van Stijn <github@gone.nl>
2024-12-17 10:50:55 +0100

    pkg/reexec: Command: separate public API from implementation
    Move the exported `Command` to a platform-agnostic file, and un-export
    the platform-specific implementations. This allows us to maintain the
    GoDoc in a single place, describing platform-specific differences where
    needed.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 8fd177d79b7db5f6e46beb78c57d31f62140646a)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/reexec/reexec_other.go
+++ pkg/reexec/reexec_other.go
@@ -6,12 +6,7 @@ import (
 	"os/exec"
 )
 
-// Command returns *exec.Cmd with its Path set to the path of the current
-// binary using the result of [Self]. For example if current binary is
-// "my-binary" at "/usr/bin/" (or "my-binary.exe" at "C:\" on Windows),
-// then cmd.Path is set to "/usr/bin/my-binary" and "C:\my-binary.exe"
-// respectively.
-func Command(args ...string) *exec.Cmd {
+func command(args ...string) *exec.Cmd {
 	return &exec.Cmd{
 		Path: Self(),
 		Args: args,
