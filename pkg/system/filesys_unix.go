4472e9b7f876bc0ec03289aece60590a4f46e7be
Sebastiaan van Stijn <github@gone.nl>
2024-12-21 14:15:21 +0100

    pkg/system: deprecate MkdirAll and remove custom volume GUID handling
    commit 86d1223a29907ffc6afba557b5138cfad7816bb4 introduced a custom version
    of `os.MkdirAll` for Windows to account for situations where the path to
    create would start with a Windows volume name (GUID path), for example,
    `"\\?\Volume{4c1b02c1-d990-11dc-99ae-806e6f6e6963}\`. At the time that patch
    was added we were using [go1.4.2], which did not have special handling
    for Windows in [MkdirAll], therefore would recognize such paths as regular
    paths, trying to create them, which would fail.
    
    This code was later updated in 46ec4c1ae2700ed638072fd7fb326afc10eded20
    to provide ACL (DACL) support on Windows.
    
    Further updates were made in cfef1b11e571ad2469cfa9631180db398f106468 and
    55ceb5047c304d3ac8ecd15801a4b0472832158e to allow for an early return when
    detecting a volume GUID path, and the code was re-aligned with the latest
    (go1.19.2) implementation in f058afc861c2f56bf9e97472e99df65c6493e694, which
    brought in the platform-specific [fixRootDirectory] handling introduced in
    go1.11. While that enhancement detected UNC volume-paths (`\\?c\`, `//?/c:`),
    it did not yet support volume GUID paths.
    
    go1.22, through [golang.org/cl/86295] added support for this, and `os.MkdirAll`
    now natively detects volume GUID paths, making our own implementation for
    this redundant.
    
    This patch:
    
    - Deprecates pkg/system.MkdirAll in favor of os.MkdirAll, which now provides
      the same functionality on go1.22 and up.
    - Renames the (non-exported) `mkdirall` function to `mkdirAllWithACL`, and
      synchronises `it` with the [implementation in go1.23.4], bringing in the
      changes from [golang.org/cl/86295] and [golang.org/cl/582499].
    - Adds a fast path to `MkdirAllWithACL` if no ACL / SDDL is provided.
    
    It's worth noting that we currently still support go1.22, and that the
    implementation changed in go1.23; those changes ([golang.org/cl/581517]
    and [golang.org/cl/566556]) were lateral moves, therefore should be
    identical to the implementation in go1.22, and we can safely use the
    implementation provided by [filepath.VolumeName] on either go1.22 or go1.23.
    
    [go1.4.2]: https://github.com/moby/moby/blob/86d1223a29907ffc6afba557b5138cfad7816bb4/Dockerfile#L77
    [MkdirAll]: https://github.com/golang/go/blob/go1.4.2/src/os/path.go#L19-L60
    [fixRootDirectory]: https://github.com/golang/go/commit/b86e76681366447798c94abb959bb60875bcc856
    [golang.org/cl/86295]: https://github.com/golang/go/commit/cd589c8a73415afbf94a8976f20cbed9d4061ba6
    [golang.org/cl/582499]: https://github.com/golang/go/commit/5616ab602566b0daa87dfd250a76c61960c4b634
    [golang.org/cl/581517]: https://github.com/golang/go/commit/ad22356ec660844ec43ccbe9a834845f1a6f7cf8
    [golang.org/cl/566556]: https://github.com/golang/go/commit/ceef0633b3c5bbf5d17a12d6e663c136b30b3f36
    [1]: https://github.com/golang/go/blob/go1.23.4/src/os/path.go#L12-L66
    [filepath.VolumeName]: https://pkg.go.dev/path/filepath#VolumeName
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/system/filesys_unix.go
+++ pkg/system/filesys_unix.go
@@ -5,12 +5,6 @@ package system // import "github.com/docker/docker/pkg/system"
 import "os"
 
 // MkdirAllWithACL is a wrapper for os.MkdirAll on unix systems.
-func MkdirAllWithACL(path string, perm os.FileMode, sddl string) error {
-	return os.MkdirAll(path, perm)
-}
-
-// MkdirAll creates a directory named path along with any necessary parents,
-// with permission specified by attribute perm for all dir created.
-func MkdirAll(path string, perm os.FileMode) error {
+func MkdirAllWithACL(path string, perm os.FileMode, _ string) error {
 	return os.MkdirAll(path, perm)
 }
