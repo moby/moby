e45f20352d425ec406d52ad074a577e8ec5f8314
Sebastiaan van Stijn <github@gone.nl>
2025-01-03 17:18:15 +0100

    pkg/fileutils: deprecate GetTotalUsedFds
    This function is only used internally and has no external consumers.
    Mark it deprecated to be removed in the next release.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/fileutils/fileutils_windows.go
+++ pkg/fileutils/fileutils_windows.go
@@ -4,6 +4,8 @@ import "context"
 
 // GetTotalUsedFds Returns the number of used File Descriptors. Not supported
 // on Windows.
+//
+// Deprecated: this function is only used internally, and will be removed in the next release.
 func GetTotalUsedFds(ctx context.Context) int {
 	return -1
 }
