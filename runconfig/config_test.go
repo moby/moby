ff64a348ccce2d2e4112a426eeb62c0e48340ba7
Sebastiaan van Stijn <github@gone.nl>
2025-01-04 16:35:51 +0100

    runconfig: fix and refactor TestDecodeContainerConfigIsolation
    This test had some broken test-case where the error would only be asserted
    if an error happened, but would be ignored if no error happened, e.g.;
    
        if err := callDecodeContainerConfigIsolation(t, "invalid"); err != nil {
            if !strings.Contains(err.Error(), `Invalid isolation: "invalid"`) {
                t.Fatal(err)
            }
        }
    
    The test also used a helper function (callDecodeContainerConfigIsolation,
    added in 15e35c447058851850155f90292e51decb482956), which had multiple
    output variables, but none of those, except for the error, were used.
    
    This patch:
    
    - removes the callDecodeContainerConfigIsolation utility
    - rewrites TestDecodeContainerConfigIsolation to use sub-tests
    - uses gotest.tools to assert errors
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- runconfig/config_test.go
+++ runconfig/config_test.go
@@ -3,16 +3,15 @@ package runconfig // import "github.com/docker/docker/runconfig"
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"os"
 	"runtime"
-	"strings"
 	"testing"
 
 	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/api/types/strslice"
 	"github.com/docker/docker/pkg/sysinfo"
+	"gotest.tools/v3/assert"
+	is "gotest.tools/v3/assert/cmp"
 )
 
 type f struct {
@@ -70,66 +69,49 @@ func TestDecodeContainerConfig(t *testing.T) {
 // to the daemon in the hostConfig structure. Note this is platform specific
 // as to what level of container isolation is supported.
 func TestDecodeContainerConfigIsolation(t *testing.T) {
-	// An Invalid isolation level
-	if _, _, _, err := callDecodeContainerConfigIsolation("invalid"); err != nil {
-		if !strings.Contains(err.Error(), `Invalid isolation: "invalid"`) {
-			t.Fatal(err)
-		}
-	}
-
-	// Blank isolation (== default)
-	if _, _, _, err := callDecodeContainerConfigIsolation(""); err != nil {
-		t.Fatal("Blank isolation should have succeeded")
-	}
-
-	// Default isolation
-	if _, _, _, err := callDecodeContainerConfigIsolation("default"); err != nil {
-		t.Fatal("default isolation should have succeeded")
-	}
-
-	// Process isolation (Valid on Windows only)
-	if runtime.GOOS == "windows" {
-		if _, _, _, err := callDecodeContainerConfigIsolation("process"); err != nil {
-			t.Fatal("process isolation should have succeeded")
-		}
-	} else {
-		if _, _, _, err := callDecodeContainerConfigIsolation("process"); err != nil {
-			if !strings.Contains(err.Error(), `Invalid isolation: "process"`) {
-				t.Fatal(err)
-			}
-		}
-	}
-
-	// Hyper-V Containers isolation (Valid on Windows only)
-	if runtime.GOOS == "windows" {
-		if _, _, _, err := callDecodeContainerConfigIsolation("hyperv"); err != nil {
-			t.Fatal("hyperv isolation should have succeeded")
-		}
-	} else {
-		if _, _, _, err := callDecodeContainerConfigIsolation("hyperv"); err != nil {
-			if !strings.Contains(err.Error(), `Invalid isolation: "hyperv"`) {
-				t.Fatal(err)
-			}
-		}
-	}
-}
-
-// callDecodeContainerConfigIsolation is a utility function to call
-// DecodeContainerConfig for validating isolation
-func callDecodeContainerConfigIsolation(isolation string) (*container.Config, *container.HostConfig, *network.NetworkingConfig, error) {
-	var (
-		b   []byte
-		err error
-	)
-	w := container.CreateRequest{
-		Config: &container.Config{},
-		HostConfig: &container.HostConfig{
-			NetworkMode: "none",
-			Isolation:   container.Isolation(isolation),
+	tests := []struct {
+		isolation   string
+		invalid     bool
+		expectedErr string
+	}{
+		{
+			isolation: "",
+		},
+		{
+			isolation: "default",
+		},
+		{
+			isolation:   "invalid",
+			invalid:     true,
+			expectedErr: `Invalid isolation: "invalid"`,
+		},
+		{
+			isolation:   "process",
+			invalid:     runtime.GOOS != "windows",
+			expectedErr: `Invalid isolation: "process"`,
+		},
+		{
+			isolation:   "hyperv",
+			invalid:     runtime.GOOS != "windows",
+			expectedErr: `Invalid isolation: "hyperv"`,
 		},
 	}
-	if b, err = json.Marshal(w); err != nil {
-		return nil, nil, nil, fmt.Errorf("Error on marshal %s", err.Error())
+	for _, tc := range tests {
+		t.Run("isolation="+tc.isolation, func(t *testing.T) {
+			// TODO(thaJeztah): consider using fixtures for the JSON requests so that we don't depend on current implementations.
+			b, err := json.Marshal(container.CreateRequest{
+				HostConfig: &container.HostConfig{
+					Isolation: container.Isolation(tc.isolation),
+				},
+			})
+			assert.NilError(t, err)
+
+			_, _, _, err = decodeContainerConfig(bytes.NewReader(b), sysinfo.New())
+			if tc.invalid {
+				assert.Check(t, is.ErrorContains(err, tc.expectedErr))
+			} else {
+				assert.NilError(t, err)
+			}
+		})
 	}
-	return decodeContainerConfig(bytes.NewReader(b), sysinfo.New())
 }
