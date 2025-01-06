docker/docker

unconvert: remove some redundant StrSlice conversions
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

daemon/exec.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/strslice"
 	"github.com/docker/docker/container"
 	"github.com/docker/docker/container/stream"
 	"github.com/docker/docker/daemon/exec"
@@ -100,8 +99,7 @@ func (daemon *Daemon) ContainerExecCreate(name string, config *types.ExecConfig)
 		return "", err
 	}
 
-	cmd := strslice.StrSlice(config.Cmd)
-	entrypoint, args := daemon.getEntrypointAndArgs(strslice.StrSlice{}, cmd)
+	entrypoint, args := daemon.getEntrypointAndArgs([]string{}, config.Cmd)
 
 	keys := []byte{}
 	if config.DetachKeys != "" {

moby/moby

daemon/exec.go
@@ -14,7 +14,6 @@ import (
 	"github.com/docker/docker/api/types/backend"
 	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/events"
-	"github.com/docker/docker/api/types/strslice"
 	"github.com/docker/docker/container"
 	"github.com/docker/docker/container/stream"
 	"github.com/docker/docker/errdefs"
@@ -100,8 +99,7 @@ func (daemon *Daemon) ContainerExecCreate(name string, options *containertypes.E
 		return "", err
 	}
 
-	cmd := strslice.StrSlice(options.Cmd)
-	entrypoint, args := daemon.getEntrypointAndArgs(strslice.StrSlice{}, cmd)
+	entrypoint, args := daemon.getEntrypointAndArgs([]string{}, options.Cmd)
 
 	keys := []byte{}
 	if options.DetachKeys != "" {