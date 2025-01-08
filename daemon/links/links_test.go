34ab9570adfdd71abea6d95728c00eb24c6b8fc0
Sebastiaan van Stijn <github@gone.nl>
2025-01-08 10:04:56 +0100

    daemon/links: TestLinkMultipleEnv: assert with gotest.tools, remove TestLinkPortRangeEnv
    Simplify the test by testing the result, instead of manually checking
    specific values. This makes sure we check the actual results, and don't
    miss values, or ignore unexpected values.
    
    TestLinkPortRangeEnv was added in 611a23aa7f443284fbbbb80716c46159e438bf52
    to test for port-ranges to produce the expected env-vars, but used the
    same input as TestLinkMultipleEnv. Now that we assert all env-vars produced,
    it became a duplicate of TestLinkMultipleEnv, so we can remove that test.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/links/links_test.go
+++ daemon/links/links_test.go
@@ -1,10 +1,7 @@
 package links // import "github.com/docker/docker/daemon/links"
 
 import (
-	"fmt"
 	"sort"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/docker/go-connections/nat"
@@ -72,104 +69,33 @@ func TestLinkMultipleEnv(t *testing.T) {
 		"6381/tcp": struct{}{},
 	})
 
-	rawEnv := link.ToEnv()
-	env := make(map[string]string, len(rawEnv))
-	for _, e := range rawEnv {
-		parts := strings.Split(e, "=")
-		if len(parts) != 2 {
-			t.FailNow()
-		}
-		env[parts[0]] = parts[1]
-	}
-	if env["DOCKER_PORT"] != "tcp://172.0.17.2:6379" {
-		t.Fatalf("Expected 172.0.17.2:6379, got %s", env["DOCKER_PORT"])
-	}
-	if env["DOCKER_PORT_6379_TCP_START"] != "tcp://172.0.17.2:6379" {
-		t.Fatalf("Expected tcp://172.0.17.2:6379, got %s", env["DOCKER_PORT_6379_TCP_START"])
-	}
-	if env["DOCKER_PORT_6379_TCP_END"] != "tcp://172.0.17.2:6381" {
-		t.Fatalf("Expected tcp://172.0.17.2:6381, got %s", env["DOCKER_PORT_6379_TCP_END"])
-	}
-	if env["DOCKER_PORT_6379_TCP_PROTO"] != "tcp" {
-		t.Fatalf("Expected tcp, got %s", env["DOCKER_PORT_6379_TCP_PROTO"])
-	}
-	if env["DOCKER_PORT_6379_TCP_ADDR"] != "172.0.17.2" {
-		t.Fatalf("Expected 172.0.17.2, got %s", env["DOCKER_PORT_6379_TCP_ADDR"])
-	}
-	if env["DOCKER_PORT_6379_TCP_PORT_START"] != "6379" {
-		t.Fatalf("Expected 6379, got %s", env["DOCKER_PORT_6379_TCP_PORT_START"])
-	}
-	if env["DOCKER_PORT_6379_TCP_PORT_END"] != "6381" {
-		t.Fatalf("Expected 6381, got %s", env["DOCKER_PORT_6379_TCP_PORT_END"])
-	}
-	if env["DOCKER_NAME"] != "/db/docker" {
-		t.Fatalf("Expected /db/docker, got %s", env["DOCKER_NAME"])
-	}
-	if env["DOCKER_ENV_PASSWORD"] != "gordon" {
-		t.Fatalf("Expected gordon, got %s", env["DOCKER_ENV_PASSWORD"])
-	}
-}
+	expectedEnv := []string{
+		"DOCKER_ENV_PASSWORD=gordon",
+		"DOCKER_NAME=/db/docker",
+		"DOCKER_PORT=tcp://172.0.17.2:6379",
+		"DOCKER_PORT_6379_TCP=tcp://172.0.17.2:6379",
+		"DOCKER_PORT_6379_TCP_ADDR=172.0.17.2",
+		"DOCKER_PORT_6379_TCP_ADDR=172.0.17.2", // FIXME(thaJeztah): duplicate?
+		"DOCKER_PORT_6379_TCP_END=tcp://172.0.17.2:6381",
+		"DOCKER_PORT_6379_TCP_PORT=6379",
+		"DOCKER_PORT_6379_TCP_PORT_END=6381",
+		"DOCKER_PORT_6379_TCP_PORT_START=6379",
+		"DOCKER_PORT_6379_TCP_PROTO=tcp",
+		"DOCKER_PORT_6379_TCP_PROTO=tcp", // FIXME(thaJeztah): duplicate?
+		"DOCKER_PORT_6379_TCP_START=tcp://172.0.17.2:6379",
 
-func TestLinkPortRangeEnv(t *testing.T) {
-	link := NewLink("172.0.17.3", "172.0.17.2", "/db/docker", []string{"PASSWORD=gordon"}, nat.PortSet{
-		"6379/tcp": struct{}{},
-		"6380/tcp": struct{}{},
-		"6381/tcp": struct{}{},
-	})
+		"DOCKER_PORT_6380_TCP=tcp://172.0.17.2:6380",
+		"DOCKER_PORT_6380_TCP_ADDR=172.0.17.2",
+		"DOCKER_PORT_6380_TCP_PORT=6380",
+		"DOCKER_PORT_6380_TCP_PROTO=tcp",
 
-	rawEnv := link.ToEnv()
-	env := make(map[string]string, len(rawEnv))
-	for _, e := range rawEnv {
-		parts := strings.Split(e, "=")
-		if len(parts) != 2 {
-			t.FailNow()
-		}
-		env[parts[0]] = parts[1]
+		"DOCKER_PORT_6381_TCP=tcp://172.0.17.2:6381",
+		"DOCKER_PORT_6381_TCP_ADDR=172.0.17.2",
+		"DOCKER_PORT_6381_TCP_PORT=6381",
+		"DOCKER_PORT_6381_TCP_PROTO=tcp",
 	}
 
-	if env["DOCKER_PORT"] != "tcp://172.0.17.2:6379" {
-		t.Fatalf("Expected 172.0.17.2:6379, got %s", env["DOCKER_PORT"])
-	}
-	if env["DOCKER_PORT_6379_TCP_START"] != "tcp://172.0.17.2:6379" {
-		t.Fatalf("Expected tcp://172.0.17.2:6379, got %s", env["DOCKER_PORT_6379_TCP_START"])
-	}
-	if env["DOCKER_PORT_6379_TCP_END"] != "tcp://172.0.17.2:6381" {
-		t.Fatalf("Expected tcp://172.0.17.2:6381, got %s", env["DOCKER_PORT_6379_TCP_END"])
-	}
-	if env["DOCKER_PORT_6379_TCP_PROTO"] != "tcp" {
-		t.Fatalf("Expected tcp, got %s", env["DOCKER_PORT_6379_TCP_PROTO"])
-	}
-	if env["DOCKER_PORT_6379_TCP_ADDR"] != "172.0.17.2" {
-		t.Fatalf("Expected 172.0.17.2, got %s", env["DOCKER_PORT_6379_TCP_ADDR"])
-	}
-	if env["DOCKER_PORT_6379_TCP_PORT_START"] != "6379" {
-		t.Fatalf("Expected 6379, got %s", env["DOCKER_PORT_6379_TCP_PORT_START"])
-	}
-	if env["DOCKER_PORT_6379_TCP_PORT_END"] != "6381" {
-		t.Fatalf("Expected 6381, got %s", env["DOCKER_PORT_6379_TCP_PORT_END"])
-	}
-	if env["DOCKER_NAME"] != "/db/docker" {
-		t.Fatalf("Expected /db/docker, got %s", env["DOCKER_NAME"])
-	}
-	if env["DOCKER_ENV_PASSWORD"] != "gordon" {
-		t.Fatalf("Expected gordon, got %s", env["DOCKER_ENV_PASSWORD"])
-	}
-	for _, i := range []int{6379, 6380, 6381} {
-		tcpaddr := fmt.Sprintf("DOCKER_PORT_%d_TCP_ADDR", i)
-		tcpport := fmt.Sprintf("DOCKER_PORT_%d_TCP_PORT", i)
-		tcpproto := fmt.Sprintf("DOCKER_PORT_%d_TCP_PROTO", i)
-		tcp := fmt.Sprintf("DOCKER_PORT_%d_TCP", i)
-		if env[tcpaddr] != "172.0.17.2" {
-			t.Fatalf("Expected env %s  = 172.0.17.2, got %s", tcpaddr, env[tcpaddr])
-		}
-		if env[tcpport] != strconv.Itoa(i) {
-			t.Fatalf("Expected env %s  = %d, got %s", tcpport, i, env[tcpport])
-		}
-		if env[tcpproto] != "tcp" {
-			t.Fatalf("Expected env %s  = tcp, got %s", tcpproto, env[tcpproto])
-		}
-		if env[tcp] != fmt.Sprintf("tcp://172.0.17.2:%d", i) {
-			t.Fatalf("Expected env %s  = tcp://172.0.17.2:%d, got %s", tcp, i, env[tcp])
-		}
-	}
+	actual := link.ToEnv()
+	sort.Strings(actual) // order of env-vars is not relevant
+	assert.DeepEqual(t, expectedEnv, actual)
 }
