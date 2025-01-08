080d8e7d638b25a87b3e4ca1440d4a95ba4264de
Sebastiaan van Stijn <github@gone.nl>
2025-01-08 09:59:17 +0100

    daemon/links: TestLinkEnv: assert with gotest.tools
    Simplify the test by testing the result, instead of manually checking
    specific values. This makes sure we check the actual results, and don't
    miss values, or ignore unexpected values.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/links/links_test.go
+++ daemon/links/links_test.go
@@ -50,36 +50,19 @@ func TestLinkEnv(t *testing.T) {
 		"6379/tcp": struct{}{},
 	})
 
-	rawEnv := link.ToEnv()
-	env := make(map[string]string, len(rawEnv))
-	for _, e := range rawEnv {
-		parts := strings.Split(e, "=")
-		if len(parts) != 2 {
-			t.FailNow()
-		}
-		env[parts[0]] = parts[1]
+	expectedEnv := []string{
+		"DOCKER_ENV_PASSWORD=gordon",
+		"DOCKER_NAME=/db/docker",
+		"DOCKER_PORT=tcp://172.0.17.2:6379",
+		"DOCKER_PORT_6379_TCP=tcp://172.0.17.2:6379",
+		"DOCKER_PORT_6379_TCP_ADDR=172.0.17.2",
+		"DOCKER_PORT_6379_TCP_PORT=6379",
+		"DOCKER_PORT_6379_TCP_PROTO=tcp",
 	}
-	if env["DOCKER_PORT"] != "tcp://172.0.17.2:6379" {
-		t.Fatalf("Expected 172.0.17.2:6379, got %s", env["DOCKER_PORT"])
-	}
-	if env["DOCKER_PORT_6379_TCP"] != "tcp://172.0.17.2:6379" {
-		t.Fatalf("Expected tcp://172.0.17.2:6379, got %s", env["DOCKER_PORT_6379_TCP"])
-	}
-	if env["DOCKER_PORT_6379_TCP_PROTO"] != "tcp" {
-		t.Fatalf("Expected tcp, got %s", env["DOCKER_PORT_6379_TCP_PROTO"])
-	}
-	if env["DOCKER_PORT_6379_TCP_ADDR"] != "172.0.17.2" {
-		t.Fatalf("Expected 172.0.17.2, got %s", env["DOCKER_PORT_6379_TCP_ADDR"])
-	}
-	if env["DOCKER_PORT_6379_TCP_PORT"] != "6379" {
-		t.Fatalf("Expected 6379, got %s", env["DOCKER_PORT_6379_TCP_PORT"])
-	}
-	if env["DOCKER_NAME"] != "/db/docker" {
-		t.Fatalf("Expected /db/docker, got %s", env["DOCKER_NAME"])
-	}
-	if env["DOCKER_ENV_PASSWORD"] != "gordon" {
-		t.Fatalf("Expected gordon, got %s", env["DOCKER_ENV_PASSWORD"])
-	}
+
+	actual := link.ToEnv()
+	sort.Strings(actual) // order of env-vars is not relevant
+	assert.DeepEqual(t, expectedEnv, actual)
 }
 
 func TestLinkMultipleEnv(t *testing.T) {
