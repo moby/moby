e183df63f96fd81e5c4462b5a7415c223752453b
Sebastiaan van Stijn <github@gone.nl>
2025-01-09 16:56:21 +0100

    pkg/ioutils: remove deprecated WriteCounter, NewWriteCounter
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/ioutils/writers.go
+++ pkg/ioutils/writers.go
@@ -49,29 +49,3 @@ func NewWriteCloserWrapper(r io.Writer, closer func() error) io.WriteCloser {
 		closer: closer,
 	}
 }
-
-// WriteCounter wraps a concrete io.Writer and hold a count of the number
-// of bytes written to the writer during a "session".
-// This can be convenient when write return is masked
-// (e.g., json.Encoder.Encode())
-//
-// Deprecated: this type is no longer used and will be removed in the next release.
-type WriteCounter struct {
-	Count  int64
-	Writer io.Writer
-}
-
-// NewWriteCounter returns a new WriteCounter.
-//
-// Deprecated: this function is no longer used and will be removed in the next release.
-func NewWriteCounter(w io.Writer) *WriteCounter {
-	return &WriteCounter{
-		Writer: w,
-	}
-}
-
-func (wc *WriteCounter) Write(p []byte) (count int, err error) {
-	count, err = wc.Writer.Write(p)
-	wc.Count += int64(count)
-	return
-}
--- pkg/ioutils/writers_test.go
+++ pkg/ioutils/writers_test.go
@@ -2,7 +2,6 @@ package ioutils // import "github.com/docker/docker/pkg/ioutils"
 
 import (
 	"bytes"
-	"strings"
 	"testing"
 )
 
@@ -37,28 +36,5 @@ func TestNopWriter(t *testing.T) {
 	}
 	if l != 1 {
 		t.Fatalf("Expected 1 got %d", l)
-	}
-}
-
-func TestWriteCounter(t *testing.T) {
-	dummy1 := "This is a dummy string."
-	dummy2 := "This is another dummy string."
-	totalLength := int64(len(dummy1) + len(dummy2))
-
-	reader1 := strings.NewReader(dummy1)
-	reader2 := strings.NewReader(dummy2)
-
-	var buffer bytes.Buffer
-	wc := NewWriteCounter(&buffer)
-
-	_, _ = reader1.WriteTo(wc)
-	_, _ = reader2.WriteTo(wc)
-
-	if wc.Count != totalLength {
-		t.Errorf("Wrong count: %d vs. %d", wc.Count, totalLength)
-	}
-
-	if buffer.String() != dummy1+dummy2 {
-		t.Error("Wrong message written")
 	}
 }
