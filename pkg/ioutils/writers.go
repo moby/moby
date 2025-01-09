d9c2cde3bed842186f678ed54db11559dd31ca58
Sebastiaan van Stijn <github@gone.nl>
2025-01-09 16:48:39 +0100

    pkg/ioutils: deprecate WriteCounter, NewWriteCounter
    it was moved to pkg/ioutils in c30a55f14dbbe3971ba0ac716ba69a60868f4490,
    and only had a single use at the time in [engine/Env.WriteTo]. That use
    was removed in 531f4122bdcd4de289f613a5ef010f4c1989f098, which removed
    the engine package.
    
    [engine/Env.WriteTo]: https://github.com/moby/moby/blob/c30a55f14dbbe3971ba0ac716ba69a60868f4490/engine/env.go#L260-L264
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 383503d3822d192eddf7ed3a0720a8f43a259147)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/ioutils/writers.go
+++ pkg/ioutils/writers.go
@@ -55,12 +55,16 @@ func NewWriteCloserWrapper(r io.Writer, closer func() error) io.WriteCloser {
 // of bytes written to the writer during a "session".
 // This can be convenient when write return is masked
 // (e.g., json.Encoder.Encode())
+//
+// Deprecated: this type is no longer used and will be removed in the next release.
 type WriteCounter struct {
 	Count  int64
 	Writer io.Writer
 }
 
 // NewWriteCounter returns a new WriteCounter.
+//
+// Deprecated: this function is no longer used and will be removed in the next release.
 func NewWriteCounter(w io.Writer) *WriteCounter {
 	return &WriteCounter{
 		Writer: w,
