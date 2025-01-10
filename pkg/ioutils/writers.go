ce8d3d1c78e7d398a51848f04ce9070045b05824
Sebastiaan van Stijn <github@gone.nl>
2025-01-10 14:22:12 +0100

    pkg/ioutils: remove deprecated NopWriteCloser
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/ioutils/writers.go
+++ pkg/ioutils/writers.go
@@ -5,19 +5,6 @@ import (
 	"sync/atomic"
 )
 
-type nopWriteCloser struct {
-	io.Writer
-}
-
-func (w *nopWriteCloser) Close() error { return nil }
-
-// NopWriteCloser returns a nopWriteCloser.
-//
-// Deprecated: This function is no longer used and will be removed in the next release.
-func NopWriteCloser(w io.Writer) io.WriteCloser {
-	return &nopWriteCloser{w}
-}
-
 type writeCloserWrapper struct {
 	io.Writer
 	closer func() error
