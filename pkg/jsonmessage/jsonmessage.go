73b0797bcc108f15b57c24c3eab482dc174c2287
Sebastiaan van Stijn <github@gone.nl>
2025-01-13 13:35:42 +0100

    pkg/jsonmessage: JSONMessage: remove deprecated ProgressMessage, ErrorMessage
    - ErrorMessage was deprecated in 3043c2641990d94298c6377b7ef14709263a4709
    - ProgressMessage was deprecated in 597e0e69b4c8521f39691d0a07d1f31b7116a337
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/jsonmessage/jsonmessage.go
+++ pkg/jsonmessage/jsonmessage.go
@@ -143,16 +143,14 @@ func (p *JSONProgress) width() int {
 // the created time, where it from, status, ID of the
 // message. It's used for docker events.
 type JSONMessage struct {
-	Stream          string        `json:"stream,omitempty"`
-	Status          string        `json:"status,omitempty"`
-	Progress        *JSONProgress `json:"progressDetail,omitempty"`
-	ProgressMessage string        `json:"progress,omitempty"` // deprecated
-	ID              string        `json:"id,omitempty"`
-	From            string        `json:"from,omitempty"`
-	Time            int64         `json:"time,omitempty"`
-	TimeNano        int64         `json:"timeNano,omitempty"`
-	Error           *JSONError    `json:"errorDetail,omitempty"`
-	ErrorMessage    string        `json:"error,omitempty"` // deprecated
+	Stream   string        `json:"stream,omitempty"`
+	Status   string        `json:"status,omitempty"`
+	Progress *JSONProgress `json:"progressDetail,omitempty"`
+	ID       string        `json:"id,omitempty"`
+	From     string        `json:"from,omitempty"`
+	Time     int64         `json:"time,omitempty"`
+	TimeNano int64         `json:"timeNano,omitempty"`
+	Error    *JSONError    `json:"errorDetail,omitempty"`
 	// Aux contains out-of-band data, such as digests for push signing and image id after building.
 	Aux *json.RawMessage `json:"aux,omitempty"`
 }
@@ -199,8 +197,6 @@ func (jm *JSONMessage) Display(out io.Writer, isTerminal bool) error {
 	}
 	if jm.Progress != nil && isTerminal {
 		fmt.Fprintf(out, "%s %s%s", jm.Status, jm.Progress.String(), endl)
-	} else if jm.ProgressMessage != "" { // deprecated
-		fmt.Fprintf(out, "%s %s%s", jm.Status, jm.ProgressMessage, endl)
 	} else if jm.Stream != "" {
 		fmt.Fprintf(out, "%s%s", jm.Stream, endl)
 	} else {
@@ -251,7 +247,7 @@ func DisplayJSONMessagesStream(in io.Reader, out io.Writer, terminalFd uintptr, 
 		if jm.Progress != nil {
 			jm.Progress.terminalFd = terminalFd
 		}
-		if jm.ID != "" && (jm.Progress != nil || jm.ProgressMessage != "") {
+		if jm.ID != "" && jm.Progress != nil {
 			line, ok := ids[jm.ID]
 			if !ok {
 				// NOTE: This approach of using len(id) to
