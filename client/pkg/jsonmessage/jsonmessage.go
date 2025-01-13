fdaccdb2331ebcff9a36e81a4bf1404c84d822ae
Sebastiaan van Stijn <github@gone.nl>
2025-01-13 13:35:42 +0100

    pkg/jsonmessage: stop printing deprecated progressDetail, errorDetail
    The API still returns it for backward-compatibility (but probably
    shouldn't), but we should no longer print it. This removes the
    use of these fields for printing, but keeps them for streamformatter
    to use.
    
    - ErrorMessage was deprecated in 3043c2641990d94298c6377b7ef14709263a4709
    - ProgressMessage was deprecated in 597e0e69b4c8521f39691d0a07d1f31b7116a337
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/pkg/jsonmessage/jsonmessage.go
+++ client/pkg/jsonmessage/jsonmessage.go
@@ -122,26 +122,15 @@ func (p *JSONProgress) width() int {
 // the created time, where it from, status, ID of the
 // message. It's used for docker events.
 type JSONMessage struct {
-	Stream   string        `json:"stream,omitempty"`
-	Status   string        `json:"status,omitempty"`
-	Progress *JSONProgress `json:"progressDetail,omitempty"`
-
-	// ProgressMessage is a pre-formatted presentation of [Progress].
-	//
-	// Deprecated: this field is deprecated since docker v0.7.1 / API v1.8. Use the information in [Progress] instead. This field will be omitted in a future release.
-	ProgressMessage string            `json:"progress,omitempty"`
-	ID              string            `json:"id,omitempty"`
-	From            string            `json:"from,omitempty"`
-	Time            int64             `json:"time,omitempty"`
-	TimeNano        int64             `json:"timeNano,omitempty"`
-	Error           *jsonstream.Error `json:"errorDetail,omitempty"`
-
-	// ErrorMessage contains errors encountered during the operation.
-	//
-	// Deprecated: this field is deprecated since docker v0.6.0 / API v1.4. Use [Error.Message] instead. This field will be omitted in a future release.
-	ErrorMessage string `json:"error,omitempty"` // deprecated
-	// Aux contains out-of-band data, such as digests for push signing and image id after building.
-	Aux *json.RawMessage `json:"aux,omitempty"`
+	Stream   string            `json:"stream,omitempty"`
+	Status   string            `json:"status,omitempty"`
+	Progress *JSONProgress     `json:"progressDetail,omitempty"`
+	ID       string            `json:"id,omitempty"`
+	From     string            `json:"from,omitempty"`
+	Time     int64             `json:"time,omitempty"`
+	TimeNano int64             `json:"timeNano,omitempty"`
+	Error    *jsonstream.Error `json:"errorDetail,omitempty"`
+	Aux      *json.RawMessage  `json:"aux,omitempty"` // Aux contains out-of-band data, such as digests for push signing and image id after building.
 }
 
 // We can probably use [aec.EmptyBuilder] for managing the output, but
@@ -201,8 +190,6 @@ func (jm *JSONMessage) Display(out io.Writer, isTerminal bool) error {
 	}
 	if jm.Progress != nil && isTerminal {
 		_, _ = fmt.Fprintf(out, "%s %s%s", jm.Status, jm.Progress.String(), endl)
-	} else if jm.ProgressMessage != "" { // deprecated
-		_, _ = fmt.Fprintf(out, "%s %s%s", jm.Status, jm.ProgressMessage, endl)
 	} else if jm.Stream != "" {
 		_, _ = fmt.Fprintf(out, "%s%s", jm.Stream, endl)
 	} else {
@@ -253,7 +240,7 @@ func DisplayJSONMessagesStream(in io.Reader, out io.Writer, terminalFd uintptr, 
 		if jm.Progress != nil {
 			jm.Progress.terminalFd = terminalFd
 		}
-		if jm.ID != "" && (jm.Progress != nil || jm.ProgressMessage != "") {
+		if jm.ID != "" && jm.Progress != nil {
 			line, ok := ids[jm.ID]
 			if !ok {
 				// NOTE: This approach of using len(id) to
