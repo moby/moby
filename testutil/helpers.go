cc120c4c053e1f9c9f8ecf8c0f60878e0f80538b
Sebastiaan van Stijn <github@gone.nl>
2025-01-14 19:20:17 +0100

    testutil: update to semconv v1.26.0
    update the version to align with BuildKit's [util/tracing/detect], which
    was updated to v1.26.0 in 48b237f7af82399198dd78af0a2739918c4ec7a2
    
    [util/tracing/detect]:  https://github.com/moby/moby/blob/2b7e859c49d3603ecd43fd4ccba1976a90064913/vendor/github.com/moby/buildkit/util/tracing/detect/resource.go#L15
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- testutil/helpers.go
+++ testutil/helpers.go
@@ -20,7 +20,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"go.opentelemetry.io/otel/trace/noop"
 	"gotest.tools/v3/icmd"
 )
