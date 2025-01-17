c75e333b6fa4cdd3816f3b28347739eba6148e14
Tianon Gravi <admwiggin@gmail.com>
2025-01-16 16:12:50 -0800

    Satisfy linter
        integration/container/run_linux_test.go:459:3: The copy of the 'for' variable "tc" can be deleted (Go 1.22+) (copyloopvar)
    
    (thanks Go)
    
    Signed-off-by: Tianon Gravi <admwiggin@gmail.com>

--- integration/container/run_linux_test.go
+++ integration/container/run_linux_test.go
@@ -456,7 +456,6 @@ func TestCgroupRW(t *testing.T) {
 		},
 	}
 	for _, tc := range testCases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			config := container.NewTestConfig(tc.ops...)
 			resp, err := container.CreateFromConfig(ctx, apiClient, config)
