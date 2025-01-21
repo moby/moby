afcf1b92a55a027d8cfeec82423acfa2e16f7918
Sebastiaan van Stijn <github@gone.nl>
2025-01-21 15:37:14 +0100

    libnetwork: TestErrorInterfaces: use errdefs helpers to check error-types
    Make sure these errors are properly detected by the errdefs helpers to
    implement the right interface / definition.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libnetwork/errors_test.go
+++ libnetwork/errors_test.go
@@ -3,17 +3,16 @@ package libnetwork
 import (
 	"testing"
 
+	"github.com/docker/docker/errdefs"
 	"github.com/docker/docker/libnetwork/types"
+	"gotest.tools/v3/assert"
+	is "gotest.tools/v3/assert/cmp"
 )
 
 func TestErrorInterfaces(t *testing.T) {
 	badRequestErrorList := []error{ErrInvalidID(""), ErrInvalidName("")}
 	for _, err := range badRequestErrorList {
-		switch u := err.(type) {
-		case types.InvalidParameterError:
-		default:
-			t.Errorf("Failed to detect err %v is of type InvalidParameterError. Got type: %T", err, u)
-		}
+		assert.Check(t, is.ErrorType(err, errdefs.IsInvalidParameter))
 	}
 
 	maskableErrorList := []error{ManagerRedirectError("")}
@@ -27,19 +26,11 @@ func TestErrorInterfaces(t *testing.T) {
 
 	notFoundErrorList := []error{&UnknownNetworkError{}, ErrNoSuchNetwork(""), ErrNoSuchEndpoint("")}
 	for _, err := range notFoundErrorList {
-		switch u := err.(type) {
-		case types.NotFoundError:
-		default:
-			t.Errorf("Failed to detect err %v is of type NotFoundError. Got type: %T", err, u)
-		}
+		assert.Check(t, is.ErrorType(err, errdefs.IsNotFound))
 	}
 
 	forbiddenErrorList := []error{&ActiveContainerError{}}
 	for _, err := range forbiddenErrorList {
-		switch u := err.(type) {
-		case types.ForbiddenError:
-		default:
-			t.Errorf("Failed to detect err %v is of type ForbiddenError. Got type: %T", err, u)
-		}
+		assert.Check(t, is.ErrorType(err, errdefs.IsForbidden))
 	}
 }
