52ea449c55d53919d8f9c1e7a59d0b233ab9c675
Sebastiaan van Stijn <github@gone.nl>
2025-01-30 14:17:35 +0100

    daemon: Daemon.restore: make legacy-link code conditional
    Make it more clear that this loop is for legacy-links, and the timer is
    only needed for that purpose.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -568,17 +568,18 @@ func (daemon *Daemon) restore(cfg *configStore) error {
 
 			logger.Debug("starting container")
 
-			// ignore errors here as this is a best effort to wait for children to be
-			//   running before we try to start the container
-			children := daemon.linkIndex.children(c)
-			timeout := time.NewTimer(5 * time.Second)
-			defer timeout.Stop()
+			// ignore errors here as this is a best effort to wait for children
+			// (legacy links) to be running before we try to start the container
+			if children := daemon.linkIndex.children(c); len(children) > 0 {
+				timeout := time.NewTimer(5 * time.Second)
+				defer timeout.Stop()
 
-			for _, child := range children {
-				if notifier, exists := restartContainers[child]; exists {
-					select {
-					case <-notifier:
-					case <-timeout.C:
+				for _, child := range children {
+					if notifier, exists := restartContainers[child]; exists {
+						select {
+						case <-notifier:
+						case <-timeout.C:
+						}
 					}
 				}
 			}
