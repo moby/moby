0f045329564de5ecc226cf272f00de1d44eb3109
Sebastiaan van Stijn <github@gone.nl>
2025-01-31 16:31:17 +0100

    errdefs: touch-up godoc for helpers
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- errdefs/is.go
+++ errdefs/is.go
@@ -39,79 +39,79 @@ func getImplementer(err error) error {
 	}
 }
 
-// IsNotFound returns if the passed in error is an ErrNotFound
+// IsNotFound returns if the passed in error is an [ErrNotFound],
 func IsNotFound(err error) bool {
 	_, ok := getImplementer(err).(ErrNotFound)
 	return ok
 }
 
-// IsInvalidParameter returns if the passed in error is an ErrInvalidParameter
+// IsInvalidParameter returns if the passed in error is an [ErrInvalidParameter].
 func IsInvalidParameter(err error) bool {
 	_, ok := getImplementer(err).(ErrInvalidParameter)
 	return ok
 }
 
-// IsConflict returns if the passed in error is an ErrConflict
+// IsConflict returns if the passed in error is an [ErrConflict].
 func IsConflict(err error) bool {
 	_, ok := getImplementer(err).(ErrConflict)
 	return ok
 }
 
-// IsUnauthorized returns if the passed in error is an ErrUnauthorized
+// IsUnauthorized returns if the passed in error is an [ErrUnauthorized].
 func IsUnauthorized(err error) bool {
 	_, ok := getImplementer(err).(ErrUnauthorized)
 	return ok
 }
 
-// IsUnavailable returns if the passed in error is an ErrUnavailable
+// IsUnavailable returns if the passed in error is an [ErrUnavailable].
 func IsUnavailable(err error) bool {
 	_, ok := getImplementer(err).(ErrUnavailable)
 	return ok
 }
 
-// IsForbidden returns if the passed in error is an ErrForbidden
+// IsForbidden returns if the passed in error is an [ErrForbidden].
 func IsForbidden(err error) bool {
 	_, ok := getImplementer(err).(ErrForbidden)
 	return ok
 }
 
-// IsSystem returns if the passed in error is an ErrSystem
+// IsSystem returns if the passed in error is an [ErrSystem].
 func IsSystem(err error) bool {
 	_, ok := getImplementer(err).(ErrSystem)
 	return ok
 }
 
-// IsNotModified returns if the passed in error is a NotModified error
+// IsNotModified returns if the passed in error is an [ErrNotModified].
 func IsNotModified(err error) bool {
 	_, ok := getImplementer(err).(ErrNotModified)
 	return ok
 }
 
-// IsNotImplemented returns if the passed in error is an ErrNotImplemented
+// IsNotImplemented returns if the passed in error is an [ErrNotImplemented].
 func IsNotImplemented(err error) bool {
 	_, ok := getImplementer(err).(ErrNotImplemented)
 	return ok
 }
 
-// IsUnknown returns if the passed in error is an ErrUnknown
+// IsUnknown returns if the passed in error is an [ErrUnknown].
 func IsUnknown(err error) bool {
 	_, ok := getImplementer(err).(ErrUnknown)
 	return ok
 }
 
-// IsCancelled returns if the passed in error is an ErrCancelled
+// IsCancelled returns if the passed in error is an [ErrCancelled].
 func IsCancelled(err error) bool {
 	_, ok := getImplementer(err).(ErrCancelled)
 	return ok
 }
 
-// IsDeadline returns if the passed in error is an ErrDeadline
+// IsDeadline returns if the passed in error is an [ErrDeadline].
 func IsDeadline(err error) bool {
 	_, ok := getImplementer(err).(ErrDeadline)
 	return ok
 }
 
-// IsDataLoss returns if the passed in error is an ErrDataLoss
+// IsDataLoss returns if the passed in error is an [ErrDataLoss].
 func IsDataLoss(err error) bool {
 	_, ok := getImplementer(err).(ErrDataLoss)
 	return ok
