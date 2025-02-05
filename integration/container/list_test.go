baa48feb92ceaeaa78599e71cb0a8ea35ce6a617
Cesar Talledo <cesar.talledo@docker.com>
2025-02-04 18:58:56 -0800

    Merge ps_test.go into list_test.go.
    Signed-off-by: Cesar Talledo <cesar.talledo@docker.com>

--- integration/container/list_test.go
+++ integration/container/list_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/integration/internal/container"
+	"github.com/docker/docker/testutil"
 	"github.com/docker/docker/testutil/request"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
@@ -76,4 +77,41 @@ func TestListAnnotations(t *testing.T) {
 			assert.Check(t, is.DeepEqual(containers[0].HostConfig.Annotations, tc.expectedAnnotations))
 		})
 	}
+}
+
+func TestListFilter(t *testing.T) {
+	ctx := setupTest(t)
+	apiClient := testEnv.APIClient()
+
+	prev := container.Create(ctx, t, apiClient)
+	top := container.Create(ctx, t, apiClient)
+	next := container.Create(ctx, t, apiClient)
+
+	containerIDs := func(containers []containertypes.Summary) []string {
+		var entries []string
+		for _, c := range containers {
+			entries = append(entries, c.ID)
+		}
+		return entries
+	}
+
+	t.Run("since", func(t *testing.T) {
+		ctx := testutil.StartSpan(ctx, t)
+		results, err := apiClient.ContainerList(ctx, containertypes.ListOptions{
+			All:     true,
+			Filters: filters.NewArgs(filters.Arg("since", top)),
+		})
+		assert.NilError(t, err)
+		assert.Check(t, is.Contains(containerIDs(results), next))
+	})
+
+	t.Run("before", func(t *testing.T) {
+		ctx := testutil.StartSpan(ctx, t)
+		results, err := apiClient.ContainerList(ctx, containertypes.ListOptions{
+			All:     true,
+			Filters: filters.NewArgs(filters.Arg("before", top)),
+		})
+		assert.NilError(t, err)
+		assert.Check(t, is.Contains(containerIDs(results), prev))
+	})
 }
