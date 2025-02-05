fc52c930920698921d9f9f26f7becacd7de5dad2
Cesar Talledo <cesar.talledo@docker.com>
2025-02-04 18:58:56 -0800

    Merge ps_test.go into list_test.go.
    Signed-off-by: Cesar Talledo <cesar.talledo@docker.com>

--- integration/container/list_test.go
+++ integration/container/list_test.go
@@ -8,9 +8,11 @@ import (
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/integration/internal/container"
+	"github.com/docker/docker/testutil"
 	"github.com/docker/docker/testutil/request"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
+	"gotest.tools/v3/skip"
 )
 
 func TestListAnnotations(t *testing.T) {
@@ -43,5 +45,68 @@ func TestListAnnotations(t *testing.T) {
 			assert.Equal(t, containers[0].ID, id)
 			assert.Check(t, is.DeepEqual(containers[0].HostConfig.Annotations, tc.expectedAnnotations))
 		})
+	}
+}
+
+func TestListFilter(t *testing.T) {
+	ctx := setupTest(t)
+	apiClient := testEnv.APIClient()
+
+	prev := container.Create(ctx, t, apiClient)
+	top := container.Create(ctx, t, apiClient)
+	next := container.Create(ctx, t, apiClient)
+
+	containerIDs := func(containers []containertypes.Summary) []string {
+		var entries []string
+		for _, c := range containers {
+			entries = append(entries, c.ID)
+		}
+		return entries
+	}
+
+	t.Run("since", func(t *testing.T) {
+		ctx := testutil.StartSpan(ctx, t)
+		results, err := apiClient.ContainerList(ctx, containertypes.ListOptions{
+			All:     true,
+			Filters: filters.NewArgs(filters.Arg("since", top)),
+		})
+		assert.NilError(t, err)
+		assert.Check(t, is.Contains(containerIDs(results), next))
+	})
+
+	t.Run("before", func(t *testing.T) {
+		ctx := testutil.StartSpan(ctx, t)
+		results, err := apiClient.ContainerList(ctx, containertypes.ListOptions{
+			All:     true,
+			Filters: filters.NewArgs(filters.Arg("before", top)),
+		})
+		assert.NilError(t, err)
+		assert.Check(t, is.Contains(containerIDs(results), prev))
+	})
+}
+
+// TestListPlatform verifies that containers have a platform set
+func TestListImageManifestPlatform(t *testing.T) {
+	skip.If(t, testEnv.IsRemoteDaemon)
+	skip.If(t, testEnv.DaemonInfo.OSType != "linux")
+	skip.If(t, !testEnv.UsingSnapshotter())
+
+	ctx := setupTest(t)
+	apiClient := testEnv.APIClient()
+
+	container.Create(ctx, t, apiClient)
+
+	containers, err := apiClient.ContainerList(ctx, containertypes.ListOptions{
+		All: true,
+	})
+	assert.NilError(t, err)
+	assert.Check(t, len(containers) > 0)
+
+	ctr := containers[0]
+	if assert.Check(t, ctr.ImageManifestDescriptor != nil && ctr.ImageManifestDescriptor.Platform != nil) {
+		// Check that at least OS and Architecture have a value. Other values
+		// depend on the platform on which we're running the test.
+		assert.Equal(t, ctr.ImageManifestDescriptor.Platform.OS, testEnv.DaemonInfo.OSType)
+		assert.Check(t, ctr.ImageManifestDescriptor.Platform.Architecture != "")
 	}
 }
