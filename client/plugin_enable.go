620713ef10218bcdc2afb14b785434bd9e54430d
Sebastiaan van Stijn <github@gone.nl>
2025-02-05 03:22:27 +0100

    client: Client.PluginEnable: validate timeout
    Add validation for negative timeouts; currently this is validated in the
    CLI; https://github.com/docker/cli/blob/dff0dc8afa0c7e6f99c6f0d24f749af76366a5f0/cli/command/plugin/enable.go#L37-L48
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/plugin_enable.go
+++ client/plugin_enable.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/errdefs"
+	"github.com/pkg/errors"
 )
 
 // PluginEnable enables a plugin
@@ -15,8 +17,12 @@ func (cli *Client) PluginEnable(ctx context.Context, name string, options types.
 		return err
 	}
 	query := url.Values{}
-	query.Set("timeout", strconv.Itoa(options.Timeout))
-
+	if v := options.Timeout; v != 0 {
+		if v < 0 {
+			return errdefs.InvalidParameter(errors.New("invalid timeout: value must be positive"))
+		}
+		query.Set("timeout", strconv.Itoa(v))
+	}
 	resp, err := cli.post(ctx, "/plugins/"+name+"/enable", query, nil, nil)
 	ensureReaderClosed(resp)
 	return err
