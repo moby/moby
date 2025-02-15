0a4c4c9e82e7ea8e5c658d1bd7224a838e436523
Sebastiaan van Stijn <github@gone.nl>
2025-02-15 16:43:27 +0100

    container: some cleanups in view tests
    - use t.TempDir() instead of TestMain creating a directory to make
      tests self-contained.
    - fix some unhandled errors, and missing assertions for error-types
    - assert with gotest.tools, but kept the Benchmark tests as-is for now,
      to make sure gotest.tools doesn't impact the results.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- container/view_test.go
+++ container/view_test.go
@@ -15,103 +15,73 @@ import (
 	is "gotest.tools/v3/assert/cmp"
 )
 
-var root string
-
-func TestMain(m *testing.M) {
-	var err error
-	root, err = os.MkdirTemp("", "docker-container-test-")
-	if err != nil {
-		panic(err)
-	}
-	defer os.RemoveAll(root)
-
-	os.Exit(m.Run())
-}
-
-func newContainer(t *testing.T) *Container {
-	var (
-		id    = uuid.New().String()
-		cRoot = filepath.Join(root, id)
-	)
-	if err := os.MkdirAll(cRoot, 0o755); err != nil {
-		t.Fatal(err)
-	}
-	c := NewBaseContainer(id, cRoot)
-	c.HostConfig = &container.HostConfig{}
-	return c
+func newContainer(t *testing.T, root string) *Container {
+	t.Helper()
+	id := uuid.New().String()
+	cRoot := filepath.Join(root, id)
+	assert.NilError(t, os.Mkdir(cRoot, 0o755))
+	return NewBaseContainer(id, cRoot)
 }
 
 func TestViewSaveDelete(t *testing.T) {
 	db, err := NewViewDB()
-	if err != nil {
-		t.Fatal(err)
-	}
-	c := newContainer(t)
-	if err := c.CheckpointTo(context.Background(), db); err != nil {
-		t.Fatal(err)
-	}
-	if err := db.Delete(c); err != nil {
-		t.Fatal(err)
-	}
+	assert.NilError(t, err)
+
+	tmpDir := t.TempDir()
+	c := newContainer(t, tmpDir)
+	assert.NilError(t, c.CheckpointTo(context.Background(), db))
+	assert.NilError(t, db.Delete(c))
 }
 
 func TestViewAll(t *testing.T) {
-	var (
-		db, _ = NewViewDB()
-		one   = newContainer(t)
-		two   = newContainer(t)
-	)
+	db, err := NewViewDB()
+	assert.NilError(t, err)
+
+	tmpDir := t.TempDir()
+	one := newContainer(t, tmpDir)
+	two := newContainer(t, tmpDir)
+
 	one.Pid = 10
-	if err := one.CheckpointTo(context.Background(), db); err != nil {
-		t.Fatal(err)
-	}
+	assert.NilError(t, one.CheckpointTo(context.Background(), db))
+
 	two.Pid = 20
-	if err := two.CheckpointTo(context.Background(), db); err != nil {
-		t.Fatal(err)
-	}
+	assert.NilError(t, two.CheckpointTo(context.Background(), db))
 
 	all, err := db.Snapshot().All()
-	if err != nil {
-		t.Fatal(err)
+	assert.NilError(t, err)
+	assert.Assert(t, is.Len(all, 2))
+
+	byID := make(map[string]int)
+	for _, c := range all {
+		byID[c.ID] = c.Pid
 	}
-	if l := len(all); l != 2 {
-		t.Fatalf("expected 2 items, got %d", l)
+	expected := map[string]int{
+		one.ID: one.Pid,
+		two.ID: two.Pid,
 	}
-	byID := make(map[string]Snapshot)
-	for i := range all {
-		byID[all[i].ID] = all[i]
-	}
-	if s, ok := byID[one.ID]; !ok || s.Pid != 10 {
-		t.Fatalf("expected something different with for id=%s: %v", one.ID, s)
-	}
-	if s, ok := byID[two.ID]; !ok || s.Pid != 20 {
-		t.Fatalf("expected something different with for id=%s: %v", two.ID, s)
-	}
+	assert.DeepEqual(t, expected, byID)
 }
 
 func TestViewGet(t *testing.T) {
-	var (
-		db, _ = NewViewDB()
-		one   = newContainer(t)
-	)
-	one.ImageID = "some-image-123"
-	if err := one.CheckpointTo(context.Background(), db); err != nil {
-		t.Fatal(err)
-	}
+	db, err := NewViewDB()
+	assert.NilError(t, err)
+
+	tmpDir := t.TempDir()
+	one := newContainer(t, tmpDir)
+
+	const imgID = "some-image-123"
+	one.ImageID = imgID
+
+	assert.NilError(t, one.CheckpointTo(context.Background(), db))
 	s, err := db.Snapshot().Get(one.ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if s == nil || s.ImageID != "some-image-123" {
-		t.Fatalf("expected ImageID=some-image-123. Got: %v", s)
-	}
+	assert.NilError(t, err)
+	assert.Equal(t, s.ID, one.ID)
 }
 
 func TestNames(t *testing.T) {
 	db, err := NewViewDB()
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NilError(t, err)
+
 	assert.Check(t, db.ReserveName("name1", "containerid1"))
 	assert.Check(t, db.ReserveName("name1", "containerid1")) // idempotent
 	assert.Check(t, db.ReserveName("name2", "containerid2"))
@@ -170,43 +140,34 @@ func TestNames(t *testing.T) {
 
 // Test case for GitHub issue 35920
 func TestViewWithHealthCheck(t *testing.T) {
-	var (
-		db, _ = NewViewDB()
-		one   = newContainer(t)
-	)
+	db, err := NewViewDB()
+	assert.NilError(t, err)
+
+	tmpDir := t.TempDir()
+	one := newContainer(t, tmpDir)
+
 	one.Health = &Health{
 		Health: container.Health{
 			Status: "starting",
 		},
 	}
-	if err := one.CheckpointTo(context.Background(), db); err != nil {
-		t.Fatal(err)
-	}
+	assert.NilError(t, one.CheckpointTo(context.Background(), db))
 	s, err := db.Snapshot().Get(one.ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if s == nil || s.Health != "starting" {
-		t.Fatalf("expected Health=starting. Got: %+v", s)
-	}
+	assert.NilError(t, err)
+	assert.Equal(t, s.Health, "starting")
 }
 
 func TestTruncIndex(t *testing.T) {
 	db, err := NewViewDB()
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NilError(t, err)
 
 	// Get on an empty index
-	if _, err := db.GetByPrefix("foobar"); err == nil {
-		t.Fatal("Get on an empty index should return an error")
-	}
+	_, err = db.GetByPrefix("foobar")
+	assert.Check(t, is.ErrorType(err, errdefs.IsNotFound))
 
-	id := "99b36c2c326ccc11e726eee6ee78a0baf166ef96"
 	// Add an id
-	if err := db.Save(&Container{ID: id}); err != nil {
-		t.Fatal(err)
-	}
+	const id = "99b36c2c326ccc11e726eee6ee78a0baf166ef96"
+	assert.NilError(t, db.Save(&Container{ID: id}))
 
 	type testacase struct {
 		name           string
@@ -256,11 +217,9 @@ func TestTruncIndex(t *testing.T) {
 		})
 	}
 
+	// Add an id with a prefix overlapping with the previous one
 	id2 := id[:6] + "blabla"
-	// Add an id
-	if err := db.Save(&Container{ID: id2}); err != nil {
-		t.Fatal(err)
-	}
+	assert.NilError(t, db.Save(&Container{ID: id2}))
 
 	for _, tc := range []testacase{
 		{
@@ -312,9 +271,7 @@ func TestTruncIndex(t *testing.T) {
 	}
 
 	// Deleting id2 should remove conflicts
-	if err := db.Delete(&Container{ID: id2}); err != nil {
-		t.Fatal(err)
-	}
+	assert.NilError(t, db.Delete(&Container{ID: id2}))
 
 	for _, tc := range []testacase{
 		{
