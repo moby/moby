94cfd307858d53925f9bf8bbd28f69ff0e0b4835
Sebastiaan van Stijn <github@gone.nl>
2025-02-25 11:06:11 +0100

    vendor: golang.org/x/crypto v0.33.0
    Relevant changes;
    
    - go.mod: update golang.org/x dependencies
    - ssh: add ServerConfig.PreAuthConnCallback, ServerPreAuthConn (banner)
      interface
    
    full diff: https://github.com/golang/crypto/compare/v0.31.0...v0.33.0
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/golang.org/x/crypto/pkcs12/crypto.go
+++ vendor/golang.org/x/crypto/pkcs12/crypto.go
@@ -26,7 +26,7 @@ type pbeCipher interface {
 	create(key []byte) (cipher.Block, error)
 	// deriveKey returns a key derived from the given password and salt.
 	deriveKey(salt, password []byte, iterations int) []byte
-	// deriveKey returns an IV derived from the given password and salt.
+	// deriveIV returns an IV derived from the given password and salt.
 	deriveIV(salt, password []byte, iterations int) []byte
 }
 
