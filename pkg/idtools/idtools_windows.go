365699511f5e25da0747203dfc65815ced7f5f6e
Derek McGowan <derek@mcg.dev>
2025-03-01 00:45:56 -0800

    Update idtools to use Mkdir funcs from moby sys/user package
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- pkg/idtools/idtools_windows.go
+++ pkg/idtools/idtools_windows.go
@@ -1,9 +1,5 @@
 package idtools
 
-import (
-	"os"
-)
-
 const (
 	SeTakeOwnershipPrivilege = "SeTakeOwnershipPrivilege"
 )
@@ -14,11 +10,3 @@ const (
 
 	ContainerUserSidString = "S-1-5-93-2-2"
 )
-
-// This is currently a wrapper around [os.MkdirAll] since currently
-// permissions aren't set through this path, the identity isn't utilized.
-// Ownership is handled elsewhere, but in the future could be support here
-// too.
-func mkdirAs(path string, _ os.FileMode, _ Identity, _, _ bool) error {
-	return os.MkdirAll(path, 0)
-}
