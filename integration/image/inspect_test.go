427049b6b2be3002b4e9b89bcf17f019b9218c7e
Paweł Gronowski <pawel.gronowski@docker.com>
2025-03-05 15:42:11 +0100

    image/inspect: Add platform selection
    `GET /image/{name}/json` now supports `platform` parameter allowing to
    specify which platform variant of a multi-platform image to inspect.
    
    For servers that do not use containerd image store integration, this
    option will cause an error if the requested platform doesn't match the
    image's actual platform
    
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- integration/image/inspect_test.go
+++ integration/image/inspect_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/internal/testutils/specialimage"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
 	"gotest.tools/v3/skip"
@@ -79,4 +80,74 @@ func TestImageInspectDescriptor(t *testing.T) {
 	assert.Assert(t, inspect.Descriptor != nil)
 	assert.Check(t, inspect.Descriptor.Digest.String() == inspect.ID)
 	assert.Check(t, inspect.Descriptor.Size > 0)
+}
+
+func TestImageInspectWithPlatform(t *testing.T) {
+	ctx := setupTest(t)
+
+	apiClient := testEnv.APIClient()
+
+	nativePlatform := ocispec.Platform{
+		OS:           testEnv.DaemonInfo.OSType,
+		Architecture: testEnv.DaemonInfo.Architecture,
+	}
+
+	// Create a platform that does not match the host platform
+	differentOS := "linux"
+	if nativePlatform.OS == "linux" {
+		differentOS = "windows"
+	}
+	differentPlatform := ocispec.Platform{
+		OS:           differentOS,
+		Architecture: "amd64",
+	}
+
+	imageID := specialimage.Load(ctx, t, apiClient, func(s string) (*ocispec.Index, error) {
+		i, _, err := specialimage.MultiPlatform(s, s, []ocispec.Platform{nativePlatform, differentPlatform})
+		return i, err
+	})
+
+	for _, tc := range []struct {
+		name              string
+		requestedPlatform *ocispec.Platform
+		expectedPlatform  *ocispec.Platform
+	}{
+		{
+			name:              "default",
+			requestedPlatform: nil,
+			expectedPlatform:  &nativePlatform,
+		},
+		{
+			name:              "native",
+			requestedPlatform: &nativePlatform,
+			expectedPlatform:  &nativePlatform,
+		},
+		{
+			name:              "different",
+			requestedPlatform: &differentPlatform,
+			expectedPlatform:  &differentPlatform,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var opts []client.ImageInspectOption
+			if tc.requestedPlatform != nil {
+				opts = append(opts, client.ImageInspectWithPlatform(tc.requestedPlatform))
+				opts = append(opts, client.ImageInspectWithManifests(true))
+			}
+			inspect, err := apiClient.ImageInspect(ctx, imageID, opts...)
+			assert.NilError(t, err)
+
+			assert.Check(t, is.Equal(inspect.Architecture, nativePlatform.Architecture))
+			assert.Check(t, is.Equal(inspect.Os, nativePlatform.OS))
+
+			assert.Assert(t, inspect.Descriptor != nil)
+			assert.Check(t, is.DeepEqual(*inspect.Descriptor.Platform, *tc.expectedPlatform))
+
+			if tc.requestedPlatform != nil {
+				t.Run("has no manifests", func(t *testing.T) {
+					assert.Check(t, is.Nil(inspect.Manifests))
+				})
+			}
+		})
+	}
 }
