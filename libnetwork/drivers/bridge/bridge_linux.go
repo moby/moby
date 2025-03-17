fce915897cab550b02f0b3aa2e3f949b5c53dc0c
Rob Murray <rob.murray@docker.com>
2025-03-17 10:30:45 +0000

    Combine firewalld reload callbacks for IPv4/IPv6
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/drivers/bridge/bridge_linux.go
+++ libnetwork/drivers/bridge/bridge_linux.go
@@ -939,9 +939,10 @@ func (d *driver) createNetwork(config *networkConfiguration) (err error) {
 
 		// We want to track firewalld configuration so that
 		// if it is started/reloaded, the rules can be applied correctly
-		{config.EnableIPv4 && d.config.EnableIPTables, network.setupFirewalld},
-		// same for IPv6
-		{config.EnableIPv6 && d.config.EnableIP6Tables, network.setupFirewalld6},
+		{
+			(config.EnableIPv4 && d.config.EnableIPTables) || (config.EnableIPv6 && d.config.EnableIP6Tables),
+			network.setupFirewalld,
+		},
 
 		// Setup DefaultGatewayIPv4
 		{config.DefaultGatewayIPv4 != nil, setupGatewayIPv4},
