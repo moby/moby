107495f6e4c0a48f375e2ee6a27dc11bc1c3f116
Sebastiaan van Stijn <github@gone.nl>
2025-03-23 16:54:21 +0100

    internal/registryclient/cache: remove prometheus metrics
    Alternatively, we can use "github.com/docker/go-metrics" directly.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/internal/registryclient/cache/cachedblobdescriptorstore.go
+++ daemon/internal/registryclient/cache/cachedblobdescriptorstore.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/containerd/log"
 	"github.com/docker/distribution"
-	prometheus "github.com/docker/distribution/metrics"
 	"github.com/opencontainers/go-digest"
 )
 
@@ -22,9 +21,6 @@ type cachedBlobStatter struct {
 	backend distribution.BlobDescriptorService
 }
 
-// cacheCount is the number of total cache request received/hits/misses
-var cacheCount = prometheus.StorageNamespace.NewLabeledCounter("cache", "The number of cache request received", "type")
-
 // NewCachedBlobStatter creates a new statter which prefers a cache and
 // falls back to a backend.
 func NewCachedBlobStatter(cache distribution.BlobDescriptorService, backend distribution.BlobDescriptorService) distribution.BlobDescriptorService {
@@ -35,7 +31,6 @@ func NewCachedBlobStatter(cache distribution.BlobDescriptorService, backend dist
 }
 
 func (cbds *cachedBlobStatter) Stat(ctx context.Context, dgst digest.Digest) (distribution.Descriptor, error) {
-	cacheCount.WithValues("Request").Inc(1)
 	desc, err := cbds.cache.Stat(ctx, dgst)
 	if err != nil {
 		if !errors.Is(err, distribution.ErrBlobUnknown) {
@@ -44,10 +39,8 @@ func (cbds *cachedBlobStatter) Stat(ctx context.Context, dgst digest.Digest) (di
 
 		goto fallback
 	}
-	cacheCount.WithValues("Hit").Inc(1)
 	return desc, nil
 fallback:
-	cacheCount.WithValues("Miss").Inc(1)
 	desc, err = cbds.backend.Stat(ctx, dgst)
 	if err != nil {
 		return desc, err
