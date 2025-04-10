aa4abaf82095ba21e20f141a29dcd15a32e7984f
Rob Murray <rob.murray@docker.com>
2025-04-10 20:06:53 +0100

    Use firewaller (iptabler) structs in iptables unit tests
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/drivers/bridge/setup_ip_tables_linux_test.go
+++ libnetwork/drivers/bridge/setup_ip_tables_linux_test.go
@@ -3,18 +3,15 @@ package bridge
 import (
 	"context"
 	"net"
+	"net/netip"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/docker/docker/internal/nlwrap"
 	"github.com/docker/docker/internal/testutils/netnsutils"
-	"github.com/docker/docker/internal/testutils/storeutils"
-	"github.com/docker/docker/libnetwork/driverapi"
 	"github.com/docker/docker/libnetwork/iptables"
-	"github.com/docker/docker/libnetwork/netlabel"
 	"github.com/docker/docker/libnetwork/types"
 	"github.com/vishvananda/netlink"
 	"gotest.tools/v3/assert"
@@ -22,41 +19,18 @@ import (
 )
 
 const (
-	iptablesTestBridgeIP = "192.168.42.1"
+	defaultBridgeName = "testbridge"
 )
-
-// A testRegisterer implements the driverapi.Registerer interface.
-type testRegisterer struct {
-	t *testing.T
-	d *driver
-}
-
-func (r *testRegisterer) RegisterDriver(name string, di driverapi.Driver, _ driverapi.Capability) error {
-	if got, want := name, "bridge"; got != want {
-		r.t.Fatalf("got driver name %s, want %s", got, want)
-	}
-	d, ok := di.(*driver)
-	if !ok {
-		r.t.Fatalf("got driver type %T, want %T", di, &driver{})
-	}
-	r.d = d
-	return nil
-}
 
 func TestProgramIPTable(t *testing.T) {
 	// Create a test bridge with a basic bridge configuration (name + IPv4).
 	defer netnsutils.SetupTestOSContext(t)()
 
-	nh, err := nlwrap.NewHandle()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	createTestBridge(getBasicTestConfig(), &bridgeInterface{nlh: nh}, t)
-	_, err = iptables.GetIptable(iptables.IPv4).NewChain(DockerForwardChain, iptables.Filter)
+	_, err := iptables.GetIptable(iptables.IPv4).NewChain(DockerForwardChain, iptables.Filter)
 	assert.NilError(t, err)
 
 	// Store various iptables chain rules we care for.
+	const iptablesTestBridgeIP = "192.168.42.1"
 	rules := []struct {
 		rule  iptables.Rule
 		descr string
@@ -71,7 +45,22 @@ func TestProgramIPTable(t *testing.T) {
 
 	// Assert the chain rules' insertion and removal.
 	for _, c := range rules {
-		assertIPTableChainProgramming(c.rule, c.descr, t)
+		// Add
+		if err := programChainRule(c.rule, c.descr, true); err != nil {
+			t.Fatalf("Failed to program iptable rule %s: %s", c.descr, err.Error())
+		}
+
+		if !c.rule.Exists() {
+			t.Fatalf("Failed to effectively program iptable rule: %s", c.descr)
+		}
+
+		// Remove
+		if err := programChainRule(c.rule, c.descr, false); err != nil {
+			t.Fatalf("Failed to remove iptable rule %s: %s", c.descr, err.Error())
+		}
+		if c.rule.Exists() {
+			t.Fatalf("Failed to effectively remove iptable rule: %s", c.descr)
+		}
 	}
 }
 
@@ -79,143 +68,68 @@ func TestSetupIPChains(t *testing.T) {
 	// Create a test bridge with a basic bridge configuration (name + IPv4).
 	defer netnsutils.SetupTestOSContext(t)()
 
-	nh, err := nlwrap.NewHandle()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	driverconfig := configuration{
-		EnableIPTables: true,
-	}
-	d := &driver{
-		config: driverconfig,
-	}
-	assertChainConfig(d, t)
-
-	config := getBasicTestConfig()
-	br := &bridgeInterface{nlh: nh}
-	createTestBridge(config, br, t)
-
-	assertBridgeConfig(config, br, d, t)
-
-	config.EnableIPMasquerade = true
-	assertBridgeConfig(config, br, d, t)
-
-	config.EnableICC = true
-	assertBridgeConfig(config, br, d, t)
-
-	config.EnableIPMasquerade = false
-	assertBridgeConfig(config, br, d, t)
-}
-
-func getBasicTestConfig() *networkConfiguration {
-	config := &networkConfiguration{
-		BridgeName:  DefaultBridgeName,
-		EnableIPv4:  true,
-		AddressIPv4: &net.IPNet{IP: net.ParseIP(iptablesTestBridgeIP), Mask: net.CIDRMask(16, 32)},
-	}
-	return config
-}
-
-func createTestBridge(config *networkConfiguration, br *bridgeInterface, t *testing.T) {
-	if err := setupDevice(config, br); err != nil {
-		t.Fatalf("Failed to create the testing Bridge: %s", err.Error())
-	}
-	if err := setupBridgeIPv4(config, br); err != nil {
-		t.Fatalf("Failed to bring up the testing Bridge: %s", err.Error())
-	}
-	if config.EnableIPv6 {
-		if err := setupBridgeIPv6(config, br); err != nil {
-			t.Fatalf("Failed to bring up the testing Bridge: %s", err.Error())
-		}
-	}
-}
-
-// Assert base function which pushes iptables chain rules on insertion and removal.
-func assertIPTableChainProgramming(rule iptables.Rule, descr string, t *testing.T) {
-	t.Helper()
-
-	// Add
-	if err := programChainRule(rule, descr, true); err != nil {
-		t.Fatalf("Failed to program iptable rule %s: %s", descr, err.Error())
-	}
-
-	if !rule.Exists() {
-		t.Fatalf("Failed to effectively program iptable rule: %s", descr)
-	}
-
-	// Remove
-	if err := programChainRule(rule, descr, false); err != nil {
-		t.Fatalf("Failed to remove iptable rule %s: %s", descr, err.Error())
-	}
-	if rule.Exists() {
-		t.Fatalf("Failed to effectively remove iptable rule: %s", descr)
-	}
-}
-
-// Assert function which create chains.
-func assertChainConfig(d *driver, t *testing.T) {
-	var err error
-
-	err = setupIPChains(iptables.IPv4, !d.config.EnableUserlandProxy)
+	ipt := firewaller{IPv4: true}
+	err := ipt.init()
 	assert.NilError(t, err)
 
-	if d.config.EnableIP6Tables {
-		err = setupIPChains(iptables.IPv6, !d.config.EnableUserlandProxy)
-		assert.NilError(t, err)
-	}
-}
-
-// Assert function which pushes chains based on bridge config parameters.
-func assertBridgeConfig(config *networkConfiguration, br *bridgeInterface, d *driver, t *testing.T) {
-	nw := bridgeNetwork{
-		config: config,
-		driver: d,
-		bridge: br,
+	nc := networkConfig{
+		IfName: defaultBridgeName,
+		Config4: networkConfigFam{
+			Prefix: netip.MustParsePrefix("192.168.42.0/24"),
+		},
 	}
 
-	fwn, err := nw.newIptablesNetwork()
-	assert.NilError(t, err)
-	assert.Check(t, fwn != nil, "no firewaller network")
+	assertBridgeConfig(t, ipt, nc)
+
+	nc.Masquerade = true
+	assertBridgeConfig(t, ipt, nc)
+
+	nc.ICC = true
+	assertBridgeConfig(t, ipt, nc)
+
+	nc.Masquerade = false
+	assertBridgeConfig(t, ipt, nc)
 }
 
 // Regression test for https://github.com/moby/moby/issues/46445
 func TestSetupIP6TablesWithHostIPv4(t *testing.T) {
 	defer netnsutils.SetupTestOSContext(t)()
-	d := newDriver(storeutils.NewTempStore(t))
-	dc := &configuration{
-		EnableIPTables:  true,
-		EnableIP6Tables: true,
+	ipt := firewaller{
+		IPv4: true,
+		IPv6: true,
 	}
-	if err := d.configure(map[string]interface{}{netlabel.GenericData: dc}); err != nil {
-		t.Fatal(err)
+	err := ipt.init()
+	assert.NilError(t, err)
+
+	nc := networkConfig{
+		IfName:     defaultBridgeName,
+		Masquerade: true,
+		Config4: networkConfigFam{
+			HostIP: netip.MustParseAddr("192.0.2.2"),
+			Prefix: netip.MustParsePrefix("192.168.42.0/24"),
+		},
+		Config6: networkConfigFam{
+			Prefix: netip.MustParsePrefix("2001:db8::/64"),
+		},
 	}
-	nc := &networkConfiguration{
-		BridgeName:         DefaultBridgeName,
-		AddressIPv4:        &net.IPNet{IP: net.ParseIP(iptablesTestBridgeIP), Mask: net.CIDRMask(16, 32)},
-		EnableIPMasquerade: true,
-		EnableIPv4:         true,
-		EnableIPv6:         true,
-		AddressIPv6:        &net.IPNet{IP: net.ParseIP("2001:db8::1"), Mask: net.CIDRMask(64, 128)},
-		HostIPv4:           net.ParseIP("192.0.2.2"),
-	}
-	nh, err := nlwrap.NewHandle()
-	if err != nil {
-		t.Fatal(err)
-	}
-	br := &bridgeInterface{nlh: nh}
-	createTestBridge(nc, br, t)
-	assertBridgeConfig(nc, br, d, t)
+	assertBridgeConfig(t, ipt, nc)
+}
+
+// Assert function which pushes chains based on bridge config parameters.
+func assertBridgeConfig(t *testing.T, ipt firewaller, nc networkConfig) {
+	t.Helper()
+	n, err := ipt.NewNetwork(nc)
+	assert.NilError(t, err)
+	err = n.delNetworkLevelRules()
+	assert.NilError(t, err)
 }
 
 func TestOutgoingNATRules(t *testing.T) {
 	br := "br-nattest"
-	brIPv4 := &net.IPNet{IP: net.ParseIP(iptablesTestBridgeIP), Mask: net.CIDRMask(16, 32)}
-	brIPv6 := &net.IPNet{IP: net.ParseIP("2001:db8::1"), Mask: net.CIDRMask(64, 128)}
-	maskedBrIPv4 := &net.IPNet{IP: brIPv4.IP.Mask(brIPv4.Mask), Mask: brIPv4.Mask}
-	maskedBrIPv6 := &net.IPNet{IP: brIPv6.IP.Mask(brIPv6.Mask), Mask: brIPv6.Mask}
-	hostIPv4 := net.ParseIP("192.0.2.2")
-	hostIPv6 := net.ParseIP("2001:db8:1::1")
+	maskedBrIPv4 := netip.MustParsePrefix("192.168.42.1/16").Masked()
+	maskedBrIPv6 := netip.MustParsePrefix("2001:db8::1/64").Masked()
+	hostIPv4 := netip.MustParseAddr("192.0.2.2")
+	hostIPv6 := netip.MustParseAddr("2001:db8:1::1")
 	for _, tc := range []struct {
 		desc               string
 		enableIPTables     bool
@@ -223,8 +137,8 @@ func TestOutgoingNATRules(t *testing.T) {
 		enableIPv4         bool
 		enableIPv6         bool
 		enableIPMasquerade bool
-		hostIPv4           net.IP
-		hostIPv6           net.IP
+		hostIPv4           netip.Addr
+		hostIPv6           netip.Addr
 		// Hairpin NAT rules are not tested here because they are orthogonal to outgoing NAT.  They
 		// exist to support the port forwarding DNAT rules: without any port forwarding there would be
 		// no need for any hairpin NAT rules, and when there is port forwarding then hairpin NAT rules
@@ -350,45 +264,34 @@ func TestOutgoingNATRules(t *testing.T) {
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
 			defer netnsutils.SetupTestOSContext(t)()
-			dc := &configuration{
-				EnableIPTables:  tc.enableIPTables,
-				EnableIP6Tables: tc.enableIP6Tables,
+
+			ipt := firewaller{
+				IPv4: tc.enableIPTables,
+				IPv6: tc.enableIP6Tables,
 			}
-			r := &testRegisterer{t: t}
-			if err := Register(r, storeutils.NewTempStore(t), map[string]interface{}{netlabel.GenericData: dc}); err != nil {
-				t.Fatal(err)
+			err := ipt.init()
+			assert.NilError(t, err)
+
+			nc := networkConfig{
+				IfName:     br,
+				Masquerade: tc.enableIPMasquerade,
+				Config4: networkConfigFam{
+					HostIP: tc.hostIPv4,
+					Prefix: maskedBrIPv4,
+				},
+				Config6: networkConfigFam{
+					HostIP: tc.hostIPv6,
+					Prefix: maskedBrIPv6,
+				},
 			}
-			if r.d == nil {
-				t.Fatal("testRegisterer.RegisterDriver never called")
-			}
-			nc := &networkConfiguration{
-				BridgeName:         br,
-				AddressIPv4:        brIPv4,
-				AddressIPv6:        brIPv6,
-				EnableIPv4:         tc.enableIPv4,
-				EnableIPv6:         tc.enableIPv6,
-				EnableIPMasquerade: tc.enableIPMasquerade,
-				HostIPv4:           tc.hostIPv4,
-				HostIPv6:           tc.hostIPv6,
-			}
-			ipv4Data := []driverapi.IPAMData{{Pool: maskedBrIPv4, Gateway: brIPv4}}
-			ipv6Data := []driverapi.IPAMData{{Pool: maskedBrIPv6, Gateway: brIPv6}}
-			if !nc.EnableIPv4 {
-				nc.AddressIPv4 = nil
-				ipv4Data = nil
-			}
-			if !nc.EnableIPv6 {
-				nc.AddressIPv6 = nil
-				ipv6Data = nil
-			}
-			if err := r.d.CreateNetwork(context.Background(), "nattest", map[string]interface{}{netlabel.GenericData: nc}, nil, ipv4Data, ipv6Data); err != nil {
-				t.Fatal(err)
-			}
+			n, err := ipt.NewNetwork(nc)
+			assert.NilError(t, err)
+
 			defer func() {
-				if err := r.d.DeleteNetwork("nattest"); err != nil {
-					t.Fatal(err)
-				}
+				err = n.delNetworkLevelRules()
+				assert.NilError(t, err)
 			}()
+
 			// Log the contents of all chains to aid troubleshooting.
 			for _, ipv := range []iptables.IPVersion{iptables.IPv4, iptables.IPv6} {
 				ipt := iptables.GetIptable(ipv)
