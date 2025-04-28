4c57ffaca7513fc62bba91b61c80354611d9c452
Sebastiaan van Stijn <github@gone.nl>
2025-04-28 15:36:30 +0200

    api/server/router/build: fix vars that shadowed (govet)
        api/server/router/build/build_routes.go:212:6: shadow: declaration of "bs" shadows declaration at line 200 (govet)
                if bs, err := parseBytesFromFormValue("max-used-space"); err != nil {
                   ^
        api/server/router/build/build_routes.go:218:6: shadow: declaration of "bs" shadows declaration at line 200 (govet)
                if bs, err := parseBytesFromFormValue("min-free-space"); err != nil {
                   ^
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/server/router/build/build_routes.go
+++ api/server/router/build/build_routes.go
@@ -197,17 +197,18 @@ func (br *buildRouter) postPrune(ctx context.Context, w http.ResponseWriter, r *
 
 	version := httputils.VersionFromContext(ctx)
 	if versions.GreaterThanOrEqualTo(version, "1.48") {
-		bs, err := parseBytesFromFormValue("reserved-space")
-		if err != nil {
+		if bs, err := parseBytesFromFormValue("reserved-space"); err != nil {
 			return err
-		} else if bs == 0 {
-			// Deprecated parameter. Only checked if reserved-space is not used.
-			bs, err = parseBytesFromFormValue("keep-storage")
-			if err != nil {
-				return err
+		} else {
+			if bs == 0 {
+				// Deprecated parameter. Only checked if reserved-space is not used.
+				bs, err = parseBytesFromFormValue("keep-storage")
+				if err != nil {
+					return err
+				}
 			}
+			opts.ReservedSpace = bs
 		}
-		opts.ReservedSpace = bs
 
 		if bs, err := parseBytesFromFormValue("max-used-space"); err != nil {
 			return err
@@ -222,11 +223,11 @@ func (br *buildRouter) postPrune(ctx context.Context, w http.ResponseWriter, r *
 		}
 	} else {
 		// Only keep-storage was valid in pre-1.48 versions.
-		bs, err := parseBytesFromFormValue("keep-storage")
-		if err != nil {
+		if bs, err := parseBytesFromFormValue("keep-storage"); err != nil {
 			return err
+		} else {
+			opts.ReservedSpace = bs
 		}
-		opts.ReservedSpace = bs
 	}
 
 	report, err := br.backend.PruneCache(ctx, opts)
