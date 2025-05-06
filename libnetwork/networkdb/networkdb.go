ec65f2d21b83a2ccb7b216357d4d979e6157966d
Cory Snider <csnider@mirantis.com>
2025-05-06 18:57:11 -0400

    libn/networkdb: fix data race in GetTableByNetwork
    The function was accessing the index map without holding the mutex, so
    it would race any mutation to the database indexes. Fetch the reference
    to the tree's root while holding a read lock. Since the radix tree is
    immutable, taking a reference to the root is equivalent to starting a
    read-only database transaction, providing a consistent view of the data
    at a snapshot in time, even as the live state is mutated concurrently.
    
    Also optimize the WalkTable function by leveraging the immutability of
    the radix tree.
    
    Signed-off-by: Cory Snider <csnider@mirantis.com>

--- libnetwork/networkdb/networkdb.go
+++ libnetwork/networkdb/networkdb.go
@@ -429,8 +429,11 @@ type TableElem struct {
 // GetTableByNetwork walks the networkdb by the give table and network id and
 // returns a map of keys and values
 func (nDB *NetworkDB) GetTableByNetwork(tname, nid string) map[string]*TableElem {
+	nDB.RLock()
+	root := nDB.indexes[byNetwork].Root()
+	nDB.RUnlock()
 	entries := make(map[string]*TableElem)
-	nDB.indexes[byTable].Root().WalkPrefix([]byte(fmt.Sprintf("/%s/%s", tname, nid)), func(k []byte, v *entry) bool {
+	root.WalkPrefix([]byte(fmt.Sprintf("/%s/%s", tname, nid)), func(k []byte, v *entry) bool {
 		if v.deleting {
 			return false
 		}
@@ -585,21 +588,14 @@ func (nDB *NetworkDB) deleteNodeTableEntries(node string) {
 // value. The walk stops if the passed function returns a true.
 func (nDB *NetworkDB) WalkTable(tname string, fn func(string, string, []byte, bool) bool) error {
 	nDB.RLock()
-	values := make(map[string]*entry)
-	nDB.indexes[byTable].Root().WalkPrefix([]byte("/"+tname), func(path []byte, v *entry) bool {
-		values[string(path)] = v
-		return false
-	})
+	root := nDB.indexes[byTable].Root()
 	nDB.RUnlock()
-
-	for k, v := range values {
-		params := strings.Split(k[1:], "/")
+	root.WalkPrefix([]byte("/"+tname), func(path []byte, v *entry) bool {
+		params := strings.Split(string(path[1:]), "/")
 		nid := params[1]
 		key := params[2]
-		if fn(nid, key, v.value, v.deleting) {
-			return nil
-		}
-	}
+		return fn(nid, key, v.value, v.deleting)
+	})
 
 	return nil
 }
