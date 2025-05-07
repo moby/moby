84ef7e48998f8444da39393f3c23e6aeb1a842b6
Rob Murray <rob.murray@docker.com>
2025-05-07 19:57:27 +0100

    Allow TestIsolated/ipv6 to unexpectedly pass
    This test normally fails due to a known issue, but it
    has recently started passing in CI ... not sure why, it
    still fails locally - so, allow it to unexpeectedly
    pass, but collect some debug info to try to undertand
    why.
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- integration/network/bridge/bridge_linux_test.go
+++ integration/network/bridge/bridge_linux_test.go
@@ -458,7 +458,12 @@ func TestIsolated(t *testing.T) {
 		defer apiClient.ContainerRemove(ctx, res.ContainerID, containertypes.RemoveOptions{Force: true})
 		if ipv == "-6" && networking.FirewalldRunning() {
 			// FIXME(robmry) - this fails due to https://github.com/moby/moby/issues/49680
-			assert.Check(t, is.Equal(res.ExitCode, 1))
+			if res.ExitCode != 1 {
+				t.Log("Unexpected pass!")
+				t.Log(icmd.RunCommand("nft", "list ruleset").Stdout())
+				t.Log(icmd.RunCommand("ip", "a").Stdout())
+				t.Log(icmd.RunCommand("route", "-6").Stdout())
+			}
 			t.Skip("XFAIL - IPv6, firewalld, isolated - see https://github.com/moby/moby/issues/49680")
 		}
 		assert.Check(t, is.Equal(res.ExitCode, 0))
