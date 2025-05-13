b6916e6dc4f717592ac779c9daf5412d4e23dfb8
Drew Erny <derny@mirantis.com>
2025-05-13 09:29:27 -0500

    25.0 Backport: Add image pull metrics
    Adds some metrics for image pulls:
    
    * ImagePullsStarted is a counter that increments every time an image
    pull starts
    * ImagePulls is a counter that increments every time an image pull
    completes, labeled with whether it succeeded or not
    * ImagePullBytes is a counter of the number of bytes downloaded. This
    can be used to compute things like image pull throughput.
    
    Backport of #49926 to the 25.0 branch. Done manually, no cherry-picks,
    because refactoring moved around a bunch of files between 25.0 and main.
    
    Signed-off-by: Drew Erny <derny@mirantis.com>

--- daemon/images/locals.go
+++ daemon/images/locals.go
@@ -4,10 +4,21 @@ import (
 	metrics "github.com/docker/go-metrics"
 )
 
-var imageActions metrics.LabeledTimer
+var (
+	imageActions      metrics.LabeledTimer
+	imagePullsStarted metrics.Counter
+	imagePulls        metrics.LabeledCounter
+)
 
 func init() {
 	ns := metrics.NewNamespace("engine", "daemon", nil)
+
+	// imagePullsStarted increments when a pull starts.
+	imagePullsStarted = ns.NewCounter("image_pulls_started", "The number of total image pulls started")
+	// imagePulls increments when a pull finishes. By subtracting this from
+	// ImagePullsStarted, the user can determine how many pulls are in progress.
+	imagePulls = ns.NewLabeledCounter("image_pulls", "The number of total image pulls", "status")
+
 	imageActions = ns.NewLabeledTimer("image_actions", "The number of seconds it takes to process each image action", "action")
 	// TODO: is it OK to register a namespace with the same name? Or does this
 	// need to be exported from somewhere?
