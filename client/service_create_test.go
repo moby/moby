26a8759c4778a6ffc8e67f726fa329e54cfa68fc
Paweł Gronowski <pawel.gronowski@docker.com>
2025-05-16 20:50:49 +0200

    client/service: use containerd cerrdefs
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- client/service_create_test.go
+++ client/service_create_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/docker/docker/api/types"
 	registrytypes "github.com/docker/docker/api/types/registry"
 	"github.com/docker/docker/api/types/swarm"
-	"github.com/docker/docker/errdefs"
+	cerrdefs "github.com/containerd/errdefs"
 	"github.com/opencontainers/go-digest"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"gotest.tools/v3/assert"
@@ -25,7 +25,7 @@ func TestServiceCreateError(t *testing.T) {
 		client: newMockClient(errorMock(http.StatusInternalServerError, "Server error")),
 	}
 	_, err := client.ServiceCreate(context.Background(), swarm.ServiceSpec{}, types.ServiceCreateOptions{})
-	assert.Check(t, is.ErrorType(err, errdefs.IsSystem))
+	assert.Check(t, is.ErrorType(err, cerrdefs.IsInternal))
 }
 
 // TestServiceCreateConnectionError verifies that connection errors occurring
