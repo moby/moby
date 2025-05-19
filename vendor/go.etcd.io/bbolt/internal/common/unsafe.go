3dd8f03f25319d0ffa572c28ef944a7f255dd44a
Paweł Gronowski <pawel.gronowski@docker.com>
2025-05-19 18:43:24 +0200

    vendor: go.etcd.io/bbolt v1.4.0
    full diff: https://github.com/etcd-io/bbolt/compare/v1.3.11...v1.4.0
    
    Changelog: https://github.com/etcd-io/bbolt/blob/main/CHANGELOG/CHANGELOG-1.4.md
    
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- vendor/go.etcd.io/bbolt/internal/common/unsafe.go
+++ vendor/go.etcd.io/bbolt/internal/common/unsafe.go
@@ -1,18 +1,18 @@
-package bbolt
+package common
 
 import (
 	"unsafe"
 )
 
-func unsafeAdd(base unsafe.Pointer, offset uintptr) unsafe.Pointer {
+func UnsafeAdd(base unsafe.Pointer, offset uintptr) unsafe.Pointer {
 	return unsafe.Pointer(uintptr(base) + offset)
 }
 
-func unsafeIndex(base unsafe.Pointer, offset uintptr, elemsz uintptr, n int) unsafe.Pointer {
+func UnsafeIndex(base unsafe.Pointer, offset uintptr, elemsz uintptr, n int) unsafe.Pointer {
 	return unsafe.Pointer(uintptr(base) + offset + uintptr(n)*elemsz)
 }
 
-func unsafeByteSlice(base unsafe.Pointer, offset uintptr, i, j int) []byte {
+func UnsafeByteSlice(base unsafe.Pointer, offset uintptr, i, j int) []byte {
 	// See: https://github.com/golang/go/wiki/cgo#turning-c-arrays-into-go-slices
 	//
 	// This memory is not allocated from C, but it is unmanaged by Go's
@@ -23,5 +23,5 @@ func unsafeByteSlice(base unsafe.Pointer, offset uintptr, i, j int) []byte {
 	// index 0.  However, the wiki never says that the address must be to
 	// the beginning of a C allocation (or even that malloc was used at
 	// all), so this is believed to be correct.
-	return (*[maxAllocSize]byte)(unsafeAdd(base, offset))[i:j:j]
+	return (*[pageMaxAllocSize]byte)(UnsafeAdd(base, offset))[i:j:j]
 }
