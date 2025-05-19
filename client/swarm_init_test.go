68a8a8f3c8a0c26a878cef084a1abf22f8a6fb6e
Paweł Gronowski <pawel.gronowski@docker.com>
2025-05-19 14:14:17 +0200

    client/swarm: use containerd errdefs checks
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- client/swarm_init_test.go
+++ client/swarm_init_test.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 	"testing"
 
+	cerrdefs "github.com/containerd/errdefs"
 	"github.com/docker/docker/api/types/swarm"
-	"github.com/docker/docker/errdefs"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
 )
@@ -21,7 +21,7 @@ func TestSwarmInitError(t *testing.T) {
 	}
 
 	_, err := client.SwarmInit(context.Background(), swarm.InitRequest{})
-	assert.Check(t, is.ErrorType(err, errdefs.IsSystem))
+	assert.Check(t, is.ErrorType(err, cerrdefs.IsInternal))
 }
 
 func TestSwarmInit(t *testing.T) {
