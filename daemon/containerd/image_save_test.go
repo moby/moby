ef25dabbcf3e55922f21059c836584c3dd8eac7f
Cesar Talledo <cesar.talledo@docker.com>
2025-05-28 13:53:08 -0700

    Add support for multiple platforms in image export and loading.
    Currently the image export and load APIs can be used to export or load all
    platforms for the image, or a single specified platform.
    
    This commit updates the API so that it accepts a list of platforms to export or
    load, thereby giving clients the ability to export only selected platforms of an
    image into a tar file, or load selected platforms from a tar file.
    
    Unit and integration tests were updated accordingly.
    
    As this requires a daemon API change, the API version was bumped.
    
    Signed-off-by: Cesar Talledo <cesar.talledo@docker.com>

--- daemon/containerd/image_save_test.go
+++ daemon/containerd/image_save_test.go
@@ -31,12 +31,17 @@ func TestImageMultiplatformSaveShallowWithNative(t *testing.T) {
 		Architecture: "arm64",
 	}
 
+	riscv64 := platforms.Platform{
+		OS:           "linux",
+		Architecture: "riscv64",
+	}
+
 	imgSvc := fakeImageService(t, ctx, store)
 	// Mock the native platform.
 	imgSvc.defaultPlatformOverride = platforms.Only(native)
 
 	idx, _, err := specialimage.PartialMultiPlatform(contentDir, "partial-with-native:latest", specialimage.PartialOpts{
-		Stored:  []ocispec.Platform{native},
+		Stored:  []ocispec.Platform{native, riscv64},
 		Missing: []ocispec.Platform{arm64},
 	})
 	assert.NilError(t, err)
@@ -49,12 +54,20 @@ func TestImageMultiplatformSaveShallowWithNative(t *testing.T) {
 		assert.NilError(t, err)
 	})
 	t.Run("export native", func(t *testing.T) {
-		err = imgSvc.ExportImage(ctx, []string{img.Name}, &native, io.Discard)
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{native}, io.Discard)
+		assert.NilError(t, err)
+	})
+	t.Run("export multiple platforms", func(t *testing.T) {
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{native, riscv64}, io.Discard)
 		assert.NilError(t, err)
 	})
 	t.Run("export missing", func(t *testing.T) {
-		err = imgSvc.ExportImage(ctx, []string{img.Name}, &arm64, io.Discard)
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{arm64}, io.Discard)
 		assert.Check(t, is.ErrorType(err, cerrdefs.IsNotFound))
+	})
+	t.Run("export multiple platforms with some missing", func(t *testing.T) {
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{arm64, riscv64}, io.Discard)
+		assert.NilError(t, err)
 	})
 }
 
@@ -74,12 +87,22 @@ func TestImageMultiplatformSaveShallowWithoutNative(t *testing.T) {
 		Architecture: "arm64",
 	}
 
+	riscv64 := platforms.Platform{
+		OS:           "linux",
+		Architecture: "riscv64",
+	}
+
+	s390x := platforms.Platform{
+		OS:           "linux",
+		Architecture: "s390x",
+	}
+
 	imgSvc := fakeImageService(t, ctx, store)
 	// Mock the native platform.
 	imgSvc.defaultPlatformOverride = platforms.Only(native)
 
 	idx, _, err := specialimage.PartialMultiPlatform(contentDir, "partial-without-native:latest", specialimage.PartialOpts{
-		Stored:  []ocispec.Platform{arm64},
+		Stored:  []ocispec.Platform{arm64, riscv64},
 		Missing: []ocispec.Platform{native},
 	})
 	assert.NilError(t, err)
@@ -93,11 +116,23 @@ func TestImageMultiplatformSaveShallowWithoutNative(t *testing.T) {
 		assert.NilError(t, err)
 	})
 	t.Run("export native", func(t *testing.T) {
-		err = imgSvc.ExportImage(ctx, []string{img.Name}, &native, io.Discard)
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{native}, io.Discard)
 		assert.Check(t, is.ErrorType(err, cerrdefs.IsNotFound))
 	})
 	t.Run("export arm64", func(t *testing.T) {
-		err = imgSvc.ExportImage(ctx, []string{img.Name}, &arm64, io.Discard)
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{arm64}, io.Discard)
 		assert.NilError(t, err)
+	})
+	t.Run("export multiple platforms", func(t *testing.T) {
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{arm64, riscv64}, io.Discard)
+		assert.NilError(t, err)
+	})
+	t.Run("export multiple platforms with some missing", func(t *testing.T) {
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{arm64, native}, io.Discard)
+		assert.Check(t, is.ErrorType(err, cerrdefs.IsNotFound))
+	})
+	t.Run("export non existing platform", func(t *testing.T) {
+		err = imgSvc.ExportImage(ctx, []string{img.Name}, []ocispec.Platform{s390x}, io.Discard)
+		assert.Check(t, is.ErrorType(err, cerrdefs.IsNotFound))
 	})
 }
