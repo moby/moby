f06c450a8e5f0a0e6824e6e62f38bb48afd943be
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-05-28 05:40:24 +0000

    libcontainerd: replace uses of errdefs package
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- libcontainerd/replace.go
+++ libcontainerd/replace.go
@@ -4,11 +4,11 @@ import (
 	"context"
 
 	containerd "github.com/containerd/containerd/v2/client"
+	cerrdefs "github.com/containerd/errdefs"
 	"github.com/containerd/log"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pkg/errors"
 
-	"github.com/docker/docker/errdefs"
 	"github.com/docker/docker/libcontainerd/types"
 )
 
@@ -19,14 +19,14 @@ func ReplaceContainer(ctx context.Context, client types.Client, id string, spec 
 		return client.NewContainer(ctx, id, spec, shim, runtimeOptions, opts...)
 	}
 	ctr, err := newContainer()
-	if err == nil || !errdefs.IsConflict(err) {
+	if err == nil || !cerrdefs.IsConflict(err) {
 		return ctr, err
 	}
 
 	log.G(ctx).WithContext(ctx).WithField("container", id).Debug("A container already exists with the same ID. Attempting to clean up the old container.")
 	ctr, err = client.LoadContainer(ctx, id)
 	if err != nil {
-		if errdefs.IsNotFound(err) {
+		if cerrdefs.IsNotFound(err) {
 			// Task failed successfully: the container no longer exists,
 			// despite us not doing anything. May as well try to create
 			// the container again. It might succeed.
@@ -36,7 +36,7 @@ func ReplaceContainer(ctx context.Context, client types.Client, id string, spec 
 	}
 	tsk, err := ctr.Task(ctx)
 	if err != nil {
-		if errdefs.IsNotFound(err) {
+		if cerrdefs.IsNotFound(err) {
 			goto deleteContainer
 		}
 		// There is no point in trying to delete the container if we
@@ -46,14 +46,14 @@ func ReplaceContainer(ctx context.Context, client types.Client, id string, spec 
 		return nil, errors.Wrap(err, "could not load stale containerd task object")
 	}
 	if err := tsk.ForceDelete(ctx); err != nil {
-		if !errdefs.IsNotFound(err) {
+		if !cerrdefs.IsNotFound(err) {
 			return nil, errors.Wrap(err, "could not delete stale containerd task object")
 		}
 		// The task might have exited on its own. Proceed with
 		// attempting to delete the container.
 	}
 deleteContainer:
-	if err := ctr.Delete(ctx); err != nil && !errdefs.IsNotFound(err) {
+	if err := ctr.Delete(ctx); err != nil && !cerrdefs.IsNotFound(err) {
 		return nil, errors.Wrap(err, "could not delete stale containerd container object")
 	}
 
