dcf253ffe25b715b2f6a790a45e60da67a71f6f9
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-05-28 05:40:55 +0000

    plugin: replace uses of errdefs package
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- plugin/executor/containerd/containerd.go
+++ plugin/executor/containerd/containerd.go
@@ -9,6 +9,7 @@ import (
 
 	containerd "github.com/containerd/containerd/v2/client"
 	"github.com/containerd/containerd/v2/pkg/cio"
+	cerrdefs "github.com/containerd/errdefs"
 	"github.com/containerd/log"
 	"github.com/docker/docker/errdefs"
 	"github.com/docker/docker/libcontainerd"
@@ -61,12 +62,12 @@ type c8dPlugin struct {
 // deleteTaskAndContainer deletes plugin task and then plugin container from containerd
 func (p c8dPlugin) deleteTaskAndContainer(ctx context.Context) {
 	if p.tsk != nil {
-		if err := p.tsk.ForceDelete(ctx); err != nil && !errdefs.IsNotFound(err) {
+		if err := p.tsk.ForceDelete(ctx); err != nil && !cerrdefs.IsNotFound(err) {
 			p.log.WithError(err).Error("failed to delete plugin task from containerd")
 		}
 	}
 	if p.ctr != nil {
-		if err := p.ctr.Delete(ctx); err != nil && !errdefs.IsNotFound(err) {
+		if err := p.ctr.Delete(ctx); err != nil && !cerrdefs.IsNotFound(err) {
 			p.log.WithError(err).Error("failed to delete plugin container from containerd")
 		}
 	}
@@ -102,14 +103,14 @@ func (e *Executor) Restore(id string, stdout, stderr io.WriteCloser) (bool, erro
 	p := c8dPlugin{log: log.G(ctx).WithField("plugin", id)}
 	ctr, err := e.client.LoadContainer(ctx, id)
 	if err != nil {
-		if errdefs.IsNotFound(err) {
+		if cerrdefs.IsNotFound(err) {
 			return false, nil
 		}
 		return false, err
 	}
 	p.tsk, err = ctr.AttachTask(ctx, attachStreamsFunc(stdout, stderr))
 	if err != nil {
-		if errdefs.IsNotFound(err) {
+		if cerrdefs.IsNotFound(err) {
 			p.deleteTaskAndContainer(ctx)
 			return false, nil
 		}
@@ -117,7 +118,7 @@ func (e *Executor) Restore(id string, stdout, stderr io.WriteCloser) (bool, erro
 	}
 	s, err := p.tsk.Status(ctx)
 	if err != nil {
-		if errdefs.IsNotFound(err) {
+		if cerrdefs.IsNotFound(err) {
 			// Task vanished after attaching?
 			p.tsk = nil
 			p.deleteTaskAndContainer(ctx)
