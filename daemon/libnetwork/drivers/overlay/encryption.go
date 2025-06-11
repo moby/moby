c6bb9951a75af53207054d3d4b830351c393f3c8
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-06-11 08:08:31 +0200

    fix valSwap from go-critic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- daemon/libnetwork/drivers/overlay/encryption.go
+++ daemon/libnetwork/drivers/overlay/encryption.go
@@ -598,9 +598,7 @@ func updateNodeKey(lIP, aIP, rIP net.IP, idxs []spi, curKeys []*key, newIdx, pri
 
 	// swap
 	if priIdx > 0 {
-		swp := spis[0]
-		spis[0] = spis[priIdx]
-		spis[priIdx] = swp
+		spis[0], spis[priIdx] = spis[priIdx], spis[0]
 	}
 	// prune
 	if delIdx != -1 {
