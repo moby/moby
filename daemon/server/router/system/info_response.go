7ec45e22dc48d816ab7770eab23885d1156af13e
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-06-11 07:55:14 +0000

    fix typeUnparen from go-critic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- daemon/server/router/system/info_response.go
+++ daemon/server/router/system/info_response.go
@@ -19,7 +19,7 @@ type infoResponse struct {
 // in API responses.
 func (sc *infoResponse) MarshalJSON() ([]byte, error) {
 	type tmp *system.Info
-	base, err := json.Marshal((tmp)(sc.Info))
+	base, err := json.Marshal(tmp(sc.Info))
 	if err != nil {
 		return nil, err
 	}
