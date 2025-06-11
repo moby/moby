f6c67fe8257994c82519e693ff91570809b1a43b
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-06-11 07:55:14 +0000

    fix typeUnparen from go-critic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- integration-cli/docker_cli_run_test.go
+++ integration-cli/docker_cli_run_test.go
@@ -1409,7 +1409,7 @@ func (s *DockerCLIRunSuite) TestRunNonRootUserResolvName(c *testing.T) {
 
 	cID := getIDByName(c, "testperm")
 
-	fmode := (os.FileMode)(0o644)
+	fmode := os.FileMode(0o644)
 	finfo, err := os.Stat(containerStorageFile(cID, "resolv.conf"))
 	if err != nil {
 		c.Fatal(err)
