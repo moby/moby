9af7b46f3eacfb186b5659cfcd11815bd76790f2
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-06-11 07:43:31 +0000

    fix assignOp from go-critic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- daemon/libnetwork/networkdb/networkdb_test.go
+++ daemon/libnetwork/networkdb/networkdb_test.go
@@ -902,8 +902,8 @@ func TestNetworkDBIslands(t *testing.T) {
 	_ = log.SetLevel("debug")
 	conf := DefaultConfig()
 	// Shorten durations to speed up test execution.
-	conf.rejoinClusterDuration = conf.rejoinClusterDuration / 10
-	conf.rejoinClusterInterval = conf.rejoinClusterInterval / 10
+	conf.rejoinClusterDuration /= 10
+	conf.rejoinClusterInterval /= 10
 	dbs := createNetworkDBInstances(t, 5, "node", conf)
 
 	// Get the node IP used currently
