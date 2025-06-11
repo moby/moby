83b2fc245d510031c0d2676013a6b74bca4a0fd9
Rob Murray <rob.murray@docker.com>
2025-06-11 12:05:46 +0100

    Revert "Fix possible overlapping IPs when ingressNA == nil"
    This reverts commit 56ad94156468e2d9cb87dede3b28a4f53a45f79a.
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- daemon/cluster/executor/container/executor.go
+++ daemon/cluster/executor/container/executor.go
@@ -213,35 +213,36 @@ func (e *executor) Configure(ctx context.Context, node *api.Node) error {
 
 	if ingressNA == nil {
 		e.backend.ReleaseIngress()
-	} else {
-		options := network.CreateOptions{
-			Driver: ingressNA.Network.DriverState.Name,
-			IPAM: &network.IPAM{
-				Driver: ingressNA.Network.IPAM.Driver.Name,
-			},
-			Options: ingressNA.Network.DriverState.Options,
-			Ingress: true,
-		}
+		return e.backend.GetAttachmentStore().ResetAttachments(attachments)
+	}
 
-		for _, ic := range ingressNA.Network.IPAM.Configs {
-			c := network.IPAMConfig{
-				Subnet:  ic.Subnet,
-				IPRange: ic.Range,
-				Gateway: ic.Gateway,
-			}
-			options.IPAM.Config = append(options.IPAM.Config, c)
-		}
+	options := network.CreateOptions{
+		Driver: ingressNA.Network.DriverState.Name,
+		IPAM: &network.IPAM{
+			Driver: ingressNA.Network.IPAM.Driver.Name,
+		},
+		Options: ingressNA.Network.DriverState.Options,
+		Ingress: true,
+	}
 
-		_, err := e.backend.SetupIngress(clustertypes.NetworkCreateRequest{
-			ID: ingressNA.Network.ID,
-			CreateRequest: network.CreateRequest{
-				Name:          ingressNA.Network.Spec.Annotations.Name,
-				CreateOptions: options,
-			},
-		}, ingressNA.Addresses[0])
-		if err != nil {
-			return err
+	for _, ic := range ingressNA.Network.IPAM.Configs {
+		c := network.IPAMConfig{
+			Subnet:  ic.Subnet,
+			IPRange: ic.Range,
+			Gateway: ic.Gateway,
 		}
+		options.IPAM.Config = append(options.IPAM.Config, c)
+	}
+
+	_, err := e.backend.SetupIngress(clustertypes.NetworkCreateRequest{
+		ID: ingressNA.Network.ID,
+		CreateRequest: network.CreateRequest{
+			Name:          ingressNA.Network.Spec.Annotations.Name,
+			CreateOptions: options,
+		},
+	}, ingressNA.Addresses[0])
+	if err != nil {
+		return err
 	}
 
 	var (
