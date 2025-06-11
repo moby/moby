2297ae3e64c543f94d104058a8d4092829d6bb29
Rob Murray <rob.murray@docker.com>
2025-06-11 12:05:41 +0100

    Revert "libn/networkdb: Watch() without race conditions"
    This reverts commit a3aea152570a67e9a0a2ff0132b838ea084e3499.
    
    Signed-off-by: Rob Murray <rob.murray@docker.com>

--- libnetwork/networkdb/watch.go
+++ libnetwork/networkdb/watch.go
@@ -2,7 +2,6 @@ package networkdb
 
 import (
 	"net"
-	"strings"
 
 	"github.com/docker/go-events"
 )
@@ -43,8 +42,7 @@ type DeleteEvent event
 // network or any combination of the tuple. If any of the
 // filter is an empty string it acts as a wildcard for that
 // field. Watch returns a channel of events, where the events will be
-// sent. The watch channel is initialized with synthetic create events for all
-// the existing table entries not owned by this node which match the filters.
+// sent.
 func (nDB *NetworkDB) Watch(tname, nid string) (*events.Channel, func()) {
 	var matcher events.Matcher
 
@@ -77,45 +75,6 @@ func (nDB *NetworkDB) Watch(tname, nid string) (*events.Channel, func()) {
 
 	if matcher != nil {
 		sink = events.NewFilter(sink, matcher)
-	}
-
-	// Synthesize events for all the existing table entries not owned by
-	// this node so that the watcher receives all state without racing with
-	// any concurrent mutations to the table.
-	nDB.RLock()
-	defer nDB.RUnlock()
-	if tname == "" {
-		var prefix []byte
-		if nid != "" {
-			prefix = []byte("/" + nid + "/")
-		} else {
-			prefix = []byte("/")
-		}
-		nDB.indexes[byNetwork].Root().WalkPrefix(prefix, func(path []byte, v *entry) bool {
-			if !v.deleting && v.node != nDB.config.NodeID {
-				tuple := strings.SplitN(string(path[1:]), "/", 3)
-				if len(tuple) == 3 {
-					entryNid, entryTname, key := tuple[0], tuple[1], tuple[2]
-					sink.Write(makeEvent(opCreate, entryTname, entryNid, key, v.value))
-				}
-			}
-			return false
-		})
-	} else {
-		prefix := []byte("/" + tname + "/")
-		if nid != "" {
-			prefix = append(prefix, []byte(nid+"/")...)
-		}
-		nDB.indexes[byTable].Root().WalkPrefix(prefix, func(path []byte, v *entry) bool {
-			if !v.deleting && v.node != nDB.config.NodeID {
-				tuple := strings.SplitN(string(path[1:]), "/", 3)
-				if len(tuple) == 3 {
-					entryTname, entryNid, key := tuple[0], tuple[1], tuple[2]
-					sink.Write(makeEvent(opCreate, entryTname, entryNid, key, v.value))
-				}
-			}
-			return false
-		})
 	}
 
 	nDB.broadcaster.Add(sink)
