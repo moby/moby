4e8bd050bf5d7f50808ca648d6dc8b7e19c1879d
Derek McGowan <derek@mcg.dev>
2025-06-27 14:28:08 -0700

    Move libcontainerd/remote to daemon/internal/libcontainerd/remote
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- libcontainerd/libcontainerd_linux.go
+++ libcontainerd/libcontainerd_linux.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	containerd "github.com/containerd/containerd/v2/client"
-	"github.com/docker/docker/libcontainerd/remote"
+	"github.com/docker/docker/daemon/internal/libcontainerd/remote"
 	libcontainerdtypes "github.com/docker/docker/libcontainerd/types"
 )
 
