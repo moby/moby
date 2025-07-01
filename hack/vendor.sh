484b7fba3066d79dc3af57d7f06bf3fbeaa06b9b
Derek McGowan <derek@mcg.dev>
2025-07-01 15:54:50 -0700

    Add options to vendor to add and remove replace
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -18,10 +18,24 @@ vendor() (
 		"${SCRIPTDIR}"/with-go-mod.sh go mod vendor -modfile vendor.mod
 )
 
+replace() (
+	set -x
+	"${SCRIPTDIR}"/with-go-mod.sh go mod edit -modfile vendor.mod -replace=github.com/moby/moby/api=./api -replace=github.com/moby/moby/client=./client
+	"${SCRIPTDIR}"/with-go-mod.sh go mod edit -modfile client/go.mod -replace=github.com/moby/moby/api=../api
+)
+
+dropreplace() (
+	set -x
+	"${SCRIPTDIR}"/with-go-mod.sh go mod edit -modfile vendor.mod -dropreplace=github.com/moby/moby/api -dropreplace=github.com/moby/moby/client
+	"${SCRIPTDIR}"/with-go-mod.sh go mod edit -modfile client/go.mod -dropreplace=github.com/moby/moby/api
+)
+
 help() {
 	printf "%s:\n" "$(basename "$0")"
 	echo "  - tidy: run go mod tidy"
 	echo "  - vendor: run go mod vendor"
+	echo "  - replace: run go mod edit replace for local modules"
+	echo "  - dropreplace: run go mod edit dropreplace for local modules"
 	echo "  - all: run tidy && vendor"
 	echo "  - help: show this help"
 }
@@ -29,6 +43,8 @@ help() {
 case "$1" in
 	tidy) tidy ;;
 	vendor) vendor ;;
+	replace) replace ;;
+	dropreplace) dropreplace ;;
 	""|all) tidy && vendor ;;
 	*) help ;;
 esac
