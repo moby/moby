a00bdc422b8ee0062172f1ff8a92be68ac0f9502
Derek McGowan <derek@mcg.dev>
2025-07-01 10:08:54 -0700

    Move libnetwork/cnmallocator to daemon/libnetwork/cnmallocator
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- daemon/cluster/noderunner.go
+++ daemon/cluster/noderunner.go
@@ -13,7 +13,7 @@ import (
 	"github.com/docker/docker/daemon/cluster/convert"
 	"github.com/docker/docker/daemon/cluster/executor/container"
 	lncluster "github.com/docker/docker/daemon/libnetwork/cluster"
-	"github.com/docker/docker/libnetwork/cnmallocator"
+	"github.com/docker/docker/daemon/libnetwork/cnmallocator"
 	swarmapi "github.com/moby/swarmkit/v2/api"
 	"github.com/moby/swarmkit/v2/manager/allocator/networkallocator"
 	swarmnode "github.com/moby/swarmkit/v2/node"
