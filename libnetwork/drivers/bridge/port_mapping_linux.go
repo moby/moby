004fca6fcf237387fd7c622b96533c134b7f4da3
Derek McGowan <derek@mcg.dev>
2025-07-01 10:21:24 -0700

    Move libnetwork/drivers/bridge/internal/rlkclient to daemon/libnetwork/drivers/bridge/internal/rlkclient
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- libnetwork/drivers/bridge/port_mapping_linux.go
+++ libnetwork/drivers/bridge/port_mapping_linux.go
@@ -16,11 +16,11 @@ import (
 
 	"github.com/containerd/log"
 	"github.com/docker/docker/daemon/libnetwork/drivers/bridge/internal/firewaller"
+	"github.com/docker/docker/daemon/libnetwork/drivers/bridge/internal/rlkclient"
 	"github.com/docker/docker/daemon/libnetwork/netutils"
 	"github.com/docker/docker/daemon/libnetwork/portallocator"
 	"github.com/docker/docker/daemon/libnetwork/portmapper"
 	"github.com/docker/docker/daemon/libnetwork/types"
-	"github.com/docker/docker/libnetwork/drivers/bridge/internal/rlkclient"
 	"github.com/ishidawataru/sctp"
 )
 
