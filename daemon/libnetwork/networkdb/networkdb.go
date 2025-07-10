b6a5fe9e717465f0e355ac062edb93df6919f0fc
Cory Snider <csnider@mirantis.com>
2025-07-10 17:24:16 -0400

    libn/networkdb: report prev value in update events
    When handling updates to existing entries, it is often necessary to know
    what the previous value was. NetworkDB knows the previous and new values
    when it broadcasts an update event for an entry. Include both values in
    the update event so the watchers do not have to do their own parallel
    bookkeeping.
    
    Unify the event types under WatchEvent as representing the operation kind
    in the type system has been inconvenient, not useful. The operation is
    now implied by the nilness of the Value and Prev event fields.
    
    Signed-off-by: Cory Snider <csnider@mirantis.com>

--- daemon/libnetwork/networkdb/networkdb.go
+++ daemon/libnetwork/networkdb/networkdb.go
@@ -537,7 +537,12 @@ func (nDB *NetworkDB) deleteNodeNetworkEntries(nid, node string) {
 
 			// Notify to the upper layer only entries not already marked for deletion
 			if !oldEntry.deleting {
-				nDB.broadcaster.Write(makeEvent(opDelete, tName, nwID, key, oldEntry.value))
+				nDB.broadcaster.Write(WatchEvent{
+					Table:     tName,
+					NetworkID: nwID,
+					Key:       key,
+					Prev:      oldEntry.value,
+				})
 			}
 			return false
 		})
@@ -557,7 +562,12 @@ func (nDB *NetworkDB) deleteNodeTableEntries(node string) {
 		nDB.deleteEntry(nwID, tName, key)
 
 		if !oldEntry.deleting {
-			nDB.broadcaster.Write(makeEvent(opDelete, tName, nwID, key, oldEntry.value))
+			nDB.broadcaster.Write(WatchEvent{
+				Table:     tName,
+				NetworkID: nwID,
+				Key:       key,
+				Prev:      oldEntry.value,
+			})
 		}
 		return false
 	})
@@ -684,7 +694,12 @@ func (nDB *NetworkDB) LeaveNetwork(nid string) error {
 			nDB.deleteEntry(nwID, tName, key)
 		}
 		if !oldEntry.deleting {
-			nDB.broadcaster.Write(makeEvent(opDelete, tName, nwID, key, oldEntry.value))
+			nDB.broadcaster.Write(WatchEvent{
+				Table:     tName,
+				NetworkID: nwID,
+				Key:       key,
+				Prev:      oldEntry.value,
+			})
 		}
 		return false
 	})
