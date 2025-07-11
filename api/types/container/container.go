3b9b04a4784baefbcbe3fbca0f28f6f2d36f791a
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2025-07-11 15:05:21 +0000

    Add Health attribute on the docker ps command
    Signed-off-by: Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>

--- api/types/container/container.go
+++ api/types/container/container.go
@@ -138,6 +138,7 @@ type Summary struct {
 		NetworkMode string            `json:",omitempty"`
 		Annotations map[string]string `json:",omitempty"`
 	}
+	Health          *HealthSummary `json:",omitempty"`
 	NetworkSettings *NetworkSettingsSummary
 	Mounts          []MountPoint
 }
