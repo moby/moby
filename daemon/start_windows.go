0fc14936545f060bf6d41ec7def2385de140e93b
Sebastiaan van Stijn <github@gone.nl>
2025-07-12 19:16:30 +0200

    pkg/system: remove EnableContainerdRuntime, ContainerdRuntimeSupported
    These functions were used internally to keep track of whether containerd
    was enabled as runtime on Windows; move it to libcontainerd.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/start_windows.go
+++ daemon/start_windows.go
@@ -4,11 +4,11 @@ import (
 	"github.com/Microsoft/hcsshim/cmd/containerd-shim-runhcs-v1/options"
 	"github.com/docker/docker/daemon/config"
 	"github.com/docker/docker/daemon/container"
-	"github.com/docker/docker/pkg/system"
+	"github.com/docker/docker/daemon/internal/libcontainerd"
 )
 
 func (daemon *Daemon) getLibcontainerdCreateOptions(*configStore, *container.Container) (string, interface{}, error) {
-	if system.ContainerdRuntimeSupported() {
+	if libcontainerd.ContainerdRuntimeEnabled {
 		opts := &options.Options{}
 		return config.WindowsV2RuntimeName, opts, nil
 	}
