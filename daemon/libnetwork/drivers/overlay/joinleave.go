c7b93702b99866c1beecd9f8b5042ccacaec0018
Cory Snider <csnider@mirantis.com>
2025-07-14 16:25:02 -0400

    libn/d/overlay: extract hashable address types
    The macAddr and ipmac types are generally useful within libnetwork. Move
    them to a dedicated package and overhaul the API to be more like that of
    the net/netip package.
    
    Update the overlay driver to utilize these types, adapting to the new
    API.
    
    Signed-off-by: Cory Snider <csnider@mirantis.com>

--- daemon/libnetwork/drivers/overlay/joinleave.go
+++ daemon/libnetwork/drivers/overlay/joinleave.go
@@ -6,12 +6,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net"
 	"net/netip"
 	"syscall"
 
 	"github.com/containerd/log"
 	"github.com/docker/docker/daemon/libnetwork/driverapi"
+	"github.com/docker/docker/daemon/libnetwork/internal/hashable"
 	"github.com/docker/docker/daemon/libnetwork/internal/netiputil"
 	"github.com/docker/docker/daemon/libnetwork/netlabel"
 	"github.com/docker/docker/daemon/libnetwork/ns"
@@ -104,7 +104,7 @@ func (d *driver) Join(ctx context.Context, nid, eid string, sboxKey string, jinf
 		return err
 	}
 
-	if err = nlh.LinkSetHardwareAddr(veth, ep.mac); err != nil {
+	if err = nlh.LinkSetHardwareAddr(veth, ep.mac.AsSlice()); err != nil {
 		return fmt.Errorf("could not set mac address (%v) to the container interface: %v", ep.mac, err)
 	}
 
@@ -187,7 +187,7 @@ func (d *driver) EventNotify(etype driverapi.EventType, nid, tableName, key stri
 		return
 	}
 
-	mac, err := net.ParseMAC(peer.EndpointMAC)
+	mac, err := hashable.ParseMAC(peer.EndpointMAC)
 	if err != nil {
 		log.G(context.TODO()).WithError(err).Errorf("Invalid mac %s received in event notify", peer.EndpointMAC)
 		return
