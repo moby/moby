fbb91c124863786d65103411ff7b5189138fc225
Olli Janatuinen <olli.janatuinen@gmail.com>
2025-07-16 08:02:42 -0700

    libnetwork: Use generic IPAM also in Windows
    Signed-off-by: Olli Janatuinen <olli.janatuinen@gmail.com>

--- daemon/libnetwork/ipams/drivers.go
+++ daemon/libnetwork/ipams/drivers.go
@@ -5,18 +5,14 @@ import (
 	"github.com/moby/moby/v2/daemon/libnetwork/ipams/defaultipam"
 	"github.com/moby/moby/v2/daemon/libnetwork/ipams/null"
 	remoteIpam "github.com/moby/moby/v2/daemon/libnetwork/ipams/remote"
-	"github.com/moby/moby/v2/daemon/libnetwork/ipams/windowsipam"
 	"github.com/moby/moby/v2/daemon/libnetwork/ipamutils"
 	"github.com/moby/moby/v2/pkg/plugingetter"
 )
 
-// Register registers all the builtin drivers (ie. default, windowsipam, null
+// Register registers all the builtin drivers (ie. default, null
 // and remote). If 'pg' is nil, the remote driver won't be registered.
 func Register(r ipamapi.Registerer, pg plugingetter.PluginGetter, lAddrPools, gAddrPools []*ipamutils.NetworkToSplit) error {
 	if err := defaultipam.Register(r, lAddrPools, gAddrPools); err != nil {
-		return err
-	}
-	if err := windowsipam.Register(r); err != nil {
 		return err
 	}
 	if err := null.Register(r); err != nil {
