7db5164f00faabfd7f3fc455e89a83e9b1aa10a7
Derek McGowan <derek@mcg.dev>
2025-07-17 15:34:09 -0700

    Move profiles to daemon/pkg/profiles
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- daemon/pkg/profiles/seccomp/generate.go
+++ daemon/pkg/profiles/seccomp/generate.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/docker/docker/profiles/seccomp"
+	"github.com/docker/docker/daemon/pkg/profiles/seccomp"
 )
 
 // saves the default seccomp profile as a json file so people can use it as a
