3e5a06179e3d934e3e193847b32f45bb44a63223
Sebastiaan van Stijn <github@gone.nl>
2025-07-20 14:53:25 +0200

    api/types: remove deprecated container-inspect types
    This was added in 1abc8f6158aa1f27b0270df772b498618f19abbb,
    which shipped in the latest v28.x releases, so we can remove these.
    
    This removes the `ContainerJSONBase`, `ContainerJSON` types.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/types/types_deprecated.go
+++ api/types/types_deprecated.go
@@ -4,25 +4,12 @@ import (
 	"context"
 
 	"github.com/docker/docker/api/types/common"
-	"github.com/docker/docker/api/types/container"
 )
 
 // IDResponse Response to an API call that returns just an Id.
 //
 // Deprecated: use either [container.CommitResponse] or [container.ExecCreateResponse]. It will be removed in the next release.
 type IDResponse = common.IDResponse
-
-// ContainerJSONBase contains response of Engine API GET "/containers/{name:.*}/json"
-// for API version 1.18 and older.
-//
-// Deprecated: use [container.InspectResponse] or [container.ContainerJSONBase]. It will be removed in the next release.
-type ContainerJSONBase = container.ContainerJSONBase
-
-// ContainerJSON is the response for the GET "/containers/{name:.*}/json"
-// endpoint.
-//
-// Deprecated: use [container.InspectResponse]. It will be removed in the next release.
-type ContainerJSON = container.InspectResponse
 
 // RequestPrivilegeFunc is a function interface that clients can supply to
 // retry operations after getting an authorization error.
