8347b054355d3296c2ecaacc8ebe4877abb98ed9
Sebastiaan van Stijn <github@gone.nl>
2025-07-20 14:50:56 +0200

    api/types: remove deprecated Container type
    This was added in 05b0e653dd1a9fdec903f9a110b23d29b353fe2e,
    which shipped in the latest v28.x releases, so we can remove these.
    
    This removes the Container type.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/types/types_deprecated.go
+++ api/types/types_deprecated.go
@@ -24,12 +24,6 @@ type ContainerJSONBase = container.ContainerJSONBase
 // Deprecated: use [container.InspectResponse]. It will be removed in the next release.
 type ContainerJSON = container.InspectResponse
 
-// Container contains response of Engine API:
-// GET "/containers/json"
-//
-// Deprecated: use [container.Summary].
-type Container = container.Summary
-
 // RequestPrivilegeFunc is a function interface that clients can supply to
 // retry operations after getting an authorization error.
 // This function returns the registry authentication header value in base64
