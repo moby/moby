a3920ae6a00f40534ecc94e84e834b23683fae41
Sebastiaan van Stijn <github@gone.nl>
2025-07-20 14:55:38 +0200

    api/types: remove deprecated IDResponse type
    This was added in b99b5422b02eff6f5865c56a5652472a14824704,
    which shipped in the latest v28.x releases, so we can remove these.
    
    This removes the `IDResponse` type.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- api/types/types_deprecated.go
+++ api/types/types_deprecated.go
@@ -2,14 +2,7 @@ package types
 
 import (
 	"context"
-
-	"github.com/docker/docker/api/types/common"
 )
-
-// IDResponse Response to an API call that returns just an Id.
-//
-// Deprecated: use either [container.CommitResponse] or [container.ExecCreateResponse]. It will be removed in the next release.
-type IDResponse = common.IDResponse
 
 // RequestPrivilegeFunc is a function interface that clients can supply to
 // retry operations after getting an authorization error.
