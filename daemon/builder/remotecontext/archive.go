9566272f301754a49d3c024c0c35d86f00ae20cc
Derek McGowan <derek@mcg.dev>
2025-07-24 12:12:23 -0700

    Move pkg/tarsum to daemon/builder/remotecontext/internal/tarsum
    Signed-off-by: Derek McGowan <derek@mcg.dev>

--- daemon/builder/remotecontext/archive.go
+++ daemon/builder/remotecontext/archive.go
@@ -6,9 +6,9 @@ import (
 	"path/filepath"
 
 	"github.com/docker/docker/daemon/builder"
+	"github.com/docker/docker/daemon/builder/remotecontext/internal/tarsum"
 	"github.com/docker/docker/pkg/longpath"
 	"github.com/docker/docker/pkg/system"
-	"github.com/docker/docker/pkg/tarsum"
 	"github.com/moby/go-archive/chrootarchive"
 	"github.com/moby/go-archive/compression"
 	"github.com/moby/sys/symlink"
