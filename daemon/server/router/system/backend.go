82c069c857236edda446a901484b215fd82f2337
Sebastiaan van Stijn <github@gone.nl>
2025-07-25 20:30:31 +0200

    api/types/system: move DiskUsage, DiskUsageOptions to api/types/backend
    These types were introduced in f07242f6d7091fe9000aa0bea0cc27b85baa945a,
    but while their description mentions it's the type used for the
    response, it actually isn't, and it's used by the backend, but
    ultimately marshaled to the "types.DiskUsage" struct;
    
    https://github.com/moby/moby/blob/7dc46c6e0c727a746ecfb8427c0c3cbd998059bc/daemon/server/router/system/system_routes.go#L254-L270
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/server/router/system/backend.go
+++ daemon/server/router/system/backend.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/moby/moby/api/types"
+	"github.com/moby/moby/api/types/backend"
 	"github.com/moby/moby/api/types/build"
 	"github.com/moby/moby/api/types/events"
 	"github.com/moby/moby/api/types/filters"
@@ -13,24 +14,12 @@ import (
 	"github.com/moby/moby/api/types/system"
 )
 
-// DiskUsageOptions holds parameters for system disk usage query.
-type DiskUsageOptions struct {
-	// Containers controls whether container disk usage should be computed.
-	Containers bool
-
-	// Images controls whether image disk usage should be computed.
-	Images bool
-
-	// Volumes controls whether volume disk usage should be computed.
-	Volumes bool
-}
-
 // Backend is the methods that need to be implemented to provide
 // system specific functionality.
 type Backend interface {
 	SystemInfo(context.Context) (*system.Info, error)
 	SystemVersion(context.Context) (types.Version, error)
-	SystemDiskUsage(ctx context.Context, opts DiskUsageOptions) (*system.DiskUsage, error)
+	SystemDiskUsage(ctx context.Context, opts backend.DiskUsageOptions) (*backend.DiskUsage, error)
 	SubscribeToEvents(since, until time.Time, ef filters.Args) ([]events.Message, chan interface{})
 	UnsubscribeFromEvents(chan interface{})
 	AuthenticateToRegistry(ctx context.Context, authConfig *registry.AuthConfig) (string, error)
