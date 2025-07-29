bae46854c5adb3ca6faa62c64576dc40bf46b997
Sebastiaan van Stijn <github@gone.nl>
2025-07-29 10:47:35 +0200

    pkg/fileutils: remove unused CopyFile utility
    It's not used, and has no external consumers; we don't have to carry it
    in the new module.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/fileutils/fileutils.go
+++ pkg/fileutils/fileutils.go
@@ -2,35 +2,9 @@ package fileutils
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 )
-
-// CopyFile copies from src to dst until either EOF is reached
-// on src or an error occurs. It verifies src exists and removes
-// the dst if it exists.
-func CopyFile(src, dst string) (int64, error) {
-	cleanSrc := filepath.Clean(src)
-	cleanDst := filepath.Clean(dst)
-	if cleanSrc == cleanDst {
-		return 0, nil
-	}
-	sf, err := os.Open(cleanSrc)
-	if err != nil {
-		return 0, err
-	}
-	defer sf.Close()
-	if err := os.Remove(cleanDst); err != nil && !os.IsNotExist(err) {
-		return 0, err
-	}
-	df, err := os.Create(cleanDst)
-	if err != nil {
-		return 0, err
-	}
-	defer df.Close()
-	return io.Copy(df, sf)
-}
 
 // ReadSymlinkedDirectory returns the target directory of a symlink.
 // The target of the symbolic link may not be a file.
