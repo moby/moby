aa9de914cafeb680d538671d68e32cd5d9c76fe4
Sebastiaan van Stijn <github@gone.nl>
2025-07-29 09:51:19 +0200

    pkg/system: remove unused ErrNotSupportedPlatform
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/system/errors.go
+++
@@ -1,6 +0,0 @@
-package system
-
-import "errors"
-
-// ErrNotSupportedPlatform means the platform is not supported.
-var ErrNotSupportedPlatform = errors.New("platform and architecture is not supported")
