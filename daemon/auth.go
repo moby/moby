f1c65c1dd6cec92dbbe02be6875959b245d2de2f
Sebastiaan van Stijn <github@gone.nl>
2025-07-29 10:26:12 +0200

    daemon/pkg/registry: Service.Auth: remove unused statusMessage return
    It was only used by the CLI, which now has its own fork.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/auth.go
+++ daemon/auth.go
@@ -9,6 +9,5 @@ import (
 
 // AuthenticateToRegistry checks the validity of credentials in authConfig
 func (daemon *Daemon) AuthenticateToRegistry(ctx context.Context, authConfig *registry.AuthConfig) (string, error) {
-	_, token, err := daemon.registryService.Auth(ctx, authConfig, dockerversion.DockerUserAgent(ctx))
-	return token, err
+	return daemon.registryService.Auth(ctx, authConfig, dockerversion.DockerUserAgent(ctx))
 }
