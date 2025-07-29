1313b8caff26584677a24404b3f931af2cc8ead1
Sebastiaan van Stijn <github@gone.nl>
2025-07-29 11:14:12 +0200

    pkg/fileutils: move ReadSymlinkedDirectory to daemon
    It has no external consumers, is written with specific behavior, making
    it not a good candidate to carry in the module.
    
    This moves it to the daemon as a non-exported `resolveSymlinkedDirectory`
    utility, so that it's only accessible where it's currently used.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/daemon_test.go
+++ daemon/daemon_test.go
@@ -3,8 +3,10 @@ package daemon
 import (
 	"net/netip"
 	"os"
+	"path"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 
 	cerrdefs "github.com/containerd/errdefs"
@@ -339,5 +341,102 @@ func TestDeriveULABaseNetwork(t *testing.T) {
 			assert.Equal(t, nw.Base, tc.expPrefix)
 			assert.Equal(t, nw.Size, 64)
 		})
+	}
+}
+
+// Reading a symlink to a directory must return the directory
+func TestResolveSymlinkedDirectoryExistingDirectory(t *testing.T) {
+	// TODO Windows: Port this test
+	if runtime.GOOS == "windows" {
+		t.Skip("Needs porting to Windows")
+	}
+
+	// On macOS, tmp itself is symlinked, so resolve this one upfront;
+	// see https://github.com/golang/go/issues/56259
+	tmpDir, err := filepath.EvalSymlinks(t.TempDir())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	srcPath := filepath.Join(tmpDir, "/testReadSymlinkToExistingDirectory")
+	dstPath := filepath.Join(tmpDir, "/dirLinkTest")
+	if err = os.Mkdir(srcPath, 0o777); err != nil {
+		t.Errorf("failed to create directory: %s", err)
+	}
+
+	if err = os.Symlink(srcPath, dstPath); err != nil {
+		t.Errorf("failed to create symlink: %s", err)
+	}
+
+	var symlinkedPath string
+	if symlinkedPath, err = resolveSymlinkedDirectory(dstPath); err != nil {
+		t.Fatalf("failed to read symlink to directory: %s", err)
+	}
+
+	if symlinkedPath != srcPath {
+		t.Fatalf("symlink returned unexpected directory: %s", symlinkedPath)
+	}
+
+	if err = os.Remove(srcPath); err != nil {
+		t.Errorf("failed to remove temporary directory: %s", err)
+	}
+
+	if err = os.Remove(dstPath); err != nil {
+		t.Errorf("failed to remove symlink: %s", err)
+	}
+}
+
+// Reading a non-existing symlink must fail
+func TestResolveSymlinkedDirectoryNonExistingSymlink(t *testing.T) {
+	tmpDir := t.TempDir()
+	symLinkedPath, err := resolveSymlinkedDirectory(path.Join(tmpDir, "/Non/ExistingPath"))
+	if err == nil {
+		t.Errorf("error expected for non-existing symlink")
+	}
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Errorf("Expected an os.ErrNotExist, got: %v", err)
+	}
+	if symLinkedPath != "" {
+		t.Fatalf("expected empty path, but '%s' was returned", symLinkedPath)
+	}
+}
+
+// Reading a symlink to a file must fail
+func TestResolveSymlinkedDirectoryToFile(t *testing.T) {
+	// TODO Windows: Port this test
+	if runtime.GOOS == "windows" {
+		t.Skip("Needs porting to Windows")
+	}
+	var err error
+	var file *os.File
+
+	// #nosec G303
+	if file, err = os.Create("/tmp/testSymlinkToFile"); err != nil {
+		t.Fatalf("failed to create file: %s", err)
+	}
+
+	_ = file.Close()
+
+	if err = os.Symlink("/tmp/testSymlinkToFile", "/tmp/fileLinkTest"); err != nil {
+		t.Errorf("failed to create symlink: %s", err)
+	}
+
+	symlinkedPath, err := resolveSymlinkedDirectory("/tmp/fileLinkTest")
+	if err == nil {
+		t.Errorf("resolveSymlinkedDirectory on a symlink to a file should've failed")
+	} else if !strings.HasPrefix(err.Error(), "canonical path points to a file") {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if symlinkedPath != "" {
+		t.Errorf("path should've been empty: %s", symlinkedPath)
+	}
+
+	if err = os.Remove("/tmp/testSymlinkToFile"); err != nil {
+		t.Errorf("failed to remove file: %s", err)
+	}
+
+	if err = os.Remove("/tmp/fileLinkTest"); err != nil {
+		t.Errorf("failed to remove symlink: %s", err)
 	}
 }
