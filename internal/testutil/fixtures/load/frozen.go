57db2ac9e55f50b6863a301beec42eeb4b22b4a6
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-08-07 19:49:41 +0200

    test: don't lazy pull and only check frozen images dir
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- internal/testutil/fixtures/load/frozen.go
+++ internal/testutil/fixtures/load/frozen.go
@@ -1,31 +1,21 @@
 package load
 
 import (
-	"bufio"
 	"context"
 	"io/fs"
 	"os"
 	"path/filepath"
-	"strings"
-	"sync"
 
 	"github.com/moby/moby/client"
 	"github.com/moby/moby/client/pkg/jsonmessage"
 	"github.com/moby/term"
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/codes"
-	"go.opentelemetry.io/otel/trace"
 )
 
 const frozenImgDir = "/docker-frozen-images"
 
-// FrozenImagesLinux loads the frozen image set for the integration suite
-// If the images are not available locally it will download them
-// TODO: This loads whatever is in the frozen image dir, regardless of what
-// images were passed in. If the images need to be downloaded, then it will respect
-// the passed in images
+// FrozenImagesLinux loads the frozen image set for the integration suite.
 func FrozenImagesLinux(ctx context.Context, apiClient client.APIClient, images ...string) error {
 	ctx, span := otel.Tracer("").Start(ctx, "LoadFrozenImages")
 	defer span.End()
@@ -34,10 +24,7 @@ func FrozenImagesLinux(ctx context.Context, apiClient client.APIClient, images .
 	for _, img := range images {
 		if !imageExists(ctx, apiClient, img) {
 			srcName := img
-			// hello-world:latest gets re-tagged as hello-world:frozen
-			// there are some tests that use hello-world:latest specifically so it pulls
-			// the image and hello-world:frozen is used for when we just want a super
-			// small image
+			// hello-world:latest gets re-tagged as hello-world:frozen.
 			if img == "hello-world:frozen" {
 				srcName = "hello-world:latest"
 			}
@@ -48,23 +35,14 @@ func FrozenImagesLinux(ctx context.Context, apiClient client.APIClient, images .
 		}
 	}
 	if len(loadImages) == 0 {
-		// everything is loaded, we're done
 		return nil
 	}
 
-	fi, err := os.Stat(frozenImgDir)
-	if err != nil || !fi.IsDir() {
-		srcImages := make([]string, 0, len(loadImages))
-		for _, img := range loadImages {
-			srcImages = append(srcImages, img.srcName)
-		}
-		if err := pullImages(ctx, apiClient, srcImages); err != nil {
-			return errors.Wrap(err, "error pulling image list")
-		}
-	} else {
-		if err := loadFrozenImages(ctx, apiClient); err != nil {
-			return err
-		}
+	if fi, err := os.Stat(frozenImgDir); err != nil || !fi.IsDir() {
+		return errors.Wrapf(err, "error checking frozen images directory %s", frozenImgDir)
+	}
+	if err := loadFrozenImages(ctx, apiClient); err != nil {
+		return err
 	}
 
 	for _, img := range loadImages {
@@ -101,7 +79,7 @@ func loadFrozenImages(ctx context.Context, apiClient client.APIClient) error {
 			return err
 		}
 		err = func(tarfile fs.FileInfo) error {
-			reader, err := os.OpenFile(filepath.Join(frozenImgDir, tarfile.Name()), os.O_RDONLY, 0644)
+			reader, err := os.OpenFile(filepath.Join(frozenImgDir, tarfile.Name()), os.O_RDONLY, 0o644)
 			if err != nil {
 				return err
 			}
@@ -121,106 +99,4 @@ func loadFrozenImages(ctx context.Context, apiClient client.APIClient) error {
 		}
 	}
 	return nil
-}
-
-func pullImages(ctx context.Context, client client.APIClient, images []string) error {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return errors.Wrap(err, "error getting path to dockerfile")
-	}
-	dockerfile := os.Getenv("DOCKERFILE")
-	if dockerfile == "" {
-		dockerfile = "Dockerfile"
-	}
-	dockerfilePath := filepath.Join(filepath.Dir(filepath.Clean(cwd)), dockerfile)
-	pullRefs, err := readFrozenImageList(ctx, dockerfilePath, images)
-	if err != nil {
-		return errors.Wrap(err, "error reading frozen image list")
-	}
-
-	var wg sync.WaitGroup
-	chErr := make(chan error, len(images))
-	for tag, ref := range pullRefs {
-		wg.Add(1)
-		go func(tag, ref string) {
-			defer wg.Done()
-			if err := pullTagAndRemove(ctx, client, ref, tag); err != nil {
-				chErr <- err
-				return
-			}
-		}(tag, ref)
-	}
-	wg.Wait()
-	close(chErr)
-	return <-chErr
-}
-
-func pullTagAndRemove(ctx context.Context, apiClient client.APIClient, ref string, tag string) (retErr error) {
-	ctx, span := otel.Tracer("").Start(ctx, "pull image: "+ref+" with tag: "+tag)
-	defer func() {
-		if retErr != nil {
-			// An error here is a real error for the span, so set the span status
-			span.SetStatus(codes.Error, retErr.Error())
-		}
-		span.End()
-	}()
-
-	resp, err := apiClient.ImagePull(ctx, ref, client.ImagePullOptions{})
-	if err != nil {
-		return errors.Wrapf(err, "failed to pull %s", ref)
-	}
-	defer resp.Close()
-	fd, isTerminal := term.GetFdInfo(os.Stdout)
-	if err := jsonmessage.DisplayJSONMessagesStream(resp, os.Stdout, fd, isTerminal, nil); err != nil {
-		return err
-	}
-
-	if _, err := apiClient.ImageTag(ctx, client.ImageTagOptions{Source: ref, Target: tag}); err != nil {
-		return errors.Wrapf(err, "failed to tag %s as %s", ref, tag)
-	}
-	_, err = apiClient.ImageRemove(ctx, ref, client.ImageRemoveOptions{})
-	return errors.Wrapf(err, "failed to remove %s", ref)
-}
-
-func readFrozenImageList(ctx context.Context, dockerfilePath string, images []string) (map[string]string, error) {
-	f, err := os.Open(dockerfilePath)
-	if err != nil {
-		return nil, errors.Wrap(err, "error reading dockerfile")
-	}
-	defer f.Close()
-	ls := make(map[string]string)
-
-	span := trace.SpanFromContext(ctx)
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := strings.Fields(scanner.Text())
-		if len(line) < 3 {
-			continue
-		}
-		if line[0] != "RUN" || line[1] != "./contrib/download-frozen-image-v2.sh" {
-			continue
-		}
-
-		for scanner.Scan() {
-			img := strings.TrimSpace(scanner.Text())
-			img = strings.TrimSuffix(img, "\\")
-			img = strings.TrimSpace(img)
-			split := strings.Split(img, "@")
-			if len(split) < 2 {
-				break
-			}
-
-			for _, i := range images {
-				if split[0] == i {
-					ls[i] = img
-					if span.IsRecording() {
-						span.AddEvent("found frozen image", trace.WithAttributes(attribute.String("image", i)))
-					}
-					break
-				}
-			}
-		}
-	}
-	return ls, nil
 }
