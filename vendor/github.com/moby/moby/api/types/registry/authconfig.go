a82bf2f9298382f2cef6b635c6779ae2e4818a85
Austin Vazquez <austin.vazquez@docker.com>
2025-08-18 20:18:22 -0500

    api/types/registry: remove deprecated decode auth config functions
    Signed-off-by: Austin Vazquez <austin.vazquez@docker.com>

--- vendor/github.com/moby/moby/api/types/registry/authconfig.go
+++ vendor/github.com/moby/moby/api/types/registry/authconfig.go
@@ -1,13 +1,9 @@
 package registry
 
 import (
-	"bytes"
 	"context"
 	"encoding/base64"
 	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
 )
 
 // AuthHeader is the name of the header used to send encoded registry
@@ -18,8 +14,7 @@ const AuthHeader = "X-Registry-Auth"
 // to retry operations after getting an authorization error.
 //
 // The function must return the [AuthHeader] value ([AuthConfig]), encoded
-// in base64url format ([RFC4648, section 5]), which can be decoded by
-// [DecodeAuthConfig].
+// in base64url format ([RFC4648, section 5]).
 //
 // It must return an error if the privilege request fails.
 //
@@ -62,65 +57,6 @@ func EncodeAuthConfig(authConfig AuthConfig) (string, error) {
 		return "", errInvalidParameter{err}
 	}
 	return base64.URLEncoding.EncodeToString(buf), nil
-}
-
-// DecodeAuthConfig decodes base64url encoded ([RFC4648, section 5]) JSON
-// authentication information as sent through the X-Registry-Auth header.
-//
-// This function always returns an [AuthConfig], even if an error occurs. It is up
-// to the caller to decide if authentication is required, and if the error can
-// be ignored.
-//
-// Deprecated: this function is deprecated and will be removed in the next release.
-//
-// [RFC4648, section 5]: https://tools.ietf.org/html/rfc4648#section-5
-func DecodeAuthConfig(authEncoded string) (*AuthConfig, error) {
-	if authEncoded == "" {
-		return &AuthConfig{}, nil
-	}
-
-	decoded, err := base64.URLEncoding.DecodeString(authEncoded)
-	if err != nil {
-		var e base64.CorruptInputError
-		if errors.As(err, &e) {
-			return &AuthConfig{}, invalid(errors.New("must be a valid base64url-encoded string"))
-		}
-		return &AuthConfig{}, invalid(err)
-	}
-
-	if bytes.Equal(decoded, []byte("{}")) {
-		return &AuthConfig{}, nil
-	}
-
-	return decodeAuthConfigFromReader(bytes.NewReader(decoded))
-}
-
-// DecodeAuthConfigBody decodes authentication information as sent as JSON in the
-// body of a request. This function is to provide backward compatibility with old
-// clients and API versions. Current clients and API versions expect authentication
-// to be provided through the X-Registry-Auth header.
-//
-// Like [DecodeAuthConfig], this function always returns an [AuthConfig], even if an
-// error occurs. It is up to the caller to decide if authentication is required,
-// and if the error can be ignored.
-//
-// Deprecated: this function is no longer used and will be removed in the next release.
-func DecodeAuthConfigBody(rdr io.ReadCloser) (*AuthConfig, error) {
-	return decodeAuthConfigFromReader(rdr)
-}
-
-func decodeAuthConfigFromReader(rdr io.Reader) (*AuthConfig, error) {
-	authConfig := &AuthConfig{}
-	if err := json.NewDecoder(rdr).Decode(authConfig); err != nil {
-		// always return an (empty) AuthConfig to increase compatibility with
-		// the existing API.
-		return &AuthConfig{}, invalid(fmt.Errorf("invalid JSON: %w", err))
-	}
-	return authConfig, nil
-}
-
-func invalid(err error) error {
-	return errInvalidParameter{fmt.Errorf("invalid X-Registry-Auth header: %w", err)}
 }
 
 type errInvalidParameter struct{ error }
