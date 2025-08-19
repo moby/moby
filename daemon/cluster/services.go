7945aed4d524792f235749a4fe8ef8e34a2533ee
Sebastiaan van Stijn <github@gone.nl>
2025-08-19 09:53:05 +0200

    daemon/cluster: use authconfig package for decoding
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/cluster/services.go
+++ daemon/cluster/services.go
@@ -2,13 +2,10 @@ package cluster
 
 import (
 	"context"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/containerd/log"
@@ -18,6 +15,7 @@ import (
 	"github.com/moby/moby/api/types/registry"
 	"github.com/moby/moby/api/types/swarm"
 	"github.com/moby/moby/v2/daemon/cluster/convert"
+	"github.com/moby/moby/v2/daemon/internal/authconfig"
 	"github.com/moby/moby/v2/daemon/internal/timestamp"
 	"github.com/moby/moby/v2/daemon/server/backend"
 	"github.com/moby/moby/v2/errdefs"
@@ -232,9 +230,9 @@ func (c *Cluster) CreateService(s swarm.ServiceSpec, encodedAuth string, queryRe
 			// retrieve auth config from encoded auth
 			authConfig := &registry.AuthConfig{}
 			if encodedAuth != "" {
-				authReader := strings.NewReader(encodedAuth)
-				dec := json.NewDecoder(base64.NewDecoder(base64.URLEncoding, authReader))
-				if err := dec.Decode(authConfig); err != nil {
+				var err error
+				authConfig, err = authconfig.DecodeAuthConfig(encodedAuth)
+				if err != nil {
 					log.G(ctx).Warnf("invalid authconfig: %v", err)
 				}
 			}
@@ -350,7 +348,9 @@ func (c *Cluster) UpdateService(serviceIDOrName string, version uint64, spec swa
 			// retrieve auth config from encoded auth
 			authConfig := &registry.AuthConfig{}
 			if encodedAuth != "" {
-				if err := json.NewDecoder(base64.NewDecoder(base64.URLEncoding, strings.NewReader(encodedAuth))).Decode(authConfig); err != nil {
+				var err error
+				authConfig, err = authconfig.DecodeAuthConfig(encodedAuth)
+				if err != nil {
 					log.G(ctx).Warnf("invalid authconfig: %v", err)
 				}
 			}
