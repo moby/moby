docker/docker

image: use oci.Platform instead of custom fields
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

builder/dockerfile/imagecontext.go
@@ -87,9 +87,11 @@ func (m *imageSources) Add(im *imageMount, platform *specs.Platform) {
 		}
 
 		im.image = &dockerimage.Image{V1Image: dockerimage.V1Image{
-			OS:           os,
-			Architecture: platform.Architecture,
-			Variant:      platform.Variant,
+			Platform: specs.Platform{
+				OS:           os,
+				Architecture: platform.Architecture,
+				Variant:      platform.Variant,
+			},
 		}}
 	default:
 		m.byImageID[im.image.ImageID()] = im

moby/moby

daemon/builder/dockerfile/imagecontext.go
@@ -87,9 +87,11 @@ func (m *imageSources) Add(im *imageMount, platform *ocispec.Platform) {
 		}
 
 		im.image = &dockerimage.Image{V1Image: dockerimage.V1Image{
-			OS:           os,
-			Architecture: platform.Architecture,
-			Variant:      platform.Variant,
+			Platform: ocispec.Platform{
+				OS:           os,
+				Architecture: platform.Architecture,
+				Variant:      platform.Variant,
+			},
 		}}
 	default:
 		m.byImageID[im.image.ImageID()] = im