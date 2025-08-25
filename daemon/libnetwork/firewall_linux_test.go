6fed89620b7dec03db34c1ae752192bf848b930e
Albin Kerouanton <albinker@gmail.com>
2025-08-25 15:05:05 +0200

    fixup! d/libnet: TestUserChain: fix error matching for nonexistent chains
    Signed-off-by: Albin Kerouanton <albinker@gmail.com>
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/libnetwork/firewall_linux_test.go
+++ daemon/libnetwork/firewall_linux_test.go
@@ -33,7 +33,9 @@ func TestUserChain(t *testing.T) {
 	res := icmd.RunCommand("iptables", "--version")
 	assert.NilError(t, res.Error)
 	noChainErr := "No chain/target/match by that name"
-	if strings.Contains(res.Combined(), "nf_tables") && versionLt(t, res.Combined(), 1, 8, 11) {
+	iptVerLt := versionLt(t, res.Combined(), 1, 8, 11)
+	t.Logf("iptables version < v1.8.11: %t", iptVerLt)
+	if strings.Contains(res.Combined(), "nf_tables") && iptVerLt {
 		// Prior to v1.8.11, iptables-nft "-S <chain>" reports the following for a non-existent chain:
 		//
 		//   ip6tables v1.8.9 (nf_tables): chain `<chain>' in table `filter' is incompatible, use 'nft' tool.
@@ -150,22 +152,17 @@ func resetIptables(t *testing.T) {
 // versionLt returns true if the iptables version returned by `iptables --version`
 // is less than the `<major>.<minor>.<patch>` version passed in as argument.
 func versionLt(t *testing.T, ver string, major, minor, patch int) bool {
-	match := regexp.MustCompile(`iptables v([0-9]+)\.([0-9]+)\.([0-9]+)`).FindStringSubmatch(ver)
-	assert.Assert(t, len(match) != 4, "could not determine iptables version from %q", ver)
+	t.Helper()
 
-	parsedMajor, err := strconv.Atoi(match[1])
-	assert.NilError(t, err)
-	if parsedMajor >= major {
-		return false
-	}
+	matches := regexp.MustCompile(`iptables v([0-9]+)\.([0-9]+)\.([0-9]+)`).FindStringSubmatch(ver)
+	assert.Assert(t, len(matches) == 4, "could not determine iptables version from %q", ver)
 
-	parsedMinor, err := strconv.Atoi(match[2])
+	parsedMajor, err := strconv.Atoi(matches[1])
 	assert.NilError(t, err)
-	if parsedMinor >= minor {
-		return false
-	}
+	parsedMinor, err := strconv.Atoi(matches[2])
+	assert.NilError(t, err)
+	parsedPatch, err := strconv.Atoi(matches[3])
+	assert.NilError(t, err)
 
-	parsedPatch, err := strconv.Atoi(match[3])
-	assert.NilError(t, err)
-	return parsedPatch < patch
+	return parsedMajor <= major && parsedMinor <= minor && parsedPatch < patch
 }
