c9fdad255249ffda4805bc486967e3b048094d7f
Austin Vazquez <austin.vazquez@docker.com>
2025-08-27 11:26:06 -0500

    daemon: marshal legacy `registry.ServiceConfig` extra fields for compatability
    This change moves the logic that is used to marshal the legacy extra fields for `registry.ServiceConfig` type to the daemon backend.
    
    Signed-off-by: Austin Vazquez <austin.vazquez@docker.com>

--- daemon/server/router/system/info_response.go
+++ daemon/server/router/system/info_response.go
@@ -24,12 +24,21 @@ func (ir *infoResponse) MarshalJSON() ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	if len(ir.extraFields) == 0 {
+	if len(ir.extraFields) == 0 && (ir.Info == nil || ir.Info.RegistryConfig == nil || len(ir.Info.RegistryConfig.ExtraFields) == 0) {
 		return base, nil
 	}
 	var merged map[string]any
 	_ = json.Unmarshal(base, &merged)
 
+	// Merge top-level extraFields
 	maps.Copy(merged, ir.extraFields)
+
+	// Merge RegistryConfig.ExtraFields if present
+	if ir.Info != nil && ir.Info.RegistryConfig != nil && len(ir.Info.RegistryConfig.ExtraFields) > 0 {
+		if rc, ok := merged["RegistryConfig"].(map[string]any); ok {
+			maps.Copy(rc, ir.Info.RegistryConfig.ExtraFields)
+		}
+	}
+
 	return json.Marshal(merged)
 }
