d70382e4424e19cf65d9837a6d8ed8fcb9a5f566
Paweł Gronowski <pawel.gronowski@docker.com>
2025-09-01 18:23:46 +0200

    integration/internal: Print Buildkit logs
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>
    (cherry picked from commit ce338dec81b34f7edc51890c0e2c64ead4e92c74)
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- integration/internal/build/build.go
+++ integration/internal/build/build.go
@@ -1,16 +1,19 @@
 package build
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"io"
 	"testing"
 
+	"github.com/containerd/containerd/v2/pkg/protobuf/proto"
 	"github.com/docker/docker/api/types/build"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/testutil/fakecontext"
+	controlapi "github.com/moby/buildkit/api/services/control"
 	"gotest.tools/v3/assert"
 )
 
@@ -31,6 +34,7 @@ func Do(ctx context.Context, t *testing.T, client client.APIClient, buildCtx *fa
 // GetImageIDFromBody reads the image ID from the build response body.
 func GetImageIDFromBody(t *testing.T, body io.Reader) string {
 	var id string
+	buf := bytes.NewBuffer(nil)
 	dec := json.NewDecoder(body)
 	for {
 		var jm jsonmessage.JSONMessage
@@ -39,6 +43,19 @@ func GetImageIDFromBody(t *testing.T, body io.Reader) string {
 			break
 		}
 		assert.NilError(t, err)
+
+		if handled := processBuildkitAux(t, &jm, &id); handled {
+			continue
+		}
+
+		buf.Reset()
+		jm.Display(buf, false)
+		if buf.Len() == 0 {
+			continue
+		}
+
+		t.Log(buf.String())
+
 		if jm.Aux == nil {
 			continue
 		}
@@ -49,11 +66,49 @@ func GetImageIDFromBody(t *testing.T, body io.Reader) string {
 				continue
 			}
 			id = br.ID
-			break
+			continue
 		}
+
+		t.Log("Raw Aux", string(*jm.Aux))
 	}
 	_, _ = io.Copy(io.Discard, body)
 
 	assert.Assert(t, id != "", "could not read image ID from build output")
 	return id
+}
+
+func processBuildkitAux(t *testing.T, jm *jsonmessage.JSONMessage, id *string) bool {
+	if jm.ID == "moby.buildkit.trace" {
+		var dt []byte
+		if err := json.Unmarshal(*jm.Aux, &dt); err != nil {
+			t.Log("Error unmarshalling buildkit trace", err)
+			return true
+		}
+		var sr controlapi.StatusResponse
+		if err := proto.Unmarshal(dt, &sr); err != nil {
+			t.Log("Error unmarshalling buildkit trace proto", err)
+			return true
+		}
+		for _, vtx := range sr.GetVertexes() {
+			t.Log(vtx.String())
+		}
+		for _, vtx := range sr.GetStatuses() {
+			t.Log(vtx.String())
+		}
+		for _, vtx := range sr.GetLogs() {
+			t.Log(vtx.String())
+		}
+		for _, vtx := range sr.GetWarnings() {
+			t.Log(vtx.String())
+		}
+		return true
+	}
+	if jm.ID == "moby.image.id" {
+		var br build.Result
+		if err := json.Unmarshal(*jm.Aux, &br); err == nil {
+			*id = br.ID
+			return true
+		}
+	}
+	return false
 }
