18f2e61d086c62944344e35e4360cba9da63f47d
Albin Kerouanton <albinker@gmail.com>
2025-09-03 12:05:10 +0200

    libnet/config: rm config_freebsd.go
    Signed-off-by: Albin Kerouanton <albinker@gmail.com>

--- daemon/libnetwork/config/config_freebsd.go
+++
@@ -1,8 +0,0 @@
-package config
-
-// FIXME(thaJeztah): ExecRoot is only used for Controller.startExternalKeyListener(), but "libnetwork-setkey" is only implemented on Linux.
-func optionExecRoot(execRoot string) Option {
-	return func(c *Config) {
-		c.ExecRoot = execRoot
-	}
-}
