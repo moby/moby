fc1ff44bc2c2ffa4070efbde76fe5daa1333c98a
Albin Kerouanton <albinker@gmail.com>
2025-09-10 16:31:47 +0200

    api/t/container: drop DefaultNetworkSettings
    All the fields in DefaultNetworkSettings were deprecated in Engine v1.9
    and scheduled for removal in v17.12. However, they're still return in
    all supported API versions.
    
    As we still need to keep backward compatibility for older API versions,
    this change leverages the newly added 'compat' package to return these
    fields despite being removed from the Go struct.
    
    Signed-off-by: Albin Kerouanton <albinker@gmail.com>

--- api/types/container/network_settings.go
+++ api/types/container/network_settings.go
@@ -7,7 +7,6 @@ import (
 // NetworkSettings exposes the network settings in the api
 type NetworkSettings struct {
 	NetworkSettingsBase
-	DefaultNetworkSettings
 	Networks map[string]*network.EndpointSettings
 }
 
@@ -35,46 +34,6 @@ type NetworkSettingsBase struct {
 	LinkLocalIPv6PrefixLen int
 	SecondaryIPAddresses   []network.Address // Deprecated: This field is never set and will be removed in a future release.
 	SecondaryIPv6Addresses []network.Address // Deprecated: This field is never set and will be removed in a future release.
-}
-
-// DefaultNetworkSettings holds the networking state for the default bridge, if the container is connected to that
-// network.
-//
-// Deprecated: this struct is deprecated since Docker v1.11 and will be removed in v29. You should look for the default
-// network in NetworkSettings.Networks instead.
-type DefaultNetworkSettings struct {
-	// EndpointID uniquely represents a service endpoint in a Sandbox
-	//
-	// Deprecated: This field will be removed in v29. You should look for the default network in NetworkSettings.Networks instead.
-	EndpointID string
-	// Gateway holds the gateway address for the network
-	//
-	// Deprecated: This field will be removed in v29. You should look for the default network in NetworkSettings.Networks instead.
-	Gateway string
-	// GlobalIPv6Address holds network's global IPv6 address
-	//
-	// Deprecated: This field will be removed in v29. You should look for the default network in NetworkSettings.Networks instead.
-	GlobalIPv6Address string
-	// GlobalIPv6PrefixLen represents mask length of network's global IPv6 address
-	//
-	// Deprecated: This field will be removed in v29. You should look for the default network in NetworkSettings.Networks instead.
-	GlobalIPv6PrefixLen int
-	// IPAddress holds the IPv4 address for the network
-	//
-	// Deprecated: This field will be removed in v29. You should look for the default network in NetworkSettings.Networks instead.
-	IPAddress string
-	// IPPrefixLen represents mask length of network's IPv4 address
-	//
-	// Deprecated: This field will be removed in v29. You should look for the default network in NetworkSettings.Networks instead.
-	IPPrefixLen int
-	// IPv6Gateway holds gateway address specific for IPv6
-	//
-	// Deprecated: This field will be removed in v29. You should look for the default network in NetworkSettings.Networks instead.
-	IPv6Gateway string
-	// MacAddress holds the MAC address for the network
-	//
-	// Deprecated: This field will be removed in v29. You should look for the default network in NetworkSettings.Networks instead.
-	MacAddress string
 }
 
 // NetworkSettingsSummary provides a summary of container's networks
