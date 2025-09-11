88abb2a376d485887218d8cd0ea1993cb5154bee
Paweł Gronowski <pawel.gronowski@docker.com>
2025-09-11 15:02:15 +0200

    client: Change `ImageHistory` functional opts
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- vendor/github.com/moby/moby/client/image_history_opts.go
+++ vendor/github.com/moby/moby/client/image_history_opts.go
@@ -1,14 +1,18 @@
 package client
 
-import ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+import (
+	"context"
 
-// ImageHistoryOption is a type representing functional options for the image history operation.
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+)
+
 type ImageHistoryOption interface {
-	Apply(*imageHistoryOpts) error
+	applyImageHistoryOption(ctx context.Context, opts *imageHistoryOpts) error
 }
+
 type imageHistoryOptionFunc func(opt *imageHistoryOpts) error
 
-func (f imageHistoryOptionFunc) Apply(o *imageHistoryOpts) error {
+func (f imageHistoryOptionFunc) applyImageHistoryOption(ctx context.Context, o *imageHistoryOpts) error {
 	return f(o)
 }
 
@@ -19,4 +23,16 @@ type imageHistoryOpts struct {
 type imageHistoryOptions struct {
 	// Platform from the manifest list to use for history.
 	Platform *ocispec.Platform
+}
+
+// WithPlatform selects the specific platform of a multi-platform image.  This
+// effectively causes the operation to work on a single-platform image manifest
+// dereferenced from the original OCI index using the provided platform.
+//
+// Minimum API version: 1.48
+func WithHistoryPlatform(platform ocispec.Platform) ImageHistoryOption {
+	return imageHistoryOptionFunc(func(opts *imageHistoryOpts) error {
+		opts.apiOptions.Platform = &platform
+		return nil
+	})
 }
