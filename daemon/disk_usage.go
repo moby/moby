9dd2e09f331c545cf532be31162a5a55b958b71a
Sebastiaan van Stijn <github@gone.nl>
2025-10-14 12:16:07 +0200

    [DNM] DiskUsage is not wired up?
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/disk_usage.go
+++ daemon/disk_usage.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/moby/moby/api/types/container"
 	"github.com/moby/moby/api/types/image"
 	"github.com/moby/moby/v2/daemon/internal/filters"
 	"github.com/moby/moby/v2/daemon/server/backend"
@@ -32,19 +31,7 @@ func (daemon *Daemon) containerDiskUsage(ctx context.Context) (*backend.Containe
 			c.ImageManifestDescriptor = nil
 		}
 
-		isActive := func(ctr *container.Summary) bool {
-			return ctr.State == container.StateRunning ||
-				ctr.State == container.StatePaused ||
-				ctr.State == container.StateRestarting
-		}
-
 		du := &backend.ContainerDiskUsage{Items: containers}
-		for _, ctr := range du.Items {
-			du.TotalSize += ctr.SizeRw
-			if !isActive(ctr) {
-				du.Reclaimable += ctr.SizeRw
-			}
-		}
 		return du, nil
 	})
 	return res, err
@@ -78,14 +65,6 @@ func (daemon *Daemon) localVolumesSize(ctx context.Context) (*backend.VolumeDisk
 		}
 
 		du := &backend.VolumeDiskUsage{Items: volumes}
-		for _, v := range du.Items {
-			if v.UsageData.Size != -1 {
-				if v.UsageData.RefCount == 0 {
-					du.Reclaimable += v.UsageData.Size
-				}
-				du.TotalSize += v.UsageData.Size
-			}
-		}
 		return du, nil
 	})
 	return volumes, err
@@ -140,20 +119,9 @@ func (daemon *Daemon) SystemDiskUsage(ctx context.Context, opts backend.DiskUsag
 	}
 
 	if opts.Images {
-		reclaimable := layersSize
-		for _, i := range images {
-			if i.Containers != 0 {
-				if i.Size == -1 || i.SharedSize == -1 {
-					continue
-				}
-				reclaimable -= i.Size - i.SharedSize
-			}
-		}
-
 		du.Images = &backend.ImageDiskUsage{
-			TotalSize:   layersSize,
-			Reclaimable: reclaimable,
-			Items:       images,
+			TotalSize: layersSize,
+			Items:     images,
 		}
 	}
 	return du, nil
