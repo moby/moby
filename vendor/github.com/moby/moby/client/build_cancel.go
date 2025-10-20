5cc1e5f800c86e90b4ffff940cf666721280f7d1
Paweł Gronowski <pawel.gronowski@docker.com>
2025-10-20 20:14:49 +0200

    client/build_cancel: Wrap options and result
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- vendor/github.com/moby/moby/client/build_cancel.go
+++ vendor/github.com/moby/moby/client/build_cancel.go
@@ -7,13 +7,15 @@ import (
 
 type BuildCancelOptions struct{}
 
+type BuildCancelResult struct{}
+
 // BuildCancel requests the daemon to cancel the ongoing build request
 // with the given id.
-func (cli *Client) BuildCancel(ctx context.Context, id string, _ BuildCancelOptions) error {
+func (cli *Client) BuildCancel(ctx context.Context, id string, _ BuildCancelOptions) (BuildCancelResult, error) {
 	query := url.Values{}
 	query.Set("id", id)
 
 	resp, err := cli.post(ctx, "/build/cancel", query, nil, nil)
 	defer ensureReaderClosed(resp)
-	return err
+	return BuildCancelResult{}, err
 }
