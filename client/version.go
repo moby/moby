3d28cb8bbd724a849d23a2651e77515bd31c58b8
Sebastiaan van Stijn <github@gone.nl>
2025-10-31 01:21:10 +0100

    client: ServerVersionResult: add back legacy fields
    Short term solution; we need to decide whether these should be kept
    or removed.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/version.go
+++ client/version.go
@@ -17,11 +17,26 @@ type ServerVersionResult struct {
 	// Platform is the platform (product name) the server is running on.
 	Platform PlatformInfo
 
+	// Version is the version of the daemon.
+	Version string
+
 	// APIVersion is the highest API version supported by the server.
 	APIVersion string
 
 	// MinAPIVersion is the minimum API version the server supports.
 	MinAPIVersion string
+
+	// Os is the operating system the server runs on.
+	Os string
+
+	// Arch is the hardware architecture the server runs on.
+	Arch string
+
+	// Experimental indicates that the daemon runs with experimental
+	// features enabled.
+	//
+	// Deprecated: this field will be removed in the next version.
+	Experimental bool
 
 	// Components contains version information for the components making
 	// up the server. Information in this field is for informational
@@ -55,8 +70,12 @@ func (cli *Client) ServerVersion(ctx context.Context, _ ServerVersionOptions) (S
 		Platform: PlatformInfo{
 			Name: v.Platform.Name,
 		},
+		Version:       v.Version,
 		APIVersion:    v.APIVersion,
 		MinAPIVersion: v.MinAPIVersion,
+		Os:            v.Os,
+		Arch:          v.Arch,
+		Experimental:  v.Experimental, //nolint:staticcheck // ignore deprecated field.
 		Components:    v.Components,
 	}, nil
 }
