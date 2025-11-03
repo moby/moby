dc22f2c8bfd1fa034d3330aabc79b26257d7bee6
Sebastiaan van Stijn <github@gone.nl>
2025-11-03 18:30:25 +0100

    client: make ImageSaveResult an interface
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/image_save_opts.go
+++ client/image_save_opts.go
@@ -2,8 +2,6 @@ package client
 
 import (
 	"fmt"
-	"io"
-	"sync"
 
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
@@ -37,35 +35,4 @@ type imageSaveOptions struct {
 	// Platforms selects the platforms to save if the image is a
 	// multi-platform image and has multiple variants.
 	Platforms []ocispec.Platform
-}
-
-func newImageSaveResult(rc io.ReadCloser) ImageSaveResult {
-	if rc == nil {
-		panic("nil io.ReadCloser")
-	}
-	return ImageSaveResult{
-		rc:    rc,
-		close: sync.OnceValue(rc.Close),
-	}
-}
-
-type ImageSaveResult struct {
-	rc    io.ReadCloser
-	close func() error
-}
-
-// Read implements io.ReadCloser
-func (r ImageSaveResult) Read(p []byte) (n int, err error) {
-	if r.rc == nil {
-		return 0, io.EOF
-	}
-	return r.rc.Read(p)
-}
-
-// Close implements io.ReadCloser
-func (r ImageSaveResult) Close() error {
-	if r.close == nil {
-		return nil
-	}
-	return r.close()
 }
