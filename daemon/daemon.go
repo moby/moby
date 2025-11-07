moby/moby

commit a69abdd90db3fc471c975d7cd9146d3c5ec6d30b

api/types/system: add type specific usage fields to `DiskUsage`
This change adds type specific fields to `GET /system/df` endpoint with high level information of disk usage. This change also introduces `verbose` query to the endpoint so that detailed information is by default excluded unless queried to reduce memory consumption. The previous top level `DiskUsage` fields (`Images`, `Containers`, `Volumes` and `BuildCache`) are now deprecated and kept for backwards compatibility.

Co-authored-by: Claude <noreply@anthropic.com>
Signed-off-by: Austin Vazquez <austin.vazquez@docker.com>

daemon/daemon.go
@@ -34,7 +34,6 @@ import (
 	"github.com/moby/buildkit/util/tracing"
 	"github.com/moby/locker"
 	containertypes "github.com/moby/moby/api/types/container"
-	imagetypes "github.com/moby/moby/api/types/image"
 	networktypes "github.com/moby/moby/api/types/network"
 	registrytypes "github.com/moby/moby/api/types/registry"
 	"github.com/moby/moby/api/types/swarm"
@@ -134,7 +133,7 @@ type Daemon struct {
 	seccompProfilePath string
 
 	usageContainers singleflight.Group[struct{}, *backend.ContainerDiskUsage]
-	usageImages     singleflight.Group[struct{}, []*imagetypes.Summary]
+	usageImages     singleflight.Group[struct{}, *backend.ImageDiskUsage]
 	usageVolumes    singleflight.Group[struct{}, *backend.VolumeDiskUsage]
 	usageLayer      singleflight.Group[struct{}, int64]
 

daemon/disk_usage.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/moby/moby/api/types/container"
-	"github.com/moby/moby/api/types/image"
 	"github.com/moby/moby/v2/daemon/internal/filters"
 	"github.com/moby/moby/v2/daemon/server/backend"
 	"github.com/moby/moby/v2/daemon/server/imagebackend"
@@ -15,7 +14,7 @@ import (
 
 // containerDiskUsage obtains information about container data disk usage
 // and makes sure that only one calculation is performed at the same time.
-func (daemon *Daemon) containerDiskUsage(ctx context.Context) (*backend.ContainerDiskUsage, error) {
+func (daemon *Daemon) containerDiskUsage(ctx context.Context, verbose bool) (*backend.ContainerDiskUsage, error) {
 	res, _, err := daemon.usageContainers.Do(ctx, struct{}{}, func(ctx context.Context) (*backend.ContainerDiskUsage, error) {
 		// Retrieve container list
 		containers, err := daemon.Containers(ctx, &backend.ContainerListOptions{
@@ -38,13 +37,23 @@ func (daemon *Daemon) containerDiskUsage(ctx context.Context) (*backend.Containe
 				ctr.State == container.StateRestarting
 		}
 
-		du := &backend.ContainerDiskUsage{Items: containers}
-		for _, ctr := range du.Items {
+		activeCount := int64(len(containers))
+
+		du := &backend.ContainerDiskUsage{TotalCount: activeCount}
+		for _, ctr := range containers {
 			du.TotalSize += ctr.SizeRw
 			if !isActive(ctr) {
 				du.Reclaimable += ctr.SizeRw
+				activeCount--
 			}
 		}
+
+		du.ActiveCount = activeCount
+
+		if verbose {
+			du.Items = containers
+		}
+
 		return du, nil
 	})
 	return res, err
@@ -52,52 +61,81 @@ func (daemon *Daemon) containerDiskUsage(ctx context.Context) (*backend.Containe
 
 // imageDiskUsage obtains information about image data disk usage from image service
 // and makes sure that only one calculation is performed at the same time.
-func (daemon *Daemon) imageDiskUsage(ctx context.Context) ([]*image.Summary, error) {
-	imgs, _, err := daemon.usageImages.Do(ctx, struct{}{}, func(ctx context.Context) ([]*image.Summary, error) {
+func (daemon *Daemon) imageDiskUsage(ctx context.Context, verbose bool) (*backend.ImageDiskUsage, error) {
+	du, _, err := daemon.usageImages.Do(ctx, struct{}{}, func(ctx context.Context) (*backend.ImageDiskUsage, error) {
 		// Get all top images with extra attributes
-		imgs, err := daemon.imageService.Images(ctx, imagebackend.ListOptions{
+		images, err := daemon.imageService.Images(ctx, imagebackend.ListOptions{
 			Filters:    filters.NewArgs(),
 			SharedSize: true,
 		})
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to retrieve image list")
 		}
-		return imgs, nil
+
+		reclaimable, _, err := daemon.usageLayer.Do(ctx, struct{}{}, func(ctx context.Context) (int64, error) {
+			return daemon.imageService.ImageDiskUsage(ctx)
+		})
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to calculate image disk usage")
+		}
+
+		activeCount := int64(len(images))
+
+		du := &backend.ImageDiskUsage{TotalCount: activeCount, TotalSize: reclaimable}
+		for _, i := range images {
+			if i.Containers == 0 {
+				activeCount--
+				if i.Size == -1 || i.SharedSize == -1 {
+					continue
+				}
+				reclaimable -= i.Size - i.SharedSize
+			}
+		}
+
+		du.Reclaimable = reclaimable
+		du.ActiveCount = activeCount
+
+		if verbose {
+			du.Items = images
+		}
+
+		return du, nil
 	})
 
-	return imgs, err
+	return du, err
 }
 
 // localVolumesSize obtains information about volume disk usage from volumes service
 // and makes sure that only one size calculation is performed at the same time.
-func (daemon *Daemon) localVolumesSize(ctx context.Context) (*backend.VolumeDiskUsage, error) {
+func (daemon *Daemon) localVolumesSize(ctx context.Context, verbose bool) (*backend.VolumeDiskUsage, error) {
 	volumes, _, err := daemon.usageVolumes.Do(ctx, struct{}{}, func(ctx context.Context) (*backend.VolumeDiskUsage, error) {
 		volumes, err := daemon.volumes.LocalVolumesSize(ctx)
 		if err != nil {
 			return nil, err
 		}
 
-		du := &backend.VolumeDiskUsage{Items: volumes}
-		for _, v := range du.Items {
+		activeCount := int64(len(volumes))
+
+		du := &backend.VolumeDiskUsage{TotalCount: activeCount}
+		for _, v := range volumes {
 			if v.UsageData.Size != -1 {
 				if v.UsageData.RefCount == 0 {
 					du.Reclaimable += v.UsageData.Size
+					activeCount--
 				}
 				du.TotalSize += v.UsageData.Size
 			}
 		}
+
+		du.ActiveCount = activeCount
+
+		if verbose {
+			du.Items = volumes
+		}
+
 		return du, nil
 	})
 	return volumes, err
-}
-
-// layerDiskUsage obtains information about layer disk usage from image service
-// and makes sure that only one size calculation is performed at the same time.
-func (daemon *Daemon) layerDiskUsage(ctx context.Context) (int64, error) {
-	usage, _, err := daemon.usageLayer.Do(ctx, struct{}{}, func(ctx context.Context) (usage int64, err error) {
-		return daemon.imageService.ImageDiskUsage(ctx)
-	})
-	return usage, err
 }
 
 // SystemDiskUsage returns information about the daemon data disk usage.
@@ -108,29 +146,21 @@ func (daemon *Daemon) SystemDiskUsage(ctx context.Context, opts backend.DiskUsag
 	du := &backend.DiskUsage{}
 	if opts.Containers {
 		eg.Go(func() (err error) {
-			du.Containers, err = daemon.containerDiskUsage(ctx)
+			du.Containers, err = daemon.containerDiskUsage(ctx, opts.Verbose)
 			return err
 		})
 	}
 
-	var (
-		layersSize int64
-		images     []*image.Summary
-	)
 	if opts.Images {
 		eg.Go(func() (err error) {
-			images, err = daemon.imageDiskUsage(ctx)
-			return err
-		})
-		eg.Go(func() (err error) {
-			layersSize, err = daemon.layerDiskUsage(ctx)
+			du.Images, err = daemon.imageDiskUsage(ctx, opts.Verbose)
 			return err
 		})
 	}
 
 	if opts.Volumes {
 		eg.Go(func() (err error) {
-			du.Volumes, err = daemon.localVolumesSize(ctx)
+			du.Volumes, err = daemon.localVolumesSize(ctx, opts.Verbose)
 			return err
 		})
 	}
@@ -139,22 +169,5 @@ func (daemon *Daemon) SystemDiskUsage(ctx context.Context, opts backend.DiskUsag
 		return nil, err
 	}
 
-	if opts.Images {
-		reclaimable := layersSize
-		for _, i := range images {
-			if i.Containers != 0 {
-				if i.Size == -1 || i.SharedSize == -1 {
-					continue
-				}
-				reclaimable -= i.Size - i.SharedSize
-			}
-		}
-
-		du.Images = &backend.ImageDiskUsage{
-			TotalSize:   layersSize,
-			Reclaimable: reclaimable,
-			Items:       images,
-		}
-	}
 	return du, nil
 }

api/types/system/disk_usage.go
@@ -24,9 +24,27 @@ const (
 // DiskUsage contains response of Engine API:
 // GET "/system/df"
 type DiskUsage struct {
-	LayersSize int64
-	Images     []*image.Summary
-	Containers []*container.Summary
-	Volumes    []*volume.Volume
-	BuildCache []*build.CacheRecord
+	LegacyDiskUsage
+
+	ImageUsage      *ImagesDiskUsage     `json:"ImageUsage,omitempty"`
+	ContainerUsage  *ContainersDiskUsage `json:"ContainerUsage,omitempty"`
+	VolumeUsage     *VolumesDiskUsage    `json:"VolumeUsage,omitempty"`
+	BuildCacheUsage *BuildCacheDiskUsage `json:"BuildCacheUsage,omitempty"`
+}
+
+type LegacyDiskUsage struct {
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [ImagesDiskUsage.TotalSize] instead.
+	LayersSize int64 `json:"LayersSize,omitempty"`
+
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [ImagesDiskUsage.Items] instead.
+	Images []*image.Summary `json:"Images,omitempty"`
+
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [ContainersDiskUsage.Items] instead.
+	Containers []*container.Summary `json:"Containers,omitempty"`
+
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [VolumesDiskUsage.Items] instead.
+	Volumes []*volume.Volume `json:"Volumes,omitempty"`
+
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [BuildCacheDiskUsage.Items] instead.
+	BuildCache []*build.CacheRecord `json:"BuildCache,omitempty"`
 }

daemon/server/backend/disk_usage.go
@@ -17,6 +17,9 @@ type DiskUsageOptions struct {
 
 	// Volumes controls whether volume disk usage should be computed.
 	Volumes bool
+
+	// Verbose indicates whether to include detailed information.
+	Verbose bool
 }
 
 // DiskUsage contains the information returned by the backend for the
@@ -30,6 +33,8 @@ type DiskUsage struct {
 
 // BuildCacheDiskUsage contains disk usage for the build cache.
 type BuildCacheDiskUsage struct {
+	ActiveCount int64
+	TotalCount  int64
 	TotalSize   int64
 	Reclaimable int64
 	Items       []*build.CacheRecord
@@ -37,6 +42,8 @@ type BuildCacheDiskUsage struct {
 
 // ContainerDiskUsage contains disk usage for containers.
 type ContainerDiskUsage struct {
+	ActiveCount int64
+	TotalCount  int64
 	TotalSize   int64
 	Reclaimable int64
 	Items       []*container.Summary
@@ -44,6 +51,8 @@ type ContainerDiskUsage struct {
 
 // ImageDiskUsage contains disk usage for images.
 type ImageDiskUsage struct {
+	ActiveCount int64
+	TotalCount  int64
 	TotalSize   int64
 	Reclaimable int64
 	Items       []*image.Summary
@@ -51,6 +60,8 @@ type ImageDiskUsage struct {
 
 // VolumeDiskUsage contains disk usage for volumes.
 type VolumeDiskUsage struct {
+	ActiveCount int64
+	TotalCount  int64
 	TotalSize   int64
 	Reclaimable int64
 	Items       []*volume.Volume

vendor/github.com/moby/moby/api/types/system/disk_usage.go
@@ -24,9 +24,27 @@ const (
 // DiskUsage contains response of Engine API:
 // GET "/system/df"
 type DiskUsage struct {
-	LayersSize int64
-	Images     []*image.Summary
-	Containers []*container.Summary
-	Volumes    []*volume.Volume
-	BuildCache []*build.CacheRecord
+	LegacyDiskUsage
+
+	ImageUsage      *ImagesDiskUsage     `json:"ImageUsage,omitempty"`
+	ContainerUsage  *ContainersDiskUsage `json:"ContainerUsage,omitempty"`
+	VolumeUsage     *VolumesDiskUsage    `json:"VolumeUsage,omitempty"`
+	BuildCacheUsage *BuildCacheDiskUsage `json:"BuildCacheUsage,omitempty"`
+}
+
+type LegacyDiskUsage struct {
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [ImagesDiskUsage.TotalSize] instead.
+	LayersSize int64 `json:"LayersSize,omitempty"`
+
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [ImagesDiskUsage.Items] instead.
+	Images []*image.Summary `json:"Images,omitempty"`
+
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [ContainersDiskUsage.Items] instead.
+	Containers []*container.Summary `json:"Containers,omitempty"`
+
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [VolumesDiskUsage.Items] instead.
+	Volumes []*volume.Volume `json:"Volumes,omitempty"`
+
+	// Deprecated: kept to maintain backwards compatibility with API < v1.52, use [BuildCacheDiskUsage.Items] instead.
+	BuildCache []*build.CacheRecord `json:"BuildCache,omitempty"`
 }

api/docs/v1.52.yaml
@@ -2059,6 +2059,47 @@ definitions:
         x-nullable: true
         $ref: "#/definitions/OCIDescriptor"
 
+  ImagesDiskUsage:
+    type: "object"
+    description: |
+      ImagesDiskUsage represents system data usage for image resources.
+    properties:
+      ActiveImages:
+        description: |
+          Count of active images.
+        type: "integer"
+        format: "int64"
+        example: 1
+      TotalImages:
+        description: |
+          Count of all images.
+        type: "integer"
+        format: "int64"
+        example: 4
+      Reclaimable:
+        description: |
+          Disk space that can be reclaimed by removing unused images.
+        type: "integer"
+        format: "int64"
+        example: 12345678
+      TotalSize:
+        description: |
+          Disk space in use by images.
+        type: "integer"
+        format: "int64"
+        example: 98765432
+      Items:
+        description: |
+          List of image summaries.
+        type: "array"
+        x-omitempty: true
+        items:
+          x-nullable: true
+          x-go-type:
+            type: Summary
+            import:
+              package: github.com/moby/moby/api/types/image
+
   AuthConfig:
     type: "object"
     properties:
@@ -2200,6 +2241,47 @@ definitions:
               The number of containers referencing this volume. This field
               is set to `-1` if the reference-count is not available.
             x-nullable: false
+
+  VolumesDiskUsage:
+    type: "object"
+    description: |
+      VolumesDiskUsage represents system data usage for volume resources.
+    properties:
+      ActiveVolumes:
+        description: |
+          Count of active volumes.
+        type: "integer"
+        format: "int64"
+        example: 1
+      TotalVolumes:
+        description: |
+          Count of all volumes.
+        type: "integer"
+        format: "int64"
+        example: 4
+      Reclaimable:
+        description: |
+          Disk space that can be reclaimed by removing inactive volumes.
+        type: "integer"
+        format: "int64"
+        example: 12345678
+      TotalSize:
+        description: |
+          Disk space in use by volumes.
+        type: "integer"
+        format: "int64"
+        example: 98765432
+      Items:
+        description: |
+          List of volumes.
+        type: "array"
+        x-omitempty: true
+        items:
+          x-nullable: true
+          x-go-type:
+            type: Volume
+            import:
+              package: github.com/moby/moby/api/types/volume
 
   VolumeCreateRequest:
     description: "Volume configuration"
@@ -2628,6 +2710,8 @@ definitions:
         type: "string"
         x-omitempty: false
         example: "02:42:ac:13:00:02"
+        x-go-type:
+          type: HardwareAddr
       IPv4Address:
         type: "string"
         x-omitempty: false
@@ -2772,6 +2856,47 @@ definitions:
         type: "integer"
         example: 26
 
+  BuildCacheDiskUsage:
+    type: "object"
+    description: |
+      BuildCacheDiskUsage represents system data usage for build cache resources.
+    properties:
+      ActiveBuildCacheRecords:
+        description: |
+          Count of active build cache records.
+        type: "integer"
+        format: "int64"
+        example: 1
+      TotalBuildCacheRecords:
+        description: |
+          Count of all build cache records.
+        type: "integer"
+        format: "int64"
+        example: 4
+      Reclaimable:
+        description: |
+          Disk space that can be reclaimed by removing inactive build cache records.
+        type: "integer"
+        format: "int64"
+        example: 12345678
+      TotalSize:
+        description: |
+          Disk space in use by build cache records.
+        type: "integer"
+        format: "int64"
+        example: 98765432
+      Items:
+        description: |
+          List of build cache records.
+        type: "array"
+        x-omitempty: true
+        items:
+          x-nullable: true
+          x-go-type:
+            type: CacheRecord
+            import:
+              package: github.com/moby/moby/api/types/build
+
   ImageID:
     type: "object"
     description: "Image ID or Digest"
@@ -2914,6 +3039,8 @@ definitions:
           MAC address for the endpoint on this network. The network driver might ignore this parameter.
         type: "string"
         example: "02:42:ac:11:00:04"
+        x-go-type:
+          type: HardwareAddr
       Aliases:
         type: "array"
         items:
@@ -5452,6 +5579,47 @@ definitions:
             description: "FailingStreak is the number of consecutive failures"
             type: "integer"
             example: 0
+
+  ContainersDiskUsage:
+    type: "object"
+    description: |
+      ContainersDiskUsage provides system data usage information for container resources.
+    properties:
+      ActiveContainers:
+        description: |
+          Count of active containers.
+        type: "integer"
+        format: "int64"
+        example: 1
+      TotalContainers:
+        description: |
+          Count of all containers.
+        type: "integer"
+        format: "int64"
+        example: 4
+      Reclaimable:
+        description: |
+          Disk space that can be reclaimed by removing inactive containers.
+        type: "integer"
+        format: "int64"
+        example: 12345678
+      TotalSize:
+        description: |
+          Disk space in use by containers.
+        type: "integer"
+        format: "int64"
+        example: 98765432
+      Items:
+        description: |
+          List of container summaries.
+        type: "array"
+        x-omitempty: true
+        items:
+          x-nullable: true
+          x-go-type:
+            type: Summary
+            import:
+              package: github.com/moby/moby/api/types/container
 
   Driver:
     description: "Driver represents a driver (network, logging, secrets)."
@@ -10332,106 +10500,14 @@ paths:
             type: "object"
             title: "SystemDataUsageResponse"
             properties:
-              LayersSize:
-                type: "integer"
-                format: "int64"
-              Images:
-                type: "array"
-                items:
-                  $ref: "#/definitions/ImageSummary"
-              Containers:
-                type: "array"
-                items:
-                  $ref: "#/definitions/ContainerSummary"
-              Volumes:
-                type: "array"
-                items:
-                  $ref: "#/definitions/Volume"
-              BuildCache:
-                type: "array"
-                items:
-                  $ref: "#/definitions/BuildCache"
-            example:
-              LayersSize: 1092588
-              Images:
-                -
-                  Id: "sha256:2b8fd9751c4c0f5dd266fcae00707e67a2545ef34f9a29354585f93dac906749"
-                  ParentId: ""
-                  RepoTags:
-                    - "busybox:latest"
-                  RepoDigests:
-                    - "busybox@sha256:a59906e33509d14c036c8678d687bd4eec81ed7c4b8ce907b888c607f6a1e0e6"
-                  Created: 1466724217
-                  Size: 1092588
-                  SharedSize: 0
-                  Labels: {}
-                  Containers: 1
-              Containers:
-                -
-                  Id: "e575172ed11dc01bfce087fb27bee502db149e1a0fad7c296ad300bbff178148"
-                  Names:
-                    - "/top"
-                  Image: "busybox"
-                  ImageID: "sha256:2b8fd9751c4c0f5dd266fcae00707e67a2545ef34f9a29354585f93dac906749"
-                  Command: "top"
-                  Created: 1472592424
-                  Ports: []
-                  SizeRootFs: 1092588
-                  Labels: {}
-                  State: "exited"
-                  Status: "Exited (0) 56 minutes ago"
-                  HostConfig:
-                    NetworkMode: "default"
-                  NetworkSettings:
-                    Networks:
-                      bridge:
-                        IPAMConfig: null
-                        Links: null
-                        Aliases: null
-                        NetworkID: "d687bc59335f0e5c9ee8193e5612e8aee000c8c62ea170cfb99c098f95899d92"
-                        EndpointID: "8ed5115aeaad9abb174f68dcf135b49f11daf597678315231a32ca28441dec6a"
-                        Gateway: "172.18.0.1"
-                        IPAddress: "172.18.0.2"
-                        IPPrefixLen: 16
-                        IPv6Gateway: ""
-                        GlobalIPv6Address: ""
-                        GlobalIPv6PrefixLen: 0
-                        MacAddress: "02:42:ac:12:00:02"
-                  Mounts: []
-              Volumes:
-                -
-                  Name: "my-volume"
-                  Driver: "local"
-                  Mountpoint: "/var/lib/docker/volumes/my-volume/_data"
-                  Labels: null
-                  Scope: "local"
-                  Options: null
-                  UsageData:
-                    Size: 10920104
-                    RefCount: 2
-              BuildCache:
-                -
-                  ID: "hw53o5aio51xtltp5xjp8v7fx"
-                  Parents: []
-                  Type: "regular"
-                  Description: "pulled from docker.io/library/debian@sha256:234cb88d3020898631af0ccbbcca9a66ae7306ecd30c9720690858c1b007d2a0"
-                  InUse: false
-                  Shared: true
-                  Size: 0
-                  CreatedAt: "2021-06-28T13:31:01.474619385Z"
-                  LastUsedAt: "2021-07-07T22:02:32.738075951Z"
-                  UsageCount: 26
-                -
-                  ID: "ndlpt0hhvkqcdfkputsk4cq9c"
-                  Parents: ["ndlpt0hhvkqcdfkputsk4cq9c"]
-                  Type: "regular"
-                  Description: "mount / from exec /bin/sh -c echo 'Binary::apt::APT::Keep-Downloaded-Packages \"true\";' > /etc/apt/apt.conf.d/keep-cache"
-                  InUse: false
-                  Shared: true
-                  Size: 51
-                  CreatedAt: "2021-06-28T13:31:03.002625487Z"
-                  LastUsedAt: "2021-07-07T22:02:32.773909517Z"
-                  UsageCount: 26
+              ImagesDiskUsage:
+                $ref: "#/definitions/ImagesDiskUsage"
+              ContainersDiskUsage:
+                $ref: "#/definitions/ContainersDiskUsage"
+              VolumesDiskUsage:
+                $ref: "#/definitions/VolumesDiskUsage"
+              BuildCacheDiskUsage:
+                $ref: "#/definitions/BuildCacheDiskUsage"
         500:
           description: "server error"
           schema:
@@ -10446,6 +10522,12 @@ paths:
           items:
             type: "string"
             enum: ["container", "image", "volume", "build-cache"]
+        - name: "verbose"
+          in: "query"
+          description: |
+            Show detailed information on space usage.
+          type: "boolean"
+          default: false
       tags: ["System"]
   /images/{name}/get:
     get:

commit fb94e241f5e5b1e6e2663a6ea55991ad8e201bc0

daemon: disk-usage: fix incorrect key for singleflight
commit a69abdd90db3fc471c975d7cd9146d3c5ec6d30b introduced a "verbose"
option for the disk-usage endpoint, which allowed omitting the items
to be included in the results.

However, it did not take into account that a singleflight is used to
allow sharing the results between requests; this means that a request
made while another request is already in flight could share the wrong
results, and either get "verbose" or "non-verbose", depending on the
request already in flight.

Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

daemon/daemon.go
@@ -131,9 +131,9 @@ type Daemon struct {
 	seccompProfile     []byte
 	seccompProfilePath string
 
-	usageContainers singleflight.Group[struct{}, *backend.ContainerDiskUsage]
-	usageImages     singleflight.Group[struct{}, *backend.ImageDiskUsage]
-	usageVolumes    singleflight.Group[struct{}, *backend.VolumeDiskUsage]
+	usageContainers singleflight.Group[bool, *backend.ContainerDiskUsage]
+	usageImages     singleflight.Group[bool, *backend.ImageDiskUsage]
+	usageVolumes    singleflight.Group[bool, *backend.VolumeDiskUsage]
 	usageLayer      singleflight.Group[struct{}, int64]
 
 	pruneRunning atomic.Bool

daemon/disk_usage.go
@@ -16,7 +16,7 @@ import (
 // containerDiskUsage obtains information about container data disk usage
 // and makes sure that only one calculation is performed at the same time.
 func (daemon *Daemon) containerDiskUsage(ctx context.Context, verbose bool) (*backend.ContainerDiskUsage, error) {
-	res, _, err := daemon.usageContainers.Do(ctx, struct{}{}, func(ctx context.Context) (*backend.ContainerDiskUsage, error) {
+	res, _, err := daemon.usageContainers.Do(ctx, verbose, func(ctx context.Context) (*backend.ContainerDiskUsage, error) {
 		// Retrieve container list
 		containers, err := daemon.Containers(ctx, &backend.ContainerListOptions{
 			Size: true,
@@ -60,7 +60,7 @@ func (daemon *Daemon) containerDiskUsage(ctx context.Context, verbose bool) (*ba
 // imageDiskUsage obtains information about image data disk usage from image service
 // and makes sure that only one calculation is performed at the same time.
 func (daemon *Daemon) imageDiskUsage(ctx context.Context, verbose bool) (*backend.ImageDiskUsage, error) {
-	du, _, err := daemon.usageImages.Do(ctx, struct{}{}, func(ctx context.Context) (*backend.ImageDiskUsage, error) {
+	du, _, err := daemon.usageImages.Do(ctx, verbose, func(ctx context.Context) (*backend.ImageDiskUsage, error) {
 		// Get all top images with extra attributes
 		images, err := daemon.imageService.Images(ctx, imagebackend.ListOptions{
 			Filters:    filters.NewArgs(),
@@ -106,7 +106,7 @@ func (daemon *Daemon) imageDiskUsage(ctx context.Context, verbose bool) (*backen
 // localVolumesSize obtains information about volume disk usage from volumes service
 // and makes sure that only one size calculation is performed at the same time.
 func (daemon *Daemon) localVolumesSize(ctx context.Context, verbose bool) (*backend.VolumeDiskUsage, error) {
-	volumes, _, err := daemon.usageVolumes.Do(ctx, struct{}{}, func(ctx context.Context) (*backend.VolumeDiskUsage, error) {
+	volumes, _, err := daemon.usageVolumes.Do(ctx, verbose, func(ctx context.Context) (*backend.VolumeDiskUsage, error) {
 		volumes, err := daemon.volumes.LocalVolumesSize(ctx)
 		if err != nil {
 			return nil, err