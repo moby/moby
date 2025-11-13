4535d63c91bd7d2d2767346965f24ec5e6e5f28d
Cory Snider <csnider@mirantis.com>
2025-11-13 14:41:12 -0500

    daemon: install OpenCensus-to-OTEL trace bridge
    Export trace spans from the github.com/microsoft/hcsshim module, which
    is instrumented with OpenCensus, to the daemon's OpenTelemetry exporter
    to provide more visibility into Windows container lifecycle operations.
    
    Signed-off-by: Cory Snider <csnider@mirantis.com>

---
+++ vendor/go.opentelemetry.io/otel/bridge/opencensus/version.go
@@ -0,0 +1,9 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package opencensus // import "go.opentelemetry.io/otel/bridge/opencensus"
+
+// Version is the current release version of the opencensus bridge.
+func Version() string {
+	return "1.38.0"
+}
