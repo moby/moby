a5dec0a779a86514786d876bfc211478f10a6cc0
Sebastiaan van Stijn <github@gone.nl>
2025-11-13 15:38:28 +0100

    client: make sure context is canceled for ContainerWait tests
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/container_wait_test.go
+++ client/container_wait_test.go
@@ -19,9 +19,12 @@ import (
 )
 
 func TestContainerWaitError(t *testing.T) {
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+
 	client, err := New(WithMockClient(errorMock(http.StatusInternalServerError, "Server error")))
 	assert.NilError(t, err)
-	wait := client.ContainerWait(t.Context(), "nothing", ContainerWaitOptions{})
+	wait := client.ContainerWait(ctx, "nothing", ContainerWaitOptions{})
 	select {
 	case result := <-wait.Result:
 		t.Fatalf("expected to not get a wait result, got %d", result.StatusCode)
@@ -35,10 +38,13 @@ func TestContainerWaitError(t *testing.T) {
 //
 // Regression test for https://github.com/docker/cli/issues/4890
 func TestContainerWaitConnectionError(t *testing.T) {
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+
 	client, err := New(WithAPIVersionNegotiation(), WithHost("tcp://no-such-host.invalid"))
 	assert.NilError(t, err)
 
-	wait := client.ContainerWait(t.Context(), "nothing", ContainerWaitOptions{})
+	wait := client.ContainerWait(ctx, "nothing", ContainerWaitOptions{})
 	select {
 	case result := <-wait.Result:
 		t.Fatalf("expected to not get a wait result, got %d", result.StatusCode)
@@ -48,6 +54,9 @@ func TestContainerWaitConnectionError(t *testing.T) {
 }
 
 func TestContainerWait(t *testing.T) {
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+
 	const expectedURL = "/containers/container_id/wait"
 	client, err := New(WithMockClient(func(req *http.Request) (*http.Response, error) {
 		if err := assertRequest(req, http.MethodPost, expectedURL); err != nil {
@@ -59,7 +68,7 @@ func TestContainerWait(t *testing.T) {
 	}))
 	assert.NilError(t, err)
 
-	wait := client.ContainerWait(t.Context(), "container_id", ContainerWaitOptions{})
+	wait := client.ContainerWait(ctx, "container_id", ContainerWaitOptions{})
 	select {
 	case err := <-wait.Error:
 		assert.NilError(t, err)
@@ -69,6 +78,9 @@ func TestContainerWait(t *testing.T) {
 }
 
 func TestContainerWaitProxyInterrupt(t *testing.T) {
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+
 	const (
 		expectedURL = "/containers/container_id/wait"
 		expErr      = "copying response body from Docker: unexpected EOF"
@@ -82,16 +94,19 @@ func TestContainerWaitProxyInterrupt(t *testing.T) {
 	}))
 	assert.NilError(t, err)
 
-	wait := client.ContainerWait(t.Context(), "container_id", ContainerWaitOptions{})
+	wait := client.ContainerWait(ctx, "container_id", ContainerWaitOptions{})
 	select {
 	case err := <-wait.Error:
 		assert.Check(t, is.ErrorContains(err, expErr))
 	case result := <-wait.Result:
-		t.Fatalf("Unexpected result: %v", result)
+		t.Errorf("Unexpected result: %v", result)
 	}
 }
 
 func TestContainerWaitProxyInterruptLong(t *testing.T) {
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+
 	const expectedURL = "/containers/container_id/wait"
 	msg := strings.Repeat("x", containerWaitErrorMsgLimit*5)
 	client, err := New(WithMockClient(func(req *http.Request) (*http.Response, error) {
@@ -102,13 +117,13 @@ func TestContainerWaitProxyInterruptLong(t *testing.T) {
 	}))
 	assert.NilError(t, err)
 
-	wait := client.ContainerWait(t.Context(), "container_id", ContainerWaitOptions{})
+	wait := client.ContainerWait(ctx, "container_id", ContainerWaitOptions{})
 	select {
 	case err := <-wait.Error:
 		// LimitReader limiting isn't exact, because of how the Readers do chunking.
 		assert.Check(t, len(err.Error()) <= containerWaitErrorMsgLimit*2, "Expected error to be limited around %d, actual length: %d", containerWaitErrorMsgLimit, len(err.Error()))
 	case result := <-wait.Result:
-		t.Fatalf("Unexpected result: %v", result)
+		t.Errorf("Unexpected result: %v", result)
 	}
 }
 
@@ -140,7 +155,7 @@ func TestContainerWaitErrorHandling(t *testing.T) {
 				assert.Check(t, is.Equal(err.Error(), test.exp.Error()))
 				return
 			case result := <-wait.Result:
-				t.Fatalf("expected to not get a wait result, got %d", result.StatusCode)
+				t.Errorf("expected to not get a wait result, got %d", result.StatusCode)
 				return
 			}
 			// Unexpected - we should not reach this line
