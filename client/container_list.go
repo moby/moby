d9cead55b2867efeb7a77e9fe041f3a699da2d3e
Sebastiaan van Stijn <github@gone.nl>
2026-01-23 14:45:24 +0100

    client: deprecate ContainerListOptions.Since, ContainerListOptions.Before
    The `Before` and `Since` options were used for the `before` and `since`
    query parameters. These parameters were replaced with the `before` and
    `since` filters in docker 1.10.0 (API 1.22)through [moby@1921c62], and
    [moby@b41dba5].
    
    The query parameters were removed from the CLI and daemon in docker 1.12.0
    (API 1.24) through [moby@91b7157], however the fields themselves were never
    removed.
    
    This patch deprecates the fields as they are non-functional.
    
    [moby@1921c62]: https://github.com/moby/moby/commit/1921c629381d25ebff7b8b8c8348a0a81525f264
    [moby@b41dba5]: https://github.com/moby/moby/commit/b41dba58a01855e0d222bd6c14af9874c298c154
    [moby@91b7157]: https://github.com/moby/moby/commit/91b715706493059d39c9f8b1d01c9d80ce1799d9
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/container_list.go
+++ client/container_list.go
@@ -14,10 +14,18 @@ type ContainerListOptions struct {
 	Size    bool
 	All     bool
 	Latest  bool
-	Since   string
-	Before  string
 	Limit   int
 	Filters Filters
+
+	// Since is no longer supported. Use the "since" filter instead.
+	//
+	// Deprecated: the Since option is no longer supported since docker 1.12 (API 1.24). Use the "since" filter instead.
+	Since string
+
+	// Before is no longer supported. Use the "since" filter instead.
+	//
+	// Deprecated: the Before option is no longer supported since docker 1.12 (API 1.24). Use the "before" filter instead.
+	Before string
 }
 
 type ContainerListResult struct {
@@ -34,14 +42,6 @@ func (cli *Client) ContainerList(ctx context.Context, options ContainerListOptio
 
 	if options.Limit > 0 {
 		query.Set("limit", strconv.Itoa(options.Limit))
-	}
-
-	if options.Since != "" {
-		query.Set("since", options.Since)
-	}
-
-	if options.Before != "" {
-		query.Set("before", options.Before)
 	}
 
 	if options.Size {
