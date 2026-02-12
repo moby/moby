ffd43fa27af525f5ea782ee77ea3d70bd86b4ece
Sebastiaan van Stijn <github@gone.nl>
2026-02-12 15:58:11 +0100

    use /usr/src/moby instead, and no GOPATH
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- internal/testutil/fixtures/plugin/plugin.go
+++ internal/testutil/fixtures/plugin/plugin.go
@@ -225,10 +225,9 @@ func ensureBasicPluginBin() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	installPath := filepath.Join(os.Getenv("GOPATH"), "bin", name)
-	sourcePath := filepath.Join("github.com", "moby", "moby", "v2", "internal", "testutil", "fixtures", "plugin", "basic")
-	cmd := exec.Command(goBin, "build", "-o", installPath, sourcePath)
-	cmd.Env = append(os.Environ(), "CGO_ENABLED=0", "GO111MODULE=off")
+	installPath := filepath.Join(os.Getenv("GOBIN"), name)
+	cmd := exec.Command(goBin, "build", "-buildvcs=false", "-o", installPath, "github.com/moby/moby/v2/internal/testutil/fixtures/plugin/basic")
+	cmd.Env = append(os.Environ(), "CGO_ENABLED=0", "GO111MODULE=auto")
 	if out, err := cmd.CombinedOutput(); err != nil {
 		return "", errors.Wrapf(err, "error building basic plugin bin: %s", string(out))
 	}
