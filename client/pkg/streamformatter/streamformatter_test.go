cf4d504c29edd6ba7045206400f0fcaf266f5dfc
Sebastiaan van Stijn <github@gone.nl>
2026-02-19 01:03:41 +0100

    client/pkg/streamformatter: fix faulty TestNewJSONProgressOutput
    This  test was added in 956c5b61cea9e476847b9d2231ac66d3ec85f018, but
    wasn't really testing anything (other than testing stdlib).
    
    Ironically there was a review comment on the PR, but about the naming
    of the test, and not about the test itself;
    
    > but it isn't really testing NewJSONProgressOutput. NewJSONProgressOutput
    > just returns a struct. IMHO it's testing WriteProgress.
    
    Let's make the test test something.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/pkg/streamformatter/streamformatter_test.go
+++ client/pkg/streamformatter/streamformatter_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/moby/moby/api/types/jsonstream"
+	"github.com/moby/moby/client/pkg/progress"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
 )
@@ -80,9 +81,50 @@ func TestJsonProgressFormatterFormatStatus(t *testing.T) {
 	assert.Check(t, is.Equal(`{"status":"a1","id":"ID"}`+streamNewline, string(res)))
 }
 
-func TestNewJSONProgressOutput(t *testing.T) {
-	b := bytes.Buffer{}
-	b.Write(formatStatus("id", "Downloading"))
-	_ = NewJSONProgressOutput(&b, false)
-	assert.Check(t, is.Equal(`{"status":"Downloading","id":"id"}`+streamNewline, b.String()))
+func TestJSONProgressOutputWriteProgress(t *testing.T) {
+	tests := []struct {
+		doc        string
+		newlines   bool
+		lastUpdate bool
+		expected   string
+	}{
+		{
+			doc:      "no newlines",
+			expected: `{"status":"Downloading","id":"id"}` + streamNewline,
+		},
+		{
+			doc:        "no newlines last update",
+			lastUpdate: true,
+			expected:   `{"status":"Downloading","id":"id"}` + streamNewline,
+		},
+		{
+			doc:        "newlines",
+			newlines:   true,
+			lastUpdate: false,
+			expected:   `{"status":"Downloading","id":"id"}` + streamNewline,
+		},
+		{
+			// Should print an extra (empty) message to add newlines after last message
+			// (LastUpdate=true); see https://github.com/moby/moby/pull/1425
+			doc:        "newlines last update",
+			newlines:   true,
+			lastUpdate: true,
+			expected:   `{"status":"Downloading","id":"id"}` + streamNewline + `{}` + streamNewline,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.doc, func(t *testing.T) {
+			var b bytes.Buffer
+			po := NewJSONProgressOutput(&b, tc.newlines)
+
+			err := po.WriteProgress(progress.Progress{
+				ID:         "id",
+				Message:    "Downloading",
+				LastUpdate: tc.lastUpdate,
+			})
+			assert.NilError(t, err)
+			assert.Check(t, is.Equal(b.String(), tc.expected))
+		})
+	}
 }
