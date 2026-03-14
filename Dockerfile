AkihiroSuda/docker

Dockerfile: go install: use Git SourceOp
Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

Dockerfile
@@ -91,11 +91,12 @@ ARG TARGETPLATFORM
 # Go-swagger is used in CI for generating types from swagger.yaml in
 # hack/validate/swagger-gen
 ARG GO_SWAGGER_VERSION=v0.33.1
+ADD https://github.com/go-swagger/go-swagger.git?tag=${GO_SWAGGER_VERSION}&keep-git-dir=1 /go/src/github.com/go-swagger/go-swagger
 RUN --mount=type=cache,target=/root/.cache/go-build,id=swagger-build-$TARGETPLATFORM \
-    --mount=type=cache,target=/go/pkg/mod \
-    --mount=type=tmpfs,target=/go/src/ <<EOT
+    --mount=type=cache,target=/go/pkg/mod <<EOT
   set -e
-  GOBIN=/build CGO_ENABLED=0 xx-go install "github.com/go-swagger/go-swagger/cmd/swagger@${GO_SWAGGER_VERSION}"
+  cd /go/src/github.com/go-swagger/go-swagger
+  GOBIN=/build CGO_ENABLED=0 xx-go install ./cmd/swagger
   xx-verify /build/swagger
 EOT
 
@@ -149,9 +150,11 @@ FROM delve-${DELVE_SUPPORTED} AS delve
 FROM base AS gowinres
 # GOWINRES_VERSION defines go-winres tool version
 ARG GOWINRES_VERSION=v0.3.1
+ADD https://github.com/tc-hib/go-winres.git?tag=${GOWINRES_VERSION}&keep-git-dir=1 /go/src/github.com/tc-hib/go-winres
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "github.com/tc-hib/go-winres@${GOWINRES_VERSION}" \
+        cd /go/src/github.com/tc-hib/go-winres && \
+        GOBIN=/build CGO_ENABLED=0 go install . \
      && /build/go-winres --help
 
 # containerd
@@ -193,30 +196,39 @@ FROM containerd-${TARGETOS} AS containerd
 
 FROM base AS golangci_lint
 ARG GOLANGCI_LINT_VERSION=v2.1.5
+ADD https://github.com/golangci/golangci-lint.git?tag=${GOLANGCI_LINT_VERSION}&keep-git-dir=1 /go/src/github.com/golangci/golangci-lint
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "github.com/golangci/golangci-lint/v2/cmd/golangci-lint@${GOLANGCI_LINT_VERSION}" \
+        cd /go/src/github.com/golangci/golangci-lint && \
+        GOBIN=/build CGO_ENABLED=0 go install ./cmd/golangci-lint \
      && /build/golangci-lint --version
 
 FROM base AS gotestsum
 # GOTESTSUM_VERSION is the version of gotest.tools/gotestsum to install.
 ARG GOTESTSUM_VERSION=v1.13.0
+ADD https://github.com/gotestyourself/gotestsum.git?tag=${GOTESTSUM_VERSION}&keep-git-dir=1 /go/src/gotest.tools/gotestsum
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "gotest.tools/gotestsum@${GOTESTSUM_VERSION}" \
+        cd /go/src/gotest.tools/gotestsum && \
+        GOBIN=/build CGO_ENABLED=0 go install . \
      && /build/gotestsum --version
 
 FROM base AS shfmt
 ARG SHFMT_VERSION=v3.8.0
+ADD https://github.com/mvdan/sh.git?tag=${SHFMT_VERSION}&keep-git-dir=1 /go/src/mvdan.cc/sh
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "mvdan.cc/sh/v3/cmd/shfmt@${SHFMT_VERSION}" \
+        cd /go/src/mvdan.cc/sh && \
+        GOBIN=/build CGO_ENABLED=0 go install ./cmd/shfmt \
      && /build/shfmt --version
 
 FROM base AS gopls
+ARG GOPLS_VERSION=v0.20.0
+ADD https://github.com/golang/tools.git?tag=gopls/${GOPLS_VERSION}&keep-git-dir=1 /go/src/golang.org/x/tools
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "golang.org/x/tools/gopls@latest" \
+        cd /go/src/golang.org/x/tools/gopls && \
+        GOBIN=/build CGO_ENABLED=0 go install . \
      && /build/gopls version
 
 FROM base AS dockercli

moby/moby

Dockerfile
@@ -134,9 +134,11 @@ FROM delve-${DELVE_SUPPORTED} AS delve
 FROM base AS gowinres
 # GOWINRES_VERSION defines go-winres tool version
 ARG GOWINRES_VERSION=v0.3.1
+ADD https://github.com/tc-hib/go-winres.git?ref=${GOWINRES_VERSION}&keep-git-dir=1 /go/src/github.com/tc-hib/go-winres
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "github.com/tc-hib/go-winres@${GOWINRES_VERSION}" \
+        cd /go/src/github.com/tc-hib/go-winres && \
+        GOBIN=/build CGO_ENABLED=0 go install . \
      && /build/go-winres --help
 
 # containerd
@@ -178,27 +180,34 @@ FROM containerd-${TARGETOS} AS containerd
 
 FROM base AS golangci_lint
 ARG GOLANGCI_LINT_VERSION=v2.8.0
+ADD https://github.com/golangci/golangci-lint.git?ref=${GOLANGCI_LINT_VERSION}&keep-git-dir=1 /go/src/github.com/golangci/golangci-lint
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "github.com/golangci/golangci-lint/v2/cmd/golangci-lint@${GOLANGCI_LINT_VERSION}" \
+        cd /go/src/github.com/golangci/golangci-lint && \
+        GOBIN=/build CGO_ENABLED=0 go install ./cmd/golangci-lint \
      && /build/golangci-lint --version
 
 FROM base AS gotestsum
 # GOTESTSUM_VERSION is the version of gotest.tools/gotestsum to install.
 ARG GOTESTSUM_VERSION=v1.13.0
+ADD https://github.com/gotestyourself/gotestsum.git?ref=${GOTESTSUM_VERSION}&keep-git-dir=1 /go/src/gotest.tools/gotestsum
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "gotest.tools/gotestsum@${GOTESTSUM_VERSION}" \
+        cd /go/src/gotest.tools/gotestsum && \
+        GOBIN=/build CGO_ENABLED=0 go install . \
      && /build/gotestsum --version
 
 FROM base AS shfmt
 ARG SHFMT_VERSION=v3.8.0
+ADD https://github.com/mvdan/sh.git?ref=${SHFMT_VERSION}&keep-git-dir=1 /go/src/mvdan.cc/sh
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
-        GOBIN=/build CGO_ENABLED=0 go install "mvdan.cc/sh/v3/cmd/shfmt@${SHFMT_VERSION}" \
+        cd /go/src/mvdan.cc/sh && \
+        GOBIN=/build CGO_ENABLED=0 go install ./cmd/shfmt \
      && /build/shfmt --version
 
 FROM base AS gopls
+# No ARG GOPLS_VERSION, as gopls is only used for devcontainer
 RUN --mount=type=cache,target=/root/.cache/go-build \
     --mount=type=cache,target=/go/pkg/mod \
         GOBIN=/build CGO_ENABLED=0 go install "golang.org/x/tools/gopls@latest" \