9dced9771408916e2d7e574419a9e2fa2e6084d7
Sebastiaan van Stijn <github@gone.nl>
2026-04-11 16:29:02 +0200

    daemon/logger/loggerutils: ParseLogTag: improve test-coverage
    - add consts for well-known templating options
    - improve test coverage, and use more realistic values in tests
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/logger/loggerutils/log_tag.go
+++ daemon/logger/loggerutils/log_tag.go
@@ -10,6 +10,17 @@ import (
 // DefaultTemplate defines the defaults template logger should use.
 const DefaultTemplate = "{{.ID}}"
 
+const (
+	ctrShortID = "{{.ID}}"
+	ctrFullID  = "{{.FullID}}"
+	ctrName    = "{{.Name}}"
+	ctrCommand = "{{.Command}}"
+	imgShortID = "{{.ImageID}}"
+	imgFullID  = "{{.ImageFullID}}"
+	imgName    = "{{.ImageName}}"
+	hostName   = "{{.Hostname}}"
+)
+
 // ParseLogTag generates a context aware tag for consistency across different
 // log drivers based on the context of the running container.
 func ParseLogTag(info logger.Info, defaultTemplate string) (string, error) {
