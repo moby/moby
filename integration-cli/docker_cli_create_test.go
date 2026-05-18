docker/docker

testing: refactor uses of "getPrefixAndSlashFromDaemonPlatform()"
Implement a `dPath()` utility function that converts paths to Windows paths
(add `c:` drive-letter, and convert to backslashes) if the daemon is running
on Windows.

Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

integration-cli/docker_cli_create_test.go
@@ -168,12 +168,11 @@ func (s *DockerSuite) TestCreateEchoStdout(c *testing.T) {
 
 func (s *DockerSuite) TestCreateVolumesCreated(c *testing.T) {
 	testRequires(c, testEnv.IsLocalDaemon)
-	prefix, slash := getPrefixAndSlashFromDaemonPlatform()
 
 	name := "test_create_volume"
-	dockerCmd(c, "create", "--name", name, "-v", prefix+slash+"foo", "busybox")
+	dockerCmd(c, "create", "--name", name, "-v", dPath("/foo"), "busybox")
 
-	dir, err := inspectMountSourceField(name, prefix+slash+"foo")
+	dir, err := inspectMountSourceField(name, dPath("/foo"))
 	assert.Assert(c, err == nil, "Error getting volume host path: %q", err)
 
 	if _, err := os.Stat(dir); err != nil && os.IsNotExist(err) {
@@ -295,9 +294,7 @@ func (s *DockerSuite) TestCreateStopSignal(c *testing.T) {
 
 func (s *DockerSuite) TestCreateWithWorkdir(c *testing.T) {
 	name := "foo"
-
-	prefix, slash := getPrefixAndSlashFromDaemonPlatform()
-	dir := prefix + slash + "home" + slash + "foo" + slash + "bar"
+	dir := dPath("/home/foo/bar")
 
 	dockerCmd(c, "create", "--name", name, "-w", dir, "busybox")
 	// Windows does not create the workdir until the container is started
@@ -312,7 +309,7 @@ func (s *DockerSuite) TestCreateWithWorkdir(c *testing.T) {
 		}
 	}
 	// TODO: rewrite this test to not use `docker cp` for verifying that the WORKDIR was created
-	dockerCmd(c, "cp", fmt.Sprintf("%s:%s", name, dir), prefix+slash+"tmp")
+	dockerCmd(c, "cp", fmt.Sprintf("%s:%s", name, dir), dPath("/tmp"))
 }
 
 func (s *DockerSuite) TestCreateWithInvalidLogOpts(c *testing.T) {

integration-cli/docker_cli_restart_test.go
@@ -54,8 +54,7 @@ func (s *DockerSuite) TestRestartRunningContainer(c *testing.T) {
 
 // Test that restarting a container with a volume does not create a new volume on restart. Regression test for #819.
 func (s *DockerSuite) TestRestartWithVolumes(c *testing.T) {
-	prefix, slash := getPrefixAndSlashFromDaemonPlatform()
-	out := runSleepingContainer(c, "-d", "-v", prefix+slash+"test")
+	out := runSleepingContainer(c, "-d", "-v", dPath("/test"))
 
 	cleanedContainerID := strings.TrimSpace(out)
 	out, err := inspectFilter(cleanedContainerID, "len .Mounts")
@@ -63,7 +62,7 @@ func (s *DockerSuite) TestRestartWithVolumes(c *testing.T) {
 	out = strings.Trim(out, " \n\r")
 	assert.Equal(c, out, "1")
 
-	source, err := inspectMountSourceField(cleanedContainerID, prefix+slash+"test")
+	source, err := inspectMountSourceField(cleanedContainerID, dPath("/test"))
 	assert.NilError(c, err)
 
 	dockerCmd(c, "restart", cleanedContainerID)
@@ -73,7 +72,7 @@ func (s *DockerSuite) TestRestartWithVolumes(c *testing.T) {
 	out = strings.Trim(out, " \n\r")
 	assert.Equal(c, out, "1")
 
-	sourceAfterRestart, err := inspectMountSourceField(cleanedContainerID, prefix+slash+"test")
+	sourceAfterRestart, err := inspectMountSourceField(cleanedContainerID, dPath("/test"))
 	assert.NilError(c, err)
 	assert.Equal(c, source, sourceAfterRestart)
 }

moby/moby

integration-cli/docker_cli_create_test.go
@@ -147,12 +147,11 @@ func (s *DockerCLICreateSuite) TestCreateEchoStdout(c *testing.T) {
 
 func (s *DockerCLICreateSuite) TestCreateVolumesCreated(c *testing.T) {
 	testRequires(c, testEnv.IsLocalDaemon)
-	prefix, slash := getPrefixAndSlashFromDaemonPlatform()
 
 	const name = "test_create_volume"
-	cli.DockerCmd(c, "create", "--name", name, "-v", prefix+slash+"foo", "busybox")
+	cli.DockerCmd(c, "create", "--name", name, "-v", dPath("/foo"), "busybox")
 
-	mnt, err := inspectMountPoint(name, prefix+slash+"foo")
+	mnt, err := inspectMountPoint(name, dPath("/foo"))
 	assert.Assert(c, err == nil, "Error getting volume host path: %q", err)
 
 	if _, err := os.Stat(mnt.Source); err != nil && os.IsNotExist(err) {
@@ -239,8 +238,7 @@ func (s *DockerCLICreateSuite) TestCreateStopSignal(c *testing.T) {
 func (s *DockerCLICreateSuite) TestCreateWithWorkdir(c *testing.T) {
 	const name = "foo"
 
-	prefix, slash := getPrefixAndSlashFromDaemonPlatform()
-	dir := prefix + slash + "home" + slash + "foo" + slash + "bar"
+	dir := dPath("/home/foo/bar")
 
 	cli.DockerCmd(c, "create", "--name", name, "-w", dir, "busybox")
 	// Windows does not create the workdir until the container is started
@@ -255,7 +253,7 @@ func (s *DockerCLICreateSuite) TestCreateWithWorkdir(c *testing.T) {
 		}
 	}
 	// TODO: rewrite this test to not use `docker cp` for verifying that the WORKDIR was created
-	cli.DockerCmd(c, "cp", fmt.Sprintf("%s:%s", name, dir), prefix+slash+"tmp")
+	cli.DockerCmd(c, "cp", fmt.Sprintf("%s:%s", name, dir), dPath("/tmp"))
 }
 
 func (s *DockerCLICreateSuite) TestCreateWithInvalidLogOpts(c *testing.T) {

integration-cli/docker_cli_restart_test.go
@@ -69,14 +69,13 @@ func (s *DockerCLIRestartSuite) TestRestartRunningContainer(c *testing.T) {
 
 // Test that restarting a container with a volume does not create a new volume on restart. Regression test for #819.
 func (s *DockerCLIRestartSuite) TestRestartWithVolumes(c *testing.T) {
-	prefix, slash := getPrefixAndSlashFromDaemonPlatform()
-	cID := runSleepingContainer(c, "-d", "-v", prefix+slash+"test")
+	cID := runSleepingContainer(c, "-d", "-v", dPath("/test"))
 	out, err := inspectFilter(cID, "len .Mounts")
 	assert.NilError(c, err, "failed to inspect %s: %s", cID, out)
 	out = strings.Trim(out, " \n\r")
 	assert.Equal(c, out, "1")
 
-	mnt, err := inspectMountPoint(cID, prefix+slash+"test")
+	mnt, err := inspectMountPoint(cID, dPath("/test"))
 	assert.NilError(c, err)
 
 	cli.DockerCmd(c, "restart", cID)
@@ -86,7 +85,7 @@ func (s *DockerCLIRestartSuite) TestRestartWithVolumes(c *testing.T) {
 	out = strings.Trim(out, " \n\r")
 	assert.Equal(c, out, "1")
 
-	mountAfterRestart, err := inspectMountPoint(cID, prefix+slash+"test")
+	mountAfterRestart, err := inspectMountPoint(cID, dPath("/test"))
 	assert.NilError(c, err)
 	assert.Equal(c, mnt.Source, mountAfterRestart.Source)
 }