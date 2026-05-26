ee9556b36285a729fb0afcb6581bd61ebd382191
Paweł Gronowski <pawel.gronowski@docker.com>
2026-05-26 16:12:16 +0200

    libnetwork: include darwin in existing build tags
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/libnetwork/osl/interface_unsupported.go
+++ daemon/libnetwork/osl/interface_unsupported.go
@@ -2,4 +2,14 @@
 
 package osl
 
+import "time"
+
 type Interface struct{}
+
+const (
+	AdvertiseAddrNMsgsMin = 0
+	AdvertiseAddrNMsgsMax = 3
+
+	AdvertiseAddrIntervalMin = 100 * time.Millisecond
+	AdvertiseAddrIntervalMax = 2 * time.Second
+)
--- daemon/libnetwork/service_unsupported.go
+++ daemon/libnetwork/service_unsupported.go
@@ -3,20 +3,33 @@
 package libnetwork
 
 import (
-	"errors"
 	"net"
+
+	"github.com/moby/moby/v2/errdefs"
 )
 
 func (c *Controller) cleanupServiceDiscovery(cleanupNID string) {}
 
 func (c *Controller) cleanupServiceBindings(nid string) {}
 
-func (c *Controller) addServiceBinding(name, sid, nid, eid string, vip net.IP, ingressPorts []*PortConfig, aliases []string, ip net.IP) error {
-	return errors.New("not supported")
+func (c *Controller) getLBIndex(_, _ string, _ []*PortConfig) int {
+	return 0
 }
 
-func (c *Controller) rmServiceBinding(name, sid, nid, eid string, vip net.IP, ingressPorts []*PortConfig, aliases []string, ip net.IP) error {
-	return errors.New("not supported")
+func (c *Controller) addContainerNameResolution(nID, eID, containerName string, taskAliases []string, ip net.IP, method string) error {
+	return errdefs.PlatformNotImplemented{Feature: "Controller.addContainerNameResolution"}
+}
+
+func (c *Controller) delContainerNameResolution(nID, eID, containerName string, taskAliases []string, ip net.IP, method string) error {
+	return errdefs.PlatformNotImplemented{Feature: "Controller.delContainerNameResolution"}
+}
+
+func (c *Controller) addServiceBinding(svcName, svcID, nID, eID, containerName string, vip net.IP, ingressPorts []*PortConfig, serviceAliases, taskAliases []string, ip net.IP, method string) error {
+	return errdefs.PlatformNotImplemented{Feature: "Controller.addServiceBinding"}
+}
+
+func (c *Controller) rmServiceBinding(svcName, svcID, nID, eID, containerName string, vip net.IP, ingressPorts []*PortConfig, serviceAliases []string, taskAliases []string, ip net.IP, method string, deleteSvcRecords bool, fullRemove bool) error {
+	return errdefs.PlatformNotImplemented{Feature: "Controller.rmServiceBinding"}
 }
 
 func (sb *Sandbox) populateLoadBalancers(*Endpoint) {}
