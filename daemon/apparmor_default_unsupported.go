1835affa3e2034ba74550b7557c144288f217ebb
Paweł Gronowski <pawel.gronowski@docker.com>
2026-05-26 16:12:20 +0200

    daemon: define AppArmor profile constants on !linux
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/apparmor_default_unsupported.go
+++ daemon/apparmor_default_unsupported.go
@@ -2,6 +2,14 @@
 
 package daemon
 
+// AppArmor profile names referenced by shared Unix container setup code.
+// AppArmor is only supported on Linux; on other platforms these are
+// unused at runtime but must exist for shared code paths to compile.
+const (
+	unconfinedAppArmorProfile = "unconfined"
+	defaultAppArmorProfile    = "docker-default"
+)
+
 func loadDefaultAppArmorProfileIfMissing() error {
 	return nil
 }
