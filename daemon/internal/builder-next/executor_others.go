6640b9c0b5fe350ce9f5b71c9172f21d89e4b43b
Paweł Gronowski <pawel.gronowski@docker.com>
2026-05-26 16:12:18 +0200

    builder-next: add bridge provider stubs on !linux !windows
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- daemon/internal/builder-next/executor_others.go
+++ daemon/internal/builder-next/executor_others.go
@@ -4,8 +4,18 @@ package buildkit
 
 import (
 	"github.com/moby/buildkit/executor"
+	"github.com/opencontainers/runtime-spec/specs-go"
 )
+
+// networkName is unused on unsupported platforms but referenced by the shared
+// bridgeProvider in executor.go.
+const networkName = ""
 
 func newExecutor(executorOpts) (executor.Executor, error) {
 	return &stubExecutor{}, nil
+}
+
+func (iface *lnInterface) Set(*specs.Spec) error {
+	<-iface.ready
+	return iface.err
 }
