b26075028d2820c44651277abc4ccaca80749ca6
Sebastiaan van Stijn <github@gone.nl>
2026-07-09 16:10:56 +0200

    integration/internal/build: rewrite GetImageIDFromBody with DisplayStream
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- integration/internal/build/build.go
+++ integration/internal/build/build.go
@@ -1,7 +1,6 @@
 package build
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"io"
@@ -15,7 +14,6 @@ import (
 	"github.com/moby/moby/client/pkg/jsonmessage"
 	"github.com/moby/moby/v2/internal/testutil/fakecontext"
 	"gotest.tools/v3/assert"
-	is "gotest.tools/v3/assert/cmp"
 )
 
 // Do builds an image from the given context with the supplied options
@@ -35,86 +33,81 @@ func Do(ctx context.Context, t *testing.T, apiClient client.APIClient, buildCtx 
 
 // GetImageIDFromBody reads the image ID from the build response body.
 func GetImageIDFromBody(t *testing.T, body io.Reader) string {
-	var id string
-	buf := bytes.NewBuffer(nil)
-	dec := json.NewDecoder(body)
-	for {
-		var jm jsonstream.Message
-		err := dec.Decode(&jm)
-		if err == io.EOF {
-			break
-		}
-		assert.NilError(t, err)
+	t.Helper()
 
-		if handled := processBuildkitAux(t, &jm, &id); handled {
-			continue
-		}
-
-		buf.Reset()
-		jsonmessage.Display(jm, buf, false, 0)
-		if buf.Len() == 0 {
-			continue
-		}
-
-		if jm.Error != nil {
-			assert.Assert(t, is.Equal(jm.Error, ""))
-		}
-
-		t.Log(buf.String())
-
-		if jm.Aux == nil {
-			continue
-		}
-
-		var br build.Result
-		if err := json.Unmarshal(*jm.Aux, &br); err == nil {
-			if br.ID == "" {
-				continue
-			}
-			id = br.ID
-			continue
-		}
-
-		t.Log("Raw Aux", string(*jm.Aux))
+	// Buffer log output from DisplayStream callbacks. Calling t.Log from inside the
+	// callbacks would attribute the output to jsonmessage rather than the test,
+	// despite this function being marked with t.Helper().
+	var logs []string
+	Log := func(msg string) {
+		logs = append(logs, msg)
 	}
-	_, _ = io.Copy(io.Discard, body)
 
+	var id string
+	err := jsonmessage.DisplayStream(body, io.Discard,
+		jsonmessage.WithAuxCallback(func(jm jsonstream.Message) {
+			t.Helper()
+
+			switch jm.ID {
+			case "moby.buildkit.trace":
+				var dt []byte
+				if err := json.Unmarshal(*jm.Aux, &dt); err != nil {
+					Log("Error unmarshalling buildkit trace " + err.Error())
+					return
+				}
+
+				var sr controlapi.StatusResponse
+				if err := proto.Unmarshal(dt, &sr); err != nil {
+					Log("Error unmarshalling buildkit trace proto" + err.Error())
+					return
+				}
+
+				for _, vtx := range sr.GetVertexes() {
+					Log(vtx.String())
+				}
+				for _, vtx := range sr.GetStatuses() {
+					Log(vtx.String())
+				}
+				for _, vtx := range sr.GetLogs() {
+					Log(vtx.String())
+				}
+				for _, vtx := range sr.GetWarnings() {
+					Log(vtx.String())
+				}
+
+			case "moby.image.id":
+				fallthrough
+			default:
+				var br build.Result
+				if err := json.Unmarshal(*jm.Aux, &br); err == nil && br.ID != "" {
+					id = br.ID
+					return
+				}
+				Log("Raw Aux " + string(*jm.Aux))
+			}
+		}),
+		jsonmessage.WithMessagePrinter(func(jm jsonstream.Message, _ io.Writer) error {
+			if jm.Error != nil {
+				return jm.Error
+			}
+			if jm.Progress != nil && (jm.Progress.Current > 0 || jm.Progress.Total > 0) {
+				return nil
+			}
+			msg := jm.Stream
+			if msg == "" {
+				msg = jm.Status
+			}
+			if jm.ID != "" {
+				msg = jm.ID + ": " + msg
+			}
+			Log(msg)
+			return nil
+		}),
+	)
+	for _, msg := range logs {
+		t.Log(msg)
+	}
+	assert.NilError(t, err)
 	assert.Assert(t, id != "", "could not read image ID from build output")
 	return id
-}
-
-func processBuildkitAux(t *testing.T, jm *jsonstream.Message, id *string) bool {
-	if jm.ID == "moby.buildkit.trace" {
-		var dt []byte
-		if err := json.Unmarshal(*jm.Aux, &dt); err != nil {
-			t.Log("Error unmarshalling buildkit trace", err)
-			return true
-		}
-		var sr controlapi.StatusResponse
-		if err := proto.Unmarshal(dt, &sr); err != nil {
-			t.Log("Error unmarshalling buildkit trace proto", err)
-			return true
-		}
-		for _, vtx := range sr.GetVertexes() {
-			t.Log(vtx.String())
-		}
-		for _, vtx := range sr.GetStatuses() {
-			t.Log(vtx.String())
-		}
-		for _, vtx := range sr.GetLogs() {
-			t.Log(vtx.String())
-		}
-		for _, vtx := range sr.GetWarnings() {
-			t.Log(vtx.String())
-		}
-		return true
-	}
-	if jm.ID == "moby.image.id" {
-		var br build.Result
-		if err := json.Unmarshal(*jm.Aux, &br); err == nil {
-			*id = br.ID
-			return true
-		}
-	}
-	return false
 }
