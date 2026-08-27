7b0abcda8085ceb58e400164e2718df10020d028
Sebastiaan van Stijn <github@gone.nl>
2026-08-27 18:14:35 +0200

    syslog: run formatter tests in synctest
    Avoid tests being flaky due to time differences;
    
    ```
    === Failed
    === FAIL: daemon/logger/syslog/internal/srslog TestDefaultFormatter (0.10s)
        formatter_test.go:16: expected <3> 2026-08-27T11:13:08Z hostname tag[6652]: content got <3> 2026-08-27T11:13:07Z hostname tag[6652]: content
    ```
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/logger/syslog/internal/srslog/formatter_test.go
+++ daemon/logger/syslog/internal/srslog/formatter_test.go
@@ -5,44 +5,53 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"testing/synctest"
 	"time"
 )
 
 func TestDefaultFormatter(t *testing.T) {
-	out := DefaultFormatter(LOG_ERR, "hostname", "tag", "content")
-	expected := fmt.Sprintf("<%d> %s %s %s[%d]: %s",
-		LOG_ERR, time.Now().Format(time.RFC3339), "hostname", "tag", os.Getpid(), "content")
-	if out != expected {
-		t.Errorf("expected %v got %v", expected, out)
-	}
+	synctest.Test(t, func(t *testing.T) {
+		out := DefaultFormatter(LOG_ERR, "hostname", "tag", "content")
+		expected := fmt.Sprintf("<%d> %s %s %s[%d]: %s",
+			LOG_ERR, time.Now().Format(time.RFC3339), "hostname", "tag", os.Getpid(), "content")
+		if out != expected {
+			t.Errorf("expected %v got %v", expected, out)
+		}
+	})
 }
 
 func TestUnixFormatter(t *testing.T) {
-	out := UnixFormatter(LOG_ERR, "hostname", "tag", "content")
-	expected := fmt.Sprintf("<%d>%s %s[%d]: %s",
-		LOG_ERR, time.Now().Format(time.Stamp), "tag", os.Getpid(), "content")
-	if out != expected {
-		t.Errorf("expected %v got %v", expected, out)
-	}
+	synctest.Test(t, func(t *testing.T) {
+		out := UnixFormatter(LOG_ERR, "hostname", "tag", "content")
+		expected := fmt.Sprintf("<%d>%s %s[%d]: %s",
+			LOG_ERR, time.Now().Format(time.Stamp), "tag", os.Getpid(), "content")
+		if out != expected {
+			t.Errorf("expected %v got %v", expected, out)
+		}
+	})
 }
 
 func TestRFC3164Formatter(t *testing.T) {
-	out := RFC3164Formatter(LOG_ERR, "hostname", "tag", "content")
-	expected := fmt.Sprintf("<%d>%s %s %s[%d]: %s",
-		LOG_ERR, time.Now().Format(time.Stamp), "hostname", "tag", os.Getpid(), "content")
-	if out != expected {
-		t.Errorf("expected %v got %v", expected, out)
-	}
+	synctest.Test(t, func(t *testing.T) {
+		out := RFC3164Formatter(LOG_ERR, "hostname", "tag", "content")
+		expected := fmt.Sprintf("<%d>%s %s %s[%d]: %s",
+			LOG_ERR, time.Now().Format(time.Stamp), "hostname", "tag", os.Getpid(), "content")
+		if out != expected {
+			t.Errorf("expected %v got %v", expected, out)
+		}
+	})
 }
 
 func TestRFC5424Formatter(t *testing.T) {
-	out := RFC5424Formatter(LOG_ERR, "hostname", "tag", "content")
-	expected := fmt.Sprintf("<%d>%d %s %s %s %d %s - %s",
-		LOG_ERR, 1, time.Now().Format(time.RFC3339), "hostname", truncateStartStr(os.Args[0], appNameMaxLength),
-		os.Getpid(), "tag", "content")
-	if out != expected {
-		t.Errorf("expected %v got %v", expected, out)
-	}
+	synctest.Test(t, func(t *testing.T) {
+		out := RFC5424Formatter(LOG_ERR, "hostname", "tag", "content")
+		expected := fmt.Sprintf("<%d>%d %s %s %s %d %s - %s",
+			LOG_ERR, 1, time.Now().Format(time.RFC3339), "hostname", truncateStartStr(os.Args[0], appNameMaxLength),
+			os.Getpid(), "tag", "content")
+		if out != expected {
+			t.Errorf("expected %v got %v", expected, out)
+		}
+	})
 }
 
 func TestTruncateStartStr(t *testing.T) {
