9a1fe28143c6ab26632fe5013490c8756c3eaf45
Sebastiaan van Stijn <github@gone.nl>
2026-09-14 15:39:21 +0200

    client: prevent response hooks from consuming response body
    Response hooks are documented not to read or close the response body, but
    currently receive the original response and can therefore consume or close
    the body before it is returned to the caller.
    
    Pass hooks a shallow copy of the response with an unreadable body whose Close
    method is a no-op. This preserves access to response metadata while preventing
    hooks from affecting the body returned to the caller.
    
    Add coverage to verify that attempts to read the body fail and closing it from
    a hook does not close the underlying response body.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- client/client_options.go
+++ client/client_options.go
@@ -58,7 +58,7 @@ type clientConfig struct {
 	// takes precedence. Either field disables API-version negotiation.
 	envAPIVersion string
 
-	// responseHooks is a list of custom response hooks to call on responses.
+	// responseHooks contains hooks to call on responses.
 	responseHooks []ResponseHook
 
 	// traceOpts is a list of options to configure the tracing span.
@@ -68,7 +68,7 @@ type clientConfig struct {
 // ResponseHook is called for each HTTP response returned by the daemon.
 // Hooks are invoked in the order they were added.
 //
-// Hooks must not read or close resp.Body.
+// The response body cannot be read or closed by the hook.
 type ResponseHook func(*http.Response)
 
 // Opt is a configuration option to initialize a [Client].
@@ -414,8 +414,6 @@ func WithTraceOptions(opts ...otelhttp.Option) Opt {
 // WithHTTPResponseHook adds a [ResponseHook] to the client. ResponseHooks are called
 // for each HTTP response returned by the daemon. Hooks are invoked in the order
 // they were added.
-//
-// Hooks must not read or close resp.Body.
 func WithHTTPResponseHook(h ResponseHook) Opt {
 	return func(c *clientConfig) error {
 		if h == nil {
